a4f7917db4877c8a009793b9fb5242644c8c1c4f
Jan Arne Petersen <jpetersen@uni-bonn.de>
2001-12-23 14:21:19 +0000

    Update Java support.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@203 961051c9-f516-0410-bf72-c9f7e237a7b7

--- samples/swing2.rb
+++ samples/swing2.rb
@@ -1,20 +1,23 @@
 # Import Java packages
-JavaObject.import "java.awt.event"
-JavaObject.import "javax.swing"
+Java.import "java.awt.event"
+Java.import "javax.swing"
 
 # Create a frame with the title "HelloWorldSwing"
-frame = JFrame.new("HelloWorldSwing")
+frame = JFrame.new "HelloWorldSwing"
 
 # Create a button with the text "Klick Me!"
-button = JButton.new("Klick Me!")
+button = JButton.new "Klick Me!"
 
 # Add an action to the button
-button.addActionListener JavaInterface.listener ("ActionListener", "actionPerformed") {|evt| JOptionPane.showMessageDialog NIL, "<html>Hello from JRuby.<br>The button was: <i>" + evt.getActionCommand + "</i>"}
+button.addActionListener ActionListener.actionPerformed { |evt| 
+  JOptionPane.showMessageDialog NIL, 
+    "<html>Hello from JRuby.<br>The button was: <i>#{evt.actionCommand}</i>"
+}
 
 # Add the button to the frame
-frame.getContentPane().add(button)
+frame.contentPane.add button
 
 # Show frame
-frame.setDefaultCloseOperation(JFrame::EXIT_ON_CLOSE)
-frame.pack();
-frame.setVisible(true);
+frame.defaultCloseOperation = JFrame::EXIT_ON_CLOSE
+frame.pack
+frame.visible = true
