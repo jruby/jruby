47cee7ff7370283fedf3ebbf8c7f1cd357dd11c8
Benoit Cerrina <b.cerrina@wanadoo.fr>
2001-12-28 20:44:41 +0000

    uses a more explicit name for the function which sets the file and line of a node
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@220 961051c9-f516-0410-bf72-c9f7e237a7b7

--- org/jruby/nodes/NodeFactory.java
+++ org/jruby/nodes/NodeFactory.java
@@ -48,34 +48,34 @@ public class NodeFactory {
 
 		ph = ruby.getParserHelper();
 	}
-	private Node setLine(Node iNode)
+	private Node setFileNLine(Node iNode)
 	{
 		iNode.setLine(ph.getLine());
 		iNode.setFile(ruby.getSourceFile());
 		return iNode;
 	}
 	public Node newMethod(Node body, int noex) {
-		return setLine(new MethodNode(body, noex));
+		return setFileNLine(new MethodNode(body, noex));
 	}
 
 	public Node newFBody(Node bodyNode, String id, RubyModule origin) {
-		return setLine(new FBodyNode(bodyNode, id, origin));
+		return setFileNLine(new FBodyNode(bodyNode, id, origin));
 	}
 
 	public Node newDefn(String mid, Node a, Node d, int p) {
-		return setLine(new DefnNode(p, mid, newRFunc(a, d)));
+		return setFileNLine(new DefnNode(p, mid, newRFunc(a, d)));
 	}
 
 	public Node newDefs(Node recvNode, String mid, Node a, Node d) {
-		return setLine(new DefsNode(recvNode, mid, newRFunc(a, d)));
+		return setFileNLine(new DefsNode(recvNode, mid, newRFunc(a, d)));
 	}
 
 	public Node newCFunc(Callback cfunc) {
-		return setLine(new CFuncNode(cfunc));
+		return setFileNLine(new CFuncNode(cfunc));
 	}
 
 	public Node newIFunc(Callback cfunc, RubyObject tval) {
-		return setLine(new IFuncNode(cfunc, tval));
+		return setFileNLine(new IFuncNode(cfunc, tval));
 	}
 
 	public ScopeNode newRFunc(Node b1, Node b2) {
@@ -83,87 +83,87 @@ public class NodeFactory {
 	}
 
 	public ScopeNode newScope(Node body) {
-		return (ScopeNode)setLine(new ScopeNode(ph.local_tbl(), null, body));
+		return (ScopeNode)setFileNLine(new ScopeNode(ph.local_tbl(), null, body));
 	}
 
 	public BlockNode newBlock(Node head) {
-		return (BlockNode)setLine(new BlockNode(head));
+		return (BlockNode)setFileNLine(new BlockNode(head));
 	}
 
 	public Node newIf(Node condition, Node body, Node elseBody) {
-		return setLine(new IfNode(condition, body, elseBody));
+		return setFileNLine(new IfNode(condition, body, elseBody));
 	}
 
 	public Node newUnless(Node condition, Node body, Node elseBody) {
-		return setLine(newIf(condition, body, elseBody));
+		return setFileNLine(newIf(condition, body, elseBody));
 	}
 
 	public Node newCase(Node head, Node body) {
-		return setLine(new CaseNode(head, body));
+		return setFileNLine(new CaseNode(head, body));
 	}
 
 	public Node newWhen(Node head, Node body, Node next) {
-		return setLine(new WhenNode(head, body, next));
+		return setFileNLine(new WhenNode(head, body, next));
 	}
 
 	public Node newOptN(Node body) {
-		return setLine(new OptNNode(body));
+		return setFileNLine(new OptNNode(body));
 	}
 
 	public Node newWhile(Node condition, Node body) {
-		return setLine(new WhileNode(condition, body));
+		return setFileNLine(new WhileNode(condition, body));
 	}
 
 	public Node newUntil(Node condition, Node body) {
-		return setLine(new UntilNode(condition, body));
+		return setFileNLine(new UntilNode(condition, body));
 	}
 
 	public Node newFor(Node var, Node iter, Node body) {
-		return setLine(new ForNode(var, body, iter));
+		return setFileNLine(new ForNode(var, body, iter));
 	}
 
 	public Node newIter(Node var, Node iter, Node body) {
-		return setLine(new IterNode(var, body, iter));
+		return setFileNLine(new IterNode(var, body, iter));
 	}
 
 	public Node newBreak() {
-		return setLine(new BreakNode());
+		return setFileNLine(new BreakNode());
 	}
 
 	public Node newNext() {
-		return setLine(new NextNode());
+		return setFileNLine(new NextNode());
 	}
 
 	public Node newRedo() {
-		return setLine(new RedoNode());
+		return setFileNLine(new RedoNode());
 	}
 
 	public Node newRetry() {
-		return setLine(new RetryNode());
+		return setFileNLine(new RetryNode());
 	}
 
 	public Node newBegin(Node body) {
-		return setLine(new BeginNode(body));
+		return setFileNLine(new BeginNode(body));
 	}
 
 	public Node newRescue(Node headNode /*b*/, Node rescueNode, Node elseNode) {
-		return setLine(new RescueNode(headNode, rescueNode, elseNode));
+		return setFileNLine(new RescueNode(headNode, rescueNode, elseNode));
 	}
 
 	public RescueBodyNode newResBody(Node args, Node body, Node head /*n*/) {
-		return (RescueBodyNode)setLine(new RescueBodyNode(head, body, args));
+		return (RescueBodyNode)setFileNLine(new RescueBodyNode(head, body, args));
 	}
 
 	public Node newEnsure(Node head /*b*/, Node ensure) {
-		return setLine(new EnsureNode(head, ensure));
+		return setFileNLine(new EnsureNode(head, ensure));
 	}
 
 	public Node newReturn(Node stts) {
-		return setLine(new ReturnNode(stts));
+		return setFileNLine(new ReturnNode(stts));
 	}
 
 	public Node newYield(Node stts) {
-		return setLine(new YieldNode(stts));
+		return setFileNLine(new YieldNode(stts));
 	}
 
 	public ArrayNode newList(Node head) {
@@ -171,263 +171,263 @@ public class NodeFactory {
 	}
 
 	public ArrayNode newArray(Node head) {
-		return (ArrayNode)setLine(new ArrayNode(head, 1, null));
+		return (ArrayNode)setFileNLine(new ArrayNode(head, 1, null));
 	}
 
 	public ZArrayNode newZArray() {
-		return (ZArrayNode)setLine(new ZArrayNode());
+		return (ZArrayNode)setFileNLine(new ZArrayNode());
 	}
 
 	public Node newHash(Node head)  {
-		return (HashNode)setLine(new HashNode(head));
+		return (HashNode)setFileNLine(new HashNode(head));
 	}
 
 	public Node newNot(Node body)   {
-		return setLine(new NotNode(body));
+		return setFileNLine(new NotNode(body));
 	}
 
 	public Node newMAsgn(Node valueNode, Node argsNode)   {
-		return setLine(new MAsgnNode(valueNode, argsNode));
+		return setFileNLine(new MAsgnNode(valueNode, argsNode));
 	}
 
 	public Node newGAsgn(String vid, Node valueNode) {
-		return setLine(new GAsgnNode(/*vid, */ valueNode, RubyGlobalEntry.getGlobalEntry(ruby, vid)));
+		return setFileNLine(new GAsgnNode(/*vid, */ valueNode, RubyGlobalEntry.getGlobalEntry(ruby, vid)));
 	}
 
 	public Node newLAsgn(String vid, Node valueNode) {
-		return setLine(new LAsgnNode(valueNode, ph.local_cnt(vid)));
+		return setFileNLine(new LAsgnNode(valueNode, ph.local_cnt(vid)));
 	}
 
 	public Node newDAsgn(String vid, Node valueNode) {
-		return setLine(new DAsgnNode(vid, valueNode));
+		return setFileNLine(new DAsgnNode(vid, valueNode));
 	}
 
 	public Node newDAsgnCurr(String vid, Node valueNode) {
-		return setLine(new DAsgnCurrNode(vid, valueNode));
+		return setFileNLine(new DAsgnCurrNode(vid, valueNode));
 	}
 
 	public Node newIAsgn(String vid, Node valueNode) {
-		return setLine(new IAsgnNode(vid, valueNode));
+		return setFileNLine(new IAsgnNode(vid, valueNode));
 	}
 
 	public Node newCDecl(String vid, Node valueNode) {
-		return setLine(new CDeclNode(vid, valueNode));
+		return setFileNLine(new CDeclNode(vid, valueNode));
 	}
 
 	public Node newCVAsgn(String vid, Node valueNode) {
-		return setLine(new CVAsgnNode(vid, valueNode));
+		return setFileNLine(new CVAsgnNode(vid, valueNode));
 	}
 
 	public Node newCVDecl(String vid, Node valueNode) {
-		return setLine(new CVDeclNode(vid, valueNode));
+		return setFileNLine(new CVDeclNode(vid, valueNode));
 	}
 
 	public Node newOpAsgn1(Node recvNode, String mid, Node argsNode) {
-		return setLine(new OpAsgn1Node(recvNode, mid, argsNode));
+		return setFileNLine(new OpAsgn1Node(recvNode, mid, argsNode));
 	}
 
 	public Node newOpAsgn2(Node recvNode, String vid, String mid, Node valueNode) {
-		return setLine(new OpAsgn2Node(recvNode, valueNode, vid, mid));
+		return setFileNLine(new OpAsgn2Node(recvNode, valueNode, vid, mid));
 	}
 
 	/*public NODE newOpAsgn22(RubyId id, RubyId o) {
-	  return setLine(new OpAsgn22Node(id, o, id.toAttrSetId()));
+	  return setFileNLine(new OpAsgn22Node(id, o, id.toAttrSetId()));
 	  }*/
 
 	public Node newOpAsgnOr(Node head, Node valueNode) {
-		return setLine(new OpAsgnOrNode(head, valueNode, null));
+		return setFileNLine(new OpAsgnOrNode(head, valueNode, null));
 	}
 
 	public Node newOpAsgnAnd(Node head, Node valueNode) {
-		return setLine(new OpAsgnAndNode(head, valueNode));
+		return setFileNLine(new OpAsgnAndNode(head, valueNode));
 	}
 
 	public Node newGVar(String id) {
-		return setLine(new GVarNode(RubyGlobalEntry.getGlobalEntry(ruby, id)));
+		return setFileNLine(new GVarNode(RubyGlobalEntry.getGlobalEntry(ruby, id)));
 	}
 
 	public Node newLVar(String vid) {
-		return setLine(new LVarNode(ph.local_cnt(vid)));
+		return setFileNLine(new LVarNode(ph.local_cnt(vid)));
 	}
 
 	public Node newDVar(String vid) {
-		return setLine(new DVarNode(vid));
+		return setFileNLine(new DVarNode(vid));
 	}
 
 	public Node newIVar(String vid) {
-		return setLine(new IVarNode(vid));
+		return setFileNLine(new IVarNode(vid));
 	}
 
 	public Node newConst(String vid) {
-		return setLine(new ConstNode(vid));
+		return setFileNLine(new ConstNode(vid));
 	}
 
 	public Node newCVar(String vid) {
-		return setLine(new CVarNode(vid));
+		return setFileNLine(new CVarNode(vid));
 	}
 
 	public Node newCVar2(String vid) {
-		return setLine(new CVar2Node(vid));
+		return setFileNLine(new CVar2Node(vid));
 	}
 
 	public Node newNthRef(int nth)  {
-		return setLine(new NthRefNode(nth));
+		return setFileNLine(new NthRefNode(nth));
 	}
 
 	public Node newBackRef(int nth) {
-		return setLine(new BackRefNode(nth));
+		return setFileNLine(new BackRefNode(nth));
 	}
 
 	public Node newMatch(Node head) {
-		return setLine(new MatchNode(head));
+		return setFileNLine(new MatchNode(head));
 	}
 
 	public Node newMatch2(Node recv, Node value) {
-		return setLine(new Match2Node(recv, value));
+		return setFileNLine(new Match2Node(recv, value));
 	}
 
 	public Node newMatch3(Node recv, Node value) {
-		return setLine(new Match3Node(recv, value));
+		return setFileNLine(new Match3Node(recv, value));
 	}
 
 	public Node newLit(RubyObject lit) {
-		return setLine(new LitNode(lit));
+		return setFileNLine(new LitNode(lit));
 	}
 
 	public Node newStr(RubyObject str) {
-		return setLine(new StrNode(str));
+		return setFileNLine(new StrNode(str));
 	}
 
 	public Node newDStr(RubyObject str) {
-		return setLine(new DStrNode(str));
+		return setFileNLine(new DStrNode(str));
 	}
 
 	public Node newXStr(RubyObject str) {
-		return setLine(new XStrNode(str));
+		return setFileNLine(new XStrNode(str));
 	}
 
 	public Node newDXStr(RubyObject str) {
-		return setLine(new DXStrNode(str));
+		return setFileNLine(new DXStrNode(str));
 	}
 
 	public Node newEVStr(String s, int len) {
-		return setLine(new EvStrNode(RubyString.newString(ruby, s, len)));
+		return setFileNLine(new EvStrNode(RubyString.newString(ruby, s, len)));
 	}
 
 	public Node newCall(Node recv, String mid, Node args) {
-		return setLine(new CallNode(recv, mid, args));
+		return setFileNLine(new CallNode(recv, mid, args));
 	}
 
 	public Node newFCall(String mid, Node args) {
-		return setLine(new FCallNode(mid, args));
+		return setFileNLine(new FCallNode(mid, args));
 	}
 
 	public Node newVCall(String mid) {
-		return setLine(new VCallNode(mid));
+		return setFileNLine(new VCallNode(mid));
 	}
 
 	public Node newSuper(Node args) {
-		return setLine(new SuperNode(args));
+		return setFileNLine(new SuperNode(args));
 	}
 
 	public Node newZSuper() {
-		return setLine(new ZSuperNode());
+		return setFileNLine(new ZSuperNode());
 	}
 
 	public Node newArgs(Integer count, Node optNode, int rest) {
-		return setLine(new ArgsNode(optNode, rest, count != null ? count.intValue() : 0));
+		return setFileNLine(new ArgsNode(optNode, rest, count != null ? count.intValue() : 0));
 	}
 
 	public Node newArgsCat(Node head /*a*/, Node body) {
-		return setLine(new ArgsCatNode(head, body));
+		return setFileNLine(new ArgsCatNode(head, body));
 	}
 
 	public Node newArgsPush(Node head /*a*/, Node body) {
-		return setLine(new ArgsPushNode(head, body));
+		return setFileNLine(new ArgsPushNode(head, body));
 	}
 
 	public Node newRestArgs(Node head) {
-		return setLine(new RestArgsNode(head));
+		return setFileNLine(new RestArgsNode(head));
 	}
 
 	public Node newBlockArg(String vid) {
-		return setLine(new BlockArgNode(ph.local_cnt(vid)));
+		return setFileNLine(new BlockArgNode(ph.local_cnt(vid)));
 	}
 
 	public Node newBlockPass(Node bodyNode) {
-		return setLine(new BlockPassNode(bodyNode));
+		return setFileNLine(new BlockPassNode(bodyNode));
 	}
 
 	public Node newAlias(String newId, String oldId) {
-		return setLine(new AliasNode(oldId, newId));
+		return setFileNLine(new AliasNode(oldId, newId));
 	}
 
 	public Node newVAlias(String newId, String oldId) {
-		return setLine(new VAliasNode(oldId, newId));
+		return setFileNLine(new VAliasNode(oldId, newId));
 	}
 
 	public Node newUndef(String mid) {
-		return setLine(new UndefNode(mid));
+		return setFileNLine(new UndefNode(mid));
 	}
 
 	public Node newClass(String cnameId, Node body, Node superNode) {
-		return setLine(new ClassNode(cnameId, newScope(body), superNode));
+		return setFileNLine(new ClassNode(cnameId, newScope(body), superNode));
 	}
 
 	public Node newSClass(Node receiverNode, Node body) {
-		return setLine(new SClassNode(receiverNode, newScope(body)));
+		return setFileNLine(new SClassNode(receiverNode, newScope(body)));
 	}
 
 	public Node newModule(String moduleName, Node body) {
-		return setLine(new ModuleNode(moduleName, newScope(body)));
+		return setFileNLine(new ModuleNode(moduleName, newScope(body)));
 	}
 
 	public Node newColon2(Node head, String mid) {
-		return setLine(new Colon2Node(head, mid));
+		return setFileNLine(new Colon2Node(head, mid));
 	}
 
 	public Node newColon3(String mid) {
-		return setLine(new Colon3Node(mid));
+		return setFileNLine(new Colon3Node(mid));
 	}
 
 	/*public Node newCRef(Object c) {
-	  return setLine(new CRefNode(c));
+	  return setFileNLine(new CRefNode(c));
 	  }*/
 
 	public Node newDot2(Node begin, Node end) {
-		return setLine(new DotNode(begin, end, false));
+		return setFileNLine(new DotNode(begin, end, false));
 	}
 
 	public Node newDot3(Node begin, Node end) {
-		return setLine(new DotNode(begin, end, true));
+		return setFileNLine(new DotNode(begin, end, true));
 	}
 
 	public Node newAttrSet(String vid /*a*/) {
-		return setLine(new AttrSetNode(vid));
+		return setFileNLine(new AttrSetNode(vid));
 	}
 
 	public Node newSelf() {
-		return setLine(new SelfNode());
+		return setFileNLine(new SelfNode());
 	}
 
 	public Node newNil() {
-		return setLine(new NilNode());
+		return setFileNLine(new NilNode());
 	}
 
 	public Node newTrue() {
-		return setLine(new TrueNode());
+		return setFileNLine(new TrueNode());
 	}
 
 	public Node newFalse() {
-		return setLine(new FalseNode());
+		return setFileNLine(new FalseNode());
 	}
 
 	public Node newDefined(Node head /* e */) {
-		return setLine(new DefinedNode(head));
+		return setFileNLine(new DefinedNode(head));
 	}
 
 	public NewlineNode newNewline(Node nextNode) {
-		return (NewlineNode)setLine(new NewlineNode(nextNode));
+		return (NewlineNode)setFileNLine(new NewlineNode(nextNode));
 	}
 
 	public Node newPreExe(Node body) {
@@ -435,14 +435,14 @@ public class NodeFactory {
 	}
 
 	public Node newPostExe() {
-		return setLine(new PostExeNode());
+		return setFileNLine(new PostExeNode());
 	}
 
 	/*public Node newDMethod(Object b) {
-	  return setLine(new DMethodNode(b));
+	  return setFileNLine(new DMethodNode(b));
 	  }
 
 	  public Node newBMethod(Object b) {
-	  return setLine(new BMethodNode(b));
+	  return setFileNLine(new BMethodNode(b));
 	  }*/
 }
