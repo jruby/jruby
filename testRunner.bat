a055e8b9cd3b0a370ad5f9c02b3bbd7d17b4ad92
Benoit Cerrina <b.cerrina@wanadoo.fr>
2002-01-11 08:19:51 +0000

    [ #502225 ] improve test support
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@244 961051c9-f516-0410-bf72-c9f7e237a7b7

--- testRunner.bat
+++ testRunner.bat
@@ -1,6 +1,6 @@
 @echo running Java tests:
 
-@java -cp build\jruby.jar;lib\junit.jar junit.textui.TestRunner org.jruby.test.MainTestSuite | ruby -p bin/JAVAException.rb
+@java -cp build\jruby.jar;lib\junit.jar junit.textui.TestRunner org.jruby.test.MainTestSuite
 @echo
 @echo running ruby tests:
 @jruby test/testSuite.rb
