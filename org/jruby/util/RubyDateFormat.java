c25476cf35893b8084b891c3e96a956142dbcd71
Jan Arne Petersen <jpetersen@uni-bonn.de>
2002-05-22 09:21:07 +0000

    Correct support for %w flag.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@398 961051c9-f516-0410-bf72-c9f7e237a7b7

--- org/jruby/util/RubyDateFormat.java
+++ org/jruby/util/RubyDateFormat.java
@@ -33,174 +33,361 @@ import java.text.NumberFormat;
 import java.text.ParsePosition;
 import java.util.*;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
+
+import sun.rmi.runtime.*;
 
 /**
  *
  * @author  jpetersen
  * @version $Revision$
  */
-public class RubyDateFormat extends SimpleDateFormat {
+public class RubyDateFormat extends DateFormat {
+    private String pattern;
+
+    private List compiledPattern;
+
+    private DateFormatSymbols formatSymbols;
+
+    private static final int FORMAT_STRING = 0;
+    private static final int FORMAT_WEEK_LONG = 1;
+    private static final int FORMAT_WEEK_SHORT = 2;
+    private static final int FORMAT_MONTH_LONG = 3;
+    private static final int FORMAT_MONTH_SHORT = 4;
+    private static final int FORMAT_DAY = 5;
+    private static final int FORMAT_HOUR = 6;
+    private static final int FORMAT_HOUR_M = 7;
+    private static final int FORMAT_DAY_YEAR = 8;
+    private static final int FORMAT_MINUTES = 9;
+    private static final int FORMAT_MONTH = 10;
+    private static final int FORMAT_MERIDIAN = 11;
+    private static final int FORMAT_SECONDS = 12;
+    private static final int FORMAT_WEEK_YEAR_S = 13;
+    private static final int FORMAT_WEEK_YEAR_M = 14;
+    private static final int FORMAT_DAY_WEEK = 15;
+    private static final int FORMAT_YEAR_LONG = 16;
+    private static final int FORMAT_YEAR_SHORT = 17;
+    private static final int FORMAT_ZONE_OFF = 18;
+    private static final int FORMAT_ZONE_ID = 19;
+
+    private static class Token {
+        private int format;
+        private Object data;
+        
+        public Token(int format) {
+            this(format, null);
+        }
+
+        public Token(int format, Object data) {
+            this.format = format;
+            this.data = data;
+        }
+        
+        /**
+         * Gets the data.
+         * @return Returns a Object
+         */
+        public Object getData() {
+            return data;
+        }
+
+        /**
+         * Gets the format.
+         * @return Returns a int
+         */
+        public int getFormat() {
+            return format;
+        }
+    }
 
     /**
      * Constructor for RubyDateFormat.
      */
     public RubyDateFormat() {
-        super();
+        this("", new DateFormatSymbols());
     }
 
-    /**
-     * Constructor for RubyDateFormat.
-     * @param pattern
-     */
-    public RubyDateFormat(String pattern) {
-        super(getSimpleDateFormatPattern(pattern));
+    public RubyDateFormat(String pattern, Locale aLocale) {
+        this(pattern, new DateFormatSymbols(aLocale));
     }
-
-    /**
-     * Constructor for RubyDateFormat.
-     * @param pattern
-     * @param locale
-     */
-    public RubyDateFormat(String pattern, Locale locale) {
-        super(getSimpleDateFormatPattern(pattern), locale);
-    }
-
-    /**
-     * Constructor for RubyDateFormat.
-     * @param pattern
-     * @param formatSymbols
-     */
+    
     public RubyDateFormat(String pattern, DateFormatSymbols formatSymbols) {
-        super(getSimpleDateFormatPattern(pattern), formatSymbols);
+        super();
+
+        this.formatSymbols = formatSymbols;
+        applyPattern(pattern);
+    }
+    
+    public void applyPattern(String pattern) {
+        this.pattern = pattern;
+        
+        compilePattern(pattern);
     }
 
-    private static String getSimpleDateFormatPattern(String pattern) {
-        StringBuffer result = new StringBuffer(100);
-
+    private void compilePattern(String pattern) {
+        compiledPattern = new LinkedList();
+        
         int len = pattern.length();
         for (int i = 0; i < len;) {
             if (pattern.charAt(i) == '%') {
                 i++;
                 switch (pattern.charAt(i)) {
                     case 'A' :
-                        result.append("EEEE");
+                        compiledPattern.add(new Token(FORMAT_WEEK_LONG));
                         break;
                     case 'a' :
-                        result.append("EEE");
+                        compiledPattern.add(new Token(FORMAT_WEEK_SHORT));
                         break;
                     case 'B' :
-                        result.append("MMMM");
+                        compiledPattern.add(new Token(FORMAT_MONTH_LONG));
                         break;
                     case 'b' :
-                        result.append("MMM");
+                        compiledPattern.add(new Token(FORMAT_MONTH_SHORT));
                         break;
                     case 'c' :
-                        result.append("EEE MMM dd HH:mm:ss yyyy");
+                        compiledPattern.add(new Token(FORMAT_WEEK_SHORT));
+                        compiledPattern.add(new Token(FORMAT_STRING, " "));
+                        compiledPattern.add(new Token(FORMAT_MONTH_SHORT));
+                        compiledPattern.add(new Token(FORMAT_STRING, " "));
+                        compiledPattern.add(new Token(FORMAT_DAY));
+                        compiledPattern.add(new Token(FORMAT_STRING, " "));
+                        compiledPattern.add(new Token(FORMAT_HOUR));
+                        compiledPattern.add(new Token(FORMAT_STRING, ":"));
+                        compiledPattern.add(new Token(FORMAT_MINUTES));
+                        compiledPattern.add(new Token(FORMAT_STRING, ":"));
+                        compiledPattern.add(new Token(FORMAT_SECONDS));
+                        compiledPattern.add(new Token(FORMAT_STRING, " "));
+                        compiledPattern.add(new Token(FORMAT_YEAR_LONG));
                         break;
                     case 'd':
-                        result.append("dd");
+                        compiledPattern.add(new Token(FORMAT_DAY));
                         break;
                     case 'H':
-                        result.append("HH");
+                        compiledPattern.add(new Token(FORMAT_HOUR));
                         break;
                     case 'I':
-                        result.append("KK");
+                        compiledPattern.add(new Token(FORMAT_HOUR_M));
                         break;
                     case 'j':
-                        result.append("DDD");
+                        compiledPattern.add(new Token(FORMAT_DAY_YEAR));
                         break;
                     case 'M':
-                        result.append("mm");
+                        compiledPattern.add(new Token(FORMAT_MINUTES));
                         break;
                     case 'm':
-                        result.append("MM");
+                        compiledPattern.add(new Token(FORMAT_MONTH));
                         break;
                     case 'p':
-                        result.append("aa");
+                        compiledPattern.add(new Token(FORMAT_MERIDIAN));
                         break;
                     case 'S':
-                        result.append("ss");
+                        compiledPattern.add(new Token(FORMAT_SECONDS));
                         break;
                     case 'U':
-                        result.append("ww");
+                        compiledPattern.add(new Token(FORMAT_WEEK_YEAR_S));
                         break;
                     case 'W':
-                        result.append("ww");
+                        compiledPattern.add(new Token(FORMAT_WEEK_YEAR_M));
                         break;
                     case 'w':
-                        result.append("E");
+                        compiledPattern.add(new Token(FORMAT_DAY_WEEK));
                         break;
                     case 'X':
-                        result.append("HH:mm:ss");
+                        compiledPattern.add(new Token(FORMAT_HOUR));
+                        compiledPattern.add(new Token(FORMAT_STRING, ":"));
+                        compiledPattern.add(new Token(FORMAT_MINUTES));
+                        compiledPattern.add(new Token(FORMAT_STRING, ":"));
+                        compiledPattern.add(new Token(FORMAT_SECONDS));
                         break;
                     case 'x':
-                        result.append("MM/dd/YY");
+                        compiledPattern.add(new Token(FORMAT_MONTH));
+                        compiledPattern.add(new Token(FORMAT_STRING, "/"));
+                        compiledPattern.add(new Token(FORMAT_DAY));
+                        compiledPattern.add(new Token(FORMAT_STRING, "/"));
+                        compiledPattern.add(new Token(FORMAT_YEAR_SHORT));
                         break;
                     case 'Y':
-                        result.append("yyyy");
+                        compiledPattern.add(new Token(FORMAT_YEAR_LONG));
                         break;
                     case 'y':
-                        result.append("yy");
+                        compiledPattern.add(new Token(FORMAT_YEAR_SHORT));
                         break;
                     case 'Z':
-                        result.append("z");
+                        compiledPattern.add(new Token(FORMAT_ZONE_ID));
                         break;
                     case 'z':
-                        result.append("Z");
+                        compiledPattern.add(new Token(FORMAT_ZONE_OFF));
                         break;
                     case '%':
-                        result.append("%");
-                        break;
-                    case '\'':
-                        result.append("%\\'");
+                        compiledPattern.add(new Token(FORMAT_STRING, "%"));
                         break;
                     default:
-                        result.append("\'%");
-                        result.append(pattern.charAt(i));
-                        result.append('\'');
+                        compiledPattern.add(new Token(FORMAT_STRING, "%" + pattern.charAt(i)));
                 }
                 i++;
             } else {
-                result.append('\'');
+                StringBuffer sb = new StringBuffer();
                 for (;i < len && pattern.charAt(i) != '%'; i++) {
-                    if (pattern.charAt(i) == '\'') {
-                        result.append('\\');
-                    }
-                    result.append(pattern.charAt(i));
+                    sb.append(pattern.charAt(i));
                 }
-                result.append('\'');
+                compiledPattern.add(new Token(FORMAT_STRING, sb.toString()));
+            }
+        }
+    }
+
+    /**
+     * @see DateFormat#format(Date, StringBuffer, FieldPosition)
+     */
+    public StringBuffer format(Date date, StringBuffer toAppendTo, FieldPosition fieldPosition) {
+        calendar.setTime(date);
+        
+        Iterator iter = compiledPattern.iterator();
+        while (iter.hasNext()) {
+            Token token = (Token) iter.next();
+            
+            switch (token.getFormat()) {
+                case FORMAT_STRING:
+                    toAppendTo.append(token.getData());
+                    break;
+                case FORMAT_WEEK_LONG:
+                    toAppendTo.append(formatSymbols.getWeekdays()[calendar.get(Calendar.DAY_OF_WEEK)]);
+                    break;
+                case FORMAT_WEEK_SHORT:
+                    toAppendTo.append(formatSymbols.getShortWeekdays()[calendar.get(Calendar.DAY_OF_WEEK)]);
+                    break;
+                case FORMAT_MONTH_LONG:
+                    toAppendTo.append(formatSymbols.getMonths()[calendar.get(Calendar.MONTH)]);
+                    break;
+                case FORMAT_MONTH_SHORT:
+                    toAppendTo.append(formatSymbols.getShortMonths()[calendar.get(Calendar.MONTH)]);
+                    break;
+                case FORMAT_DAY:
+                    int value = calendar.get(Calendar.DAY_OF_MONTH);
+                    if (value < 10) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_HOUR:
+                    value = calendar.get(Calendar.HOUR_OF_DAY);
+                    if (value < 10) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_HOUR_M:
+                    value = calendar.get(Calendar.HOUR);
+                    if (value < 10) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_DAY_YEAR:
+                    value = calendar.get(Calendar.DAY_OF_YEAR);
+                    if (value < 10) {
+                        toAppendTo.append("00");
+                    } else if (value < 100) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_MINUTES:
+                    value = calendar.get(Calendar.MINUTE);
+                    if (value < 10) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_MONTH:
+                    value = calendar.get(Calendar.MONTH) + 1;
+                    if (value < 10) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_MERIDIAN:
+                    if (calendar.get(Calendar.AM_PM) == Calendar.AM) {
+                        toAppendTo.append("AM");
+                    } else {
+                        toAppendTo.append("PM");
+                    }
+                    break;
+                case FORMAT_SECONDS:
+                    value = calendar.get(Calendar.SECOND);
+                    if (value < 10) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_WEEK_YEAR_M:
+                    value = calendar.get(Calendar.WEEK_OF_YEAR);
+                    if (value < 10) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_WEEK_YEAR_S:
+                    value = calendar.get(Calendar.WEEK_OF_YEAR);
+                    if (value < 10) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_DAY_WEEK:
+                    value = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_YEAR_LONG:
+                    value = calendar.get(Calendar.YEAR);
+                    if (value < 10) {
+                        toAppendTo.append("000");
+                    } else if (value < 100) {
+                        toAppendTo.append("00");
+                    } else if (value < 1000) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_YEAR_SHORT:
+                    value = calendar.get(Calendar.YEAR) % 100;
+                    if (value < 10) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_ZONE_OFF:
+                    value = calendar.getTimeZone().getOffset(calendar.getTimeInMillis());
+                    if (value < 0) {
+                        toAppendTo.append('+');
+                    } else {
+                        toAppendTo.append('-');
+                    }
+                    value = Math.abs(value);
+                    if ((value / 3600000) < 10) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value / 3600000);
+                    value = value % 3600000 / 60000;
+                    if (value < 10) {
+                        toAppendTo.append('0');
+                    }
+                    toAppendTo.append(value);
+                    break;
+                case FORMAT_ZONE_ID:
+                    toAppendTo.append(calendar.getTimeZone().getDisplayName(calendar.get(Calendar.DST_OFFSET) != 0, TimeZone.SHORT));
+                    break;
             }
         }
 
-        return result.toString();
-    }
-    /**
-     * @see SimpleDateFormat#applyLocalizedPattern(String)
-     */
-    public void applyLocalizedPattern(String pattern) {
-        super.applyLocalizedPattern(getSimpleDateFormatPattern(pattern));
+        return toAppendTo;
     }
 
     /**
-     * @see SimpleDateFormat#applyPattern(String)
+     * @see DateFormat#parse(String, ParsePosition)
      */
-    public void applyPattern(String pattern) {
-        super.applyPattern(getSimpleDateFormatPattern(pattern));
-    }
-
-    /**
-     * @see SimpleDateFormat#toLocalizedPattern()
-     */
-    public String toLocalizedPattern() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * @see SimpleDateFormat#toPattern()
-     */
-    public String toPattern() {
+    public Date parse(String source, ParsePosition pos) {
         throw new UnsupportedOperationException();
     }
 }
