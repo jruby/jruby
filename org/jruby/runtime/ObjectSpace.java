84614f00985b4df5514994c4b44ebae080693d70
Jan Arne Petersen <jpetersen@uni-bonn.de>
2002-07-02 14:54:01 +0000

    - throw UnsupportedOperationException if you want to remove from a ObjectSpaceIterator. - update header to programming guide draft.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@436 961051c9-f516-0410-bf72-c9f7e237a7b7

--- org/jruby/runtime/ObjectSpace.java
+++ org/jruby/runtime/ObjectSpace.java
@@ -1,35 +1,26 @@
 /*
- * ObjectSpace.java
- * Created on 27 May 2002
- * 
- * Copyright (C) 2001, 2002 Jan Arne Petersen, Alan Moore,
- * Benoit Cerrina, Chad Fowler, Anders Bengtsson
- * Jan Arne Petersen <jpetersen@uni-bonn.de>
- * Alan Moore <alan_moore@gmx.net>
- * Benoit Cerrina <b.cerrina@wanadoo.fr>
- * Chad Fowler <chadfowler@chadfowler.com>
- * Anders Bengtsson <ndrsbngtssn@yahoo.se>
+ * Copyright (C) 2002 Anders Bengtsson <ndrsbngtssn@yahoo.se>
+ * Copyright (C) 2002 Jan Arne Petersen <jpetersen@uni-bonn.de>
  *
  * JRuby - http://jruby.sourceforge.net
  * 
  * This file is part of JRuby
  * 
- * JRuby is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * JRuby is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
  * 
  * JRuby is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with JRuby; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ * You should have received a copy of the GNU General Public
+ * License along with JRuby; if not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307 USA
  */
-
 package org.jruby.runtime;
 
 import java.util.*;
@@ -41,7 +32,6 @@ import org.jruby.RubyModule;
  *
  * @author Anders
  */
-
 public class ObjectSpace {
     private Set references = new HashSet();
     private ReferenceQueue deadReferences = new ReferenceQueue();
@@ -61,7 +51,6 @@ public class ObjectSpace {
             references.remove(reference);
         }
     }
-
 
     private class ObjectSpaceIterator implements Iterator {
         private final RubyModule rubyClass;
@@ -89,7 +78,7 @@ public class ObjectSpace {
         }
 
         public void remove() {
-            // FIXME: throw exception
+            throw new UnsupportedOperationException();
         }
 
         private void prefetch() {
