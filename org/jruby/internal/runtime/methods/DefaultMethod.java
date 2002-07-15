937a612132f1c823d234b38759ef33d333e91f81
Anders Bengtsson <ndrsbngtssn@yahoo.se>
2002-07-15 18:40:14 +0000

    Continuing the refactoring of DefaultMethod.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@504 961051c9-f516-0410-bf72-c9f7e237a7b7

--- org/jruby/internal/runtime/methods/DefaultMethod.java
+++ org/jruby/internal/runtime/methods/DefaultMethod.java
@@ -31,19 +31,14 @@ public final class DefaultMethod extends AbstractMethod {
      * @see IMethod#execute(Ruby, RubyObject, String, RubyObject[], boolean)
      */
     final public RubyObject execute(final Ruby ruby, final RubyObject receiver, final String name, RubyObject[] args, final boolean noSuper) {
-        if (args == null) {
-            args = new RubyObject[0];
-        }
 
         ruby.getScope().push();
 
         Namespace savedNamespace = null;
 
-        if (namespace != null) {
-            savedNamespace = ruby.getNamespace();
-            ruby.setNamespace(namespace);
-            ruby.getActFrame().setNamespace(namespace);
-        }
+        savedNamespace = ruby.getNamespace();
+        ruby.setNamespace(namespace);
+        ruby.getActFrame().setNamespace(namespace);
 
         if (body.getLocalNames() != null) {
             ruby.getScope().setLocalNames(body.getLocalNames());
@@ -53,56 +48,7 @@ public final class DefaultMethod extends AbstractMethod {
 
         try {
             if (argsNode != null) {
-                int expectedArgsCount = argsNode.getArgsCount();
-                if (expectedArgsCount > args.length) {
-                    throw new ArgumentError(ruby, "Wrong # of arguments(" + args.length + " for " + expectedArgsCount + ")");
-                }
-                if (argsNode.getRestArg() == -1 && argsNode.getOptArgs() != null) {
-                    int opt = expectedArgsCount;
-
-                    Iterator iter = argsNode.getOptArgs().iterator();
-                    while (iter.hasNext()) {
-                        iter.next();
-                        opt++;
-                    }
-
-                    if (opt < args.length) {
-                        throw new ArgumentError(ruby, "wrong # of arguments(" + args.length + " for " + opt + ")");
-                    }
-
-                    ruby.getActFrame().setArgs(args);
-                }
-
-                if (ruby.getScope().hasLocalValues()) {
-                    if (expectedArgsCount > 0) {
-                        for (int i = 0; i < expectedArgsCount; i++) {
-                            ruby.getScope().setValue(i + 2, args[i]);
-                        }
-                    }
-
-                    if (argsNode.getOptArgs() != null) {
-                        IListNode optArgs = argsNode.getOptArgs();
-
-                        Iterator iter = optArgs.iterator();
-                        for (int i = expectedArgsCount; i < args.length && iter.hasNext(); i++) {
-                            new AssignmentVisitor(ruby, receiver).assign((INode)iter.next(), args[i], true);
-                            expectedArgsCount++;
-                        }
-
-                        // assign the default values.
-                        while (iter.hasNext()) {
-                            EvaluateVisitor.createVisitor(receiver).eval((INode)iter.next());
-                        }
-                    }
-
-                    if (argsNode.getRestArg() >= 0) {
-                        RubyArray array = RubyArray.newArray(ruby, args.length - expectedArgsCount);
-                        for (int i = expectedArgsCount; i < args.length; i++) {
-                            array.append(args[i]);
-                        }
-                        ruby.getScope().setValue(argsNode.getRestArg(), array);
-                    }
-                }
+                prepareArguments(ruby, receiver, args);
             }
 
             if (argsNode.getBlockArgNode() != null && ruby.isBlockGiven()) {
@@ -112,19 +58,66 @@ public final class DefaultMethod extends AbstractMethod {
 
             traceCall(ruby, receiver, name);
 
-            return receiver.eval(body.getBodyNode()); // skip scope assignment
-        } catch (ReturnException rExcptn) {
-            return rExcptn.getReturnValue();
+            return receiver.eval(body.getBodyNode());
+
+        } catch (ReturnException re) {
+            return re.getReturnValue();
         } finally {
             RubyVarmap.pop(ruby);
-
             ruby.getScope().pop();
+            ruby.setNamespace(savedNamespace);
+            traceReturn(ruby, receiver, name);
+        }
+    }
 
-            if (savedNamespace != null) {
-                ruby.setNamespace(savedNamespace);
+    private void prepareArguments(final Ruby ruby, final RubyObject receiver, RubyObject[] args) {
+        if (args == null) {
+            args = new RubyObject[0];
+        }
+
+        int expectedArgsCount = argsNode.getArgsCount();
+        if (expectedArgsCount > args.length) {
+            throw new ArgumentError(ruby, "Wrong # of arguments(" + args.length + " for " + expectedArgsCount + ")");
+        }
+        if (argsNode.getRestArg() == -1 && argsNode.getOptArgs() != null) {
+            int opt = expectedArgsCount + argsNode.getOptArgs().size();
+
+            if (opt < args.length) {
+                throw new ArgumentError(ruby, "wrong # of arguments(" + args.length + " for " + opt + ")");
             }
 
-            traceReturn(ruby, receiver, name);
+            ruby.getActFrame().setArgs(args);
+        }
+
+        if (ruby.getScope().hasLocalValues()) {
+            if (expectedArgsCount > 0) {
+                for (int i = 0; i < expectedArgsCount; i++) {
+                    ruby.getScope().setValue(i + 2, args[i]);
+                }
+            }
+
+            if (argsNode.getOptArgs() != null) {
+                IListNode optArgs = argsNode.getOptArgs();
+
+                Iterator iter = optArgs.iterator();
+                for (int i = expectedArgsCount; i < args.length && iter.hasNext(); i++) {
+                    new AssignmentVisitor(ruby, receiver).assign((INode)iter.next(), args[i], true);
+                    expectedArgsCount++;
+                }
+
+                // assign the default values.
+                while (iter.hasNext()) {
+                    EvaluateVisitor.createVisitor(receiver).eval((INode)iter.next());
+                }
+            }
+
+            if (argsNode.getRestArg() >= 0) {
+                RubyArray array = RubyArray.newArray(ruby, args.length - expectedArgsCount);
+                for (int i = expectedArgsCount; i < args.length; i++) {
+                    array.append(args[i]);
+                }
+                ruby.getScope().setValue(argsNode.getRestArg(), array);
+            }
         }
     }
 
