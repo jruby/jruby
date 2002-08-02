0afda8058570df8581ac103f9b818340fd778cbe
Anders Bengtsson <ndrsbngtssn@yahoo.se>
2002-08-02 15:48:21 +0000

    Some more block tests.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@559 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/testBlock.rb
+++ test/testBlock.rb
@@ -13,3 +13,19 @@ end
 
 test_ok([2,4,6,8,10] == values)
 
+values = []
+[1,2,3].each {|v| values << v; break }
+test_equal([1], values)
+
+values = []
+result = [1,2,3,4,5].collect {|v|
+  if v > 2
+    break
+  end
+  values << v
+  v
+}
+test_equal([1,2], values)
+test_ok(result.nil?)
+
+test_print_report
