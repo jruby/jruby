3eeffdc291aefdcd1338bcaea11dfa151c1367ae
Anders Bengtsson <ndrsbngtssn@yahoo.se>
2002-08-21 12:21:30 +0000

    Made the backquote test independent on configuration; re-activated it in the test suite.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@689 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/testBackquote.rb
+++ test/testBackquote.rb
@@ -1,10 +1,7 @@
 require 'minirunit'
-test_check "Test backquote and config:"
-require "rbconfig"
-interpreter = File.join(Config::CONFIG["bindir"], Config::CONFIG["RUBY_INSTALL_NAME"])
-version = `#{interpreter} --version`
+test_check "Test backquote:"
 
-test_ok(version =~ /ruby (\d+\.\d+\.\d+)\s+\((.*?)\)\s+\[(.*?)\]/)
-test_equal("1.6.7", $1)
-test_equal("java", $3)
-
+if File.exists?("/bin/echo")
+  output = `/bin/echo hello`
+  test_equal("hello\n", output)
+end
