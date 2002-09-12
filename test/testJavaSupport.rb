c8e81b06cc301008990ebbab17f33bbd5af35d15
Anders Bengtsson <ndrsbngtssn@yahoo.se>
2002-09-12 23:27:16 +0000

    More test for java's inner classes.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@814 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/testJavaSupport.rb
+++ test/testJavaSupport.rb
@@ -32,5 +32,11 @@ if defined? Java
     test_equal("java.lang.Character$UnicodeBlock",
                Character::UnicodeBlock.class_eval("@java_class.name"))
     test_ok(Character::UnicodeBlock.methods.include?("of"))
+
+    h = HashMap.new
+    h.put("a", 1)
+    iter = h.entrySet.iterator
+    inner_instance_entry = iter.next
+    test_equal("a", inner_instance_entry.getKey)
   end
 end
