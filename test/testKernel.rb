0de93809f246fce0054dba000bf2102e6819cdf7
Anders Bengtsson <ndrsbngtssn@yahoo.se>
2002-10-09 18:28:32 +0000

    Added some tests for Kernel::eval()
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@960 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ test/testKernel.rb
@@ -0,0 +1,11 @@
+require 'test/minirunit'
+test_check "kernel"
+
+test_ok(! eval("defined? some_unknown_variable"))
+x = 1
+test_equal(1, eval("x"))
+eval("x = 2")
+test_equal(2, x)
+eval("unknown = 3")
+#test_equal(2, x)     # Make sure eval() didn't destroy locals
+test_ok(! defined? unknown)
