a2c6ce6d3aef3a4b7c48511dec27fcadb8b90f3d
Benoit Cerrina <b.cerrina@wanadoo.fr>
2002-10-14 02:52:48 +0000

    correct case for when the system path separator is \\
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@983 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/testLoad.rb
+++ test/testLoad.rb
@@ -3,6 +3,6 @@ require "test/minirunit"
 test_check("Test Ruby-Init")
 
 $ruby_init = false
-require File::dirname(__FILE__) + "/RubyInitTest.jar"
+require File::dirname(__FILE__.gsub('\\\\', '/')) + "/RubyInitTest.jar"
 test_ok($ruby_init)
 
