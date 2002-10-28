4a0c92657dfa07a0032367894759b875ee66c351
Anders Bengtsson <ndrsbngtssn@yahoo.se>
2002-10-28 22:13:10 +0000

    Added tests for case-statement
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1048 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ test/testCase.rb
@@ -0,0 +1,47 @@
+require 'test/minirunit'
+test_check "case"
+
+x = nil
+case
+when true
+  x = 1
+when false
+  x = 2
+end
+test_equal(1, x)
+
+x = nil
+case
+when false
+  x = 1
+when true
+  x = 2
+end
+test_equal(2, x)
+
+x = nil
+case 10
+when 1..3
+  x = 'a'
+when 4..8
+  x = 'b'
+when 9..22
+  x = 'c'
+else
+  x = 'd'
+end
+test_equal('c', x)
+
+x = nil
+case 10
+when 1
+  x = 'a'
+when 100
+  x = 'b'
+else
+  x = 'c'
+end
+test_equal('c', x)
+
+test_print_report
+
