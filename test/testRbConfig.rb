8982a1f7c0a425a4785b10398e2c1baadf159362
Thomas Enebo <tom.enebo@gmail.com>
2004-04-23 13:53:25 +0000

    Wrong minor version to check against
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1294 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/testRbConfig.rb
+++ test/testRbConfig.rb
@@ -4,7 +4,7 @@ test_check "rbconfig"
 require 'rbconfig'
 
 test_equal("1", Config::CONFIG['MAJOR'])
-test_equal("6", Config::CONFIG['MINOR'])
+test_equal("8", Config::CONFIG['MINOR'])
 
 test_equal(File.join(Dir.pwd, "bin"), Config::CONFIG['bindir'])
 test_ok(["jruby.rb", "jruby.sh", "jruby.bat"].include?(Config::CONFIG['RUBY_INSTALL_NAME']))
