46c557fa833a40b672e28d9a7917ab12110b94f9
Thomas Enebo <tom.enebo@gmail.com>
2004-10-23 20:46:09 +0000

    Make JavaClass#for_name work with JDBC and add missing attribution/copyright notice at top
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1362 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/javasupport/JavaSupport.java
+++ src/org/jruby/javasupport/JavaSupport.java
@@ -1,3 +1,33 @@
+/*
+ * JavaSupport.java
+ *
+ * Copyright (C) 2002 Jan Arne Petersen
+ * Copyright (C) 2002-2004 Anders Bengtsson
+ * Copyright (C) 2004 Thomas E Enebo
+ * 
+ * Jan Arne Petersen <jpetersen@uni-bonn.de>
+ * Anders Bengtsson <ndrsbngtssn@yahoo.se>
+ * Thomas E Enebo <enebo@acm.org>
+ *
+ * JRuby - http://jruby.sourceforge.net
+ *
+ * This file is part of JRuby
+ *
+ * JRuby is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * JRuby is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JRuby; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 package org.jruby.javasupport;
 
 import org.jruby.Ruby;
@@ -24,7 +54,7 @@ public class JavaSupport {
 
     private Map exceptionHandlers = new HashMap();
 
-    private ClassLoader javaClassLoader = Thread.currentThread().getContextClassLoader();
+    private ClassLoader javaClassLoader = this.getClass().getClassLoader();
 
     private Map javaObjectMap = new HashMap();
 
@@ -36,7 +66,7 @@ public class JavaSupport {
         try {
             Class result = primitiveClass(className);
             if (result == null) {
-                return javaClassLoader.loadClass(className);
+                return Class.forName(className, true, javaClassLoader);
             }
             return result;
         } catch (ClassNotFoundException cnfExcptn) {
