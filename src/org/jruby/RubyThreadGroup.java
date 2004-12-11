6494926719d4df126c73fdaa14020ecfc6b91762
Charles Oliver Nutter <headius@headius.com>
2004-12-11 07:47:42 +0000

    cleaned up imports
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1466 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/RubyThreadGroup.java
+++ src/org/jruby/RubyThreadGroup.java
@@ -24,13 +24,8 @@
  */
 package org.jruby;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.jruby.exceptions.TypeError;
 import org.jruby.runtime.CallbackFactory;
