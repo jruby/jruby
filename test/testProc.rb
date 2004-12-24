0d0b92678ad2de46b40e0c2f213bb4b79f3af01a
Stefan Matthias Aust <sma@3plus4.de>
2004-12-24 10:37:26 +0000

    add proc arity test cases
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1548 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ test/testProc.rb
@@ -0,0 +1,17 @@
+require 'test/minirunit'
+
+test_check "Test Procs"
+
+test_equal(-1, Proc.new {}.arity)
+test_equal(0, Proc.new {||}.arity)
+test_equal(1, Proc.new {|a|}.arity)
+test_equal(2, Proc.new {|a, b|}.arity)
+test_equal(3, Proc.new {|a, b, c|}.arity)
+test_equal(-1, Proc.new {|*a|}.arity)
+test_equal(-2, Proc.new {|a, *b|}.arity)
+test_equal(-3, Proc.new {|a, b, *c|}.arity)
+
+# when adding arity to blocks, I broke the for loop
+j = 0; for i in 1..3 do j += i end; test_equal(6, j)
+j = 0; for $i in 1..3 do j += $i end; test_equal(6, j)
+j = 0; for i, k in {1=>2, 3=>4} do j += i + k end; test_equal(10, j)
