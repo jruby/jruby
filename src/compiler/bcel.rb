f38ee286f87026dd13b19ceaf680e79c624cf11e
Stefan Matthias Aust <sma@3plus4.de>
2004-12-24 12:30:45 +0000

    remove stuff related to dead byte code compiler
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1550 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/compiler/bcel.rb
+++
@@ -1,7 +0,0 @@
-
-require 'java'
-
-module BCEL
-  include_package 'org.apache.bcel.generic'
-  include_package 'org.apache.bcel'
-end
--- src/compiler/jruby.rb
+++
@@ -1,8 +0,0 @@
-require 'java'
-
-module JRuby
-  include_package 'org.jruby.ast.visitor'
-  include_package 'org.jruby.ast'
-
-  include_package 'org.jruby'
-end
