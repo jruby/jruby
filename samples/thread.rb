ba0de76a51b8f04721d973a70556fc326344c5eb
Thomas Enebo <tom.enebo@gmail.com>
2005-01-22 19:21:45 +0000

    Change .num to 0.num in thread sample
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1589 961051c9-f516-0410-bf72-c9f7e237a7b7

--- samples/thread.rb
+++ samples/thread.rb
@@ -1,4 +1,4 @@
-x = Thread.new { sleep .1; print "x"; print "y"; print "z" }
-a = Thread.new { print "a"; print "b"; sleep .2; print "c" }
+x = Thread.new { sleep 0.1; print "x"; print "y"; print "z" }
+a = Thread.new { print "a"; print "b"; sleep 0.2; print "c" }
 x.join # Let the threads finish before
 a.join # main thread exits...
