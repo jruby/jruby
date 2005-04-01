09d8f30f6aebff1911b6b5fb96b24e45094f8cd7
Charles Oliver Nutter <headius@headius.com>
2005-04-01 02:30:36 +0000

    fix for when there are no results from doing the split
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1656 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/util/Split.java
+++ src/org/jruby/util/Split.java
@@ -15,6 +15,7 @@
  * Copyright (C) 2002-2004 Anders Bengtsson <ndrsbngtssn@yahoo.se>
  * Copyright (C) 2004 Thomas E Enebo <enebo@acm.org>
  * Copyright (C) 2004 Stefan Matthias Aust <sma@3plus4.de>
+ * Copyright (C) 2005 Charles O Nutter <headius@headius.com>
  * 
  * Alternatively, the contents of this file may be used under the terms of
  * either of the GNU General Public License Version 2 or later (the "GPL"),
@@ -108,7 +109,7 @@ public class Split {
 		} else if (pos <= len) {
 			addResult(substring(splitee, pos, len - pos));
 		}
-		if (limit == 0) {
+		if (limit == 0 && result.size() > 0) {
 			while (((String) result.get(result.size() - 1)).length() == 0) {
 				result.remove(result.size() - 1);
 			}
