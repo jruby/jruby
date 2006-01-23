91c90ad7983c7147495dd2f81b507c8a28fce425
Charles Oliver Nutter <headius@headius.com>
2006-01-23 06:20:50 +0000

    removing Stack import
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1862 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/runtime/ThreadContext.java
+++ src/org/jruby/runtime/ThreadContext.java
@@ -35,7 +35,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Stack;
 
 import org.jruby.IRuby;
 import org.jruby.IncludedModuleWrapper;
