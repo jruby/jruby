1d0b663ce8136c942acc241be8f810fd2c2645b5
Charles Oliver Nutter <headius@headius.com>
2006-02-14 22:17:40 +0000

    re-added the missing "single digit" octal fixer
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1885 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/RegexpTranslator.java
+++ src/org/jruby/RegexpTranslator.java
@@ -13,7 +13,7 @@ class RegexpTranslator {
 	private static final Pattern SPACE_IN_CHARACTER_CLASS_PATTERN = Pattern.compile("(\\[[^]]*) (.*?])");
 	private static final Pattern COMMENT_PATTERN = Pattern.compile("\\(\\?#[^)]*\\)");
     private static final Pattern HEX_SINGLE_DIGIT_PATTERN = Pattern.compile("\\\\x(\\p{XDigit})(?!\\p{XDigit})");
-    private static final Pattern OCTAL_THREE_DIGIT_PATTERN = Pattern.compile("\\\\([1-3][0-7]{2})");
+    private static final Pattern OCTAL_SINGLE_DIGIT_PATTERN = Pattern.compile("\\\\([0-7])(?![0-7])");
     private static final Pattern OCTAL_MISSING_ZERO_PATTERN = Pattern.compile("\\\\([1-7][0-7]{1,2})");
     
     public Pattern translate(String regex, int options, int javaRegexFlags) {
@@ -25,7 +25,7 @@ class RegexpTranslator {
 	String translatePattern(String regex, boolean commentsAllowed) {
 		regex = COMMENT_PATTERN.matcher(regex).replaceAll("");
 		regex = HEX_SINGLE_DIGIT_PATTERN.matcher(regex).replaceAll("\\\\"+"x0$1");
-		regex = OCTAL_THREE_DIGIT_PATTERN.matcher(regex).replaceAll("\\\\"+"0$1");
+		regex = OCTAL_SINGLE_DIGIT_PATTERN.matcher(regex).replaceAll("\\\\"+"0$1");
 		regex = OCTAL_MISSING_ZERO_PATTERN.matcher(regex).replaceAll("\\\\"+"0$1");
 		if (commentsAllowed) {
 			regex = SPACE_IN_CHARACTER_CLASS_PATTERN.matcher(regex).replaceAll("$1\\\\x20$2");
