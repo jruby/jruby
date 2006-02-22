caee442dd337ce870b320a2b9f961b1f12acd96f
Thomas Enebo <tom.enebo@gmail.com>
2006-02-22 03:02:37 +0000

    Back out single digit octal substitute one more time...A single digit octal value cannot be safely escaped (zero-padded) in a regex since \0-\9 are valid regexp substitution patterns.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1890 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/RegexpTranslator.java
+++ src/org/jruby/RegexpTranslator.java
@@ -13,7 +13,6 @@ class RegexpTranslator {
 	private static final Pattern SPACE_IN_CHARACTER_CLASS_PATTERN = Pattern.compile("(\\[[^]]*) (.*?])");
 	private static final Pattern COMMENT_PATTERN = Pattern.compile("\\(\\?#[^)]*\\)");
     private static final Pattern HEX_SINGLE_DIGIT_PATTERN = Pattern.compile("\\\\x(\\p{XDigit})(?!\\p{XDigit})");
-    private static final Pattern OCTAL_SINGLE_DIGIT_PATTERN = Pattern.compile("\\\\([0-7])(?![0-7])");
     private static final Pattern OCTAL_MISSING_ZERO_PATTERN = Pattern.compile("\\\\([1-7][0-7]{1,2})");
     
     public Pattern translate(String regex, int options, int javaRegexFlags) {
@@ -25,18 +24,17 @@ class RegexpTranslator {
 	String translatePattern(String regex, boolean commentsAllowed) {
 		regex = COMMENT_PATTERN.matcher(regex).replaceAll("");
 		regex = HEX_SINGLE_DIGIT_PATTERN.matcher(regex).replaceAll("\\\\"+"x0$1");
-		regex = OCTAL_SINGLE_DIGIT_PATTERN.matcher(regex).replaceAll("\\\\"+"0$1");
 		regex = OCTAL_MISSING_ZERO_PATTERN.matcher(regex).replaceAll("\\\\"+"0$1");
 		if (commentsAllowed) {
 			regex = SPACE_IN_CHARACTER_CLASS_PATTERN.matcher(regex).replaceAll("$1\\\\x20$2");
 			regex = SHARP_IN_CHARACTER_CLASS_PATTERN.matcher(regex).replaceAll("$1\\\\x23$2");
 		}
-		
+        
 		return regex;
 	}
 	
 	int translateFlags(int options) {
-		int flags = Pattern.MULTILINE;
+        int flags = Pattern.MULTILINE;
         if ((options & ReOptions.RE_OPTION_IGNORECASE) > 0) {
             flags |= Pattern.CASE_INSENSITIVE;
         }
