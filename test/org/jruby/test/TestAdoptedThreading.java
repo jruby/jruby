ad0441363337e7499d26df6aba4b7647ab3c2445
Charles Oliver Nutter <headius@headius.com>
2006-03-29 17:39:28 +0000

    cleaned up the "adopted thread" test case
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1960 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/org/jruby/test/TestAdoptedThreading.java
+++ test/org/jruby/test/TestAdoptedThreading.java
@@ -1,3 +1,28 @@
+/***** BEGIN LICENSE BLOCK *****
+ * Version: CPL 1.0/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Common Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the CPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the CPL, the GPL or the LGPL.
+ ***** END LICENSE BLOCK *****/
 package org.jruby.test;
 
 import java.util.logging.Level;
@@ -9,22 +34,26 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+/**
+ * A simple "adopted thread" concurrency test case.
+ */
 public class TestAdoptedThreading extends TestCase {
     private static Logger LOGGER = Logger.getLogger(TestAdoptedThreading.class
             .getName());
 
+    // Uncomment the "puts" lines if you want to see more detail
     private static final String SCRIPT = "require 'java'\n"
             + "include_class 'org.jruby.test.ITest'\n"
             + "class TestImpl < ITest\n" + "    def exec(_value)\n"
-            + "        puts \"start executing!\"\n"
+            + "        #puts \"start executing!\"\n"
             + "        1000.times do | item |\n"
             + "           value = \"#{item}\"\n" + "        end\n"
-            + "        puts \"end executing1!\"\n" + "        exec2(_value)\n"
+            + "        #puts \"end executing1!\"\n" + "        exec2(_value)\n"
             + "    end\n" + "    def exec2(_value)\n"
-            + "        puts \"start executing2!\"\n"
+            + "        #puts \"start executing2!\"\n"
             + "        5000.times do | item |\n"
             + "           value = \"#{item}\"\n" + "        end\n"
-            + "        puts \"end executing2!\"\n"
+            + "        #puts \"end executing2!\"\n"
             + "        \"VALUE: #{_value}\"\n" + "    end\n" + "end";
 
     public TestAdoptedThreading(String _name) {
@@ -41,70 +70,69 @@ public class TestAdoptedThreading extends TestCase {
     private BSFManager manager_;
 
     protected void setUp() throws Exception {
-        try {
-            LOGGER.log(Level.INFO, SCRIPT);
-            BSFManager.registerScriptingEngine("ruby",
-                    "org.jruby.javasupport.bsf.JRubyEngine",
-                    new String[] { "rb" });
-            manager_ = new BSFManager();
-            manager_.exec("ruby", "(java)", 1, 1, SCRIPT);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        LOGGER.log(Level.FINEST, SCRIPT);
+        BSFManager.registerScriptingEngine("ruby",
+                "org.jruby.javasupport.bsf.JRubyEngine",
+                new String[] { "rb" });
+        manager_ = new BSFManager();
+        manager_.exec("ruby", "(java)", 1, 1, SCRIPT);
     }
+    
+    private static final int RUNNER_COUNT = 10;
+    private static final int RUNNER_LOOP_COUNT = 10;
 
     public void testThreading() {
-        Runner r00 = new Runner("R1", 10);
-        Runner r01 = new Runner("R1", 10);
-        Runner r02 = new Runner("R1", 10);
-        Runner r03 = new Runner("R1", 10);
-        Runner r04 = new Runner("R1", 10);
-        Runner r05 = new Runner("R1", 10);
-        Runner r06 = new Runner("R1", 10);
-        Runner r07 = new Runner("R1", 10);
-        Runner r08 = new Runner("R1", 10);
-        Runner r09 = new Runner("R1", 10);
-
-        r00.start();
-        r01.start();
-        r02.start();
-        r03.start();
-        r04.start();
-        r05.start();
-        r06.start();
-        r07.start();
-        r08.start();
-        r09.start();
+        Runner[] runners = new Runner[RUNNER_COUNT];
+        
+        for (int i = 0; i < RUNNER_COUNT; i++) runners[i] = new Runner("R" + i, RUNNER_LOOP_COUNT);
+        for (int i = 0; i < RUNNER_COUNT; i++) runners[i].start();
 
         try {
-            r00.join();
-            r01.join();
-            r02.join();
-            r03.join();
-            r04.join();
-            r05.join();
-            r06.join();
-            r07.join();
-            r08.join();
-            r09.join();
+            for (int i = 0; i < RUNNER_COUNT; i++) runners[i].join();
         } catch (InterruptedException ie) {
             // XXX: do something?
+        }
+        
+        for (int i = 0; i < RUNNER_COUNT; i++) {
+            if (runners[i].isFailed()) {
+                if (runners[i].getFailureException() != null) {
+                    throw runners[i].getFailureException();
+                }
+            }
         }
     }
 
     class Runner extends Thread {
         private int count_;
+        private boolean failed;
+        private RuntimeException failureException;
 
         public Runner(String _name, int _count) {
             count_ = _count;
+        }
+        
+        public boolean isFailed() {
+            return failed;
+        }
+        
+        public RuntimeException getFailureException() {
+            return failureException;
         }
 
         public void run() {
             for (int i = 0; i < count_; i++) {
                 ITest test = getTest();
                 if (test != null) {
-                    LOGGER.log(Level.INFO, "[NAME:" + getName() + "VALUE: "
-                            + test.exec("KABOOM!") + ", COUNT: " + i + "]");
+                    try {
+                        Object result = test.exec("foo");
+                        if (!result.toString().equals("VALUE: 5000")) {
+                            failed = true;
+                        }
+                    } catch (RuntimeException re) {
+                        failed = true;
+                        failureException = re;
+                        break;
+                    }
                 }
             }
         }
