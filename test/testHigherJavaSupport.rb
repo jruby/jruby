e11e4722a8033c9e659bce0524cc15c6597ed369
Charles Oliver Nutter <headius@headius.com>
2006-04-06 08:28:39 +0000

    repaired JString methods test to only look for selected methods
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@1971 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/testHigherJavaSupport.rb
+++ test/testHigherJavaSupport.rb
@@ -223,22 +223,18 @@ if defined? Java
   
   test_equal(java_date.getTime, Time.at(0).to_i)
   
-  # test that the list of methods produced matches expectations
-  jstring_methods = ["<", "<=", "<=>", "==", "===", "=~", ">", ">=", "__id__", "__send__", "between?",
-    "bytes", "charAt", "char_at", "class", "clone", "compareTo", "compareToIgnoreCase", "compare_to",
-    "compare_to_ignore_case", "concat", "contentEquals", "content_equals", "display", "dup", "endsWith",
-    "ends_with", "eql?", "equal?", "equals", "equalsIgnoreCase", "equals_ignore_case", "extend", "freeze",
-    "frozen?", "getBytes", "getChars", "getClass", "get_bytes", "get_chars", "get_class", "hash",
-    "hashCode", "hash_code", "id", "include_class", "indexOf", "index_of", "inspect", "instance_eval",
-    "instance_of?", "instance_variable_get", "instance_variable_set", "instance_variables", "intern",
-    "is_a?", "java_class", "java_object", "java_object=", "kind_of?", "lastIndexOf", "last_index_of",
-    "length", "matches", "method", "methods", "nil?", "notify", "notifyAll", "notify_all", "object_id",
-    "private_methods", "protected_methods", "public_methods", "regionMatches", "region_matches", "replace",
-    "replaceAll", "replaceFirst", "replace_all", "replace_first","respond_to?", "send", "singleton_methods",
-    "split", "startsWith", "starts_with", "subSequence", "sub_sequence", "substring", "taint", "tainted?",
-    "toCharArray", "toLowerCase", "toString", "toUpperCase", "to_a", "to_char_array", "to_java_object",
-    "to_lower_case","to_s", "to_string", "to_upper_case", "trim", "type", "untaint", "wait"]
+  # test that the list of JString methods contains selected methods from Java
+  jstring_methods = ["bytes", "charAt", "char_at","compareTo", "compareToIgnoreCase", "compare_to",
+    "compare_to_ignore_case", "concat", "contentEquals", "content_equals", "endsWith",
+    "ends_with", "equals", "equalsIgnoreCase", "equals_ignore_case", "getBytes", "getChars",
+    "getClass", "get_bytes", "get_chars", "get_class", "hashCode", "hash_code", "indexOf",
+    "index_of", "intern", "java_class", "java_object", "java_object=", "lastIndexOf", "last_index_of",
+    "length", "matches", "notify", "notifyAll", "notify_all", "regionMatches", "region_matches", "replace",
+    "replaceAll", "replaceFirst", "replace_all", "replace_first", "split", "startsWith", "starts_with",
+    "subSequence", "sub_sequence", "substring", "taint", "tainted?", "toCharArray", "toLowerCase",
+    "toString", "toUpperCase", "to_char_array", "to_java_object", "to_lower_case", "to_string",
+    "to_upper_case", "trim", "wait"]
   
-  test_equal(jstring_methods, JString.public_instance_methods.sort)
+  jstring_methods.each { |method| test_ok(JString.public_instance_methods.include?(method)) }
   
 end
