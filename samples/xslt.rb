7ae210c384e64bed907b2cbf56ad62350c9a6146
Thomas Enebo <tom.enebo@gmail.com>
2006-06-21 23:14:51 +0000

    Itched work scratch.  Improve xslt example to be able to provide xslt params to transformer, catch Java exceptions when parsing errors occur, and default to stdout if no third argument provided.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@2080 961051c9-f516-0410-bf72-c9f7e237a7b7

--- samples/xslt.rb
+++ samples/xslt.rb
@@ -1,18 +1,52 @@
-require "java"
+require 'java'
+require 'optparse'
+require 'ostruct'
 
+include_class "java.lang.System"
+include_class("java.lang.Exception") {|p,n| "J" + n }
 include_class "java.io.FileOutputStream"
 include_class "javax.xml.transform.TransformerFactory"
 include_class ["StreamSource", "StreamResult"].map {|e| "javax.xml.transform.stream." + e}
 
-if (ARGV.length < 3) 
-  puts "Usage: xslt {xml} {xslt} {result}"
+class XSLTOptions
+  def self.parse(args)
+    options = OpenStruct.new
+    options.parameters = {}
+       
+    opts = OptionParser.new do |opts|
+      opts.banner = "Usage: [options] xslt {xml} {xslt} [{result}]"
+      opts.separator ""
+      opts.separator "Specific options:"
+      
+      opts.on("-p", "--parameters name=value,name1=value1", Array) do |n|
+	n.collect do |v| 
+	  name, value = v.split(/\s*=\s*/)
+	  options.parameters[name] = value
+	end
+      end
+
+    end  
+    opts.parse!(args)
+    options
+  end
+end
+
+options = XSLTOptions.parse(ARGV)
+
+if (ARGV.length < 2 || ARGV.length > 3) 
+  puts "Usage: xslt {xml} {xslt} [{result}]"
   exit
 end
 
 document =   StreamSource.new(ARGV[0])
 stylesheet = StreamSource.new(ARGV[1])
-result =     StreamResult.new(FileOutputStream.new(ARGV[2]))
+output =     ARGV.length == 2 ? System::out : FileOutputStream.new(ARGV[2])
+result =     StreamResult.new(output)
 
-transformer = TransformerFactory.newInstance.newTransformer(stylesheet)
-transformer.transform(document, result)
-
+begin
+  transformer = TransformerFactory.newInstance.newTransformer(stylesheet)
+  options.parameters.each {|name, value| transformer.setParameter name, value }
+  transformer.transform(document, result)
+rescue Exception => e
+  puts e
+end
