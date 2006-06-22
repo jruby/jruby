80b60915bf09fc2c7b3d9b9825f725222c217a20
Thomas Enebo <tom.enebo@gmail.com>
2006-06-22 14:50:06 +0000

    Add heap arg to the lame bat scripts I made last night
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@2090 961051c9-f516-0410-bf72-c9f7e237a7b7

--- bin/gemwhich.bat
+++ bin/gemwhich.bat
@@ -80,7 +80,7 @@ rem echo Using Args:       %*
 
 rem ----- Execute The Requested Command ---------------------------------------
 
-%_STARTJAVA% -ea -cp "%CLASSPATH%" -Djruby.base="%JRUBY_BASE%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat org.jruby.Main %JRUBY_OPTS% "%JRUBY_HOME%\bin\gemwhich" %*
+%_STARTJAVA% -Xmx256m -ea -cp "%CLASSPATH%" -Djruby.base="%JRUBY_BASE%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat org.jruby.Main %JRUBY_OPTS% "%JRUBY_HOME%\bin\gemwhich" %*
 set E=%ERRORLEVEL%
 
 rem ----- Restore Environment Variables ---------------------------------------
