016f631aea015395c672bea597dbc91e7712a0df
Charles Oliver Nutter <headius@headius.com>
2007-01-02 20:10:30 +0000

    Adding a method-dispatch benchmark. First ten results are for just variable accesses (control run), then 100 runs with method calls.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@2647 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ test/bench/bench_method_dispatch.rb
@@ -0,0 +1,45 @@
+require 'benchmark'
+
+puts "Control: 100k loops accessing a local variable 100 times"
+10.times {
+puts Benchmark.measure {
+  t = Time.now; a = 5; 
+  i = 0;
+  while i < 100000
+    a; a; a; a; a; a; a; a; a; a;
+    a; a; a; a; a; a; a; a; a; a;
+    a; a; a; a; a; a; a; a; a; a;
+    a; a; a; a; a; a; a; a; a; a;
+    a; a; a; a; a; a; a; a; a; a;
+    a; a; a; a; a; a; a; a; a; a;
+    a; a; a; a; a; a; a; a; a; a;
+    a; a; a; a; a; a; a; a; a; a;
+    a; a; a; a; a; a; a; a; a; a;
+    a; a; a; a; a; a; a; a; a; a;
+    i += 1;
+  end
+  puts Time.now - t;
+}
+}
+
+puts "Test: 100k loops accessing a fixnum var and calling to_i 100 times"
+100.times {
+puts Benchmark.measure {
+  t = Time.now; a = 5; 
+  i = 0;
+  while i < 100000
+    a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
+    a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
+    a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
+    a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
+    a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
+    a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
+    a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
+    a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
+    a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
+    a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
+    i += 1;
+  end
+  puts Time.now - t;
+}
+}
