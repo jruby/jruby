6463defa7ba9c65f2cf1e9f98d0b55f3bdb651a9
Nick Sieger <nick@nicksieger.com>
2007-03-04 04:30:56 +0000

    test case cleans up after itself
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@3118 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/org/jruby/ast/visitor/rewriter/TestReWriteVisitor.java
+++ test/org/jruby/ast/visitor/rewriter/TestReWriteVisitor.java
@@ -12,7 +12,7 @@
  * rights and limitations under the License.
  *
  * Copyright (C) 2006 Mirko Stocker <me@misto.ch>
- * 
+ *
  * Alternatively, the contents of this file may be used under the terms of
  * either of the GNU General Public License Version 2 or later (the "GPL"),
  * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
@@ -50,76 +50,80 @@ import org.jruby.util.ByteList;
 
 public class TestReWriteVisitor extends TestCase {
 
-	static final SourcePosition emptyPosition = new SourcePosition("", 0, 0, 0, 0);
-	
-	private String visitNode(Node n) {
-		StringWriter out = new StringWriter();
-		ReWriteVisitor visitor = new ReWriteVisitor(out, "");
-		n.accept(visitor);
-		visitor.flushStream();
-		return out.getBuffer().toString();
-	}
-	
-	public void testVisitRegexpNode() {
-		RegexpNode n = new RegexpNode(new SourcePosition("", 0, 0, 2, 4), ByteList.create(".*"), 0);
-		assertEquals("/.*/", visitNode(n));
-	}
-	
-	public void testGetLocalVarIndex() {
-		assertEquals(ReWriteVisitor.getLocalVarIndex(new LocalVarNode(emptyPosition, 5, "")), 5);
-		assertEquals(ReWriteVisitor.getLocalVarIndex(new LocalVarNode(emptyPosition, 1, "")), 1);
-		assertEquals(ReWriteVisitor.getLocalVarIndex(null), -1);
-	}
-	
-	private ReWriteVisitor getVisitor() {
-		return new ReWriteVisitor(new StringWriter(), "");
-	}
+    static final SourcePosition emptyPosition = new SourcePosition("", 0, 0, 0, 0);
 
-	public void testVisitOptNNode() {
-		assertNull(getVisitor().visitOptNNode(new OptNNode(emptyPosition, null)));
-	}
+    private String visitNode(Node n) {
+        StringWriter out = new StringWriter();
+        ReWriteVisitor visitor = new ReWriteVisitor(out, "");
+        n.accept(visitor);
+        visitor.flushStream();
+        return out.getBuffer().toString();
+    }
 
-	public void testVisitPostExeNode() {
-		assertNull(getVisitor().visitPostExeNode(new PostExeNode(emptyPosition, null)));
-	}
-	
-	public void testUnwrapSingleArrayNode() {
-		ArrayNode arrayNode = new ArrayNode(emptyPosition);
-		ConstNode constNode = new ConstNode(emptyPosition, "const");
-		ConstNode anotherConstNode = new ConstNode(emptyPosition, "const");
-		arrayNode.add(constNode);
-		
-		assertEquals(ReWriteVisitor.unwrapSingleArrayNode(arrayNode), constNode);
-		assertEquals(ReWriteVisitor.unwrapSingleArrayNode(constNode), constNode);
+    public void testVisitRegexpNode() {
+        RegexpNode n = new RegexpNode(new SourcePosition("", 0, 0, 2, 4), ByteList.create(".*"), 0);
+        assertEquals("/.*/", visitNode(n));
+    }
 
-		arrayNode.add(anotherConstNode);
-		assertEquals(ReWriteVisitor.unwrapSingleArrayNode(arrayNode), arrayNode);
-	}
-	
-	public void testUnescapeChar() {
-		assertEquals(ReWriteVisitor.unescapeChar('\f'), "f");
-		assertEquals(ReWriteVisitor.unescapeChar('\r'), "r");
-		assertEquals(ReWriteVisitor.unescapeChar('\t'), "t");
-		assertEquals(ReWriteVisitor.unescapeChar('n'), null);
-	}
-	
-	public void testArgumentNode() {
-		Node node = new ArgumentNode(new SourcePosition(), "name");
-		assertEquals("name", ReWriteVisitor.createCodeFromNode(node, ""));
-	}
-	
-	public void testFileOutputStream() throws IOException {
-		String fileName = "outputTest";
-		String testString = "test";
-		FileOutputStream stream = new FileOutputStream(fileName);
-		ReWriteVisitor visitor = new ReWriteVisitor(stream, "");
-		ConstNode node = new ConstNode(emptyPosition, testString);
-		node.accept(visitor);
-		visitor.flushStream();
-		stream.close();
-		BufferedReader reader = new BufferedReader(new FileReader(fileName));
-		assertEquals(reader.readLine(), testString);
-	}
+    public void testGetLocalVarIndex() {
+        assertEquals(ReWriteVisitor.getLocalVarIndex(new LocalVarNode(emptyPosition, 5, "")), 5);
+        assertEquals(ReWriteVisitor.getLocalVarIndex(new LocalVarNode(emptyPosition, 1, "")), 1);
+        assertEquals(ReWriteVisitor.getLocalVarIndex(null), -1);
+    }
+
+    private ReWriteVisitor getVisitor() {
+        return new ReWriteVisitor(new StringWriter(), "");
+    }
+
+    public void testVisitOptNNode() {
+        assertNull(getVisitor().visitOptNNode(new OptNNode(emptyPosition, null)));
+    }
+
+    public void testVisitPostExeNode() {
+        assertNull(getVisitor().visitPostExeNode(new PostExeNode(emptyPosition, null)));
+    }
+
+    public void testUnwrapSingleArrayNode() {
+        ArrayNode arrayNode = new ArrayNode(emptyPosition);
+        ConstNode constNode = new ConstNode(emptyPosition, "const");
+        ConstNode anotherConstNode = new ConstNode(emptyPosition, "const");
+        arrayNode.add(constNode);
+
+        assertEquals(ReWriteVisitor.unwrapSingleArrayNode(arrayNode), constNode);
+        assertEquals(ReWriteVisitor.unwrapSingleArrayNode(constNode), constNode);
+
+        arrayNode.add(anotherConstNode);
+        assertEquals(ReWriteVisitor.unwrapSingleArrayNode(arrayNode), arrayNode);
+    }
+
+    public void testUnescapeChar() {
+        assertEquals(ReWriteVisitor.unescapeChar('\f'), "f");
+        assertEquals(ReWriteVisitor.unescapeChar('\r'), "r");
+        assertEquals(ReWriteVisitor.unescapeChar('\t'), "t");
+        assertEquals(ReWriteVisitor.unescapeChar('n'), null);
+    }
+
+    public void testArgumentNode() {
+        Node node = new ArgumentNode(new SourcePosition(), "name");
+        assertEquals("name", ReWriteVisitor.createCodeFromNode(node, ""));
+    }
+
+    public void testFileOutputStream() throws IOException {
+        String fileName = "outputTest";
+        try {
+            String testString = "test";
+            FileOutputStream stream = new FileOutputStream(fileName);
+            ReWriteVisitor visitor = new ReWriteVisitor(stream, "");
+            ConstNode node = new ConstNode(emptyPosition, testString);
+            node.accept(visitor);
+            visitor.flushStream();
+            stream.close();
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            assertEquals(reader.readLine(), testString);
+        } finally {
+            new java.io.File(fileName).delete();
+        }
+    }
 }
 
 
