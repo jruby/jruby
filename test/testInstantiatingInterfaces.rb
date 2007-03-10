c5aeff6fa27ca182862004da5324eaf5b2bcaab5
Nick Sieger <nick@nicksieger.com>
2007-03-10 23:12:25 +0000

    Changing interface usage to Runnable, CharSequence instead of awt
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@3165 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/testInstantiatingInterfaces.rb
+++ test/testInstantiatingInterfaces.rb
@@ -5,26 +5,26 @@ if defined? Java
   require 'java'
 
   # Tests unimplemented interface methods
-  class A < java.awt.event.ActionListener
+  class A < java.lang.Runnable
   end
   test_exception(NoMethodError) do
-    A.new.actionPerformed(nil)
+    A.new.run
   end
 
   foo = nil
-  ev = java.awt.event.ActionListener.impl do
+  ev = java.lang.Runnable.impl do
     foo = "ran"
   end
-  ev.actionPerformed(nil) rescue nil
+  ev.run rescue nil
   test_equal("ran", foo)
 
-  fl = java.awt.event.FocusListener.impl(:focusGained) do |sym, event|
-    test_equal(:focusGained, sym)
-    test_ok(event != nil)
+  cs = java.lang.CharSequence.impl(:charAt) do |sym, index|
+    test_equal(:charAt, sym)
+    test_equal(0, index)
   end
 
-  test_no_exception { fl.focusGained(java.awt.event.FocusEvent.new(java.awt.TextField.new, 0)) }
+  test_no_exception { cs.charAt(0) }
   test_exception(NoMethodError) do
-    fl.focusLost(nil)
+    cs.length
   end
 end
