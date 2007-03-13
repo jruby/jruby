df1f2c13b993b965bcdbe54ac44cae1811a3717b
Charles Oliver Nutter <headius@headius.com>
2007-03-13 06:16:49 +0000

    Added a non-STI benchmark.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@3183 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_method_dispatch.rb
+++ test/bench/bench_method_dispatch.rb
@@ -21,8 +21,8 @@ puts Benchmark.measure {
 }
 }
 
-puts "Test: 100k loops accessing a fixnum var and calling to_i 100 times"
-100.times {
+puts "Test STI: 100k loops accessing a fixnum var and calling to_i 100 times"
+10.times {
 puts Benchmark.measure {
   a = 5; 
   i = 0;
@@ -37,6 +37,27 @@ puts Benchmark.measure {
     a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
     a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
     a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i; a.to_i;
+    i += 1;
+  end
+}
+}
+
+puts "Test non-STI: 100k loops accessing an Array var and calling to_a 100 times"
+10.times {
+puts Benchmark.measure {
+  a = []; 
+  i = 0;
+  while i < 100000
+    a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a;
+    a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a;
+    a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a;
+    a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a;
+    a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a;
+    a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a;
+    a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a;
+    a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a;
+    a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a;
+    a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a; a.to_a;
     i += 1;
   end
 }
