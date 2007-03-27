75827c4aafdd52fc14766119e65133e1c9c01924
Charles Oliver Nutter <headius@headius.com>
2007-03-27 10:26:14 +0000

    Adding or updating license preamble
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@3292 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/internal/runtime/RubyNativeThread.java
+++ src/org/jruby/internal/runtime/RubyNativeThread.java
@@ -11,7 +11,7 @@
  * implied. See the License for the specific language governing
  * rights and limitations under the License.
  *
- * Copyright (C) 2004 Charles O Nutter <headius@headius.com>
+ * Copyright (C) 2007 Charles O Nutter <headius@headius.com>
  * Copyright (C) 2004 Jan Arne Petersen <jpetersen@uni-bonn.de>
  *
  * Alternatively, the contents of this file may be used under the terms of
