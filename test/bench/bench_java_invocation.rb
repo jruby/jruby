5140f489b936af531d65bd34293bcbe0c124a3eb
Charles Oliver Nutter <headius@headius.com>
2007-03-27 15:01:34 +0000

    Improved test of Java invocation overhead, calling ByteList.append directly and via Ruby as String#<<
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@3301 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_java_invocation.rb
+++ test/bench/bench_java_invocation.rb
@@ -1,5 +1,24 @@
 require 'benchmark'
 require 'java'
 
-puts Benchmark.measure { sb = java.lang.StringBuffer.new; 100000.times { sb.append("foo") } }
-puts Benchmark.measure { sb = java.lang.StringBuffer.new; 100000.times { sb.append("foo") } }
+puts "Measure bytelist appends (via Java integration)"
+5.times { 
+  puts Benchmark.measure { 
+    sb = org.jruby.util.ByteList.new 
+    foo = org.jruby.util.ByteList.plain("foo") 
+    1000000.times { 
+      sb.append(foo) 
+    } 
+  }
+}
+
+puts "Measure string appends (via normal Ruby)"
+5.times { 
+  puts Benchmark.measure { 
+    str = "" 
+    foo = "foo" 
+    1000000.times { 
+      str << foo 
+    } 
+  } 
+}
