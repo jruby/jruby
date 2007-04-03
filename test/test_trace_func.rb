594f9ee754a55441cbfc9945994cfbdd6aef46bc
Charles Oliver Nutter <headius@headius.com>
2007-04-03 23:46:56 +0000

    JRUBY-591: fix the test; bad output got committed by mistake
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@3370 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/test_trace_func.rb
+++ test/test_trace_func.rb
@@ -12,11 +12,11 @@ class TestTraceFunc < Test::Unit::TestCase
 
     set_trace_func nil
 
-    expected = ["    line test/test_trace_func.rb.rb:10 test_class TestTraceFunc\n",
-      "   class test/test_trace_func.rb.rb:10 test_class TestTraceFunc\n",
-      "     end test/test_trace_func.rb.rb:10 test_class TestTraceFunc\n",
-      "    line test/test_trace_func.rb.rb:13 test_class TestTraceFunc\n",
-      "  c-call test/test_trace_func.rb.rb:13 set_trace_func Kernel  \n"]
+    expected = ["    line test/test_trace_func.rb:10 test_class TestTraceFunc\n",
+    "   class test/test_trace_func.rb:10 test_class TestTraceFunc\n",
+    "     end test/test_trace_func.rb:10 test_class TestTraceFunc\n",
+    "    line test/test_trace_func.rb:13 test_class TestTraceFunc\n",
+    "  c-call test/test_trace_func.rb:13 set_trace_func Kernel  \n"]
     assert_equal(expected, output);
   end
 
@@ -33,13 +33,13 @@ class TestTraceFunc < Test::Unit::TestCase
 
     set_trace_func nil
 
-    expected = ["    line test/test_trace_func.rb.rb:29 test_block_and_vars TestTraceFunc\n",
-      "  c-call test/test_trace_func.rb.rb:29      times  Integer\n",
-      "    line test/test_trace_func.rb.rb:30 test_block_and_vars TestTraceFunc\n",
-      "    line test/test_trace_func.rb.rb:31 test_block_and_vars TestTraceFunc\n",
-      "c-return test/test_trace_func.rb.rb:29      times  Integer\n",
-      "    line test/test_trace_func.rb.rb:34 test_block_and_vars TestTraceFunc\n",
-      "  c-call test/test_trace_func.rb.rb:34 set_trace_func   Kernel\n"]
+    expected = ["    line test/test_trace_func.rb:29 test_block_and_vars TestTraceFunc\n",
+    "  c-call test/test_trace_func.rb:29      times  Integer\n",
+    "    line test/test_trace_func.rb:30 test_block_and_vars TestTraceFunc\n",
+    "    line test/test_trace_func.rb:31 test_block_and_vars TestTraceFunc\n",
+    "c-return test/test_trace_func.rb:29      times  Integer\n",
+    "    line test/test_trace_func.rb:34 test_block_and_vars TestTraceFunc\n",
+    "  c-call test/test_trace_func.rb:34 set_trace_func   Kernel\n"]
     assert_equal(expected, output)
   end
 end
