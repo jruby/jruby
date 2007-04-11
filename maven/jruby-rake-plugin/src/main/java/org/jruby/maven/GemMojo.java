29bd12dea4797b05ece21afbee23982169301334
Nick Sieger <nick@nicksieger.com>
2007-04-11 02:16:38 +0000

    Split Gem mojo into generic "invoke JRuby" mojo and gem-specific mojo that extends it
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@3420 961051c9-f516-0410-bf72-c9f7e237a7b7

--- maven/jruby-rake-plugin/src/main/java/org/jruby/maven/GemMojo.java
+++ maven/jruby-rake-plugin/src/main/java/org/jruby/maven/GemMojo.java
@@ -1,24 +1,16 @@
-
 package org.jruby.maven;
 
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.tools.ant.taskdefs.Java;
 
 /**
  * @goal gem
  */
-public class GemMojo extends AbstractJRubyMojo {
-    /**
-     * @parameter
-     */
-    private String args = null;
-
+public class GemMojo extends JRubyMojo {
     public void execute() throws MojoExecutionException {
         String commandString = "--command gem";
         if (args != null) {
             commandString += " " + args;
         }
-        Java jruby = jruby(commandString.split("\\s+"));
-        jruby.execute();        
+        executeCmd(commandString);
     }
 }
