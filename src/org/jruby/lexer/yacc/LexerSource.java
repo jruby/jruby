9ca48e4ff68fca8d9cb08e600798542ab37712c2
Ola Bini <ola.bini@gmail.com>
2007-04-19 09:45:47 +0000

    Fix for JRUBY-644, by Thomas Corbat. Problem with \\r giving positioning problems when used at line endings
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@3508 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/lexer/yacc/LexerSource.java
+++ src/org/jruby/lexer/yacc/LexerSource.java
@@ -253,25 +253,16 @@ public class LexerSource {
      * @return the current char or EOF (at EOF or on error)
      */
     private char wrappedRead() throws IOException {
-            int c = reader.read();
-        	
-            // If \r\n then just pass along \n (windows)
-            // If \r[^\n] then pass along \n (MAC)
-            if (c == '\r') {
-                if ((c = reader.read()) != '\n') {
-                    unread((char)c);
-                    c = '\n';
-                } else {
-                    // Position within source must reflect the actual offset and column.  Since
-                	// we ate an extra character here (this accounting is normally done in read
-                	// ), we should update position info.
-                    offset++;
-                    column++;
-                }
-            }
-        	
-            return c != -1 ? (char) c : '\0';
-
+        int c = reader.read();
+        
+        // If \r[^\n] then pass along \n (MAC)
+        if (c == '\r') {
+            if(!peek('\n')) {
+                c = '\n';
+            } 
+        }
+        
+        return c != -1 ? (char) c : '\0';
     }
     
     /**
