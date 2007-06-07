e97d967b4d3037764ae832822d5181eba895c9f0
Nick Sieger <nick@nicksieger.com>
2007-06-07 15:39:24 +0000

    JRUBY-1116, JRUBY-1117: copy windows test changes to trunk
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@3856 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/externals/rubinius/spec/language/literals/execution_spec.rb
+++ test/externals/rubinius/spec/language/literals/execution_spec.rb
@@ -3,13 +3,13 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 context "Execution literal" do
   specify "`` should return the result of the executed sub-process" do
     ip = 'world'
-    `echo disc #{ip}`.should == "disc world\n"
+    `echo disc #{ip}`.chomp.should == "disc world"
   end
 
   # NOTE: Interpolation ? It's not consistant with %w for example.
   specify "%x() is the same (with also interpolation)" do
     ip = 'world'
-    %x(echo disc #{ip}).should == "disc world\n"
+    %x(echo disc #{ip}).chomp.should == "disc world"
   end
 
   # NOTE: %X doesn't exist.
