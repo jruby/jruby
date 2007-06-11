d47885ece344c27cc223b4744a76d9b40aa268ad
Ola Bini <ola.bini@gmail.com>
2007-06-11 18:34:46 +0000

    Change benchParser to let hotspot kick in and add the new only one method dispatch test
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@3862 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/benchParser.rb
+++ test/bench/benchParser.rb
@@ -4,13 +4,22 @@ require 'benchmark'
 require 'rbconfig'
 
 # benchmark 100 parses of the RDoc rb parser
-ITER_COUNT = 100
+ITER_COUNT = 25
 filename = Config::CONFIG['rubylibdir'] + "/rdoc/parsers/parse_rb.rb"
 src = File.read(filename)
 
-parsetime = Benchmark.measure { ITER_COUNT.times { JRuby.parse(src, "parse_rb.rb") } }.real
-
 puts "file: " + filename
 puts "size: " + src.size.to_s
-puts "time: " + parsetime.to_s
-puts "average: " + (parsetime / ITER_COUNT).to_s
+
+fulltime = 0
+
+5.times do 
+  parsetime = Benchmark.measure { ITER_COUNT.times { JRuby.parse(src, "parse_rb.rb") } }.real
+  
+  puts "time: " + parsetime.to_s
+  fulltime += parsetime
+end
+
+puts "full time: " + fulltime.to_s
+puts "average: " + (fulltime / (ITER_COUNT*5)).to_s
+
