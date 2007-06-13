a5dd88dbc4b235e3512f8f49a46cd33f19af4990
Nick Sieger <nick@nicksieger.com>
2007-06-13 04:00:48 +0000

    Support signal numbers as well as names
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@3869 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/builtin/jsignal.rb
+++ src/builtin/jsignal.rb
@@ -1,22 +1,28 @@
 module Kernel
+  SIGNALS = {
+    1  => "SIGHUP", 2  => "SIGINT", 3  => "SIGQUIT", 4  => "SIGILL", 5  => "SIGTRAP", 6  => "SIGABRT",
+    7  => "SIGPOLL", 8  => "SIGFPE", 9  => "SIGKILL", 10 => "SIGBUS", 11 => "SIGSEGV", 12 => "SIGSYS",
+    13 => "SIGPIPE", 14 => "SIGALRM", 15 => "SIGTERM", 16 => "SIGURG", 17 => "SIGSTOP", 18 => "SIGTSTP",
+    19 => "SIGCONT", 20 => "SIGCHLD", 21 => "SIGTTIN", 22 => "SIGTTOU", 24 => "SIGXCPU", 25 => "SIGXFSZ",
+    26 => "SIGVTALRM", 27 => "SIGPROF", 30 => "SIGUSR1", 31 => "SIGUSR2"
+  }
   begin
-    class RubySignalHandler
-      include Java::sun.misc.SignalHandler
-      def initialize(proc)
-        @proc = proc
-      end
-      def handle(sig)
-        @proc.call
-      end
-    end
     def trap(sig, &block)
-      if sig.kind_of?(String)
-        sig = sig.sub(/^SIG(.+)/,'\1')
-        signal = Java::sun.misc.Signal
-        signal.handle(signal.new(sig), RubySignalHandler.new(block))
-      else
-        warn "integer signal numbers not supported"
+      sig = SIGNALS[sig] if sig.kind_of?(Fixnum)
+      sig = sig.sub(/^SIG(.+)/,'\1')
+      signal_class = Java::sun.misc.Signal
+      signal_class.send :attr_accessor, :prev_handler
+      signal_object = signal_class.new(sig)
+      signal_handler = Java::sun.misc.SignalHandler.impl do
+        begin
+          block.call
+        ensure
+          # re-register the handler
+          signal_class.handle(signal_object, signal_handler)
+        end
       end
+      handler = signal_class.handle(signal_object, signal_handler)
+      signal_object.prev_handler = handler
     end
   rescue NameError
     def trap(sig, &block)
