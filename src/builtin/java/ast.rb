5940c8381efabb861cee069c8f572f2c3a8f5659
Ola Bini <ola.bini@gmail.com>
2007-07-11 18:18:27 +0000

    Add some more help methods for navigating an AST, including the possiblity to run arbritrary pieces of the tree (provided they make sense in isolation)
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@4004 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/builtin/java/ast.rb
+++ src/builtin/java/ast.rb
@@ -1,4 +1,42 @@
 class Java::OrgJrubyAst::Node
+  POS = org.jruby.lexer.yacc.SimpleSourcePosition.new("-",-1)
+  def [](ix)
+    self.child_nodes[ix]
+  end
+  
+  def first
+    self.child_nodes[0] if self.child_nodes.size > 0
+  end
+  
+  def last
+    self.child_nodes[self.child_nodes.size-1] if self.child_nodes.size > 0
+  end
+  
+  def +(other)
+    blk = org.jruby.ast.BlockNode.new POS
+    blk.add self
+    blk.add other
+    blk
+  end
+  
+  attr_accessor :locals
+  
+  def run
+    unless defined?(JRuby)
+      require 'jruby'
+    end
+    root = self
+    unless org.jruby.ast.RootNode === root
+      pos = POS
+      scope1 = org.jruby.parser.LocalStaticScope.new(nil)
+      scope1.setVariables(java.lang.String[self.locals || 40].new)
+      scope = org.jruby.runtime.DynamicScope.new(scope1, nil)
+      root = org.jruby.ast.RootNode.new(pos, scope, self)
+    end
+
+    JRuby::runtime.eval root
+  end
+  
   def inspect(indent = 0)
     s = ' '*indent + self.class.name.split('::').last
 
@@ -9,7 +47,7 @@ class Java::OrgJrubyAst::Node
       s << " ==#{self.value.inspect}"
     end
 
-    [:receiver_node, :args_node, :iter_node, :body_node, :next_node].each do |mm|
+    [:receiver_node, :args_node, :value_node, :iter_node, :body_node, :next_node].each do |mm|
       if self.respond_to?(mm)
         begin 
           s << "\n#{self.send(mm).inspect(indent+2)}" if self.send(mm)
@@ -35,7 +73,7 @@ class Java::OrgJrubyAst::Node
     content = []
     content << {'name' => self.name} if self.respond_to?(:name)
     content << {'value' => self.value} if self.respond_to?(:value)
-    [:receiver_node, :args_node, :iter_node, :body_node, :next_node].each do |mm|
+    [:receiver_node, :args_node, :value_node, :iter_node, :body_node, :next_node].each do |mm|
       if self.respond_to?(mm)
         content << self.send(mm) if self.send(mm)
       end
