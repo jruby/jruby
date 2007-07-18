cd81364f53249af634c68add458620b1c9a986b0
Charles Oliver Nutter <headius@headius.com>
2007-07-18 05:32:36 +0000

    Committing a second test of Java integration performance
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@4043 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_java_invocation.rb
+++ test/bench/bench_java_invocation.rb
@@ -1,6 +1,8 @@
 require 'benchmark'
 require 'java'
 
+import java.lang.System
+
 puts "Measure bytelist appends (via Java integration)"
 5.times { 
   puts Benchmark.measure { 
@@ -21,4 +23,28 @@ puts "Measure string appends (via normal Ruby)"
       str << foo 
     } 
   } 
+}
+
+puts "Measure System.currentTimeMillis, int becoming Fixnum"
+def System.bench
+  x = nil
+  1000000.times {
+    x = currentTimeMillis
+  }
+end
+
+5.times {
+  puts Benchmark.measure {
+    System.bench
+  }
+}
+
+puts "Measure string.length, integer length to fixnum"
+5.times {
+  puts Benchmark.measure {
+    x = ""
+    1000000.times {
+      y = x.length
+    }
+  }
 }
