e72067a0f8a31b163332dcb59d729a85834340ad
Charles Oliver Nutter <headius@headius.com>
2007-07-27 06:52:01 +0000

    Adding a property to enable the AST inspector
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@4064 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/compiler/ASTInspector.java
+++ src/org/jruby/compiler/ASTInspector.java
@@ -113,10 +113,11 @@ public class ASTInspector {
         hasRestArg = true;
     }
     
+    public static final boolean ENABLED = Boolean.getBoolean("jruby.astInspector.enabled");
+    
     public void inspect(Node node) {
         // TODO: This code effectively disables all inspection-based optimizations; none of them are 100% safe yet
-        boolean disabled = true;
-        if (disabled) disable();
+        if (!ENABLED) disable();
 
         if (node == null) return;
         
