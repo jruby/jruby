e5517abe133271329b73e7e96536510b5a450e6e
Thomas Enebo <tom.enebo@gmail.com>
2007-09-05 21:35:06 +0000

    JRUBY-1242: ClassCastException: org.jruby.ast.NthRefNode w/match variables used as #$1 in here document in string substitution (test case by skaar) JRUBY-1318:  :$`, :$&, :$\, :$+ goes boom JRUBY-1317:  Heredocs ending in non-curly expansion do not include final newline
    git-svn-id: http://svn.codehaus.org/jruby/branches/jruby-1_0@4278 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/testHereDocument.rb
+++ test/testHereDocument.rb
@@ -1,25 +1,8 @@
 require 'test/minirunit'
 
 test_check "Test here document:"
-aString = <<END_OF_STRING
-    The body of the string
-    is the input lines up to
-    one ending with the same
-    text that followed the '<<'
-END_OF_STRING
 
-result = 
-"    The body of the string
-    is the input lines up to
-    one ending with the same
-    text that followed the '<<'
-"
-test_equal("    The body of the string
-    is the input lines up to
-    one ending with the same
-    text that followed the '<<'
-", aString)
-
+# Multiple Heredocs as arguments
 multiDoc = <<-STRING1, <<-STRING2
    Concat
    STRING1
@@ -28,6 +11,7 @@ multiDoc = <<-STRING1, <<-STRING2
 
 test_equal(["   Concat\n","      enate\n"], multiDoc)
 
+# Should retain extra newlines at end of heredocs
 str = <<-EOL
 blah-blah
 
@@ -36,6 +20,8 @@ EOL
 
 test_equal("blah-blah\n\n\n",str)
 
+# - (indent) operation should result in same string
+# as non-indented version
 str1 = <<EOL
 baw-waw
 
@@ -49,3 +35,49 @@ baw-waw
               EOL
               
 test_equal(str1, str2)
+
+$global = "global value"
+s =<<EOT
+#$global
+EOT
+test_equal($global + "\n", s)
+
+value = "some value"
+value.sub!(/\A(\S*)(.*?)(\S*)\Z/m) do |m|
+  <<EOT
+#$global
+EOT
+end
+test_equal($global + "\n", value)
+
+value = "some value"
+value.sub!(/\A(\S*)(.*?)(\S*)\Z/m) do |m|
+  <<EOT
+#{$1} other #{$3}
+EOT
+end
+test_equal("some other value\n", value)
+
+value = "some value"
+value.sub!(/\A(\S*)(.*?)(\S*)\Z/m) do |m|
+  <<EOT
+#$1 other #$3
+EOT
+end
+test_equal("some other value\n", value)
+
+value = "some value"
+value.sub!(/\A(\S*)(.*?)(\S*)\Z/m) do |m|
+  <<"EOT"
+#{$1} other #{$3}
+EOT
+end
+test_equal("some other value\n", value)
+
+value = "some value"
+value.sub!(/\A(\S*)(.*?)(\S*)\Z/m) do |m|
+  <<'EOT'
+#$1 other #$3
+EOT
+end
+test_equal('#$1 other #$3', value.chomp)
