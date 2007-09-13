3571a2520659322130a68d0cb08a51345238d771
Charles Oliver Nutter <headius@headius.com>
2007-09-13 22:02:40 +0000

    Fix for JRUBY-1348, pass absolute path to process-launched chmod command
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@4318 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/util/Chmod.java
+++ src/org/jruby/util/Chmod.java
@@ -61,7 +61,7 @@ public class Chmod {
         } else {
             // slow version
             try {
-                Process chmod = Runtime.getRuntime().exec("chmod " + mode + " " + file.getName());
+                Process chmod = Runtime.getRuntime().exec("/bin/chmod " + mode + " " + file.getAbsolutePath());
                 chmod.waitFor();
                 return chmod.exitValue();
             } catch (IOException ioe) {
