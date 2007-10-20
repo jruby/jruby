be5d93fa4e7a68a84de093252c0ca78fc37ec4b2
Marcin Mielżyński <lopx@gazeta.pl>
2007-10-20 01:30:48 +0000

    ARGF needs a native allocator (CCE during ARGF.clone).
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@4632 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/RubyArgsFile.java
+++ src/org/jruby/RubyArgsFile.java
@@ -35,6 +35,7 @@ import org.jruby.anno.JRubyMethod;
 import org.jruby.runtime.Arity;
 import org.jruby.runtime.Block;
 import org.jruby.runtime.CallbackFactory;
+import org.jruby.runtime.ObjectAllocator;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.builtin.IRubyObject;
 
@@ -60,6 +61,7 @@ public class RubyArgsFile extends RubyObject {
         CallbackFactory callbackFactory = getRuntime().callbackFactory(RubyArgsFile.class);
         
         RubyClass argfClass = getMetaClass();
+        argfClass.getRealClass().setAllocator(ARGF_ALLOCATOR);
 
         argfClass.defineAnnotatedMethods(RubyArgsFile.class);
         
@@ -73,6 +75,12 @@ public class RubyArgsFile extends RubyObject {
         // it passes tests now.
         //        currentFile = (RubyIO) getRuntime().getGlobalVariables().get("$stdin");
     }
+
+    private static ObjectAllocator ARGF_ALLOCATOR = new ObjectAllocator() {
+        public IRubyObject allocate(Ruby runtime, RubyClass klass) {
+            return new RubyArgsFile(runtime);
+        }
+    };
 
     protected boolean nextArgsFile() {
         RubyArray args = (RubyArray)getRuntime().getGlobalVariables().get("$*");
