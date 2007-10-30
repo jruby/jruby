f0ee772866dd50af51f684814d5cf36beb6cb0eb
Nick Sieger <nick@nicksieger.com>
2007-10-30 21:51:36 +0000

    JRUBY-1484: trunk fix for .bat arg processing - also bump up default memory as with shell scripts
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@4809 961051c9-f516-0410-bf72-c9f7e237a7b7

--- bin/_jrubyvmopts.bat
+++ bin/_jrubyvmopts.bat
@@ -1,5 +1,5 @@
 @echo off
-set _MEM=-Xmx256m
+set _MEM=-Xmx378m
 set _STK=-Xss1024k
 set _VM_OPTS=
 set _RUBY_OPTS=
@@ -10,8 +10,12 @@ rem Can you believe I'm rewriting batch arg processing in batch files because ba
 rem file arg processing sucks so bad? Can you believe this is even possible?
 rem http://support.microsoft.com/kb/71247
 
+rem escape any quotes. use -q == ", -d == -.
+set _ARGS=%*
+set _ARGS=%_ARGS:-=-d%
+set _ARGS=%_ARGS:"=-q%
 rem prequote all args for 'for' statement
-set _ARGS="%*"
+set _ARGS="%_ARGS%"
 
 :vmoptsLoop
 rem split args by spaces into first and rest
@@ -29,11 +33,21 @@ goto :EOF
 :procarg
 if "%_CMP%" == "" goto vmoptsDone
 
-if not "%_CMP:~0,2%" == "-J" (
-  set _RUBY_OPTS=%_RUBY_OPTS% %_CMP%
-  goto vmoptsNext
-)
+rem now unescape -q and -d
+set _CMP=%_CMP:-q="%
+set _CMP=%_CMP:-d=-%
+set _CMP1=%_CMP:~0,1%
 
+rem detect first character is a quote; skip directly to rubyarg
+rem this avoids a batch syntax error
+if "%_CMP1:"=\\%" == "\\" goto rubyarg
+if "%_CMP:~0,2%" == "-J" goto jvmarg
+
+:rubyarg
+set _RUBY_OPTS=%_RUBY_OPTS% %_CMP%
+goto vmoptsNext
+
+:jvmarg
 set _VAL=%_CMP:~2%
 
 if "%_VAL:~0,4%" == "-Xmx" (
@@ -52,6 +66,8 @@ set _VM_OPTS=%_VM_OPTS% %_VAL%
 set _CMP=
 goto vmoptsLoop
 
+@echo off
+
 :vmoptsDone
 set _VM_OPTS=%_VM_OPTS% %_MEM% %_STK% %_DFLT_VM_OPTS%
 set _DFLT_VM_OPTS=
@@ -60,3 +76,4 @@ set _STK=
 set _ARGS=
 set _VAL=
 set _CMP=
+set _CMP1=
