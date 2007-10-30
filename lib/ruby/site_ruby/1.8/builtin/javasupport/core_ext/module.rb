060ae0a5e8527a28b5a47b108a863c94bcfb9a26
Charles Oliver Nutter <headius@headius.com>
2007-10-30 20:58:40 +0000

    Some fixes to import_java, wasn't working correctly the way it was wired up.
    git-svn-id: http://svn.codehaus.org/jruby/branches/jruby-1_0@4802 961051c9-f516-0410-bf72-c9f7e237a7b7

--- lib/ruby/site_ruby/1.8/builtin/javasupport/core_ext/module.rb
+++ lib/ruby/site_ruby/1.8/builtin/javasupport/core_ext/module.rb
@@ -34,13 +34,6 @@ class Module
   # to try that package when constants are missing.
   # DEPRECATED: This method conflicts with rake and is not recommended for general use.
   def import(package_name)
-    import_java(package_name);
-  end
-  
-  # Imports the package specified by +package_name+, first by trying to scan JAR resources
-  # for the file in question, and failing that by adding a const_missing hook
-  # to try that package when constants are missing.
-  def import_java(package_name)
     return super(package_name) if package_name.respond_to?(:java_class) || package_name.split(/\./).last =~ /^[A-Z]/
 
     package_name = package_name._name if package_name.respond_to?(:_name)
@@ -56,6 +49,13 @@ class Module
       
       JavaUtilities.create_proxy_class(class_name, fqclass, self)
     end
+  end
+  
+  # Imports the package specified by +package_name+, first by trying to scan JAR resources
+  # for the file in question, and failing that by adding a const_missing hook
+  # to try that package when constants are missing.
+  def import_java(package_name)
+    import(package_name)
   end
 
   def java_alias(new_id, old_id)
