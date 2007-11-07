08c5dabebdd126f9647ba6397e57ef0cf8f5b537
Charles Oliver Nutter <headius@headius.com>
2007-11-07 01:29:19 +0000

    Added indexed method benchmarks
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@4870 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_full_startup.rb
+++ test/bench/bench_full_startup.rb
@@ -1,13 +1,23 @@
 require 'benchmark'
 
-Benchmark.bm(30) do |bm|
+Benchmark.bm(50) do |bm|
   5.times do
-    bm.report('launching JRuby client VM') do
+    bm.report('launching JRuby client VM, indexed methods') do
+      system "jruby -O -J-Djruby.indexed.methods=true -e \"require 'net/http'; require 'irb'\""
+    end
+  end
+  5.times do
+    bm.report('launching JRuby server VM, indexed methods') do
+      system "jruby -J-server -O -J-Djruby.indexed.methods=true -e \"require 'net/http'; require 'irb'\""
+    end
+  end
+  5.times do
+    bm.report('launching JRuby client VM, individual methods') do
       system "jruby -O -e \"require 'net/http'; require 'irb'\""
     end
   end
   5.times do
-    bm.report('launching JRuby server VM') do
+    bm.report('launching JRuby server VM, individual methods') do
       system "jruby -J-server -O -e \"require 'net/http'; require 'irb'\""
     end
   end
