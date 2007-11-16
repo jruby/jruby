93dfc2e3cf0070ed5dda0bfb537b951f76cbb043
Charles Oliver Nutter <headius@headius.com>
2007-11-16 03:22:56 +0000

    Missed JavaCompiledScript. Doh!
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@4946 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ src/org/jruby/runtime/load/JavaCompiledScript.java
@@ -0,0 +1,85 @@
+/***** BEGIN LICENSE BLOCK *****
+ * Version: CPL 1.0/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Common Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * Copyright (C) 2007 The JRuby Community
+ * 
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the CPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the CPL, the GPL or the LGPL.
+ ***** END LICENSE BLOCK *****/
+package org.jruby.runtime.load;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.jruby.Ruby;
+import org.jruby.ast.executable.Script;
+import org.jruby.runtime.Block;
+import org.jruby.runtime.builtin.IRubyObject;
+import org.jruby.util.JRubyClassLoader;
+import org.objectweb.asm.ClassReader;
+
+public class JavaCompiledScript implements Library {
+    private final LoadServiceResource resource;
+    
+    public JavaCompiledScript(LoadServiceResource resource) {
+        this.resource = resource;
+    }
+
+    public void load(Ruby runtime) {
+        InputStream in = null;
+        try {
+            in = new BufferedInputStream(resource.getURL().openStream());
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buf = new byte[8196];
+            int read = 0;
+            while ((read = in.read(buf)) != -1) {
+                baos.write(buf, 0, read);
+            }
+            buf = baos.toByteArray();
+            JRubyClassLoader jcl = runtime.getJRubyClassLoader();
+            ClassReader cr = new ClassReader(buf);
+            String className = cr.getClassName().replace('/', '.');
+
+            Class clazz = null;
+            try {
+                clazz = jcl.loadClass(className);
+            } catch (ClassNotFoundException cnfe) {
+                clazz = jcl.defineClass(className, buf);
+            }
+            
+            ((Script)clazz.newInstance()).load(runtime.getCurrentContext(), runtime.getTopSelf(), IRubyObject.NULL_ARRAY, Block.NULL_BLOCK);
+        } catch (IOException e) {
+            throw runtime.newIOErrorFromException(e);
+        } catch (InstantiationException ie) {
+            throw runtime.newLoadError("Error loading compiled script: " + ie);
+        } catch (IllegalAccessException iae) {
+            throw runtime.newLoadError("Error loading compiled script: " + iae);
+        } finally {
+            try {
+                in.close();
+            } catch (IOException ioe) {
+                throw runtime.newIOErrorFromException(ioe);
+            }
+        }
+    }
+}
