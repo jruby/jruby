04c6d8aff4aacaea2e8818edf796b3ce81abac5e
Charles Oliver Nutter <headius@headius.com>
2007-11-20 11:13:27 +0000

    Beginnings of Ruby-based compiler; Signature is a Ruby version of CodegenUtils, but with a little cleaner look.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@4972 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ lib/ruby/site_ruby/1.8/compiler/signature.rb
@@ -0,0 +1,98 @@
+include Java
+
+module Compiler
+  module JavaTypes
+    import java.lang.Object
+    import java.lang.Byte
+    import java.lang.Boolean
+    import java.lang.Short
+    import java.lang.Character
+    import java.lang.Integer
+    import java.lang.Long
+    import java.lang.Float
+    import java.lang.Double
+    import java.lang.Void
+  end
+  module Signature
+    def classname(path)
+      path.gsub('/', '.')
+    end
+    alias c classname
+    module_function :classname, :c
+    
+    def path(cls)
+      cls = cls.java_class if Class === cls
+      cls.name.gsub('.', '/')
+    end
+    alias p path
+    module_function :path, :p
+    
+    def class_id(cls)
+      cls = cls.java_class if Class === cls
+      if cls.array?
+        cls = cls.component_type
+        if cls.primitive?
+          case cls
+          when JavaTypes::Byte::TYPE
+            return "[B"
+          when JavaTypes::Boolean::TYPE
+            return "[Z"
+          when JavaTypes::Short::TYPE
+            return "[S"
+          when JavaTypes::Character::TYPE
+            return "[C"
+          when JavaTypes::Integer::TYPE
+            return "[I"
+          when JavaTypes::Long::TYPE
+            return "[J"
+          when JavaTypes::Float::TYPE
+            return "[F"
+          when JavaTypes::Double::TYPE
+            return "[D"
+          else
+            raise "Unknown type in compiler: " + cls.name
+          end
+        else
+          return "[#{class_id(cls)}"
+        end
+      else
+        if cls.primitive?
+          case cls
+          when JavaTypes::Byte::TYPE
+            return "B"
+          when JavaTypes::Boolean::TYPE
+            return "Z"
+          when JavaTypes::Short::TYPE
+            return "S"
+          when JavaTypes::Character::TYPE
+            return "C"
+          when JavaTypes::Integer::TYPE
+            return "I"
+          when JavaTypes::Long::TYPE
+            return "J"
+          when JavaTypes::Float::TYPE
+            return "F"
+          when JavaTypes::Double::TYPE
+            return "D"
+          when JavaTypes::Void::TYPE
+            return "V"
+          else
+            raise "Unknown type in compiler: " + cls.name
+          end
+        else
+          return "L#{path(cls)};"
+        end
+      end
+    end
+    alias ci class_id
+    module_function :class_id, :ci
+    
+    def signature(return_class, *arg_classes)
+      sig_string = "("
+      arg_classes.each {|arg_class| sig_string << class_id(arg_class)}
+      sig_string << ")#{class_id(return_class)}"
+    end
+    alias sig signature
+    module_function :signature, :sig
+  end
+end
---
+++ test/compiler/test_signature.rb
@@ -0,0 +1,60 @@
+require 'test/unit'
+require 'compiler/signature'
+
+class TestSignature < Test::Unit::TestCase
+  include Java
+  import java.util.ArrayList
+  import java.lang.String
+  import java.lang.Byte
+  import java.lang.Short
+  import java.lang.Character
+  import java.lang.Boolean
+  import java.lang.Integer
+  import java.lang.Long
+  import java.lang.Float
+  import java.lang.Double
+  import java.lang.Void
+
+  Signature = Compiler::Signature
+  
+  def test_classname(path)
+    assert_equal("java.util.ArrayList", "java/util/ArrayList")
+  end
+  
+  def test_path
+    assert_equal("java/util/ArrayList", Signature.path(ArrayList))
+    assert_equal("java/util/ArrayList", Signature.p(ArrayList))
+  end
+  
+  def test_class_id
+    assert_equal("Ljava/util/ArrayList;", Signature.class_id(ArrayList))
+    assert_equal("Ljava/util/ArrayList;", Signature.ci(ArrayList))
+    assert_equal("B", Signature.class_id(Byte::TYPE))
+    assert_equal("Z", Signature.class_id(Boolean::TYPE))
+    assert_equal("S", Signature.class_id(Short::TYPE))
+    assert_equal("C", Signature.class_id(Character::TYPE))
+    assert_equal("I", Signature.class_id(Integer::TYPE))
+    assert_equal("J", Signature.class_id(Long::TYPE))
+    assert_equal("F", Signature.class_id(Float::TYPE))
+    assert_equal("D", Signature.class_id(Double::TYPE))
+    assert_equal("V", Signature.class_id(Void::TYPE))
+  end
+  
+  def test_signature
+    assert_equal("(Ljava/util/ArrayList;)V", Signature.signature(Void::TYPE, ArrayList))
+    assert_equal("(Ljava/util/ArrayList;)V", Signature.sig(Void::TYPE, ArrayList))
+    assert_equal("([Ljava/lang/String;)V", Signature.signature(Void::TYPE, String[]))
+    assert_equal("(BZSCIJFDLjava/util/ArrayList;)V",
+      Signature.signature(
+        Void::TYPE,
+        Byte::TYPE,
+        Boolean::TYPE,
+        Short::TYPE,
+        Character::TYPE,
+        Integer::TYPE,
+        Long::TYPE,
+        Float::TYPE,
+        Double::TYPE,
+        ArrayList))
+  end
+end
