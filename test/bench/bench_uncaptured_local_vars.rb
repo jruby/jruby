c9da4109fc1410df7ebbc889034878b5a402acab
Charles Oliver Nutter <headius@headius.com>
2007-11-22 04:30:54 +0000

    Add a local var assignment test to the uncaptured var benchmark
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@4985 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_uncaptured_local_vars.rb
+++ test/bench/bench_uncaptured_local_vars.rb
@@ -1,5 +1,6 @@
 require 'benchmark'
 
+puts '10M * 100 local var accesses'
 10.times {
   puts Benchmark.measure {
     a = 1
@@ -16,6 +17,28 @@ require 'benchmark'
       a; a; a; a; a; a; a; a; a; a
       a; a; a; a; a; a; a; a; a; a
       a; a; a; a; a; a; a; a; a; a
+      a += 1
+    end
+  }
+}
+
+puts '10M * 100 local var assignments and retrievals'
+10.times {
+  puts Benchmark.measure {
+    a = 1
+    # contained closure forces heap-based vars in compatibility mode
+    1.times { }
+    while a < 10_000_000
+      a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a
+      a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a
+      a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a
+      a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a
+      a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a
+      a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a
+      a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a
+      a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a
+      a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a
+      a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a; a=a
       a += 1
     end
   }
