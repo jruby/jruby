526f6eccbdb04517ab87ab3c126b69ead32f0e61
Charles Oliver Nutter <headius@headius.com>
2007-11-23 07:29:25 +0000

    modify hash literal bench to accept a TIMES param and add a control loop
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@4995 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_literal_hash.rb
+++ test/bench/bench_literal_hash.rb
@@ -1,20 +1,27 @@
 require 'benchmark'
 
+TIMES = (ARGV[0] || 5).to_i
 Benchmark.bm(30) {|bm|
-  5.times {
+  TIMES.times {
+    bm.report("loop alone") {
+      a = 1; while a < 100_000; a += 1; end
+    }
+  }
+
+  TIMES.times {
     bm.report("no-element hash") {
       a = 1; while a < 100_000; {};{};{};{};{};{};{};{};{};{}; a += 1; end
     }
   }
 
-  5.times {
+  TIMES.times {
     bm.report("1-element hash") {
       x = 1
       a = 1; while a < 100_000; {x=>x}; {x=>x}; {x=>x}; {x=>x}; {x=>x}; {x=>x}; {x=>x}; {x=>x}; {x=>x}; {x=>x}; a += 1; end
     }
   }
 
-  5.times {
+  TIMES.times {
     bm.report("2-element hash") {
       x = 1
       y = 2
@@ -22,7 +29,7 @@ Benchmark.bm(30) {|bm|
     }
   }
 
-  5.times {
+  TIMES.times {
     bm.report("3-element hash") {
       x = 1
       y = 2
@@ -31,7 +38,7 @@ Benchmark.bm(30) {|bm|
     }
   }
 
-  5.times {
+  TIMES.times {
     bm.report("4-element hash") {
       x = 1
       y = 2
