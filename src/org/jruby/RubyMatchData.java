jruby/jruby

commit 3ebdff689db8e1e3e87a5dc4db546047d74d7fc9

First phase of Joni related String/Regexp optimizations: - Major RubyRegexp and MatchData cleanup (Compiler/ASTInterpreter synchronized). - Enabled lazy Region instance creation (only when groups defined). - Heavy lifting for String#scan, String#sub, String#gsub, String#split by direct joni Matcher access and smarter backref management. - Enabled joni Matcher reusing for aforementioned methods. - Enabled thread local Regexp cache for String arguments that are converted to regexps. - Global variable ($=) made ineffective. In most cases the modified methods are now 2x - 5x faster (depending on string size)
git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@5207 961051c9-f516-0410-bf72-c9f7e237a7b7

src/org/jruby/RubyMatchData.java
@@ -45,7 +45,9 @@ import org.jruby.runtime.builtin.IRubyObject;
  * @author olabini
  */
 public class RubyMatchData extends RubyObject {
-    Region regs;
+    Region regs;        // captures
+    int begin;          // begin and end are used when not groups defined
+    int end;
     RubyString str;
     
     public static RubyClass createMatchDataClass(Ruby runtime) {
@@ -54,10 +56,10 @@ public class RubyMatchData extends RubyObject {
         runtime.setMatchData(matchDataClass);
         runtime.defineGlobalConstant("MatchingData", matchDataClass);
         matchDataClass.kindOf = new RubyModule.KindOf() {
-                public boolean isKindOf(IRubyObject obj, RubyModule type) {
-                    return obj instanceof RubyMatchData;
-                }
-            };
+            public boolean isKindOf(IRubyObject obj, RubyModule type) {
+                return obj instanceof RubyMatchData;
+            }
+        };
 
         CallbackFactory callbackFactory = runtime.callbackFactory(RubyMatchData.class);
 
@@ -76,28 +78,37 @@ public class RubyMatchData extends RubyObject {
     public final static int MATCH_BUSY = USER2_F;
 
     // rb_match_busy
-    public void use() { 
-        this.flags |= MATCH_BUSY; 
+    public final void use() {
+        flags |= MATCH_BUSY; 
     }
 
     public final boolean used() {
-        return (this.flags & MATCH_BUSY) != 0;
+        return (flags & MATCH_BUSY) != 0;
     }
 
     private RubyArray match_array(int start) {
-        RubyArray arr = getRuntime().newArray(regs.numRegs-start);
-        for(int i=start;i<regs.numRegs;i++) {
-            if(regs.beg[i] == -1) {
-                arr.append(getRuntime().getNil());
+        if (regs == null) {
+            if (begin == -1) {
+                return getRuntime().newArray(getRuntime().getNil());
             } else {
-                RubyString ss = str.makeShared(regs.beg[i], regs.end[i] - regs.beg[i]);
-                if(isTaint()) {
-                    ss.setTaint(true); 
-                }
-                arr.append(ss);
+                RubyString ss = str.makeShared(begin, end - begin);
+                if (isTaint()) ss.setTaint(true);
+                return getRuntime().newArray(ss);
             }
+        } else {
+            RubyArray arr = getRuntime().newArray(regs.numRegs - start);
+            for (int i=start; i<regs.numRegs; i++) {
+                if (regs.beg[i] == -1) {
+                    arr.append(getRuntime().getNil());
+                } else {
+                    RubyString ss = str.makeShared(regs.beg[i], regs.end[i] - regs.beg[i]);
+                    if (isTaint()) ss.setTaint(true); 
+                    arr.append(ss);
+                }
+            }
+            return arr;
         }
-        return arr;
+        
     }
 
     public IRubyObject group(long n) {
@@ -157,7 +168,7 @@ public class RubyMatchData extends RubyObject {
      */
     @JRubyMethod(name = {"size", "length"})
     public IRubyObject size() {
-        return getRuntime().newFixnum(regs.numRegs);
+        return regs == null ? RubyFixnum.one(getRuntime()) : RubyFixnum.newFixnum(getRuntime(), regs.numRegs);
     }
 
     /** match_begin
@@ -166,14 +177,16 @@ public class RubyMatchData extends RubyObject {
     @JRubyMethod(name = "begin", required = 1)
     public IRubyObject begin(IRubyObject index) {
         int i = RubyNumeric.num2int(index);
-        
-        if(i < 0 || regs.numRegs <= i) {
-            throw getRuntime().newIndexError("index " + i + " out of matches");
+
+        if (regs == null) {
+            if (i != 0) throw getRuntime().newIndexError("index " + i + " out of matches");
+            if (begin < 0) return getRuntime().getNil();
+            return RubyFixnum.newFixnum(getRuntime(), begin);
+        } else {
+            if (i < 0 || regs.numRegs <= i) throw getRuntime().newIndexError("index " + i + " out of matches");
+            if (regs.beg[i] < 0) return getRuntime().getNil();
+            return RubyFixnum.newFixnum(getRuntime(), regs.beg[i]);
         }
-        if(regs.beg[i] < 0) {
-            return getRuntime().getNil();
-        }
-        return getRuntime().newFixnum(regs.beg[i]);
     }
 
     /** match_end
@@ -182,14 +195,16 @@ public class RubyMatchData extends RubyObject {
     @JRubyMethod(name = "end", required = 1)
     public IRubyObject end(IRubyObject index) {
         int i = RubyNumeric.num2int(index);
-        
-        if(i < 0 || regs.numRegs <= i) {
-            throw getRuntime().newIndexError("index " + i + " out of matches");
+
+        if (regs == null) {
+            if (i != 0) throw getRuntime().newIndexError("index " + i + " out of matches");
+            if (end < 0) return getRuntime().getNil();
+            return RubyFixnum.newFixnum(getRuntime(), begin);
+        } else {
+            if (i < 0 || regs.numRegs <= i) throw getRuntime().newIndexError("index " + i + " out of matches");
+            if (regs.end[i] < 0) return getRuntime().getNil();
+            return RubyFixnum.newFixnum(getRuntime(), regs.end[i]);
         }
-        if(regs.end[i] < 0) {
-            return getRuntime().getNil();
-        }
-        return getRuntime().newFixnum(regs.end[i]);
     }
 
     /** match_offset
@@ -198,15 +213,17 @@ public class RubyMatchData extends RubyObject {
     @JRubyMethod(name = "offset", required = 1)
     public IRubyObject offset(IRubyObject index) {
         int i = RubyNumeric.num2int(index);
+        Ruby runtime = getRuntime();
 
-        if(i < 0 || regs.numRegs <= i) {
-            throw getRuntime().newIndexError("index " + i + " out of matches");
+        if (regs == null) {
+            if (i != 0) throw getRuntime().newIndexError("index " + i + " out of matches");
+            if (begin < 0) return runtime.newArray(runtime.getNil(), runtime.getNil());
+            return runtime.newArray(RubyFixnum.newFixnum(runtime, begin),RubyFixnum.newFixnum(runtime, end));            
+        } else {
+            if (i < 0 || regs.numRegs <= i) throw runtime.newIndexError("index " + i + " out of matches");
+            if (regs.beg[i] < 0) return runtime.newArray(runtime.getNil(), runtime.getNil());
+            return runtime.newArray(RubyFixnum.newFixnum(runtime, regs.beg[i]),RubyFixnum.newFixnum(runtime, regs.end[i]));
         }
-
-        if(regs.beg[i] < 0) {
-            return getRuntime().newArray(getRuntime().getNil(),getRuntime().getNil());
-        }
-        return getRuntime().newArray(getRuntime().newFixnum(regs.beg[i]),getRuntime().newFixnum(regs.end[i]));
     }
 
     /** match_pre_match
@@ -214,13 +231,17 @@ public class RubyMatchData extends RubyObject {
      */
     @JRubyMethod(name = "pre_match")
     public IRubyObject pre_match() {
-        if(regs.beg[0] == -1) {
-            return getRuntime().getNil();
+        RubyString ss;
+        
+        if (regs == null) {
+            if(begin == -1) return getRuntime().getNil();
+            ss = str.makeShared(0, begin);
+        } else {
+            if(regs.beg[0] == -1) return getRuntime().getNil();
+            ss = str.makeShared(0, regs.beg[0]);
         }
-        RubyString ss = str.makeShared(0, regs.beg[0]);
-        if(isTaint()) {
-            ss.setTaint(true);
-        }
+        
+        if (isTaint()) ss.setTaint(true);
         return ss;
     }
 
@@ -229,13 +250,17 @@ public class RubyMatchData extends RubyObject {
      */
     @JRubyMethod(name = "post_match")
     public IRubyObject post_match() {
-        if(regs.beg[0] == -1) {
-            return getRuntime().getNil();
+        RubyString ss;
+        
+        if (regs == null) {
+            if (begin == -1) return getRuntime().getNil();
+            ss = str.makeShared(end, str.getByteList().length() - end);
+        } else {
+            if (regs.beg[0] == -1) return getRuntime().getNil();
+            ss = str.makeShared(regs.end[0], str.getByteList().length() - regs.end[0]);
         }
-        RubyString ss = str.makeShared(regs.end[0], str.getByteList().length() - regs.end[0]);
-        if(isTaint()) {
-            ss.setTaint(true);
-        }
+        
+        if(isTaint()) ss.setTaint(true);
         return ss;
     }
 
@@ -245,12 +270,8 @@ public class RubyMatchData extends RubyObject {
     @JRubyMethod(name = "to_s")
     public IRubyObject to_s() {
         IRubyObject ss = RubyRegexp.last_match(this);
-        if(ss.isNil()) {
-            ss = RubyString.newEmptyString(getRuntime(), getRuntime().getString());
-        }
-        if(isTaint()) {
-            ss.setTaint(true);
-        }
+        if (ss.isNil()) ss = RubyString.newEmptyString(getRuntime());
+        if (isTaint()) ss.setTaint(true);
         return ss;
     }
 

src/org/jruby/RubyGlobal.java
@@ -132,7 +132,7 @@ public class RubyGlobal {
         runtime.defineVariable(new LastExitStatusVariable(runtime, "$?"));
 
         runtime.defineVariable(new ErrorInfoGlobalVariable(runtime, "$!", runtime.getNil()));
-        runtime.defineVariable(new GlobalVariable(runtime, "$=", runtime.getFalse()));
+        runtime.defineVariable(new NonEffectiveGlobalVariable(runtime, "$=", runtime.getFalse()));
         runtime.defineVariable(new GlobalVariable(runtime, "$;", runtime.getNil()));
 
         runtime.defineVariable(new SafeGlobalVariable(runtime, "$SAFE"));
@@ -212,6 +212,22 @@ public class RubyGlobal {
         runtime.defineGlobalConstant("ENV_JAVA", new StringOnlyRubyHash(
                 runtime, systemProps, runtime.getNil()));
         
+    }
+
+    private static class NonEffectiveGlobalVariable extends GlobalVariable {
+        public NonEffectiveGlobalVariable(Ruby runtime, String name, IRubyObject value) {
+            super(runtime, name, value);
+        }
+
+        public IRubyObject set(IRubyObject value) {
+            runtime.getWarnings().warn("warning: variable " + name + " is no longer effective; ignored");
+            return value;
+        }
+
+        public IRubyObject get() {
+            runtime.getWarnings().warn("warning: variable " + name + " is no longer effective");
+            return runtime.getFalse();
+        }
     }
 
     private static class LastExitStatusVariable extends GlobalVariable {

src/org/jruby/ast/RegexpNode.java
@@ -76,10 +76,6 @@ public class RegexpNode extends Node implements ILiteralNode {
         return value;
     }
 
-    public int getFlags() {
-        return options;
-    }
-
     public void setPattern(RubyRegexp p) {
         this.pattern = p;
         this.pattern.setLiteral();

src/org/jruby/compiler/ASTCompiler.java
@@ -1722,17 +1722,7 @@ public class ASTCompiler {
                     }
                 };
 
-        int opts = dregexpNode.getOptions();
-        String lang = ((opts & 16) != 0) ? "n" : null;
-        if ((opts & 48) == 48) { // param s
-            lang = "s";
-        } else if ((opts & 32) == 32) { // param e
-            lang = "e";
-        } else if ((opts & 64) != 0) { // param s
-            lang = "u";
-        }
-
-        context.createNewRegexp(createStringCallback, opts, lang);
+        context.createNewRegexp(createStringCallback, dregexpNode.getOptions());
     }
 
     public static void compileDStr(Node node, MethodCompiler context) {
@@ -2739,17 +2729,7 @@ public class ASTCompiler {
 
         RegexpNode reNode = (RegexpNode) node;
 
-        int opts = reNode.getOptions();
-        String lang = ((opts & 16) == 16) ? "n" : null;
-        if ((opts & 48) == 48) { // param s
-            lang = "s";
-        } else if ((opts & 32) == 32) { // param e
-            lang = "e";
-        } else if ((opts & 64) != 0) { // param u
-            lang = "u";
-        }
-
-        context.createNewRegexp(reNode.getValue(), reNode.getOptions(), lang);
+        context.createNewRegexp(reNode.getValue(), reNode.getOptions());
     }
 
     public static void compileRescue(Node node, MethodCompiler context) {

src/org/jruby/compiler/MethodCompiler.java
@@ -388,8 +388,8 @@ public interface MethodCompiler {
 
     public void match3();
 
-    public void createNewRegexp(ByteList value, int options, String lang);
-    public void createNewRegexp(CompilerCallback createStringCallback, int options, String lang);
+    public void createNewRegexp(ByteList value, int options);
+    public void createNewRegexp(CompilerCallback createStringCallback, int options);
     
     public void pollThreadEvents();
 

src/org/jruby/compiler/impl/StandardASMCompiler.java
@@ -1320,7 +1320,7 @@ public class StandardASMCompiler implements ScriptCompiler, Opcodes {
             invokeUtilityMethod("match3", cg.sig(IRubyObject.class, RubyRegexp.class, IRubyObject.class, ThreadContext.class));
         }
 
-        public void createNewRegexp(final ByteList value, final int options, final String lang) {
+        public void createNewRegexp(final ByteList value, final int options) {
             String regexpField = getNewConstant(cg.ci(RubyRegexp.class), "lit_reg_");
 
             // in current method, load the field to see if we've created a Pattern yet
@@ -1337,13 +1337,7 @@ public class StandardASMCompiler implements ScriptCompiler, Opcodes {
             method.ldc(regexpString); //[R, rS]
             method.ldc(new Integer(options)); //[R, rS, opts]
 
-            if (null == lang) {
-                method.aconst_null(); //[R, rS, opts, null]
-            } else {
-                method.ldc(lang); //[R, rS, opts, lang]
-            }
-
-            method.invokestatic(cg.p(RubyRegexp.class), "newRegexp", cg.sig(RubyRegexp.class, cg.params(Ruby.class, String.class, Integer.TYPE, String.class))); //[reg]
+            method.invokestatic(cg.p(RubyRegexp.class), "newRegexp", cg.sig(RubyRegexp.class, cg.params(Ruby.class, String.class, Integer.TYPE))); //[reg]
 
             method.aload(THIS); //[reg, T]
             method.swap(); //[T, reg]
@@ -1353,7 +1347,7 @@ public class StandardASMCompiler implements ScriptCompiler, Opcodes {
             method.getfield(classname, regexpField, cg.ci(RubyRegexp.class)); 
         }
 
-        public void createNewRegexp(CompilerCallback createStringCallback, final int options, final String lang) {
+        public void createNewRegexp(CompilerCallback createStringCallback, final int options) {
             boolean onceOnly = (options & ReOptions.RE_OPTION_ONCE) != 0;   // for regular expressions with the /o flag
             Label alreadyCreated = null;
             String regexpField = null;
@@ -1375,13 +1369,7 @@ public class StandardASMCompiler implements ScriptCompiler, Opcodes {
             createStringCallback.compile(this);
             method.ldc(new Integer(options));
 
-            if (null == lang) {
-                method.aconst_null();
-            } else {
-                method.ldc(lang);
-            }
-
-            method.invokestatic(cg.p(RubyRegexp.class), "newRegexp", cg.sig(RubyRegexp.class, cg.params(Ruby.class, String.class, Integer.TYPE, String.class))); //[reg]
+            method.invokestatic(cg.p(RubyRegexp.class), "newRegexp", cg.sig(RubyRegexp.class, cg.params(Ruby.class, String.class, Integer.TYPE))); //[reg]
 
             // only alter the code if the /o flag was present
             if (onceOnly) {

commit dd4b4c2467f881d7608ddebcbdc0b7293057b682

Fix bug introduced in r5207 and restore ActionPack test suite to pre r5207 state.
git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@5217 961051c9-f516-0410-bf72-c9f7e237a7b7

src/org/jruby/RubyMatchData.java
@@ -88,6 +88,7 @@ public class RubyMatchData extends RubyObject {
 
     private RubyArray match_array(int start) {
         if (regs == null) {
+            if (start != 0) return getRuntime().newArray();
             if (begin == -1) {
                 return getRuntime().newArray(getRuntime().getNil());
             } else {
@@ -199,7 +200,7 @@ public class RubyMatchData extends RubyObject {
         if (regs == null) {
             if (i != 0) throw getRuntime().newIndexError("index " + i + " out of matches");
             if (end < 0) return getRuntime().getNil();
-            return RubyFixnum.newFixnum(getRuntime(), begin);
+            return RubyFixnum.newFixnum(getRuntime(), end);
         } else {
             if (i < 0 || regs.numRegs <= i) throw getRuntime().newIndexError("index " + i + " out of matches");
             if (regs.end[i] < 0) return getRuntime().getNil();