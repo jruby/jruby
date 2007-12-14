48cf1a1cfdb170979c9a8a2ca9b40e907b859fa2
Charles Oliver Nutter <headius@headius.com>
2007-12-14 06:20:28 +0000

    Add a longer case/when test
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@5258 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_case.rb
+++ test/bench/bench_case.rb
@@ -52,6 +52,77 @@ def do_bench2
   end
 end
 
+def do_case3
+  case 1
+  when 10
+  when 9
+  when 8
+  when 7
+  when 6
+  when 5
+  when 4
+  when 3
+  when 2
+  when 1
+  end
+  case 1
+  when 10
+  when 9
+  when 8
+  when 7
+  when 6
+  when 5
+  when 4
+  when 3
+  when 2
+  when 1
+  end
+  case 1
+  when 10
+  when 9
+  when 8
+  when 7
+  when 6
+  when 5
+  when 4
+  when 3
+  when 2
+  when 1
+  end
+  case 1
+  when 10
+  when 9
+  when 8
+  when 7
+  when 6
+  when 5
+  when 4
+  when 3
+  when 2
+  when 1
+  end
+  case 1
+  when 10
+  when 9
+  when 8
+  when 7
+  when 6
+  when 5
+  when 4
+  when 3
+  when 2
+  when 1
+  end
+end
+
+def do_bench3
+  a = 0
+  while a < 1_000_000
+    do_case3
+    a += 1
+  end
+end
+
 puts "1m calls to a method with five case statements with a case argument and a single when"
 5.times {
   puts Benchmark.measure {
@@ -63,5 +134,12 @@ puts "1m calls to a method with five case statements with a case argument and a 
 5.times {
   puts Benchmark.measure {
     do_bench2
+  }
+}
+
+puts "1m calls to a method with five case statements with a case argument and 10 whens (last matches)"
+5.times {
+  puts Benchmark.measure {
+    do_bench3
   }
 }
