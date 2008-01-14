6bc2264051a84b76f34456da1ce97607a4271abb
Charles Oliver Nutter <headius@headius.com>
2008-01-14 00:57:54 +0000

    Added another case to the define_method invocation benchmark
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@5589 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_define_method_methods.rb
+++ test/bench/bench_define_method_methods.rb
@@ -2,6 +2,9 @@ require 'benchmark'
 
 class Object
   define_method(:foo) { }
+  define_method(:bar) {a = 1}
+  b = 1
+  define_method(:baz) {b = 2}
 end
 
 puts "define_method(:foo) {}, 100k * 100 invocations"
@@ -19,6 +22,46 @@ puts "define_method(:foo) {}, 100k * 100 invocations"
       foo; foo; foo; foo; foo; foo; foo; foo; foo; foo
       foo; foo; foo; foo; foo; foo; foo; foo; foo; foo
       foo; foo; foo; foo; foo; foo; foo; foo; foo; foo
+      a += 1
+    end
+  }
+}
+
+puts "define_method(:bar) {a = 1}, 100k * 100 invocations"
+5.times {
+  puts Benchmark.measure {
+    a = 0
+    while a < 100000
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      a += 1
+    end
+  }
+}
+
+puts "b = 1; define_method(:baz) {b = 2}, 100k * 100 invocations"
+5.times {
+  puts Benchmark.measure {
+    a = 0
+    while a < 100000
+      baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
+      baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
+      baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
+      baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
+      baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
+      baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
+      baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
+      baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
+      baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
+      baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
       a += 1
     end
   }
