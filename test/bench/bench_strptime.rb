4d1fdb68e293e52b346349610101ae050274ea62
Thomas Enebo <tom.enebo@gmail.com>
2008-01-23 22:52:26 +0000

    update strptime bench
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@5706 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_strptime.rb
+++ test/bench/bench_strptime.rb
@@ -1,5 +1,10 @@
+require 'benchmark'
 require 'date'
 
-for i in (1..10000)
-    DateTime.strptime("27/Nov/2007:15:01:43 -0800", "%d/%b/%Y:%H:%M:%S %z")
+10.times() do
+  puts(Benchmark.realtime() do
+    for i in (1..10000)
+      DateTime.strptime("27/Nov/2007:15:01:43 -0800", "%d/%b/%Y:%H:%M:%S %z")
+    end
+  end)
 end
