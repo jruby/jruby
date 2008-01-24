4a81c249a4d270c3ef20853970a08424ba8d73a8
Ola Bini <ola.bini@gmail.com>
2008-01-24 14:59:09 +0000

    Fix for JRUBY-2010, handle Errno's more correctly
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@5715 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/RubySystemCallError.java
+++ src/org/jruby/RubySystemCallError.java
@@ -2,6 +2,8 @@ package org.jruby;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.jruby.anno.JRubyMethod;
 import org.jruby.runtime.Block;
@@ -16,6 +18,95 @@ import org.jruby.runtime.marshal.UnmarshalStream;
 
 public class RubySystemCallError extends RubyException {
     private IRubyObject errno;
+
+    private final static Map<String, String> defaultMessages = new HashMap<String, String>();
+    static {
+        defaultMessages.put("Errno::EPERM", "Operation not permitted");
+        defaultMessages.put("Errno::ENOENT", "No such file or directory");
+        defaultMessages.put("Errno::ESRCH", "No such process");
+        defaultMessages.put("Errno::EINTR", "Interrupted system call");
+        defaultMessages.put("Errno::EIO", "Input/output error");
+        defaultMessages.put("Errno::ENXIO", "Device not configured");
+        defaultMessages.put("Errno::E2BIG", "Argument list too long");
+        defaultMessages.put("Errno::ENOEXEC", "Exec format error");
+        defaultMessages.put("Errno::EBADF", "Bad file descriptor");
+        defaultMessages.put("Errno::ECHILD", "No child processes");
+        defaultMessages.put("Errno::EDEADLK", "Resource deadlock avoided");
+        defaultMessages.put("Errno::ENOMEM", "Cannot allocate memory");
+        defaultMessages.put("Errno::EACCES", "Permission denied");
+        defaultMessages.put("Errno::EFAULT", "Bad address");
+        defaultMessages.put("Errno::ENOTBLK", "Block device required");
+        defaultMessages.put("Errno::EBUSY", "Resource busy");
+        defaultMessages.put("Errno::EEXIST", "File exists");
+        defaultMessages.put("Errno::EXDEV", "Cross-device link");
+        defaultMessages.put("Errno::ENODEV", "Operation not supported by device");
+        defaultMessages.put("Errno::ENOTDIR", "Not a directory");
+        defaultMessages.put("Errno::EISDIR", "Is a directory");
+        defaultMessages.put("Errno::EINVAL", "Invalid argument");
+        defaultMessages.put("Errno::ENFILE", "Too many open files in system");
+        defaultMessages.put("Errno::EMFILE", "Too many open files");
+        defaultMessages.put("Errno::ENOTTY", "Inappropriate ioctl for device");
+        defaultMessages.put("Errno::ETXTBSY", "Text file busy");
+        defaultMessages.put("Errno::EFBIG", "File too large");
+        defaultMessages.put("Errno::ENOSPC", "No space left on device");
+        defaultMessages.put("Errno::ESPIPE", "Illegal seek");
+        defaultMessages.put("Errno::EROFS", "Read-only file system");
+        defaultMessages.put("Errno::EMLINK", "Too many links");
+        defaultMessages.put("Errno::EPIPE", "Broken pipe");
+        defaultMessages.put("Errno::EDOM", "Numerical argument out of domain");
+        defaultMessages.put("Errno::ERANGE", "Result too large");
+        defaultMessages.put("Errno::EAGAIN", "Resource temporarily unavailable");
+        defaultMessages.put("Errno::EAGAIN", "Resource temporarily unavailable");
+        defaultMessages.put("Errno::EINPROGRESS", "Operation now in progress");
+        defaultMessages.put("Errno::EALREADY", "Operation already in progress");
+        defaultMessages.put("Errno::ENOTSOCK", "Socket operation on non-socket");
+        defaultMessages.put("Errno::EDESTADDRREQ", "Destination address required");
+        defaultMessages.put("Errno::EMSGSIZE", "Message too long");
+        defaultMessages.put("Errno::EPROTOTYPE", "Protocol wrong type for socket");
+        defaultMessages.put("Errno::ENOPROTOOPT", "Protocol not available");
+        defaultMessages.put("Errno::EPROTONOSUPPORT", "Protocol not supported");
+        defaultMessages.put("Errno::ESOCKTNOSUPPORT", "Socket type not supported");
+        defaultMessages.put("Errno::EPFNOSUPPORT", "Protocol family not supported");
+        defaultMessages.put("Errno::EAFNOSUPPORT", "Address family not supported by protocol family");
+        defaultMessages.put("Errno::EADDRINUSE", "Address already in use");
+        defaultMessages.put("Errno::EADDRNOTAVAIL", "Can't assign requested address");
+        defaultMessages.put("Errno::ENETDOWN", "Network is down");
+        defaultMessages.put("Errno::ENETUNREACH", "Network is unreachable");
+        defaultMessages.put("Errno::ENETRESET", "Network dropped connection on reset");
+        defaultMessages.put("Errno::ECONNABORTED", "Software caused connection abort");
+        defaultMessages.put("Errno::ECONNRESET", "Connection reset by peer");
+        defaultMessages.put("Errno::ENOBUFS", "No buffer space available");
+        defaultMessages.put("Errno::EISCONN", "Socket is already connected");
+        defaultMessages.put("Errno::ENOTCONN", "Socket is not connected");
+        defaultMessages.put("Errno::ESHUTDOWN", "Can't send after socket shutdown");
+        defaultMessages.put("Errno::ETOOMANYREFS", "Too many references: can't splice");
+        defaultMessages.put("Errno::ETIMEDOUT", "Operation timed out");
+        defaultMessages.put("Errno::ECONNREFUSED", "Connection refused");
+        defaultMessages.put("Errno::ELOOP", "Too many levels of symbolic links");
+        defaultMessages.put("Errno::ENAMETOOLONG", "File name too long");
+        defaultMessages.put("Errno::EHOSTDOWN", "Host is down");
+        defaultMessages.put("Errno::EHOSTUNREACH", "No route to host");
+        defaultMessages.put("Errno::ENOTEMPTY", "Directory not empty");
+        defaultMessages.put("Errno::EUSERS", "Too many users");
+        defaultMessages.put("Errno::EDQUOT", "Disc quota exceeded");
+        defaultMessages.put("Errno::ESTALE", "Stale NFS file handle");
+        defaultMessages.put("Errno::EREMOTE", "Too many levels of remote in path");
+        defaultMessages.put("Errno::ENOLCK", "No locks available");
+        defaultMessages.put("Errno::ENOSYS", "Function not implemented");
+        defaultMessages.put("Errno::EOVERFLOW", "Value too large to be stored in data type");
+        defaultMessages.put("Errno::EIDRM", "Identifier removed");
+        defaultMessages.put("Errno::ENOMSG", "No message of desired type");
+        defaultMessages.put("Errno::EILSEQ", "Illegal byte sequence");
+        defaultMessages.put("Errno::EBADMSG", "Bad message");
+        defaultMessages.put("Errno::EMULTIHOP", "EMULTIHOP (Reserved)");
+        defaultMessages.put("Errno::ENODATA", "No message available on STREAM");
+        defaultMessages.put("Errno::ENOLINK", "ENOLINK (Reserved)");
+        defaultMessages.put("Errno::ENOSR", "No STREAM resources");
+        defaultMessages.put("Errno::ENOSTR", "Not a STREAM");
+        defaultMessages.put("Errno::EPROTO", "Protocol error");
+        defaultMessages.put("Errno::ETIME", "STREAM ioctl timeout");
+        defaultMessages.put("Errno::EOPNOTSUPP", "Operation not supported");
+    }
 
     protected RubySystemCallError(Ruby runtime, RubyClass rubyClass) {
         this(runtime, rubyClass, null, 0);
@@ -77,16 +168,23 @@ public class RubySystemCallError extends RubyException {
         return exceptionClass;
     }
     
-    @JRubyMethod(optional = 2, required=1, frame = true, visibility = Visibility.PRIVATE)
+    @JRubyMethod(optional = 2, required=0, frame = true, visibility = Visibility.PRIVATE)
     public IRubyObject initialize(IRubyObject[] args, Block block) {
+        String name = getMetaClass().getRealClass().getName();
         if (args.length >= 1) message = args[0];
         if (args.length == 2) {
             errno = args[1].convertToInteger();
-        } else {
-            // FIXME: Get current errno value if one is set?
-           errno = getRuntime().getNil();
         }
-        
+
+        if(args.length == 0) {
+            String val = defaultMessages.get(name);
+            if(null == val) {
+                val = name;
+            } 
+
+            message = getRuntime().newString(val);
+        }
+
         return this;
     }
 
--- test/test_system_error.rb
+++ test/test_system_error.rb
@@ -3,10 +3,11 @@ require 'test/unit'
 class TestSystemError < Test::Unit::TestCase
   def setup
     #sucked these right from ruby 1.8.6 on osx
-    @r186_errcodes=[["ENOTCONN", 57], ["EXDEV", 18], ["ENOLCK", 77], ["ENOTSOCK", 38], ["ENOLINK", 97], ["ENETDOWN", 50], ["EAGAIN", 35], ["EROFS", 30], ["ENOMSG", 91], ["EPROTONOSUPPORT", 43], ["EHOSTDOWN", 64], ["EINTR", 4], ["ENFILE", 23], ["EBUSY", 16], ["EDEADLK", 11], ["EILSEQ", 92], ["ENOBUFS", 55], ["EBADF", 9], ["ENOSPC", 28], ["ENOSR", 98], ["EADDRINUSE", 48], ["EDQUOT", 69], ["ENOENT", 2], ["EISDIR", 21], ["ELOOP", 62], ["EPROTOTYPE", 41], ["ETIMEDOUT", 60], ["ECONNABORTED", 53], ["EFAULT", 14], ["EDOM", 33], ["EBADMSG", 94], ["EPFNOSUPPORT", 46], ["EINPROGRESS", 36], ["E2BIG", 7], ["ETXTBSY", 26], ["ENODATA", 96], ["ENOSYS", 78], ["EDESTADDRREQ", 39], ["ESHUTDOWN", 58], ["ENODEV", 19], ["EMLINK", 31], ["EPROTO", 100], ["ENETUNREACH", 51], ["ENOMEM", 12], ["EIO", 5], ["EMFILE", 24], ["EIDRM", 90], ["ESOCKTNOSUPPORT", 44], ["EHOSTUNREACH", 65], ["EEXIST", 17], ["ENAMETOOLONG", 63], ["EUSERS", 68], ["EISCONN", 56], ["ECHILD", 10], ["ESPIPE", 29], ["EREMOTE", 71], ["EADDRNOTAVAIL", 49], ["ENOPROTOOPT", 42], ["ECONNREFUSED", 61], ["ESRCH", 3], ["EINVAL", 22], ["EAGAIN", 35], ["EOVERFLOW", 84], ["ECONNRESET", 54], ["ENOTBLK", 15], ["ERANGE", 34], ["ENOEXEC", 8], ["EAFNOSUPPORT", 47], ["ETIME", 101], ["EFBIG", 27], ["ESTALE", 70], ["EPERM", 1], ["EMSGSIZE", 40], ["ENOTEMPTY", 66], ["ENOTDIR", 20], ["ETOOMANYREFS", 59], ["EMULTIHOP", 95], ["EPIPE", 32], ["EACCES", 13], ["ENETRESET", 52], ["EOPNOTSUPP", 102], ["ENOSTR", 99], ["ENOTTY", 25], ["ENXIO", 6], ["EALREADY", 37]].sort{|x,y| x[1]<=>y[1]}
+    @r186_errcodes=[["ENOTCONN", 57, "Socket is not connected"], ["EXDEV", 18, "Cross-device link"], ["ENOLCK", 77, "No locks available"], ["ENOTSOCK", 38, "Socket operation on non-socket"], ["ENOLINK", 97, "ENOLINK (Reserved)"], ["ENETDOWN", 50, "Network is down"], ["EAGAIN", 35, "Resource temporarily unavailable"], ["EROFS", 30, "Read-only file system"], ["ENOMSG", 91, "No message of desired type"], ["EPROTONOSUPPORT", 43, "Protocol not supported"], ["EHOSTDOWN", 64, "Host is down"], ["EINTR", 4, "Interrupted system call"], ["ENFILE", 23, "Too many open files in system"], ["EBUSY", 16, "Resource busy"], ["EDEADLK", 11, "Resource deadlock avoided"], ["EILSEQ", 92, "Illegal byte sequence"], ["ENOBUFS", 55, "No buffer space available"], ["EBADF", 9, "Bad file descriptor"], ["ENOSPC", 28, "No space left on device"], ["ENOSR", 98, "No STREAM resources"], ["EADDRINUSE", 48, "Address already in use"], ["EDQUOT", 69, "Disc quota exceeded"], ["ENOENT", 2, "No such file or directory"], ["EISDIR", 21, "Is a directory"], ["ELOOP", 62, "Too many levels of symbolic links"], ["EPROTOTYPE", 41, "Protocol wrong type for socket"], ["ETIMEDOUT", 60, "Operation timed out"], ["ECONNABORTED", 53, "Software caused connection abort"], ["EFAULT", 14, "Bad address"], ["EDOM", 33, "Numerical argument out of domain"], ["EBADMSG", 94, "Bad message"], ["EPFNOSUPPORT", 46, "Protocol family not supported"], ["EINPROGRESS", 36, "Operation now in progress"], ["E2BIG", 7, "Argument list too long"], ["ETXTBSY", 26, "Text file busy"], ["ENODATA", 96, "No message available on STREAM"], ["ENOSYS", 78, "Function not implemented"], ["EDESTADDRREQ", 39, "Destination address required"], ["ESHUTDOWN", 58, "Can't send after socket shutdown"], ["ENODEV", 19, "Operation not supported by device"], ["EMLINK", 31, "Too many links"], ["EPROTO", 100, "Protocol error"], ["ENETUNREACH", 51, "Network is unreachable"], ["ENOMEM", 12, "Cannot allocate memory"], ["EIO", 5, "Input/output error"], ["EMFILE", 24, "Too many open files"], ["EIDRM", 90, "Identifier removed"], ["ESOCKTNOSUPPORT", 44, "Socket type not supported"], ["EHOSTUNREACH", 65, "No route to host"], ["EEXIST", 17, "File exists"], ["ENAMETOOLONG", 63, "File name too long"], ["EUSERS", 68, "Too many users"], ["EISCONN", 56, "Socket is already connected"], ["ECHILD", 10, "No child processes"], ["ESPIPE", 29, "Illegal seek"], ["EREMOTE", 71, "Too many levels of remote in path"], ["EADDRNOTAVAIL", 49, "Can't assign requested address"], ["ENOPROTOOPT", 42, "Protocol not available"], ["ECONNREFUSED", 61, "Connection refused"], ["ESRCH", 3, "No such process"], ["EINVAL", 22, "Invalid argument"], ["EAGAIN", 35, "Resource temporarily unavailable"], ["EOVERFLOW", 84, "Value too large to be stored in data type"], ["ECONNRESET", 54, "Connection reset by peer"], ["ENOTBLK", 15, "Block device required"], ["ERANGE", 34, "Result too large"], ["ENOEXEC", 8, "Exec format error"], ["EAFNOSUPPORT", 47, "Address family not supported by protocol family"], ["ETIME", 101, "STREAM ioctl timeout"], ["EFBIG", 27, "File too large"], ["ESTALE", 70, "Stale NFS file handle"], ["EPERM", 1, "Operation not permitted"], ["EMSGSIZE", 40, "Message too long"], ["ENOTEMPTY", 66, "Directory not empty"], ["ENOTDIR", 20, "Not a directory"], ["ETOOMANYREFS", 59, "Too many references: can't splice"], ["EMULTIHOP", 95, "EMULTIHOP (Reserved)"], ["EPIPE", 32, "Broken pipe"], ["EACCES", 13, "Permission denied"], ["ENETRESET", 52, "Network dropped connection on reset"], ["EOPNOTSUPP", 102, "Operation not supported"], ["ENOSTR", 99, "Not a STREAM"], ["ENOTTY", 25, "Inappropriate ioctl for device"], ["ENXIO", 6, "Device not configured"], ["EALREADY", 37, "Operation already in progress"]].sort_by{|v| v[1] }
   end
+
   def test_has_186_ERRNO_constants
-    @r186_errcodes.each do |e,c|
+    @r186_errcodes.each do |e,c,m|
       assert Errno.constants.include?(e), "missing constant #{e}"
     end
     allcodes = Errno.constants.map{|x| eval "Errno::#{x}::Errno"}
@@ -14,10 +15,26 @@ class TestSystemError < Test::Unit::TestCase
     #puts "dupes: #{dupes.join(',')}"
     assert_equal allcodes.size, allcodes.uniq.size
     assert_equal [], dupes
-
-
   end
 
+  def test_can_raise_errno_without_message
+    @r186_errcodes.each do |e,c,m|
+      err = Errno.const_get(e)
+      assert_raise_msg(err,m) do 
+        raise err
+      end
+    end    
+  end
+
+  def assert_raise_msg(error, message)
+    begin 
+      yield
+    rescue Exception => e
+      assert_kind_of error, e
+      assert_equal message, e.message
+    end
+  end
+  
   def print_report
     @r186_errcodes.each do |e,c|
       if Errno.constants.include?(e)
