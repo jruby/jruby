19442ee4637028025fdc456dcff064cb2d5ed9a0
Charles Oliver Nutter <headius@headius.com>
2008-01-25 09:04:13 +0000

    Add an op assignment benchmark, using + operation (see bench_op_asgn_or for an || based benchmark)
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@5724 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ test/bench/bench_op_assign.rb
@@ -0,0 +1,89 @@
+require 'benchmark'
+
+class Foo < Array
+  attr_accessor :bar
+  
+  def initialize
+    @bar = 1
+    self[1] = 1
+  end
+
+  def control
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+    self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1; self; 1 + 1
+  end
+  
+  def hundred_assigns
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+    self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1; self.bar += 1
+  end
+  
+  def hundred_array_assigns
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+    self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1; self[1] += 1
+  end
+end
+
+foo = Foo.new
+
+(ARGV[0] || 10).to_i.times {
+  Benchmark.bm(20) {|bm|
+    bm.report("control") { 100000.times { foo.control } }
+    bm.report("100 asgns") { 100000.times { foo.hundred_assigns } }
+    bm.report("100 array asgns") { 100000.times { foo.hundred_array_assigns } }
+  }
+}
