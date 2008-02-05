aa4ffa1be5572b91327ce11238a623b1bd681b27
Charles Oliver Nutter <headius@headius.com>
2008-02-05 06:18:45 +0000

    Fix botched merge that doubled new file content.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@5822 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/util/io/STDIO.java
+++ src/org/jruby/util/io/STDIO.java
@@ -25,30 +25,3 @@ public enum STDIO {
         return false;
     }
 }
-package org.jruby.util.io;
-
-public enum STDIO {
-
-    IN, OUT, ERR;
-
-    public int fileno() {
-        switch (this) {
-        case IN:
-            return 0;
-        case OUT:
-            return 1;
-        case ERR:
-            return 2;
-        default:
-            throw new RuntimeException();
-        }
-    }
-
-    public static boolean isSTDIO(int fileno) {
-        if (fileno >= 0 && fileno <= 2) {
-            return true;
-        }
-
-        return false;
-    }
-}
