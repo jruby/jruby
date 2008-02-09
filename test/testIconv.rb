b24567f6ff23cbac6fe494d883c7351dc809fd5f
Ola Bini <ola.bini@gmail.com>
2008-02-09 14:09:32 +0000

    Add tests for JRUBY-867, make sure that we ignore translit strings for now, instead of throwing exceptions
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@5884 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/testIconv.rb
+++ test/testIconv.rb
@@ -16,3 +16,19 @@ test_equal("ol\351", result)
 # FIXME: Java does not support transliteration in core jdk?
 #Iconv.iconv("ASCII//TRANSLIT", "UTF-8", "ol\303\251")
 
+# JRUBY-867, make us ignore transliteration stuff since we can't support it
+
+result = Iconv.conv("ISO-8859-1//TRANSLIT", "UTF-8", "ol\303\251")
+test_equal("ol\351", result)
+result = Iconv.conv("ISO-8859-1//IGNORE", "UTF-8", "ol\303\251")
+test_equal("ol\351", result)
+result = Iconv.conv("ISO-8859-1//IGNORE//TRANSLIT", "UTF-8", "ol\303\251")
+test_equal("ol\351", result)
+
+result = Iconv.conv("ISO-8859-1", "UTF-8//TRANSLIT", "ol\303\251")
+test_equal("ol\351", result)
+result = Iconv.conv("ISO-8859-1", "UTF-8//IGNORE", "ol\303\251")
+test_equal("ol\351", result)
+result = Iconv.conv("ISO-8859-1", "UTF-8//IGNORE//TRANSLIT", "ol\303\251")
+test_equal("ol\351", result)
+
