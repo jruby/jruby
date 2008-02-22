49abad906fc1ba2132beaee60d2fbb2f3ef47b83
Ola Bini <ola.bini@gmail.com>
2008-02-22 10:47:52 +0000

    Added RDoc annotation and a sample annotation in RubyHash
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6002 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ src/org/jruby/anno/RDoc.java
@@ -0,0 +1,18 @@
+package org.jruby.anno;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author olabini
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface RDoc {
+    String callSeq() default "";
+    String doc() default "";
+    String name() default "";
+}
