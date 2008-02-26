ea846b67301e44277da3f7a5171724d931c92052
Charles Oliver Nutter <headius@headius.com>
2008-02-26 19:21:53 +0000

    Modification to bench_alias to allow embedding in a larger benchmark run
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6039 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/language/bench_alias.rb
+++ test/bench/language/bench_alias.rb
@@ -1,17 +1,22 @@
 require 'benchmark'
 
-module Foo
-  (ARGV[0] || 10).to_i.times {
-    class << self
-      Benchmark.bm(30) {|bm|
-        bm.report("1m aliases") { 1000000.times { alias xxx to_s } }
-        bm.report("1m alias_methods") { 1000000.times { alias_method :yyy, :to_s } }
-      }
+def bench_alias(bm)
+  oldbm = $bm
+  $bm = bm
+  # this is to avoid errors about module def in method body
+  class << self
+    module Foo
+      class << self
+        $bm.report("1m aliases") { 1000000.times { alias xxx to_s } }
+        $bm.report("1m alias_methods") { 1000000.times { alias_method :yyy, :to_s } }
+      end
+      $bm.report("control: 1m to_s on topself") { 1000000.times { to_s } }
+      $bm.report("1m alias'ed calls") { 1000000.times { xxx } }
+      $bm.report("1m alias_method'ed calls") { 1000000.times { yyy } }
     end
-    Benchmark.bm(30) {|bm|
-      bm.report("control: 1m to_s on topself") { 1000000.times { to_s } }
-      bm.report("1m alias'ed calls") { 1000000.times { xxx } }
-      bm.report("1m alias_method'ed calls") { 1000000.times { yyy } }
-    }
-  }
+  end
+end
+
+if $0 == __FILE__
+  (ARGV[0] || 10).to_i.times { Benchmark.bm(30) {|bm| bench_alias(bm)} }
 end
