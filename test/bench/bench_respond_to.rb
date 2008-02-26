6c6f0efeb6378b588b311016a14bfc1af4649411
Charles Oliver Nutter <headius@headius.com>
2008-02-26 18:56:14 +0000

    Fix typo in respond_to(?) test
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6038 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_respond_to.rb
+++ test/bench/bench_respond_to.rb
@@ -3,6 +3,6 @@ require 'benchmark'
 (ARGV[0] || 10).to_i.times do
   Benchmark.bm(30) do |bm|
     bm.report("control, 1 and :to_ary") { 1_000_000.times { 1; :to_ary }}
-    bm.report("1m 1.respond_to(:to_ary)") { 1_000_000.times { 1.respond_to? :to_ary }}
+    bm.report("1m 1.respond_to?(:to_ary)") { 1_000_000.times { 1.respond_to? :to_ary }}
   end
 end
