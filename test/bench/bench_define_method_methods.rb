66196e0463b7ca97b886a5c683d932a9854dcf74
Charles Oliver Nutter <headius@headius.com>
2008-03-02 11:06:00 +0000

    Improve define_method benchmark by adding an eval'ed version (which seems too run too fast) and put it in batchable form.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6079 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_define_method_methods.rb
+++ test/bench/bench_define_method_methods.rb
@@ -1,17 +1,17 @@
 require 'benchmark'
 
 class Object
-  define_method(:foo) { }
+  define_method(:foo) {1}
   define_method(:bar) {a = 1}
   b = 1
   define_method(:baz) {b = 2}
+  eval "define_method(:boo) {1}"
 end
 
-puts "define_method(:foo) {}, 100k * 100 invocations"
-5.times {
-  puts Benchmark.measure {
+def bench_define_method_methods(bm)
+  bm.report "define_method(:foo) {1}, 10k * 100 invocations" do
     a = 0
-    while a < 100000
+    while a < 10000
       foo; foo; foo; foo; foo; foo; foo; foo; foo; foo
       foo; foo; foo; foo; foo; foo; foo; foo; foo; foo
       foo; foo; foo; foo; foo; foo; foo; foo; foo; foo
@@ -24,34 +24,45 @@ puts "define_method(:foo) {}, 100k * 100 invocations"
       foo; foo; foo; foo; foo; foo; foo; foo; foo; foo
       a += 1
     end
-  }
-}
+  end
 
-puts "define_method(:bar) {a = 1}, 100k * 100 invocations"
-5.times {
-  puts Benchmark.measure {
+  bm.report "eval'ed define_method(:baz) {1}, 10k * 100 invocations" do
     a = 0
-    while a < 100000
-      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
-      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
-      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
-      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
-      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
-      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
-      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
-      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
-      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
-      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+    while a < 10000
+      boo; boo; boo; boo; boo; boo; boo; boo; boo; boo
+      boo; boo; boo; boo; boo; boo; boo; boo; boo; boo
+      boo; boo; boo; boo; boo; boo; boo; boo; boo; boo
+      boo; boo; boo; boo; boo; boo; boo; boo; boo; boo
+      boo; boo; boo; boo; boo; boo; boo; boo; boo; boo
+      boo; boo; boo; boo; boo; boo; boo; boo; boo; boo
+      boo; boo; boo; boo; boo; boo; boo; boo; boo; boo
+      boo; boo; boo; boo; boo; boo; boo; boo; boo; boo
+      boo; boo; boo; boo; boo; boo; boo; boo; boo; boo
+      boo; boo; boo; boo; boo; boo; boo; boo; boo; boo
       a += 1
     end
-  }
-}
+  end
 
-puts "b = 1; define_method(:baz) {b = 2}, 100k * 100 invocations"
-5.times {
-  puts Benchmark.measure {
+  bm.report "define_method(:bar) {a = 1}, 10k * 100 invocations" do
     a = 0
-    while a < 100000
+    while a < 10000
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      bar; bar; bar; bar; bar; bar; bar; bar; bar; bar
+      a += 1
+    end
+  end
+
+  bm.report "b = 1; define_method(:baz) {b = 2}, 10k * 100 invocations" do
+    a = 0
+    while a < 10000
       baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
       baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
       baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
@@ -64,5 +75,9 @@ puts "b = 1; define_method(:baz) {b = 2}, 100k * 100 invocations"
       baz; baz; baz; baz; baz; baz; baz; baz; baz; baz
       a += 1
     end
-  }
-}
+  end
+end
+
+if $0 == __FILE__
+  Benchmark.bmbm {|bm| bench_define_method_methods(bm)}
+end
