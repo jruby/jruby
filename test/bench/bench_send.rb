77068be36b0735d9713e5c9ceca2f689c69ad238
Charles Oliver Nutter <headius@headius.com>
2008-03-03 18:05:33 +0000

    Add a simple send benchmark.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6088 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ test/bench/bench_send.rb
@@ -0,0 +1,18 @@
+require 'benchmark'
+
+class Foo
+  def foo; self; end
+end
+
+class A < Foo; end; class B < A; end; class C < B; end; class D < C; end; class E < D; end
+class F < E; end; class G < F; end; class H < G; end; class I < H; end; class J < I; end
+
+def bench_send(bm)
+  bm.report("1m foo calls") { f = Foo.new; 10_000_000.times { f.foo } }
+  bm.report("1m foo sends") { f = Foo.new; 10_000_000.times { f.send :foo } }
+  bm.report("1m subclass foo sends") { f = J.new; 10_000_000.times { f.send :foo } }
+end
+
+if $0 == __FILE__
+  Benchmark.bmbm(40) {|bm| bench_send(bm)}
+end
