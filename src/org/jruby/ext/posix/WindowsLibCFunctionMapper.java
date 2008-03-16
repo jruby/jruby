b980f3630b7b233476f717db035e55a3baf4f154
Nick Sieger <nick@nicksieger.com>
2008-03-16 20:22:54 +0000

    JRUBY-2274: mapping File.lchmod to chmod on windows to avoid blowing up nastily
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6222 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/ext/posix/WindowsLibCFunctionMapper.java
+++ src/org/jruby/ext/posix/WindowsLibCFunctionMapper.java
@@ -23,6 +23,7 @@ public class WindowsLibCFunctionMapper implements FunctionMapper {
         methodNameMap.put("stat", "_stat");
         methodNameMap.put("mkdir", "_mkdir");
         methodNameMap.put("umask", "_umask");
+        methodNameMap.put("lchmod", "_chmod");
     }
   
     public String getFunctionName(NativeLibrary library, Method method) {
