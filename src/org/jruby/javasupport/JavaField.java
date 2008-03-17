b87c62c2fd2898cf74d965c2efd4b3fbebd25110
MenTaLguY <mental@rydia.net>
2008-03-17 05:42:04 +0000

    enable static field access in security-restricted mode
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6234 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/javasupport/JavaField.java
+++ src/org/jruby/javasupport/JavaField.java
@@ -162,20 +162,17 @@ public class JavaField extends JavaAccessibleObject {
     }
 
     public JavaObject static_value() {
-        
-        if (Ruby.isSecurityRestricted())
-            return null;
-        else {
-            try {
-                // TODO: Only setAccessible to account for pattern found by
-                // accessing constants included from a non-public interface.
-                // (aka java.util.zip.ZipConstants being implemented by many
-                // classes)
+        try {
+            // TODO: Only setAccessible to account for pattern found by
+            // accessing constants included from a non-public interface.
+            // (aka java.util.zip.ZipConstants being implemented by many
+            // classes)
+            if (!Ruby.isSecurityRestricted()) {
                 field.setAccessible(true);
-                return JavaObject.wrap(getRuntime(), field.get(null));
-            } catch (IllegalAccessException iae) {
-                throw getRuntime().newTypeError("illegal static value access: " + iae.getMessage());
             }
+            return JavaObject.wrap(getRuntime(), field.get(null));
+        } catch (IllegalAccessException iae) {
+            throw getRuntime().newTypeError("illegal static value access: " + iae.getMessage());
         }
     }
 
