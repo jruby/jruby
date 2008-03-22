607ab12b954403447f1432367761875ba7846b6e
Ola Bini <ola.bini@gmail.com>
2008-03-22 13:22:25 +0000

    Make sure to test performance of respond_to on something that doesn't exist
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6314 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_respond_to.rb
+++ test/bench/bench_respond_to.rb
@@ -4,5 +4,6 @@ require 'benchmark'
   Benchmark.bm(30) do |bm|
     bm.report("control, 1 and :to_ary") { 1_000_000.times { 1; :to_ary }}
     bm.report("1m 1.respond_to?(:to_ary)") { 1_000_000.times { 1.respond_to? :to_ary }}
+    bm.report("1m 1.respond_to?(:to_arry)") { 1_000_000.times { 1.respond_to? :to_arry }}
   end
 end
