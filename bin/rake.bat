e188079cdfd81e9cc9767f3db1eca2f4ee73bc9d
Thomas Enebo <tom.enebo@gmail.com>
2008-04-10 17:47:18 +0000

    JRUBY-2371: src dist is not including Rakefile + Add rake.bat
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6454 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ bin/rake.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+rem ---------------------------------------------------------------------------
+
+call "%~dp0_jrubyvars" %*
+
+if %JRUBY_BAT_ERROR%==0 "%_STARTJAVA%" %_VM_OPTS% -cp "%CLASSPATH%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat org.jruby.Main %JRUBY_OPTS% "%JRUBY_HOME%\bin\rake" %_RUBY_OPTS%
+set E=%ERRORLEVEL%
+
+call "%~dp0_jrubycleanup"
+endlocal
