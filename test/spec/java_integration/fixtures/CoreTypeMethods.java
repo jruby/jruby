109854cdf5a1c03c9e35abebc44db714e37427cb
Charles Oliver Nutter <headius@headius.com>
2008-04-18 01:05:01 +0000

    More type-coercion and overloaded type-selection specs for JI.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6521 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/spec/java_integration/fixtures/CoreTypeMethods.java
+++ test/spec/java_integration/fixtures/CoreTypeMethods.java
@@ -44,4 +44,88 @@ public class CoreTypeMethods {
     public static Object getNull() {
         return null;
     }
+    
+    public static String setString(String s) {
+        return s;
+    }
+    
+    public static String setByte(byte b) {
+        return String.valueOf(b);
+    }
+    
+    public static String setShort(short s) {
+        return String.valueOf(s);
+    }
+    
+    public static String setChar(char c) {
+        return String.valueOf(c);
+    }
+    
+    public static String setInt(int i) {
+        return String.valueOf(i);
+    }
+    
+    public static String setLong(long l) {
+        return String.valueOf(l);
+    }
+    
+    public static String setFloat(float f) {
+        return String.valueOf(f);
+    }
+    
+    public static String setDouble(double d) {
+        return String.valueOf(d);
+    }
+    
+    public static String setBooleanTrue(boolean t) {
+        return String.valueOf(t);
+    }
+    
+    public static String setBooleanFalse(boolean f) {
+        return String.valueOf(f);
+    }
+    
+    public static String setNull(Object nil) {
+        return String.valueOf(nil);
+    }
+    
+    public static String getType(byte b) {
+        return "byte";
+    }
+    
+    public static String getType(short s) {
+        return "short";
+    }
+    
+    public static String getType(char c) {
+        return "char";
+    }
+    
+    public static String getType(int i) {
+        return "int";
+    }
+    
+    public static String getType(long l) {
+        return "long";
+    }
+    
+    public static String getType(float f) {
+        return "float";
+    }
+    
+    public static String getType(double d) {
+        return "double";
+    }
+    
+    public static String getType(CharSequence cs) {
+        return "CharSequence";
+    }
+    
+    public static String getType(String s) {
+        return "String";
+    }
+
+    public static String getType(boolean b) {
+        return "boolean";
+    }
 }
--- test/spec/java_integration/types/coercion_spec.rb
+++ test/spec/java_integration/types/coercion_spec.rb
@@ -2,18 +2,26 @@ require File.dirname(__FILE__) + "/../spec_helper"
 
 import "spec.java_integration.fixtures.CoreTypeMethods"
 
-describe "Java core and primitive types" do
+describe "Java String and primitive-typed methods" do
   it "should coerce to Ruby types when returned" do 
     CoreTypeMethods.getString.should be_kind_of(String)
+    CoreTypeMethods.getString.should == "foo";
     
     CoreTypeMethods.getByte.should be_kind_of(Fixnum)
+    CoreTypeMethods.getByte.should == 1
     CoreTypeMethods.getShort.should be_kind_of(Fixnum)
+    CoreTypeMethods.getShort.should == 2
     CoreTypeMethods.getChar.should be_kind_of(Fixnum)
+    CoreTypeMethods.getChar.should == 2
     CoreTypeMethods.getInt.should be_kind_of(Fixnum)
+    CoreTypeMethods.getInt.should == 4
     CoreTypeMethods.getLong.should be_kind_of(Fixnum)
+    CoreTypeMethods.getLong.should == 8
     
     CoreTypeMethods.getFloat.should be_kind_of(Float)
+    CoreTypeMethods.getFloat.should == 4
     CoreTypeMethods.getDouble.should be_kind_of(Float)
+    CoreTypeMethods.getDouble.should == 8
     
     CoreTypeMethods.getBooleanTrue.should be_kind_of(TrueClass)
     CoreTypeMethods.getBooleanTrue.should == true
@@ -22,5 +30,47 @@ describe "Java core and primitive types" do
     
     CoreTypeMethods.getNull.should be_kind_of(NilClass)
     CoreTypeMethods.getNull.should == nil
+  end
+  
+  it "should be coerced from Ruby types when passing parameters" do
+    CoreTypeMethods.setString("string").should == "string"
+    
+    CoreTypeMethods.setByte(1).should == "1"
+    CoreTypeMethods.setShort(1).should == "1"
+    CoreTypeMethods.setChar(1).should == "\001"
+    CoreTypeMethods.setInt(1).should == "1"
+    CoreTypeMethods.setLong(1).should == "1"
+    
+    CoreTypeMethods.setFloat(1.0).should == "1.0"
+    CoreTypeMethods.setDouble(1.0).should == "1.0"
+    
+    CoreTypeMethods.setBooleanTrue(true).should == "true"
+    CoreTypeMethods.setBooleanFalse(false).should == "false"
+    
+    CoreTypeMethods.setNull(nil).should == "null"
+  end
+  
+  it "should raise errors when passed values can not be precisely coerced" do
+    pending("precision failure does not raise error") do
+      lambda { CoreTypeMethods.setByte(1 << 8) }.should raise_error(TypeError)
+      lambda { CoreTypeMethods.setShort(1 << 16) }.should raise_error(TypeError)
+      lambda { CoreTypeMethods.setChar(1 << 16) }.should raise_error(TypeError)
+      lambda { CoreTypeMethods.setInt(1 << 32) }.should raise_error(TypeError)
+      lambda { CoreTypeMethods.setLong(1 << 64) }.should raise_error(TypeError)
+    end
+  end
+  
+  it "should select the most narrow and precise overloaded method" do
+    pending("Fixnum always selects long method") do
+      CoreTypeMethods.getType(1).should == "byte"
+      CoreTypeMethods.getType(1 << 8).should == "short"
+      CoreTypeMethods.getType(1 << 16).should == "int"
+    end
+    CoreTypeMethods.getType(1 << 32).should == "long"
+    
+    CoreTypeMethods.getType(1.0).should == "double"
+    
+    CoreTypeMethods.getType("foo").should == "String"
+    CoreTypeMethods.getType(nil).should == "CharSequence"
   end
 end
