efce1e3da81dbcb17024114d60b9951ee0fd9c20
Vladimir Sizikov <vsizikov@gmail.com>
2008-04-22 17:15:51 +0000

    Regression tests for JRUBY-2388: Calling Module.start resolves to RubyGC#start.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6554 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/testGC.rb
+++ test/testGC.rb
@@ -8,3 +8,8 @@ result = ObjectSpace.each_object(Symbol) {|o|
   count += 1
 }
 test_equal(count, result)
+
+# JRUBY-2388
+test_ok(!Module.respond_to?(:enable))
+test_ok(!Kernel.respond_to?(:start))
+test_ok(!String.respond_to?(:enable))
