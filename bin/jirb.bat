d8c4a3dc062dac4f1f8ed02a17396d56026cee8e
Vladimir Sizikov <vsizikov@gmail.com>
2008-05-02 10:40:50 +0000

    JRUBY-2485: Most BAT starter scripts are broken on Windows
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6614 961051c9-f516-0410-bf72-c9f7e237a7b7

--- bin/jirb.bat
+++ bin/jirb.bat
@@ -6,7 +6,7 @@ rem
 setlocal
 call "%~dp0_jrubyvars" %*
 
-if %JRUBY_BAT_ERROR%==0 "%_STARTJAVA%" %_VM_OPTS% -cp "%CLASSPATH%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat org.jruby.Main %JRUBY_OPTS% "%JRUBY_HOME%\bin\jirb" %_RUBY_OPTS%
+if %JRUBY_BAT_ERROR%==0 "%_STARTJAVA%" %_VM_OPTS% -Xbootclasspath/a:"%CP%" -cp "%CLASSPATH%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat org.jruby.Main %JRUBY_OPTS% "%JRUBY_HOME%\bin\jirb" %_RUBY_OPTS%
 set E=%ERRORLEVEL%
 
 call "%~dp0_jrubycleanup"
--- bin/rdoc.bat
+++ bin/rdoc.bat
@@ -4,7 +4,7 @@ rem ---------------------------------------------------------------------------
 
 call "%~dp0_jrubyvars" %*
 
-if %JRUBY_BAT_ERROR%==0 "%_STARTJAVA%" %_VM_OPTS% -cp "%CLASSPATH%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat org.jruby.Main %JRUBY_OPTS% "%JRUBY_HOME%\bin\rdoc" %_RUBY_OPTS%
+if %JRUBY_BAT_ERROR%==0 "%_STARTJAVA%" %_VM_OPTS% -Xbootclasspath/a:"%CP%" -cp "%CLASSPATH%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat org.jruby.Main %JRUBY_OPTS% "%JRUBY_HOME%\bin\rdoc" %_RUBY_OPTS%
 set E=%ERRORLEVEL%
 
 call "%~dp0_jrubycleanup"
