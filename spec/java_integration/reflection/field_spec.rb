58f255dddef823b2cbed8c525376eeae82e29af8
Charles Oliver Nutter <headius@headius.com>
2008-05-25 19:20:33 +0000

    Rework, expand, tighten up Evan Light's field-reflection specs a bit, based on discussions.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6786 961051c9-f516-0410-bf72-c9f7e237a7b7

--- spec/java_integration/reflection/field_spec.rb
+++ spec/java_integration/reflection/field_spec.rb
@@ -5,128 +5,163 @@ import "java_integration.fixtures.PrivateField"
 import "java_integration.fixtures.ProtectedField"
 import "java_integration.fixtures.PublicField"
 
-describe "Java field reflection" do
-  it "should provide a look up for a private field using a Java formatted name" do
+describe "A JavaClass" do
+  it "should provide a look up for fields using a Java formatted name" do
     PrivateField.java_class.declared_field(:strField).should_not == nil
-  end
-
-  it "should provide a look up for a private field using a Ruby formatted name" do
-    pending "JavaClass does not provide underscore access to fields" do
-      PrivateField.java_class.declared_field(:str_field).should_not == nil
-    end
-  end
-  
-  it "should provide a look up for a protected field using a Java formatted name" do
     ProtectedField.java_class.declared_field(:strField).should_not == nil
-  end
-
-  it "should provide a look up for a protected field using a Ruby formatted name" do
-    pending "JavaClass does not provide underscore access to fields" do
-      ProtectedField.java_class.declared_field(:str_field).should_not == nil
-    end
-  end
-  
-  it "should provide a look up for a public field using a Java formatted name" do
     PublicField.java_class.declared_field(:strField).should_not == nil
   end
 
-  it "should provide a look up for a public field using a Ruby formatted name" do
+  it "should provide a look up for a fields using a Ruby formatted name" do
     pending "JavaClass does not provide underscore access to fields" do
+      PrivateField.java_class.declared_field(:str_field).should_not == nil
+      ProtectedField.java_class.declared_field(:str_field).should_not == nil
       PublicField.java_class.declared_field(:str_field).should_not == nil
     end
   end
-  
-  describe "when using a private member" do
-    before(:each) do
-      @obj = PrivateField.new
-      @field = PrivateField.java_class.declared_field :strField
-      @field.accessible = true
-    end
+end
 
-    it "should set non-null Ruby values on private members" do
-      pending "JavaField does not automatically coerce" do
-        lambda { @field.set_value @obj.java_object, "42" }.should_not raise_error
+describe "A JavaField" do
+  describe "given a partially unwrapped Java object" do
+    describe "with a private field" do
+      before(:each) do
+        @obj = PrivateField.new
+        @field = PrivateField.java_class.declared_field :strField
+        @field.accessible = true
       end
-    end
-    
-    it "should set null Ruby values on private members" do
-      pending "JavaField does not automatically coerce" do
-        lambda { @field.set_value @obj.java_object, nil }.should_not raise_error
+
+      it "should set Ruby values" do
+        pending "JavaField does not automatically coerce" do
+          lambda { @field.set_value @obj.java_object, "42" }.should_not raise_error
+          lambda { @field.set_value @obj.java_object, nil }.should_not raise_error
+        end
       end
+
+      it "should set Java values" do
+        lambda { @field.set_value @obj.java_object, Java.ruby_to_java("42") }.should_not raise_error
+        lambda { @field.set_value @obj.java_object, Java.ruby_to_java(nil) }.should_not raise_error
+      end
+
+      it "should get Ruby values"
     end
     
-    it "should set non-null Java values on protected members" do
-      lambda { @field.set_value @obj.java_object, Java.ruby_to_java("42") }.should_not raise_error
+    describe "with a protected field" do
+      before(:each) do
+        @obj = ProtectedField.new
+        @field = ProtectedField.java_class.declared_field :strField
+        @field.accessible = true
+      end
+
+      it "should set Ruby values" do
+        pending "JavaField does not automatically coerce" do
+          lambda { @field.set_value @obj.java_object, "42" }.should_not raise_error
+          lambda { @field.set_value @obj.java_object, nil }.should_not raise_error
+        end
+      end
+
+      it "should set Java values" do
+        lambda { @field.set_value @obj.java_object, Java.ruby_to_java("42") }.should_not raise_error
+        lambda { @field.set_value @obj.java_object, Java.ruby_to_java(nil) }.should_not raise_error
+      end
+
+      it "should get Ruby values"
     end
     
-    it "should set null Java values on protected members" do
-      lambda { @field.set_value @obj.java_object, Java.ruby_to_java(nil) }.should_not raise_error
+    describe "with a public field" do
+      before(:each) do
+        @obj = PublicField.new
+        @field = PublicField.java_class.declared_field :strField
+        @field.accessible = true
+      end
+
+      it "should set Ruby values" do
+        pending "JavaField does not automatically coerce" do
+          lambda { @field.set_value @obj.java_object, "42" }.should_not raise_error
+          lambda { @field.set_value @obj.java_object, nil }.should_not raise_error
+        end
+      end
+
+      it "should set Java values" do
+        lambda { @field.set_value @obj.java_object, Java.ruby_to_java("42") }.should_not raise_error
+        lambda { @field.set_value @obj.java_object, Java.ruby_to_java(nil) }.should_not raise_error
+      end
+
+      it "should get Ruby values"
     end
-    
-    it "should get non-null Ruby values from private members"
-    it "should get null Ruby values from private members"
   end
   
-  describe "when using a protected member" do
-    before(:each) do
-      @obj = ProtectedField.new
-      @field = ProtectedField.java_class.declared_field :strField
-      @field.accessible = true
-    end
-
-    it "should set non-null Ruby values on protected members" do
-      pending "JavaField does not automatically coerce" do
-        lambda { @field.set_value @obj.java_object, "42" }.should_not raise_error
+  describe "given a Ruby-wrapped Java object" do
+    describe "with a private field" do
+      before(:each) do
+        @obj = PrivateField.new
+        @field = PrivateField.java_class.declared_field :strField
+        @field.accessible = true
       end
-    end
-    
-    it "should set null Ruby values on protected members" do
-      pending "JavaField does not automatically coerce" do
-        lambda { @field.set_value @obj.java_object, nil }.should_not raise_error
+
+      it "should set Ruby values" do
+        pending "JavaField does not automatically coerce" do
+          lambda { @field.set_value @obj, "42" }.should_not raise_error
+          lambda { @field.set_value @obj, nil }.should_not raise_error
+        end
       end
-    end    
 
-    it "should set non-null Java values on protected members" do
-      lambda { @field.set_value @obj.java_object, Java.ruby_to_java("42") }.should_not raise_error
-    end
-    
-    it "should set null Java values on protected members" do
-      lambda { @field.set_value @obj.java_object, Java.ruby_to_java(nil) }.should_not raise_error
-    end
-    
-    it "should get non-null Ruby values from private members"
-    it "should get null Ruby values from private members"    
-  end
-  
-  describe "when using a public member" do
-    before(:each) do
-      @obj = PublicField.new
-      @field = PublicField.java_class.declared_field :strField
-      @field.accessible = true
-    end
-
-    it "should set null Ruby values on public members" do
-      pending "JavaField does not automatically coerce" do
-        lambda { @field.set_value @obj.java_object, "42" }.should_not raise_error
+      it "should set Java values" do
+        pending "JavaField can not accept a Ruby-wrapped Java object" do
+          lambda { @field.set_value @obj, Java.ruby_to_java("42") }.should_not raise_error
+          lambda { @field.set_value @obj, Java.ruby_to_java(nil) }.should_not raise_error
+        end
       end
-    end
 
-    it "should set Ruby non-null values on public members" do
-      pending "JavaField does not automatically coerce" do
-        lambda { @field.set_value @obj.java_object, nil }.should_not raise_error
+      it "should get Ruby values"
+    end
+    
+    describe "with a protected field" do
+      before(:each) do
+        @obj = ProtectedField.new
+        @field = ProtectedField.java_class.declared_field :strField
+        @field.accessible = true
       end
-    end      
-    
-    it "should set non-null Java values on protected members" do
-      lambda { @field.set_value @obj.java_object, Java.ruby_to_java("42") }.should_not raise_error
-    end
-    
-    it "should set null Java values on protected members" do
-      lambda { @field.set_value @obj.java_object, Java.ruby_to_java(nil) }.should_not raise_error
-    end
 
-    it "should get non-null Ruby values from private members"
-    it "should get null Ruby values from private members"
+      it "should set Ruby values" do
+        pending "JavaField does not automatically coerce" do
+          lambda { @field.set_value @obj, "42" }.should_not raise_error
+          lambda { @field.set_value @obj, nil }.should_not raise_error
+        end
+      end
+
+      it "should set Java values" do
+        pending "JavaField can not accept a Ruby-wrapped Java object" do
+          lambda { @field.set_value @obj, Java.ruby_to_java("42") }.should_not raise_error
+          lambda { @field.set_value @obj, Java.ruby_to_java(nil) }.should_not raise_error
+        end
+      end
+
+      it "should get Ruby values"
+    end
+    
+    describe "with a public field" do
+      before(:each) do
+        @obj = PublicField.new
+        @field = PublicField.java_class.declared_field :strField
+        @field.accessible = true
+      end
+
+      it "should set Ruby values" do
+        pending "JavaField does not automatically coerce" do
+          lambda { @field.set_value @obj, "42" }.should_not raise_error
+          lambda { @field.set_value @obj, nil }.should_not raise_error
+        end
+      end
+
+      it "should set Java values" do
+        pending "JavaField can not accept a Ruby-wrapped Java object" do
+          lambda { @field.set_value @obj, Java.ruby_to_java("42") }.should_not raise_error
+          lambda { @field.set_value @obj, Java.ruby_to_java(nil) }.should_not raise_error
+        end
+      end
+
+      it "should get Ruby values"
+    end
   end
 end
     
