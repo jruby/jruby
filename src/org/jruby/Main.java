299a9fa791f7345d4958eb1b1cb2e5e212835531
Charles Oliver Nutter <headius@headius.com>
2008-05-27 17:42:36 +0000

    Revert "Adding a hard exit to the bottom of Main.main, since that entry point is only ever used by command-line execution, and command-line execution should shut down the VM when the main thread terminates as in Ruby."
    This reverts commit b4696e55925721a75c0248a87b1e35298a839937.
    
    I'm reverting the JRUBY-1650 fix because it makes me too nervous.
    
    
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6831 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/Main.java
+++ src/org/jruby/Main.java
@@ -78,11 +78,9 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         int status = main.run(args);
-        
-        // For JRUBY-1650, we do a hard exit here, since this entry point
-        // should only be used by command-line execution, and we will want to
-        // shut down the VM at this point
-        System.exit(status);
+        if (status != 0) {
+            System.exit(status);
+        }
     }
 
     public int run(String[] args) {
