3d45e6511e32230b6f9ca04ce9d1946355eef248
Vladimir Sizikov <vsizikov@gmail.com>
2008-05-29 15:27:39 +0000

    JRUBY-2590: [1.8.7] SecureRandom crashes, fails new rubyspecs
    Fixed all failures and crashes.
    
    
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6855 961051c9-f516-0410-bf72-c9f7e237a7b7

--- lib/ruby/site_ruby/1.8/securerandom.rb
+++ lib/ruby/site_ruby/1.8/securerandom.rb
@@ -1,6 +1,39 @@
+# = Secure random number generator interface.
+#
+# This library is an interface for secure random number generator which is
+# suitable for generating session key in HTTP cookies, etc.
+#
+# It supports following secure random number generators.
+#
+# * Java's java.security.SecureRandom.
+#
+# == Example
+#
+# # random hexadecimal string.
+# p SecureRandom.hex(10) #=> "52750b30ffbc7de3b362"
+# p SecureRandom.hex(10) #=> "92b15d6c8dc4beb5f559"
+# p SecureRandom.hex(11) #=> "6aca1b5c58e4863e6b81b8"
+# p SecureRandom.hex(12) #=> "94b2fff3e7fd9b9c391a2306"
+# p SecureRandom.hex(13) #=> "39b290146bea6ce975c37cfc23"
+# ...
+#
+# # random base64 string.
+# p SecureRandom.base64(10) #=> "EcmTPZwWRAozdA=="
+# p SecureRandom.base64(10) #=> "9b0nsevdwNuM/w=="
+# p SecureRandom.base64(10) #=> "KO1nIU+p9DKxGg=="
+# p SecureRandom.base64(11) #=> "l7XEiFja+8EKEtY="
+# p SecureRandom.base64(12) #=> "7kJSM/MzBJI+75j8"
+# p SecureRandom.base64(13) #=> "vKLJ0tXBHqQOuIcSIg=="
+# ...
+#
+# # random binary string.
+# p SecureRandom.random_bytes(10) #=> "\016\t{\370g\310pbr\301"
+# p SecureRandom.random_bytes(10) #=> "\323U\030TO\234\357\020\a\337"
+# ...
+
 require 'java'
 
-# Implement 1.9'S SecureRandom class in Java with java.security.SecureRandom.
+# Implements 1.8.7/1.9's SecureRandom class with java.security.SecureRandom.
 class SecureRandom
   # SecureRandom.random_bytes generates a random binary string.
   #
@@ -13,6 +46,9 @@ class SecureRandom
   # NotImplementedError is raised.
   def self.random_bytes(n=nil)
     n ||= 16
+    raise ArgumentError, "non-integer argument: #{n}" unless n.is_a?(Fixnum)
+    raise ArgumentError, "negative argument: #{n}" if n < 0
+
     bytes = Java::byte[n].new
     java.security.SecureRandom.new.nextBytes(bytes)
     String.from_java_bytes bytes
@@ -56,7 +92,7 @@ class SecureRandom
   # SecureRandom.random_number returns an float:
   # 0.0 <= SecureRandom.random_number() < 1.0.
   def self.random_number(n=0)
-    if n == 0
+    if 0 < n
       java.security.SecureRandom.new.nextInt(n)
     else
       java.security.SecureRandom.new.nextDouble
