c0b5a113a3f7d92f77402163b93972b3de0b03e5
Vladimir Sizikov <vsizikov@gmail.com>
2008-05-31 19:58:51 +0000

    JRUBY-2598: Rubygems installs non-functioning BAT files on Windows.
    Now, jruby.bat can handle situations when it's been invoked from
    another BAT file. Did I mentioned that I HATE WINDOWS? :)
    
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6860 961051c9-f516-0410-bf72-c9f7e237a7b7

--- bin/jruby.bat
+++ bin/jruby.bat
@@ -5,12 +5,18 @@ rem
 rem for info on environment variables, see internal batch script _jrubyvars.bat
 
 setlocal
-call "%~dp0_jrubyvars" %*
+
+rem Sometimes, when jruby.bat is being invoked from another BAT file,
+rem %~dp0 is incorrect and points to the current dir, not to JRuby's bin dir,
+rem so we look on the PATH in such cases.
+IF EXIST "%~dp0_jrubyvars.bat" (set FULL_PATH=%~dp0) ELSE (set FULL_PATH=%~dp$PATH:0)
+
+call "%FULL_PATH%_jrubyvars.bat" %*
 
 if %JRUBY_BAT_ERROR%==0 "%_STARTJAVA%" %_VM_OPTS% -Xbootclasspath/a:"%CP%" -classpath "%CLASSPATH%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat org.jruby.Main %JRUBY_OPTS% %_RUBY_OPTS%
 set E=%ERRORLEVEL%
 
-call "%~dp0_jrubycleanup"
+call "%FULL_PATH%_jrubycleanup"
 
 rem 1. exit must be on the same line in order to see local %E% variable!
 rem 2. we must use cmd /c in order for the exit code properly returned!
