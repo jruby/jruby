edb1f9b7e0e9db4ed4dfc4c7850e616a51552c29
Vladimir Sizikov <vsizikov@gmail.com>
2008-06-17 17:01:39 +0000

    More test cases for respond_to? benchmark.
    Notes:
    * 1.respond_to?(:to_ary) is 2x slower than 1.respond_to?(:next)
    * respond_to? with true second arg has no major impact on performance
    * redefined respond_to? is still fast
    
    
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@6992 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_respond_to.rb
+++ test/bench/bench_respond_to.rb
@@ -1,9 +1,15 @@
 require 'benchmark'
 
 (ARGV[0] || 10).to_i.times do
-  Benchmark.bm(30) do |bm|
+  obj = Object.new
+  def obj.respond_to?(sym)
+    true
+  end
+  Benchmark.bm(35) do |bm|
     bm.report("control, 1 and :to_ary") { 1_000_000.times { 1; :to_ary }}
     bm.report("1m 1.respond_to?(:to_ary)") { 1_000_000.times { 1.respond_to? :to_ary }}
+    bm.report("1m 1.respond_to?(:to_ary,true)") { 1_000_000.times { 1.respond_to?(:to_ary, true) }}
     bm.report("1m 1.respond_to?(:next)") { 1_000_000.times { 1.respond_to? :next }}
+    bm.report("1m redefined obj.respond_to?") { 1_000_000.times { obj.respond_to? :next }}
   end
 end
