75f2ba5ceb0e8e01ab084ac9a89686c0533e86bd
Vladimir Sizikov <vsizikov@gmail.com>
2008-06-26 12:22:43 +0000

    Rubygems customization: Make proper jruby executable name on Windows
    * jruby.bat on Windows, not jruby.bat.bat
    
    
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@7032 961051c9-f516-0410-bf72-c9f7e237a7b7

--- lib/ruby/site_ruby/1.8/rubygems/defaults/jruby.rb
+++ lib/ruby/site_ruby/1.8/rubygems/defaults/jruby.rb
@@ -3,9 +3,25 @@ module Gem
   # Default home directory path to be used if an alternate value is not
   # specified in the environment.
   #
-  # We don't want gems installed in lib/jruby/gems, but rather to
-  # preserve the old location: lib/ruby/gems.
+  # JRuby: We don't want gems installed in lib/jruby/gems, but rather
+  # to preserve the old location: lib/ruby/gems.
   def self.default_dir
     File.join ConfigMap[:libdir], 'ruby', 'gems', ConfigMap[:ruby_version]
   end
+
+  ##
+  # The path to the running Ruby interpreter.
+  #
+  # JRuby: Don't append ConfigMap[:EXEEXT] to @jruby, since that would
+  # make it jruby.bat.bat on Windows.
+  def self.ruby
+    if @ruby.nil? then
+      @ruby = File.join(ConfigMap[:bindir],
+                        ConfigMap[:ruby_install_name])
+      # @ruby << ConfigMap[:EXEEXT]
+    end
+
+    @ruby
+  end
+
 end
