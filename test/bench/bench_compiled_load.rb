ac08e6f5ddfa4ccf747a6e0afcc1ecced48cd9c8
Charles Oliver Nutter <headius@headius.com>
2008-07-02 16:06:59 +0000

    Adding Peter Chan's "many defs" case to the compiled load benchmark. JRUBY-2714.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@7075 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/bench_compiled_load.rb
+++ test/bench/bench_compiled_load.rb
@@ -1,4 +1,9 @@
 require 'benchmark'
+jrubyc = "jruby jrubyc"
+
+def jrubyc(file)
+    system "jrubyc -p 'ruby' #{file}"
+end
 
 def bench_compiled_load(bm)
   File.open('compiled_load_test_simple.rb', 'w') do |f|
@@ -24,13 +29,24 @@ end
     "
   end
   
-  system 'jrubyc compiled_load_test_simple.rb'
-  system 'jrubyc compiled_load_test_simple_strings.rb'
-  system 'jrubyc compiled_load_test_complex_flat.rb'
-  system 'jrubyc compiled_load_test_complex_tree.rb'
+  File.open('compiled_load_test_many_defs.rb', 'w') do |f|
+    f.write "
+      def foo; end
+      def bar; end
+      def oof; end
+      def rab; end
+  "
+  end
+  
+  jrubyc 'compiled_load_test_simple.rb'
+  jrubyc 'compiled_load_test_simple_strings.rb'
+  jrubyc 'compiled_load_test_complex_flat.rb'
+  jrubyc 'compiled_load_test_complex_tree.rb'
+  jrubyc 'compiled_load_test_many_defs.rb'
 #  
   $: << '.'
   bm.report("control, 1000.times") { 1000.times {1} }
+
   bm.report("control, 10000 loads simple") do
     10000.times { load 'compiled_load_test_simple.rb' }
   end
@@ -43,6 +59,10 @@ end
   bm.report("control, 100 loads complex tree") do
     100.times { load 'compiled_load_test_complex_tree.rb' }
   end
+  bm.report("control, 1000 loads many defs") do
+    1000.times { load 'compiled_load_test_many_defs.rb' }
+  end
+
   bm.report("10000 loads simple compiled") do
     10000.times { load 'ruby/compiled_load_test_simple.class' }
   end
@@ -55,6 +75,9 @@ end
   bm.report("100 loads complex tree compiled") do
     100.times { load 'ruby/compiled_load_test_complex_tree.class' }
   end
+  bm.report("1000 loads many defs compiled") do
+    1000.times { load 'ruby/compiled_load_test_many_defs.class' }
+  end
 end
 
 if __FILE__ == $0
@@ -63,8 +86,10 @@ if __FILE__ == $0
   File.delete("compiled_load_test_simple_strings.rb")
   File.delete("compiled_load_test_complex_flat.rb")
   File.delete("compiled_load_test_complex_tree.rb")
+  File.delete("compiled_load_test_many_defs.rb")
   File.delete("ruby/compiled_load_test_simple.class")
   File.delete("ruby/compiled_load_test_simple_strings.class")
   File.delete("ruby/compiled_load_test_complex_flat.class")
   File.delete("ruby/compiled_load_test_complex_tree.class")
+  File.delete("ruby/compiled_load_test_many_defs.class")  
 end
