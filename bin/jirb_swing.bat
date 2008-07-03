8014dd53c62c6a1f316a5d12ab962a6eca9200bf
Vladimir Sizikov <vsizikov@gmail.com>
2008-07-03 09:13:22 +0000

    - JRUBY-2742: Major code duplication in BAT files - Additional fix for JRUBY-2720: 3rd party library can't get class loader
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@7092 961051c9-f516-0410-bf72-c9f7e237a7b7

--- bin/jirb_swing.bat
+++ bin/jirb_swing.bat
@@ -1,11 +1,6 @@
-@echo off
-rem ---------------------------------------------------------------------------
-
-setlocal
-call "%~dp0_jrubyvars" %*
-
-if %JRUBY_BAT_ERROR%==0 "%_STARTJAVA%" %_VM_OPTS% -Xbootclasspath/a:"%CP%" -cp "%CLASSPATH%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.shell="cmd.exe" -Djruby.script=jruby.bat org.jruby.Main %JRUBY_OPTS% "%JRUBY_HOME%\bin\jirb_swing" %_RUBY_OPTS%
-set E=%ERRORLEVEL%
-
-call "%~dp0_jrubycleanup"
-endlocal
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"jruby.bat" -S "jirb_swing" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"jruby.bat" "%~dpn0" %*
--- bin/jrubyd.bat
+++ bin/jrubyd.bat
@@ -7,7 +7,7 @@ rem ----- Execute The Requested Command ---------------------------------------
 setlocal
 call "%~dp0_jrubyvars" %*
 
-if %JRUBY_BAT_ERROR%==0 "%_STARTJAVA%" %_VM_OPTS% -Xdebug -Xrunjdwp:transport=dt_shmem,server=y,suspend=y  -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.script="jruby.bat" -Djruby.shell="cmd.exe" org.jruby.Main %JRUBY_OPTS% %_RUBY_OPTS%
+if %JRUBY_BAT_ERROR%==0 "%_STARTJAVA%" %_VM_OPTS% -Xdebug -Xrunjdwp:transport=dt_shmem,server=y,suspend=y  -Xbootclasspath/a:"%JRUBY_CP%" -classpath "%CP%;%CLASSPATH%" -Djruby.home="%JRUBY_HOME%" -Djruby.lib="%JRUBY_HOME%\lib" -Djruby.script="jruby.bat" -Djruby.shell="cmd.exe" org.jruby.Main %JRUBY_OPTS% %_RUBY_OPTS%
 set E=%ERRORLEVEL%
 
 call "%~dp0_jrubycleanup"
