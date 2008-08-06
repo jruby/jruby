f8d9cdebe7a26326ab7d3ad37c64210e691e2141
Charles Oliver Nutter <headius@headius.com>
2008-08-06 21:43:52 +0000

    Add /test/bench/java subdir, move java benches there, and add an Array#to_java benchmark.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@7393 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ test/bench/java/bench_array_to_java.rb
@@ -0,0 +1,13 @@
+require 'java'
+require 'benchmark'
+
+TIMES = (ARGV[0] || 5).to_i
+
+TIMES.times do
+  Benchmark.bm(30) do |bm|
+    bm.report("control") {a = [1,2,3,4]; 100_000.times {a}}
+    bm.report("ary.to_java") {a = [1,2,3,4]; 100_000.times {a.to_java}}
+    bm.report("ary.to_java :object") {a = [1,2,3,4]; 100_000.times {a.to_java :object}}
+    bm.report("ary.to_java :string") {a = [1,2,3,4]; 100_000.times {a.to_java :string}}
+  end
+end
