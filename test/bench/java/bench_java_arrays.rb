5ed496a94d5ea09c806ddb635fe40bdc357db1c0
Charles Oliver Nutter <headius@headius.com>
2008-08-08 02:24:06 +0000

    Add a bench for java_ary.to_a, which still dispatches through Ruby code.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@7406 961051c9-f516-0410-bf72-c9f7e237a7b7

--- test/bench/java/bench_java_arrays.rb
+++ test/bench/java/bench_java_arrays.rb
@@ -9,6 +9,7 @@ TIMES.times do
     bm.report("java_ary[0]") {a = [1,2,3,4].to_java; 100_000.times {a[0]}}
     bm.report("java_ary[1,2]") {a = [1,2,3,4].to_java; 100_000.times {a[1,2]}}
     bm.report("java_ary.each {|x|}") {a = [1,2,3,4].to_java; 100_000.times {a.each {|x|}}}
-    bm.report("java_ary + java_ary") {a = [1].to_java; 100_000.times {a + a}}
+    bm.report("java_ary + java_ary") {a = [1,2,3,4].to_java; 100_000.times {a + a}}
+    bm.report("java_ary.to_a") {a = [1,2,3,4].to_java; 100_000.times {a.to_a}}
   end
 end
