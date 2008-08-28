5f8365a8301f2503d3157bd874c83d4d71dcb39b
Thomas Enebo <tom.enebo@gmail.com>
2008-08-28 06:02:31 +0000

    Fix issue with field returning unpopulated JavaObject
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@7562 961051c9-f516-0410-bf72-c9f7e237a7b7

--- spec/java_integration/fixtures/PrivateField.java
+++ spec/java_integration/fixtures/PrivateField.java
@@ -2,7 +2,7 @@ package java_integration.fixtures;
 
 public class PrivateField {
   private String strField = "1764";
-  
+
   public String getStrField() {
     return strField;
   }
--- spec/java_integration/fixtures/PublicField.java
+++ spec/java_integration/fixtures/PublicField.java
@@ -1,9 +1,8 @@
 package java_integration.fixtures;
 
+import java.util.Date;
+
 public class PublicField {
-  public String strField;
-  
-  public String getStrField() {
-    return strField;
-  }
+  public String strField = "1767";
+  public Date dateField = new Date();
 }
