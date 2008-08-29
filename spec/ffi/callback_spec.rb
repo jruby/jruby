db26b91a9e1cc16404c13c4e0f5ccd29621ef7eb
Wayne Meissner <wmeissner@gmail.com>
2008-08-29 11:34:20 +0000

    Add a test for substituting a block instead of passing in a proc as the last arg
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@7579 961051c9-f516-0410-bf72-c9f7e237a7b7

--- spec/ffi/callback_spec.rb
+++ spec/ffi/callback_spec.rb
@@ -14,5 +14,18 @@ describe "Callback" do
     # this is a bit dodgey, as it relies on qsort passing the args in order
     LibC.qsort(p, 2, 4, cmp)
     args.should == [ 1, 2 ]
+  end
+
+  it "Block can be substituted for Callback as last argument" do
+    p = MemoryPointer.new(:int, 2)
+    p.put_array_of_int32(0, [ 1 , 2 ])
+    args = []
+    # this is a bit dodgey, as it relies on qsort passing the args in order
+    LibC.qsort(p, 2, 4) do |p1, p2| 
+      args.push(p1.get_int(0))
+      args.push(p2.get_int(0))
+      0
+    end
+    args.should == [ 1, 2 ]
   end  
 end
