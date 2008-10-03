aa9fa5241d71b412f380c510d6a0ee167eb7d9d3
Wayne Meissner <wmeissner@gmail.com>
2008-10-03 22:42:35 +0000

    Make samples/ffi/ffi.rb run on MRI
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@7824 961051c9-f516-0410-bf72-c9f7e237a7b7

--- samples/ffi/ffi.rb
+++ samples/ffi/ffi.rb
@@ -5,8 +5,8 @@ module POSIX
   # this line isn't really necessary since libc is always linked into JVM
   ffi_lib 'c'
   
-  attach_function :getuid, :getuid, [], :uid_t
-  attach_function :getpid, :getpid, [], :pid_t
+  attach_function :getuid, :getuid, [], :uint
+  attach_function :getpid, :getpid, [], :uint
 end
 
 puts "Process #{POSIX.getpid} running as user #{POSIX.getuid}"
