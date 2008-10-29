448592de34c40b2a0072b0e954b75e591c4ffbde
Charles Oliver Nutter <headius@headius.com>
2008-10-29 04:12:46 +0000

    Add a couple tests for class hierarchy events that should cause caches to flush.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@7937 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ test/test_method_cache.rb
@@ -0,0 +1,85 @@
+require 'test/unit'
+
+class TestMethodCache < Test::Unit::TestCase
+  def test_simple_hierarchy
+    obj = Object.new
+    class << obj
+      class A; def foo; 1; end; end
+      class B < A; end
+      class C < B; def bar; foo; end; end
+
+      $test_simple_hierarchy1 = C.new.bar
+      class B; def foo; 2; end; end
+      $test_simple_hierarchy2 = C.new.bar
+    end
+    assert_equal(1, $test_simple_hierarchy1)
+    assert_equal(2, $test_simple_hierarchy2)
+  end
+
+  def test_simple_include
+    obj = Object.new
+    class << obj
+      class A; def foo; 1; end; end
+      module B; end
+      class C < A; include B; def bar; foo; end; end
+      $test_simple_include1 = C.new.bar
+
+      # modules included into modules don't cause flush
+      module X; def foo; 2; end; end
+      module B; include X; end
+      $test_simple_include2 = C.new.bar
+      module X; def foo; 3; end; end
+      $test_simple_include3 = C.new.bar
+
+      # changes in included modules do cause flush
+      module B; def foo; 4; end; end
+      $test_simple_include4 = C.new.bar
+    end
+
+    assert_equal(1, $test_simple_include1)
+    assert_equal(1, $test_simple_include2)
+    assert_equal(1, $test_simple_include3)
+    assert_equal(4, $test_simple_include4)
+  end
+
+  def test_simple_hierarchy_send
+    obj = Object.new
+    class << obj
+      class A; def foo; 1; end; end
+      class B < A; end
+      class C < B; def bar; foo; end; end
+
+      $test_simple_hierarchy1 = C.new.send :bar
+      class B; def foo; 2; end; end
+      $test_simple_hierarchy2 = C.new.send :bar
+    end
+    assert_equal(1, $test_simple_hierarchy1)
+    assert_equal(2, $test_simple_hierarchy2)
+  end
+
+  def test_simple_include_send
+    obj = Object.new
+    class << obj
+      class A; def foo; 1; end; end
+      module B; end
+      class C < A; include B; def bar; foo; end; end
+      $test_simple_include1 = C.new.send :bar
+
+      # modules included into modules don't cause flush
+      module X; def foo; 2; end; end
+      module B; include X; end
+      $test_simple_include2 = C.new.send :bar
+      module X; def foo; 3; end; end
+      $test_simple_include3 = C.new.send :bar
+
+      # changes in included modules do cause flush
+      module B; def foo; 4; end; end
+      $test_simple_include4 = C.new.send :bar
+    end
+
+    assert_equal(1, $test_simple_include1)
+    assert_equal(1, $test_simple_include2)
+    assert_equal(1, $test_simple_include3)
+    assert_equal(4, $test_simple_include4)
+  end
+end
