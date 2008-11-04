350ab0efe23e93e602f0430ef541aa727bf51dc1
Wayne Meissner <wmeissner@gmail.com>
2008-11-04 11:20:24 +0000

    Use auto-layout if running on ruby 1.9
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@7999 961051c9-f516-0410-bf72-c9f7e237a7b7

--- samples/ffi/gettimeofday.rb
+++ samples/ffi/gettimeofday.rb
@@ -1,8 +1,12 @@
 require 'rubygems'
 require 'ffi'
 class Timeval < FFI::Struct
-#  layout :tv_sec => :ulong, :tv_usec => :ulong
-  layout :tv_sec, :ulong, 0, :tv_usec, :ulong, 4
+  rb_maj, rb_min, rb_micro = RUBY_VERSION.split('.')
+  if rb_maj.to_i >= 1 && rb_min.to_i >= 9 || RUBY_PLATFORM =~ /java/
+    layout :tv_sec => :ulong, :tv_usec => :ulong
+  else
+    layout :tv_sec, :ulong, 0, :tv_usec, :ulong, 4
+  end
 end
 module LibC
   extend FFI::Library
