c3106552d78f780f906e15441532222d96719aa2
Charles Oliver Nutter <headius@headius.com>
2008-11-29 19:50:42 +0000

    Fix for JRUBY-3195: jruby-openssl fails to load but reports no error
    I modified the load logic to no longer swallow all errors, or even LoadError; it only swallows Gem::LoadError when trying to *activate* the gem. It allows errors during jruby-openssl requiring to propagate, as well as errors in setting up that gem or the dummy wrapper. This should correct all cases where errors cause jruby-openssl to fail loading and not tell you why.
    
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@8209 961051c9-f516-0410-bf72-c9f7e237a7b7

--- lib/ruby/site_ruby/1.8/jruby/openssl/gem.rb
+++ lib/ruby/site_ruby/1.8/jruby/openssl/gem.rb
@@ -1,17 +1,10 @@
-begin
-  old_verbose, $VERBOSE = $VERBOSE, nil # silence warnings
-  require 'rubygems'
-  tried_gem = false
-  begin
-    require 'openssl.rb'
-  rescue LoadError
-    unless tried_gem
-      tried_gem = true
-      gem 'jruby-openssl'
-      retry
-    end
-  end
-ensure
-  $VERBOSE = old_verbose
-end
+require 'rubygems'
 
+# try to activate jruby-openssl gem, and only require from the gem if it's there
+begin
+  gem 'jruby-openssl'
+  gem_success = true
+rescue Gem::LoadError => e
+  gem_success = false
+end
+require 'openssl.rb' if gem_success
