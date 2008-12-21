292ccf116b2040c8720259b46e1e1dfd7cfbb96c
Charles Oliver Nutter <headius@headius.com>
2008-12-21 22:43:19 +0000

    Fix my regression: when there's no local variables, it was creating an array of one empty string, instead of an empty String[].
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@8427 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/ast/executable/AbstractScript.java
+++ src/org/jruby/ast/executable/AbstractScript.java
@@ -174,11 +174,17 @@ public abstract class AbstractScript implements Script {
 
     private BlockBody createBlockBody(ThreadContext context, int index, String descriptor) throws NumberFormatException {
         String[] firstSplit = descriptor.split(",");
-        String[] secondSplit = firstSplit[2].split(";");
+        String[] secondSplit;
 
-        // FIXME: Big fat hack here, because scope names are expected to be interned strings by the parser
-        for (int i = 0; i < secondSplit.length; i++) {
-            secondSplit[i] = secondSplit[i].intern();
+        if (firstSplit[2].length() == 0) {
+            secondSplit = new String[0];
+        } else {
+            secondSplit = firstSplit[2].split(";");
+
+            // FIXME: Big fat hack here, because scope names are expected to be interned strings by the parser
+            for (int i = 0; i < secondSplit.length; i++) {
+                secondSplit[i] = secondSplit[i].intern();
+            }
         }
 
         BlockBody body = RuntimeHelpers.createCompiledBlockBody(
