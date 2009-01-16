2e0b9f5a445bf9fd6d16888fe7b35271b39cd2c0
Thomas Enebo <tom.enebo@gmail.com>
2009-01-16 14:34:38 +0000

    Update tempfile.rb benchmark to include writing
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@8801 961051c9-f516-0410-bf72-c9f7e237a7b7

--- bench/bench_tempfile.rb
+++ bench/bench_tempfile.rb
@@ -1,12 +1,35 @@
 require 'benchmark'
 require 'tempfile'
 
-puts "10k Tempfile.open(file)"
+COUNT = 1_000
+WRITE_COUNT = 30
+
+puts "#{COUNT} Tempfile.new(file)"
 10.times {
   puts Benchmark.measure {
     i = 0
-    while i < 1_000
-      Tempfile.new("heh")
+    while i < COUNT
+      tf = Tempfile.new("heh")
+      i = i + 1
+    end
+  }
+}
+
+puts "#{COUNT} Tempfile.new(file);write;close"
+10.times {
+  puts Benchmark.measure {
+    i = 0
+    while i < COUNT
+      tf = Tempfile.new("heh")
+      j = 0
+      while j < WRITE_COUNT
+        tf.write "hehheheheheheheehehe\n"
+        j = j + 1
+      end
+      tf.flush
+      puts tf.path
+      gets
+      tf.close
       i = i + 1
     end
   }
