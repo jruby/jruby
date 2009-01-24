3725c1de28f273a124a4bfd356d87dffafbe7ee2
Charles Oliver Nutter <headius@headius.com>
2009-01-24 18:35:00 +0000

    Expand delegate bench a bit to include wrapped Ruby methods.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@8879 961051c9-f516-0410-bf72-c9f7e237a7b7

--- bench/bench_delegate.rb
+++ bench/bench_delegate.rb
@@ -4,9 +4,14 @@ require 'delegate'
 TIMES = (ARGV[0] || 1).to_i
 TIMES.times {
   Benchmark.bm(30) {|bm|
-    obj = ''
-    dele = SimpleDelegator.new(obj)
-    bm.report("control, direct calls") { 10_000_000.times { obj.to_str }}
-    bm.report("SimpleDelegate wrapper") { 10_000_000.times { dele.to_str }}
+    obj1 = ''
+    obj2 = Object.new
+    def obj2.to_str(); 'foo'; end
+    dele1 = SimpleDelegator.new(obj1)
+    dele2 = SimpleDelegator.new(obj2)
+    bm.report("control, direct calls, native") { 10_000_000.times { obj1.to_str }}
+    bm.report("SimpleDelegate wrapper, native") { 10_000_000.times { dele1.to_str }}
+    bm.report("control, direct calls, ruby") { 10_000_000.times { obj2.to_str }}
+    bm.report("SimpleDelegate wrapper, ruby") { 10_000_000.times { dele2.to_str }}
   }
 }
