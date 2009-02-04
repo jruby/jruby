cfad0aa46e1e5ffbd9f0298e80fffa54ae85f501
Charles Oliver Nutter <headius@headius.com>
2009-02-04 10:30:37 +0000

    Fix for JRUBY-2346: Nailgun does not compile under HP-UX
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@8995 961051c9-f516-0410-bf72-c9f7e237a7b7

--- tool/nailgun/src/c/ng.c
+++ tool/nailgun/src/c/ng.c
@@ -62,6 +62,10 @@
 #define MIN(a,b) ((a<b)?(a):(b))
 #endif
 
+#ifndef MSG_WAITALL
+#define MSG_WAITALL 0x40 /* wait for full request or error */
+#endif
+
 #ifdef WIN32
 	#define NAILGUN_FILESEPARATOR "NAILGUN_FILESEPARATOR=\\"
 	#define NAILGUN_PATHSEPARATOR "NAILGUN_PATHSEPARATOR=;"
