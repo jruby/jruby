705e1317664a576d2ddea3565b645d6d43fee1bb
Charles Oliver Nutter <headius@headius.com>
2009-02-08 20:44:24 +0000

    Add specs for JRUBY-414.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@9067 961051c9-f516-0410-bf72-c9f7e237a7b7

---
+++ spec/java_integration/object/marshal_spec.rb
@@ -0,0 +1,16 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+import "java.util.ArrayList"
+
+describe "A Java object" do
+  it "marshals as custom Ruby marshal data" do
+    list = ArrayList.new
+    list << 'foo'
+
+    hash = {:foo => list}
+
+    marshaled = Marshal.load(Marshal.dump(hash))
+    marshaled[:foo].class.should == ArrayList
+    marshaled[:foo][0].should == 'foo'
+  end
+end
