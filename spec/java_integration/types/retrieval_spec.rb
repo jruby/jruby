0d14dc2fb06325ac7b342a1008650ca81e4f6adf
Charles Oliver Nutter <headius@headius.com>
2009-02-10 00:53:57 +0000

    Specs for now-working JRUBY-3016: Nested Class/Enum issue with JRockit
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@9081 961051c9-f516-0410-bf72-c9f7e237a7b7

--- spec/java_integration/types/retrieval_spec.rb
+++ spec/java_integration/types/retrieval_spec.rb
@@ -1,5 +1,7 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
+import "java_integration.fixtures.ClassWithEnums"
+
 describe "Java::JavaClass.for_name" do
   it "should return primitive classes for Java primitive type names" do
     Java::JavaClass.for_name("byte").should == Java::byte.java_class
@@ -10,5 +12,11 @@ describe "Java::JavaClass.for_name" do
     Java::JavaClass.for_name("long").should == Java::long.java_class
     Java::JavaClass.for_name("float").should == Java::float.java_class
     Java::JavaClass.for_name("double").should == Java::double.java_class
+  end
+end
+
+describe "Java classes with nested enums" do
+  it "should allow access to the values() method on the enum" do
+    ClassWithEnums::Enums.values.map{|e|e.to_s}.should == ["A", "B", "C"];
   end
 end
