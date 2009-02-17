5c624ebf4fa5bfe2c43b38fcbc85f6b2d7cb1765
Charles Oliver Nutter <headius@headius.com>
2009-02-17 06:18:19 +0000

    Remove unused import.
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@9211 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/ext/socket/RubyIPSocket.java
+++ src/org/jruby/ext/socket/RubyIPSocket.java
@@ -39,7 +39,6 @@ import org.jruby.exceptions.RaiseException;
 import org.jruby.runtime.ObjectAllocator;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.builtin.IRubyObject;
-import org.jruby.util.ByteList;
 
 /**
  * @author <a href="mailto:ola.bini@ki.se">Ola Bini</a>
