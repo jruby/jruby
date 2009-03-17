a1998d861012426ab91ddf2a12527421589256ff
Ola Bini <ola.bini@gmail.com>
2009-03-17 12:17:53 +0000

    Recommit new jna-posix plus other security related things from Ryan Brown
    git-svn-id: http://svn.codehaus.org/jruby/trunk/jruby@9426 961051c9-f516-0410-bf72-c9f7e237a7b7

--- src/org/jruby/util/NormalizedFile.java
+++ src/org/jruby/util/NormalizedFile.java
@@ -33,11 +33,13 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.URI;
 
+import org.jruby.ext.posix.JavaSecuredFile;
+
 /**
  * This class provides a File implementation that normalizes all path separators to forward slashes.
  * This mimics the behavior of C Ruby, where all paths are internally made UNIX-style, even on Windows.
  */
-public class NormalizedFile extends File {
+public class NormalizedFile extends JavaSecuredFile {
     private static final long serialVersionUID = 7630618150344842227L;
 
     public NormalizedFile(String pathname) {
