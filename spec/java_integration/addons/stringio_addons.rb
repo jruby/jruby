601736c1d830a0253f109032fc213606d0d3ecb1
Charles Oliver Nutter <headius@headius.com>
2009-05-19 15:55:01 -0500

    Add StringIO#to_inputstream, to_outputstream, and to_channel for JRUBY-3586

---
+++ spec/java_integration/addons/stringio_addons.rb
@@ -0,0 +1,39 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+require 'tempfile'
+require 'stringio'
+
+describe "Ruby StringIO" do
+  it "should be coercible to java.io.InputStream with StringIO#to_inputstream" do
+    file = StringIO.new('abcdefghij')
+    stream = file.to_inputstream
+    java.io.InputStream.should === stream
+    
+    bytes = "0000000000".to_java_bytes
+    stream.read(bytes).should == 10
+    String.from_java_bytes(bytes).should == 'abcdefghij'
+  end
+
+  it "should be coercible to java.io.OutputStream with StringIO#to_outputstream" do
+    file = StringIO.new
+    java.io.OutputStream.should === stream 
+    
+    bytes = "1234567890".to_java_bytes
+    stream.write(bytes)
+    stream.flush
+    file.seek(0)
+    str = file.read(10)
+    str.should == String.from_java_bytes(bytes)
+  end
+
+  it "should be coercible to java.nio.channels.Channel with StringIO#to_channel" do
+    file = StringIO.new
+    channel = file.to_channel
+    java.nio.channels.Channel.should === channel 
+    
+    bytes = java.nio.ByteBuffer.wrap("1234567890".to_java_bytes)
+    channel.write(bytes)
+    file.seek(0)
+    str = file.read(10)
+    str.should == String.from_java_bytes(bytes.array)
+  end
+end
--- src/org/jruby/java/addons/IOJavaAddons.java
+++ src/org/jruby/java/addons/IOJavaAddons.java
@@ -7,12 +7,20 @@ import org.jruby.anno.JRubyMethod;
 import org.jruby.javasupport.JavaUtil;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.builtin.IRubyObject;
+import org.jruby.util.IOChannel;
+import org.jruby.util.IOInputStream;
+import org.jruby.util.IOOutputStream;
 import org.jruby.util.io.BadDescriptorException;
 import org.jruby.util.io.InvalidValueException;
 import org.jruby.util.io.PipeException;
 
 public class IOJavaAddons {
-    @JRubyMethod
+    // FIXME This whole thing could probably be implemented as a module and
+    // mixed into appropriate classes, especially if it uses either
+    // IOInput/OutputStream or is smart about the kind of IO-like object
+    // it's being used against.
+    
+    @JRubyMethod(frame = true)
     public static IRubyObject to_inputstream(ThreadContext context, IRubyObject self) {
         RubyIO io = (RubyIO)self;
         Ruby runtime = context.getRuntime();
@@ -32,7 +40,7 @@ public class IOJavaAddons {
         return JavaUtil.convertJavaToUsableRubyObject(context.getRuntime(), io.getInStream());
     }
     
-    @JRubyMethod
+    @JRubyMethod(frame = true)
     public static IRubyObject to_outputstream(ThreadContext context, IRubyObject self) {
         RubyIO io = (RubyIO)self;
         Ruby runtime = context.getRuntime();
@@ -51,11 +59,38 @@ public class IOJavaAddons {
         
         return JavaUtil.convertJavaToUsableRubyObject(context.getRuntime(), io.getOutStream());
     }
-    
-    @JRubyMethod
-    public static IRubyObject to_channel(ThreadContext context, IRubyObject self) {
-        RubyIO io = (RubyIO)self;
-        
-        return JavaUtil.convertJavaToUsableRubyObject(context.getRuntime(), io.getChannel());
+
+    public static class AnyIO {
+        @JRubyMethod(name = "to_inputstream", frame = true)
+        public static IRubyObject any_to_inputstream(ThreadContext context, IRubyObject self) {
+            // using IOInputStream may not be the most performance way, but it's easy.
+            return JavaUtil.convertJavaToUsableRubyObject(context.getRuntime(), new IOInputStream(self));
+        }
+
+        @JRubyMethod(name = "to_outputstream", frame = true)
+        public static IRubyObject any_to_outputstream(ThreadContext context, IRubyObject self) {
+            // using IOOutputStream may not be the most performance way, but it's easy.
+            return JavaUtil.convertJavaToUsableRubyObject(context.getRuntime(), new IOOutputStream(self));
+        }
+
+        @JRubyMethod(name = "to_channel", frame = true)
+        public static IRubyObject any_to_channel(ThreadContext context, IRubyObject self) {
+            // using IOChannel may not be the most performant way, but it's easy.
+            IOChannel channel;
+            if (self.respondsTo("read")) {
+                if (self.respondsTo("write") || self.respondsTo("<<")) {
+                    channel = new IOChannel.IOReadableWritableByteChannel(self);
+                } else {
+                    channel = new IOChannel.IOReadableByteChannel(self);
+                }
+            } else {
+                if (self.respondsTo("write") || self.respondsTo("<<")) {
+                    channel = new IOChannel.IOWritableByteChannel(self);
+                } else {
+                    throw context.getRuntime().newTypeError("object does not respond to any of read, write, or <<");
+                }
+            }
+            return JavaUtil.convertJavaToUsableRubyObject(context.getRuntime(), channel);
+        }
     }
 }
