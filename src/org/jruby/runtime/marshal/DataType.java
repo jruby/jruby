6bafaf522f1c5b383974763478d223c94a9f0641
Thomas E. Enebo <tom.enebo@gmail.com>
2009-05-19 15:32:38 -0500

    Method/Marshal cannot be marshalled

---
+++ src/org/jruby/runtime/marshal/DataType.java
@@ -0,0 +1,9 @@
+package org.jruby.runtime.marshal;
+
+/**
+ *
+ * DataType is similiar to T_DATA to represent types which are incapable of getting
+ * marshalled.  This is just a marker interface.
+ */
+public interface DataType {
+}
