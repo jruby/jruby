7678860bea1433b079cd7a91bbe64ac8641b710a
Ola Bini <ola.bini@gmail.com>
2009-07-24 22:39:36 +0200

    Add new yecht and a benchmark of a small piece of YAML

--- bench/bench_yaml.rb
+++ bench/bench_yaml.rb
@@ -7,12 +7,19 @@ def bench_yaml(bm)
   $bm = bm
 
   str = File.read(File.join(File.dirname(__FILE__), "big_yaml.yml"))
+  str_small = File.read(File.join(File.dirname(__FILE__), "small_yaml.yml"))
   result = YAML.load(str)
+  result_small = YAML.load(str_small)
   
-  $bm.report("1k parse") { 1_000.times { YAML.parse(str) } }
-  $bm.report("1k loads") { 1_000.times { YAML.load(str) } }
-  $bm.report("1k dumps") { 1_000.times { result.to_yaml } }
-  $bm.report("1k roundtrip") { 1_000.times { YAML.load(result.to_yaml) } }
+  $bm.report("1k small parse") { 1_000.times { YAML.parse(str_small) } }
+  $bm.report("1k small loads") { 1_000.times { YAML.load(str_small) } }
+  $bm.report("1k small dumps") { 1_000.times { result_small.to_yaml } }
+  $bm.report("1k small roundtrip") { 1_000.times { YAML.load(result_small.to_yaml) } }
+
+  $bm.report("1k big parse") { 1_000.times { YAML.parse(str) } }
+  $bm.report("1k big loads") { 1_000.times { YAML.load(str) } }
+  $bm.report("1k big dumps") { 1_000.times { result.to_yaml } }
+  $bm.report("1k big roundtrip") { 1_000.times { YAML.load(result.to_yaml) } }
 end
 
 if $0 == __FILE__
