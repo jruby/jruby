9b8dd53ab5f9738e30c3b23fd099e0182c0672b4
Charles Oliver Nutter <headius@headius.com>
2009-08-24 19:56:17 -0500

    Clean up jruby-jars gem to get a 1.3.1 release out.

--- gem/lib/jruby-jars.rb
+++ gem/lib/jruby-jars.rb
@@ -1,11 +1,13 @@
+require File.join(File.expand_path(File.dirname(__FILE__)), 'jruby-jars/version.rb')
+
 module JRubyJars
   PATH = File.expand_path(File.dirname(__FILE__))
 
-  def self.jruby_jar_path
-    PATH + "/jruby-#{JRUBY_VERSION}.jar"
+  def self.core_jar_path
+    PATH + "/jruby-core-#{VERSION}.jar"
   end
 
   def self.stdlib_jar_path
-    PATH + "/jruby-stdlib-#{JRUBY_VERSION}.jar"
+    PATH + "/jruby-stdlib-#{VERSION}.jar"
   end
 end
