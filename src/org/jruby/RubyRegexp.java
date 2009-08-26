be030f7b364538d194320f15057ff0fe53d0c8f5
David Calavera <david.calavera@gmail.com>
2009-08-26 11:06:35 +0200

    fixes JRUBY-3858: NPE in RubyRegexp.java with --1.9
    Signed-off-by: Nick Sieger <nick@nicksieger.com>

--- src/org/jruby/RubyRegexp.java
+++ src/org/jruby/RubyRegexp.java
@@ -1177,6 +1177,7 @@ public class RubyRegexp extends RubyObject implements ReOptions, EncodingCapable
     // rb_reg_initialize
     private RubyRegexp initializeCommon19(ByteList bytes, Encoding enc, int options) {
         Ruby runtime = getRuntime();        
+        setKCode(runtime, options);
         if (!isTaint() && runtime.getSafeLevel() >= 4) throw runtime.newSecurityError("Insecure: can't modify regexp");
         checkFrozen();
         if (isLiteral()) throw runtime.newSecurityError("can't modify literal regexp");
---
+++ test/test_regexp_1_9.rb
@@ -0,0 +1,7 @@
+require 'test/unit'
+
+class TestRegex19 < Test::Unit::TestCase
+  def test_to_s
+    assert_equal '(?-mix:foo|bar)', /#{Regexp.union("foo", "bar")}/
+  end
+end
