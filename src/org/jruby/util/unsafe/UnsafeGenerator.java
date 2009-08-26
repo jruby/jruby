2749934380c772e15388590e1976ddfa16739038
Charles Oliver Nutter <headius@headius.com>
2009-08-25 19:00:25 -0500

    Various changes to build and some code to make coverage reporting work better and be easier to use.

--- src/org/jruby/util/unsafe/UnsafeGenerator.java
+++ src/org/jruby/util/unsafe/UnsafeGenerator.java
@@ -24,12 +24,14 @@ public class UnsafeGenerator {
         
         SkinnyMethodAdapter method = new SkinnyMethodAdapter(cw.visitMethod(ACC_PUBLIC, "<init>", sig(void.class), null, null));
         method.start();
+        method.line(0);
         method.aload(0);
         method.invokespecial(p(Object.class), "<init>", sig(void.class));
         method.voidreturn();
         method.end();
         
         method = new SkinnyMethodAdapter(cw.visitMethod(ACC_PUBLIC, "throwException", sig(void.class, Throwable.class), null, null));
+        method.line(0);
         method.start();
         method.aload(1);
         method.athrow();
