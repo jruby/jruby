5e5991879bb8d99bcffde2f23092c8152ad32c6a
Thomas E. Enebo <tom.enebo@gmail.com>
2009-09-18 21:29:34 -0700

    remove bad 1.9 test

--- test/test_loading_builtin_libraries_1_9.rb
+++
@@ -1,9 +0,0 @@
-require 'test/unit'
-
-class TestLoadingBuiltinLibraries < Test::Unit::TestCase
-  def test_late_bound_libraries
-    assert_nothing_raised {
-      require 'fiber.so'
-    }
-  end
-end
