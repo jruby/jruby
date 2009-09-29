13e8e7420a8e0c937f78cae53d23d3dc138baa6e
Vladimir Sizikov <vsizikov@gmail.com>
2009-09-29 20:54:46 +0200

    JRUBY-4017: [1.8] Method#name and UnboundMethod#name should return strings, not symbols
    Rubyspecs are updated, to have the proper specs for the above.

--- src/org/jruby/RubyMethod.java
+++ src/org/jruby/RubyMethod.java
@@ -256,8 +256,13 @@ public class RubyMethod extends RubyObject implements DataType {
         return str;
     }
 
-    @JRubyMethod(name = "name")
+    @JRubyMethod(name = "name", compat = CompatVersion.RUBY1_8)
     public IRubyObject name(ThreadContext context) {
+        return context.getRuntime().newString(methodName);
+    }
+
+    @JRubyMethod(name = "name", compat = CompatVersion.RUBY1_9)
+    public IRubyObject name19(ThreadContext context) {
         return context.getRuntime().newSymbol(methodName);
     }
 
