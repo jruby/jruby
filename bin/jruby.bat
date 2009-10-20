c1e73ef6eafc6fc6c6a7f816ef4e8e46e178002b
Vladimir Sizikov <vsizikov@gmail.com>
2009-10-20 12:24:37 +0200

    Additional fix for JRUBY-4121: Most *.bat commands don't work anymore.
    Corrected the jruby.bat script.

--- bin/jruby.bat
+++ bin/jruby.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"jruby.exe" -S "gem" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"jruby.exe" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
-@"jruby.exe" "%~dpn0" %*
+@"%~dpn0.exe" %*
