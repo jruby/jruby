33d7db3a0473a810f04c1039085536b60d75cd3b
Charles Oliver Nutter <headius@headius.com>
2009-10-27 10:38:12 -0500

    More fixes for JRUBY-2543: Lots of new Iconv rubyspec failures

--- src/org/jruby/RubyIconv.java
+++ src/org/jruby/RubyIconv.java
@@ -59,6 +59,7 @@ public class RubyIconv extends RubyObject {
 
     private CharsetDecoder fromEncoding;
     private CharsetEncoder toEncoding;
+    private int count;
 
     public RubyIconv(Ruby runtime, RubyClass type) {
         super(runtime, type);
@@ -211,6 +212,7 @@ public class RubyIconv extends RubyObject {
 
             fromEncoding = Charset.forName(getCharset(from)).newDecoder();
             toEncoding = Charset.forName(getCharset(to)).newEncoder();
+            count = 0;
 
             if (isIgnore(to)) {
                 fromEncoding.onUnmappableCharacter(CodingErrorAction.IGNORE);
@@ -236,6 +238,8 @@ public class RubyIconv extends RubyObject {
 
     @JRubyMethod(name = "close")
     public IRubyObject close() {
+        if (toEncoding == null && fromEncoding == null)
+            return getRuntime().getNil();
         toEncoding = null;
         fromEncoding = null;
         return RubyString.newEmptyString(getRuntime());
@@ -294,7 +298,7 @@ public class RubyIconv extends RubyObject {
 
     // FIXME: We are assuming that original string will be raw bytes.  If -Ku is provided
     // this will not be true, but that is ok for now.  Deal with that when someone needs it.
-    private IRubyObject _iconv(RubyString str, int start, int end) {
+    private IRubyObject _iconv(RubyString str, int start, int length) {
         if (fromEncoding == null) {
             throw getRuntime().newArgumentError("closed iconv");
         }
@@ -305,18 +309,16 @@ public class RubyIconv extends RubyObject {
         if (start < 0) {
             start += bytes.length();
         }
-        
-        if (end < 0) {
-            end += 1 + bytes.length();
-        } else if (end > bytes.length()) {
-            end = bytes.length();
-        }
-        
-        if (start < 0 || end < start) { // invalid ranges result in an empty string
+
+        if (start < 0 || start > bytes.length()) { // invalid ranges result in an empty string
             return RubyString.newEmptyString(getRuntime());
         }
+
+        if (length < 0 || length > bytes.length() - start) {
+            length = bytes.length() - start;
+        }
         
-        ByteBuffer buf = ByteBuffer.wrap(bytes.unsafeBytes(), bytes.begin() + start, end - start);
+        ByteBuffer buf = ByteBuffer.wrap(bytes.unsafeBytes(), bytes.begin() + start, length);
         
         try {
             CharBuffer cbuf = fromEncoding.decode(buf);
@@ -331,8 +333,25 @@ public class RubyIconv extends RubyObject {
             throw getRuntime().newIllegalSequence(str.toString());
         }
         byte[] arr = buf.array();
+
+        start = 0;
+        if (count > 0 && arr.length >= 2) { // minimum Byte Order Mark (BOM) length
+            if (toEncoding.charset().displayName().toLowerCase().startsWith("utf-16")) {
+                if ((arr[0] == (byte)0xfe && arr[1] == (byte)0xff) ||
+                        (arr[0] == (byte)0xff && arr[1] == (byte)0xfe)) {
+                    start = 2;
+                }
+            } else if (toEncoding.charset().displayName().toLowerCase().startsWith("utf-32")
+                    && arr.length >= 4) {
+                if ((arr[0] == (byte)0x00 && arr[1] == (byte)0x00 && arr[2] == (byte)0xfe && arr[3] == (byte)0xff) ||
+                        (arr[0] == (byte)0xff && arr[1] == (byte)0xfe && arr[2] == (byte)0x00 && arr[3] == (byte)0x00)) {
+                    start = 4;
+                }
+            }
+        }
         
-        return getRuntime().newString(new ByteList(arr, 0, buf.limit()));
+        count++;
+        return getRuntime().newString(new ByteList(arr, start, buf.limit() - start));
     }
 
     @JRubyMethod(name = "iconv", required = 2, rest = true, meta = true, backtrace = true)
