9f86a5c6295d05998ac743c78fac0ae1ffc96694
Charles Oliver Nutter <headius@headius.com>
2009-11-02 17:59:32 -0800

    Add inline caching for __jcreate! invocation in concrete Java object construction.

--- src/org/jruby/java/proxies/ConcreteJavaProxy.java
+++ src/org/jruby/java/proxies/ConcreteJavaProxy.java
@@ -5,6 +5,8 @@ import org.jruby.RubyClass;
 import org.jruby.RubyModule;
 import org.jruby.javasupport.util.RuntimeHelpers;
 import org.jruby.runtime.Block;
+import org.jruby.runtime.CallSite;
+import org.jruby.runtime.MethodIndex;
 import org.jruby.runtime.ObjectAllocator;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.Visibility;
@@ -27,45 +29,46 @@ public class ConcreteJavaProxy extends JavaProxy {
         });
         
         concreteJavaProxy.addMethod("initialize", new org.jruby.internal.runtime.methods.JavaMethod(concreteJavaProxy, Visibility.PUBLIC) {
+            private final CallSite jcreateSite = MethodIndex.getFunctionalCallSite("__jcreate!");
             @Override
             public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject[] args, Block block) {
-                return RuntimeHelpers.invoke(context, self, "__jcreate!", args, block);
+                return jcreateSite.call(context, self, self, args, block);
             }
             @Override
             public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, Block block) {
-                return RuntimeHelpers.invoke(context, self, "__jcreate!", block);
+                return jcreateSite.call(context, self, self, block);
             }
             @Override
             public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject arg0, Block block) {
-                return RuntimeHelpers.invoke(context, self, "__jcreate!", arg0, block);
+                return jcreateSite.call(context, self, self, arg0, block);
             }
             @Override
             public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject arg0, IRubyObject arg1, Block block) {
-                return RuntimeHelpers.invoke(context, self, "__jcreate!", arg0, arg1, block);
+                return jcreateSite.call(context, self, self, arg0, arg1, block);
             }
             @Override
             public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject arg0, IRubyObject arg1, IRubyObject arg2, Block block) {
-                return RuntimeHelpers.invoke(context, self, "__jcreate!", arg0, arg1, arg2, block);
+                return jcreateSite.call(context, self, self, arg0, arg1, arg2, block);
             }
             @Override
             public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject[] args) {
-                return RuntimeHelpers.invoke(context, self, "__jcreate!", args);
+                return jcreateSite.call(context, self, self, args);
             }
             @Override
             public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name) {
-                return RuntimeHelpers.invoke(context, self, "__jcreate!");
+                return jcreateSite.call(context, self, self);
             }
             @Override
             public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject arg0) {
-                return RuntimeHelpers.invoke(context, self, "__jcreate!", arg0);
+                return jcreateSite.call(context, self, self, arg0);
             }
             @Override
             public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject arg0, IRubyObject arg1) {
-                return RuntimeHelpers.invoke(context, self, "__jcreate!", arg0, arg1);
+                return jcreateSite.call(context, self, self, arg0, arg1);
             }
             @Override
             public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject arg0, IRubyObject arg1, IRubyObject arg2) {
-                return RuntimeHelpers.invoke(context, self, "__jcreate!", arg0, arg1, arg2);
+                return jcreateSite.call(context, self, self, arg0, arg1, arg2);
             }
         });
         
