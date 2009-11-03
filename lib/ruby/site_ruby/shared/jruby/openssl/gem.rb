b8b62295978b0052b27d30ac88c35156356550c6
Nick Sieger <nick@nicksieger.com>
2009-11-03 09:09:46 -0600

    JRUBY-4188: Fix way jruby-openssl is loaded for bundler

--- lib/ruby/site_ruby/shared/jruby/openssl/gem.rb
+++ lib/ruby/site_ruby/shared/jruby/openssl/gem.rb
@@ -3,8 +3,7 @@ require 'rubygems'
 # try to activate jruby-openssl gem, and only require from the gem if it's there
 begin
   gem 'jruby-openssl'
-  gem_success = true
-rescue Gem::LoadError => e
-  gem_success = false
+  require 'openssl.rb'
+rescue LoadError
 end
-require 'openssl.rb' if gem_success
+
