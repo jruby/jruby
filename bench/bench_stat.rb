654247ff4e138409c2b832240a2520f3ac736fea
Vladimir Sizikov <vsizikov@gmail.com>
2009-11-16 22:15:07 +0100

    Make bench_stat work on Windows.

--- bench/bench_stat.rb
+++ bench/bench_stat.rb
@@ -1,13 +1,13 @@
 require 'benchmark'
+tmp = ENV['TMP'] || ENV['TEMP'] || ENV['TMPDIR'] || ENV["USERPROFILE"] || '/tmp'
 
-puts "10k File.stat(file)"
+puts "10k File.stat(dir)"
 10.times {
   puts Benchmark.measure {
     i = 0
     while i < 10_000
-      File.stat("/tmp")
+      File.stat(tmp)
       i = i + 1
     end
   }
 }
-
