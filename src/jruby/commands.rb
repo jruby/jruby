6bf62d24aebf68e2495bdc7e692bae4a30fbe046
Vladimir Sizikov <vsizikov@gmail.com>
2009-11-17 23:18:36 +0100

    JRUBY-4245:  maybe_install_gems always reinstalls gems

--- src/jruby/commands.rb
+++ src/jruby/commands.rb
@@ -15,22 +15,55 @@ module JRuby
 
       def maybe_install_gems
         require 'rubygems'
+        require 'rubygems/format'
+
         # Want the kernel gem method here; expose a backdoor b/c RubyGems 1.3.1 made it private
         Object.class_eval { def __gem(g); gem(g); end }
         gem_loader = Object.new
+
         ARGV.delete_if do |g|
-          begin
-            gem_loader.__gem(g)
-            puts "#{g} already installed"
-            true
-          rescue Gem::LoadError
-            false
+          # skip options
+          next false if g =~ /^-/
+
+          if File.exist?(g) # local gem
+            begin
+              gem = Gem::Format.from_file_by_path(g)
+              name = gem.spec.name
+              ver = gem.spec.version
+              dep = Gem::Dependency.new(name, ver)
+
+              # check, whether the same gem is already installed
+              if Gem.source_index.search(dep).empty?
+                false
+              else
+                puts "#{g} already installed"
+                true
+              end
+            rescue Gem::Exception
+              false
+            end
+          else
+            # remote gem
+            begin
+              gem_loader.__gem(g)
+              puts "#{g} already installed"
+              true
+            rescue Gem::LoadError
+              false
+            end
           end
         end
+
         Object.class_eval { remove_method :__gem }
+
         unless ARGV.reject{|a| a =~ /^-/}.empty?
           ARGV.unshift "install"
-          load Config::CONFIG['bindir'] + "/gem"
+          begin
+            load Config::CONFIG['bindir'] + "/gem"
+          rescue SystemExit => e
+            # don't exit in case of 0 return value from 'gem'
+            exit(e.status) unless e.success?
+          end
         end
         generate_bat_stubs
       end
@@ -42,10 +75,12 @@ module JRuby
           next if File.exist?("#{fn}.bat")
           next unless File.open(fn) {|io| (io.readline rescue "") =~ /^#!.*ruby/}
           puts "Generating #{File.basename(fn)}.bat"
+          # FIXME: Outdated BAT template, update!
           File.open("#{fn}.bat", "wb") do |f|
             f << "@echo off\n"
             f << "call \"%~dp0jruby\" -S #{File.basename(fn)} %*\n"
           end
+          # FIXME: foo.bat? Really? There is no foo.bat anywhere.
         end if File.writable?(File.join(Config::CONFIG['bindir'], 'foo.bat'))
       end
 
