353a73ab503de1d3deb80a58930c4dee06f699af
Vladimir Sizikov <vsizikov@gmail.com>
2009-11-25 10:08:20 +0100

    bench_load updated to include loading of compiled classes as well
    Initial numbers are not good. JRuby is *10x* times slower than MRI.

--- bench/bench_load.rb
+++ bench/bench_load.rb
@@ -1,6 +1,11 @@
 require 'benchmark'
 
+DIR = File.expand_path(File.dirname(__FILE__))
 DATA_FILE1 = File.join(File.expand_path(File.dirname(__FILE__)), "load_data.rb")
+
+DATA_FILE1_COMP = File.join(File.expand_path(File.dirname(__FILE__)), "load_data.class")
+File.delete(DATA_FILE1_COMP) if File.exist?(DATA_FILE1_COMP)
+
 DATA_FILE2 = File.join(File.expand_path(File.dirname(__FILE__)), "..", "lib/ruby/1.8", "rational.rb")
 
 (ARGV[0] || 1).to_i.times do
@@ -17,12 +22,25 @@ DATA_FILE2 = File.join(File.expand_path(File.dirname(__FILE__)), "..", "lib/ruby
       }
     }
 
+    # jruby-specific benchmark
+    if defined?(JRUBY_VERSION)
+      Dir.chdir(DIR) {
+        `jrubyc #{DATA_FILE1}`
+      }
+
+      bm.report(" 1K load compiled 'fileutils-like'") {
+        1_000.times {
+          load DATA_FILE1_COMP
+        }
+      }
+    end
+
     bm.report(" 1K load 'rational'") {
       1_000.times {
         begin
           load DATA_FILE2
         rescue LoadError
-          p 5
+          p "ERROR: can't load #{DATA_FILE2}"
         end
       }
     }
