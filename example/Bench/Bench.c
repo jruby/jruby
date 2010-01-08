f4ff4342ea831b61463902eca5cda05090489f6a
Wayne Meissner <wmeissner@gmail.com>
2010-01-09 00:04:42 +1000

    Move the benchmarking into its own example dir

---
+++ example/Bench/Bench.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ruby.h>
+
+VALUE HelloModule = Qnil;
+VALUE HelloClass = Qnil;
+
+VALUE say_hello(VALUE self, VALUE hello);
+VALUE get_hello(VALUE self);
+
+void
+Init_bench()
+{
+    HelloClass = rb_define_class("Hello", rb_cObject);
+    rb_define_method(HelloClass, "get_hello", get_hello, 0);
+    rb_define_method(HelloClass, "say_hello", say_hello, 1);
+}
+
+VALUE
+say_hello(VALUE self, VALUE hello)
+{
+    return Qnil;
+}
+VALUE
+get_hello(VALUE self)
+{
+    return rb_str_new2("Hello, World");
+}
+
+const char*
+ffi_get_hello(void)
+{
+  return "Hello, World";
+}
--- example/DefineClass/DefineClass.rb
+++ example/DefineClass/DefineClass.rb
@@ -2,7 +2,7 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
  
-include Java
+require 'java'
 
 m = org.jruby.cext.ModuleLoader.new
 m.load(self, "defineclass")
@@ -11,16 +11,3 @@ h = Hello.new
 puts "Hello.new returned #{h.inspect}"
 puts "h.get_hello returns #{h.get_hello}"
 
-def ruby_hello
- "Hello from Ruby"
-end
-
-require 'benchmark'
-
-iter = 100_000
-10.times do
- Benchmark.bmbm do |bm|
-   bm.report("C") { iter.times { h.get_hello } }
-   bm.report("Ruby") { iter.times { ruby_hello } }
- end
-end
