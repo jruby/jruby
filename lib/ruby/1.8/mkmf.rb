bff2492180162b6baa1bbcf6f981d0d84fb92ea7
Vladimir Sizikov <vsizikov@gmail.com>
2010-01-09 20:01:03 +0100

    JRUBY-4432: Dead links to old wiki in help messages, readme files
    Patch by Lars Westergren. Thanks!

--- lib/ruby/1.8/mkmf.rb
+++ lib/ruby/1.8/mkmf.rb
@@ -4,7 +4,7 @@ enabled=false
 
 # JRuby does not support mkmf yet, so we fail hard here with a useful message
 if !enabled
-  warn "WARNING: JRuby does not support native extensions or the `mkmf' library.\n         Check wiki.jruby.org for alternatives."
+  warn "WARNING: JRuby does not support native extensions or the `mkmf' library.\n         Check http://kenai.com/projects/jruby/pages/Home for alternatives."
 else
 
 # We're missing this in our rbconfig
