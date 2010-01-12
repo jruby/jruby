165b87d10ce27cffb112b7478a25e123479acf34
Vladimir Sizikov <vsizikov@gmail.com>
2010-01-12 21:03:04 +0100

    Fix test failures on Windows.

--- test/test_process.rb
+++ test/test_process.rb
@@ -53,20 +53,23 @@ class TestProcess < Test::Unit::TestCase
   end
 
   if (WINDOWS)
+    def test_gid_windows
+      assert_equal 0, Process.gid
+      assert_equal 0, Process.egid
+    end
+
     # JRUBY-2352
     def test_not_implemented_methods_on_windows
       # The goal here is to make sure that those "weird"
       # POSIX methods don't break JRuby, since there were
       # numerous regressions in this area.
       assert_raise(NotImplementedError) { Process.uid = 5 }
-      assert_raise(NotImplementedError) { Process.gid }
       assert_raise(NotImplementedError) { Process.gid = 5 }
 
       # TODO: JRUBY-2705, doesn't work on x64 JVM
       assert_equal 0, Process.euid unless WINDOWS_JVM_64
 
       assert_raise(NotImplementedError) { Process.euid = 5 }
-      assert_raise(NotImplementedError) { Process.egid }
       assert_raise(NotImplementedError) { Process.egid = 5 }
       assert_raise(NotImplementedError) { Process.getpgid(100) }
       assert_raise(NotImplementedError) { Process.setpgid(100, 555) }
