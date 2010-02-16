356c06a63851e716fa1e167ab5ef428faa36bee3
Wayne Meissner <wmeissner@gmail.com>
2010-02-16 20:17:19 +1000

    Convert arguments to FFI.map_library_name() to a String

--- lib/ruby/site_ruby/shared/ffi/ffi.rb
+++ lib/ruby/site_ruby/shared/ffi/ffi.rb
@@ -82,6 +82,7 @@ require 'ffi/enum'
 module FFI
   
   def self.map_library_name(lib)
+    lib = lib.to_s
     # Mangle the library name to reflect the native library naming conventions
     lib = Platform::LIBC if lib == 'c'
     if lib && File.basename(lib) == lib
