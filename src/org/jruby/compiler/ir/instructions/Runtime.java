94f056b03629d4f4d7685aefa47a1a959db9147d
Thomas E. Enebo <tom.enebo@gmail.com>
2010-05-28 10:20:14 -0500

    Add simple marker annotation for runtime-useful methods for IR classes

---
+++ src/org/jruby/compiler/ir/instructions/Runtime.java
@@ -0,0 +1,8 @@
+package org.jruby.compiler.ir.instructions;
+
+/**
+ * Marker Enumeration for indicating which methods in the IR are for runtime
+ * use versus compiler use.
+ */
+public @interface Runtime {
+}
