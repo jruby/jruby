e03b55f3e8c88b085917e24bb270b7dbd7dfbc21
Thomas E. Enebo <tom.enebo@gmail.com>
2010-06-04 17:58:28 -0500

    A GlobalVariable is not a LocalVariable

--- src/org/jruby/compiler/ir/operands/GlobalVariable.java
+++ src/org/jruby/compiler/ir/operands/GlobalVariable.java
@@ -1,13 +1,28 @@
 package org.jruby.compiler.ir.operands;
 import org.jruby.compiler.ir.representations.InlinerInfo;
 
-public class GlobalVariable extends LocalVariable {
-    public GlobalVariable(String n) { 
-        super(n);
+public class GlobalVariable extends Variable {
+    final public String name;
+    public GlobalVariable(String name) {
+        super();
+
+        this.name = name;
     }
 
     @Override
     public Operand cloneForInlining(InlinerInfo ii) { 
        return this;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public int compareTo(Object arg0) {
+        // ENEBO: what should compareTo when it is not comparable?
+        if (!(arg0 instanceof GlobalVariable)) return 0;
+
+        return name.compareTo(((GlobalVariable) arg0).name);
     }
 }
