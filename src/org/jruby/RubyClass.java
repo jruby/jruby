2d1eff774da4d84fac405862a2c6e9baf141a7c8
Rohit Arondekar <rohit.arondekar@gmail.com>
2010-06-30 01:38:45 -0700

    Remove unused import java.util.concurrent.atomic.AtomicInteger in RubyClass.java
    Signed-off-by: Vladimir Sizikov <vsizikov@gmail.com>

--- src/org/jruby/RubyClass.java
+++ src/org/jruby/RubyClass.java
@@ -43,7 +43,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.jruby.anno.JRubyMethod;
 import org.jruby.anno.JRubyClass;
 
