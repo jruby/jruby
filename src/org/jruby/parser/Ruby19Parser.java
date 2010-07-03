97f8fddfe655f453e9ddf1922d2d770612c630b4
David Calavera <david.calavera@gmail.com>
2010-07-03 13:09:45 +0200

    fixes JRUBY-4836: [1.9] Symbol with empty string is allowed

--- src/org/jruby/parser/Ruby19Parser.java
+++ src/org/jruby/parser/Ruby19Parser.java
@@ -1832,17 +1832,18 @@ states[439] = new ParserState() {
                      /* DStrNode: :"some text #{some expression}"*/
                      /* StrNode: :"some text"*/
                      /* EvStrNode :"#{some expression}"*/
-                     if (((Node)yyVals[-1+yyTop]) == null) {
-                       support.yyerror("empty symbol literal");
-                     }
+//                     if (((Node)yyVals[-1+yyTop]) == null) {
+//                       support.yyerror("empty symbol literal");
+//                     }
                      /* FIXME: No node here seems to be an empty string
                         instead of an error
                         if (!($$ = $2)) {
                         $$ = NEW_LIT(ID2SYM(rb_intern("")));
                         }
                      */
-
-                     if (((Node)yyVals[-1+yyTop]) instanceof DStrNode) {
+                     if (((Node)yyVals[-1+yyTop]) == null) {
+                       yyVal = new SymbolNode(((Token)yyVals[-2+yyTop]).getPosition(), "");
+                     } else if (((Node)yyVals[-1+yyTop]) instanceof DStrNode) {
                          yyVal = new DSymbolNode(((Token)yyVals[-2+yyTop]).getPosition(), ((DStrNode)yyVals[-1+yyTop]));
                      } else if (((Node)yyVals[-1+yyTop]) instanceof StrNode) {
                          yyVal = new SymbolNode(((Token)yyVals[-2+yyTop]).getPosition(), ((StrNode)yyVals[-1+yyTop]).getValue().toString().intern());
