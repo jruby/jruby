784ce03359a14761ca0a4ad17257cde293fbe42d
Tim Felgentreff <timfelgentreff@gmail.com>
2010-07-10 15:24:59 +0200

    block example

--- wmeissner-jruby-cext/example/MyTest/MyTest.rb
+++ wmeissner-jruby-cext/example/MyTest/MyTest.rb
@@ -2,10 +2,16 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
  
-include Java
-
-m = org.jruby.cext.ModuleLoader.new
-ext = File.join(File.dirname(__FILE__), "mytest")
-m.load(self, ext)
+require 'mytest.bundle'
 include MyTest
 puts test1
+block_given
+
+def foo
+  if block_given
+    rb_yield
+  end
+end
+
+foo { puts "BLOCK!" }
+
