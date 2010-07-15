02df049d3bf16714dfbe3261fffbe375d3a44912
Wayne Meissner <wmeissner@gmail.com>
2010-07-15 18:09:53 +1000

    Add some more tests to StringTest

--- wmeissner-jruby-cext/example/StringTest/string.c
+++ wmeissner-jruby-cext/example/StringTest/string.c
@@ -8,11 +8,14 @@ void Init_string(void)
 {
   CString = rb_define_class("CString", rb_cObject);
   rb_define_method(CString, "hello", method_hello, 1);
+  rb_const_set(CString, rb_intern("fubar"), Qtrue);
 }
 
 VALUE method_hello(VALUE self, VALUE str)
 {
-  printf("In C, this string has the length: %d\n", (int)rb_str_len(str));
+  printf("In C, this string has the length: %d\n", (int)RSTRING_LEN(str));
+  printf("string address=%p\n", RSTRING_PTR(str));
+  printf("fubar=%llx\n", (long long) rb_const_get(self, rb_intern("fubar")));
   return rb_str_new2(RSTRING_PTR(str));
 }
 
