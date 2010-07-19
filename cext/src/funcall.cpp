jruby/jruby

commit 24119d8e3fd75cc126bf04cf38a4dec95424c390

rb_instance_eval

cext/src/funcall.cpp
@@ -69,3 +69,22 @@ rb_call_super(int argc, const VALUE *argv)
     
     return (VALUE) result;
 }
+
+extern "C" VALUE
+rb_obj_instance_eval(int argc, VALUE* argv, VALUE self) {
+    JLocalEnv env;
+
+    jobjectArray argArray = env->NewObjectArray(argc, IRubyObject_class, NULL);
+    checkExceptions(env);
+
+    for (int i = 0; i < argc; i++) {
+        env->SetObjectArrayElement(argArray, i, valueToObject(env, argv[i]));
+        checkExceptions(env);
+    }
+
+    jlong result = env->CallStaticLongMethod(JRuby_class, JRuby_instanceEval, getRuntime(), self, argArray);
+    checkExceptions(env);
+
+    return (VALUE) result;
+}
+

cext/src/jruby-cext.cpp
@@ -54,6 +54,7 @@ namespace jruby {
     jmethodID JRuby_callMethod2;
     jmethodID JRuby_callMethod3;
     jmethodID JRuby_callSuperMethod;
+    jmethodID JRuby_instanceEval;
     jmethodID JRuby_clearErrorInfo;
     jmethodID JRuby_newString;
     jmethodID JRuby_ll2inum;
@@ -252,6 +253,8 @@ loadIds(JNIEnv* env)
     JRuby_callMethod3 = getStaticMethodID(env, JRuby_class, "callRubyMethod3",
             "(Lorg/jruby/runtime/builtin/IRubyObject;Ljava/lang/Object;Lorg/jruby/runtime/builtin/IRubyObject;Lorg/jruby/runtime/builtin/IRubyObject;Lorg/jruby/runtime/builtin/IRubyObject;)J");
     JRuby_callSuperMethod = getStaticMethodID(env, JRuby_class, "callSuperMethod",
+            "(Lorg/jruby/Ruby;Lorg/jruby/runtime/builtin/IRubyObject;[Lorg/jruby/runtime/builtin/IRubyObject;)J");
+    JRuby_instanceEval = getStaticMethodID(env, JRuby_class, "instanceEval",
             "(Lorg/jruby/Ruby;[Lorg/jruby/runtime/builtin/IRubyObject;)J");
     JRuby_clearErrorInfo = getStaticMethodID(env, JRuby_class, "clearErrorInfo",
             "(Lorg/jruby/Ruby;)V");

src/org/jruby/cext/JRuby.java
@@ -83,6 +83,12 @@ public class JRuby {
         return Handle.nativeHandle(retval);
     }
 
+    public static long instanceEval(Ruby runtime, IRubyObject self, IRubyObject[] args) {
+        ThreadContext currentContext = runtime.getCurrentContext();
+        Block block = currentContext.getFrameBlock();
+        IRubyObject retval = self.callMethod(currentContext, "instance_eval", args, block);
+        return Handle.nativeHandle(retval);
+    }
 
 
     public static long newString(Ruby runtime, byte[] bytes, boolean tainted) {

cext/src/jruby.h
@@ -93,6 +93,7 @@ namespace jruby {
     extern jmethodID JRuby_callMethod2;
     extern jmethodID JRuby_callMethod3;
     extern jmethodID JRuby_callSuperMethod;
+    extern jmethodID JRuby_instanceEval;
     extern jmethodID JRuby_clearErrorInfo;
     extern jmethodID JRuby_newString;
     extern jmethodID JRuby_newFloat;

commit ff03ae2b241d0951952d6457837235722c1a6333

Broke the library with commit 24119d8e3fd75cc126bf04cf38a4dec95424c390. Fixed

cext/src/funcall.cpp
@@ -82,7 +82,7 @@ rb_obj_instance_eval(int argc, VALUE* argv, VALUE self) {
         checkExceptions(env);
     }
 
-    jlong result = env->CallStaticLongMethod(JRuby_class, JRuby_instanceEval, getRuntime(), self, argArray);
+    jlong result = env->CallStaticLongMethod(JRuby_class, JRuby_instanceEval, self, argArray);
     checkExceptions(env);
 
     return (VALUE) result;

cext/src/jruby-cext.cpp
@@ -253,9 +253,9 @@ loadIds(JNIEnv* env)
     JRuby_callMethod3 = getStaticMethodID(env, JRuby_class, "callRubyMethod3",
             "(Lorg/jruby/runtime/builtin/IRubyObject;Ljava/lang/Object;Lorg/jruby/runtime/builtin/IRubyObject;Lorg/jruby/runtime/builtin/IRubyObject;Lorg/jruby/runtime/builtin/IRubyObject;)J");
     JRuby_callSuperMethod = getStaticMethodID(env, JRuby_class, "callSuperMethod",
-            "(Lorg/jruby/Ruby;Lorg/jruby/runtime/builtin/IRubyObject;[Lorg/jruby/runtime/builtin/IRubyObject;)J");
-    JRuby_instanceEval = getStaticMethodID(env, JRuby_class, "instanceEval",
             "(Lorg/jruby/Ruby;[Lorg/jruby/runtime/builtin/IRubyObject;)J");
+    JRuby_instanceEval = getStaticMethodID(env, JRuby_class, "instanceEval",
+            "(Lorg/jruby/runtime/builtin/IRubyObject;[Lorg/jruby/runtime/builtin/IRubyObject;)J");
     JRuby_clearErrorInfo = getStaticMethodID(env, JRuby_class, "clearErrorInfo",
             "(Lorg/jruby/Ruby;)V");
     JRuby_newString = getStaticMethodID(env, JRuby_class, "newString",

src/org/jruby/cext/JRuby.java
@@ -83,10 +83,10 @@ public class JRuby {
         return Handle.nativeHandle(retval);
     }
 
-    public static long instanceEval(Ruby runtime, IRubyObject self, IRubyObject[] args) {
-        ThreadContext currentContext = runtime.getCurrentContext();
-        Block block = currentContext.getFrameBlock();
-        IRubyObject retval = self.callMethod(currentContext, "instance_eval", args, block);
+    public static long instanceEval(IRubyObject self, IRubyObject[] args) {
+        ThreadContext ctxt = self.getRuntime().getCurrentContext();
+        Block block = ctxt.getFrameBlock();
+        IRubyObject retval = self.callMethod(ctxt, "instance_eval", args, block);
         return Handle.nativeHandle(retval);
     }
 