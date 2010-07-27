6e25f8161fc6bfb343a770bd49ef633003c8470b
Tim Felgentreff <timfelgentreff@gmail.com>
2010-07-27 23:44:48 +0200

    add todo for rb_str_resize

--- cext/src/string.cpp
+++ cext/src/string.cpp
@@ -258,7 +258,15 @@ rb_check_string_type(VALUE val)
 
 extern "C" VALUE
 rb_str_resize(VALUE str, long size) {
-    return callMethod(str, "slice", 2, INT2FIX(0), LONG2FIX(size));
+    long length = (long) jruby_str_length(str);
+    if (size != length) {
+        if (length < size) {
+            // FIXME: Need to enlarge the internal realSize of the RubyString
+        } else {
+            callMethod(str, "slice!", 2, INT2FIX(0), LONG2FIX(size));
+        }
+    }
+    return str;
 }
 
 extern "C" void
