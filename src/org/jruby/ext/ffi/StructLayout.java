6d40f9c289b87ec5c8ac0182e3902df92f8b0352
Wayne Meissner <wmeissner@gmail.com>
2010-08-15 13:23:03 +1000

    Add bench for struct field access

--- src/org/jruby/ext/ffi/StructLayout.java
+++ src/org/jruby/ext/ffi/StructLayout.java
@@ -35,7 +35,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import org.jruby.Ruby;
