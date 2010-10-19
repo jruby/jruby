b691883749923ba5d7ddfbc9a037f43fab0bca54
Hiroshi Nakamura <nahi@ruby-lang.org>
2010-10-19 22:15:55 +0900

    Added test for JRUBY-5141: Object#!= behavior in 1.9 mode.

--- test/test_digest2.rb
+++ test/test_digest2.rb
@@ -8,4 +8,12 @@ class TestDigest2 < Test::Unit::TestCase
     assert_equal(Digest::SHA256.new, Digest::SHA2.new)
   end
 
+  # JRUBY-5141: fails on 1.9 mode only
+  def test_equality
+    a = Digest::MD5.new
+    b = Digest::MD5.new
+    assert_equal(true, (a == b))
+    assert_equal(false, (a != b))
+    assert_equal(false, (a.eql?(b)))
+  end
 end
