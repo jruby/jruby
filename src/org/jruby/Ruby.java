a92e5f6da97f54b0140e83405216500bd032a750
Charles Oliver Nutter <headius@headius.com>
2010-10-22 02:01:41 -0500

    Fix for JRUBY-4966: TCPSocket raises Errno::ECONNREFUSED instead of Errno::EHOSTUNREACH
    Java was raising the expected exception (NoRouteToHostException), but our finally layout was not allowing it to bubble out (and we weren't catching it anyway). Restructured finally/catch blocks and actually catching NRTHE and re-throwing EHOSTUNREACH.

--- src/org/jruby/Ruby.java
+++ src/org/jruby/Ruby.java
@@ -3059,6 +3059,10 @@ public final class Ruby {
         return newRaiseException(getErrno().fastGetClass("EADDRINUSE"), message);
     }
 
+    public RaiseException newErrnoEHOSTUNREACHError(String message) {
+        return newRaiseException(getErrno().fastGetClass("EHOSTUNREACH"), message);
+    }
+
     public RaiseException newErrnoEINVALError() {
         return newRaiseException(getErrno().fastGetClass("EINVAL"), "Invalid file");
     }
