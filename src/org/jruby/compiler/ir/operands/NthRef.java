ab993a0c6fd1cb5ef85a614ab68758c422a77c9d
Thomas E. Enebo <tom.enebo@gmail.com>
2010-10-22 12:15:28 -0500

    Add .. nthref support

--- src/org/jruby/compiler/ir/operands/NthRef.java
+++ src/org/jruby/compiler/ir/operands/NthRef.java
@@ -1,15 +1,32 @@
 package org.jruby.compiler.ir.operands;
 
 // Represents a $1 .. $9 node in Ruby code
+
+import org.jruby.RubyRegexp;
+import org.jruby.interpreter.InterpreterContext;
+import org.jruby.runtime.ThreadContext;
+
 //
 // NOTE: This operand is only used in the initial stages of optimization
 // Further down the line, it could get converted to calls
 //
-public class NthRef extends Operand
-{
-    final public int _matchNumber; 
+public class NthRef extends Operand {
+    final public int matchNumber;
 
-    public NthRef(int n) { _matchNumber = n; }
+    public NthRef(int matchNumber) {
+        this.matchNumber = matchNumber;
+    }
 
-    public String toString() { return "$" + _matchNumber; }
+    @Override
+    public String toString() {
+        return "$" + matchNumber;
+    }
+
+    @Override
+    public Object retrieve(InterpreterContext interp) {
+        ThreadContext context = interp.getContext();
+
+        return RubyRegexp.nth_match(matchNumber,
+                context.getCurrentScope().getBackRef(context.getRuntime()));
+    }
 }
