5c31e451e0774cfbd1c20b6bafb4a26b01b974ea
Thomas E. Enebo <tom.enebo@gmail.com>
2010-10-23 12:48:14 -0500

    Add more IRScope documentation

--- src/org/jruby/compiler/ir/IRScope.java
+++ src/org/jruby/compiler/ir/IRScope.java
@@ -8,7 +8,10 @@ import org.jruby.compiler.ir.compiler_pass.CompilerPass;
 import org.jruby.compiler.ir.operands.LocalVariable;
 
 /**
- *  Script, Module, Class, Method, Closure
+ * IRScope is the interface for all lexically scoped constructs: Script, Module,
+ * Class, Method, and Closure.  It is important to understand that a single class (e.g. MyFoo)
+ * may be opened lexically in several locations in source code.  Each one of these locations will
+ * have their own instance of an IRScope.
  */
 public interface IRScope {
     /**
