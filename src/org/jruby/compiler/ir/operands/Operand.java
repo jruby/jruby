70dc9f99b46e00cb07029dd70470154ce5a53a94
Thomas E. Enebo <tom.enebo@gmail.com>
2010-10-27 14:58:25 -0500

    Style commit

--- src/org/jruby/compiler/ir/operands/Operand.java
+++ src/org/jruby/compiler/ir/operands/Operand.java
@@ -6,18 +6,20 @@ import org.jruby.compiler.ir.IRClass;
 import org.jruby.compiler.ir.Interp;
 import org.jruby.compiler.ir.representations.InlinerInfo;
 import org.jruby.interpreter.InterpreterContext;
-import org.jruby.runtime.builtin.IRubyObject;
 
-public abstract class Operand
-{
+public abstract class Operand {
     public static final Operand[] EMPTY_ARRAY = new Operand[0];
 
 // ---------- These methods below are used during compile-time optimizations ------- 
-    public boolean isConstant() { return false; }
+    public boolean isConstant() {
+        return false;
+    }
 
     // Arrays, Ranges, etc. are compound values
     // Variables, fixnums, floats, etc. are "atomic" values
-    public boolean isNonAtomicValue() { return false; }
+    public boolean isNonAtomicValue() {
+        return false;
+    }
 
     // SSS: HUH? Use better names than this .. The distinction is not very clear!
     //
@@ -34,20 +36,31 @@ public abstract class Operand
     // Note that b,c,d are all compound objects, and c has a reference to objects a and b, and d has a reference to c.
     // So, if contents of b is modified, the "simplified value"s of c and d also change!  This difference
     // is captured by these two methods.
-    public Operand getSimplifiedOperand(Map<Operand, Operand> valueMap) { return this; }
-    public Operand getValue(Map<Operand, Operand> valueMap) { return this; }
+    public Operand getSimplifiedOperand(Map<Operand, Operand> valueMap) {
+        return this;
+    }
+
+    public Operand getValue(Map<Operand, Operand> valueMap) {
+        return this;
+    }
 
     // if (getSubArray) is false, returns the 'index' element of the array, else returns the subarray starting at that element
-    public Operand fetchCompileTimeArrayElement(int index, boolean getSubArray) { return null; }
+    public Operand fetchCompileTimeArrayElement(int index, boolean getSubArray) {
+        return null;
+    }
 
     // Get the target class of this operand, if we know it!
-    public IRClass getTargetClass() { return null; }
+    public IRClass getTargetClass() {
+        return null;
+    }
 
     /** Append the list of variables used in this operand to the input list */
     public void addUsedVariables(List<Variable> l) { /* Nothing to do by default */ }
 
-	 public Operand cloneForInlining(InlinerInfo ii) { return this; }
-     
+    public Operand cloneForInlining(InlinerInfo ii) {
+        return this;
+    }
+
     @Interp
     public Object retrieve(InterpreterContext interp) {
         throw new RuntimeException(this.getClass().getSimpleName() + " should not be directly interpreted");
