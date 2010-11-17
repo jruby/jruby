e27d69ee43484c86f054bdde34caf7388345a548
Nick Sieger <nick@nicksieger.com>
2010-11-17 14:45:58 -0600

    ji/net_spec.rb: fix file: URLs to be windows friendly

--- spec/java_integration/addons/net_spec.rb
+++ spec/java_integration/addons/net_spec.rb
@@ -3,7 +3,7 @@ require 'tempfile'
 
 describe java.net.URL do
   it "should have an #open extension mechanism which yields an IO object" do
-    contents = java.net.URL.new("file://#{File.expand_path(__FILE__)}").open do |io|
+    contents = java.net.URL.new("file:/#{File.expand_path(__FILE__)}").open do |io|
       io.read
     end
     contents.should == File.read(__FILE__)
@@ -11,7 +11,7 @@ describe java.net.URL do
 
   it "can used with 'open-uri' and passed to #open and yield an IO" do
     require 'open-uri'
-    contents = open(java.net.URL.new("file://#{File.expand_path(__FILE__)}")) do |io|
+    contents = open(java.net.URL.new("file:/#{File.expand_path(__FILE__)}")) do |io|
       io.read
     end
     contents.should == File.read(__FILE__)
