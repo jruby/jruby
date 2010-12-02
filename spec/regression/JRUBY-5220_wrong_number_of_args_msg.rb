9219ba2fd36a112a51b1f9ec268b33755e69e4e1
Douglas Campos <qmx@qmx.me>
2010-12-02 15:00:07 -0200

    regression tests for JRUBY-5220
    Signed-off-by: Douglas Campos <qmx@qmx.me>
    Signed-off-by: Hiroshi Nakamura <nahi@ruby-lang.org>

---
+++ spec/regression/JRUBY-5220_wrong_number_of_args_msg.rb
@@ -0,0 +1,19 @@
+describe "Calling behavior: JRUBY-5220" do
+
+  context "on calling blocks with wrong number of arguments" do
+    it "should use the same message MRI uses" do
+      lambda do
+        lambda {||}.call(1) 
+      end.should raise_error(ArgumentError, "wrong number of arguments (1 for 0)")
+    end
+  end
+  context "on calling methods with wrong number of arguments" do
+    it "should use the same message MRI uses" do
+      lambda do
+        def foo()
+        end
+        foo(1)
+      end.should raise_error(ArgumentError, "wrong number of arguments (1 for 0)")
+    end
+  end
+end
