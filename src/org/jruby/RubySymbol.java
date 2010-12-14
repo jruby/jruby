bc6ec1766c98230b027fe14aed527c4d20613340
Thomas E. Enebo <tom.enebo@gmail.com>
2010-12-13 19:58:04 -0600

    Gotta breaks eggs if you want to make an omelette convertToString should behave sanely

--- src/org/jruby/RubySymbol.java
+++ src/org/jruby/RubySymbol.java
@@ -135,12 +135,6 @@ public class RubySymbol extends RubyObject {
     }
 
     @Override
-    public RubyString convertToString() {
-        Ruby runtime = getRuntime();
-        return runtime.is1_9() ? newShared(runtime) : super.convertToString(); 
-    }
-
-    @Override
     public String toString() {
         return symbol;
     }
