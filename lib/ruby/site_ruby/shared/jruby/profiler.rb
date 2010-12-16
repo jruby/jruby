254747d058e9e8f0c1d1652adcf3defc320d7a9d
Daniel Lucraft <dan@fluentradical.com>
2010-12-16 09:28:50 +0000

    Graph profiler: added jruby/profiler class with some non-working methods

---
+++ lib/ruby/site_ruby/shared/jruby/profiler.rb
@@ -0,0 +1,32 @@
+
+require 'java'
+
+module JRuby
+  module Profiler
+    def self.start
+      current_thread_context.start_profiling
+    end
+    
+    def self.stop
+      current_thread_context.stop_profiling
+    end
+    
+    def self.results
+      current_thread_context.profile_data
+    end
+    
+    def self.profile
+      yield
+    end
+    
+    private
+    
+    def self.runtime
+      org.jruby.Ruby.getGlobalRuntime
+    end
+    
+    def self.current_thread_context
+      runtime.get_thread_service.get_current_context
+    end
+  end
+end
