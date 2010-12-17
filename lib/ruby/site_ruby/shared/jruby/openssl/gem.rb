f36090fe31cc10d29a2d05bf2cdd21c55b706eff
Nick Sieger <nick@nicksieger.com>
2010-12-17 15:02:39 -0600

    Follow win32ole/stub, try to activate openssl.rb without loading rubygems

--- lib/ruby/site_ruby/shared/jruby/openssl/gem.rb
+++ lib/ruby/site_ruby/shared/jruby/openssl/gem.rb
@@ -1,9 +1,12 @@
-require 'rubygems'
-
 # try to activate jruby-openssl gem, and only require from the gem if it's there
+tried_gem = false
 begin
-  gem 'jruby-openssl'
   require 'openssl.rb'
 rescue LoadError
+  unless tried_gem
+    require 'rubygems'
+    gem 'jruby-openssl'
+    tried_gem = true
+    retry
+  end
 end
-
