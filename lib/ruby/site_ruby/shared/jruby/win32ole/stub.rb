933a45214ea93d4a2ae940ca2b75aa4d0a378e95
Nick Sieger <nick@nicksieger.com>
2010-12-17 14:53:14 -0600

    Allow jruby-win32ole to be used outside of rubygems

--- lib/ruby/site_ruby/shared/jruby/win32ole/stub.rb
+++ lib/ruby/site_ruby/shared/jruby/win32ole/stub.rb
@@ -1,8 +1,13 @@
-require 'rubygems'
+tried_gem = false
 begin
-  gem 'jruby-win32ole'
+  require 'jruby-win32ole'
 rescue LoadError
-  warn "!!!! Missing jruby-win32ole gem: jruby -S gem install jruby-win32ole"
-  raise $!
+  if tried_gem
+    warn "!!!! Missing jruby-win32ole gem: jruby -S gem install jruby-win32ole"
+    raise $!
+  end
+  require 'rubygems'
+  gem 'jruby-win32ole'
+  tried_gem = true
+  retry
 end
-require 'jruby-win32ole'
