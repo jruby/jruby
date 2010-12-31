3c5ad5ae7c8ef9443180bf6c467b96790acc6cfc
Charles Oliver Nutter <headius@headius.com>
2010-12-30 20:49:18 -0600

    Fix JRUBY-5244: [RubySpec][1.9] Kernel#require_relative raises a TypeError if argument does not respond to #to_str

--- src/builtin/prelude.rb
+++ src/builtin/prelude.rb
@@ -29,11 +29,16 @@ end
 module Kernel
   module_function
   def require_relative(relative_feature)
+    unless relative_feature.respond_to? :to_str
+      raise TypeError, "cannot convert #{relative_feature.class} into String"
+    end
+    relative_feature = relative_feature.to_str
+    
     c = caller.first
     e = c.rindex(/:\d+:in /)
     file = $`
     if /\A\((.*)\)/ =~ file # eval, etc.
-      raise LoadError, "require_relative is called in #{$1}"
+      raise LoadError, "cannot infer basepath"
     end
     absolute_feature = File.join(File.dirname(File.realpath(file)), relative_feature)
     require absolute_feature
