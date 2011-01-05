1fc3d9a2e3ed87860151d8714c333a7b5a69094a
Thomas E. Enebo <tom.enebo@gmail.com>
2011-01-05 17:15:03 -0600

    trivial - removal of import from earlier commit

--- src/org/jruby/lexer/yacc/InputStreamLexerSource.java
+++ src/org/jruby/lexer/yacc/InputStreamLexerSource.java
@@ -6,7 +6,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-import org.jcodings.Encoding;
 
 import org.jruby.util.ByteList;
 
