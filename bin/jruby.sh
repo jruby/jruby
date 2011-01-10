e7e8e95e9a8b9eabfe286515913fc1a2a7f935e2
Nick Sieger <nick@nicksieger.com>
2011-01-10 10:42:49 -0600

    JRUBY-5198: Add -Xa.b.c=d => -Da.b.c=d to shell-script launchers

--- bin/jruby.sh
+++ bin/jruby.sh
@@ -185,6 +185,13 @@ do
             java_args="${java_args} ${1:2}"
         fi
         ;;
+     # Match -Xa.b.c=d to translate to -Da.b.c=d as a java option
+     -X*)
+	val=${1:2}
+	if expr "$val" : '.*[.]' > /dev/null; then
+	  java_args="${java_args} -D${val}"
+	fi
+	;;
      # Match switches that take an argument
      -C|-e|-I|-S) ruby_args="${ruby_args} $1 $2"; shift ;;
      # Match same switches with argument stuck together
