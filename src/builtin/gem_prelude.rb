12f69173462644c1a4e223dfb379293905bb8d05
Charles Oliver Nutter <headius@headius.com>
2011-01-13 14:09:40 -0600

    Fix JRUBY-5333: java -jar lib/jruby.jar in 1.9 mode raises LoadError for Rubygems
    * Gracefully fail to load RubyGems if not available.
    * Warn in verbose mode that gems have been disabled.

--- src/builtin/gem_prelude.rb
+++ src/builtin/gem_prelude.rb
@@ -8,7 +8,13 @@
 
 # Note: We have disabled all of gem_prelude and just load rubygems here, since the
 # logic below is terribly broken.
-require 'rubygems'
+
+begin
+  require 'rubygems'
+rescue LoadError
+  # For JRUBY-5333, gracefully fail to load, since stdlib may not be available
+  warn 'rubygems.rb not found; disabling gems' if $VERBOSE
+end
 
 #if defined?(Gem) then
 #
