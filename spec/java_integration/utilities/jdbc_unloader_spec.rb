7b615d2f258cc7e9638ca4618cd89db77bf11904
Nick Sieger <nick@nicksieger.com>
2011-01-20 13:25:18 -0600

    Make jdbc_unloader_spec more sensitive to possibility of existing drivers

--- spec/java_integration/utilities/jdbc_unloader_spec.rb
+++ spec/java_integration/utilities/jdbc_unloader_spec.rb
@@ -4,26 +4,25 @@ require 'jruby'
 require File.expand_path('../../fixtures/tinySQL-2.26.jar', __FILE__)
 
 describe "JDBCDriverUnloader" do
+  def drivers
+    iterator = JRuby.runtime.getJRubyClassLoader.getJDBCDriverUnloader.iterator
+    drivers = []
+    while iterator.has_next
+      drivers << iterator.next
+    end
+    drivers
+  end
+
   before :each do
+    @driver_count = drivers.size
     # loading the driver causes it to be registered
     Java::com.sqlmagic.tinysql.textFileDriver
   end
 
   it "unregisters the drivers" do
-    iterator = JRuby.runtime.getJRubyClassLoader.getJDBCDriverUnloader.iterator
-    drivers = []
-    while iterator.has_next
-      drivers << iterator.next
-    end
-    drivers.size.should == 1
-    drivers[0].should be_kind_of(Java::com.sqlmagic.tinysql.textFileDriver)
-
+    drivers.size.should == @driver_count + 1
+    drivers.detect {|d| d.kind_of?(Java::com.sqlmagic.tinysql.textFileDriver) }.should be_true
     JRuby.runtime.getJRubyClassLoader.getJDBCDriverUnloader.run
-    iterator = JRuby.runtime.getJRubyClassLoader.getJDBCDriverUnloader.iterator
-    drivers = []
-    while iterator.has_next
-      drivers << iterator.next
-    end
     drivers.should be_empty
   end
 end
