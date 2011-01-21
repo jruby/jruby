3280a72a0a30144ef9fd63a184d30be778313617
Nick Sieger <nick@nicksieger.com>
2011-01-21 17:50:19 -0600

    Change jdbc_unloader_spec to also not pollute main classloader

--- spec/java_integration/utilities/jdbc_unloader_spec.rb
+++ spec/java_integration/utilities/jdbc_unloader_spec.rb
@@ -1,11 +1,15 @@
 require File.dirname(__FILE__) + "/../spec_helper"
-require 'jruby'
-
-require File.expand_path('../../fixtures/tinySQL-2.26.jar', __FILE__)
 
 describe "JDBCDriverUnloader" do
+  let(:driver_jar) { File.expand_path('../../fixtures/tinySQL-2.26.jar', __FILE__) }
+  let(:container) do
+    org.jruby.embed.ScriptingContainer.new(org.jruby.embed.LocalContextScope::SINGLETHREAD).tap do |c|
+      c.runScriptlet("require 'jruby'; require '#{driver_jar}'")
+    end
+  end
+
   def drivers
-    iterator = JRuby.runtime.getJRubyClassLoader.getJDBCDriverUnloader.iterator
+    iterator = container.runScriptlet('JRuby.runtime.getJRubyClassLoader.getJDBCDriverUnloader.iterator')
     drivers = []
     while iterator.has_next
       drivers << iterator.next
@@ -16,13 +20,13 @@ describe "JDBCDriverUnloader" do
   before :each do
     @driver_count = drivers.size
     # loading the driver causes it to be registered
-    Java::com.sqlmagic.tinysql.textFileDriver
+    container.runScriptlet('Java::com.sqlmagic.tinysql.textFileDriver')
   end
 
   it "unregisters the drivers" do
     drivers.size.should == @driver_count + 1
-    drivers.detect {|d| d.kind_of?(Java::com.sqlmagic.tinysql.textFileDriver) }.should be_true
-    JRuby.runtime.getJRubyClassLoader.getJDBCDriverUnloader.run
+    drivers.detect {|d| d.java_class.name == 'com.sqlmagic.tinysql.textFileDriver' }.should be_true
+    container.runScriptlet('JRuby.runtime.getJRubyClassLoader.getJDBCDriverUnloader.run')
     drivers.should be_empty
   end
 end
