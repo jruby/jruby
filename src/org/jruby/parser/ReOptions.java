4c72a2497ea539d89ba16b4d14cbca2868793a87
Thomas E. Enebo <tom.enebo@gmail.com>
2011-02-03 10:01:22 -0600

    Whoops 0x80 is 128 :)

--- src/org/jruby/parser/ReOptions.java
+++ src/org/jruby/parser/ReOptions.java
@@ -39,8 +39,9 @@ public interface ReOptions {
     int RE_OPTION_LONGEST      = 16;
     int RE_MAY_IGNORECASE      = 32;
     int RE_UNICODE             = 64;
-    int RE_LITERAL             = 64; // reusing regexp_options since we used 
-                                     // and we won't escape regexp_options.
-    int RE_DEFAULT = 128; // Only for RubyRegexp. for kcode default
-    int RE_OPTION_ONCE         = 0x80; // odd...but it is odd in ruby too.
+    int RE_OPTION_ONCE         = 128; // odd...but it is odd in ruby too.    
+    int RE_LITERAL             = 256; // reusing regexp_options since we used 
+                                      // and we won't escape regexp_options.
+    int RE_DEFAULT = 512; // Only for RubyRegexp. for kcode default
+
 }
