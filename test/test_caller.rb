254d3d0cde5b27eba920ff26b08b5adf68f9ab5e
Charles Oliver Nutter <headius@headius.com>
2011-02-12 05:22:42 -0600

    Test for JRUBY-5477: Caller stacks now include AbstractScript.java

--- test/test_caller.rb
+++ test/test_caller.rb
@@ -11,4 +11,16 @@ class TestCaller < Test::Unit::TestCase
     # simple test, make sure the trace is more than one entry
     assert(trace.length > 1)
   end
+
+  def test_jitted_caller_excludes_abstractscript
+    eval 'def jitted_method(*args); caller(0); end'
+
+    # call a number of times with args and block to ensure it
+    # JITs and passes through AbstractScript.__file__
+    100.times { jitted_method(1,2,3) {} }
+
+    # check caller to ensure no AbstractScript.java shows up
+    caller = jitted_method(1,2,3) {}
+    assert caller.to_s !~ /AbstractScript/
+  end
 end
