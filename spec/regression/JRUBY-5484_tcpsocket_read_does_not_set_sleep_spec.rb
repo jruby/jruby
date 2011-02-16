48fa2beb839c3e5e74c05fea6c8cb94c5e4555c9
Charles Oliver Nutter <headius@headius.com>
2011-02-16 01:28:08 -0600

    Regression spec for JRUBY-5484: Thread#status isn't set to "sleep" when blocking on a TCPSocket#read

---
+++ spec/regression/JRUBY-5484_tcpsocket_read_does_not_set_sleep_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require 'socket'
+
+describe "TCPSocket method" do
+  before :each do
+    @serv_sock = TCPServer.new("localhost", 0)
+    @serv_thread = Thread.new{ @serv_sock.accept }
+    @port = @serv_sock.addr[1]
+  end
+
+  def check_status(&blk)
+    @client_sock = TCPSocket.new("localhost", @port)
+    @client_thread = Thread.new &blk
+    sleep(0.1) # potentially too short?
+    @client_thread.status.should == 'sleep'
+  end
+
+  it "read sets thread status to sleep" do
+    check_status{ @client_sock.read }
+  end
+
+  it "readline sets thread status to sleep" do
+    check_status{ @client_sock.readline }
+  end
+end
