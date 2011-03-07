ec6c1e656005089d6c58ad60d72f3a702b182066
Thomas E. Enebo <tom.enebo@gmail.com>
2011-03-07 12:18:03 -0600

    Allow Kernel.open to allow hash as second arg

--- src/org/jruby/RubyFile.java
+++ src/org/jruby/RubyFile.java
@@ -469,11 +469,16 @@ public class RubyFile extends RubyIO implements EncodingCapable {
 
         try {
             if (args.length > 1) {
-                modes = parseModes19(context, args[1]);
-                if (args[1] instanceof RubyFixnum) {
-                    perm = getFilePermissions(args);
+                if (args[1] instanceof RubyHash) {
+                    modes = parseOptions(context, args[1], modes);
                 } else {
-                    modeString = args[1].convertToString().toString();
+                    modes = parseModes19(context, args[1]);
+                    
+                    if (args[1] instanceof RubyFixnum) {
+                        perm = getFilePermissions(args);
+                    } else {
+                        modeString = args[1].convertToString().toString();
+                    }
                 }
             } else {
                 modes = parseModes19(context, RubyString.newString(runtime, modeString));
