f46bb46d02c3ecdb26c1509f688d7a41c843abc0
Thomas E. Enebo <tom.enebo@gmail.com>
2011-03-25 14:31:05 -0500

    Add EQQInstr impl

--- src/org/jruby/compiler/ir/instructions/EQQInstr.java
+++ src/org/jruby/compiler/ir/instructions/EQQInstr.java
@@ -1,9 +1,12 @@
 package org.jruby.compiler.ir.instructions;
 
 import org.jruby.compiler.ir.Operation;
+import org.jruby.compiler.ir.operands.Label;
 import org.jruby.compiler.ir.operands.Operand;
 import org.jruby.compiler.ir.operands.Variable;
 import org.jruby.compiler.ir.representations.InlinerInfo;
+import org.jruby.interpreter.InterpreterContext;
+import org.jruby.runtime.builtin.IRubyObject;
 
 // If v2 is an array, compare v1 with every element of v2 and stop on first match!
 public class EQQInstr extends TwoOperandInstr {
@@ -14,4 +17,15 @@ public class EQQInstr extends TwoOperandInstr {
     public Instr cloneForInlining(InlinerInfo ii) {
         return new EQQInstr(ii.getRenamedVariable(result), operand1.cloneForInlining(ii), operand2.cloneForInlining(ii));
     }
+
+    @Override
+    public Label interpret(InterpreterContext interp, IRubyObject self) {
+        IRubyObject receiver = (IRubyObject) getOperand1().retrieve(interp);
+        IRubyObject value = (IRubyObject) getOperand2().retrieve(interp);
+        getResult().store(interp, receiver.callMethod(interp.getContext(), "===", value));
+        
+        return null;
+    }
+
+
 }
