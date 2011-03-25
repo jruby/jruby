8da866b68b54376fece0075ad037e490eba45d1b
Thomas E. Enebo <tom.enebo@gmail.com>
2011-03-25 16:12:13 -0500

    Implement backtick operand

--- src/org/jruby/compiler/ir/operands/BacktickString.java
+++ src/org/jruby/compiler/ir/operands/BacktickString.java
@@ -5,35 +5,47 @@ import org.jruby.compiler.ir.representations.InlinerInfo;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
+import org.jruby.RubyString;
+import org.jruby.interpreter.InterpreterContext;
+import org.jruby.runtime.builtin.IRubyObject;
 
 // This represents a backtick string in Ruby
 // Ex: `ls .`; `cp #{src} #{dst}`
 //
 // NOTE: This operand is only used in the initial stages of optimization.
 // Further down the line, this string operand could get converted to calls
-public class BacktickString extends Operand
-{
-    final public List<Operand> _pieces;
+public class BacktickString extends Operand {
+    final public List<Operand> pieces;
 
-    public BacktickString(Operand val) { _pieces = new ArrayList<Operand>(); _pieces.add(val); }
-    public BacktickString(List<Operand> pieces) { _pieces = pieces; }
-
-    public boolean isConstant() {
-       for (Operand o: _pieces)
-          if (!o.isConstant())
-             return false;
-
-       return true;
+    public BacktickString(Operand val) {
+        pieces = new ArrayList<Operand>();
+        pieces.add(val);
     }
 
-    public boolean isNonAtomicValue() { return true; }
+    public BacktickString(List<Operand> pieces) {
+        this.pieces = pieces;
+    }
 
-    public Operand getSimplifiedOperand(Map<Operand, Operand> valueMap)
-    {
+    @Override
+    public boolean isConstant() {
+        for (Operand o : pieces) {
+            if (!o.isConstant()) return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean isNonAtomicValue() {
+        return true;
+    }
+
+    @Override
+    public Operand getSimplifiedOperand(Map<Operand, Operand> valueMap) {
         int i = 0;
-        for (Operand p: _pieces) {
-           _pieces.set(i, p.getSimplifiedOperand(valueMap));
-           i++;
+        for (Operand p : pieces) {
+            pieces.set(i, p.getSimplifiedOperand(valueMap));
+            i++;
         }
 
         return this;
@@ -41,22 +53,32 @@ public class BacktickString extends Operand
 
     /** Append the list of variables used in this operand to the input list */
     @Override
-    public void addUsedVariables(List<Variable> l)
-    {
-        for (Operand o: _pieces)
+    public void addUsedVariables(List<Variable> l) {
+        for (Operand o : pieces) {
             o.addUsedVariables(l);
+        }
     }
 
+    @Override
     public Operand cloneForInlining(InlinerInfo ii) {
-        if (isConstant()) {
-            return this;
+        if (isConstant()) return this;
+
+        List<Operand> newPieces = new ArrayList<Operand>();
+        for (Operand p : pieces) {
+            newPieces.add(p.cloneForInlining(ii));
         }
-        else {
-            List<Operand> newPieces = new ArrayList<Operand>();
-            for (Operand p: _pieces) {
-                newPieces.add(p.cloneForInlining(ii));
-            }
-            return new BacktickString(newPieces);
+        
+        return new BacktickString(newPieces);
+    }
+
+    @Override
+    public Object retrieve(InterpreterContext interp) {
+        RubyString newString = interp.getRuntime().newString();
+
+        for (Operand p: pieces) {
+            newString.append((IRubyObject) p.retrieve(interp));
         }
+        
+        return ((IRubyObject) interp.getSelf()).callMethod(interp.getContext(), "`", newString);
     }
 }
