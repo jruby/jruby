ee2d5e9611866183716d1bd4b42ac6bda3dcc187
Wayne Meissner <wmeissner@gmail.com>
2011-03-26 22:41:27 +1000

    Add Struct#initialize_copy

--- src/org/jruby/ext/ffi/Struct.java
+++ src/org/jruby/ext/ffi/Struct.java
@@ -8,6 +8,7 @@ import org.jruby.RubyModule;
 import org.jruby.RubyObject;
 import org.jruby.anno.JRubyClass;
 import org.jruby.anno.JRubyMethod;
+import org.jruby.embed.util.SystemPropertyCatcher;
 import org.jruby.exceptions.RaiseException;
 import org.jruby.runtime.ObjectAllocator;
 import org.jruby.runtime.ThreadContext;
@@ -139,6 +140,21 @@ public class Struct extends RubyObject implements StructLayout.Storage {
         
         return this;
     }
+
+    @JRubyMethod(name = "initialize_copy")
+    public IRubyObject initialize_copy(ThreadContext context, IRubyObject other) {
+        if (other == this) {
+            return this;
+        }
+        if (!(other instanceof Struct)) {
+            throw context.getRuntime().newTypeError("not an instance of Struct");
+        }
+        Struct orig = (Struct) other;
+        memory = (AbstractMemory) orig.getMemory().slice(context.getRuntime(), 0, layout.getSize()).dup();
+        System.arraycopy(orig.referenceCache, 0, referenceCache, 0, referenceCache.length);
+        return this;
+    }
+
 
     private static final Struct allocateStruct(ThreadContext context, IRubyObject klass, int flags) {
         Ruby runtime = context.getRuntime();
