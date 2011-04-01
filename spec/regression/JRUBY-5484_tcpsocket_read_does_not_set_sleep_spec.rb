10b22d202724b088ee191242531032b83ec9ad33
Charles Oliver Nutter <headius@headius.com>
2011-04-01 02:26:59 -0500

    Clean up regression spec for JRUBY-5484 to be a bit more robust.

--- spec/regression/JRUBY-5484_tcpsocket_read_does_not_set_sleep_spec.rb
+++ spec/regression/JRUBY-5484_tcpsocket_read_does_not_set_sleep_spec.rb
@@ -5,6 +5,7 @@ describe "TCPSocket method" do
   before :each do
     @serv_sock = TCPServer.new("localhost", 0)
     @serv_thread = Thread.new{ @serv_sock.accept }
+    Thread.pass until @serv_thread.status == 'sleep'
     @port = @serv_sock.addr[1]
   end
 
@@ -13,13 +14,14 @@ describe "TCPSocket method" do
     @client_thread = Thread.new &blk
     sleep(0.1) # potentially too short?
     @client_thread.status.should == 'sleep'
+    @client_thread.kill
   end
 
   it "read sets thread status to sleep" do
-    check_status{ @client_sock.read }
+    check_status { @client_sock.read }
   end
 
   it "readline sets thread status to sleep" do
-    check_status{ @client_sock.readline }
+    check_status { @client_sock.readline }
   end
 end
