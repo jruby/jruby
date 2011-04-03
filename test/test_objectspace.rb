80075e78c98ff8876bf91c22b1e6cdad945b1d98
Charles Oliver Nutter <headius@headius.com>
2011-04-02 21:20:39 -0500

    Move ObjectSpace-related tests to full test run and disable ObjectSpace for day-to-day run.

--- test/test_objectspace.rb
+++ test/test_objectspace.rb
@@ -17,37 +17,37 @@ class TestObjectSpace < Test::Unit::TestCase
     o2 = "ho"
     id1 = o1.object_id
     id2 = o2.object_id
-    test_equal(o1, ObjectSpace._id2ref(id1))
-    test_equal(o2, ObjectSpace._id2ref(id2))
+    assert_equal(o1, ObjectSpace._id2ref(id1))
+    assert_equal(o2, ObjectSpace._id2ref(id2))
 
     # Fixnums
     o1 = 17
     o2 = 100001
     id1 = o1.object_id
     id2 = o2.object_id
-    test_equal(o1, ObjectSpace._id2ref(id1))
-    test_equal(o2, ObjectSpace._id2ref(id2))
+    assert_equal(o1, ObjectSpace._id2ref(id1))
+    assert_equal(o2, ObjectSpace._id2ref(id2))
 
-    test_equal(1, 0.object_id)
-    test_equal(3, 1.object_id)
-    test_equal(201, 100.object_id)
-    test_equal(-1, -1.object_id)
-    test_equal(-19, -10.object_id)
+    assert_equal(1, 0.object_id)
+    assert_equal(3, 1.object_id)
+    assert_equal(201, 100.object_id)
+    assert_equal(-1, -1.object_id)
+    assert_equal(-19, -10.object_id)
 
-    test_equal(0, false.object_id)
-    test_equal(2, true.object_id)
-    test_equal(4, nil.object_id)
+    assert_equal(0, false.object_id)
+    assert_equal(2, true.object_id)
+    assert_equal(4, nil.object_id)
 
-    test_equal(false, ObjectSpace._id2ref(0))
-    test_equal(true, ObjectSpace._id2ref(2))
-    test_equal(nil, ObjectSpace._id2ref(4))
+    assert_equal(false, ObjectSpace._id2ref(0))
+    assert_equal(true, ObjectSpace._id2ref(2))
+    assert_equal(nil, ObjectSpace._id2ref(4))
   end
   
   def test_jruby_objectspace
     JRuby.objectspace = false
-    test_equal(false, JRuby.objectspace)
+    assert_equal(false, JRuby.objectspace)
     JRuby.objectspace = true
-    test_equal(true, JRuby.objectspace)
+    assert_equal(true, JRuby.objectspace)
   end
   
   # JRUBY-4330
@@ -56,7 +56,7 @@ class TestObjectSpace < Test::Unit::TestCase
     id_with_objectspace = obj.object_id
     JRuby.objectspace = false
     id_without_objectspace = obj.object_id
-    test_equal(id_with_objectspace, id_without_objectspace)
+    assert_equal(id_with_objectspace, id_without_objectspace)
   end
 
   def finalizer(results)
@@ -85,7 +85,7 @@ class TestObjectSpace < Test::Unit::TestCase
 
       t = Time.now
       (JRuby.gc; sleep 0.1) until (Time.now - t > 5) || results.length > 0
-      test_equal ["finalizing #{obj1_id}"], results
+      assert_equal ["finalizing #{obj1_id}"], results
     end
   end
 end
