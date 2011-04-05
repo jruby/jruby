ee0830332ca1f3420f1e277af25caa70166b5e9e
Aman Gupta <aman@tmm1.net>
2011-04-04 20:12:45 -0700

    add rb_range_beg_len (copied from rbx)
    Signed-off-by: Tim Felgentreff <timfelgentreff@gmail.com>

--- cext/src/range.cpp
+++ cext/src/range.cpp
@@ -34,3 +34,38 @@ rb_range_new(VALUE beg, VALUE end, int exclude_end)
 {
     return callMethod(rb_cRange, "new", 3, beg, end, exclude_end ? Qtrue : Qfalse);
 }
+
+extern "C" VALUE
+rb_range_beg_len(VALUE range, long* begp, long* lenp, long len, int err)
+{
+    long beg, end, b, e;
+
+    if(!rb_obj_is_kind_of(range, rb_cRange)) return Qfalse;
+
+    beg = b = NUM2LONG(rb_funcall(range, rb_intern("begin"), 0));
+    end = e = NUM2LONG(rb_funcall(range, rb_intern("end"), 0));
+
+    if(beg < 0) {
+        beg += len;
+        if(beg < 0) goto out_of_range;
+    }
+    if(err == 0 || err == 2) {
+        if(beg > len) goto out_of_range;
+        if(end > len) end = len;
+    }
+    if(end < 0) end += len;
+    if(!RTEST(rb_funcall(range, rb_intern("exclude_end?"), 0))) end++;    /* include end point */
+    len = end - beg;
+    if(len < 0) len = 0;
+
+    *begp = beg;
+    *lenp = len;
+    return Qtrue;
+
+out_of_range:
+    if(err) {
+        rb_raise(rb_eRangeError, "%ld..%ld out of range",
+                 b, e);
+    }
+    return Qnil;
+}
