0bf233162c1220c93ae8cf90606f8a50cf570e7d
Thomas E. Enebo <tom.enebo@gmail.com>
2011-04-07 11:23:55 -0500

    Native exec turned on (only windows currently supported in jnr-posix). Fix some permissions

--- src/org/jruby/RubyKernel.java
+++ src/org/jruby/RubyKernel.java
@@ -1717,15 +1717,28 @@ public class RubyKernel {
         }
         
         int resultCode;
+        boolean nativeFailed = false;
         try {
-            // TODO: exec should replace the current process.
-            // This could be possible with JNA. 
-            resultCode = ShellLauncher.execAndWait(runtime, args);
+            try {
+                String[] argv = new String[args.length];
+                for (int i = 0; i < args.length; i++) {
+                    argv[i] = args[i].asJavaString();
+                }
+                resultCode = runtime.getPosix().exec(null, argv);
+                // Only here because native exec could not exec (always -1)
+                nativeFailed = true;
+            } catch (RaiseException e) {  // Not implemented error
+                // Fall back onto our existing code if native not available
+                // FIXME: Make jnr-posix Pure-Java backend do this as well
+                resultCode = ShellLauncher.execAndWait(runtime, args);
+            }
         } catch (RaiseException e) {
             throw e; // no need to wrap this exception
         } catch (Exception e) {
             throw runtime.newErrnoENOENTError("cannot execute");
         }
+        
+        if (nativeFailed) throw runtime.newErrnoENOENTError("cannot execute");
         
         exit(runtime, new IRubyObject[] {runtime.newFixnum(resultCode)}, true);
 
