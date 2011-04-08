ad3a2e02b59155c529eb936127e04ddde08d2d53
Thomas E. Enebo <tom.enebo@gmail.com>
2011-04-08 16:30:09 -0500

    Remove some dead code

--- src/org/jruby/compiler/ir/IRMetaClass.java
+++ src/org/jruby/compiler/ir/IRMetaClass.java
@@ -1,27 +1,17 @@
 package org.jruby.compiler.ir;
 
 import org.jruby.compiler.ir.operands.Operand;
-import org.jruby.compiler.ir.operands.LocalVariable;
 import org.jruby.compiler.ir.operands.MetaObject;
 import org.jruby.parser.StaticScope;
 
 public class IRMetaClass extends IRClass {
     static IRMetaClass CLASS_METACLASS;    // SSS FIXME: Needs initialization
 
-    private Operand methodClassedObject;   // Object (classes too, obviously) that has been metaclassed
-
     public IRMetaClass(IRScope s, Operand receiver, StaticScope staticScope) {
         // Super class is always <Class:Class>
         // This metaclass is always top-level, hence the null container.
         // SSS FIXME: class name -- can be unknown at compile time ... How do we handle this? 
         super(s, null, MetaObject.create(CLASS_METACLASS), "<FIXME>", staticScope);
-
-        if ((receiver instanceof LocalVariable) && (((LocalVariable)receiver).isSelf())) {
-            IRMethod classRootMethod = (IRMethod)s;
-            methodClassedObject = (MetaObject)classRootMethod.getContainer();
-        } else {
-            methodClassedObject = receiver;
-        }
     }
 
     @Override
