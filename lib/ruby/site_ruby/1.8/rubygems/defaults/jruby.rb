5fdc758dcba5c2c1770b0871ad9c692391fc5262
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-05-10 23:28:25 +0900

    Avoid circular require.

--- lib/ruby/site_ruby/1.8/rubygems/defaults/jruby.rb
+++ lib/ruby/site_ruby/1.8/rubygems/defaults/jruby.rb
@@ -1,4 +1,3 @@
-require 'rubygems/config_file'
 require 'rubygems/maven_gemify' # Maven support
 require 'rbconfig'
 
--- lib/ruby/site_ruby/1.8/rubygems/maven_gemify.rb
+++ lib/ruby/site_ruby/1.8/rubygems/maven_gemify.rb
@@ -1,6 +1,4 @@
 require 'uri'
-require 'rubygems/spec_fetcher'
-require 'rubygems/remote_fetcher'
 
 module Gem
   module MavenUtils
