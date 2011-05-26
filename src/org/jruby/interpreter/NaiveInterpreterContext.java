6475731d8ad455c831456ce3b951beaa25188afa
Subramanya Sastry <sss.lists@gmail.com>
2011-05-26 14:55:23 -0500

    Fixed problem with missed setModule call for certain closure invocations

--- src/org/jruby/interpreter/NaiveInterpreterContext.java
+++ src/org/jruby/interpreter/NaiveInterpreterContext.java
@@ -44,6 +44,7 @@ public class NaiveInterpreterContext implements InterpreterContext {
 	 // - the class in which the closure is lexically defined in if we are executing a closure
     public NaiveInterpreterContext(ThreadContext context, RubyModule currentModule, IRubyObject self, int localVariablesSize, int temporaryVariablesSize, int renamedVariablesSize, IRubyObject[] parameters, Block block) {
         context.preMethodFrameOnly(currentModule, null, self, block);
+		  context.getCurrentScope().getStaticScope().setModule(currentModule);
         this.frame = context.getCurrentFrame();
 
         this.context = context;
