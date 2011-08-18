4589fb027e216ecbecb4f109bc2a2be676a1c8d9
Wayne Meissner <wmeissner@gmail.com>
2011-08-18 20:15:23 +1000

    Remove unused fast-long result/parameter converters

--- src/org/jruby/ext/ffi/jffi/LongParameterConverter.java
+++
@@ -1,9 +0,0 @@
-
-package org.jruby.ext.ffi.jffi;
-
-import org.jruby.runtime.ThreadContext;
-import org.jruby.runtime.builtin.IRubyObject;
-
-interface LongParameterConverter {
-    long longValue(ThreadContext context, IRubyObject value);
-}
--- src/org/jruby/ext/ffi/jffi/LongResultConverter.java
+++
@@ -1,9 +0,0 @@
-
-package org.jruby.ext.ffi.jffi;
-
-import org.jruby.runtime.ThreadContext;
-import org.jruby.runtime.builtin.IRubyObject;
-
-public interface LongResultConverter {
-    IRubyObject fromNative(ThreadContext context, long value);
-}
