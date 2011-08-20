6ed406f07edc1bcb5346241b75b761aaa8973802
Hiro Asari <asari.ruby@gmail.com>
2011-08-20 00:22:57 -0400

    Test for JRUBY-5922

---
+++ test/test_runaway_magic_comment_1_9.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+
+class MagicCommentTest19 < Test::Unit::TestCase
+
+  # JRUBY-5922
+  def test_magic_comment_parse
+    path = File.join(File.dirname(__FILE__), 'runaway_magic_comment.rb')
+    assert(system "ruby --1.9 #{path}")
+  end
+end
