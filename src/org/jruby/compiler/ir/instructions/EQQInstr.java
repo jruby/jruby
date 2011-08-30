76d7a43f79e7ce34f0598c63c4777f45e922f385
Subramanya Sastry <sss.lists@gmail.com>
2011-08-30 00:30:50 -0500

    [IR] Bug fix interpreting EQQInstr

--- src/org/jruby/compiler/ir/instructions/EQQInstr.java
+++ src/org/jruby/compiler/ir/instructions/EQQInstr.java
@@ -8,6 +8,7 @@ import org.jruby.compiler.ir.representations.InlinerInfo;
 import org.jruby.interpreter.InterpreterContext;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.builtin.IRubyObject;
+import org.jruby.RubyArray;
 
 // If v2 is an array, compare v1 with every element of v2 and stop on first match!
 public class EQQInstr extends TwoOperandInstr {
@@ -23,7 +24,21 @@ public class EQQInstr extends TwoOperandInstr {
     public Label interpret(InterpreterContext interp, ThreadContext context, IRubyObject self) {
         IRubyObject receiver = (IRubyObject) getOperand1().retrieve(interp, context, self);
         IRubyObject value = (IRubyObject) getOperand2().retrieve(interp, context, self);
-        getResult().store(interp, context, self, receiver.callMethod(context, "===", value));
+
+        if (receiver instanceof RubyArray) {
+            RubyArray testVals = (RubyArray)receiver;
+            for (int i = 0, n = testVals.getLength(); i < n; i++) {
+                IRubyObject eqqVal = testVals.eltInternal(i).callMethod(context, "===", value);
+                if (eqqVal.isTrue()) {
+                    getResult().store(interp, context, self, eqqVal);
+                    return null;
+                }
+            }
+            getResult().store(interp, context, self, context.getRuntime().newBoolean(false));
+        }
+        else {
+            getResult().store(interp, context, self, receiver.callMethod(context, "===", value));
+        }
         
         return null;
     }
