938edc420612430e31e3efc861d495de6d13ce1b
Charles Oliver Nutter <headius@headius.com>
2011-08-31 18:45:08 -0500

    Minor hack to eliminate rspec 2.6ish warnings about deprecated features.

--- spec/java_integration/spec_helper.rb
+++ spec/java_integration/spec_helper.rb
@@ -2,9 +2,12 @@ require 'java'
 $CLASSPATH << File.expand_path('../../../build/classes/test', __FILE__)
 require 'rspec'
 
-RSpec.configure do |config|
-  require File.expand_path('../../../test/test_helper', __FILE__)
-  config.include TestHelper
+unless $is_rspec_reconfigured
+  RSpec.configure do |config|
+    require File.expand_path('../../../test/test_helper', __FILE__)
+    config.include TestHelper
+  end
+  $is_rspec_reconfigured = true
 end
 
 # Works like 'should include('str1', 'str2') but for arrays of
