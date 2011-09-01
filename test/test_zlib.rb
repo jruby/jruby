f8df1ab20e4e77452043da7aec0964d24d39e890
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-09-01 19:44:25 +0900

    Raise DataError properly from Zlib::Inflate#inflate
    For broken gzip trailer, we only raise from Zlib::Inflate#finish since
    we only parsed gzip trailer from the finish method. To follow zlib.c's
    behavior, we should parse the gzip trailer for Inflate#<< and
    Inflate#inflate as well if the gzip body is finished.
    
    Reported by Atsuhiko Yamanaka at
    https://twitter.com/#!/ymnk/status/107118737743814658 Thanks!

--- test/test_zlib.rb
+++ test/test_zlib.rb
@@ -587,6 +587,14 @@ class TestZlibInflateGzip < Test::Unit::TestCase
       z.finish
     end
   end
+
+  def test_wrong_length
+    gzip = "\x1f\x8b\x08\x00\x1a\x96\xe0\x4c\x00\x03\xcb\x48\xcd\xc9\xc9\x07\x00\x86\xa6\x10\x36\x04\x00\x00\x00"
+    z = Zlib::Inflate.new(Zlib::MAX_WBITS + 16)
+    assert_raise(Zlib::DataError) do
+      z.inflate(gzip)
+    end
+  end
 end
 
 # Test for MAX_WBITS + 32
@@ -668,6 +676,14 @@ class TestZlibInflateAuto < Test::Unit::TestCase
     assert_equal("hello", z.inflate(gzip))
     assert_raise(Zlib::BufError) do
       z.finish
+    end
+  end
+
+  def test_wrong_length
+    gzip = "\x1f\x8b\x08\x00\x1a\x96\xe0\x4c\x00\x03\xcb\x48\xcd\xc9\xc9\x07\x00\x86\xa6\x10\x36\x04\x00\x00\x00"
+    z = Zlib::Inflate.new(Zlib::MAX_WBITS + 32)
+    assert_raise(Zlib::DataError) do
+      z.inflate(gzip)
     end
   end
 end
