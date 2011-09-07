9b5e375bf0486409be2b82601965a53c63cd88dc
Hiroshi Nakamura <nahi@ruby-lang.org>
2011-09-07 15:01:55 +0900

    Move Zlib encoding related implementations to RubyIO.java
    I expected there're common impls for encoding parameter parsing, but I
    could not refactor now. Should be done in the future.

--- src/org/jruby/RubyIO.java
+++ src/org/jruby/RubyIO.java
@@ -4025,6 +4025,127 @@ public class RubyIO extends RubyObject {
         return modes;
     }
 
+    public static class EncodingOption {
+        private Encoding externalEncoding;
+        private Encoding internalEncoding;
+        private boolean bom;
+
+        public EncodingOption(Encoding externalEncoding, Encoding internalEncoding, boolean bom) {
+            this.externalEncoding = externalEncoding;
+            this.internalEncoding = internalEncoding;
+            this.bom = bom;
+        }
+
+        public Encoding getExternalEncoding() {
+            return externalEncoding;
+        }
+
+        public Encoding getInternalEncoding() {
+            return internalEncoding;
+        }
+
+        public boolean hasBom() {
+            return bom;
+        }
+    }
+
+    // c: rb_io_extract_encoding_option
+    public static EncodingOption extractEncodingOptions(IRubyObject options) {
+        RubyHash opts = (RubyHash) options;
+        if (opts.isNil()) {
+            return null;
+        }
+        Ruby runtime = options.getRuntime();
+        IRubyObject encOption = opts.fastARef(runtime.newSymbol("encoding"));
+        IRubyObject extOption = opts.fastARef(runtime.newSymbol("external_encoding"));
+        IRubyObject intOption = opts.fastARef(runtime.newSymbol("internal_encoding"));
+        if (encOption != null && !encOption.isNil()) {
+            if (extOption != null) {
+                runtime.getWarnings().warn(
+                        "Ignoring encoding parameter '" + encOption
+                                + "': external_encoding is used");
+                encOption = runtime.getNil();
+            } else if (intOption != null) {
+                runtime.getWarnings().warn(
+                        "Ignoring encoding parameter '" + encOption
+                                + "': internal_encoding is used");
+                encOption = runtime.getNil();
+            } else {
+                IRubyObject tmp = encOption.checkStringType19();
+                if (!tmp.isNil()) {
+                    return parseModeEncodingOption(runtime, tmp.convertToString().toString());
+                }
+                return createEncodingOption(runtime, runtime.getEncodingService()
+                        .getEncodingFromObject(encOption), null, false);
+            }
+        }
+        boolean set = false;
+        Encoding extEncoding = null;
+        Encoding intEncoding = null;
+        if (extOption != null) {
+            set = true;
+            if (!extOption.isNil()) {
+                extEncoding = runtime.getEncodingService().getEncodingFromObject(extOption);
+            }
+        }
+        if (intOption != null) {
+            set = true;
+            if (intOption.isNil()) {
+                // null;
+            } else if (intOption.convertToString().toString().equals("-")) {
+                // null;
+            } else {
+                intEncoding = runtime.getEncodingService().getEncodingFromObject(intOption);
+            }
+        }
+        if (!set)
+            return null;
+        return createEncodingOption(runtime, extEncoding, intEncoding, false);
+    }
+
+    // c: parse_mode_enc
+    private static EncodingOption parseModeEncodingOption(Ruby runtime, String option) {
+        Encoding extEncoding = null;
+        Encoding intEncoding = null;
+        boolean isBom = false;
+        String[] encs = option.split(":", 2);
+        if (encs[0].toLowerCase().startsWith("bom|utf-")) {
+            isBom = true;
+            encs[0] = encs[0].substring(4);
+        }
+        extEncoding = runtime.getEncodingService().getEncodingFromObject(runtime.newString(encs[0]));
+        if (encs.length > 1) {
+            if (encs[1].equals("-")) {
+                // null;
+            } else {
+                intEncoding = runtime.getEncodingService().getEncodingFromObject(runtime.newString(encs[1]));
+            }
+        }
+        return createEncodingOption(runtime, extEncoding, intEncoding, isBom);
+    }
+
+    private static EncodingOption createEncodingOption(Ruby runtime, Encoding extEncoding,
+            Encoding intEncoding, boolean isBom) {
+        boolean defaultExt = false;
+        if (extEncoding == null) {
+            extEncoding = runtime.getDefaultExternalEncoding();
+            defaultExt = true;
+        }
+        if (intEncoding == null
+                && extEncoding != runtime.getEncodingService().getAscii8bitEncoding()) {
+            intEncoding = runtime.getDefaultInternalEncoding();
+        }
+        if (intEncoding == null || intEncoding == extEncoding) {
+            if (defaultExt && intEncoding != extEncoding) {
+                intEncoding = null;
+            } else {
+                intEncoding = extEncoding;
+            }
+            extEncoding = null;
+        }
+        return new EncodingOption(extEncoding, intEncoding, isBom);
+    }
+    
     /**
      * Try for around 1s to destroy the child process. This is to work around
      * issues on some JVMs where if you try to destroy the process too quickly
