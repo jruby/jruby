07b1022a8ec35cfb05b64997a5a0099221ed556d
Subramanya Sastry <sss.lists@gmail.com>
2011-09-12 16:12:56 -0500

    [IR] Remove really old hack and associated FIXME from DefineModuleInstr

--- src/org/jruby/compiler/ir/instructions/DefineModuleInstr.java
+++ src/org/jruby/compiler/ir/instructions/DefineModuleInstr.java
@@ -29,14 +29,6 @@ public class DefineModuleInstr extends OneOperandInstr {
         IRScope scope = mmo.scope;
         RubyModule container = mmo.getContainer(interp, context, self);
         RubyModule module = container.defineOrGetModuleUnder(scope.getName());
-
-		  // SSS FIXME: Hack/side-effect to get the meta-class instantiated for certain scenarios!
-		  // We need to find out why 'foo.extend(module)' is not creating the meta class automatically.
-		  // The correct fix is to find out who is implicitly calling getSingletonClass in the current
-		  // AST interpreter, remove side effect of getSingletonClass and make the build of this metaclass
-		  // explicit (probably in the extend methods?).
-        module.getSingletonClass();
-
         getResult().store(interp, context, self, mmo.interpretBody(interp, context, module));
         return null;
     }
