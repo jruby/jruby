e17cdf7b421401685be82fdc1696e667528bce52
Charles Oliver Nutter <headius@headius.com>
2011-09-13 15:28:59 -0500

    Fix JRuby ext loading to report better errors (where it reported none before). (cherry picked from commit 68976c39c44dda4618a49d8f47ddee2e59a663df)

--- src/org/jruby/runtime/load/LoadService.java
+++ src/org/jruby/runtime/load/LoadService.java
@@ -613,11 +613,20 @@ public class LoadService {
                 }
 
                 // quietly try to load the class
-                Class theClass = runtime.getJavaSupport().loadJavaClassQuiet(className);
+                Class theClass = runtime.getJavaSupport().loadJavaClass(className);
                 state.library = new ClassExtensionLibrary(className + ".java", theClass);
-            } catch (Exception ee) {
-                state.library = null;
-                runtime.getGlobalVariables().clear("$!");
+            } catch (ClassNotFoundException cnfe) {
+                if (runtime.isDebug()) cnfe.printStackTrace();
+                // we ignore this and assume the jar is not an extension
+            } catch (UnsupportedClassVersionError ucve) {
+                if (runtime.isDebug()) ucve.printStackTrace();
+                throw runtime.newLoadError("JRuby ext built for wrong Java version in `" + finName + "': " + ucve);
+            } catch (IOException ioe) {
+                if (runtime.isDebug()) ioe.printStackTrace();
+                throw runtime.newLoadError("IOException loading extension `" + finName + "`: " + ioe);
+            } catch (Exception e) {
+                if (runtime.isDebug()) e.printStackTrace();
+                throw runtime.newLoadError("Exception loading extension `" + finName + "`: " + e);
             }
 
             // If there was a good library before, we go back to that
