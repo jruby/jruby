424f0dfc8f5b44f47ed1cd6a5ea5bc83025ed643
Charles Oliver Nutter <headius@headius.com>
2011-09-13 12:36:49 -0500

    Update Ruby 1.9 stdlib to freshly-merged jruby-ruby_1_9_2 copy.

--- lib/ruby/1.9/psych/visitors/to_ruby.rb
+++ lib/ruby/1.9/psych/visitors/to_ruby.rb
@@ -60,7 +60,7 @@ module Psych
         when "tag:yaml.org,2002:float", "!float"
           Float(@ss.tokenize(o.value))
         when "!ruby/regexp"
-          o.value =~ /^\/(.*)\/([mix]*)$/
+          o.value =~ /^\/(.*)\/([mixn]*)$/
           source  = $1
           options = 0
           lang    = nil
@@ -69,6 +69,9 @@ module Psych
             when 'x' then options |= Regexp::EXTENDED
             when 'i' then options |= Regexp::IGNORECASE
             when 'm' then options |= Regexp::MULTILINE
+
+            # FIXME: there is no constant for ARG_ENCODING_NONE
+            when 'n' then options |= 32
             else lang = option
             end
           end
@@ -187,8 +190,17 @@ module Psych
           o.children.each_slice(2) { |k,v|
             key = accept(k)
 
-            if key == '<<' && Nodes::Alias === v
-              hash.merge! accept(v)
+            if key == '<<'
+              case v
+              when Nodes::Alias
+                hash.merge! accept(v)
+              when Nodes::Sequence
+                accept(v).reverse_each do |value|
+                  hash.merge! value
+                end
+              else
+                hash[key] = accept(v)
+              end
             else
               hash[key] = accept(v)
             end
