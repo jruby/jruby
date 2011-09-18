09f75dd25b2225385d60bc34cf60b765239cc16a
Charles Oliver Nutter <headius@headius.com>
2011-09-19 07:34:24 +0800

    Generify IRubyObject.to_java to reduce the need for casting the return value.

--- src/org/jruby/runtime/builtin/IRubyObject.java
+++ src/org/jruby/runtime/builtin/IRubyObject.java
@@ -259,7 +259,7 @@ public interface IRubyObject {
      *
      * @param cls The target type to which the object should be converted.
      */
-    Object toJava(Class cls);
+    <T> T toJava(Class<T> cls);
 
     /**
      * RubyMethod dup.
