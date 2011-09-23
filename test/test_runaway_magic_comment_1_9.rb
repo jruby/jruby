5c086a9e765bc04da954f358bd78d9ad96c19cd9
Charles Oliver Nutter <headius@headius.com>
2011-09-23 04:57:10 -0500

    Tweak to get some 1.9 tests running with new out-of-process default launching.

--- test/test_runaway_magic_comment_1_9.rb
+++ test/test_runaway_magic_comment_1_9.rb
@@ -5,6 +5,6 @@ class MagicCommentTest19 < Test::Unit::TestCase
   # JRUBY-5922
   def test_magic_comment_parse
     path = File.join(File.dirname(__FILE__), 'runaway_magic_comment.rb')
-    assert(system "ruby --1.9 #{path}")
+    assert(system "#{ENV_JAVA['jruby.home']}/bin/jruby --1.9 #{path}")
   end
 end
