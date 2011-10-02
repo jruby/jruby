06d16025efa6dbdaaf021ded99e5388e69235a1e
Hiro Asari <asari.ruby@gmail.com>
2011-10-02 02:21:01 -0400

    Fix the sieve and formatting

--- bench/bench_sieve.rb
+++ bench/bench_sieve.rb
@@ -5,17 +5,15 @@ p RUBY_DESCRIPTION
 N = 1299709 #100K-th prime
 PRIMES = (2..N).to_a
 5.times do
-Benchmark.bm(30) { |bm|
-
-  bm.report("100K primes") do
-    index = 2
-    primes = PRIMES.dup
-    while primes[index]**2 < primes.last
-      prime = primes[index]
-      primes = primes.select {|x| x == prime || x%prime != 0}
-      index += 1
+  primes = PRIMES.dup
+  Benchmark.bm(30) { |bm|
+    bm.report("100K primes") do
+      index = 0
+      while primes[index]**2 < primes.last
+        prime = primes[index]
+        primes = primes.select {|x| x == prime || x%prime != 0}
+        index += 1
+      end
     end
-  #p primes
-  end
-}
+  }
 end
