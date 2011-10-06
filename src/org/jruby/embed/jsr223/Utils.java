4b8854688f0ac7ee2c1fc9b3128cae8043723754
Yoko Harada <yokolet@gmail.com>
2011-10-06 14:07:19 -0400

    A little tweek for JRUBY-6101 with a little hope.

--- src/org/jruby/embed/jsr223/Utils.java
+++ src/org/jruby/embed/jsr223/Utils.java
@@ -169,7 +169,7 @@ public class Utils {
     }
     
     private static RubyIO getRubyIO(Ruby runtime, Writer writer) throws IOException, BadDescriptorException {
-        PrintStream pstream = new PrintStream(new WriterOutputStream(writer));
+        PrintStream pstream = new PrintStream(new WriterOutputStream(writer), true);
         RubyIO io = new RubyIO(runtime, pstream, false);
         io.getOpenFile().getMainStreamSafe().setSync(true);
         io.getOpenFile().getMainStreamSafe().fflush();
