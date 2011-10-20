jruby/jruby

commit 982bac0d8d5b6d2879423ebd4d13300f71fdc387

19: Make regexps use encoding from options explicit encodings in a roundabout way...

src/org/jruby/parser/ParserSupport.java
@@ -1623,7 +1623,9 @@ public class ParserSupport {
 
             for (Node fragment: dStrNode.childNodes()) {
                 if (fragment instanceof StrNode) {
-                    regexpFragmentCheck(end, ((StrNode) fragment).getValue());
+                    ByteList frag = ((StrNode) fragment).getValue();
+                    regexpFragmentCheck(end, frag);
+                    encoding = frag.getEncoding();
                 }
             }
 
@@ -1631,7 +1633,9 @@ public class ParserSupport {
         }
 
         // EvStrNode: #{val}: no fragment check, but at least set encoding
-        regexpFragmentCheck(end, ByteList.create(""));
+        ByteList empty = ByteList.create("");
+        regexpFragmentCheck(end, empty);
+        encoding = empty.getEncoding();
         return new DRegexpNode(position, options, encoding).add(contents);
     }
     

commit c3d04c6f21c8c63488e7e06ed756e209a9588b9b

Tweak commit 982bac0, which introduced 3 "defined" spec failures

src/org/jruby/parser/ParserSupport.java
@@ -1625,7 +1625,9 @@ public class ParserSupport {
                 if (fragment instanceof StrNode) {
                     ByteList frag = ((StrNode) fragment).getValue();
                     regexpFragmentCheck(end, frag);
-                    encoding = frag.getEncoding();
+                    if (!lexer.isOneEight()) {
+                        encoding = frag.getEncoding();
+                    }
                 }
             }
 
@@ -1635,7 +1637,9 @@ public class ParserSupport {
         // EvStrNode: #{val}: no fragment check, but at least set encoding
         ByteList empty = ByteList.create("");
         regexpFragmentCheck(end, empty);
-        encoding = empty.getEncoding();
+        if (!lexer.isOneEight()) {
+            encoding = empty.getEncoding();
+        }
         return new DRegexpNode(position, options, encoding).add(contents);
     }
     