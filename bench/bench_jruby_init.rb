cb1d6a404bf8afb2140817c51c9d3ccf027f23a2
Charles Oliver Nutter <headius@headius.com>
2011-10-25 15:02:53 -0500

    Modify bench_jruby_init to take iteration count and cli params.

--- bench/bench_jruby_init.rb
+++ bench/bench_jruby_init.rb
@@ -2,10 +2,19 @@ require 'java'
 require 'benchmark'
 
 Ruby = org.jruby.Ruby
+Cfg = org.jruby.RubyInstanceConfig
 System = java.lang.System
 
-Benchmark.bm(20) { |bench|
-  bench.report("100 JRuby runtime inits") {
-    100.times { Ruby.newInstance(System.in, System.out, System.err) }
+args = ARGV[1]
+
+(ARGV[0] || 5).to_i.times do
+  Benchmark.bm(20) { |bench|
+    bench.report("in-process `jruby #{args}`") {
+      cfg = Cfg.new
+      if args
+        cfg.process_arguments args.split.to_java(:string)
+      end
+      Ruby.newInstance(cfg)
+    }
   }
-}
+end
