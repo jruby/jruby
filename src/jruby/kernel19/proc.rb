7d3e0ee5b123f568623153cbb0fa3d9450a20f1f
Charles Oliver Nutter <headius@headius.com>
2011-10-31 13:57:17 -0500

    Incorporate modified Proc#curry from @xaviershay.

--- src/jruby/kernel19/proc.rb
+++ src/jruby/kernel19/proc.rb
@@ -1,82 +1,41 @@
 class Proc
-  def curry(arity = self.arity)
-    check = check_arity(arity, self.arity, lambda?)
-    if lambda? && arity != self.arity && !check
-      argument_error(arity, self.arity)
-    end
+  def curry(curried_arity = nil)
+    if lambda? && curried_arity
+      if arity > 0 && curried_arity != arity
+        raise ArgumentError, "Wrong number of arguments (%i for %i)" % [
+          curried_arity,
+          arity
+        ]
+      end
 
-    make_curry_proc(self, [], arity)
-  end
-
-  private
-  def make_curry_proc(proc, passed, arity)
-    passed.freeze
-    proc_lambda = proc.lambda?
-    curried = proc_or_lambda(proc_lambda) do |*args, &blk|
-      newpassed = passed + args
-      if check_arity(newpassed.length, arity, proc_lambda)
-        call(*newpassed, &blk)
-      else
-        make_curry_proc(proc, newpassed, arity)
+      if arity < 0 && curried_arity < (-arity - 1)
+        raise ArgumentError, "Wrong number of arguments (%i for %i)" % [
+          curried_arity,
+          -arity - 1
+        ]
       end
     end
-    curried.singleton_class.send(:define_method, :binding) {
-      raise ArgumentError, "cannot create binding from curried proc"
+
+    args = []
+
+    m = lambda? ? :lambda : :proc
+    f = send(m) {|*x|
+      args += x
+      if args.length >= arity
+        self[*args]
+      else
+        f
+      end
     }
-    curried.singleton_class.send(:define_method, :parameters) {
+    f.singleton_class.send(:define_method, :binding) {
+      raise ArgumentError, "cannot create binding from f proc"
+    }
+    f.singleton_class.send(:define_method, :parameters) {
       [[:rest]]
     }
-    curried.singleton_class.send(:define_method, :source_location) {
+    f.singleton_class.send(:define_method, :source_location) {
       nil
     }
-    curried
-  end
-
-  def check_arity(args_arity, target_arity, lambda)
-    if lambda
-      if target_arity >= 0
-        if args_arity == target_arity
-          true
-        elsif args_arity > target_arity
-          argument_error(args_arity, target_arity)
-        else
-          false
-        end
-      else
-        if args_arity >= -target_arity
-          true
-        elsif args_arity == (-target_arity) - 1
-          true
-        else
-          false
-        end
-      end
-    else
-      if target_arity >= 0
-        if args_arity >= target_arity
-          true
-        else
-          false
-        end
-      else
-        if args_arity >= (-target_arity) - 1
-          true
-        else
-          false
-        end
-      end
-    end
-  end
-
-  def argument_error(args_arity, target_arity)
-    raise ArgumentError, "wrong number of arguments (#{args_arity} for #{target_arity})"
-  end
-
-  def proc_or_lambda(bool, &block)
-    if bool
-      lambda(&block)
-    else
-      proc(&block)
-    end
+    f
   end
 end
