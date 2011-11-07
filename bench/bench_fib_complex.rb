0b37e4a022915cbd059caffeeba89500211913fb
Charles Oliver Nutter <headius@headius.com>
2011-11-06 23:03:27 -0300

    Modify fib_complex to be an IPS benchmark.

--- bench/bench_fib_complex.rb
+++ bench/bench_fib_complex.rb
@@ -1,4 +1,9 @@
-require 'benchmark'
+require 'rubygems'
+begin
+  require 'benchmark/ips'
+rescue loadError
+  fail "install the benchmark-suite gem"
+end
 
 ONE = 1
 TWO = 2
@@ -19,6 +24,16 @@ def fib_ruby(n)
     n
   else
     fib_ruby(n - 2) + fib_ruby(n - 1)
+  end
+end
+
+def fib_ruby1(n)
+  a = 1
+  b = 2
+  if n < b
+    n
+  else
+    fib_ruby(n - b) + fib_ruby(n - a)
   end
 end
 
@@ -46,15 +61,26 @@ def fib_ruby4(n)
   end
 end
 
-TIMES = (ARGV[0] || 5).to_i
 N = (ARGV[1] || 30).to_i
-TIMES.times {
-  puts "normal fib"
-  puts Benchmark.measure { puts fib_ruby(N) }
-  puts "fib with constants"
-  puts Benchmark.measure { puts fib_ruby2(N) }
-  puts "fib with additional calls"
-  puts Benchmark.measure { puts fib_ruby3(N) }
-  puts "fib with constants and additional calls"
-  puts Benchmark.measure { puts fib_ruby4(N) }
-}
+
+Benchmark.ips do |x|
+  x.report "normal fib" do
+    fib_ruby(N)
+  end
+  
+  x.report "fib with variables" do
+    fib_ruby1(N)
+  end
+  
+  x.report "fib with constants" do
+    fib_ruby2(N)
+  end
+  
+  x.report "fib with additional calls" do
+    fib_ruby3(N)
+  end
+  
+  x.report "fib with constants and additional calls" do
+    fib_ruby4(N)
+  end
+end  
