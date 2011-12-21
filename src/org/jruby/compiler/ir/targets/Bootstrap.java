521330fd4bea4720def36c0404c35926c1c80e28
Charles Oliver Nutter <headius@headius.com>
2011-12-21 12:12:05 -0600

    Add missing jzlib to shared module.

---
+++ src/org/jruby/compiler/ir/targets/Bootstrap.java
@@ -0,0 +1,11 @@
+package org.jruby.compiler.ir.targets;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: headius
+ * Date: 12/21/11
+ * Time: 8:55 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Bootstrap {
+}
