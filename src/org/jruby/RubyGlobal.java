3a2c1ca01d4423cde86557ec4762074fe2a707cf
Yoko Harada <yokolet@gmail.com>
2012-01-04 12:41:49 -0500

    $ARGV and $* should not be readonly to meet with JSR223 spec.

--- src/org/jruby/RubyGlobal.java
+++ src/org/jruby/RubyGlobal.java
@@ -83,7 +83,7 @@ public class RubyGlobal {
             argvArray.append(RubyString.newStringShared(runtime, argv[i].getBytes()));
         }
         runtime.defineGlobalConstant("ARGV", argvArray);
-        globals.defineReadonly("$*", new ValueAccessor(argvArray));
+        globals.define("$*", new ValueAccessor(argvArray));
 
         IAccessor d = new ValueAccessor(runtime.newString(
                 runtime.getInstanceConfig().displayedFileName()));
