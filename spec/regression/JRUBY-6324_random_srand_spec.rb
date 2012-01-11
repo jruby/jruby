b2f3e4ff6fc2183e7769baf9b39407ee5ffa55f6
Hiroshi Nakamura <nahi@ruby-lang.org>
2012-01-11 13:43:46 +0900

    Revert "Temporal fix for spec failure"
    This reverts commit f0c769d84dce2f589db476a5989cbcebba9f8b11.

--- spec/regression/JRUBY-6324_random_srand_spec.rb
+++ spec/regression/JRUBY-6324_random_srand_spec.rb
@@ -9,6 +9,6 @@ describe "JRUBY-6324: random seed for srand is not initialized properly" do
   it "initializes initial seed for PRNG" do
     rt = Java.org.jruby.embed.ScriptingContainer.new
     id1, id2 = rt.run_scriptlet("n = 2**128; srand(n); [n.object_id, srand.object_id]")
-    (id1 == id2).should == true
+    id1.should == id2
   end
 end
