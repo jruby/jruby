70f9ed1cd474350d6683b34bb285047bb6a072da
Hiroshi Nakamura <nahi@ruby-lang.org>
2012-01-11 15:49:42 +0900

    This spec depends on 'test' build

--- spec/regression/JRUBY-5471_extension_attrs_are_private_spec.rb
+++ spec/regression/JRUBY-5471_extension_attrs_are_private_spec.rb
@@ -1,12 +1,16 @@
-require 'rspec'
-require 'java'
-$CLASSPATH << File.expand_path('../../../build/classes/test', __FILE__)
-require 'dummy/dummy'
+path = File.expand_path('../../../build/classes/test', __FILE__)
+# This test depends on test build. Skip if test classes are not built.
+if File.exist?(File.join(path, 'dummy'))
+  require 'rspec'
+  require 'java'
+  $CLASSPATH << path
+  require 'dummy/dummy'
 
-describe 'A Java-based BasicLibraryService extension' do
-  it "gets its own frame with public visibility" do
-    d = XYZ_Dummy_XYZ.new
-    d.dummy_attr = 'foo'
-    d.dummy_attr.should == 'foo'
+  describe 'A Java-based BasicLibraryService extension' do
+    it "gets its own frame with public visibility" do
+      d = XYZ_Dummy_XYZ.new
+      d.dummy_attr = 'foo'
+      d.dummy_attr.should == 'foo'
+    end
   end
 end
