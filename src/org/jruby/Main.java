ea75dac58cac28be6e41158acf209b047fe2b487
Thomas E. Enebo <tom.enebo@gmail.com>
2012-01-18 09:19:02 -0600

    JRUBY-6338: JRuby does not look for .jrubyrc in home directory on Windows

--- src/org/jruby/Main.java
+++ src/org/jruby/Main.java
@@ -44,6 +44,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import org.jruby.exceptions.MainExitException;
@@ -93,38 +95,51 @@ public class Main {
         config.setHardExit(hardExit);
     }
     
-    public void processDotfile() {
-        // try current dir, then home dir
-        String home = SafePropertyAccessor.getProperty("user.dir");
+    private static List<String> getDotfileDirectories() {
+        ArrayList<String> searchList = new ArrayList<String>();
+        for (String homeProp : new String[] {"user.dir", "user.home"}) {
+            String home = SafePropertyAccessor.getProperty(homeProp);
+            if (home != null) searchList.add(home);
+        }
+        
+        // JVM sometimes picks odd location for user.home based on a registry entry
+        // (see http://bugs.sun.com/view_bug.do?bug_id=4787931).  Add extra check in 
+        // case that entry is wrong. Add before user.home in search list.
+        if (Platform.IS_WINDOWS) {
+            String homeDrive = System.getenv("HOMEDRIVE");
+            String homePath = System.getenv("HOMEPATH");
+            if (homeDrive != null && homePath != null) {
+                searchList.add(1, (homeDrive + homePath).replace('\\', '/'));
+            }
+        }
+        
+        return searchList;
+    }
+    
+    public static void processDotfile() {
+        for (String home : getDotfileDirectories()) {
+            File dotfile = new File(home + "/.jrubyrc");
+            if (dotfile.exists()) loadJRubyProperties(dotfile);
+        }
+    }
+
+    private static void loadJRubyProperties(File dotfile) {
         FileInputStream fis = null;
         
         try {
-            File dotfile = new File(home + "/.jrubyrc");
-            if (!dotfile.exists()) {
-                home = SafePropertyAccessor.getProperty("user.home");
-                dotfile = new File(home + "/.jrubyrc");
-            }
-
-            // no dotfile!
-            if (!dotfile.exists()) return;
-
             // update system properties with long form jruby properties from .jrubyrc
             Properties sysProps = System.getProperties();
             Properties newProps = new Properties();
-
             // load properties and re-set as jruby.*
             fis = new FileInputStream(dotfile);
             newProps.load(fis);
             for (Map.Entry entry : newProps.entrySet()) {
                 sysProps.put("jruby." + entry.getKey(), entry.getValue());
             }
-
-            // replace system properties
-            System.setProperties(sysProps);
         } catch (IOException ioe) {
-            // do anything?
+            System.err.println("exception loading " + dotfile + ": " + ioe.getMessage());
         } catch (SecurityException se) {
-            // do anything?
+            System.err.println("security exception loading " + dotfile + ": " + se.getMessage());
         } finally {
             if (fis != null) try {fis.close();} catch (Exception e) {}
         }
