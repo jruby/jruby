eccec6d183ef252ea864336c5ea22169afadbc92
Hiro Asari <asari.ruby@gmail.com>
2012-01-23 14:58:37 -0500

    Make git_bisect_run_general.sh more general purpose, with pre- and post-commands to run around "ant clean jar".

--- tool/git_bisect_run_mspec.sh
+++ tool/git_bisect_run_mspec.sh
@@ -1,13 +1,4 @@
 #! /bin/sh
 
-# simple script for use with 'git bisect run'
-# e.g., git bisect run tool/git_bisect_run_mspec.sh -T--1.9 spec/ruby/language/defined_spec.rb
-# arguments will be passed on to mspec.
-
-cd `dirname $0`/..
-if [ ! -e spec/mspec ]; then
-  ant fetch-stable-specs
-fi
-
-ant clean jar
-jruby spec/mspec/bin/mspec $*
+# see git_bisect_run_general.sh for more explanation
+exec `dirname $0`/git_bisect_run_general.sh -c "spec/mspec/bin/mspec $*"
