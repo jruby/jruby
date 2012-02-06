441ca4c488c1ec5c112666586857e48dd8dd46d0
Charles Oliver Nutter <headius@headius.com>
2012-02-06 10:26:08 -0600

    Add an array set benchmark.

--- bench/java/bench_java_arrays.rb
+++ bench/java/bench_java_arrays.rb
@@ -7,6 +7,7 @@ TIMES.times do
   Benchmark.bm(30) do |bm|
     bm.report("control") {a = [1,2,3,4].to_java; 100_000.times {a}}
     bm.report("java_ary[0]") {a = [1,2,3,4].to_java; 100_000.times {a[0]}}
+    bm.report("java_ary[0] = 1") {a = [1,2,3,4].to_java; 100_000.times {a[0] = 1}}
     bm.report("java_ary[1,2]") {a = [1,2,3,4].to_java; 100_000.times {a[1,2]}}
     bm.report("java_ary.each {|x|}") {a = [1,2,3,4].to_java; 100_000.times {a.each {|x|}}}
     bm.report("java_ary + java_ary") {a = [1,2,3,4].to_java; 100_000.times {a + a}}
