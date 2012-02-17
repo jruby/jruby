9de45fa41e1e502bbd1d1e0381f409286bab6f54
Charles Oliver Nutter <headius@headius.com>
2012-02-17 13:11:02 -0600

    Minor reformat.

--- src/jruby/java/java_utilities.rb
+++ src/jruby/java/java_utilities.rb
@@ -1,10 +1,10 @@
 module JavaUtilities
-  def JavaUtilities.extend_proxy(java_class_name, &block)
+  def self.extend_proxy(java_class_name, &block)
     java_class = Java::JavaClass.for_name(java_class_name)
     java_class.extend_proxy JavaInterfaceExtender.new(java_class_name, &block)
   end
 
-  def JavaUtilities.print_class(java_type, indent="")
+  def self.print_class(java_type, indent="")
      while (!java_type.nil? && java_type.name != "java.lang.Class")
         puts "#{indent}Name:  #{java_type.name}, access: #{ JavaUtilities.access(java_type) }  Interfaces: "
         java_type.interfaces.each { |i| print_class(i, "  #{indent}") }
@@ -13,5 +13,4 @@ module JavaUtilities
         java_type = java_type.superclass
      end
   end
-
 end
