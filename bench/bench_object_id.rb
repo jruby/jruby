a9e0ec17d0ea297734d4e9e513cb0756b3d32ccf
Charles Oliver Nutter <headius@headius.com>
2012-02-19 00:21:37 -0600

    Reduce contention and overhead in object_id allocation.

--- bench/bench_object_id.rb
+++ bench/bench_object_id.rb
@@ -6,8 +6,18 @@ require 'benchmark'
       o = Object.new
       1_000_000.times { o.object_id }
     end
+
     bm.report("1M calls to Object.new.object_id") do
       1_000_000.times { Object.new.object_id }
+    end
+
+    bm.report("1M * 8 threaded calls to obj.object_id") do
+      o = Object.new
+      (1..8).map do
+        Thread.new do
+          1_000_000.times { o.object_id }
+        end
+      end.map(&:join)
     end
   end
 end
