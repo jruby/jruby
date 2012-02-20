d56e1184011280fab387cdad0e75bf425e780dc3
Hiroshi Nakamura <nahi@ruby-lang.org>
2012-02-20 18:02:47 +0900

    Refix JRUBY-6208: GzipWriter#write should honor writeEncoding, not readEncoding
    Title says all.  The regression spec I added before wrongly used
    GzipReader#read() without size parameter.  GzipReader#read() uses the
    specified read encoding (it is Encoding.default_external if not
    specified) and it hides this bug.

--- spec/regression/JRUBY-6208_gzip_bin_write_spec.rb
+++ spec/regression/JRUBY-6208_gzip_bin_write_spec.rb
@@ -3,14 +3,15 @@ require 'zlib'
 
 describe "JRUBY-6208: GzipWriter creates corrupted gzip stream" do
   if RUBY_VERSION >= "1.9.0"
-    it "works should not try to convert binary to default_external" do
+    it "should not try to convert binary to default_external" do
       t = Tempfile.new(['out', 'gz'])
       gzfile = t.path
-      content = "\x80"
+      content = (0..255).to_a.pack('c*')
+      content.force_encoding("BINARY")
       Zlib::GzipWriter.open(gzfile) do |gz|
         gz.print(content)
       end
-      subject = Zlib::GzipReader.open(gzfile) { |gz| gz.read }
+      subject = Zlib::GzipReader.open(gzfile) { |gz| gz.read(1024) }
       subject.should == content
     end
   end
