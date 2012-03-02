bed27b139c7cf450da35b7b4038423244d9bb1b1
Thomas E. Enebo <tom.enebo@gmail.com>
2012-03-02 13:37:47 -0600

    IR: BuildLambda is not a call

--- src/org/jruby/compiler/ir/instructions/ruby19/BuildLambdaInstr.java
+++ src/org/jruby/compiler/ir/instructions/ruby19/BuildLambdaInstr.java
@@ -1,49 +1,62 @@
 package org.jruby.compiler.ir.instructions.ruby19;
 
+import java.util.Map;
 import org.jruby.RubyProc;
 import org.jruby.compiler.ir.IRClosure;
 import org.jruby.compiler.ir.Operation;
-import org.jruby.compiler.ir.instructions.CallInstr;
 import org.jruby.compiler.ir.instructions.Instr;
-import org.jruby.compiler.ir.operands.MethAddr;
+import org.jruby.compiler.ir.instructions.ResultInstr;
 import org.jruby.compiler.ir.operands.Operand;
 import org.jruby.compiler.ir.operands.Variable;
 import org.jruby.compiler.ir.operands.WrappedIRClosure;
 import org.jruby.compiler.ir.representations.InlinerInfo;
 import org.jruby.lexer.yacc.ISourcePosition;
 import org.jruby.runtime.Block;
-import org.jruby.runtime.CallType;
 import org.jruby.runtime.DynamicScope;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.builtin.IRubyObject;
 
-public class BuildLambdaInstr extends CallInstr {
+public class BuildLambdaInstr extends Instr implements ResultInstr {
     /** The position for the block */
     private final ISourcePosition position;
+    private Variable result;
+    private Operand[] operands;
 
     public BuildLambdaInstr(Variable lambda, IRClosure lambdaBody, ISourcePosition position) {
-        super(Operation.LAMBDA, CallType.UNKNOWN, lambda, MethAddr.NO_METHOD, null, EMPTY_OPERANDS, new WrappedIRClosure(lambdaBody));
+        super(Operation.LAMBDA);
+     
+        this.result = lambda;
+        this.operands = new Operand[] { new WrappedIRClosure(lambdaBody) };
         this.position = position;
     }
 
     @Override
     public Operand[] getOperands() {
-        return new Operand[] { getClosureArg(getLambdaBody().getManager().getNil()) };
+        return operands;
+    }
+    
+    public Variable getResult() {
+        return result;
     }
 
-    @Override
-    public Instr discardResult() {
-        return this;
+    public void updateResult(Variable v) {
+        this.result = v;
     }
-
+    
     @Override
     public Instr cloneForInlining(InlinerInfo ii) {
         return new BuildLambdaInstr(ii.getRenamedVariable(getResult()), getLambdaBody(), position);
     }
+    
+    @Override
+    public void simplifyOperands(Map<Operand, Operand> valueMap, boolean force) {
+        for (int i = 0; i < operands.length; i++) {
+            operands[i] = operands[i].getSimplifiedOperand(valueMap, force);
+        }        
+    }    
 
     private IRClosure getLambdaBody() {
-        // We know this is wrapping an actual block so passing null should be ok (yuck)
-        return ((WrappedIRClosure)getClosureArg(null)).getClosure();
+        return ((WrappedIRClosure) operands[0]).getClosure();
     }
 
     @Override
@@ -53,6 +66,10 @@ public class BuildLambdaInstr extends CallInstr {
         // JRUBY-5686: do this before executing so first time sets cref module
         getLambdaBody().getStaticScope().determineModule();
 
-        return RubyProc.newProc(context.getRuntime(), (Block)getClosureArg(context.getRuntime().getIRManager().getNil()).retrieve(context, self, currDynScope, temp), Block.Type.LAMBDA, position);
+        IRClosure body = getLambdaBody();
+        // ENEBO: Now can live nil be passed as block reference?
+        return RubyProc.newProc(context.getRuntime(), 
+                (Block) (body == null ? context.getRuntime().getIRManager().getNil() : operands[0]).retrieve(context, self, currDynScope, temp),
+                Block.Type.LAMBDA, position);
     }
 }
