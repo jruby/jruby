ba7af62827be716f0d36ee49bc4c25f112f05d64
Thomas E. Enebo <tom.enebo@gmail.com>
2012-03-05 15:40:33 -0600

    IR: Add specialize and simple 1 arg case for noresultinstr

--- src/org/jruby/compiler/ir/instructions/NoResultCallInstr.java
+++ src/org/jruby/compiler/ir/instructions/NoResultCallInstr.java
@@ -1,24 +1,43 @@
 package org.jruby.compiler.ir.instructions;
 
 import org.jruby.compiler.ir.Operation;
+import org.jruby.compiler.ir.instructions.specialized.OneOperandArgNoBlockNoResultCallInstr;
 import org.jruby.compiler.ir.operands.MethAddr;
 import org.jruby.compiler.ir.operands.Operand;
 import org.jruby.compiler.ir.representations.InlinerInfo;
 import org.jruby.compiler.ir.targets.JVM;
-import org.jruby.runtime.Block;
 import org.jruby.runtime.CallType;
-import org.jruby.runtime.ThreadContext;
-import org.jruby.runtime.builtin.IRubyObject;
 
 public class NoResultCallInstr extends CallBase {
-    public NoResultCallInstr(Operation op, CallType callType, MethAddr methAddr, Operand receiver, Operand[] args, Operand closure) {
+    public NoResultCallInstr(Operation op, CallType callType, MethAddr methAddr,
+            Operand receiver, Operand[] args, Operand closure) {
         super(op, callType, methAddr, receiver, args, closure);
+    }
+    
+    public NoResultCallInstr(NoResultCallInstr instr) {
+        this(instr.getOperation(), instr.getCallType(), instr.methAddr, 
+                instr.receiver, instr.arguments, instr.closure);
     }
     
     @Override
     public Instr cloneForInlining(InlinerInfo ii) {
         return new NoResultCallInstr(getOperation(), getCallType(), (MethAddr) getMethodAddr().cloneForInlining(ii), 
                 receiver.cloneForInlining(ii), cloneCallArgs(ii), closure == null ? null : closure.cloneForInlining(ii));
+    }
+
+    @Override
+    public CallBase specializeForInterpretation() {
+        if (hasClosure() || containsSplat()) return this;
+        
+        switch (getCallArgs().length) {
+//            case 0:
+//                return new ZeroOperandArgNoBlockNoResultCallInstr(this);
+            case 1:
+//                if (isAllFixnums()) return new OneFixnumArgNoBlockNoResultCallInstr(this);
+
+                return new OneOperandArgNoBlockNoResultCallInstr(this);
+        }
+        return this;
     }
 
     @Override
---
+++ src/org/jruby/compiler/ir/instructions/specialized/OneOperandArgNoBlockNoResultCallInstr.java
@@ -0,0 +1,30 @@
+package org.jruby.compiler.ir.instructions.specialized;
+
+import org.jruby.compiler.ir.instructions.NoResultCallInstr;
+import org.jruby.runtime.Block;
+import org.jruby.runtime.DynamicScope;
+import org.jruby.runtime.ThreadContext;
+import org.jruby.runtime.builtin.IRubyObject;
+
+/**
+ *
+ * @author enebo
+ */
+public class OneOperandArgNoBlockNoResultCallInstr extends NoResultCallInstr {
+    public OneOperandArgNoBlockNoResultCallInstr(NoResultCallInstr call) {
+        super(call);
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + "{1O}";
+    }    
+    
+    @Override
+    public Object interpret(ThreadContext context, DynamicScope dynamicScope, IRubyObject self, Object[] temp, Block block) {
+        IRubyObject object = (IRubyObject) receiver.retrieve(context, self, dynamicScope, temp);
+        IRubyObject arg1 = (IRubyObject) getCallArgs()[0].retrieve(context, self, dynamicScope, temp);
+        
+        return getCallSite().call(context, self, object, arg1);
+    }        
+}
