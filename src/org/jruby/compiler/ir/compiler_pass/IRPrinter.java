3bd11efc6c0c315258f6bb5261fe0224ff7fec62
Thomas E. Enebo <tom.enebo@gmail.com>
2012-03-07 17:32:05 -0600

    IR: Make IRPrinter optionally depend on CFG Builder

--- src/org/jruby/compiler/ir/compiler_pass/IRPrinter.java
+++ src/org/jruby/compiler/ir/compiler_pass/IRPrinter.java
@@ -1,19 +1,30 @@
 package org.jruby.compiler.ir.compiler_pass;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.jruby.compiler.ir.IRScope;
+import org.jruby.compiler.ir.Tuple;
 import org.jruby.compiler.ir.representations.CFG;
 import org.jruby.util.log.Logger;
 import org.jruby.util.log.LoggerFactory;
 
 public class IRPrinter extends CompilerPass {
     public static String[] NAMES = new String[] { "printer", "p" };
-
+    public static List<Tuple<Class<CompilerPass>, DependencyType>> DEPENDENCIES = new ArrayList<Tuple<Class<CompilerPass>, DependencyType>>() {{
+       add(new Tuple(CFGBuilder.class, CompilerPass.DependencyType.OPTIONAL)); 
+    }};
+    
     private static final Logger LOG = LoggerFactory.getLogger("IR_Printer");
     
     public String getLabel() {
         return "Printer";
     }
-
+    
+    @Override
+    public List<Tuple<Class<CompilerPass>, DependencyType>> getDependencies() {
+        return DEPENDENCIES;
+    }
+    
     // Should we run this pass on the current scope before running it on nested scopes?
     public boolean isPreOrder() {
         return true;
@@ -24,7 +35,7 @@ public class IRPrinter extends CompilerPass {
         LOG.info(scope.toString());
 
         // If the cfg of the method is around, print the CFG!
-        CFG c = scope.getCFG();
+        CFG c = (CFG) data[0];
         if (c != null) {
             LOG.info("\nGraph:\n" + c.toStringGraph());
             LOG.info("\nInstructions:\n" + c.toStringInstrs());
