fcc439e2a0d410b155c22762fdbef0df718f3832
Hiro Asari <asari.ruby@gmail.com>
2012-04-19 07:57:09 -0400

    Add spec for JRUBY-6571

---
+++ spec/java_integration/regex/matcher_end_spec.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe "java.util.regex.Matcher.end" do
+	it "does not blow stack" do
+		# See https://jira.codehaus.org/browse/JRUBY-6571
+		import 'java.util.regex.Pattern'
+		import 'java.util.regex.Matcher'
+
+		s = 'hello world'
+		r = Pattern.compile("[eo]")
+		m = r.matcher(s)
+
+		while m.find()
+			lambda{ m.end(0) }.should_not raise_error
+		end
+	end
+end
