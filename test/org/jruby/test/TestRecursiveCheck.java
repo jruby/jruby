1bf66f8d55a9bd1af9b68dff7545da7e2e632575
Ben Browning <bbrownin@redhat.com>
2012-04-25 18:52:53 -0400

    Fix recursiveKey usage across multiple threads (JRUBY-6621)
    Instead of calling recursiveKey.set when the runtime is initialized,
    we implement getInitialValue of ThreadLocal to supply the default
    value when recursiveKey.get() is called for the first time on a given
    thread.
    
    This avoids an NPE when a runtime is initialized on one thread and
    subsequently used on another thread under 1.9.

---
+++ test/org/jruby/test/TestRecursiveCheck.java
@@ -0,0 +1,33 @@
+package org.jruby.test;
+
+import junit.framework.TestCase;
+import org.jruby.CompatVersion;
+
+import org.jruby.Ruby;
+import org.jruby.RubyInstanceConfig;
+
+public class TestRecursiveCheck extends TestCase {
+    private Ruby runtime;
+
+    public TestRecursiveCheck(String name) {
+        super(name);
+    }
+
+    public void setUp() {
+        runtime = Ruby.newInstance(new RubyInstanceConfig() {
+            {
+                setCompatVersion(CompatVersion.RUBY1_9);
+            }
+        });
+    }
+
+    public void testWorksFromMultipleThreads() throws Exception {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                assertNotNull(runtime.evalScriptlet("[].hash").convertToInteger());
+            }
+        });
+        thread.start();
+        thread.join();
+    }
+}
