68cca38e1621d5a82e9d8bf66b44a5b312e52912
Charles Oliver Nutter <headius@headius.com>
2012-04-28 18:57:11 -0500

    Pass null for tags, since SnakeYAML uses that to indicate "no tags"

--- src/org/jruby/ext/psych/PsychEmitter.java
+++ src/org/jruby/ext/psych/PsychEmitter.java
@@ -110,7 +110,7 @@ public class PsychEmitter extends RubyObject {
     public IRubyObject start_document(ThreadContext context, IRubyObject version, IRubyObject tags, IRubyObject implicit) {
         Integer[] versionInts = null;
         boolean implicitBool = implicit.isTrue();
-        Map<String, String> tagsMap = Collections.EMPTY_MAP;
+        Map<String, String> tagsMap = null;
 
         RubyArray versionAry = version.convertToArray();
         if (versionAry.size() == 2) {
