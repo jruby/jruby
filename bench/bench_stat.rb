24b481bb53c6859f21770cec6dd32403c33f3efd
Thomas E. Enebo <tom.enebo@gmail.com>
2012-05-09 16:33:57 -0500

    Update stat bench to do more

--- bench/bench_stat.rb
+++ bench/bench_stat.rb
@@ -5,8 +5,30 @@ puts "10k File.stat(dir)"
 10.times {
   puts Benchmark.measure {
     i = 0
-    while i < 10_000
+    while i < 30_000
       File.stat(tmp)
+      i = i + 1
+    end
+  }
+}
+
+puts "10k File.ctime(dir)"
+10.times {
+  puts Benchmark.measure {
+    i = 0
+    while i < 30_000
+      File.ctime(tmp)
+      i = i + 1
+    end
+  }
+}
+
+puts "10k File.mtime(dir)"
+10.times {
+  puts Benchmark.measure {
+    i = 0
+    while i < 30_000
+      File.mtime(tmp)
       i = i + 1
     end
   }
