b0f5eaceabe4b9265afc9887bbde8c07cbd25305
Wayne Meissner <wmeissner@gmail.com>
2012-05-15 07:13:57 +1000

    Remove FFI::TypeDefs, as it is now defined in java code

--- lib/ruby/shared/ffi/types.rb
+++ lib/ruby/shared/ffi/types.rb
@@ -28,7 +28,6 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 module FFI
-  TypeDefs = Hash.new
 
   def self.typedef(old, add)
     TypeDefs[add] = self.find_type(old)
