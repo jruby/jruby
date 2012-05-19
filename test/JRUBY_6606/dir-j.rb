49f59657bb3dccb71e13e3f39f817e9e1b478831
ryenus <ryenus@gmail.com>
2012-05-19 23:19:43 +0800

    align file name with content encoding

--- test/JRUBY_6606/dir-j.rb
+++ test/JRUBY_6606/dir-j.rb
@@ -1,6 +1,6 @@
-# encoding: Shift_JIS
+# encoding: EUC-JP
 
 Dir.chdir(File.dirname __FILE__) do
-  Dir.entries('res/‚ ‚¢').each{|n| p n unless n == '.' || n == '..'}
-  Dir.foreach('res/‚ ‚¢') {|n| p n unless n == '.' || n == '..'}
+  Dir.entries('res/¤¢¤¤').each{|n| p n unless n == '.' || n == '..'}
+  Dir.foreach('res/¤¢¤¤') {|n| p n unless n == '.' || n == '..'}
 end
--- test/JRUBY_6606/dir-js.rb
+++ test/JRUBY_6606/dir-js.rb
@@ -1,6 +1,6 @@
-# encoding: EUC-JP
+# encoding: Shift_JIS
 
 Dir.chdir(File.dirname __FILE__) do
-  Dir.entries('res/¤¢¤¤').each{|n| p n unless n == '.' || n == '..'}
-  Dir.foreach('res/¤¢¤¤') {|n| p n unless n == '.' || n == '..'}
+  Dir.entries('res/‚ ‚¢').each{|n| p n unless n == '.' || n == '..'}
+  Dir.foreach('res/‚ ‚¢') {|n| p n unless n == '.' || n == '..'}
 end
