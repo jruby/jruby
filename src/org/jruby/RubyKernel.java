0ed48885ea34b89fd30390742fb28fcd740eea3e
Marcin Mielzynski <lopx@gazeta.pl>
2012-05-27 22:22:23 +0200

    whoops, fix compat version

--- src/org/jruby/RubyKernel.java
+++ src/org/jruby/RubyKernel.java
@@ -1789,7 +1789,7 @@ public class RubyKernel {
         return ((RubyBasicObject)self).type();
     }
 
-    @JRubyMethod(name = "type", compat = RUBY1_9)
+    @JRubyMethod(name = "type", compat = RUBY1_8)
     public static RubyClass type_deprecated(IRubyObject self) {
         return ((RubyBasicObject)self).type_deprecated();
     }
