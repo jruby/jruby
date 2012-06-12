179d2485f47108cdda25a3d8be85e17dd435c372
Aliaksei Palkanau <lifusexitus@gmail.com>
2012-06-13 02:24:29 +0300

    IRPersistenceFacade#persist spec test is corresponds to current development stage

--- spec/ir/persistence/ir_persistence_facade_spec.rb
+++ spec/ir/persistence/ir_persistence_facade_spec.rb
@@ -1,27 +1,40 @@
 require 'java'
+require 'jruby'
 require 'fileutils.rb'
 
+java_import org.jruby.Ruby
+java_import org.jruby.RubyInstanceConfig
 java_import org.jruby.ast.Node
 java_import org.jruby.ir.IRBuilder
 java_import org.jruby.ir.IRManager
 java_import org.jruby.ir.IRScope
-java_import org.jruby.ir.persistence.IRPeristenceFacade
+java_import org.jruby.ir.persistence.IRPersistenceFacade
 
 # Fibbonacci example from http://www.engineyard.com/blog/2012/oss-grant-roundup-jruby-runtime/ is used here
-describe IRPeristenceFacade do
+describe IRPersistenceFacade do
   # Pathes
-  REFERENCE_DIR = "#{File.expand_path(File.dirname(__FILE__))}/reference"
-  REFERENCE_RUBY_FILE_PATH = "#{REFERENCE_DIR}/fib_ruby.rb"
+  CURRENT_DIR = File.expand_path(File.dirname(__FILE__))
+  REFERENCE_DIR = "#{CURRENT_DIR}/reference"
+  RB_FILE_NAME = "fib_ruby.rb"
+  REFERENCE_RUBY_FILE_PATH = "#{REFERENCE_DIR}/#{RB_FILE_NAME}"
   REFERENCE_IR_FILE_PATH = "#{REFERENCE_DIR}/fib_ir.ir"
   EXPECTED_CREATED_IR_FOLDER_PATH = "#{REFERENCE_DIR}/ir"
   EXPECTED_CREATED_IR_FILE_PATH = "#{EXPECTED_CREATED_IR_FOLDER_PATH}/fib_ruby.ir"
 
-  # Produce IRScope from .rb file
+  # Produce Ruby runtime and IRScope from .rb file
   before :all do
-    is_command_line_script = false
-    ast = IRBuilder.buildAST(is_command_line_script, REFERENCE_RUBY_FILE_PATH) 
-    manager = IRManager.new
-    @scope = IRBuilder.createIRBuilder(manager).buildRoot(ast)
+    instance_config = RubyInstanceConfig.new
+    instance_config.script_file_name = RB_FILE_NAME
+    instance_config.current_directory = REFERENCE_DIR
+
+    @runtime = Ruby.new_instance(instance_config)
+
+    rb_file_content = File.read(REFERENCE_RUBY_FILE_PATH)
+    ast = JRuby.parse(rb_file_content)
+    manager = @runtime.ir_manager
+    manager.dry_run = true
+    is_19 = false
+    @scope = IRBuilder.createIRBuilder(manager, is_19).buildRoot(ast)
   end
 
   # Clean up
@@ -34,12 +47,7 @@ describe IRPeristenceFacade do
 
   describe '.persist' do    
     before :all do
-      IRPersistenceFacade.persist(@scope)
-    end
-
-    it 'should produce IR as output' do
-      output = IRPersistence.persist(@scope)
-      output.should == @scope.toStringInstrs
+      IRPersistenceFacade.persist(@scope, @runtime)
     end
     
     it 'should create .ir file in ir directory relatively to rb file' do
@@ -47,13 +55,8 @@ describe IRPeristenceFacade do
     end
 
     it 'should create file identical to the reference one' do
-      FileUtils.identical?(REFERENCE_RUBY_FILE_PATH, EXPECTED_CREATED_IR_FILE_PATH).should be_true
+      FileUtils.identical?(REFERENCE_IR_FILE_PATH, EXPECTED_CREATED_IR_FILE_PATH).should be_true
     end
-  end
-
-  # not quite sure whether we need IRPersistenceFacade.execute method or IRPersistenceFacade.read will be enough
-  describe '.execute' do
-    it 'should execute persisted code with expected result'
   end
 
   describe '.read' do
