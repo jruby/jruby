12da208989d7ac96337a35c2b36687e3b430cd8d
Charles Oliver Nutter <headius@headius.com>
2012-06-27 14:47:21 -0500

    Guard 1.9-only regression spec against 1.8 mode.

--- spec/regression/JRUBY-6740_custom_respond_to_on_BasicObject_spec.rb
+++ spec/regression/JRUBY-6740_custom_respond_to_on_BasicObject_spec.rb
@@ -1,11 +1,13 @@
 require 'rspec'
 
-describe "A custom respond_to? on a BasicObject subclass" do
-  it "should not try to invoke respond_to_missing? on false result" do
-    cls = Class.new(BasicObject) do
-      def respond_to?(meth); false; end
-    end
+if RUBY_VERSION =~ /1\.9/
+  describe "A custom respond_to? on a BasicObject subclass" do
+    it "should not try to invoke respond_to_missing? on false result" do
+      cls = Class.new(BasicObject) do
+        def respond_to?(meth); false; end
+      end
 
-    cls.new.respond_to?(:hello).should == false
+      cls.new.respond_to?(:hello).should == false
+    end
   end
 end
