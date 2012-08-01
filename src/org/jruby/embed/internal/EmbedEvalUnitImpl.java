5fe8b451407ff0a08170be8bb630129d7e67e90a
Uwe Kubosch <uwe@kubosch.no>
2012-08-01 08:58:02 +0200

    * Added temporary debug output to track JRUBY-6792

--- src/org/jruby/embed/internal/EmbedEvalUnitImpl.java
+++ src/org/jruby/embed/internal/EmbedEvalUnitImpl.java
@@ -110,6 +110,8 @@ public class EmbedEvalUnitImpl implements EmbedEvalUnit {
         try {
             if (sharing_variables) {
                 vars.inject(scope, 0, null);
+                // FIXME(uwe): Temporary debug output to track JRUBY-6792
+                System.out.println("Push scope: " + Thread.currentThread() + ", " + scope);
                 runtime.getCurrentContext().pushScope(scope);
             }
             IRubyObject ret;
@@ -137,6 +139,8 @@ public class EmbedEvalUnitImpl implements EmbedEvalUnit {
         } finally {
             if (sharing_variables) {
                 runtime.getCurrentContext().popScope();
+                // FIXME(uwe): Temporary debug output to track JRUBY-6792
+                System.out.println("Pop scope: " + Thread.currentThread() + ", " + scope);
             }
             vars.terminate();
             /* Below lines doesn't work. Neither does classCache.flush(). How to clear cache?
