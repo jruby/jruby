01a0a734ba5f8f3fe1b090594d66d40a12d58c9b
Charles Oliver Nutter <headius@headius.com>
2012-08-03 11:56:28 -0500

    Changes to invokedynamic enabling.
    * On Java 6, on only if explicitly turned on (for JSR292 backport)
    * On OpenJDK Java 7, on only if explicitly turned on (temporarily)
    * On OpenJDK Java 8, on by default unless turned off

--- src/org/jruby/RubyInstanceConfig.java
+++ src/org/jruby/RubyInstanceConfig.java
@@ -1529,9 +1529,18 @@ public class RubyInstanceConfig {
     
     public static final boolean USE_INVOKEDYNAMIC;
     static {
-        if (JAVA_VERSION == Opcodes.V1_7) {
-            // if on Java 7, on by default unless turned off
-            // TODO: turned off temporarily due to the lack of 100% working OpenJDK indy support
+        boolean isHotspot =
+                SafePropertyAccessor.getProperty("java.vm.name", "").contains("Hotspot") ||
+                        SafePropertyAccessor.getProperty("java.vm.name", "").contains("OpenJDK");
+
+        String version = SafePropertyAccessor.getProperty("java.specification.version", "1.6");
+        
+        if (isHotspot && version.equals("1.7")) {
+            // if on OpenJDK 7, on by default unless turned off
+            // TODO: turned off temporarily due to the lack of 100% working OpenJDK7 indy support
+            USE_INVOKEDYNAMIC = Options.COMPILE_INVOKEDYNAMIC.load() && Options.COMPILE_INVOKEDYNAMIC.isSpecified();
+        } else if (isHotspot && version.equals("1.8")) {
+            // OpenJDK 8 will have the new 100% working logic soon, so we enable by default
             USE_INVOKEDYNAMIC = Options.COMPILE_INVOKEDYNAMIC.load();
         } else {
             // if not on Java 7, on only if explicitly turned on
