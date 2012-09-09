0889a96a0063499ad00fc9c29f5fc936f3a88569
Prathamesh Sonpatki <prathamesh.sonpatki@betterlabs.net>
2012-09-09 18:07:36 +0530

    Added test for checking class of newly created object

--- spec/ir/directed_graph/directed_graph_spec.rb
+++ spec/ir/directed_graph/directed_graph_spec.rb
@@ -5,4 +5,8 @@ import 'org.jruby.ir.util.DirectedGraph'
 # This is spec for Directed Graph Library
 
 describe "Directed Graph Utility" do
+  it "should create object of DirectedGraph" do
+    graph = DirectedGraph.new
+    graph.class.should == Java::OrgJrubyIrUtil::DirectedGraph
+  end
 end
