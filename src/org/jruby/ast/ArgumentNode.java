8a309ad1578e1ed8b6f985f16c501adfb39307bb
Charles Oliver Nutter <headius@headius.com>
2012-09-16 22:21:38 -0700

    The accept method should never raise.

--- src/org/jruby/ast/ArgumentNode.java
+++ src/org/jruby/ast/ArgumentNode.java
@@ -60,7 +60,7 @@ public class ArgumentNode extends Node implements INameNode {
     }
     
     public Object accept(NodeVisitor visitor) {
-        throw new RuntimeException("ArgumentNode should never be evaluated");
+        return null;
     }
 
     /**
