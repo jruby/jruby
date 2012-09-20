d72d2b7c7a627d8bbda2a0be193dd9ca416a0e17
Charles Oliver Nutter <headius@headius.com>
2012-09-20 09:55:57 -0500

    Add GC::Profiler.report logic.

--- src/jruby/kernel19/gc.rb
+++ src/jruby/kernel19/gc.rb
@@ -57,45 +57,167 @@ module GC
     all_stats
   end
 
-  module Profiler
-    def self.enabled?
-      @gc_beans != nil
-    end
+  begin
+    require 'java'
+    java.lang.Class.forName('com.sun.management.GarbageCollectionNotificationInfo')
 
-    def self.enable
-      require 'java'
-      @gc_beans ||= java.lang.management.ManagementFactory.garbage_collector_mx_beans
-      clear
-    end
+    # class exists, proceed with GC notification version
+    module Profiler
+      HEADER = "   ID  Type                      Timestamp(sec)    Before(kB)     After(kB)    Delta(kB)        Heap(kB)          GC Time(ms) "
+      FORMAT = "%5d  %-20s %19.4f %13i %13i %12i %15i %20.10f"
 
-    def self.disable
-      @gc_beans = nil
-    end
+      class GCListener
+        include javax.management.NotificationListener
 
-    def self.clear
-      return unless @gc_beans
+        def initialize
+          @lines = []
+        end
 
-      time = 0
-      @gc_beans.each do |gc_bean|
-        time += gc_bean.collection_time
+        attr_accessor :lines
+
+        def handleNotification(notification, o)
+          lines << notification
+        end
+
+        def clear
+          lines.clear
+        end
       end
-      @start_time = time
-    end
 
-    def self.result
-      nil
-    end
-
-    def self.report
-      nil
-    end
-
-    def self.total_time
-      time = 0
-      @gc_beans.each do |gc_bean|
-        time += gc_bean.collection_time
+      def self.enabled?
+        @gc_beans != nil
       end
-      (time - @start_time) / 1000.0
+
+      def self.enable
+        require 'java'
+        @gc_listener ||= GCListener.new
+        java.lang.management.ManagementFactory.garbage_collector_mx_beans.each do |gc_bean|
+          gc_bean.add_notification_listener @gc_listener, nil, nil
+        end
+      end
+
+      def self.disable
+        require 'java'
+        java.lang.management.ManagementFactory.garbage_collector_mx_beans.each do |gc_bean|
+          gc_bean.remove_notification_listener @gc_listener
+        end
+      end
+
+      def self.clear
+        @gc_listener.clear
+      end
+
+      def self.report
+        puts result
+      end
+
+      def self.result
+        lines = @gc_listener.lines.dup
+
+        counts = Hash.new(0)
+        report_lines = []
+
+        lines.each_with_index do |line, i|
+          report_lines << HEADER if i % 20 == 0
+
+          gc_notification = com.sun.management.GarbageCollectionNotificationInfo.from(line.user_data)
+          gc_info = gc_notification.gc_info
+
+          mem_before = gc_info.memory_usage_before_gc
+          mem_after = gc_info.memory_usage_after_gc
+
+          after = 0
+          before = 0
+          commit = 0
+
+          mem_after.entry_set.each do |entry|
+            name = entry.key
+
+            before_usage = mem_before[name]
+            before += before_usage.used
+
+            after_usage = entry.value
+            after += after_usage.used
+
+            commit += after_usage.committed
+          end
+
+          counts[gc_notification.gc_name] += 1
+
+          report_lines << sprintf(
+                   FORMAT,
+                   gc_info.id,
+                   gc_notification.gc_name,
+                   gc_info.start_time/1000000.0,
+                   before / 1024,
+                   after / 1024,
+                   (before - after) / 1024,
+                   commit / 1024,
+                   gc_info.duration/1000.0)
+        end
+
+        "GC: #{counts.map{|k,v| "#{v} #{k}"}.join(', ')}\n" + report_lines.join("\n")
+      end
+
+      def self.total_time
+        duration = 0
+
+        @gc_listener.lines.each_with_index do |line, i|
+          puts HEADER if i % 20 == 0
+
+          gc_notification = com.sun.management.GarbageCollectionNotificationInfo.from(line.user_data)
+          gc_info = gc_notification.gc_info
+
+          duration += gc_info.duration
+        end
+
+        return duration / 1000.0
+      end
+    end
+
+  rescue java.lang.ClassNotFoundException # loading GarbageCollectionNotificationInfo
+
+    # No GC notifications, use polled version
+    module Profiler
+      def self.enabled?
+        @gc_beans != nil
+      end
+
+      def self.enable
+        require 'java'
+        @gc_beans ||= java.lang.management.ManagementFactory.garbage_collector_mx_beans
+        clear
+      end
+
+      def self.disable
+        @gc_beans = nil
+      end
+
+      def self.clear
+        return unless @gc_beans
+
+        time = 0
+        @gc_beans.each do |gc_bean|
+          time += gc_bean.collection_time
+        end
+        @start_time = time
+      end
+
+      def self.result
+        nil
+      end
+
+      def self.report
+        nil
+      end
+
+      def self.total_time
+        time = 0
+        @gc_beans.each do |gc_bean|
+          time += gc_bean.collection_time
+        end
+        (time - @start_time) / 1000.0
+      end
     end
   end
 end
