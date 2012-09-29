4a96cefe2ff353d47e148d2ac2942006ab9ab46e
Charles Oliver Nutter <headius@headius.com>
2012-09-28 21:45:28 -0500

    Mask 1.9-specific regression spec.

--- spec/regression/JRUBY-6510_hash_treat_as_encoding_spec.rb
+++ spec/regression/JRUBY-6510_hash_treat_as_encoding_spec.rb
@@ -1,16 +1,18 @@
 # encoding: UTF-8
 require 'rspec'
 
-describe 'JRUBY-6510: String.encode!' do
-  it 'should also accept a hash as the only argument' do
-    enc_dft_in = Encoding.default_internal
-    begin
-      Encoding.default_internal = 'ISO-8859-1'
-      s = "äöü"
-      s.encode!({:invalid => :replace, :undef => :replace})
-      s.encoding.name.should == 'ISO-8859-1'
-    ensure
-      Encoding.default_internal = enc_dft_in unless enc_dft_in.nil?
+unless RUBY_VERSION =~ /1\.8/
+  describe 'JRUBY-6510: String.encode!' do
+    it 'should also accept a hash as the only argument' do
+      enc_dft_in = Encoding.default_internal
+      begin
+	Encoding.default_internal = 'ISO-8859-1'
+	s = "äöü"
+	s.encode!({:invalid => :replace, :undef => :replace})
+	s.encoding.name.should == 'ISO-8859-1'
+      ensure
+	Encoding.default_internal = enc_dft_in unless enc_dft_in.nil?
+      end
     end
   end
 end
