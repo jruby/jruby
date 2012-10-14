bc587272d3a7c89f78ab1a19d7d219f17b8e7791
Hiro Asari <asari.ruby@gmail.com>
2012-10-13 23:28:09 -0400

    The spec needs to return outside of RSpec example in order to avoid returning a failure.

--- spec/regression/gh-299_env_with_nil_value_spec.rb
+++ spec/regression/gh-299_env_with_nil_value_spec.rb
@@ -1,14 +1,14 @@
 require 'rspec'
 
+# in 1.8, IO.popen does not take the first array as additional ENV.
+return if RUBY_VERSION =~ /\A1\.8/
+
 describe "IO.popen" do
-  context "with ENV with nil as value" do
+  context "with ENV which has nil as value" do
     it "ignores the environment variable" do
       env = {
         "ANYTHING_NIL" => nil
       }
-      
-      # in 1.8, IO.popen does not take the first array as additional ENV.
-      return if RUBY_VERSION =~ /\A1\.8/
       
       lambda {
         IO.popen([env, 'true'], "r") do |x|
