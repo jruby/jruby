ca3eba20a7815a355c9a18e2eaf61c02d1753a5f
Hiro Asari <asari.ruby@gmail.com>
2012-10-13 23:05:23 -0400

    This spec should run only in the 1.9 mode.

--- spec/regression/gh-299_env_with_nil_value_spec.rb
+++ spec/regression/gh-299_env_with_nil_value_spec.rb
@@ -1,14 +1,17 @@
 require 'rspec'
 
 describe "IO.popen" do
-  context "when ENV with nil as key value" do
+  context "with ENV with nil as value" do
     it "ignores the environment variable" do
       env = {
         "ANYTHING_NIL" => nil
       }
       
+      # in 1.8, IO.popen does not take the first array as additional ENV.
+      return if RUBY_VERSION =~ /\A1\.8/
+      
       lambda {
-        IO.popen([env, 'jruby -v'], "r") do |x|
+        IO.popen([env, 'true'], "r") do |x|
           ENV.has_key?("ANYTHING_NIL").should be_false
         end
         }.should_not raise_error
