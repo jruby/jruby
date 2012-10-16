55364a2ca35f9af2d039761af168967ae222d07a
Charles Oliver Nutter <headius@headius.com>
2012-10-16 17:04:14 -0500

    Add comments about the limitations of our current io/console impl.

--- lib/ruby/shared/io/console.rb
+++ lib/ruby/shared/io/console.rb
@@ -1,3 +1,24 @@
+# This implementation of io/console is a little hacky. It shells out to `stty`
+# for most operations, which does not work on Windows, in secured environments,
+# and so on. In addition, because on Java 6 we can't actually launch
+# subprocesses with tty control, stty will not actually manipulate the
+# controlling terminal.
+#
+# For platforms where shelling to stty does not work, most operations will
+# just be pass-throughs. This allows them to function, but does not actually
+# change any tty flags.
+#
+# Finally, since we're using stty to shell out, we can only manipulate stdin/
+# stdout tty rather than manipulating whatever terminal is actually associated
+# with the IO we're calling against. This will produce surprising results if
+# anyone is actually using io/console against non-stdio ttys...but that case
+# seems like it would be pretty rare.
+#
+# Note: we are incorporating this into 1.7.0 since RubyGems uses io/console
+# when pushing gems, in order to mask the password entry. Worst case is that
+# we don't actually disable echo and the password is shown...we will try to
+# do a better version of this in 1.7.1.
+
 warn "io/console on JRuby shells out to stty for most operations"
 
 # attempt to call stty; if failure, fall back on stubbed version
