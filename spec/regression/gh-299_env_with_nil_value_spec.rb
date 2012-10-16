e4039a60264ee7c58cba0953590e337cd1a8de8e
Hiro Asari <asari.ruby@gmail.com>
2012-10-15 21:28:11 -0400

    While the previous version did not present any problem, it should have raised *some* sort of Exception. (See http://bugs.jruby.org/6940.)

--- spec/regression/gh-299_env_with_nil_value_spec.rb
+++ spec/regression/gh-299_env_with_nil_value_spec.rb
@@ -1,21 +1,22 @@
 require 'rspec'
 
 # in 1.8, IO.popen does not take the first array as additional ENV.
-return if RUBY_VERSION =~ /\A1\.8/
+unless RUBY_VERSION =~ /\A1\.8/
 
-describe "IO.popen" do
-  context "with ENV which has nil as value" do
-    it "ignores the environment variable" do
-      env = {
-        "ANYTHING_NIL" => nil
-      }
+  describe "IO.popen" do
+    context "with ENV which has nil as value" do
+      it "ignores the environment variable" do
+        env = {
+          "ANYTHING_NIL" => nil
+        }
       
-      lambda {
-        IO.popen([env, 'true'], "r") do |x|
-          ENV.has_key?("ANYTHING_NIL").should be_false
-        end
-        }.should_not raise_error
+        lambda {
+          IO.popen([env, 'true'], "r") do |x|
+            ENV.has_key?("ANYTHING_NIL").should be_false
+          end
+          }.should_not raise_error
+      end
     end
   end
-end
 
+end
