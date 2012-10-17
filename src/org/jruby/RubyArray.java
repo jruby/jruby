2d823634e054a559faecc56285446f5e327ba67c
Charles Oliver Nutter <headius@headius.com>
2012-10-16 22:11:06 -0500

    Add a common == path for Array, to avoid respond_to? checks on nil

--- src/org/jruby/RubyArray.java
+++ src/org/jruby/RubyArray.java
@@ -1933,12 +1933,17 @@ public class RubyArray extends RubyObject implements List, RandomAccess {
     @JRubyMethod(name = "==", required = 1)
     @Override
     public IRubyObject op_equal(ThreadContext context, IRubyObject obj) {
+        Ruby runtime = context.runtime;
+
         if (this == obj) {
-            return context.runtime.getTrue();
+            return runtime.getTrue();
         }
+
         if (!(obj instanceof RubyArray)) {
+            if (obj == context.nil) return runtime.getFalse();
+
             if (!obj.respondsTo("to_ary")) {
-                return context.runtime.getFalse();
+                return runtime.getFalse();
             }
             return RuntimeHelpers.rbEqual(context, obj, this);
         }
