64279e6184e87461e69cbe5cda55dd4205690c0e
Charles Oliver Nutter <headius@headius.com>
2012-10-18 15:02:04 -0500

    These specs only run properly under 1.9 mode.

--- spec/regression/JRUBY-6952_time_plus_minus_rounding_errors_spec.rb
+++ spec/regression/JRUBY-6952_time_plus_minus_rounding_errors_spec.rb
@@ -1,12 +1,14 @@
 require 'rspec'
 
-describe 'Time#+' do
-  it 'does not introduce rounding errors' do
-    time = Time.new(2012, 10, 17, 23, 59, 59, 999999.9)
-    time_p1 = time + 1
-    time_p1_m1 = time_p1 - 1
+if RUBY_VERSION > '1.9'
+  describe 'Time#+' do
+    it 'does not introduce rounding errors' do
+      time = Time.new(2012, 10, 17, 23, 59, 59, 999999.9)
+      time_p1 = time + 1
+      time_p1_m1 = time_p1 - 1
 
-    (time_p1.to_i - time.to_i).should == 1
-    time_p1_m1.to_i.should == time.to_i
+      (time_p1.to_i - time.to_i).should == 1
+      time_p1_m1.to_i.should == time.to_i
+    end
   end
 end
