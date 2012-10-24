428a3dd6252907ced2b415beab9dadcf1f8fd1b0
Wayne Meissner <wmeissner@gmail.com>
2012-10-25 01:36:14 +1100

    Restore IO#winsize and IO#winsize= functionality for darwin by using the stty fallback.

--- lib/ruby/1.9/io/console.rb
+++ lib/ruby/1.9/io/console.rb
@@ -227,11 +227,17 @@ if RbConfig::CONFIG['host_os'].downcase =~ /darwin/
         getc
       end
 
+      # Not all systems return same format of stty -a output
+      IEEE_STD_1003_2 = '(?<rows>\d+) rows; (?<columns>\d+) columns'
+      UBUNTU = 'rows (?<rows>\d+); columns (?<columns>\d+)'
+
       def winsize
-        [25, 80]
+        match = `stty -a`.match(/#{IEEE_STD_1003_2}|#{UBUNTU}/)
+        [match[:rows].to_i, match[:columns].to_i]
       end
 
       def winsize=(size)
+        `stty rows #{size[0]} cols #{size[1]}`
       end
 
       def iflush
