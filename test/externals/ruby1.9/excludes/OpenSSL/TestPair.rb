2842f66392ff38b822e37f9bc1ecfbb8b4788714
Charles Oliver Nutter <headius@headius.com>
2012-10-24 19:55:30 -0500

    Remove more passing OpenSSL tests for read/write nonblock.

--- test/externals/ruby1.9/excludes/OpenSSL/TestPair.rb
+++ test/externals/ruby1.9/excludes/OpenSSL/TestPair.rb
@@ -1,4 +1,2 @@
 exclude :test_connect_accept_nonblock, 'needs investigation'
-exclude :test_read_nonblock, 'needs investigation'
-exclude :test_write_nonblock_with_buffered_data, 'needs investigation'
 exclude :test_readpartial, 'broken in read_partial? started failing with recent test update'
