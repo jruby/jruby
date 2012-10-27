6019e419de9fab4d30594edfda14d6a204a3b01e
Charles Oliver Nutter <headius@headius.com>
2012-10-26 23:49:20 -0500

    Link jruby-openssl version.rb to jruby proper.

--- gems/jruby-openssl/lib/shared/jopenssl/version.rb
+++
@@ -1,5 +0,0 @@
-module Jopenssl
-  module Version
-    VERSION = "0.8.0.pre1"
-  end
-end
--- lib/ruby/shared/jopenssl/version.rb
+++ lib/ruby/shared/jopenssl/version.rb
@@ -1,5 +1,5 @@
 module Jopenssl
   module Version
-    VERSION = "0.7.5.dev"
+    VERSION = "0.8.0.pre2"
   end
 end
