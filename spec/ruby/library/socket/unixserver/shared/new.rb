5a01e6d3f3cd3f5abf5b241576dc492e331a8401
Charles Oliver Nutter <headius@headius.com>
2012-11-04 10:35:06 -0700

    Cleanup UNIXServer.new spec a bit, add spec for open with block.

--- spec/ruby/library/socket/unixserver/shared/new.rb
+++ spec/ruby/library/socket/unixserver/shared/new.rb
@@ -4,13 +4,21 @@ require 'tempfile'
 
 describe :unixserver_new, :shared => true do
   platform_is_not :windows do
+    before :each do
+      @path = tmp("unixserver_spec")
+      File.unlink(@path) if File.exists?(@path)
+    end
+
+    after :each do
+      @server.close if @server
+      @server = nil
+      File.unlink(@path) rescue nil
+    end
+
     it "creates a new UNIXServer" do
-      path = tmp("unixserver_spec")
-      File.unlink(path) if File.exists?(path)
-      unix = UNIXServer.send(@method, path)
-      unix.path.should == path
-      unix.addr.should == ["AF_UNIX", path]
-      File.unlink(path)
+      @server = UNIXServer.send(@method, @path)
+      @server.path.should == @path
+      @server.addr.should == ["AF_UNIX", @path]
     end
   end
 end
