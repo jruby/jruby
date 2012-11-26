843bd786748e94384b7aef1d9e8ab0e815087d21
Hiro Asari <asari.ruby@gmail.com>
2012-11-25 22:14:07 -0500

    Array#delete should return the last object *inside* the array, not the one that is given as the argument to it, even when the objects are equal via '=='.
    This addresses #411.

--- src/org/jruby/RubyArray.java
+++ src/org/jruby/RubyArray.java
@@ -2459,13 +2459,17 @@ public class RubyArray extends RubyObject implements List, RandomAccess {
     @JRubyMethod(required = 1)
     public IRubyObject delete(ThreadContext context, IRubyObject item, Block block) {
         int i2 = 0;
+        IRubyObject value = item;
 
         Ruby runtime = context.runtime;
         for (int i1 = 0; i1 < realLength; i1++) {
             // Do not coarsen the "safe" check, since it will misinterpret AIOOBE from equalInternal
             // See JRUBY-5434
             IRubyObject e = safeArrayRef(values, begin + i1);
-            if (equalInternal(context, e, item)) continue;
+            if (equalInternal(context, e, item)) {
+                value = e;
+                continue;
+            }
             if (i1 != i2) store(i2, e);
             i2++;
         }
@@ -2492,7 +2496,7 @@ public class RubyArray extends RubyObject implements List, RandomAccess {
             concurrentModification();
         }
 
-        return item;
+        return value;
     }
 
     /** rb_ary_delete_at
