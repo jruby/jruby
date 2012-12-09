0ff28f03331c86998ee7364fc090e5f43888f815
Hiro Asari <asari.ruby@gmail.com>
2012-12-08 22:13:07 -0500

    At some point, 1.8 also got the support for '%-d' where '-' means "drop the leading zero".
    Fixes #433.

--- src/org/jruby/util/RubyDateFormat.java
+++ src/org/jruby/util/RubyDateFormat.java
@@ -391,12 +391,10 @@ public class RubyDateFormat extends DateFormat {
     }
 
     private int addOutputFormatter(String pattern, int index) {
-        if (ruby_1_9) {
-            TimeOutputFormatter outputFormatter = TimeOutputFormatter.getFormatter(pattern.substring(index - 1));
-            if (outputFormatter != null) {
-                index += outputFormatter.getFormatter().length();
-                compiledPattern.add(new Token(FORMAT_OUTPUT, outputFormatter));
-            }
+        TimeOutputFormatter outputFormatter = TimeOutputFormatter.getFormatter(pattern.substring(index - 1));
+        if (outputFormatter != null) {
+            index += outputFormatter.getFormatter().length();
+            compiledPattern.add(new Token(FORMAT_OUTPUT, outputFormatter));
         }
         return index;
     }
