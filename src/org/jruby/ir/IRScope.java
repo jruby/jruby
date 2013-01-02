jruby/jruby

commit b95700de9e5a183f7da8d44a4454682bb8aa67f4

[IR] Improve upon c73cc11 - add uncaught break handling on demand.
* Commit c73cc11 added global rescue to catch uncaught breaks
  in lambda closures and throw a local-jump-error.  But, this is
  a little excessive and impacts performance some for the
  interpreter.  Updated code to add the global rescue only when
  the interpreter encounters a lambda.  This leaves correctness
  intact and eliminates the try-catch on the vast majority of
  closures in ruby code.

* This opt. is not yet available to the JIT code as of this patch.
  To be explored later.

* Recovers lost performance from commit c73cc11.

src/org/jruby/ir/IRScope.java
@@ -637,7 +637,7 @@ public abstract class IRScope {
         // SSS FIXME: Why is this again?  Document this weirdness!
         // Forcibly clear out the shared eval-scope variable allocator each time this method executes
         initEvalScopeVariableAllocator(true); 
-        
+
         // SSS FIXME: We should configure different optimization levels
         // and run different kinds of analysis depending on time budget.  Accordingly, we need to set
         // IR levels/states (basic, optimized, etc.) and the
@@ -651,7 +651,15 @@ public abstract class IRScope {
     }
 
     /** Run any necessary passes to get the IR ready for interpretation */
-    public synchronized Instr[] prepareForInterpretation() {
+    public synchronized Instr[] prepareForInterpretation(boolean isLambda) {
+        if (isLambda) {
+            // Add a global ensure block to catch uncaught breaks
+            // and throw a LocalJumpError.
+            if (((IRClosure)this).addGEBForUncaughtBreaks()) {
+                this.relinearizeCFG = true;
+            }
+        }
+
         checkRelinearization();
 
         if (linearizedInstrArray != null) return linearizedInstrArray;
@@ -668,6 +676,17 @@ public abstract class IRScope {
     public Tuple<Instr[], Map<Integer,Label[]>> prepareForCompilation() {
         // Build CFG and run compiler passes, if necessary
         if (getCFG() == null) runCompilerPasses();
+
+        // Add this always since we dont re-JIT a previously
+        // JIT-ted closure.  But, check if there are other
+        // smarts available to us and eliminate adding this
+        // code to every closure there is.
+        //
+        // Add a global ensure block to catch uncaught breaks
+        // and throw a LocalJumpError.
+        if (((IRClosure)this).addGEBForUncaughtBreaks()) {
+            this.relinearizeCFG = true;
+        }
 
         try {
             buildLinearization(); // FIXME: compiler passes should have done this

src/org/jruby/internal/runtime/methods/InterpretedIRMethod.java
@@ -66,7 +66,7 @@ public class InterpretedIRMethod extends DynamicMethod implements IRMethodArgs, 
     public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject[] args, Block block) {
         // SSS FIXME: Move this out of here to some other place?
         // Prepare method if not yet done so we know if the method has an explicit/implicit call protocol
-        if (method.getInstrsForInterpretation() == null) method.prepareForInterpretation();
+        if (method.getInstrsForInterpretation() == null) method.prepareForInterpretation(false);
 
         if (IRRuntimeHelpers.isDebug()) {
             // FIXME: name should probably not be "" ever.

src/org/jruby/ir/IRBuilder.java
@@ -795,30 +795,6 @@ public class IRBuilder {
         return UnexecutableNil.U_NIL;
     }
 
-    // These two methods could have been DRY-ed out if we had closures.
-    // For now, just duplicating code.
-    protected void catchUncaughtBreakInLambdas(IRClosure s) {
-        Label rBeginLabel = s.getNewLabel();
-        Label rEndLabel   = s.getNewLabel();
-        Label rescueLabel = s.getNewLabel();
-
-        // protect the entire body as it exists now with the global ensure block
-        s.addInstrAtBeginning(new ExceptionRegionStartMarkerInstr(rBeginLabel, rEndLabel, null, rescueLabel));
-        s.addInstr(new ExceptionRegionEndMarkerInstr());
-
-        // Receive exceptions (could be anything, but the handler only processes IRBreakJumps)
-        s.addInstr(new LabelInstr(rescueLabel));
-        Variable exc = s.getNewTemporaryVariable();
-        s.addInstr(new ReceiveExceptionInstr(exc, false));  // no type-checking
-
-        // Handle break using runtime helper
-        // --> IRRuntimeHelpers.catchUncaughtBreakInLambdas(context, scope, bj, blockType)
-        s.addInstr(new RuntimeHelperCall(null, "catchUncaughtBreakInLambdas", new Operand[]{exc} ));
-
-        // End
-        s.addInstr(new LabelInstr(rEndLabel));
-    }
-
     private void handleNonlocalReturnInMethod(IRScope s) {
         Label rBeginLabel = s.getNewLabel();
         Label rEndLabel   = s.getNewLabel();
@@ -2130,8 +2106,6 @@ public class IRBuilder {
             closure.addInstr(new ReturnInstr(closureRetVal));
         }
 
-        // No need to add "catchUncaughtBreakInLambdas" since this closure cannot be a lambda!
-
         return new WrappedIRClosure(closure);
     }
 
@@ -2285,10 +2259,6 @@ public class IRBuilder {
         if (closureRetVal != U_NIL) { // can be U_NIL if the node is an if node with returns in both branches.
             closure.addInstr(new ReturnInstr(closureRetVal));
         }
-
-        // SSS FIXME: We can skip this if the closure has no calls
-        // since breaks can never get here in that case.
-        catchUncaughtBreakInLambdas(closure);
 
         return new WrappedIRClosure(closure);
     }

src/org/jruby/ir/IRBuilder19.java
@@ -375,6 +375,35 @@ public class IRBuilder19 extends IRBuilder {
         return ret;
     }
 
+/* ------------------------------------------------------------------
+ * This code is added on demand at runtime in the interpreter code.
+ * For JIT, this may have to be added always!
+
+    // These two methods could have been DRY-ed out if we had closures.
+    // For now, just duplicating code.
+    private void catchUncaughtBreakInLambdas(IRClosure s) {
+        Label rBeginLabel = s.getNewLabel();
+        Label rEndLabel   = s.getNewLabel();
+        Label rescueLabel = s.getNewLabel();
+
+        // protect the entire body as it exists now with the global ensure block
+        s.addInstrAtBeginning(new ExceptionRegionStartMarkerInstr(rBeginLabel, rEndLabel, null, rescueLabel));
+        s.addInstr(new ExceptionRegionEndMarkerInstr());
+
+        // Receive exceptions (could be anything, but the handler only processes IRBreakJumps)
+        s.addInstr(new LabelInstr(rescueLabel));
+        Variable exc = s.getNewTemporaryVariable();
+        s.addInstr(new ReceiveExceptionInstr(exc, false));  // no type-checking
+
+        // Handle break using runtime helper
+        // --> IRRuntimeHelpers.catchUncaughtBreakInLambdas(context, scope, bj, blockType)
+        s.addInstr(new RuntimeHelperCall(null, "catchUncaughtBreakInLambdas", new Operand[]{exc} ));
+
+        // End
+        s.addInstr(new LabelInstr(rEndLabel));
+    }
+ * ------------------------------------------------------------------ */
+
     public Operand buildLambda(LambdaNode node, IRScope s) {
         IRClosure closure = new IRClosure(manager, s, false, node.getPosition().getStartLine(), node.getScope(), Arity.procArityOf(node.getArgs()), node.getArgumentType(), true);
         s.addClosure(closure);
@@ -395,7 +424,8 @@ public class IRBuilder19 extends IRBuilder {
         // can be U_NIL if the node is an if node with returns in both branches.
         if (closureRetVal != U_NIL) closure.addInstr(new ReturnInstr(closureRetVal));
 
-        catchUncaughtBreakInLambdas(closure);
+        // Added as part of 'prepareForInterpretation' code.
+        // catchUncaughtBreakInLambdas(closure);
 
         Variable lambda = s.getNewTemporaryVariable();
         s.addInstr(new BuildLambdaInstr(lambda, closure, node.getPosition()));

src/org/jruby/ir/IRClosure.java
@@ -16,7 +16,10 @@ import org.jruby.ir.operands.TemporaryClosureVariable;
 import org.jruby.ir.operands.Variable;
 import org.jruby.ir.instructions.Instr;
 import org.jruby.ir.instructions.ReceiveArgBase;
+import org.jruby.ir.instructions.ReceiveExceptionInstr;
 import org.jruby.ir.instructions.ReceiveRestArgBase;
+import org.jruby.ir.instructions.RuntimeHelperCall;
+import org.jruby.ir.representations.BasicBlock;
 import org.jruby.ir.representations.CFG;
 import org.jruby.ir.transformations.inlining.InlinerInfo;
 import org.jruby.parser.StaticScope;
@@ -45,6 +48,8 @@ public class IRClosure extends IRScope {
     /** The parameter names, for Proc#parameters */
     private String[] parameterList;
 
+    public boolean addedGEBForUncaughtBreaks;
+
     /** Used by cloning code */
     private IRClosure(IRClosure c, IRScope lexicalParent) {
         super(c, lexicalParent);
@@ -54,6 +59,7 @@ public class IRClosure extends IRScope {
         this.endLabel = getNewLabel(getName() + "_END");
         this.body = (c.body instanceof InterpretedIRBlockBody19) ? new InterpretedIRBlockBody19(this, c.body.arity(), c.body.getArgumentType())
                                                                  : new InterpretedIRBlockBody(this, c.body.arity(), c.body.getArgumentType());
+        this.addedGEBForUncaughtBreaks = false;
     }
 
     public IRClosure(IRManager manager, IRScope lexicalParent, boolean isForLoopBody,
@@ -267,5 +273,42 @@ public class IRClosure extends IRScope {
         clonedClosure.setCFG(getCFG().cloneForCloningClosure(clonedClosure, ii));
 
         return clonedClosure;
+    }
+
+    // Add a global-ensure-block to catch uncaught breaks
+    // This is usually required only if this closure is being
+    // used as a lambda, but it is safe to add this for any closure
+
+    protected boolean addGEBForUncaughtBreaks() {
+        // Nothing to do if already done
+        if (addedGEBForUncaughtBreaks) {
+            return false;
+        }
+
+        CFG        cfg = cfg();
+        BasicBlock geb = cfg.getGlobalEnsureBB();
+        if (geb == null) {
+            geb = new BasicBlock(cfg, new Label("_GLOBAL_ENSURE_BLOCK"));
+            Variable exc = getNewTemporaryVariable();
+            geb.addInstr(new ReceiveExceptionInstr(exc, false)); // No need to check type since it is not used before rethrowing
+            // Handle uncaught break using runtime helper
+            // --> IRRuntimeHelpers.catchUncaughtBreakInLambdas(context, scope, bj, blockType)
+            geb.addInstr(new RuntimeHelperCall(null, "catchUncaughtBreakInLambdas", new Operand[]{exc} ));
+            cfg.addGlobalEnsureBB(geb);
+        } else {
+            // SSS FIXME: Assumptions:
+            //
+            // First instr is a 'ReceiveExceptionInstr'
+            // Last instr is a 'ThrowExceptionInstr'
+
+            List<Instr> instrs = geb.getInstrs();
+            Variable exc = ((ReceiveExceptionInstr)instrs.get(0)).getResult();
+            instrs.set(instrs.size(), new RuntimeHelperCall(null, "catchUncaughtBreakInLambdas", new Operand[]{exc} ));
+        }
+
+        // Update scope
+        addedGEBForUncaughtBreaks = true;
+
+        return true;
     }
 }

src/org/jruby/ir/interpreter/Interpreter.java
@@ -98,7 +98,7 @@ public class Interpreter {
         StaticScope ss = rootNode.getStaticScope();
         IRScope containingIRScope = getEvalContainerScope(runtime, ss);
         IREvalScript evalScript = IRBuilder.createIRBuilder(runtime.getIRManager(), is_1_9).buildEvalRoot(ss, containingIRScope, file, lineNumber, rootNode);
-        evalScript.prepareForInterpretation();
+        evalScript.prepareForInterpretation(false);
 //        evalScript.runCompilerPass(new CallSplitter());
         ThreadContext context = runtime.getCurrentContext();
         runBeginEndBlocks(evalScript.getBeginBlocks(), context, self, null); // FIXME: No temp vars yet right?
@@ -120,7 +120,7 @@ public class Interpreter {
 
         for (IRClosure b: beBlocks) {
             // SSS FIXME: Should I piggyback on WrappedIRClosure.retrieve or just copy that code here?
-            b.prepareForInterpretation();
+            b.prepareForInterpretation(false);
             Block blk = (Block)(new WrappedIRClosure(b)).retrieve(context, self, context.getCurrentScope(), temp);
             blk.yield(context, null);
         }
@@ -339,7 +339,7 @@ public class Interpreter {
         Instr[] instrs = scope.getInstrsForInterpretation();
 
         // The base IR may not have been processed yet
-        if (instrs == null) instrs = scope.prepareForInterpretation();
+        if (instrs == null) instrs = scope.prepareForInterpretation(blockType == Block.Type.LAMBDA);
 
         int temporaryVariablesSize = scope.getTemporaryVariableSize();
         Object[] temp = temporaryVariablesSize > 0 ? new Object[temporaryVariablesSize] : null;

commit a4cadd74f6f5da8e5861562f1bc205e0060f9838

[IR] Fix IR JIT breakage from b95700d
* Was missing an instanceof check. Fixed now.

src/org/jruby/ir/IRScope.java
@@ -684,7 +684,7 @@ public abstract class IRScope {
         //
         // Add a global ensure block to catch uncaught breaks
         // and throw a LocalJumpError.
-        if (((IRClosure)this).addGEBForUncaughtBreaks()) {
+        if (this instanceof IRClosure && ((IRClosure)this).addGEBForUncaughtBreaks()) {
             this.relinearizeCFG = true;
         }
 