bb62491a4b42198fec403e5778830854a9b9e237
the8472 <github@infinite-source.de>
2013-01-04 14:24:31 +0100

    improve spec and bench

--- spec/regression/GH-476_ivar_concurrency_spec.rb
+++ spec/regression/GH-476_ivar_concurrency_spec.rb
@@ -4,9 +4,11 @@ describe 'Accessing instance variables' do
   it 'should not lose concurrent writes under growth operations' do
     lost_writes = false  
   
-    (0..1000).each do |i|
+    (0..100).each do |i|
       clazz = Class.new
       object = clazz.new
+      
+      mutate = true
       
       # probing thread
       t1 = Thread.new do
@@ -18,9 +20,10 @@ describe 'Accessing instance variables' do
       end
       
       # mutating thread
-      t2 = Thread.new{(0..10000).each{object.instance_variable_set(:"@bar_#{rand(100000)}",1)}}
+      t2 = Thread.new{(0..10000).each{break unless mutate;object.instance_variable_set(:"@bar_#{rand(100000)}",1)}}
       
       t1.join
+      mutate = false
       t2.join
       
       break if lost_writes
