22c0cf4b5d3af1966b77c754381c02c3c932ceae
Isaiah Peng <isaiah.peng@vcint.com>
2013-01-09 10:18:27 +0100

    fix grammar & typo in gh-482 regression spec

--- spec/regression/gh-482_array_zip_cycle.rb
+++ spec/regression/gh-482_array_zip_cycle.rb
@@ -5,17 +5,17 @@ describe "zip an array with" do
     [1,2].zip([0].cycle).should == [[1,0], [2,0]]
   end
 
-  it "infinite enum append block is yield with corrent argument" do
+  it "infinite enum, appending block is yielded with correct argument" do
     arr = []
     [1,2].zip([0].cycle){|a| arr << a}
     arr.should == [[1,0], [2,0]]
   end
 
   it "another array returns a correct new array" do
-    [1,2].zip([0]).should == [[1,0],[2,nil]]
+    [1,2].zip([0]).should == [[1,0], [2,nil]]
   end
 
-  it "another array append block is yield with corrent argument" do
+  it "another array, appending block is yielded with correct argument" do
     arr = []
     [1,2].zip([0]){|a| arr << a}
     arr.should == [[1,0], [2,nil]]
