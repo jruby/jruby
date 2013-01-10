99585732e9099c814901f96ba36c30f2483eaf0d
Hiro Asari <asari.ruby@gmail.com>
2013-01-09 20:30:29 -0500

    Unused import

--- src/org/jruby/RubyInstanceConfig.java
+++ src/org/jruby/RubyInstanceConfig.java
@@ -39,8 +39,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.lang.NumberFormatException;
-import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
