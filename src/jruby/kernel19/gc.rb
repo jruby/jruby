a66de225c1db5445c6a174d8f87d94e483323528
Charles Oliver Nutter <headius@headius.com>
2013-05-14 00:25:26 -0500

    Fix and refactor slightly GC and GC::Profiler.

--- src/jruby/kernel19/gc.rb
+++ src/jruby/kernel19/gc.rb
@@ -1,74 +1,72 @@
 module GC
   def self.stat(all_stats = {})
-    if defined? ::Java
-      gc_beans = java.lang.management.ManagementFactory.garbage_collector_mx_beans
-      pool_beans = {}
-      java.lang.management.ManagementFactory.memory_pool_mx_beans.each do |pool_bean|
-        pool_beans[pool_bean.name] = pool_bean
-      end
-
-      all_stats.merge!({
-          :count => 0,
-          :time => 0,
-          :committed => 0.0,
-          :init => 0.0,
-          :max => 0.0,
-          :used => 0.0,
-          :peak_committed => 0.0,
-          :peak_init => 0.0,
-          :peak_max => 0.0,
-          :peak_used => 0.0,
-          :last_committed => 0.0,
-          :last_init => 0.0,
-          :last_max => 0.0,
-          :last_used => 0.0
-      })
-
-      gc_beans.each do |gc_bean|
-        gc_stats = all_stats[gc_bean.name] = {}
-        all_stats[:count] += gc_stats[:count] = gc_bean.collection_count
-        all_stats[:time] += gc_stats[:time] = gc_bean.collection_time
-
-        gc_bean.memory_pool_names.each do |pool_name|
-          pool_bean = pool_beans[pool_name]
-
-          all_pools = gc_stats[:pools] = {}
-          pool_stats = all_pools[pool_name] = {}
-
-          usage = pool_bean.usage
-          peak_usage = pool_bean.peak_usage
-          last_usage = pool_bean.collection_usage
-
-          all_stats[:committed] += pool_stats[:committed] = usage.committed
-          all_stats[:init] += pool_stats[:init] = usage.init
-          all_stats[:max] += pool_stats[:max] = usage.max
-          all_stats[:used] += pool_stats[:used] = usage.used
-          all_stats[:peak_committed] += pool_stats[:peak_committed] = peak_usage.committed
-          all_stats[:peak_init] += pool_stats[:peak_init] = peak_usage.init
-          all_stats[:peak_max] += pool_stats[:peak_max] = peak_usage.max
-          all_stats[:peak_used] += pool_stats[:peak_used] = peak_usage.used
-          all_stats[:last_committed] += pool_stats[:last_committed] = last_usage.committed
-          all_stats[:last_init] += pool_stats[:last_init] = last_usage.init
-          all_stats[:last_max] += pool_stats[:last_max] = last_usage.max
-          all_stats[:last_used] += pool_stats[:last_used] = last_usage.used
-        end
-      end
-
-      all_stats
-    else
-      []
+    gc_beans = java.lang.management.ManagementFactory.garbage_collector_mx_beans
+    pool_beans = {}
+    java.lang.management.ManagementFactory.memory_pool_mx_beans.each do |pool_bean|
+      pool_beans[pool_bean.name] = pool_bean
     end
+
+    all_stats.merge!({
+        :count => 0,
+        :time => 0,
+        :committed => 0.0,
+        :init => 0.0,
+        :max => 0.0,
+        :used => 0.0,
+        :peak_committed => 0.0,
+        :peak_init => 0.0,
+        :peak_max => 0.0,
+        :peak_used => 0.0,
+        :last_committed => 0.0,
+        :last_init => 0.0,
+        :last_max => 0.0,
+        :last_used => 0.0
+    })
+
+    gc_beans.each do |gc_bean|
+      gc_stats = all_stats[gc_bean.name] = {}
+      all_stats[:count] += gc_stats[:count] = gc_bean.collection_count
+      all_stats[:time] += gc_stats[:time] = gc_bean.collection_time
+
+      gc_bean.memory_pool_names.each do |pool_name|
+        pool_bean = pool_beans[pool_name]
+
+        all_pools = gc_stats[:pools] = {}
+        pool_stats = all_pools[pool_name] = {}
+
+        usage = pool_bean.usage
+        peak_usage = pool_bean.peak_usage
+        last_usage = pool_bean.collection_usage
+
+        all_stats[:committed] += pool_stats[:committed] = usage.committed
+        all_stats[:init] += pool_stats[:init] = usage.init
+        all_stats[:max] += pool_stats[:max] = usage.max
+        all_stats[:used] += pool_stats[:used] = usage.used
+        all_stats[:peak_committed] += pool_stats[:peak_committed] = peak_usage.committed
+        all_stats[:peak_init] += pool_stats[:peak_init] = peak_usage.init
+        all_stats[:peak_max] += pool_stats[:peak_max] = peak_usage.max
+        all_stats[:peak_used] += pool_stats[:peak_used] = peak_usage.used
+        all_stats[:last_committed] += pool_stats[:last_committed] = last_usage.committed
+        all_stats[:last_init] += pool_stats[:last_init] = last_usage.init
+        all_stats[:last_max] += pool_stats[:last_max] = last_usage.max
+        all_stats[:last_used] += pool_stats[:last_used] = last_usage.used
+      end
+    end
+
+    all_stats
   end
 
   begin
     module Profiler
       def self.__setup__
-        java.lang.Class.forName('com.sun.management.GarbageCollectionNotificationInfo')
+        @profiler ||= begin
+          java.lang.Class.forName('com.sun.management.GarbageCollectionNotificationInfo')
 
-        # class exists, proceed with GC notification version
-        @profiler = NotifiedProfiler.new
-      rescue java.lang.ClassNotFoundException
-        @profiler = NormalProfiler.new
+          # class exists, proceed with GC notification version
+          @profiler = NotifiedProfiler.new
+        rescue java.lang.ClassNotFoundException
+          @profiler = NormalProfiler.new
+        end
       end
 
       def self.enabled?
@@ -76,32 +74,32 @@ module GC
         @profiler.enabled?
       end
 
-      def enable
+      def self.enable
         __setup__
         @profiler.enable
       end
 
-      def disable
+      def self.disable
         __setup__
         @profiler.disable
       end
 
-      def clear
+      def self.clear
         __setup__
         @profiler.clear
       end
 
-      def result
+      def self.result
         __setup__
         @profiler.result
       end
 
-      def report
+      def self.report
         __setup__
         @profiler.report
       end
 
-      def total_time
+      def self.total_time
         __setup__
         @profiler.total_time
       end
@@ -135,7 +133,6 @@ module GC
           end
 
           def enable
-            require 'java'
             @gc_listener ||= GCListener.new
             java.lang.management.ManagementFactory.garbage_collector_mx_beans.each do |gc_bean|
               gc_bean.add_notification_listener @gc_listener, nil, nil
@@ -143,14 +140,13 @@ module GC
           end
 
           def disable
-            require 'java'
             java.lang.management.ManagementFactory.garbage_collector_mx_beans.each do |gc_bean|
               gc_bean.remove_notification_listener @gc_listener
             end
           end
 
           def clear
-            @gc_listener.clear
+            @gc_listener.clear if @gc_listener
           end
 
           def report
@@ -158,6 +154,8 @@ module GC
           end
 
           def result(out = "")
+            return nil unless @gc_listener
+            
             lines = @gc_listener.lines.dup
 
             counts = Hash.new(0)
@@ -217,7 +215,7 @@ module GC
               gc_info = gc_notification.gc_info
 
               duration += gc_info.duration
-            end
+            end if @gc_listener
 
             return duration / 1000.0
           end
