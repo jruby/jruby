3a0c60a2c974d27321fa17ca21a922a2d4874430
Alex Tambellini <atambellini@gmail.com>
2013-05-18 11:51:39 -0400

    Add date tests to mri 1.9 test suite

---
+++ test/externals/ruby1.9/excludes/TestDateConv.rb
@@ -0,0 +1,1 @@
+exclude :test_to_time__from_datetime, "needs investigation"
---
+++ test/externals/ruby1.9/excludes/TestSH.rb
@@ -0,0 +1,3 @@
+exclude :test_marshal, "needs investigation"
+exclude :test_period2, "needs investigation"
+exclude :test_strftime, "needs investigation"
