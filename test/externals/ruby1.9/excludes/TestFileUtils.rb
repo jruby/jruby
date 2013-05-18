222bc97b45757affbdbe3cb63a685a5004e27455
Alex Tambellini <atambellini@gmail.com>
2013-05-18 16:04:41 -0400

    Add FileUtils tests to mri 1.9 index

---
+++ test/externals/ruby1.9/excludes/TestFileUtils.rb
@@ -0,0 +1,4 @@
+exclude :test_copy_entry_symlink, "needs investigation"
+exclude :test_cp_r_symlink, "needs investigation"
+exclude :test_ln_s, "needs investigation"
+exclude :test_ln_sf, "needs investigation"
