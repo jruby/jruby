b667922a91c8ee49c5aaf86619b76ddf2e7bdc8d
Alex Tambellini <atambellini@gmail.com>
2013-05-18 15:49:26 -0400

    Add erb tests to mri 1.9 index

---
+++ test/externals/ruby1.9/excludes/TestERB.rb
@@ -0,0 +1,4 @@
+exclude :test_concurrent_default_binding, "needs investigation"
+exclude :test_safe_04, "needs investigation"
+exclude :test_literal_encoding, "needs investigation"
+exclude :test_result_encoding, "needs investigation"
