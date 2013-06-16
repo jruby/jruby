a4751edfad3e3557c13e93695e4f06bec5e23657
Hiro Asari <asari.ruby@gmail.com>
2013-06-16 00:23:59 -0400

    Spec for #801

---
+++ spec/regression/gh-801_date_inspect_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+
+describe 'Date#inspect' do
+  subject { Time.new(2013,4,22,14,18,05,'-05:00') }
+
+  it 'returns correct offset' do
+    subject.inspect.should == "2013-04-22 04:18:05 -0500"
+  end
+  
+end
