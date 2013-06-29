ac6dd13150dc5dd12d90d32c90b25f2513b0e416
Charles Oliver Nutter <headius@headius.com>
2013-06-29 08:50:19 -0400

    Fix jline require in readline.rb.

--- lib/ruby/shared/readline.rb
+++ lib/ruby/shared/readline.rb
@@ -1,5 +1,6 @@
 # load jline and our readline into classpath
 begin
+  require File.dirname(__FILE__) + "/readline/jline-2.11.jar"
   require File.dirname(__FILE__) + "/readline/readline.jar"
 rescue LoadError
   # try to proceed as though classes are already in classloader
