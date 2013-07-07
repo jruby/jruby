161ee3b95447061efd0fc2f500230f5ba9ae1ccd
Benoit Daloze <eregontp@gmail.com>
2013-07-07 20:03:49 +0200

    Pathname hybrid PR #857 fixed 2 examples

--- test/externals/ruby1.9/excludes/TestPathname.rb
+++ test/externals/ruby1.9/excludes/TestPathname.rb
@@ -1,5 +1,3 @@
 exclude :test_open, "needs investigation"
 exclude :test_realdirpath, "needs investigation"
 exclude :test_realpath, "needs investigation"
-exclude :test_rename, "needs investigation"
-exclude :test_lchmod, "needs investigation"
