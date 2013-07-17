511057149bca73213e27ce94764b88a7e5d25d81
Alex Tambellini <atambellini@gmail.com>
2013-07-17 19:18:02 -0400

    Remove excludes for passing encoding tests

--- test/externals/ruby1.9/excludes/TestEncoding.rb
+++ test/externals/ruby1.9/excludes/TestEncoding.rb
@@ -1,4 +1,3 @@
 exclude :test_find, "needs investigation"
 exclude :test_replicate, "needs investigation"
 exclude :test_unsafe, "needs investigation"
-exclude :test_compatible_p, "needs investigation"
