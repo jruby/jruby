120ef5bdad725e27d3a2ec28fca114508fa15087
Benoit Daloze <eregontp@gmail.com>
2013-09-02 17:33:25 +0200

    account only for significant digits in ConvertDouble

--- core/src/main/java/org/jruby/util/ConvertDouble.java
+++ core/src/main/java/org/jruby/util/ConvertDouble.java
@@ -53,11 +53,22 @@ public class ConvertDouble {
         private boolean is19;
         private char[] chars; // result string we use to parse.
         private int charsIndex;
+        private int significantDigitsProcessed;
+        private int adjustExponent;
+        private boolean wroteExponent;
+        private double result;
+
         // To match MRI so that we end up rounding on values greater than being storable in a 64
         // bit value in the same fashion we are using 30.  Based on wikipedia a note recommends 
         //17 + 3 (or 20), but I don't know why that is recommended and we would rather be compatible 
         //behaviorally with MRI.
-        private final int DECIMAL_IGNORE_LIMIT = 30;  
+        private final static int SIGNIFICANT_DIGITS_LIMIT = 30;
+        // Maximal exponent is +308, minimal -308, -324 with subnormal float
+        private final static int EXPONENT_DIGITS_LIMIT = 3;
+        private final static int MAX_EXPONENT = (int) (Math.pow(10, EXPONENT_DIGITS_LIMIT)) - 1;
+        // Max length is '-' + significant_digits + '.' + 'E' + '-' + exponent_digits
+        // + 1 (first digit before point) + 1 (first decimal digit).
+        private final static int MAX_LENGTH = SIGNIFICANT_DIGITS_LIMIT + EXPONENT_DIGITS_LIMIT + 6;
 
         public DoubleConverter() {}
 
@@ -67,8 +78,16 @@ public class ConvertDouble {
             endIndex = index + list.length();
             this.isStrict = isStrict;
             this.is19 = is19;
-            chars = new char[list.length()+1]; // +1 for implicit '0' for terminating 'E'
+            // +2 for added exponent: E...
+            // The algorithm trades digits for inc/dec exponent.
+            // Worse case is adding E-1 when no exponent,
+            // it trades one digit for 3 chars.
+            chars = new char[Math.min(list.length()+2, MAX_LENGTH)];
             charsIndex = 0;
+            significantDigitsProcessed = 0;
+            adjustExponent = 0;
+            wroteExponent = false;
+            result = -1.0; // -1.0 is "parse String"
         }
 
         private byte next() {
@@ -81,16 +100,20 @@ public class ConvertDouble {
          */
         private boolean previous() {
             index--;
-            
             return false;
-        }
-
-        private boolean isDigit(byte b) {
-            return b >= '0' && b <= '9';
         }
 
         private boolean isEOS() {
             return index >= endIndex;
+        }
+
+        private boolean stopParsing() {
+            index = endIndex;
+            return true;
+        }
+
+        private boolean isDigit(byte b) {
+            return b >= '0' && b <= '9';
         }
 
         private boolean isExponent(byte b) {
@@ -103,6 +126,13 @@ public class ConvertDouble {
 
         private void addToResult(byte b) {
             chars[charsIndex++] = (char) b;
+        }
+
+        private void addExponentToResult(int exponent) {
+            String exp = Integer.toString(exponent);
+            for (int i = 0; i < exp.length(); i++) {
+                addToResult((byte) exp.charAt(i));
+            }
         }
 
         private boolean eatUnderscores() {
@@ -121,22 +151,28 @@ public class ConvertDouble {
         }
 
         private double completeCalculation() {
-            // ENEBO: Not sure about the '-' check here.
-            if (charsIndex == 0 || (charsIndex == 1 && chars[0] == '-')) {
-                if (isStrict) strictError(); // Strict requires at least one digit.
-                return 0.0;
+            if (charsIndex == 0 || (charsIndex == 1 && chars[0] == '-')) { // "" or "-"
+                strictError(); // Strict requires at least one digit.
+                return 0.0; // Treated as 0.0 (not -0.0) in non-strict.
             } else if (isExponent((byte) chars[charsIndex - 1])) { // Covers 12.0efrog
-                if (isStrict) strictError();
-                addToResult((byte) '0');
+                strictError();
+                addExponentToResult(adjustExponent);
             } else if (isStrict && !isEOS()) {
                 strictError(); // We know it is not whitespace at this point
+            } else if (!wroteExponent && adjustExponent != 0) {
+                addToResult((byte) 'E');
+                addExponentToResult(adjustExponent);
             }
 
-            return SafeDoubleParser.parseDouble(new String(chars));
+            return SafeDoubleParser.parseDouble(new String(chars, 0, charsIndex));
         }
         
-        private void strictError() {
-            throw new NumberFormatException("does not meet strict criteria");
+        private boolean strictError() {
+            if (isStrict) {
+                throw new NumberFormatException("does not meet strict criteria");
+            } else {
+                return true; // means EOS for non-strict
+            }
         }
 
         public double parse(ByteList list, boolean strict, boolean is19) {
@@ -146,6 +182,10 @@ public class ConvertDouble {
             if (parseOptionalSign()) return completeCalculation();
 
             parseDigits();
+
+            if (result != -1.0) { // abnormal result
+                return result;
+            }
 
             return completeCalculation();
         }
@@ -185,7 +225,11 @@ public class ConvertDouble {
             if (!isEOS()) {
                 byte value = next();
 
-               if (isDigit(value)) {
+                if (isDigit(value)) {
+                    // Always add the first digit, in case it is the only digit
+                    if (value != '0') {
+                        significantDigitsProcessed++;
+                    }
                     addToResult(value);
                 } else if (value == '.') {
                     addToResult(value);
@@ -193,7 +237,7 @@ public class ConvertDouble {
                 } else {
                     return isEOS();
                 }
-            } else if (isStrict) {
+            } else {
                 strictError();
             }
             
@@ -201,7 +245,15 @@ public class ConvertDouble {
                 byte value = next();
 
                 if (isDigit(value)) {
-                    addToResult(value);
+                    if (significantDigitsProcessed < SIGNIFICANT_DIGITS_LIMIT) {
+                        // Ignore leading 0s
+                        if (value != '0' || significantDigitsProcessed > 0) {
+                            significantDigitsProcessed++;
+                            addToResult(value);
+                        }
+                    } else {
+                        adjustExponent++;
+                    }
                 } else if (value == '.') {
                     addToResult(value);
                     return parseDecimalDigits();
@@ -213,9 +265,7 @@ public class ConvertDouble {
                 } else if (isWhitespace(value)) {
                     return skipWhitespace();
                 } else {
-                    if (isStrict) strictError();
-                    
-                    return isEOS();
+                    return strictError();
                 }
  
             }
@@ -224,39 +274,43 @@ public class ConvertDouble {
         }
 
         private boolean parseDecimalDigits() {
-            if (isEOS()) {
-                if (isStrict) strictError();
-                
-                return true;
-            }
+            if (isEOS()) return strictError();
             
-            int digitsProcessed = 0;
             byte value = next();
 
+            // Wonky, but 12._2e2 is 1200.0 and 12.__e2 is 12.0
+            if (value == '_') {
+                strictError();
+                if (isEOS()) return strictError();
+                value = next();
+            }
+
             if (isDigit(value)) {
-                if (++digitsProcessed <= DECIMAL_IGNORE_LIMIT) addToResult(value);
-            } else if (value == '_') {
-                if (isStrict) strictError();
-                while (!isEOS()) {    // Wonky, but 12._2e2 is 1200.0 and 12.__e2 is 12.0
-                    value = next();
-                    
-                    if (isDigit(value)) {
-                        if (++digitsProcessed <= DECIMAL_IGNORE_LIMIT) addToResult(value);
-                        break;
-                    } else if (value != '_') {
-                        return true;
+                if (significantDigitsProcessed < SIGNIFICANT_DIGITS_LIMIT) {
+                    if (!(value == '0' && significantDigitsProcessed == 0)) { // not a leading 0
+                        significantDigitsProcessed++;
                     }
+                } else {
+                    value = '0';
                 }
+                // Always add a digit after the .
+                addToResult(value);
             } else {
-                if (isStrict) strictError();
-                return true; // Found garbage must be at end of string.
+                return strictError();
             } 
 
             while (!isEOS()) {
                 value = next();
 
                 if (isDigit(value)) {
-                    if (++digitsProcessed <= DECIMAL_IGNORE_LIMIT) addToResult(value);
+                    if (significantDigitsProcessed < SIGNIFICANT_DIGITS_LIMIT) {
+                        if (value == '0' && significantDigitsProcessed == 0) { // leading 0
+                            adjustExponent--;
+                        } else {
+                            significantDigitsProcessed++;
+                            addToResult(value);
+                        }
+                    } // otherwise ignore it
                 } else if (isExponent(value)) {
                     addToResult(value);
                     return parseExponent();
@@ -265,9 +319,7 @@ public class ConvertDouble {
                 } else if (isWhitespace(value)) {
                     return skipWhitespace();
                 } else {
-                    if (isStrict) strictError();
-
-                    return true;
+                    return strictError();
                 }
             }
 
@@ -279,43 +331,73 @@ public class ConvertDouble {
 
             byte value = next();
 
-            if (value == '-' || value == '+') {
-                addToResult(value);
-            } else {
-                previous();
+            int exponent = 0;
+            int digits = 0;
+            boolean negative = false;
+
+            if (value == '-') {
+                negative = true;
+            } else if (value != '+') {
+                previous(); // backup...not a sign-char
             }
 
             while (!isEOS()) {
                 value = next();
 
                 if (isDigit(value)) {
-                    addToResult(value);
+                    if (digits < EXPONENT_DIGITS_LIMIT) {
+                        // Ignore leading 0s
+                        if (value != '0' || digits > 0) {
+                            digits++;
+                            exponent = 10 * exponent + (value - '0');
+                        }
+                    } else {
+                        return tooLargeExponent(chars[0] == '-', negative);
+                    }
                 } else if (isWhitespace(value)) {
                     return skipWhitespace();
                 } else if (value == '_') {
                     verifyNumberAfterUnderscore();
                 } else {
-                    if (isStrict) strictError();
-
-                    return true;
+                    return strictError();
                 }
             } 
 
-            return true; // Exponent end of double...let's finish.
+            if (negative) {
+                exponent = -exponent;
+            }
+
+            exponent += adjustExponent;
+
+            if (-MAX_EXPONENT <= exponent && exponent <= MAX_EXPONENT) {
+                addExponentToResult(exponent);
+                wroteExponent = true;
+                return true; // Exponent end of double...let's finish.
+            } else {
+                return tooLargeExponent(chars[0] == '-', negative);
+            }
+        }
+
+        private boolean tooLargeExponent(boolean negativeFloat, boolean negativeExponent) {
+            if (negativeExponent) {
+                if (negativeFloat) {
+                    result = -0.0;
+                } else {
+                    result = 0.0;
+                }
+            } else {
+                if (negativeFloat) {
+                    result = Double.NEGATIVE_INFINITY;
+                } else {
+                    result = Double.POSITIVE_INFINITY;
+                }
+            }
+            return stopParsing();
         }
 
         private void verifyNumberAfterUnderscore() {
-            if (isStrict) {
-                if (isEOS()) strictError();
-
-                byte value = next();
-                
-                if (!isDigit(value)) {
-                    previous();
-                    strictError();
-                } else {
-                    addToResult(value);
-                }
+            if (isStrict && (isEOS() || !isDigit(bytes[index]))) {
+                strictError();
             }
         }
     }
