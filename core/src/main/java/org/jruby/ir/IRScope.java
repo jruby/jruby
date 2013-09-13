9eefc2739cee7b3384b41dca08b5880776f19af1
Thomas E. Enebo <tom.enebo@gmail.com>
2013-09-13 16:28:17 -0500

    HEH double equals == double good

--- core/src/main/java/org/jruby/ir/IRScope.java
+++ core/src/main/java/org/jruby/ir/IRScope.java
@@ -368,14 +368,6 @@ public abstract class IRScope {
         return scopeId == ((IRScope) other).scopeId;
     }
     
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) return false;
-        if (getClass() != obj.getClass()) return false;
-        final IRScope other = (IRScope) obj;
-        return this.scopeId == other.scopeId;
-    }
-    
     protected void addChildScope(IRScope scope) {
         lexicalChildren.add(scope);
     }
