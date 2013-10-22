adc0307f5ccd2455045b104e85908dff35b81f14
Charles Oliver Nutter <headius@headius.com>
2013-10-22 15:55:22 -0500

    Do not define specialized backtick when ProcessBuilder is unavail.
    Fixes #1148.

--- core/src/main/ruby/jruby/kernel.rb
+++ core/src/main/ruby/jruby/kernel.rb
@@ -9,4 +9,13 @@ load 'jruby/kernel/signal.rb'
 
 # Java 7 process launching support
 spec_version = ENV_JAVA['java.specification.version']
-load 'jruby/kernel/jruby/process_manager.rb' if spec_version && spec_version >= '1.7'
+
+# GH-1148: ProcessBuilder is not available on GAE
+begin
+  # Try to access ProcessBuilder; if it fails, don't define our special process logic
+  java.lang.ProcessBuilder
+  load 'jruby/kernel/jruby/process_manager.rb' if spec_version && spec_version >= '1.7'
+rescue Exception
+  warn "ProcessBuilder unavailable; using default backtick" if $VERBOSE
+  # leave old backtick logic in place
+end
