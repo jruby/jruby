dc697e74474f7895f48b5b053e44afbebb43fa4e
Charles Oliver Nutter <headius@headius.com>
2013-10-22 17:56:55 -0500

    Sync up jruby-openssl bits with master.

--- lib/ruby/shared/jopenssl/load.rb
+++ lib/ruby/shared/jopenssl/load.rb
@@ -14,7 +14,9 @@ require 'jruby'
 require 'jopenssl.jar'
 org.jruby.ext.openssl.OSSLLibrary.new.load(JRuby.runtime, false)
 
-if RUBY_VERSION >= '1.9.0'
+if RUBY_VERSION >= '2.1.0'
+  load('jopenssl21/openssl.rb')
+elsif RUBY_VERSION >= '1.9.0'
   load('jopenssl19/openssl.rb')
 else
   load('jopenssl18/openssl.rb')
