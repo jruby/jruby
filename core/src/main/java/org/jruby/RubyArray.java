4c1db9144ca9c02ef52fe4f506b45d5b0772a156
David Masover <masover@iastate.edu>
2013-12-02 20:30:55 -0600

    subList toIndex should be exclusive. Fixes #1274.
    Issue at https://github.com/jruby/jruby/issues/1274

--- core/src/main/java/org/jruby/RubyArray.java
+++ core/src/main/java/org/jruby/RubyArray.java
@@ -4193,7 +4193,7 @@ public class RubyArray extends RubyObject implements List, RandomAccess {
             throw new IndexOutOfBoundsException();
         }
         
-        IRubyObject subList = subseq(fromIndex, toIndex - fromIndex + 1);
+        IRubyObject subList = subseq(fromIndex, toIndex - fromIndex);
 
         return subList.isNil() ? null : (List) subList;
     }
