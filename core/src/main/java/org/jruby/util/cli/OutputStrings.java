2242a97b064616607cb65ca96a7cfcd72f86a293
Charles Oliver Nutter <headius@headius.com>
2014-01-09 13:53:12 -0600

    Fix alignment.

--- core/src/main/java/org/jruby/util/cli/OutputStrings.java
+++ core/src/main/java/org/jruby/util/cli/OutputStrings.java
@@ -82,7 +82,7 @@ public class OutputStrings {
                 .append("  -X+O        run with ObjectSpace enabled (reduces performance)\n")
                 .append("  -X-C        disable all compilation\n")
                 .append("  -X+C        force compilation of all scripts before they are run (except eval)\n")
-                .append("  -X+T   use Truffle\n");
+                .append("  -X+T        use Truffle\n");
 
         return sb.toString();
     }
