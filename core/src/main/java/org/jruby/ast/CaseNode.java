18279d8614a8f66a80b19abcf9ce521714f678c7
Charles Oliver Nutter <headius@headius.com>
2014-01-15 18:52:22 -0600

    Don't use iterator for walking nodes to avoid allocating one.

--- core/src/main/java/org/jruby/ast/CaseNode.java
+++ core/src/main/java/org/jruby/ast/CaseNode.java
@@ -122,7 +122,9 @@ public class CaseNode extends Node {
 
         context.pollThreadEvents();
 
-        for (Node child : cases.childNodes()) {
+        List<Node> cases = this.cases.childNodes();
+        for (int i = 0; i < cases.size(); i++) {
+            Node child = cases.get(i);
             WhenNode when = (WhenNode) child;
             ISourcePosition position = child.getPosition();
 
