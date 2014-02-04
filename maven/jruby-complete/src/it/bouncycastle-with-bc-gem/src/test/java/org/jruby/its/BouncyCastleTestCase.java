9b19f4b21204b3a21a3463f808e5013ca81e66ba
Christian Meier <m.kristian@web.de>
2014-02-04 16:05:21 +0000

    adjust the BC version for jruby-complete tests [skip ci]

--- maven/jruby-complete/src/it/bouncycastle-with-bc-gem/src/test/java/org/jruby/its/BouncyCastleTestCase.java
+++ maven/jruby-complete/src/it/bouncycastle-with-bc-gem/src/test/java/org/jruby/its/BouncyCastleTestCase.java
@@ -23,7 +23,7 @@ public class BouncyCastleTestCase {
     @Test
     public void ruby(){
         ScriptingContainer container = new ScriptingContainer();
-        Object result = container.parse( "gem 'bouncy-castle-java', '1.5.0146'; require 'bouncy-castle-java'; Java::OrgBouncycastleJceProvider::BouncyCastleProvider.new.info").run();
+        Object result = container.parse( "gem 'bouncy-castle-java', '1.5.0146.1'; require 'bouncy-castle-java'; Java::OrgBouncycastleJceProvider::BouncyCastleProvider.new.info").run();
         assertEquals( "BouncyCastle Security Provider v1.45", result.toString() );
     }
 }
