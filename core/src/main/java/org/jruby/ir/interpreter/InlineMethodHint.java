2da2b49f36022753dc7770b09b153337fbe9fd57
Subramanya Sastry <sss.lists@gmail.com>
2014-02-05 20:53:13 -0600

    [IR] Deleted unused class.

--- core/src/main/java/org/jruby/ir/interpreter/InlineMethodHint.java
+++
@@ -1,8 +0,0 @@
-package org.jruby.ir.interpreter;
-
-import org.jruby.ir.IRMethod;
-
-public class InlineMethodHint extends RuntimeException {
-   public final IRMethod inlineableMethod;
-   public InlineMethodHint(IRMethod m) { inlineableMethod = m; }
-}
