23e6c9bc6e4253cd2fe94a77aa069ab480b27d1d
Thomas E. Enebo <tom.enebo@gmail.com>
2014-02-07 15:04:00 -0600

    trivial remove unused import

--- core/src/main/java/org/jruby/ir/dataflow/DataFlowConstants.java
+++ core/src/main/java/org/jruby/ir/dataflow/DataFlowConstants.java
@@ -4,10 +4,6 @@ import org.jruby.ir.dataflow.analyses.LiveVariablesProblem;
 import org.jruby.ir.dataflow.analyses.LoadLocalVarPlacementProblem;
 import org.jruby.ir.dataflow.analyses.StoreLocalVarPlacementProblem;
 import org.jruby.ir.dataflow.analyses.UnboxableOpsAnalysisProblem;
-import org.jruby.ir.operands.Operand;
-import org.jruby.ir.operands.OperandType;
-import org.jruby.ir.operands.Variable;
-import org.jruby.ir.transformations.inlining.InlinerInfo;
 
 public class DataFlowConstants {
     public static final String LVP_NAME = LiveVariablesProblem.NAME;
