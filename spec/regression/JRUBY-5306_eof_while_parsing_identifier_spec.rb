3f61f29413cde87b90b7e8a922be4b1c60ed7caf
Charles Oliver Nutter <headius@headius.com>
2014-02-18 12:57:51 -0600

    Remove kcode logic not useful in 1.9+.

--- spec/regression/JRUBY-5306_eof_while_parsing_identifier_spec.rb
+++ spec/regression/JRUBY-5306_eof_while_parsing_identifier_spec.rb
@@ -1,10 +1,5 @@
 describe "Lexer" do
   it "should parse identifier at the end of stream" do
-    $KCODE, backup = "s", $KCODE if RUBY_VERSION < '1.9'
-    begin
-      eval("$a").should == nil
-    ensure
-      $KCODE = backup if RUBY_VERSION < '1.9'
-    end
+    eval("$a").should == nil
   end
 end
