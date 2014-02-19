jruby/jruby

commit 90eb9b7bd7c20268a40bebca60e45b543d0ac14d

[Truffle] - RubyModule#alias raises a noMethodError exception if the method doesn't exist

core/src/main/java/org/jruby/truffle/JRubyTruffleBridge.java
@@ -10,27 +10,24 @@
 package org.jruby.truffle;
 
 import com.oracle.truffle.api.CallTarget;
-import com.oracle.truffle.api.Source;
-import com.oracle.truffle.api.SourceSection;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.MaterializedFrame;
+import org.jruby.Ruby;
 import org.jruby.RubyBoolean;
 import org.jruby.RubyNil;
-import org.jruby.ast.Node;
 import org.jruby.ast.ArgsNode;
+import org.jruby.ast.Node;
 import org.jruby.internal.runtime.methods.DynamicMethod;
+import org.jruby.runtime.builtin.IRubyObject;
 import org.jruby.truffle.nodes.core.CoreMethodNodeManager;
 import org.jruby.truffle.nodes.methods.MethodDefinitionNode;
-import org.jruby.truffle.translator.TranslatorDriver;
-import org.jruby.truffle.runtime.*;
+import org.jruby.truffle.runtime.NilPlaceholder;
+import org.jruby.truffle.runtime.RubyArguments;
+import org.jruby.truffle.runtime.RubyContext;
+import org.jruby.truffle.runtime.RubyParserResult;
 import org.jruby.truffle.runtime.control.*;
 import org.jruby.truffle.runtime.core.array.RubyArray;
-
-import org.jruby.Ruby;
-import org.jruby.runtime.builtin.IRubyObject;
-
-import java.io.InputStream;
-import java.io.Reader;
+import org.jruby.truffle.translator.TranslatorDriver;
 
 public class JRubyTruffleBridge {
 

core/src/main/java/org/jruby/truffle/nodes/objects/OpenModuleNode.java
@@ -39,7 +39,7 @@ public class OpenModuleNode extends RubyNode {
         final RubyModule module = (RubyModule) definingModule.execute(frame);
         definitionMethod.executeMethod(frame).call(frame.pack(), module, null);
 
-        return NilPlaceholder.INSTANCE;
+        return module;
     }
 
 }

core/src/main/java/org/jruby/truffle/runtime/core/RubyModule.java
@@ -9,15 +9,22 @@
  */
 package org.jruby.truffle.runtime.core;
 
-import java.util.*;
+import com.oracle.truffle.api.Assumption;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.frame.Frame;
+import com.oracle.truffle.api.frame.FrameSlot;
+import com.oracle.truffle.api.frame.PackedFrame;
+import com.oracle.truffle.api.utilities.CyclicAssumption;
+import org.jruby.truffle.runtime.RubyContext;
+import org.jruby.truffle.runtime.control.RaiseException;
+import org.jruby.truffle.runtime.lookup.LookupFork;
+import org.jruby.truffle.runtime.lookup.LookupNode;
+import org.jruby.truffle.runtime.lookup.LookupTerminal;
+import org.jruby.truffle.runtime.methods.RubyMethod;
+import org.jruby.truffle.runtime.methods.Visibility;
+import org.jruby.truffle.runtime.objects.RubyBasicObject;
 
-import com.oracle.truffle.api.*;
-import com.oracle.truffle.api.frame.*;
-import com.oracle.truffle.api.utilities.*;
-import org.jruby.truffle.runtime.*;
-import org.jruby.truffle.runtime.lookup.*;
-import org.jruby.truffle.runtime.methods.*;
-import org.jruby.truffle.runtime.objects.*;
+import java.util.*;
 
 /**
  * Represents the Ruby {@code Module} class.
@@ -202,7 +209,7 @@ public class RubyModule extends RubyObject implements LookupNode {
 
         if (method == null) {
             CompilerDirectives.transferToInterpreter();
-            throw new RuntimeException("Couldn't alias as coudln't find " + oldName);
+            throw new RaiseException(getRubyClass().getContext().getCoreLibrary().noMethodError(oldName, toString()));
         }
 
         addMethod(method.withNewName(newName));

commit 9b40b2df455b50561649ea28f9fbb18cd649f282

[Truffle] - Making jruby/truffle work by command line again.
This was broken by the commit 90eb9b7bd7.

core/src/main/java/org/jruby/truffle/JRubyTruffleBridge.java
@@ -27,6 +27,7 @@ import org.jruby.truffle.runtime.RubyContext;
 import org.jruby.truffle.runtime.RubyParserResult;
 import org.jruby.truffle.runtime.control.*;
 import org.jruby.truffle.runtime.core.array.RubyArray;
+import org.jruby.truffle.runtime.objects.RubyBasicObject;
 import org.jruby.truffle.translator.TranslatorDriver;
 
 public class JRubyTruffleBridge {
@@ -93,7 +94,7 @@ public class JRubyTruffleBridge {
     }
 
     public IRubyObject toJRuby(Object object) {
-        if (object instanceof NilPlaceholder) {
+        if (object instanceof NilPlaceholder || object instanceof RubyBasicObject) {
             return runtime.getNil();
         } else if (object == truffleContext.getCoreLibrary().getKernelModule()) {
             return runtime.getKernel();