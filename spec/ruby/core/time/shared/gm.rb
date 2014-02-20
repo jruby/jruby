b5b17e42d88c4474071fc807c832ba950c78b860
Dmitry Ratnikov <ratnikov@google.com>
2014-02-20 14:14:40 -0500

    Make RubyTime argument checking be same as mri.
    This should fix https://github.com/jruby/jruby/issues/1501

--- spec/ruby/core/time/shared/gm.rb
+++ spec/ruby/core/time/shared/gm.rb
@@ -20,4 +20,23 @@ describe :time_gm, :shared => true do
       time.inspect.should == "2000-01-01 20:15:01 UTC"
     end
   end
+
+  it "allows leap seconds like MRI does" do
+    # From MRI code: https://github.com/ruby/ruby/blob/trunk/time.c#L2609
+    next_day = Time.send(@method, 2000, 1, 2)
+
+    Time.send(@method, 2000, 1, 1, 24).should == next_day
+    Time.send(@method, 2000, 1, 1, 23, 59, 60).should == next_day
+  end
+
+  it "validates time like MRI does" do
+    [
+      [ 2000, 1, 1, 24, 1 ],
+      [ 2000, 1, 1, 23, 59, 61 ],
+      [ 2000, 1, 1, 25, 0, 0 ]
+    ].each do |bad_args|
+      proc { Time.send(@method, *bad_args) }.should raise_error(
+        ArgumentError, /out of range/)
+    end
+  end
 end
