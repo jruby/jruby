9cb8243408288061d95f8d49de3e4fd63ad97dd4
Charles Oliver Nutter <headius@headius.com>
2014-02-21 15:58:04 -0600

    Properly get one-byte bytelists from cache when indexing with byte
    Fixes #1361.

---
+++ spec/regression/GH-1361_ascii_string_with_high_bytes_each_char_spec.rb
@@ -0,0 +1,10 @@
+describe "An ASCII string with high-range bytes" do
+  it "can each_char through the high bytes successfully" do
+    ary = []
+    str = "M\xA1xico".force_encoding("ASCII")
+    str.each_char {|c| ary << c}
+
+    expect(ary.length).to eq 6
+    expect(ary).to eq(['M', "\xA1", 'x', 'i', 'c', 'o'])
+  end
+end unless RUBY_VERSION < '1.9'
