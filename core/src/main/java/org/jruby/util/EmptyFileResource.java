c4aa019614f24a94a1f0bdc93733f2bbce2e2625
Dmitry Ratnikov <ratnikov@google.com>
2014-03-04 11:16:45 -0500

    Always return same instance

--- core/src/main/java/org/jruby/util/EmptyFileResource.java
+++ core/src/main/java/org/jruby/util/EmptyFileResource.java
@@ -6,76 +6,80 @@ import org.jruby.exceptions.RaiseException;
 import org.jruby.runtime.ThreadContext;
 
 class EmptyFileResource implements FileResource {
-  public static EmptyFileResource create(String pathname) {
-    return (pathname == null || "".equals(pathname)) ?
-      new EmptyFileResource() : null;
-  }
+    // All empty resources are the same and immutable, so may as well
+    // cache the instance
+    private static final EmptyFileResource INSTANCE = new EmptyFileResource();
 
-  @Override
-  public String absolutePath() {
-    return "";
-  }
+    public static EmptyFileResource create(String pathname) {
+        return (pathname == null || "".equals(pathname)) ?
+            INSTANCE : null;
+    }
 
-  @Override
-  public boolean exists() {
-    return false;
-  }
+    @Override
+    public String absolutePath() {
+        return "";
+    }
 
-  @Override
-  public boolean isDirectory() {
-    return false;
-  }
+    @Override
+    public boolean exists() {
+        return false;
+    }
 
-  @Override
-  public boolean isFile() {
-    return false;
-  }
+    @Override
+    public boolean isDirectory() {
+        return false;
+    }
 
-  @Override
-  public boolean canRead() {
-    return false;
-  }
+    @Override
+    public boolean isFile() {
+        return false;
+    }
 
-  @Override
-  public boolean canWrite() {
-    return false;
-  }
+    @Override
+    public boolean canRead() {
+        return false;
+    }
 
-  @Override
-  public boolean isSymLink() {
-    return false;
-  }
+    @Override
+    public boolean canWrite() {
+        return false;
+    }
 
-  @Override
-  public String[] list() {
-    return new String[0];
-  }
+    @Override
+    public boolean isSymLink() {
+        return false;
+    }
 
-  @Override
-  public long lastModified() {
-    throw new UnsupportedOperationException();
-  }
+    @Override
+    public String[] list() {
+        return new String[0];
+    }
 
-  @Override
-  public long length() {
-    throw new UnsupportedOperationException();
-  }
+    @Override
+    public long lastModified() {
+        throw new UnsupportedOperationException();
+    }
 
-  @Override
-  public FileStat stat(POSIX posix) {
-    throw new UnsupportedOperationException();
-  }
+    @Override
+    public long length() {
+        throw new UnsupportedOperationException();
+    }
 
-  @Override
-  public FileStat lstat(POSIX posix) {
-    throw new UnsupportedOperationException();
-  }
+    @Override
+    public FileStat stat(POSIX posix) {
+        throw new UnsupportedOperationException();
+    }
 
-  @Override
-  public JRubyFile hackyGetJRubyFile() {
-    // It is somewhat weird that we're returning the NOT_EXIST instance that this resource is
-    // intending to replace. However, that should go away once we get rid of the hacky method, so
-    // should be okay for now.
-    return JRubyNonExistentFile.NOT_EXIST;
-  }
+    @Override
+    public FileStat lstat(POSIX posix) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public JRubyFile hackyGetJRubyFile() {
+        // It is somewhat weird that we're returning the NOT_EXIST instance that this resource is
+        // intending to replace. However, that should go away once we get rid of the hacky method, so
+        // should be okay for now.
+        return JRubyNonExistentFile.NOT_EXIST;
+    }
 }
