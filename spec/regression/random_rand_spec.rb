c180076de73dad3b845f19d0ca43126cc3284934
tduehr <tduehr@gmail.com>
2014-03-27 11:45:52 -0500

    fix Random.rand

---
+++ spec/regression/random_rand_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+
+describe "Random.rand(1.0)" do
+  it "returns a Float" do
+    Random.rand(1.0).should be_kind_of(Float)
+  end
+  it "does not always equal 0" do
+    (1..1000).map{|x| Random.rand(1.0)}.all?(&:zero?).should be_false
+  end
+end
+
+describe "Random::DEFAULT" do
+  it "returns a Float for a Float argument" do
+    Random.rand(1.0).should be_kind_of(Float)
+  end
+end
