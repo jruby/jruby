68856fd09c26ac9f7be04260362cb53dbda2d32b
Thomas E. Enebo <tom.enebo@gmail.com>
2014-04-14 12:26:27 -0500

    Remove to package on maven+mavericks...leaving in master to make sure we resolve real issue with FS packaging

--- test/JRUBY_6606/dir-ct.rb
+++
@@ -1,6 +0,0 @@
-# encoding: BIG5
-
-Dir.chdir(File.dirname __FILE__) do
-  Dir.entries('res/Ʀƨ').each{|n| p n unless n == '.' || n == '..'}
-  Dir.foreach('res/Ʀƨ') {|n| p n unless n == '.' || n == '..'}
-end
