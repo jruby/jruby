54d308070968fd65723ec283e0ee1fed55254cdc
kares <self@kares.org>
2014-04-24 14:31:42 +0200

    moved and fixed tests around so that they are usable (during development) once again

--- ext/openssl/src/test/ruby/pkcs7/test_attribute.rb
+++ ext/openssl/src/test/ruby/pkcs7/test_attribute.rb
@@ -1,13 +1,15 @@
+require File.expand_path('../pkcs7_helper', File.dirname(__FILE__))
+
 module PKCS7Test
-  class TestJavaAttribute < Test::Unit::TestCase
+  class TestAttribute < Test::Unit::TestCase
     def test_attributes
       val = ASN1::OctetString.new("foo".to_java_bytes)
       val2 = ASN1::OctetString.new("bar".to_java_bytes)
       attr = Attribute.create(123, 444, val)
-      assert_raise NoMethodError do 
+      assert_raise NoMethodError do
         attr.type = 12
       end
-      assert_raise NoMethodError do 
+      assert_raise NoMethodError do
         attr.value = val2
       end
 
@@ -15,9 +17,9 @@ module PKCS7Test
       assert_equal val, attr.set.get(0)
 
       attr2 = Attribute.create(123, 444, val)
-      
+
       assert_equal attr, attr2
-      
+
       assert_not_equal Attribute.create(124, 444, val), attr
       assert_not_equal Attribute.create(123, 444, val2), attr
     end
