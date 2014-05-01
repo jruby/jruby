ad4521e9b2aa8f5ea9295e77ff3d541059912387
Gustav Munkby <grddev@gmail.com>
2014-05-01 20:28:48 +0200

    Optimize key-based Hash access for empty hashes
    This avoids performing the potentially expensive hash computation in
    the event that the Hash is empty, as there cannot possibly be a match.

--- core/src/main/java/org/jruby/RubyHash.java
+++ core/src/main/java/org/jruby/RubyHash.java
@@ -545,6 +545,9 @@ public class RubyHash extends RubyObject implements Map {
     }
 
     protected RubyHashEntry internalGetEntry(IRubyObject key) {
+        if (size == 0) {
+          return NO_ENTRY;
+        }
         final int hash = hashValue(key.hashCode());
         for (RubyHashEntry entry = table[bucketIndex(hash, table.length)]; entry != null; entry = entry.next) {
             if (internalKeyExist(entry, hash, key)) {
@@ -563,6 +566,9 @@ public class RubyHash extends RubyObject implements Map {
 
 
     protected RubyHashEntry internalDelete(final IRubyObject key) {
+        if (size == 0) {
+          return NO_ENTRY;
+        }
         return internalDelete(hashValue(key.hashCode()), MATCH_KEY, key);
     }
 
