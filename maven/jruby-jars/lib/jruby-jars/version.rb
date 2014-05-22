61b6673a120e21cae8a14b626b2301a9712e027d
Uwe Kubosch <uwe@kubosch.no>
2014-05-22 09:33:36 +0200

    * Keep consistent Gem version using '.' instead of '-' as delimiter.

--- maven/jruby-jars/lib/jruby-jars/version.rb
+++ maven/jruby-jars/lib/jruby-jars/version.rb
@@ -1,3 +1,4 @@
 module JRubyJars
-  VERSION = Dir[ File.expand_path(File.dirname(File.dirname(__FILE__))) + '/jruby-core-complete-*jar' ].first.gsub( /^.*jruby-core-complete-|.jar$/, '' )
+  VERSION = Dir[ File.expand_path(File.dirname(File.dirname(__FILE__))) + '/jruby-core-complete-*jar' ].
+      first.gsub( /^.*jruby-core-complete-|.jar$/, '' ).gsub('-', '.')
 end
