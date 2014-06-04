abb5ef68413c8d35c23b1de1cb038bd459d952f6
Alex Dowad <alexinbeijing@gmail.com>
2014-06-04 09:26:22 +0200

    Make behavior of Array#eql? more closely match MRI

--- core/src/main/java/org/jruby/RubyArray.java
+++ core/src/main/java/org/jruby/RubyArray.java
@@ -1985,6 +1985,9 @@ public class RubyArray extends RubyObject implements List, RandomAccess {
      */
     @JRubyMethod(name = "eql?", required = 1)
     public IRubyObject eql(ThreadContext context, IRubyObject obj) {
+        if(!(obj instanceof RubyArray)) {
+            return runtime.getFalse();
+        }
         return RecursiveComparator.compare(context, MethodNames.EQL, this, obj);
     }
 
