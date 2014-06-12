0c797f1e068a75e4d43d02f05fad371900143cde
Thomas E. Enebo <tom.enebo@gmail.com>
2014-06-12 12:20:53 -0500

    Fixes #1743. Use nil 1.9+ for type in set_trace_func

--- core/src/main/java/org/jruby/Ruby.java
+++ core/src/main/java/org/jruby/Ruby.java
@@ -2993,7 +2993,7 @@ public final class Ruby {
         public void eventHandler(ThreadContext context, String eventName, String file, int line, String name, IRubyObject type) {
             if (!context.isWithinTrace()) {
                 if (file == null) file = "(ruby)";
-                if (type == null) type = getFalse();
+                if (type == null) type = is1_8() ? getFalse() : getNil();
                 
                 RubyBinding binding = RubyBinding.newBinding(Ruby.this, context.currentBinding());
 
