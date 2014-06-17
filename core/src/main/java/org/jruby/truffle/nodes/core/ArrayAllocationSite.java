60bd63897898ed5d5709e06c2f248fec9ec26135
Chris Seaton <chris@chrisseaton.com>
2014-06-17 14:45:57 +0100

    [Truffle] Make the previous an option.

--- core/src/main/java/org/jruby/truffle/nodes/core/ArrayAllocationSite.java
+++ core/src/main/java/org/jruby/truffle/nodes/core/ArrayAllocationSite.java
@@ -3,6 +3,7 @@ package org.jruby.truffle.nodes.core;
 import com.oracle.truffle.api.Assumption;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.Truffle;
+import org.jruby.util.cli.Options;
 
 public class ArrayAllocationSite {
 
@@ -11,13 +12,19 @@ public class ArrayAllocationSite {
 
     @CompilerDirectives.SlowPath
     public void convertedIntToLong() {
-        convertedIntToLong = true;
-        assumption.invalidate();
+        if (Options.TRUFFLE_ARRAYS_OPTIMISTIC_LONG.load()) {
+            convertedIntToLong = true;
+            assumption.invalidate();
+        }
     }
 
     public boolean hasConvertedIntToLong() {
-        assumption.isValid();
-        return convertedIntToLong;
+        if (Options.TRUFFLE_ARRAYS_OPTIMISTIC_LONG.load()) {
+            assumption.isValid();
+            return convertedIntToLong;
+        } else {
+            return false;
+        }
     }
 
 }
