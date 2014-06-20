0c1c315a522ee41f936ee7d49bf3597712552df8
Alex Tambellini <atambellini@gmail.com>
2014-06-20 00:47:31 -0400

    Fix incorrect regression spec
    After the fix for issue #1517 this regression spec can now have
    the correct local hour.

--- spec/regression/gh-801_date_inspect_spec.rb
+++ spec/regression/gh-801_date_inspect_spec.rb
@@ -5,7 +5,7 @@ if RUBY_VERSION > '1.9'
     subject { Time.new(2013,4,22,14,18,05,'-05:00') }
 
     it 'returns correct offset' do
-      subject.inspect.should == "2013-04-22 04:18:05 -0500"
+      subject.inspect.should == "2013-04-22 14:18:05 -0500"
     end
 
   end
