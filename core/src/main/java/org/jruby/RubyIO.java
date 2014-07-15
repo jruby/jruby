d810e32f1df4af41b40f1701bb6f24fcef3f7786
Charles Oliver Nutter <headius@headius.com>
2014-07-15 18:22:13 -0500

    Fix IO#initialize_copy to flush the right stream.

--- core/src/main/java/org/jruby/RubyIO.java
+++ core/src/main/java/org/jruby/RubyIO.java
@@ -1688,12 +1688,12 @@ public class RubyIO extends RubyObject implements IOEncodable {
         RubyIO write_io;
         long pos;
 
-        RubyIO io = (RubyIO)TypeConverter.ioCheckIO(runtime, _io);
+        RubyIO io = TypeConverter.ioGetIO(runtime, _io);
         if (!OBJ_INIT_COPY(dest, io)) return dest;
         orig = io.getOpenFileChecked();
         fptr = dest.MakeOpenFile();
 
-        flush(context);
+        io.flush(context);
 
         /* copy rb_io_t structure */
         fptr.setMode(orig.getMode() & ~OpenFile.PREP);
@@ -1941,7 +1941,7 @@ public class RubyIO extends RubyObject implements IOEncodable {
 //        }
 
         RubyIO io = GetWriteIO();
-        fptr = getOpenFileChecked();
+        fptr = io.getOpenFileChecked();
 
         if ((fptr.getMode() & OpenFile.WRITABLE) != 0) {
             if (fptr.io_fflush(context) < 0)
