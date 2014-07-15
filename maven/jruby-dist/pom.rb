e1ff53f8716fe3b29dcbb60354ff1f2c49dd093d
Christian Meier <m.kristian@web.de>
2014-07-15 20:27:38 +0100

    sync with master

--- maven/jruby-dist/pom.rb
+++ maven/jruby-dist/pom.rb
@@ -1,6 +1,6 @@
 project 'JRuby Dist' do
 
-  version = '1.7.10' #File.read( File.join( basedir, '..', '..', 'VERSION' ) )
+  version = '1.7.14-SNAPSHOT' #File.read( File.join( basedir, '..', '..', 'VERSION' ) )
 
   model_version '4.0.0'
   id "org.jruby:jruby-dist:#{version}"
@@ -9,7 +9,7 @@ project 'JRuby Dist' do
 
   properties( 'tesla.dump.pom' => 'pom.xml',
               'tesla.dump.readonly' => true,
-              'jruby.basedir' => '${basedir}/../..',
+              'jruby.home' => '${basedir}/../..',
               'main.basedir' => '${project.parent.parent.basedir}' )
 
   phase 'package' do
@@ -25,7 +25,7 @@ project 'JRuby Dist' do
                                             'outputDirectory' =>  '${project.build.directory}' } ] )
     end
     
-    execute :fix_executable_bits, 'package' do |ctx|
+    execute :fix_executable_bits do |ctx|
       Dir[ File.join( ctx.project.build.directory,
                       'META-INF', 
                       'jruby.home', 
@@ -39,6 +39,7 @@ project 'JRuby Dist' do
     end
 
     plugin( :assembly, '2.4',
+            'finalName' => "#{model.artifact_id}-#{version.sub(/-SNAPSHOT/, '')}",
             'tarLongFileMode' =>  'gnu',
             'descriptors' => [ 'src/main/assembly/jruby.xml' ] ) do
       execute_goals( 'single' )
@@ -58,29 +59,31 @@ project 'JRuby Dist' do
                    'localRepositoryPath' =>  '${project.build.directory}/local-repo' )
   end
 
+  # since the source packages are done from the git repository we need
+  # to inside git controlled directory. for example the source packages
+  # itself does not contain the git repository !!
+
   profile 'source dist' do
 
     activation do
       file( :exists => '../../.git' )
     end
 
-    phase :package do
-      execute :pack_sources, 'package' do |ctx|
-        require 'fileutils'
+    execute :pack_sources, 'package' do |ctx|
+      require 'fileutils'
 
-        revision = `git show`.gsub( /\n.*|commit /, '' )
+      revision = `git show`.gsub( /\n.*|commit /, '' )
 
-        basefile = "#{ctx.project.build.directory}/#{ctx.project.artifactId}-#{ctx.project.version}-src"
+      basefile = "#{ctx.project.build.directory}/#{ctx.project.artifactId}-#{ctx.project.version}-src".sub(/-SNAPSHOT/, '')
 
-        FileUtils.cd( File.join( ctx.project.basedir.to_s, '..', '..' ) ) do
-          [ 'tar', 'zip' ].each do |format|
-            puts "create #{basefile}.#{format}"
-            system( "git archive --prefix 'jruby-#{ctx.project.version}/' --format #{format} #{revision} . -o #{basefile}.#{format}" ) || raise( "error creating #{format}-file" )
-          end
+      FileUtils.cd( File.join( ctx.project.basedir.to_s, '..', '..' ) ) do
+        [ 'tar', 'zip' ].each do |format|
+          puts "create #{basefile}.#{format}"
+          system( "git archive --prefix 'jruby-#{ctx.project.version}/' --format #{format} #{revision} . -o #{basefile}.#{format}" ) || raise( "error creating #{format}-file" )
         end
-        puts "zipping #{basefile}.tar"
-        system( "gzip #{basefile}.tar -f" ) || raise( "error zipping #{basefile}.tar" )
       end
+      puts "zipping #{basefile}.tar"
+      system( "gzip #{basefile}.tar -f" ) || raise( "error zipping #{basefile}.tar" )
     end
   end
 
