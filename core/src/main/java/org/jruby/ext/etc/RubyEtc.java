3538512f8c25ac9307790595b8cc5c36aca2e05a
Uwe Kubosch <uwe@kubosch.no>
2014-07-19 18:44:30 +0200

    * Use java.io.tmpdir for default Etc.systmpdir.  Enables changing Dir::tmpdir using a Java system property.

--- core/src/main/java/org/jruby/ext/etc/RubyEtc.java
+++ core/src/main/java/org/jruby/ext/etc/RubyEtc.java
@@ -398,7 +398,7 @@ public class RubyEtc {
     @JRubyMethod(module = true)
     public static IRubyObject systmpdir(ThreadContext context, IRubyObject recv) {
         Ruby runtime = context.getRuntime();
-        ByteList tmp = ByteList.create("/tmp"); // default for all platforms except Windows
+        ByteList tmp = ByteList.create(System.getProperty("java.io.tmpdir")); // default for all platforms except Windows
         
         if (Platform.IS_WINDOWS) {
             String commonAppData = System.getenv("CSIDL_COMMON_APPDATA");
