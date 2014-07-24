5173cc151a5e706bb2f8276204e42e90d7563452
Uwe Kubosch <uwe@kubosch.no>
2014-07-24 23:47:54 +0200

    * Set new development version: 1.7.14.dev to get corresponding jruby-jars gem file name and version.

--- maven/jruby-jars/lib/jruby-jars/version.rb
+++ maven/jruby-jars/lib/jruby-jars/version.rb
@@ -1,4 +1,5 @@
 module JRubyJars
-  VERSION = Dir[ File.expand_path(File.dirname(File.dirname(__FILE__))) + '/jruby-core-complete-*jar' ].
-      first.gsub( /^.*jruby-core-complete-|.jar$/, '' ).gsub('-', '.')
+  MAVEN_VERSION = Dir[File.expand_path(File.dirname(File.dirname(__FILE__))) + '/jruby-core-complete-*jar' ].
+      first.gsub(/^.*jruby-core-complete-|.jar$/, '')
+  VERSION = MAVEN_VERSION.sub('-SNAPSHOT', '').gsub('-', '.')
 end
