dc5c90c07de1c582c582af2fff0b59686640c625
Christian Meier <m.kristian@web.de>
2014-07-25 13:00:38 +0100

    find resources via uri as well - not sure how this broke but it just did not work anymore after the big loadservice patch

--- core/src/main/java/org/jruby/runtime/load/LoadService.java
+++ core/src/main/java/org/jruby/runtime/load/LoadService.java
@@ -1569,6 +1569,7 @@ public class LoadService {
         return false;
     }
 
+    private static final Pattern URI_PATTERN = Pattern.compile("([a-z]+?://.*)$");
     protected LoadServiceResource getClassPathResource(ClassLoader classLoader, String name) {
         boolean isClasspathScheme = false;
 
@@ -1583,8 +1584,24 @@ public class LoadService {
             name = name.substring(name.indexOf("!/") + 2);
         }
 
-        debugLogTry("fileInClasspath", name);
-        URL loc = classLoader.getResource(name);
+        URL loc;
+        Matcher m = URI_PATTERN.matcher( name );
+        final String pn;
+        if (m.matches()) {
+            debugLogTry("fileInClassloader", m.group( 1 ) );
+            try
+            {
+                loc = new URL( m.group( 1 ) );
+            }
+            catch (MalformedURLException e)
+            {
+                loc = null;
+            }
+        }
+        else {
+            debugLogTry("fileInClasspath", name);
+            loc = classLoader.getResource(name);
+        }
 
         if (loc != null) { // got it
             String path = "classpath:/" + name;
