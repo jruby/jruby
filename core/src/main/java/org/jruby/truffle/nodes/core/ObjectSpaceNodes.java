900ff5f63f4dd01e99f02b3b815e994be8a98190
Matthias Grimmer <grimmer@ssw.jku.at>
2014-08-11 16:23:42 +0200

    [Truffle] put GC behind SlowPath

--- core/src/main/java/org/jruby/truffle/nodes/core/ObjectSpaceNodes.java
+++ core/src/main/java/org/jruby/truffle/nodes/core/ObjectSpaceNodes.java
@@ -127,6 +127,11 @@ public abstract class ObjectSpaceNodes {
 
         @Specialization
         public NilPlaceholder garbageCollect() {
+            return doGC();
+        }
+
+        @CompilerDirectives.SlowPath
+        private NilPlaceholder doGC() {
             notDesignedForCompilation();
 
             final RubyThread runningThread = getContext().getThreadManager().leaveGlobalLock();
