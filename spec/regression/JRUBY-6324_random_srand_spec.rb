c5107782828d36dea8b63581920d2af14a23b11c
Charles Oliver Nutter <headius@headius.com>
2014-08-12 18:03:40 -0500

    Use appropriate isolated scope for ScriptingContainer here.
    The default constructor uses SINGLETON scope, which shares a Ruby
    instance with the VM running the spec. When the container goes
    out of scope and eventually GCs, it also tears itself down. This
    in turn causes stdio streams to be marked as closed (though not
    actually closed), so the parent runtime can no longer use them.
    
    This is not really a bug, since container teardown should trigger
    runtime teardown, and runtime teardown should flush and terminate
    all IO streams. However, the old IO logic acted more aggressively
    to preserve stdio streams, exposing this flaw in the test.
    
    It may be we need to restore that preservation logic before
    release, but for the moment I'm just modifying the test to use the
    right scope.

--- spec/regression/JRUBY-6324_random_srand_spec.rb
+++ spec/regression/JRUBY-6324_random_srand_spec.rb
@@ -2,12 +2,12 @@ require 'java'
 
 describe "JRUBY-6324: random seed for srand is not initialized properly" do
   it "initializes initial seed for PRNG" do
-    rt = Java.org.jruby.embed.ScriptingContainer.new
+    rt = Java.org.jruby.embed.ScriptingContainer.new(org.jruby.embed.LocalContextScope::SINGLETHREAD)
     rt.run_scriptlet("srand").should > 0
   end
 
   it "initializes initial seed for PRNG" do
-    rt = Java.org.jruby.embed.ScriptingContainer.new
+    rt = Java.org.jruby.embed.ScriptingContainer.new(org.jruby.embed.LocalContextScope::SINGLETHREAD)
     id1, id2 = rt.run_scriptlet("n = 2**128; srand(n); [n.object_id, srand.object_id]")
 
     # You cannot do id1.should == id2 here because the Objects are from
