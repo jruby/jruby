c32e0f73eb33ce94e20b6f88a89bbdb24261bbf5
Christian Meier <m.kristian@web.de>
2014-08-16 10:29:50 +0200

    use jar-dependencies-0.0.8

--- lib/pom.rb
+++ lib/pom.rb
@@ -32,7 +32,7 @@ default_gems =
    ImportedGem.new( 'krypt-core', KRYPT_VERSION, true ),
    ImportedGem.new( 'krypt-provider-jdk', KRYPT_VERSION, true ),
    ImportedGem.new( 'ffi', '1.9.3', true ),
-   ImportedGem.new( 'jar-dependencies', '0.0.7', true )
+   ImportedGem.new( 'jar-dependencies', '0.0.8', true )
   ]
 
 project 'JRuby Lib Setup' do
