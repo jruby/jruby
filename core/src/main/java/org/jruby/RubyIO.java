31f2c1102b1309cd805cc2f8d6eb21bec4275d9c
Charles Oliver Nutter <headius@headius.com>
2014-08-18 14:18:52 -0500

    Use MRI lib for open3, deprecate our hidden impls.
    We provided IO.popen3 and popen4 previously because open3.rb used
    forking. Now that it uses spawn, which is fully supported in 9k,
    we can go back to the regular open3.rb. This provides better
    compatibility with MRI.
    
    popen4 will be removed. It was never officially available for
    users, and it can be achieved by using Open3 and getting the pid
    off the IO objects.
    
    The internal implementations of popen3/4 also tried to unwrap the
    subprocess's streams from java.lang.Process. This is broken on
    Java 7+ because Process logic now eagerly attempts to drain the
    child streams in a separate thread, making the unwrapped inner
    stream/channel useless. Because 9k currently only supports Java
    7+, and because the internal popen3/4 are now deprecated and
    unused, I have removed this wrapping.

--- core/src/main/java/org/jruby/RubyIO.java
+++ core/src/main/java/org/jruby/RubyIO.java
@@ -3390,10 +3390,6 @@ public class RubyIO extends RubyObject implements IOEncodable {
             file.close();
         }
     }
-   
-    public static IRubyObject popen(ThreadContext context, IRubyObject recv, IRubyObject[] args, Block block) {
-        return popen19(context, recv, args, block);
-    }
 
     private void setupPopen(ModeFlags modes, POpenProcess process) throws RaiseException {
         Ruby runtime = getRuntime();
@@ -3501,7 +3497,7 @@ public class RubyIO extends RubyObject implements IOEncodable {
     }
 
     @JRubyMethod(name = "popen", required = 1, optional = 2, meta = true)
-    public static IRubyObject popen19(ThreadContext context, IRubyObject recv, IRubyObject[] args, Block block) {
+    public static IRubyObject popen(ThreadContext context, IRubyObject recv, IRubyObject[] args, Block block) {
         Ruby runtime = context.runtime;
 
         if (runtime.getPosix().isNative() && !Platform.IS_WINDOWS) {
@@ -3593,181 +3589,6 @@ public class RubyIO extends RubyObject implements IOEncodable {
             throw runtime.newIOErrorFromException(e);
         } catch (InterruptedException e) {
             throw runtime.newThreadError("unexpected interrupt");
-        }
-    }
-   
-    public static IRubyObject popen3(ThreadContext context, IRubyObject recv, IRubyObject[] args, Block block) {
-        return popen3_19(context, recv, args, block);
-    }
-   
-    @JRubyMethod(name = "popen3", rest = true, meta = true)
-    public static IRubyObject popen3_19(ThreadContext context, IRubyObject recv, IRubyObject[] args, Block block) {
-        final Ruby runtime = context.runtime;
-
-        // TODO: handle opts
-        if (args.length > 0 && args[args.length - 1] instanceof RubyHash) {
-            args = Arrays.copyOf(args, args.length - 1);
-        }
-
-        final POpenTuple tuple = popenSpecial(context, args);
-        final long pid = ShellLauncher.getPidFromProcess(tuple.process);
-
-        // array trick to be able to reference enclosing RubyThread
-        final RubyThread[] waitThread = new RubyThread[1];
-        waitThread[0] = new RubyThread(
-                runtime,
-                (RubyClass) runtime.getClassFromPath("Process::WaitThread"),
-                new ThreadedRunnable() {
-
-            volatile Thread javaThread;
-
-            @Override
-            public Thread getJavaThread() {
-                return javaThread;
-            }
-
-            @Override
-            public void run() {
-                javaThread = Thread.currentThread();
-                RubyThread rubyThread;
-                // spin a bit until this happens; should almost never spin
-                while ((rubyThread = waitThread[0]) == null) {
-                    Thread.yield();
-                }
-                
-                ThreadContext context = runtime.getThreadService().registerNewThread(rubyThread);
-
-                rubyThread.op_aset(
-                        runtime.newSymbol("pid"),
-                        runtime.newFixnum(pid));
-
-                try {
-                    int exitValue = tuple.process.waitFor();
-
-                    // RubyStatus uses real native status now, so we unshift Java's shifted exit status
-                    RubyProcess.RubyStatus status = RubyProcess.RubyStatus.newProcessStatus(
-                            runtime,
-                            exitValue << 8,
-                            pid);
-                    
-                    rubyThread.cleanTerminate(status);
-                } catch (Throwable t) {
-                    rubyThread.exceptionRaised(t);
-                } finally {
-                    rubyThread.dispose();
-                }
-            }
-
-        });
-
-        RubyArray yieldArgs = RubyArray.newArrayLight(runtime,
-                tuple.output,
-                tuple.input,
-                tuple.error,
-                waitThread[0]);
-
-        if (block.isGiven()) {
-            try {
-                return block.yield(context, yieldArgs);
-            } finally {
-                cleanupPOpen(tuple);
-                
-                IRubyObject status = waitThread[0].join(IRubyObject.NULL_ARRAY);
-                context.setLastExitStatus(status);
-            }
-        }
-        
-        return yieldArgs;
-    }
-
-    @JRubyMethod(rest = true, meta = true)
-    public static IRubyObject popen4(ThreadContext context, IRubyObject recv, IRubyObject[] args, Block block) {
-        Ruby runtime = context.runtime;
-
-        try {
-            POpenTuple tuple = popenSpecial(context, args);
-
-            RubyArray yieldArgs = RubyArray.newArrayLight(runtime,
-                    runtime.newFixnum(ShellLauncher.getPidFromProcess(tuple.process)),
-                    tuple.output,
-                    tuple.input,
-                    tuple.error);
-
-            if (block.isGiven()) {
-                try {
-                    return block.yield(context, yieldArgs);
-                } finally {
-                    cleanupPOpen(tuple);
-                    // RubyStatus uses real native status now, so we unshift Java's shifted exit status
-                    context.setLastExitStatus(RubyProcess.RubyStatus.newProcessStatus(runtime, tuple.process.waitFor() << 8, ShellLauncher.getPidFromProcess(tuple.process)));
-                }
-            }
-            return yieldArgs;
-        } catch (InterruptedException e) {
-            throw runtime.newThreadError("unexpected interrupt");
-        }
-    }
-
-    private static void cleanupPOpen(POpenTuple tuple) {
-        if (tuple.input.openFile.isOpen()) {
-            tuple.input.close();
-        }
-        if (tuple.output.openFile.isOpen()) {
-            tuple.output.close();
-        }
-        if (tuple.error.openFile.isOpen()) {
-            tuple.error.close();
-        }
-    }
-
-    private static class POpenTuple {
-        public POpenTuple(RubyIO i, RubyIO o, RubyIO e, Process p) {
-            input = i; output = o; error = e; process = p;
-        }
-        public final RubyIO input;
-        public final RubyIO output;
-        public final RubyIO error;
-        public final Process process;
-    }
-
-    public static POpenTuple popenSpecial(ThreadContext context, IRubyObject[] args) {
-        Ruby runtime = context.runtime;
-
-        try {
-            ShellLauncher.POpenProcess process = ShellLauncher.popen3(runtime, args, false);
-            RubyIO input = process.getInput() != null ?
-                new RubyIO(runtime, process.getInput()) :
-                new RubyIO(runtime, process.getInputStream());
-            RubyIO output = process.getOutput() != null ?
-                new RubyIO(runtime, process.getOutput()) :
-                new RubyIO(runtime, process.getOutputStream());
-            RubyIO error = process.getError() != null ?
-                new RubyIO(runtime, process.getError()) :
-                new RubyIO(runtime, process.getErrorStream());
-
-            // ensure the OpenFile knows it's a process; see OpenFile#finalize
-            input.getOpenFile().setProcess(process);
-            output.getOpenFile().setProcess(process);
-            error.getOpenFile().setProcess(process);
-
-            // set all streams as popenSpecial streams, so we don't shut down process prematurely
-            input.popenSpecial = true;
-            output.popenSpecial = true;
-            error.popenSpecial = true;
-            
-            // process streams are not seekable
-//            input.getOpenFile().getMainStreamSafe().getDescriptor().
-//              setCanBeSeekable(false);
-//            output.getOpenFile().getMainStreamSafe().getDescriptor().
-//              setCanBeSeekable(false);
-//            error.getOpenFile().getMainStreamSafe().getDescriptor().
-//              setCanBeSeekable(false);
-
-            return new POpenTuple(input, output, error, process);
-//        } catch (BadDescriptorException e) {
-//            throw runtime.newErrnoEBADFError();
-        } catch (IOException e) {
-            throw runtime.newIOErrorFromException(e);
         }
     }
 
@@ -4626,6 +4447,185 @@ public class RubyIO extends RubyObject implements IOEncodable {
     @Deprecated
     public static IRubyObject writeStatic(ThreadContext context, IRubyObject recv, IRubyObject[] argv, Block unusedBlock) {
         return write(context, recv, argv);
+    }
+
+    @Deprecated
+    public static IRubyObject popen3(ThreadContext context, IRubyObject recv, IRubyObject[] args, Block block) {
+        return popen3_19(context, recv, args, block);
+    }
+
+    @Deprecated
+    public static IRubyObject popen3_19(ThreadContext context, IRubyObject recv, IRubyObject[] args, Block block) {
+        final Ruby runtime = context.runtime;
+
+        // TODO: handle opts
+        if (args.length > 0 && args[args.length - 1] instanceof RubyHash) {
+            args = Arrays.copyOf(args, args.length - 1);
+        }
+
+        final POpenTuple tuple = popenSpecial(context, args);
+        final long pid = ShellLauncher.getPidFromProcess(tuple.process);
+
+        // array trick to be able to reference enclosing RubyThread
+        final RubyThread[] waitThread = new RubyThread[1];
+        waitThread[0] = new RubyThread(
+                runtime,
+                (RubyClass) runtime.getClassFromPath("Process::WaitThread"),
+                new ThreadedRunnable() {
+
+                    volatile Thread javaThread;
+
+                    @Override
+                    public Thread getJavaThread() {
+                        return javaThread;
+                    }
+
+                    @Override
+                    public void run() {
+                        javaThread = Thread.currentThread();
+                        RubyThread rubyThread;
+                        // spin a bit until this happens; should almost never spin
+                        while ((rubyThread = waitThread[0]) == null) {
+                            Thread.yield();
+                        }
+
+                        ThreadContext context = runtime.getThreadService().registerNewThread(rubyThread);
+
+                        rubyThread.op_aset(
+                                runtime.newSymbol("pid"),
+                                runtime.newFixnum(pid));
+
+                        try {
+                            int exitValue = tuple.process.waitFor();
+
+                            // RubyStatus uses real native status now, so we unshift Java's shifted exit status
+                            RubyProcess.RubyStatus status = RubyProcess.RubyStatus.newProcessStatus(
+                                    runtime,
+                                    exitValue << 8,
+                                    pid);
+
+                            rubyThread.cleanTerminate(status);
+                        } catch (Throwable t) {
+                            rubyThread.exceptionRaised(t);
+                        } finally {
+                            rubyThread.dispose();
+                        }
+                    }
+
+                });
+
+        RubyArray yieldArgs = RubyArray.newArrayLight(runtime,
+                tuple.output,
+                tuple.input,
+                tuple.error,
+                waitThread[0]);
+
+        if (block.isGiven()) {
+            try {
+                return block.yield(context, yieldArgs);
+            } finally {
+                cleanupPOpen(tuple);
+
+                IRubyObject status = waitThread[0].join(IRubyObject.NULL_ARRAY);
+                context.setLastExitStatus(status);
+            }
+        }
+
+        return yieldArgs;
+    }
+
+    @Deprecated
+    public static IRubyObject popen4(ThreadContext context, IRubyObject recv, IRubyObject[] args, Block block) {
+        Ruby runtime = context.runtime;
+
+        try {
+            POpenTuple tuple = popenSpecial(context, args);
+
+            RubyArray yieldArgs = RubyArray.newArrayLight(runtime,
+                    runtime.newFixnum(ShellLauncher.getPidFromProcess(tuple.process)),
+                    tuple.output,
+                    tuple.input,
+                    tuple.error);
+
+            if (block.isGiven()) {
+                try {
+                    return block.yield(context, yieldArgs);
+                } finally {
+                    cleanupPOpen(tuple);
+                    // RubyStatus uses real native status now, so we unshift Java's shifted exit status
+                    context.setLastExitStatus(RubyProcess.RubyStatus.newProcessStatus(runtime, tuple.process.waitFor() << 8, ShellLauncher.getPidFromProcess(tuple.process)));
+                }
+            }
+            return yieldArgs;
+        } catch (InterruptedException e) {
+            throw runtime.newThreadError("unexpected interrupt");
+        }
+    }
+
+    @Deprecated
+    private static void cleanupPOpen(POpenTuple tuple) {
+        if (tuple.input.openFile.isOpen()) {
+            tuple.input.close();
+        }
+        if (tuple.output.openFile.isOpen()) {
+            tuple.output.close();
+        }
+        if (tuple.error.openFile.isOpen()) {
+            tuple.error.close();
+        }
+    }
+
+    @Deprecated
+    private static class POpenTuple {
+        public POpenTuple(RubyIO i, RubyIO o, RubyIO e, Process p) {
+            input = i; output = o; error = e; process = p;
+        }
+        public final RubyIO input;
+        public final RubyIO output;
+        public final RubyIO error;
+        public final Process process;
+    }
+
+    @Deprecated
+    public static POpenTuple popenSpecial(ThreadContext context, IRubyObject[] args) {
+        Ruby runtime = context.runtime;
+
+        try {
+            ShellLauncher.POpenProcess process = ShellLauncher.popen3(runtime, args, false);
+            RubyIO input = process.getInput() != null ?
+                    new RubyIO(runtime, process.getInput()) :
+                    new RubyIO(runtime, process.getInputStream());
+            RubyIO output = process.getOutput() != null ?
+                    new RubyIO(runtime, process.getOutput()) :
+                    new RubyIO(runtime, process.getOutputStream());
+            RubyIO error = process.getError() != null ?
+                    new RubyIO(runtime, process.getError()) :
+                    new RubyIO(runtime, process.getErrorStream());
+
+            // ensure the OpenFile knows it's a process; see OpenFile#finalize
+            input.getOpenFile().setProcess(process);
+            output.getOpenFile().setProcess(process);
+            error.getOpenFile().setProcess(process);
+
+            // set all streams as popenSpecial streams, so we don't shut down process prematurely
+            input.popenSpecial = true;
+            output.popenSpecial = true;
+            error.popenSpecial = true;
+
+            // process streams are not seekable
+//            input.getOpenFile().getMainStreamSafe().getDescriptor().
+//              setCanBeSeekable(false);
+//            output.getOpenFile().getMainStreamSafe().getDescriptor().
+//              setCanBeSeekable(false);
+//            error.getOpenFile().getMainStreamSafe().getDescriptor().
+//              setCanBeSeekable(false);
+
+            return new POpenTuple(input, output, error, process);
+//        } catch (BadDescriptorException e) {
+//            throw runtime.newErrnoEBADFError();
+        } catch (IOException e) {
+            throw runtime.newIOErrorFromException(e);
+        }
     }
     
     protected OpenFile openFile;
