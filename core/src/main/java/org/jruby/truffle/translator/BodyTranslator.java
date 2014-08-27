0d9419d7eb939d5d48e63e4b9facbc9ea52ee760
Chris Seaton <chris@chrisseaton.com>
2014-08-27 04:07:10 +0100

    [Truffle] Implement another type of multiple assignment.

--- core/src/main/java/org/jruby/truffle/translator/BodyTranslator.java
+++ core/src/main/java/org/jruby/truffle/translator/BodyTranslator.java
@@ -1288,6 +1288,7 @@ public class BodyTranslator extends Translator {
         final SourceSection sourceSection = translate(node.getPosition());
 
         final org.jruby.ast.ArrayNode preArray = (org.jruby.ast.ArrayNode) node.getPre();
+        final org.jruby.ast.ArrayNode postArray = (org.jruby.ast.ArrayNode) node.getPost();
         final org.jruby.ast.Node rhs = node.getValueNode();
 
         RubyNode rhsTranslated;
@@ -1299,14 +1300,11 @@ public class BodyTranslator extends Translator {
             rhsTranslated = rhs.accept(this);
         }
 
-        /*
-         * One very common case is to do
-         *
-         * a, b = c, d
-         */
-
-        if (preArray != null && node.getPost() == null && node.getRest() == null && rhsTranslated instanceof ArrayLiteralNode.UninitialisedArrayLiteralNode &&
-                ((ArrayLiteralNode.UninitialisedArrayLiteralNode) rhsTranslated).getValues().length == preArray.size()) {
+        if (preArray != null
+                && node.getPost() == null
+                && node.getRest() == null
+                && rhsTranslated instanceof ArrayLiteralNode.UninitialisedArrayLiteralNode
+                && ((ArrayLiteralNode.UninitialisedArrayLiteralNode) rhsTranslated).getValues().length == preArray.size()) {
             /*
              * We can deal with this common case be rewriting as
              *
@@ -1405,9 +1403,15 @@ public class BodyTranslator extends Translator {
             }
 
             return SequenceNode.sequence(context, sourceSection, sequence);
-        } else if (node.getPre() == null && node.getPost() == null && node.getRest() instanceof org.jruby.ast.StarNode) {
+        } else if (node.getPre() == null
+                && node.getPost() == null
+                && node.getRest() instanceof org.jruby.ast.StarNode) {
             return rhsTranslated;
-        } else if (node.getPre() == null && node.getPost() == null && node.getRest() != null && rhs != null && !(rhs instanceof org.jruby.ast.ArrayNode)) {
+        } else if (node.getPre() == null
+                && node.getPost() == null
+                && node.getRest() != null
+                && rhs != null
+                && !(rhs instanceof org.jruby.ast.ArrayNode)) {
             /*
              * *a = b
              *
@@ -1436,7 +1440,11 @@ public class BodyTranslator extends Translator {
             final SplatCastNode rhsSplatCast = SplatCastNodeFactory.create(context, sourceSection, translatingNextExpression ? SplatCastNode.NilBehavior.EMPTY_ARRAY : SplatCastNode.NilBehavior.ARRAY_WITH_NIL, rhsTranslated);
 
             return restRead.makeWriteNode(rhsSplatCast);
-        } else if (node.getPre() == null && node.getPost() == null && node.getRest() != null && rhs != null && rhs instanceof org.jruby.ast.ArrayNode) {
+        } else if (node.getPre() == null
+                && node.getPost() == null
+                && node.getRest() != null
+                && rhs != null
+                && rhs instanceof org.jruby.ast.ArrayNode) {
             /*
              * *a = [b, c]
              *
@@ -1463,6 +1471,51 @@ public class BodyTranslator extends Translator {
             }
 
             return restRead.makeWriteNode(rhsTranslated);
+        } else if (node.getPre() == null && node.getRest() != null && node.getPost() != null) {
+            /*
+             * Something like
+             *
+             *     *a,b = [1, 2, 3, 4]
+             */
+
+            // This is very similar to the case with pre and rest, so unify with that
+
+            /*
+             * Create a temp for the array.
+             */
+
+            final String tempName = environment.allocateLocalTemp("array");
+
+            /*
+             * Create a sequence of instructions, with the first being the literal array assigned to
+             * the temp.
+             */
+
+            final List<RubyNode> sequence = new ArrayList<>();
+
+            final RubyNode splatCastNode = SplatCastNodeFactory.create(context, sourceSection, translatingNextExpression ? SplatCastNode.NilBehavior.EMPTY_ARRAY : SplatCastNode.NilBehavior.ARRAY_WITH_NIL, rhsTranslated);
+
+            final RubyNode writeTemp = ((ReadNode) environment.findLocalVarNode(tempName, sourceSection)).makeWriteNode(splatCastNode);
+
+            sequence.add(writeTemp);
+
+            /*
+             * Then index the temp array for each assignment on the LHS.
+             */
+
+            if (node.getRest() != null) {
+                final ArrayDropTailNode assignedValue = ArrayDropTailNodeFactory.create(context, sourceSection, postArray.size(), environment.findLocalVarNode(tempName, sourceSection));
+
+                sequence.add(translateDummyAssignment(node.getRest(), assignedValue));
+            }
+
+            for (int n = 0; n < postArray.size(); n++) {
+                final ArrayIndexNode assignedValue = ArrayIndexNodeFactory.create(context, sourceSection, -(postArray.size() - n), environment.findLocalVarNode(tempName, sourceSection));
+
+                sequence.add(translateDummyAssignment(postArray.get(n), assignedValue));
+            }
+
+            return SequenceNode.sequence(context, sourceSection, sequence);
         } else {
             context.getRuntime().getWarnings().warn(IRubyWarnings.ID.TRUFFLE, node.getPosition().getFile(), node.getPosition().getStartLine(), node + " unknown form of multiple assignment");
             return new NilLiteralNode(context, sourceSection);
