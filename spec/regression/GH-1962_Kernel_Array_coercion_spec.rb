2032d7619f098d04d2beda87b3d3e268dae291de
Charles Oliver Nutter <headius@headius.com>
2014-09-10 18:06:05 -0500

    This is not correct for 1.8, so mask it.

--- spec/regression/GH-1962_Kernel_Array_coercion_spec.rb
+++ spec/regression/GH-1962_Kernel_Array_coercion_spec.rb
@@ -1,10 +1,12 @@
-describe "GH-1962: Kernel::Array" do
-  it "coerces Array-like objects that only define method_missing" do
-    o = Object.new
-    def o.method_missing(name, *args)
-      []
-    end
+if RUBY_VERSION >= "1.9"
+  describe "GH-1962: Kernel::Array" do
+    it "coerces Array-like objects that only define method_missing" do
+      o = Object.new
+      def o.method_missing(name, *args)
+        []
+      end
 
-    expect(Array(o)).to eq([])
+      expect(Array(o)).to eq([])
+    end
   end
 end
