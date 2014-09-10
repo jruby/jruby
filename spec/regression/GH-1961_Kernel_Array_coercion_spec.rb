d4eb922be9bb5965e9bee9482c575791ffd25126
Charles Oliver Nutter <headius@headius.com>
2014-09-10 17:37:25 -0500

    Specs for #1961 and #1962.

---
+++ spec/regression/GH-1961_Kernel_Array_coercion_spec.rb
@@ -0,0 +1,10 @@
+describe "GH-1961: Kernel::Array" do
+  it "coerces Array-like objects that only define method_missing" do
+    o = Object.new
+    def o.method_missing(name, *args)
+      []
+    end
+
+    expect(Array(o)).to eq([])
+  end
+end
