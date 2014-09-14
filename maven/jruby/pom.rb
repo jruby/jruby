d77d77f28950954c23b6c1512071fb0c09b6c58e
Christian Meier <m.kristian@web.de>
2014-09-14 10:29:01 +0100

    extract the osgi and j2ee integration tests into their own profile

--- maven/jruby/pom.rb
+++ maven/jruby/pom.rb
@@ -58,7 +58,7 @@ project 'JRuby Main Maven Artifact' do
     activation do
       jdk '1.8'
     end
-    plugin :invoker, :pomExcludes => ['osgi_all_inclusive_felix-3.2/pom.xml']
+    plugin :invoker, :pomExcludes => ['osgi_all_inclusive_felix-3.2/pom.xml', '${its.j2ee}', '${its.osgi}']
   end
   profile :id => :jdk6 do
     activation do
