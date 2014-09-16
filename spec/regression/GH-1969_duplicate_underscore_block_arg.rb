2fcee7ecb9536ea7b50192f1b74f97631a337de8
Subramanya Sastry <sss.lists@gmail.com>
2014-09-16 09:19:29 -0500

    Update regression spec

--- spec/regression/GH-1969_duplicate_underscore_block_arg.rb
+++ spec/regression/GH-1969_duplicate_underscore_block_arg.rb
@@ -7,8 +7,8 @@ def foo; yield 1,2,3,4,5,6,7; end
 describe 'Blocks with duplicate _ args' do
   it 'should not crash' do
     foo do |x,_,_,_,a,b,c|
-      bar {}
-      [x,a,b,c]
-    end.should_be [1,5,6,7]
+      yielder {}
+      [x,_,a,b,c]
+    end.should_be [1,2,5,6,7]
   end
 end
