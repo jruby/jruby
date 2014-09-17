57222ee986a5305dc84f657a08669f2fa6c4b1a3
Subramanya Sastry <sss.lists@gmail.com>
2014-09-16 22:57:38 -0500

    Fix #1980: Do not attempt to reuse IRReturnJump/IRBreakJump
    * Removed dump opt from b26a85e.
    * Added a regression spec.

--- core/src/main/java/org/jruby/ir/runtime/IRBreakJump.java
+++ core/src/main/java/org/jruby/ir/runtime/IRBreakJump.java
@@ -4,9 +4,6 @@ import org.jruby.exceptions.Unrescuable;
 import org.jruby.runtime.DynamicScope;
 import org.jruby.runtime.builtin.IRubyObject;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-
 public class IRBreakJump extends RuntimeException implements Unrescuable {
     public DynamicScope scopeToReturnTo;
     public IRubyObject breakValue;
@@ -15,19 +12,8 @@ public class IRBreakJump extends RuntimeException implements Unrescuable {
 
     private IRBreakJump() {}
 
-    // See https://jira.codehaus.org/browse/JRUBY-6523
-    // Dont use static threadlocals because they leak classloaders.
-    // Instead, use soft/weak references so that GC can collect these.
-
-    private static ThreadLocal<Reference<IRBreakJump>> threadLocalBJ = new ThreadLocal<Reference<IRBreakJump>>();
-
     public static IRBreakJump create(DynamicScope scopeToReturnTo, IRubyObject rv) {
-        IRBreakJump bj;
-        Reference<IRBreakJump> bjRef = threadLocalBJ.get();
-        if (bjRef == null || (bj = bjRef.get()) == null) {
-            bj = new IRBreakJump();
-            threadLocalBJ.set(new SoftReference<IRBreakJump>(bj));
-        }
+        IRBreakJump bj = new IRBreakJump();
         bj.scopeToReturnTo = scopeToReturnTo;
         bj.breakValue = rv;
         bj.caughtByLambda = false;
--- core/src/main/java/org/jruby/ir/runtime/IRReturnJump.java
+++ core/src/main/java/org/jruby/ir/runtime/IRReturnJump.java
@@ -3,28 +3,14 @@ package org.jruby.ir.runtime;
 import org.jruby.exceptions.Unrescuable;
 import org.jruby.runtime.DynamicScope;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-
 public class IRReturnJump extends RuntimeException implements Unrescuable {
     public DynamicScope methodToReturnFrom;
     public Object returnValue;
 
     private IRReturnJump() {}
 
-    // See https://jira.codehaus.org/browse/JRUBY-6523
-    // Dont use static threadlocals because they leak classloaders.
-    // Instead, use soft/weak references so that GC can collect these.
-
-    private static ThreadLocal<Reference<IRReturnJump>> threadLocalRJ = new ThreadLocal<Reference<IRReturnJump>>();
-
     public static IRReturnJump create(DynamicScope scope, Object rv) {
-        IRReturnJump rj;
-        Reference<IRReturnJump> rjRef = threadLocalRJ.get();
-        if (rjRef == null || (rj = rjRef.get()) == null) {
-            rj = new IRReturnJump();
-            threadLocalRJ.set(new SoftReference<IRReturnJump>(rj));
-        }
+        IRReturnJump rj = new IRReturnJump();
         rj.methodToReturnFrom = scope;
         rj.returnValue = rv;
         return rj;
