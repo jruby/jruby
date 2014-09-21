4e15ab21580f156c8293b1ccb895cf2f99450f7c
Uwe Kubosch <uwe@kubosch.no>
2014-09-21 22:55:53 +0200

    * Ensure the gem reports the correct version.

--- maven/jruby-jars/lib/jruby-jars/version.rb
+++ maven/jruby-jars/lib/jruby-jars/version.rb
@@ -1,5 +1,5 @@
 module JRubyJars
   MAVEN_VERSION = Dir[File.expand_path(File.dirname(File.dirname(__FILE__))) + '/jruby-core-complete-*jar' ].
       first.gsub(/^.*jruby-core-complete-|.jar$/, '')
-  VERSION = MAVEN_VERSION.sub('-SNAPSHOT', '').gsub('-', '.')
+  VERSION = MAVEN_VERSION.sub('-SNAPSHOT', '.dev').gsub('-', '.')
 end
