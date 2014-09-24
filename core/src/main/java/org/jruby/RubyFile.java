084eef9c25860cccdd65b625a877e7e230414175
Thomas E. Enebo <tom.enebo@gmail.com>
2014-09-24 12:59:08 -0500

    WTF is all this logic for?  Make readlink pass some more rubyspecs

--- core/src/main/java/org/jruby/RubyFile.java
+++ core/src/main/java/org/jruby/RubyFile.java
@@ -954,22 +954,10 @@ public class RubyFile extends RubyIO implements EncodingCapable {
     public static IRubyObject readlink(ThreadContext context, IRubyObject recv, IRubyObject path) {
         Ruby runtime = context.runtime;
         JRubyFile link = file(path);
-        
+
         try {
             String realPath = runtime.getPosix().readlink(link.toString());
-        
-            if (!RubyFileTest.exist_p(recv, path).isTrue()) {
-                throw runtime.newErrnoENOENTError(path.toString());
-            }
-        
-            if (!RubyFileTest.symlink_p(recv, path).isTrue()) {
-                // Can not check earlier, File.exist? might return false yet the symlink be there
-                if (!RubyFileTest.exist_p(recv, path).isTrue()) {
-                    throw runtime.newErrnoENOENTError(path.toString());
-                }
-                throw runtime.newErrnoEINVALError(path.toString());
-            }
-        
+
             if (realPath == null) {
                 throw runtime.newErrnoFromLastPOSIXErrno();
             }
