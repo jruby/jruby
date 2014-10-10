2bd4fe65c88782e02d78e549c2d0f9019f2cb54e
Benoit Daloze <eregontp@gmail.com>
2014-10-10 18:00:12 +0200

    [Truffle] Remove the last usage of isModuleFunction and needsSelf.

--- core/src/main/java/org/jruby/truffle/nodes/rubinius/ByteArrayNodes.java
+++ core/src/main/java/org/jruby/truffle/nodes/rubinius/ByteArrayNodes.java
@@ -8,6 +8,7 @@ import org.jruby.truffle.nodes.core.CoreClass;
 import org.jruby.truffle.nodes.core.CoreMethod;
 import org.jruby.truffle.nodes.core.CoreMethodNode;
 import org.jruby.truffle.nodes.dispatch.DispatchHeadNode;
+import org.jruby.truffle.runtime.RubyArguments;
 import org.jruby.truffle.runtime.RubyContext;
 import org.jruby.truffle.runtime.control.RaiseException;
 import org.jruby.truffle.runtime.core.RubyClass;
@@ -34,7 +35,8 @@ public abstract class ByteArrayNodes {
         }
     }
 
-    @CoreMethod(names = {"new", "allocate_sized"}, isModuleFunction = true, needsSelf = true, minArgs = 1, maxArgs = 1)
+    // FIXME(eregon): this should only be defined on the singleton class, not as an instance method.
+    @CoreMethod(names = {"new", "allocate_sized"}, isModuleFunction = true, needsSelf = false, minArgs = 1, maxArgs = 1)
     public abstract static class AllocateSizedNode extends CoreMethodNode {
         @Child
         protected DispatchHeadNode bytesToIntNode;
@@ -50,7 +52,8 @@ public abstract class ByteArrayNodes {
         }
 
         @Specialization
-        public RubyObject allocate_sized(VirtualFrame frame, RubyClass self, RubyObject bytes) {
+        public RubyObject allocate_sized(VirtualFrame frame, RubyObject bytes) {
+            RubyClass self = (RubyClass) RubyArguments.getSelf(frame.getArguments());
             return RubiniusByteArray.allocate_sized(this, self, TypeConversionUtils.convertToLong(this, bytesToIntNode, frame, bytes));
         }
     }
