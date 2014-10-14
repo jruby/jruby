0a161078bf4446b1897c7785605a177853ebf1c4
Chris Seaton <chris@chrisseaton.com>
2014-10-14 02:42:57 +0100

    [Truffle] PE specs work! PSD acid test can now be run every commit.

--- test/truffle/pe/core/float_pe.rb
+++ test/truffle/pe/core/float_pe.rb
@@ -1,9 +1,69 @@
 PETests.tests do
 
-  describe "A Fixnum" do
+  describe "A Float" do
     
     example "literal" do
       truffle_assert_constant 14.2
+    end
+    
+    describe "#+" do
+      
+      example "a Fixnum" do
+        truffle_assert_constant 14.0 + 2
+      end
+      
+      counter_example "a Bignum" do
+        truffle_assert_constant 14.0 + 0xfffffffffffffffffffffffffffffff
+      end
+      
+      example "a Float" do
+        truffle_assert_constant 14.0 + 2.0
+      end
+      
+      counter_example "rand" do
+        truffle_assert_constant 14.0 + rand
+      end
+
+    end
+    
+    describe "#*" do
+      
+      example "a Fixnum" do
+        truffle_assert_constant 14.0 * 2
+      end
+      
+      counter_example "a Bignum" do
+        truffle_assert_constant 14.0 * 0xfffffffffffffffffffffffffffffff
+      end
+      
+      example "a Float" do
+        truffle_assert_constant 14.0 * 2.0
+      end
+      
+      counter_example "rand" do
+        truffle_assert_constant 14.0 * rand
+      end
+
+    end
+    
+    describe "#/" do
+      
+      example "a Fixnum" do
+        truffle_assert_constant 14.0 / 2
+      end
+      
+      counter_example "a Bignum" do
+        truffle_assert_constant 14.0 / 0xfffffffffffffffffffffffffffffff
+      end
+      
+      example "a Float" do
+        truffle_assert_constant 14.0 / 2.0
+      end
+      
+      counter_example "rand" do
+        truffle_assert_constant 14.0 / rand
+      end
+
     end
 
   end
