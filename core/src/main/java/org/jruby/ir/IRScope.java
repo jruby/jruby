jruby/jruby

commit fa44bc1f6ed376ed16a96a66d61c2d0932df31f2

Dont force build CFG - let dependencies take care of it.
* OptimizeTempVarsPass does not run if CFG has been built.
* It has been disabled for a while.
* Improves interpreter performance.

core/src/main/java/org/jruby/ir/IRScope.java
@@ -596,11 +596,6 @@ public abstract class IRScope implements ParseResult {
     }
 
     protected void initScope(boolean jitMode) {
-        // Build CFG and run compiler passes, if necessary
-        if (getCFG() == null) {
-            buildCFG();
-        }
-
         runCompilerPasses(getManager().getCompilerPasses(this));
 
         if (!jitMode && RubyInstanceConfig.IR_COMPILER_PASSES == null) {

commit a33c383131865df97b8641da9648668b864b2e90

Ensure CFG is built at the end of IRScope.initScope()
* Unbreaks failing MRI tests that I broke in fa44bc1f

core/src/main/java/org/jruby/ir/IRScope.java
@@ -606,6 +606,12 @@ public abstract class IRScope implements ParseResult {
             //   commandline (so as to honor the commandline request).
             optimizeSimpleScopes();
         }
+
+        // If at the end, the cfg is still not build, build it.
+        // (ex: unsafe scopes for which passes don't run).
+        if (getCFG() == null) {
+            buildCFG();
+        }
     }
 
     /** Make version specific to scope which needs it (e.g. Closure vs non-closure). */