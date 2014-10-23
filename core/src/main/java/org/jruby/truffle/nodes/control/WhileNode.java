e6fc529a5ba6460cd363c3685be9d304ea3a2a2c
Chris Seaton <chris@chrisseaton.com>
2014-10-23 03:52:14 +0100

    [Truffle] Use new LoopNode for while (gives us OSR for the first time).

--- core/src/main/java/org/jruby/truffle/nodes/control/WhileNode.java
+++ core/src/main/java/org/jruby/truffle/nodes/control/WhileNode.java
@@ -19,58 +19,53 @@ import org.jruby.truffle.nodes.cast.*;
 import org.jruby.truffle.runtime.*;
 import org.jruby.truffle.runtime.control.*;
 
-/**
- * Represents a Ruby {@code while} statement.
- */
 public class WhileNode extends RubyNode {
 
-
-
-    @Child protected BooleanCastNode condition;
-    @Child protected RubyNode body;
-
-    private final BranchProfile breakProfile = BranchProfile.create();
-    private final BranchProfile nextProfile = BranchProfile.create();
-    private final BranchProfile redoProfile = BranchProfile.create();
+    @Child protected LoopNode loopNode;
 
     public WhileNode(RubyContext context, SourceSection sourceSection, BooleanCastNode condition, RubyNode body) {
         super(context, sourceSection);
-        this.condition = condition;
-        this.body = body;
+        loopNode = Truffle.getRuntime().createLoopNode(new WhileRepeatingNode(condition, body));
     }
 
     @Override
     public Object execute(VirtualFrame frame) {
-        int count = 0;
-
         try {
-            outer: while (condition.executeBoolean(frame)) {
-                while (true) {
-                    if (CompilerDirectives.inInterpreter()) {
-                        count++;
-                    }
-
-                    try {
-                        body.execute(frame);
-                        continue outer;
-                    } catch (BreakException e) {
-                        breakProfile.enter();
-                        return e.getResult();
-                    } catch (NextException e) {
-                        nextProfile.enter();
-                        continue outer;
-                    } catch (RedoException e) {
-                        redoProfile.enter();
-                    }
-                }
-            }
-        } finally {
-            if (CompilerDirectives.inInterpreter()) {
-                ((RubyRootNode) getRootNode()).reportLoopCountThroughBlocks(count);
-            }
+            loopNode.executeLoop(frame);
+        } catch (BreakException e) {
+            return e.getResult();
         }
 
         return getContext().getCoreLibrary().getNilObject();
+    }
+
+    public class WhileRepeatingNode extends Node implements RepeatingNode {
+
+        @Child protected BooleanCastNode condition;
+        @Child protected RubyNode body;
+
+        public WhileRepeatingNode(BooleanCastNode condition, RubyNode body) {
+            this.condition = condition;
+            this.body = body;
+        }
+
+        @Override
+        public boolean executeRepeating(VirtualFrame frame) {
+            if (!condition.executeBoolean(frame)) {
+                return false;
+            }
+
+            while (true) {
+                try {
+                    body.execute(frame);
+                    return true;
+                } catch (NextException e) {
+                    return true;
+                } catch (RedoException e) {
+                }
+            }
+        }
+
     }
 
 }
