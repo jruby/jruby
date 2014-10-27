6f416d89c5f9bf95f6f401987c9e5a49933e0dfc
Benoit Daloze <eregontp@gmail.com>
2014-10-27 18:43:09 +0100

    Revert "[Truffle] Tag a couple of failing PE tests."
    This reverts commit c55e31068387b1582aa6b14ba37597457f846b81.

--- test/truffle/pe/core/hash_pe.rb
+++ test/truffle/pe/core/hash_pe.rb
@@ -7,7 +7,7 @@ PETests.tests do
       truffle_assert_constant hash[:b]
     end
     
-    broken_example "mapped to an Array and indexed by a constant" do
+    example "mapped to an Array and indexed by a constant" do
       hash = {a: 0, b: 1, c: 2}
       array = hash.map { |k, v| v }
       truffle_assert_constant array[0]
