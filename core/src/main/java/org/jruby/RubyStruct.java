5f717bd67690fddbc317646f90c30d3e1aaa731f
Subramanya Sastry <sss.lists@gmail.com>
2014-10-28 19:57:21 -0500

    Fix RubyStruct to set the proper eval-type for new struct bodies.
    * Added new unit tests for JRuby (perhaps this should go into
      rubyspec as well).

--- core/src/main/java/org/jruby/RubyStruct.java
+++ core/src/main/java/org/jruby/RubyStruct.java
@@ -252,6 +252,8 @@ public class RubyStruct extends RubyObject {
         }
         
         if (block.isGiven()) {
+            // Since this defines a new class, run the block as a module-eval.
+            block.setEvalType(EvalType.MODULE_EVAL);
             // Struct bodies should be public by default, so set block visibility to public. JRUBY-1185.
             block.getBinding().setVisibility(Visibility.PUBLIC);
             block.yieldNonArray(runtime.getCurrentContext(), null, newStruct);
