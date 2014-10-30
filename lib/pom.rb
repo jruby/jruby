23390098bdc7dd02b15cff3f275c952382e85d96
Charles Oliver Nutter <headius@headius.com>
2014-10-30 12:30:45 -0500

    Bump test-unit to 3.0.3.

--- lib/pom.rb
+++ lib/pom.rb
@@ -34,7 +34,7 @@ default_gems =
    ImportedGem.new( 'ffi', '1.9.3', true ),
    ImportedGem.new( 'jar-dependencies', '0.1.2', true ),
    ImportedGem.new( 'minitest', '5.4.1', true ),
-   ImportedGem.new( 'test-unit', '3.0.1', true ),
+   ImportedGem.new( 'test-unit', '3.0.3', true ),
    ImportedGem.new( 'power_assert', '0.1.4', true )
   ]
 
