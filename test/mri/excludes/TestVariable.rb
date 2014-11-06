dda85eef746388a120d1e66f59b6fb2671e6eb9e
Charles Oliver Nutter <headius@headius.com>
2014-11-06 11:25:21 -0600

    Exclude variable shadowing tests for #2129.

--- test/mri/excludes/TestVariable.rb
+++ test/mri/excludes/TestVariable.rb
@@ -1,2 +1,4 @@
 exclude :test_global_variable_0, "needs investigation"
+exclude :test_shadowing_local_variables, "#2129"
+exclude :test_shadowing_block_local_variables, "#2129"
 exclude :test_variable, "needs investigation"
