35b59c70818293db6e77345aedc07afe3af06cb7
Chris Seaton <chris@chrisseaton.com>
2014-11-18 13:09:15 -0800

    [Truffle] Also build a distribution with no version numbers to make integration easier.

--- tool/build-graal-bundles.sh
+++ tool/build-graal-bundles.sh
@@ -57,6 +57,10 @@ function pack {
     targetname=jruby-dist-$version+graal-$1-x86_64-bin.tar.gz
     tar -zcf $targetname jruby-$version || exit $?
     shasum -a 1 $targetname > $targetname.sha1 || exit $?
+    cp -r jruby-$version jruby-master || exit $?
+    targetname=jruby-dist-master+graal-$1-x86_64-bin.tar.gz
+    tar -zcf $targetname jruby-master || exit $?
+    shasum -a 1 $targetname > $targetname.sha1 || exit $?
 }
 
 pack "linux" "b132" "0.5"
