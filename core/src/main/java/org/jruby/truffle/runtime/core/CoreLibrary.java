664774c9ccf8a96808fb1ae9d00348f1e77910dc
Chris Seaton <chris@chrisseaton.com>
2014-11-21 12:35:56 -0800

    [Truffle] Fix language specs.

--- core/src/main/java/org/jruby/truffle/runtime/core/CoreLibrary.java
+++ core/src/main/java/org/jruby/truffle/runtime/core/CoreLibrary.java
@@ -328,6 +328,7 @@ public class CoreLibrary {
         encodingClass.setConstant(null, "EUC_JP", RubyEncoding.getEncoding(context, EUCJPEncoding.INSTANCE));
         encodingClass.setConstant(null, "Windows_31J", RubyEncoding.getEncoding(context, SJISEncoding.INSTANCE));
         encodingClass.setConstant(null, "Big5", RubyEncoding.getEncoding(context, BIG5Encoding.INSTANCE));
+        encodingClass.setConstant(null, "IBM437", RubyEncoding.getEncoding(context, USASCIIEncoding.INSTANCE));
 
     }
 
