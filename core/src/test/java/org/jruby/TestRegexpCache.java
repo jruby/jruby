6e5f32ed2106dfa30c8eb85868d44663fa25e044
Charles Oliver Nutter <headius@headius.com>
2014-11-24 11:59:53 -0600

    Different strategy for weak cache test.

--- core/src/test/java/org/jruby/TestRegexpCache.java
+++ core/src/test/java/org/jruby/TestRegexpCache.java
@@ -10,33 +10,20 @@ public class TestRegexpCache extends TestRubyBase {
     private static final ByteList GH2078_TEST_BYTELIST = ByteList.create("GH2078");
     public void testCacheRetention() {
         RubyRegexp.quotedPatternCache.clear();
-        Regex regex = RubyRegexp.getQuotedRegexpFromCache(runtime, GH2078_TEST_BYTELIST, GH2078_TEST_BYTELIST.getEncoding(), RegexpOptions.NULL_OPTIONS);
+
+        // Should be the same object if cached
+        assertSame(RubyRegexp.getQuotedRegexpFromCache(runtime, GH2078_TEST_BYTELIST, GH2078_TEST_BYTELIST.getEncoding(), RegexpOptions.NULL_OPTIONS), RubyRegexp.getQuotedRegexpFromCache(runtime, GH2078_TEST_BYTELIST, GH2078_TEST_BYTELIST.getEncoding(), RegexpOptions.NULL_OPTIONS));
 
         // Should only have one entry
         assertEquals(1, RubyRegexp.quotedPatternCache.size());
 
-        // Should be the same object if cached
-        assertSame(regex, RubyRegexp.getQuotedRegexpFromCache(runtime, GH2078_TEST_BYTELIST, GH2078_TEST_BYTELIST.getEncoding(), RegexpOptions.NULL_OPTIONS));
-
-        // clear reference and attempt to trigger GC
-        regex = null;
-        System.gc();
-        Thread.yield();
-        System.gc();
-        Thread.yield();
-        System.gc();
-        Thread.yield();
-        System.gc();
-        Thread.yield();
-        System.gc();
-        Thread.yield();
-        System.gc();
-        Thread.yield();
-        System.gc();
-        Thread.yield();
-        System.gc();
-        Thread.yield();
-        System.gc();
+        // attempt to trigger GC and clean up cache for ten seconds
+        long time = System.currentTimeMillis();
+        while ((System.currentTimeMillis() - time) < 10000) {
+            Thread.yield();
+            System.gc();
+            if (RubyRegexp.quotedPatternCache.size() == 0) break;
+        }
 
         // Should be no retained references once cleaned
         assertEquals(0, RubyRegexp.quotedPatternCache.size());
