3ce13291dc442d206b2042c87d4f187a79c6514e
Christian Meier <m.kristian@web.de>
2014-11-25 09:40:44 +0000

    clean up classloader type hierachy
    * only the classloader runtime.getJRubyClassLoader needs to have
      all the extra features. all other classloaders just needs to
      be able to define classes and do not need the extra overhead of
      loading and finding resources and adding jar files during runtime.
    * the OneShotClassloader is only used with runtime.getJRubyClassLoader
      so ensure this relation via types
    * the JRubyClassLoader is used at various places also as "flag via instanceof".
      keep this and all other uses of JRubyClassLoader as it was before
    * replace on use of OneShotClassloader with JRubyClassLoader to check if it possible
      to load byte code at runtime

--- core/src/main/java/org/jruby/util/OneShotClassLoader.java
+++ core/src/main/java/org/jruby/util/OneShotClassLoader.java
@@ -6,14 +6,12 @@ import java.security.ProtectionDomain;
  * Represents a class loader designed to load exactly one class.
 */
 public class OneShotClassLoader extends ClassLoader implements ClassDefiningClassLoader {
-    private final static ProtectionDomain DEFAULT_DOMAIN =
-        JRubyClassLoader.class.getProtectionDomain();
 
-    public OneShotClassLoader(ClassLoader parent) {
+    public OneShotClassLoader(JRubyClassLoader parent) {
         super(parent);
     }
 
     public Class<?> defineClass(String name, byte[] bytes) {
-        return super.defineClass(name, bytes, 0, bytes.length, DEFAULT_DOMAIN);
+        return super.defineClass(name, bytes, 0, bytes.length, JRubyClassLoader.DEFAULT_DOMAIN);
      }
 }
