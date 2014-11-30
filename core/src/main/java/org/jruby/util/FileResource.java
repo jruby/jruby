57c25133fc8acc3ba2d6942284a51159074dae4f
Christian Meier <m.kristian@web.de>
2014-11-30 12:21:20 +0000

    refactor FileResource.inputStream to be easier to use
    for any client code which needs to convert a uri-like path to an inputstream
    the way to go is ```JRubyFile.create(runtime, path).inputStream()```

--- core/src/main/java/org/jruby/util/FileResource.java
+++ core/src/main/java/org/jruby/util/FileResource.java
@@ -1,10 +1,11 @@
 package org.jruby.util;
 
+import java.io.InputStream;
+
 import jnr.posix.FileStat;
-import jnr.posix.POSIX;
+
 import org.jruby.util.io.ChannelDescriptor;
 import org.jruby.util.io.ModeFlags;
-import java.io.InputStream;
 
 /**
  * This is a shared interface for files loaded as {@link java.io.File} and {@link java.util.zip.ZipEntry}.
@@ -38,9 +39,16 @@ public interface FileResource {
     // otherwise.
     JRubyFile hackyGetJRubyFile();
 
-    // Opens a new input stream to read the contents of a resource and returns it.
-    // Note that implementations may be allocating native memory for the stream, so
-    // callers need to close this when they are done with it.
-    InputStream openInputStream();
+
+    /**
+     * Opens a new input stream to read the contents of a resource and returns it.
+     * Note that implementations may be allocating native memory for the stream, so
+     * callers need to close this when they are done with it. users of this 
+     * method should follow the pattern: close the stream where you open it.
+     *
+     * @return just opened InputStream
+     * @throws ResourceException is the file does not exists or if the resource is a directory
+     */
+    InputStream inputStream() throws ResourceException;
     ChannelDescriptor openDescriptor(ModeFlags flags, int perm) throws ResourceException;
 }
