a30c5372203ab84643cdc9c3aceb8a68dd753a94
Charles Oliver Nutter <headius@headius.com>
2014-12-01 18:54:27 -0600

    Remove never-implemented flag --indy. Fixes #2231.

--- core/src/main/java/org/jruby/util/cli/OutputStrings.java
+++ core/src/main/java/org/jruby/util/cli/OutputStrings.java
@@ -64,7 +64,6 @@ public class OutputStrings {
                 .append("  --client          use the non-optimizing \"client\" JVM\n")
                 .append("                      (improves startup; default)\n")
                 .append("  --server          use the optimizing \"server\" JVM (improves perf)\n")
-                .append("  --(no-)indy       use invokedynamic (Java 7+) to optimize (perf++, startup--)\n")
                 .append("  --manage          enable remote JMX management and monitoring of the VM\n")
                 .append("                      and JRuby\n")
                 .append("  --headless        do not launch a GUI window, no matter what\n")
