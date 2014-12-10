986bd8d514352edba2267d23e40a45454b76dd3f
Charles Oliver Nutter <headius@headius.com>
2014-12-10 12:38:36 -0600

    Strip trailing options hash in Open3.popen3 and warn if non-empty.
    Fixes #2298.

---
+++ spec/regression/GH-2298_capture3_accepts_trailing_options_spec.rb
@@ -0,0 +1,10 @@
+require 'open3'
+
+describe "Open3.popen3" do
+  it "accepts and ignores empty trailing options hash" do
+    # capture3 is an easy way to test popen3 options behavior
+    result = Open3.capture3('echo')
+
+    expect(result[0]).to eq "\n"
+  end
+end
