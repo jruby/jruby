f9fbfa789ad1faeffef31456c2ce98c227f9c6f3
Kevin Menard <kevin@nirvdrum.com>
2014-12-09 21:19:20 -0500

    Commented out flaky spec that also wasn't testing what it was supposed to.
    See #2299: 'Improve FFI::Function can wrap a blocking function spec' for details.

--- spec/ffi/function_spec.rb
+++ spec/ffi/function_spec.rb
@@ -63,12 +63,12 @@ describe FFI::Function do
   end
 
   it 'can wrap a blocking function' do
-    fp = FFI::Function.new(:void, [ :int ], @libtest.find_function('testBlocking'), :blocking => true)
-    time = Time.now
-    threads = []
-    threads << Thread.new { fp.call(2) }
-    threads << Thread.new(time) { expect(Time.now - time).to be < 1 }
-    threads.each { |t| t.join }
+    # fp = FFI::Function.new(:void, [ :int ], @libtest.find_function('testBlocking'), :blocking => true)
+    # time = Time.now
+    # threads = []
+    # threads << Thread.new { fp.call(2) }
+    # threads << Thread.new(time) { expect(Time.now - time).to be < 1 }
+    # threads.each { |t| t.join }
   end
 
   it 'autorelease flag is set to true by default' do
