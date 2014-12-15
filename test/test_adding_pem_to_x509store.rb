463f8ad1920230c7c66499fe53e71550e629c76a
Christian Meier <m.kristian@web.de>
2014-12-15 13:22:12 +0100

    fix pem test since some are not self signed

--- test/test_adding_pem_to_x509store.rb
+++ test/test_adding_pem_to_x509store.rb
@@ -3,17 +3,19 @@ require 'openssl'
 
 class TestOpenssl < Test::Unit::TestCase
   def test_adding_pem
+OpenSSL.debug = true
     # mimic what rubygems/request#add_rubygems_trusted_certs does
     # to find the pem certificates
     base = $LOAD_PATH.detect { |p| p =~ /shared/ }
     pems = Dir[ File.join(base, 'rubygems/ssl_certs/*pem') ]
-    assert_equal( 7, pems.size )
+    assert_equal( 9, pems.size )
     pems.each do |pem|
       store = OpenSSL::X509::Store.new
       cert = OpenSSL::X509::Certificate.new(File.read(pem))
       assert( !store.verify(cert) )
       store.add_file(pem)
-      assert( store.verify(cert) )
+      # only verify on self signed certifactes
+      assert( store.verify(cert) ) unless pem =~ /AddTrustExternalCARoot.pem/
     end
   end
 end
