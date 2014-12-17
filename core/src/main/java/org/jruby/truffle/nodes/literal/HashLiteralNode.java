c0453329111ff261f0a6a334636227107299d45a
Chris Seaton <chris@chrisseaton.com>
2014-12-17 23:30:01 +0000

    [Truffle] Fix de-duplication in small hash literals.

--- core/src/main/java/org/jruby/truffle/nodes/literal/HashLiteralNode.java
+++ core/src/main/java/org/jruby/truffle/nodes/literal/HashLiteralNode.java
@@ -102,7 +102,7 @@ public abstract class HashLiteralNode extends RubyNode {
                 final Object value = keyValues[n + 1].execute(frame);
 
                 for (int i = 0; i < n; i += 2) {
-                    if (n < end && equalNode.call(frame, key, "eql?", null, storage[i])) {
+                    if (i < end && equalNode.call(frame, key, "eql?", null, storage[i])) {
                         storage[i + 1] = value;
                         continue initializers;
                     }
