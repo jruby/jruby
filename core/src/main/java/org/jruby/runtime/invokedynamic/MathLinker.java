dcd9597d78dcd21200799ecefa4ce36b67878256
Charles Oliver Nutter <headius@headius.com>
2014-12-22 15:46:50 -0600

    Plug in specialized fixnum RHS 1-arg calls, as in 1.7.

--- core/src/main/java/org/jruby/runtime/invokedynamic/MathLinker.java
+++ core/src/main/java/org/jruby/runtime/invokedynamic/MathLinker.java
@@ -51,44 +51,44 @@ import org.jruby.util.log.LoggerFactory;
 public class MathLinker {
     private static final Logger LOG = LoggerFactory.getLogger("MathLinker");
     
-//    public static CallSite fixnumOperatorBootstrap(Lookup lookup, String name, MethodType type, long value, String file, int line) throws NoSuchMethodException, IllegalAccessException {
-//        String[] names = name.split(":");
-//        String operator = JavaNameMangler.demangleMethodName(names[1]);
-//        JRubyCallSite site = new JRubyCallSite(lookup, type, CallType.NORMAL, file, line, operator, false, false, true);
-//
-//        MethodHandle target = lookup.findStatic(MathLinker.class, "fixnumOperator",
-//                methodType(IRubyObject.class, ThreadContext.class, IRubyObject.class, IRubyObject.class, JRubyCallSite.class, long.class));
-//        target = insertArguments(target, 3, site, value);
-//
-//        site.setTarget(target);
-//        return site;
-//    }
-//
-//    public static CallSite fixnumBooleanBootstrap(Lookup lookup, String name, MethodType type, long value, String file, int line) throws NoSuchMethodException, IllegalAccessException {
-//        String[] names = name.split(":");
-//        String operator = JavaNameMangler.demangleMethodName(names[1]);
-//        JRubyCallSite site = new JRubyCallSite(lookup, type, CallType.NORMAL, file, line, operator, false, false, true);
-//
-//        MethodHandle target = lookup.findStatic(MathLinker.class, "fixnumBoolean",
-//                methodType(boolean.class, ThreadContext.class, IRubyObject.class, IRubyObject.class, JRubyCallSite.class, long.class));
-//        target = insertArguments(target, 3, site, value);
-//
-//        site.setTarget(target);
-//        return site;
-//    }
-//
-//    public static CallSite floatOperatorBootstrap(Lookup lookup, String name, MethodType type, double value, String file, int line) throws NoSuchMethodException, IllegalAccessException {
-//        String[] names = name.split(":");
-//        String operator = JavaNameMangler.demangleMethodName(names[1]);
-//        JRubyCallSite site = new JRubyCallSite(lookup, type, CallType.NORMAL, file, line, operator, false, false, true);
-//
-//        MethodHandle target = lookup.findStatic(MathLinker.class, "floatOperator",
-//                methodType(IRubyObject.class, ThreadContext.class, IRubyObject.class, IRubyObject.class, JRubyCallSite.class, double.class));
-//        target = insertArguments(target, 3, site, value);
-//
-//        site.setTarget(target);
-//        return site;
-//    }
+    public static CallSite fixnumOperatorBootstrap(Lookup lookup, String name, MethodType type, long value, String file, int line) throws NoSuchMethodException, IllegalAccessException {
+        String[] names = name.split(":");
+        String operator = JavaNameMangler.demangleMethodName(names[1]);
+        JRubyCallSite site = new JRubyCallSite(lookup, type, CallType.NORMAL, file, line, operator, true);
+
+        MethodHandle target = lookup.findStatic(MathLinker.class, "fixnumOperator",
+                methodType(IRubyObject.class, ThreadContext.class, IRubyObject.class, IRubyObject.class, JRubyCallSite.class, long.class));
+        target = insertArguments(target, 3, site, value);
+
+        site.setTarget(target);
+        return site;
+    }
+
+    public static CallSite fixnumBooleanBootstrap(Lookup lookup, String name, MethodType type, long value, String file, int line) throws NoSuchMethodException, IllegalAccessException {
+        String[] names = name.split(":");
+        String operator = JavaNameMangler.demangleMethodName(names[1]);
+        JRubyCallSite site = new JRubyCallSite(lookup, type, CallType.NORMAL, file, line, operator, true);
+
+        MethodHandle target = lookup.findStatic(MathLinker.class, "fixnumBoolean",
+                methodType(boolean.class, ThreadContext.class, IRubyObject.class, IRubyObject.class, JRubyCallSite.class, long.class));
+        target = insertArguments(target, 3, site, value);
+
+        site.setTarget(target);
+        return site;
+    }
+
+    public static CallSite floatOperatorBootstrap(Lookup lookup, String name, MethodType type, double value, String file, int line) throws NoSuchMethodException, IllegalAccessException {
+        String[] names = name.split(":");
+        String operator = JavaNameMangler.demangleMethodName(names[1]);
+        JRubyCallSite site = new JRubyCallSite(lookup, type, CallType.NORMAL, file, line, operator, true);
+
+        MethodHandle target = lookup.findStatic(MathLinker.class, "floatOperator",
+                methodType(IRubyObject.class, ThreadContext.class, IRubyObject.class, IRubyObject.class, JRubyCallSite.class, double.class));
+        target = insertArguments(target, 3, site, value);
+
+        site.setTarget(target);
+        return site;
+    }
     
     public static IRubyObject fixnumOperator(ThreadContext context, IRubyObject caller, IRubyObject self, JRubyCallSite site, long value) throws Throwable {
         String operator = site.name();
