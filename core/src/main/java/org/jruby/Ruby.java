4710b5f9d20ac6975b9eb2b1fa40ba1ab805baaa
Kevin Menard <kevin@nirvdrum.com>
2014-12-31 10:16:20 -0500

    Fixed compilation error.

--- core/src/main/java/org/jruby/Ruby.java
+++ core/src/main/java/org/jruby/Ruby.java
@@ -1537,7 +1537,11 @@ public final class Ruby implements Constantizable {
         }
 
         // Fiber depends on thread library, so we load it here
-        new ThreadLibrary().load(this, false);
+        try {
+            new ThreadLibrary().load(this, false);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         new ThreadFiberLibrary().load(this, false);
 
