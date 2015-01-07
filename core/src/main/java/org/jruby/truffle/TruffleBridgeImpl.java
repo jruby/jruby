2ecddc65943c20f1375be4b9f7b9d3bfde208f14
Chris Seaton <chris@chrisseaton.com>
2015-01-07 01:21:48 +0000

    [Truffle] Import nil from Rubinius.

--- core/src/main/java/org/jruby/truffle/TruffleBridgeImpl.java
+++ core/src/main/java/org/jruby/truffle/TruffleBridgeImpl.java
@@ -80,7 +80,6 @@ public class TruffleBridgeImpl implements TruffleBridge {
         CoreMethodNodeManager.addCoreMethodNodes(rubyObjectClass, MatchDataNodesFactory.getFactories());
         CoreMethodNodeManager.addCoreMethodNodes(rubyObjectClass, MathNodesFactory.getFactories());
         CoreMethodNodeManager.addCoreMethodNodes(rubyObjectClass, ModuleNodesFactory.getFactories());
-        CoreMethodNodeManager.addCoreMethodNodes(rubyObjectClass, NilClassNodesFactory.getFactories());
         CoreMethodNodeManager.addCoreMethodNodes(rubyObjectClass, NumericNodesFactory.getFactories());
         CoreMethodNodeManager.addCoreMethodNodes(rubyObjectClass, ObjectSpaceNodesFactory.getFactories());
         CoreMethodNodeManager.addCoreMethodNodes(rubyObjectClass, ProcessNodesFactory.getFactories());
