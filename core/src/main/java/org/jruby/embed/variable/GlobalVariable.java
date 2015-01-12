7b72baab243db129b3ed9999587580d9fc5c5ac9
kares <self@kares.org>
2015-01-12 15:17:20 +0100

    [embed] cleanup predefined global variable name checking

--- core/src/main/java/org/jruby/embed/variable/GlobalVariable.java
+++ core/src/main/java/org/jruby/embed/variable/GlobalVariable.java
@@ -128,13 +128,15 @@ public class GlobalVariable extends AbstractVariable {
         IRubyObject value = globalVars.get(key);
         updateGlobalVar(vars, (RubyObject) runtime.getTopSelf(), key, value);
     }
-    
-    private static String[] patterns = {
-            "\\$([\\u0021-\\u0040]|\\u005c|[\\u005e-\\u0060]|\\u007e)",
-            "\\$-(\\d|[A-z])"
-        };
-    
-    private static String [] predefined_names = {
+
+    private static final String[] PREDEFINED_PATTERNS = {
+        "\\$([\\u0021-\\u0040]|\\u005c|[\\u005e-\\u0060]|\\u007e)",
+        "\\$-(\\d|[A-z])"
+    };
+    private static final Set<String> PREDEFINED_NAMES = new HashSet<String>();
+
+    static {
+        final String[] NAMES = {
             "$DEBUG", "$F", "$FILENAME", "$KCODE", "$LOAD_PATH", "$SAFE", "$VERBOSE", "$CLASSPATH", "$LOADED_FEATURES",
             "$PROGRAM_NAME", "$FIELD_SEPARATOR", "$ERROR_POSITION", "$DEFAULT_OUTPUT", "$PREMATCH", "$RS", "$MATCH",
             "$LAST_READ_LINE", "$FS", "$INPUT_RECORD_SEPARATOR", "$PID", "$NR", "$ERROR_INFO", "$PROCESS_ID",
@@ -142,33 +144,14 @@ public class GlobalVariable extends AbstractVariable {
             "$IGNORECASE", "$DEFAULT_INPUT", "$OFS", "$OUTPUT_FIELD_SEPARATOR", "$POSTMATCH", "$ORS",
             "$configure_args", "$deferr", "$defout", "$expect_verbose", "$stderr", "$stdin", "$stdout"
         };
-    private static Set<String> predefined = new HashSet<String>();
-    
-    static {
-        predefined.addAll(Arrays.asList(predefined_names));
+        PREDEFINED_NAMES.addAll(Arrays.asList(NAMES));
     }
 
-    protected static boolean isPredefined(String name) {
-        /*
-        String[] patterns = {
-            "\\$([\\u0021-\\u0040]|\\u005c|[\\u005e-\\u0060]|\\u007e)",
-            "\\$-(\\d|[A-z])",
-            "\\$(DEBUG|F|FILENAME|KCODE|LOAD_PATH|SAFE|VERBOSE|CLASSPATH|LOADED_FEATURES|PROGRAM_NAME)",
-            "\\$(FIELD_SEPARATOR|ERROR_POSITION|DEFAULT_OUTPUT|PREMATCH|RS|MATCH|LAST_READ_LINE|FS|INPUT_RECORD_SEPARATOR)",
-            "\\$(PID|NR|ERROR_INFO|PROCESS_ID|OUTPUT_RECORD_SEPARATOR|INPUT_LINE_NUMBER|LAST_PAREN_MATCH|LAST_MATCH_INFO|CHILD_STATUS)",
-            "\\$(IGNORECASE|DEFAULT_INPUT|OFS|OUTPUT_FIELD_SEPARATOR|POSTMATCH|ORS)",
-            "\\$(configure_args|deferr|defout|expect_verbose|stderr|stdin|stdout)"
-        };
-         */
-        
-        for (String p : GlobalVariable.patterns) {
-            if (name.matches(p)) {
-                return true;
-            }
+    protected static boolean isPredefined(final String name) {
+        for ( String pattern : PREDEFINED_PATTERNS ) {
+            if ( name.matches(pattern) ) return true;
         }
-        
-        if (GlobalVariable.predefined.contains(name)) return true;
-        return false;
+        return GlobalVariable.PREDEFINED_NAMES.contains(name);
     }
 
     /**
