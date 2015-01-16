60831545df8f11092ab37405081ec7f27a2bc587
Smit Shah <who828@gmail.com>
2015-01-17 00:00:49 +0530

    Implemented String#scrub! method

--- core/src/main/java/org/jruby/RubyString.java
+++ core/src/main/java/org/jruby/RubyString.java
@@ -5742,6 +5742,19 @@ public class RubyString extends RubyObject implements EncodingCapable, MarshalEn
         return newStr;
     }
 
+    @JRubyMethod(name="scrub!")
+    public IRubyObject scrub_bang(ThreadContext context, Block block) {
+        return scrub_bang(context, context.nil, block);
+    }
+
+    // MRI: str_scrub arity 1
+    @JRubyMethod(name="scrub!")
+    public IRubyObject scrub_bang(ThreadContext context, IRubyObject repl, Block block) {
+        IRubyObject newStr = strScrub(context, repl, block);
+        if (!newStr.isNil()) return replace(newStr);
+        return this;
+    }
+
     /**
      * Mutator for internal string representation.
      *
