b69dd1b6195212ac9a354e6541668eb5f41c4489
Chris Seaton <chris@chrisseaton.com>
2015-01-25 18:35:01 +0000

    [Truffle] Some of Numeric#step

--- core/src/main/ruby/jruby/truffle/core/rubinius/kernel/common/numeric.rb
+++ core/src/main/ruby/jruby/truffle/core/rubinius/kernel/common/numeric.rb
@@ -210,4 +210,59 @@ class Numeric
     end
   end
 
+  def step(limit, step=1)
+    return to_enum(:step, limit, step) unless block_given?
+
+    raise ArgumentError, "step cannot be 0" if step == 0
+
+    value = self
+    if value.kind_of? Float or limit.kind_of? Float or step.kind_of? Float
+      # Ported from MRI
+
+      value = FloatValue(value)
+      limit = FloatValue(limit)
+      step =  FloatValue(step)
+
+      if step.infinite?
+        yield value if step > 0 ? value <= limit : value >= limit
+      else
+        err = (value.abs + limit.abs + (limit - value).abs) / step.abs * Float::EPSILON
+        if err.finite?
+          err = 0.5 if err > 0.5
+          n = ((limit - value) / step + err).floor
+          i = 0
+          if step > 0
+            while i <= n
+              d = i * step + value
+              d = limit if limit < d
+              yield d
+              i += 1
+            end
+          else
+            while i <= n
+              d = i * step + value
+              d = limit if limit > d
+              yield d
+              i += 1
+            end
+          end
+        end
+      end
+    else
+      if step > 0
+        until value > limit
+          yield value
+          value += step
+        end
+      else
+        until value < limit
+          yield value
+          value += step
+        end
+      end
+    end
+
+    return self
+  end
+
 end
