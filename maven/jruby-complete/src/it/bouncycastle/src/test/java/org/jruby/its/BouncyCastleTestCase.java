0e20c3dea70a3d38654d814583c13c7d8203b163
Charles Oliver Nutter <headius@headius.com>
2015-01-29 02:12:52 -0600

    Reformat code properly.

--- maven/jruby-complete/src/it/bouncycastle/src/test/java/org/jruby/its/BouncyCastleTestCase.java
+++ maven/jruby-complete/src/it/bouncycastle/src/test/java/org/jruby/its/BouncyCastleTestCase.java
@@ -5,22 +5,23 @@ import junit.framework.TestCase;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jruby.embed.ScriptingContainer;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BouncyCastleTestCase {
     @Test
-    public void java(){
-        assertEquals( "BouncyCastle Security Provider v1.47", new BouncyCastleProvider().getInfo() );
+    public void java() {
+        assertEquals("BouncyCastle Security Provider v1.47", new BouncyCastleProvider().getInfo());
     }
 
     @Test
-    public void ruby(){
-	System.setProperty( "jruby.self.first.class.loader", "true" );
+    public void ruby() {
+        System.setProperty("jruby.self.first.class.loader", "true");
         ScriptingContainer container = new ScriptingContainer();
-        Object result = container.parse( "require 'openssl'; Java::OrgBouncycastleJceProvider::BouncyCastleProvider.new.info").run();
-        assertEquals( "BouncyCastle Security Provider v1.49", result.toString() );
+        Object result = container.parse("require 'openssl'; Java::OrgBouncycastleJceProvider::BouncyCastleProvider.new.info").run();
+        assertEquals("BouncyCastle Security Provider v1.49", result.toString());
 
-	result = container.parse( "JRuby.runtime.jruby_class_loader").run();
-        assertEquals( "org.jruby.util.SelfFirstJRubyClassLoader", result.toString().replaceFirst( "@.*$", "" ) );
+        result = container.parse("JRuby.runtime.jruby_class_loader").run();
+        assertEquals("org.jruby.util.SelfFirstJRubyClassLoader", result.toString().replaceFirst("@.*$", ""));
     }
 }
