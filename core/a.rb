844c4b1759b06890debbd72b4ab5a28aa4f44ee7
Matthias Springer <me@matthiasspringer.de>
2015-02-04 17:53:31 +0100

    Update.

--- core/a.rb
+++ core/a.rb
@@ -16,8 +16,8 @@ end
 
     beginning_time = Time.now
     (1..1000000).each do |i|
-        n(1, 2)
-        #m(foo: 1, bar: 2)
+        #n(1, 2)
+        m(foo: 1, bar: 2)
     end
     end_time = Time.now
     puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
