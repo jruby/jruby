97e9624c2f6bd54d55ef790d982be97f8c5e75e5
Thomas E. Enebo <tom.enebo@gmail.com>
2015-02-07 10:06:18 -0600

    Split InterpretedIRMethod into InterpretedIRBodyMethod and InterpretedIRMethod.
    Modules/Classes/MetaClasses now call through their own call path.  Methods get their own.
    This removes conditional check for synthetic method and gets rid of box count logic for
    JITing bodies vs methods.

---
+++ core/src/main/java/org/jruby/internal/runtime/methods/InterpretedIRBodyMethod.java
@@ -0,0 +1,81 @@
+package org.jruby.internal.runtime.methods;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.jruby.RubyModule;
+import org.jruby.ir.IRScope;
+import org.jruby.ir.interpreter.InterpreterContext;
+import org.jruby.ir.runtime.IRRuntimeHelpers;
+import org.jruby.runtime.Block;
+import org.jruby.runtime.ThreadContext;
+import org.jruby.runtime.Visibility;
+import org.jruby.runtime.builtin.IRubyObject;
+
+/**
+ * Created by enebo on 2/6/15.
+ */
+public class InterpretedIRBodyMethod extends InterpretedIRMethod {
+    public InterpretedIRBodyMethod(IRScope method, RubyModule implementationClass) {
+        super(method, Visibility.PUBLIC, implementationClass);
+
+        this.box.callCount = -1;
+    }
+
+    @Override
+    public List<String[]> getParameterList() {
+        return new ArrayList<String[]>();
+    }
+
+    @Override
+    public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject[] args, Block block) {
+        return call(context, self, clazz, name, block);
+    }
+
+    @Override
+    public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, Block block) {
+        if (IRRuntimeHelpers.isDebug()) doDebug();
+
+        InterpreterContext ic = ensureInstrsReady();
+        return ic.engine.interpret(context, self, ic, getImplementationClass().getMethodLocation(), name, block, null);
+    }
+
+    @Override
+    public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject arg0, Block block) {
+        return call(context, self, clazz, name, block);
+    }
+
+    @Override
+    public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject arg0, IRubyObject arg1, Block block) {
+        return call(context, self, clazz, name, block);
+    }
+
+    @Override
+    public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject arg0, IRubyObject arg1, IRubyObject arg2, Block block) {
+        return call(context, self, clazz, name, block);
+    }
+
+    @Override
+    public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject[] args) {
+        return call(context, self, clazz, name, Block.NULL_BLOCK);
+    }
+
+    @Override
+    public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name) {
+        return call(context, self, clazz, name, Block.NULL_BLOCK);
+    }
+
+    @Override
+    public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject arg0) {
+        return call(context, self, clazz, name, Block.NULL_BLOCK);
+    }
+
+    @Override
+    public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject arg0, IRubyObject arg1) {
+        return call(context, self, clazz, name, Block.NULL_BLOCK);
+    }
+
+    @Override
+    public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject arg0, IRubyObject arg1, IRubyObject arg2) {
+        return call(context, self, clazz, name, Block.NULL_BLOCK);
+    }
+}
