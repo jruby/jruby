bccceb742b046676c9c1a28985349b7a8e0ccc9a
Benoit Daloze <eregontp@gmail.com>
2015-02-10 12:09:45 +0100

    [Truffle] A super node should change the current method!

--- truffle/src/main/java/org/jruby/truffle/nodes/supercall/GeneralSuperReCallNode.java
+++ truffle/src/main/java/org/jruby/truffle/nodes/supercall/GeneralSuperReCallNode.java
@@ -9,9 +9,12 @@
  */
 package org.jruby.truffle.nodes.supercall;
 
+import java.util.Arrays;
+
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.source.SourceSection;
+
 import org.jruby.truffle.runtime.RubyArguments;
 import org.jruby.truffle.runtime.RubyContext;
 
@@ -31,17 +34,17 @@ public class GeneralSuperReCallNode extends AbstractGeneralSuperCallNode {
             lookup(frame);
         }
 
-        if (method == null || callNode == null) {
-            throw new IllegalStateException("No call node installed");
-        }
-
-        final Object[] superArguments;
+        final Object[] originalArguments;
 
         if (inBlock) {
-            superArguments = RubyArguments.getDeclarationFrame(frame.getArguments()).getArguments();
+            originalArguments = RubyArguments.getDeclarationFrame(frame.getArguments()).getArguments();
         } else {
-            superArguments = frame.getArguments();
+            originalArguments = frame.getArguments();
         }
+
+        final Object[] superArguments = Arrays.copyOf(originalArguments, originalArguments.length);
+
+        superArguments[RubyArguments.METHOD_INDEX] = superMethod;
 
         return callNode.call(frame, superArguments);
     }
