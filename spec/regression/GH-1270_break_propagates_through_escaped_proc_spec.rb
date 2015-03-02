c4cdb077dc3c1f7f74e2787e5ba39731b3d88fe1
Charles Oliver Nutter <headius@headius.com>
2015-03-02 14:07:25 -0600

    Restore original one-liner in spec for #1270.

--- spec/regression/GH-1270_break_propagates_through_escaped_proc_spec.rb
+++ spec/regression/GH-1270_break_propagates_through_escaped_proc_spec.rb
@@ -33,22 +33,6 @@ describe "A break passing through an intermediate escaped closure" do
       end
     end
 
-    # This is hacked a bit because the single-method case is not currently fixable in 1.7:
-    #   Enumerator1270.new {|y| y.yield 1}.each {break 1}
-    # This case shares a frame -- and therefore a jumpTarget -- which causes the escaped
-    # Generator proc to prematurely turn the break into a LocalJumpError. We only have the
-    # one identifier for non-local jumps, so there's no easy way to fix this at the
-    # moment.
-    #
-    # JRuby 9k avoids this by associating jumps with the originating closure rather than
-    # the target method. The single-line version works correctly in JRuby 9k.
-    obj = Object.new
-    def obj.make_enum
-      Enumerator1270.new {|y| y.yield 1}
-    end
-    def obj.main
-      make_enum.each {break 1}
-    end
-    obj.main.should == 1
+    Enumerator1270.new {|y| y.yield 1}.each {break 1}.should == 1
   end
 end
