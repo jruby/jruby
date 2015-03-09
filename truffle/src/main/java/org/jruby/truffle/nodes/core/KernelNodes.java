a060a78da5c7e1761500d5eacba1f5e4c6b78425
Kevin Menard <kevin@nirvdrum.com>
2015-03-09 13:07:34 -0400

    [Truffle] Implemented Kernel#exit(boolean).

--- truffle/src/main/java/org/jruby/truffle/nodes/core/KernelNodes.java
+++ truffle/src/main/java/org/jruby/truffle/nodes/core/KernelNodes.java
@@ -650,6 +650,15 @@ public abstract class KernelNodes {
             return null;
         }
 
+        @Specialization
+        public Object exit(boolean status) {
+            notDesignedForCompilation();
+
+            getContext().shutdown();
+            System.exit(status ? 0 : -1);
+            return null;
+        }
+
     }
 
     @CoreMethod(names = "exit!", isModuleFunction = true, optional = 1)
