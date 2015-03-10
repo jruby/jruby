ec9ba5dd9797a1d47bbaebf16bf9d4f7608c84aa
Brandon Fish <brandon.j.fish@gmail.com>
2015-03-10 11:57:00 -0500

    [Truffle] Adding Array#sample to array.rb

--- truffle/src/main/ruby/core/rubinius/common/array.rb
+++ truffle/src/main/ruby/core/rubinius/common/array.rb
@@ -505,6 +505,52 @@ class Array
     replace ary
   end
 
+  def sample(count=undefined, options=undefined)
+    return at Kernel.rand(size) if undefined.equal? count
+
+    if undefined.equal? options
+      if o = Rubinius::Type.check_convert_type(count, Hash, :to_hash)
+        options = o
+        count = nil
+      else
+        options = nil
+        count = Rubinius::Type.coerce_to_collection_index count
+      end
+    else
+      count = Rubinius::Type.coerce_to_collection_index count
+      options = Rubinius::Type.coerce_to options, Hash, :to_hash
+    end
+
+    if count and count < 0
+      raise ArgumentError, "count must be greater than 0"
+    end
+
+    rng = options[:random] if options
+    rng = Kernel unless rng and rng.respond_to? :rand
+
+    unless count
+      random = Rubinius::Type.coerce_to_collection_index rng.rand(size)
+      raise RangeError, "random value must be >= 0" if random < 0
+      raise RangeError, "random value must be less than Array size" unless random < size
+
+      return at random
+    end
+
+    count = size if count > size
+    result = Array.new self
+    tuple = Rubinius::Mirror::Array.reflect(result).tuple
+
+    count.times do |i|
+      random = Rubinius::Type.coerce_to_collection_index rng.rand(size)
+      raise RangeError, "random value must be >= 0" if random < 0
+      raise RangeError, "random value must be less than Array size" unless random < size
+
+      tuple.swap i, random
+    end
+
+    return count == size ? result : result[0, count]
+  end
+
   def find_index(obj=undefined)
     super
   end
