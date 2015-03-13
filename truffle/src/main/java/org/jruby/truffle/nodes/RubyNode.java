5b35bd4232311467b77056f860a427e368b07585
Chris Seaton <chris@chrisseaton.com>
2015-03-13 19:46:34 +0000

    [Truffle] Make notDesignedForCompilation use bailout rather than an assert.

--- truffle/src/main/java/org/jruby/truffle/nodes/RubyNode.java
+++ truffle/src/main/java/org/jruby/truffle/nodes/RubyNode.java
@@ -10,6 +10,7 @@
 package org.jruby.truffle.nodes;
 
 import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.dsl.TypeSystemReference;
@@ -254,7 +255,7 @@ public abstract class RubyNode extends Node {
     }
 
     public static void notDesignedForCompilation() {
-        CompilerAsserts.neverPartOfCompilation();
+        CompilerDirectives.bailout("this code either doesn't implement Ruby semantics properly, or is a basic implementation that will not compile");
     }
 
     public boolean isTrue(boolean value) {
