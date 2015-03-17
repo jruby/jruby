0b1d0bc6dfaa085e78e8c47c739c5200ab1e1218
Benoit Daloze <eregontp@gmail.com>
2015-03-17 17:36:49 +0100

    Squashed 'spec/ruby/' changes from 0c3c64b..63695c3
    63695c3 Fix name in the describe block of File.exists?.
    c4c8e46 Fix name in the describe block of Dir.exists?.
    
    git-subtree-dir: spec/ruby
    git-subtree-split: 63695c3d2f4387beb9b5186c1a8c260fb637ce07

--- core/dir/exists_spec.rb
+++ core/dir/exists_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/common', __FILE__)
 require File.expand_path('../shared/exist', __FILE__)
 
-describe "Dir.exist?" do
+describe "Dir.exists?" do
   before :all do
     DirSpecs.create_mock_dirs
   end
