eed837f2f66ccb2ef07099b63ccd7a381df1af38
Brandon Fish <brandon.j.fish@gmail.com>
2015-03-18 11:28:15 -0500

    [Truffle] Adding annotation to return an enumerator if no block is given to a method.

--- truffle/src/main/java/org/jruby/truffle/nodes/core/CoreMethodNodeManager.java
+++ truffle/src/main/java/org/jruby/truffle/nodes/core/CoreMethodNodeManager.java
@@ -210,6 +210,11 @@ public abstract class CoreMethodNodeManager {
         final CheckArityNode checkArity = new CheckArityNode(context, sourceSection, arity);
         RubyNode sequence = SequenceNode.sequence(context, sourceSection, checkArity, methodNode);
 
+        if (methodDetails.getMethodAnnotation().returnsEnumeratorIfNoBlock()) {
+            // TODO BF 3-18-2015 Handle multiple method names correctly
+            sequence = new ReturnEnumeratorIfNoBlockNode(methodDetails.getMethodAnnotation().names()[0], sequence);
+        }
+
         if (methodDetails.getMethodAnnotation().taintFromSelf() || methodDetails.getMethodAnnotation().taintFromParameters().length > 0) {
             sequence = new TaintResultNode(methodDetails.getMethodAnnotation().taintFromSelf(),
                                            methodDetails.getMethodAnnotation().taintFromParameters(),
