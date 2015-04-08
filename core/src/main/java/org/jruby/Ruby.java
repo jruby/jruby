a41293bf8256bf512d6a7f1146ad8dc7e5deb75a
Christian Meier <m.kristian@web.de>
2015-04-08 11:36:07 +0530

    make ObjectSpacer public
    with this you can have a custom implementation of ObjectSpacer and plug it into the runtime
    via reflection.

--- core/src/main/java/org/jruby/Ruby.java
+++ core/src/main/java/org/jruby/Ruby.java
@@ -4931,7 +4931,7 @@ public final class Ruby {
     private EnumMap<DefinedMessage, RubyString> definedMessages = new EnumMap<DefinedMessage, RubyString>(DefinedMessage.class);
     private EnumMap<RubyThread.Status, RubyString> threadStatuses = new EnumMap<RubyThread.Status, RubyString>(RubyThread.Status.class);
 
-    private interface ObjectSpacer {
+    public interface ObjectSpacer {
         public void addToObjectSpace(Ruby runtime, boolean useObjectSpace, IRubyObject object);
     }
 
