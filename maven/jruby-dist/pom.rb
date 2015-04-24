6550480833db80ecfdd5ae0022dccf011eb1fd18
Christian Meier <m.kristian@web.de>
2015-04-24 23:04:58 +0200

    remove more jruby.home properties from build

--- maven/jruby-dist/pom.rb
+++ maven/jruby-dist/pom.rb
@@ -11,10 +11,6 @@ project 'JRuby Dist' do
               'tesla.dump.readonly' => true,
               'main.basedir' => '${project.parent.parent.basedir}' )
 
-  unless version =~ /-SNAPSHOT/
-    properties 'jruby.home' => '${basedir}/../..'
-  end
-
   # pre-installed gems - not default gems !
   gem 'ruby-maven', '3.1.1.0.8', :scope => 'provided'
 
