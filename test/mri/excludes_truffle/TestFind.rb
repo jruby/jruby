1e80aa29070beb4a94e87240982072e7c367fc7a
Brandon Fish <brandon.j.fish@gmail.com>
2015-05-05 10:08:24 -0500

    [Truffle] Add find to stdlib

---
+++ test/mri/excludes_truffle/TestFind.rb
@@ -0,0 +1,4 @@
+exclude :test_change_dir_to_file, "needs investigation"
+exclude :test_change_dir_to_symlink_loop, "needs investigation"
+exclude :test_unreadable_dir, "needs investigation"
+exclude :test_unsearchable_dir, "needs investigation"
