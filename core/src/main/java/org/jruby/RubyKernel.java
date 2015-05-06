76ba4b6998bc8ad04dde4c5e2f0a271628cf7018
Charles Oliver Nutter <headius@headius.com>
2015-05-05 20:52:17 -0500

    Set thread-local call type and visibility around all m_m dispatch.
    This also simplifies the pre-built "visibility m_m's" and
    eliminates a few inner classes.
    
    Fixes #540.

--- core/src/main/java/org/jruby/RubyKernel.java
+++ core/src/main/java/org/jruby/RubyKernel.java
@@ -99,17 +99,21 @@ import static org.jruby.anno.FrameField.*;
 public class RubyKernel {
     public final static Class<?> IRUBY_OBJECT = IRubyObject.class;
 
-    public static abstract class MethodMissingMethod extends JavaMethodNBlock {
-        public MethodMissingMethod(RubyModule implementationClass) {
+    public static class MethodMissingMethod extends JavaMethodNBlock {
+        private final Visibility visibility;
+        private final CallType callType;
+
+        public MethodMissingMethod(RubyModule implementationClass, Visibility visibility, CallType callType) {
             super(implementationClass, Visibility.PRIVATE, CallConfiguration.FrameFullScopeNone);
+
+            this.callType = callType;
+            this.visibility = visibility;
         }
 
         @Override
         public IRubyObject call(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject[] args, Block block) {
-            return methodMissing(context, self, clazz, name, args, block);
+            return RubyKernel.methodMissing(context, self, name, visibility, callType, args, block);
         }
-
-        public abstract IRubyObject methodMissing(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject[] args, Block block);
 
     }
     public static RubyModule createKernelModule(Ruby runtime) {
@@ -120,40 +124,11 @@ public class RubyKernel {
         
         module.setFlag(RubyObject.USER7_F, false); //Kernel is the only normal Module that doesn't need an implementor
 
-        runtime.setPrivateMethodMissing(new MethodMissingMethod(module) {
-            @Override
-            public IRubyObject methodMissing(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject[] args, Block block) {
-                return RubyKernel.methodMissing(context, self, name, PRIVATE, CallType.NORMAL, args, block);
-            }
-        });
-
-        runtime.setProtectedMethodMissing(new MethodMissingMethod(module) {
-            @Override
-            public IRubyObject methodMissing(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject[] args, Block block) {
-                return RubyKernel.methodMissing(context, self, name, PROTECTED, CallType.NORMAL, args, block);
-            }
-        });
-
-        runtime.setVariableMethodMissing(new MethodMissingMethod(module) {
-            @Override
-            public IRubyObject methodMissing(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject[] args, Block block) {
-                return RubyKernel.methodMissing(context, self, name, PUBLIC, CallType.VARIABLE, args, block);
-            }
-        });
-
-        runtime.setSuperMethodMissing(new MethodMissingMethod(module) {
-            @Override
-            public IRubyObject methodMissing(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject[] args, Block block) {
-                return RubyKernel.methodMissing(context, self, name, PUBLIC, CallType.SUPER, args, block);
-            }
-        });
-
-        runtime.setNormalMethodMissing(new MethodMissingMethod(module) {
-            @Override
-            public IRubyObject methodMissing(ThreadContext context, IRubyObject self, RubyModule clazz, String name, IRubyObject[] args, Block block) {
-                return RubyKernel.methodMissing(context, self, name, PUBLIC, CallType.NORMAL, args, block);
-            }
-        });
+        runtime.setPrivateMethodMissing(new MethodMissingMethod(module, PRIVATE, CallType.NORMAL));
+        runtime.setProtectedMethodMissing(new MethodMissingMethod(module, PROTECTED, CallType.NORMAL));
+        runtime.setVariableMethodMissing(new MethodMissingMethod(module, PUBLIC, CallType.VARIABLE));
+        runtime.setSuperMethodMissing(new MethodMissingMethod(module, PUBLIC, CallType.SUPER));
+        runtime.setNormalMethodMissing(new MethodMissingMethod(module, PUBLIC, CallType.NORMAL));
 
         recacheBuiltinMethods(runtime);
 
