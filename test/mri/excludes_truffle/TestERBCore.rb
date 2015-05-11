d3f89eaa428c454ee6010e06cbc45447d01c07da
Brandon Fish <brandon.j.fish@gmail.com>
2015-05-11 07:21:39 -0500

    [Truffle] Add test_erb.rb to the mri test index with excludes.

--- test/mri/excludes_truffle/TestERBCore.rb
+++ test/mri/excludes_truffle/TestERBCore.rb
@@ -1,3 +1,4 @@
+exclude :test_def_erb_method, "needs investigation"
 exclude :test_def_method_with_fname, "needs investigation"
 exclude :test_def_method_without_filename, "needs investigation"
 exclude :test_keep_lineno, "needs investigation"
