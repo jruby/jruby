4315821918e9978ea39cd83854080efd8b969c26
Chris Seaton <chris@chrisseaton.com>
2015-05-24 15:02:35 +0100

    [Truffle] Rack works.
    Thanks to bjfish for the script.

--- test/truffle/simple-webrick-server.rb
+++ test/truffle/simple-webrick-server.rb
@@ -1,7 +1,7 @@
 require 'webrick'
 
 root = File.expand_path '~/public_html'
-server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
+server = WEBrick::HTTPServer.new :Port => 8080, :DocumentRoot => root
 
 trap 'INT' do server.shutdown end
 
