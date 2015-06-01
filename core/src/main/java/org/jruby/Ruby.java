33b0efc8df65b2ac3caf8f2dfc85432eaa502db2
Thomas E. Enebo <tom.enebo@gmail.com>
2015-06-01 16:21:12 -0500

    inverted check.  we only want to dedup with it is stringclass

--- core/src/main/java/org/jruby/Ruby.java
+++ core/src/main/java/org/jruby/Ruby.java
@@ -4740,7 +4740,7 @@ public final class Ruby implements Constantizable {
      * @return the freeze-duped version of the string
      */
     public RubyString freezeAndDedupString(RubyString string) {
-        if (string.getMetaClass() == stringClass) {
+        if (string.getMetaClass() != stringClass) {
             // never cache a non-natural String
             RubyString duped = string.strDup(this);
             duped.setFrozen(true);
