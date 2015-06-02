b0a41e74d2c38affc6ec78e1a91aa5dc37025888
Charles Oliver Nutter <headius@headius.com>
2015-06-02 14:45:21 -0500

    Prefer ENV for proxy settings, because:
    * It is a new feature in 2.0+ stdlib, so our old logic using
      ENV_JAVA as default is now invalid.
    * http.nonProxyHosts defaults to a non-empty value, making it
      impossible to fall back on ENV.
    
    Fixes failing MRI test on master.

--- lib/ruby/stdlib/uri/generic.rb
+++ lib/ruby/stdlib/uri/generic.rb
@@ -1550,24 +1550,24 @@ module URI
     end
 
     def http_proxy_from_env
-      proxy_host = ENV_JAVA['http.proxyHost']
+      proxy_uri = ENV['http_proxy']
 
-      if proxy_host
-        begin
-          proxy_port = (ENV_JAVA['http.proxyPort'] || 80).to_i
-          if proxy_port > 0
-            proxy_uri = "http://#{proxy_host}:#{proxy_port}"
-          else
-            warn "invalid http.proxyPort property: #{ENV_JAVA['http.proxyPort']}"
-          end
-        end
+      if proxy_uri.nil? && (proxy_uri = ENV['HTTP_PROXY'])
+        warn 'The environment variable HTTP_PROXY is discouraged.  Use http_proxy.'
       end
 
       if proxy_uri.nil? || proxy_uri.empty?
-        proxy_uri = ENV['http_proxy']
+        proxy_host = ENV_JAVA['http.proxyHost']
 
-        if proxy_uri.nil? && (proxy_uri = ENV['HTTP_PROXY'])
-          warn 'The environment variable HTTP_PROXY is discouraged.  Use http_proxy.'
+        if proxy_host
+          begin
+            proxy_port = (ENV_JAVA['http.proxyPort'] || 80).to_i
+            if proxy_port > 0
+              proxy_uri = "http://#{proxy_host}:#{proxy_port}"
+            else
+              warn "invalid http.proxyPort property: #{ENV_JAVA['http.proxyPort']}"
+            end
+          end
         end
       end
 
@@ -1575,12 +1575,13 @@ module URI
     end
     private :http_proxy_from_env
 
+    DEFAULT_JVM_NON_PROXY_HOSTS = "local|*.local|169.254/16|*.169.254/16|127.0.0.1|localhost|*.localhost"
     def no_proxy_from_env
-      no_proxy = ENV_JAVA['http.nonProxyHosts']
+      name = "no_proxy"
+      no_proxy = ENV[name] || ENV[name.upcase]
 
       if no_proxy.nil? || no_proxy.empty?
-        name = "no_proxy"
-        no_proxy = ENV[name] || ENV[name.upcase]
+        no_proxy = ENV_JAVA['http.nonProxyHosts']
       end
 
       no_proxy
