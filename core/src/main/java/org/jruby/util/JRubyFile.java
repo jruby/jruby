4395806b940be2e80c976db36cb86e45d63b97a5
Christian Meier <m.kristian@web.de>
2015-06-07 13:42:57 +0200

    treat classpath:path/to/file.rb with JRubyFile.createResource

--- core/src/main/java/org/jruby/util/JRubyFile.java
+++ core/src/main/java/org/jruby/util/JRubyFile.java
@@ -91,7 +91,7 @@ public class JRubyFile extends JavaSecuredFile {
 
         if (pathname.contains(":")) { // scheme-oriented resources
             if (pathname.startsWith("classpath:")) {
-                pathname = pathname.replace("classpath:/", "uri:classloader:/");
+                pathname = pathname.replace("classpath:", "uri:classloader:/");
             }
 
             // replace is needed for maven/jruby-complete/src/it/app_using_classpath_uri to work
