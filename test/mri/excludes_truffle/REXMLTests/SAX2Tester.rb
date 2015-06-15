5ed43e494aa29b239026c570add64be6ba99ef67
Brandon Fish <brandon.j.fish@gmail.com>
2015-06-15 18:17:03 -0500

    [Truffle] Add rexml tests to mri test index

---
+++ test/mri/excludes_truffle/REXMLTests/SAX2Tester.rb
@@ -0,0 +1,2 @@
+exclude :"test_char_ref_dom", "needs investigation"
+exclude :"test_socket", "needs investigation"
---
+++ test/mri/excludes_truffle/TestJSONGenericObject.rb
@@ -0,0 +1,1 @@
+exclude :"test_json_generic_object_load", "regressed at/near https://github.com/jruby/jruby/commit/673421468513ae6e8d71fcc0ccb7f8d5deb90526"
