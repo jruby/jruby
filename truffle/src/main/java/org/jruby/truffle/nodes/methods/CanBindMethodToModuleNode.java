bd75f634942b7f8eb1518fef46295b36b39d76da
Benoit Daloze <eregontp@gmail.com>
2015-06-26 23:58:52 +0200

    [Truffle] Cache canBindMethodTo() and make UnboundMethod#bind compilable.

---
+++ truffle/src/main/java/org/jruby/truffle/nodes/methods/CanBindMethodToModuleNode.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2015 Oracle and/or its affiliates. All rights reserved. This
+ * code is released under a tri EPL/GPL/LGPL license. You can use it,
+ * redistribute it and/or modify it under the terms of the:
+ *
+ * Eclipse Public License version 1.0
+ * GNU General Public License version 2
+ * GNU Lesser General Public License version 2.1
+ */
+package org.jruby.truffle.nodes.methods;
+
+import org.jruby.truffle.nodes.RubyNode;
+import org.jruby.truffle.nodes.dispatch.DispatchNode;
+import org.jruby.truffle.runtime.ModuleOperations;
+import org.jruby.truffle.runtime.RubyContext;
+import org.jruby.truffle.runtime.core.RubyModule;
+import org.jruby.truffle.runtime.methods.InternalMethod;
+
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.NodeChildren;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.source.SourceSection;
+
+@NodeChildren({
+        @NodeChild("method"),
+        @NodeChild("module")
+})
+public abstract class CanBindMethodToModuleNode extends RubyNode {
+
+    public CanBindMethodToModuleNode(RubyContext context, SourceSection sourceSection) {
+        super(context, sourceSection);
+    }
+
+    public abstract boolean executeCanBindMethodToModule(VirtualFrame frame, InternalMethod method, RubyModule module);
+
+    @Specialization(
+            guards = { "method.getDeclaringModule() == declaringModule", "module == cachedModule" },
+            limit = "getCacheLimit()")
+    protected boolean canBindMethodToCached(VirtualFrame frame, InternalMethod method, RubyModule module,
+            @Cached("method.getDeclaringModule()") RubyModule declaringModule,
+            @Cached("module") RubyModule cachedModule,
+            @Cached("canBindMethodTo(declaringModule, cachedModule)") boolean canBindMethodTo) {
+        return canBindMethodTo;
+    }
+
+    @Specialization
+    protected boolean canBindMethodToUncached(VirtualFrame frame, InternalMethod method, RubyModule module) {
+        final RubyModule declaringModule = method.getDeclaringModule();
+        return canBindMethodTo(declaringModule, module);
+    }
+
+    protected boolean canBindMethodTo(RubyModule declaringModule, RubyModule module) {
+        return ModuleOperations.canBindMethodTo(declaringModule, module);
+    }
+
+    protected static int getCacheLimit() {
+        return DispatchNode.DISPATCH_POLYMORPHIC_MAX;
+    }
+
+}
