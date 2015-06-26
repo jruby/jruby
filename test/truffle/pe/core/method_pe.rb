717610c49eed046e528db6426d139f1ebd7964b4
Benoit Daloze <eregontp@gmail.com>
2015-06-27 00:24:38 +0200

    [Truffle] Add PE tests for Method#call.
    * And possibly future tests for Kernel#method, Method#unbind and UnboundMethod#bind.

---
+++ test/truffle/pe/core/method_pe.rb
@@ -0,0 +1,15 @@
+# Copyright (c) 2014, 2015 Oracle and/or its affiliates. All rights reserved. This
+# code is released under a tri EPL/GPL/LGPL license. You can use it,
+# redistribute it and/or modify it under the terms of the:
+#
+# Eclipse Public License version 1.0
+# GNU General Public License version 2
+# GNU Lesser General Public License version 2.1
+
+example "1.method(:abs).call"
+example "1.method(:abs).unbind.bind(-2).call"
+
+# These 3 are not constant currently since they produce new objects
+# example "1.method(:abs)"
+# example "1.method(:abs).unbind"
+# example "1.method(:abs).unbind.bind(-2)"
