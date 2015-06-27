938b16de69625f87969e5acf846f2287387fed26
Benoit Daloze <eregontp@gmail.com>
2015-06-27 16:52:18 +0200

    [Truffle] Add a couple PE tests for super and zsuper.

--- test/truffle/pe/pe.rb
+++ test/truffle/pe/pe.rb
@@ -46,6 +46,7 @@ counter_example "rand"
 require_relative 'language/closures_pe.rb'
 require_relative 'language/constant_pe.rb'
 require_relative 'language/metaprogramming_pe.rb'
+require_relative 'language/super_pe.rb'
 require_relative 'core/truefalse_pe.rb'
 require_relative 'core/fixnum_pe.rb'
 require_relative 'core/float_pe.rb'
