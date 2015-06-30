11bbc8aefe232f0618c6505152726dad06d3e38e
Thomas E. Enebo <tom.enebo@gmail.com>
2015-06-30 10:43:40 -0500

    Revert "Fixes #3056. java.lang.IncompatibleClassChangeError in 9.0.0.0 rc1"
    This fixed the issue but @subbuss rightly points out this scope should have
    been marked as BINDING_HAS_ESCAPED and thus this pass should not have even
    ran.
    
    This reverts commit 467e2f7ca660f8d678757efaae77a0358d08c673.

--- core/src/main/java/org/jruby/ir/passes/OptimizeDelegationPass.java
+++ core/src/main/java/org/jruby/ir/passes/OptimizeDelegationPass.java
@@ -18,6 +18,16 @@ public class OptimizeDelegationPass extends CompilerPass {
 
     @Override
     public Object execute(IRScope s, Object... data) {
+        /**
+         * SSS FIXME: too late at night to think straight if this
+         * is required to run on all nested scopes or not. Doesn't
+         * look like it, but leaving behind in case it is.
+         *
+        for (IRClosure c: s.getClosures()) {
+            run(c, false, true);
+        }
+         **/
+
         if (s.getFlags().contains(IRFlags.BINDING_HAS_ESCAPED)) return null;
         if (!s.getFlags().contains(IRFlags.RECEIVES_CLOSURE_ARG)) return null;
 
@@ -32,7 +42,9 @@ public class OptimizeDelegationPass extends CompilerPass {
         return false;
     }
 
-    private static void calculateBlockUsage(IRScope s, Map<Operand, Operand> unusedExplicitBlocks) {
+    private static void optimizeDelegatedVars(IRScope s) {
+        Map<Operand, Operand> unusedExplicitBlocks = new HashMap<>();
+
         for (BasicBlock bb: s.getCFG().getBasicBlocks()) {
             for (Instr i: bb.getInstrs()) {
                 if (i instanceof ReifyClosureInstr) {
@@ -50,12 +62,6 @@ public class OptimizeDelegationPass extends CompilerPass {
             }
         }
 
-        for (IRScope child: s.getClosures()) {
-            calculateBlockUsage(child, unusedExplicitBlocks);
-        }
-    }
-
-    private static void replaceBlockUsage(IRScope s, Map<Operand, Operand> unusedExplicitBlocks) {
         for (BasicBlock bb: s.getCFG().getBasicBlocks()) {
             ListIterator<Instr> instrs = bb.getInstrs().listIterator();
             while (instrs.hasNext()) {
@@ -72,16 +78,6 @@ public class OptimizeDelegationPass extends CompilerPass {
                 }
             }
         }
-
-        for (IRScope child: s.getClosures()) {
-            replaceBlockUsage(child, unusedExplicitBlocks);
-        }
-    }
-    private static void optimizeDelegatedVars(IRScope s) {
-        Map<Operand, Operand> unusedExplicitBlocks = new HashMap<>();
-
-        calculateBlockUsage(s, unusedExplicitBlocks);
-        replaceBlockUsage(s, unusedExplicitBlocks);
     }
 
     private static boolean usesVariableAsNonClosureArg(Instr i, Variable v) {
