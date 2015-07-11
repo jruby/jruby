ea18316fd2acb3d17c3dd052236cd5d1bfa8f0e5
Kevin Menard <kevin@nirvdrum.com>
2015-07-11 12:22:11 -0400

    Build the complete JAR for the 'snapshots' profile.

--- maven/pom.rb
+++ maven/pom.rb
@@ -26,7 +26,7 @@ project 'JRuby Artifacts' do
 
   # module to profile map
   map = { 'jruby' => [ :release, :main, :osgi, :j2ee ],
-    'jruby-complete' => [ :release, :complete, :osgi, :'jruby_complete_jar_extended'],
+    'jruby-complete' => [ :release, :complete, :osgi, :'jruby_complete_jar_extended', :snapshots],
     'jruby-jars' => [ :release, :'jruby-jars' ],
     'jruby-dist' => [ :release, :dist ]
   }
