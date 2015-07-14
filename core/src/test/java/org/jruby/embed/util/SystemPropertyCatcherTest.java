3b68bdb9977e83c3ffda199e746da793c14cd6e6
kares <self@kares.org>
2015-07-14 14:05:21 +0200

    test cleanup - assure no left-overs

--- core/src/test/java/org/jruby/embed/util/SystemPropertyCatcherTest.java
+++ core/src/test/java/org/jruby/embed/util/SystemPropertyCatcherTest.java
@@ -2,38 +2,34 @@ package org.jruby.embed.util;
 
 import org.jruby.embed.LocalContextScope;
 import org.jruby.embed.PropertyName;
-import org.junit.After;
+
 import org.junit.AfterClass;
-import static org.junit.Assert.*;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  *
  * @author yoko
  */
 public class SystemPropertyCatcherTest {
-    
-    public SystemPropertyCatcherTest() {
-    }
-    
+
+    private static String LOCALCONTEXT_PROPERTY;
+
     @BeforeClass
     public static void setUpClass() {
+        LOCALCONTEXT_PROPERTY = System.getProperty(PropertyName.LOCALCONTEXT_SCOPE.toString());
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-        
-    }
-    
-    @After
-    public void tearDown() {
-        
+        if ( LOCALCONTEXT_PROPERTY == null ) {
+            System.clearProperty(PropertyName.LOCALCONTEXT_SCOPE.toString());
+        }
+        else {
+            System.setProperty(PropertyName.LOCALCONTEXT_SCOPE.toString(), LOCALCONTEXT_PROPERTY);
+        }
     }
 
     /**
@@ -42,29 +38,29 @@ public class SystemPropertyCatcherTest {
     @Test
     public void testGetScope() {
         System.setProperty(PropertyName.LOCALCONTEXT_SCOPE.toString(), "");
-        
+
         LocalContextScope defaultScope = LocalContextScope.SINGLETON;
-        
+
         // no setting
         LocalContextScope result = SystemPropertyCatcher.getScope(defaultScope);
         assertEquals(defaultScope, result);
-        
+
         System.setProperty(PropertyName.LOCALCONTEXT_SCOPE.toString(), "singleton");
         result = SystemPropertyCatcher.getScope(defaultScope);
         assertEquals(LocalContextScope.SINGLETON, result);
-        
+
         System.setProperty(PropertyName.LOCALCONTEXT_SCOPE.toString(), "singlethread");
         result = SystemPropertyCatcher.getScope(defaultScope);
         assertEquals(LocalContextScope.SINGLETHREAD, result);
-        
+
         System.setProperty(PropertyName.LOCALCONTEXT_SCOPE.toString(), "threadsafe");
         result = SystemPropertyCatcher.getScope(defaultScope);
         assertEquals(LocalContextScope.THREADSAFE, result);
-        
+
         System.setProperty(PropertyName.LOCALCONTEXT_SCOPE.toString(), "concurrent");
         result = SystemPropertyCatcher.getScope(defaultScope);
         assertEquals(LocalContextScope.CONCURRENT, result);
-        
+
         System.setProperty(PropertyName.LOCALCONTEXT_SCOPE.toString(), "");
     }
 }
