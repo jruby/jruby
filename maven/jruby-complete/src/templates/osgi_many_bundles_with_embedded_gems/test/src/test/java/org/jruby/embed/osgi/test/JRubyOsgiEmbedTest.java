3474eea51f4555e731c68299908280892a352333
kares <self@kares.org>
2015-07-15 22:17:52 +0200

    do not assume gem order in (osgi) embed test

--- maven/jruby-complete/src/templates/osgi_many_bundles_with_embedded_gems/test/src/test/java/org/jruby/embed/osgi/test/JRubyOsgiEmbedTest.java
+++ maven/jruby-complete/src/templates/osgi_many_bundles_with_embedded_gems/test/src/test/java/org/jruby/embed/osgi/test/JRubyOsgiEmbedTest.java
@@ -111,14 +111,14 @@ public class JRubyOsgiEmbedTest {
         assertEquals(true, loaded);
 
         jruby.runScriptlet( "require 'jar-dependencies'" );
-        list = (String) jruby.runScriptlet( "Gem.loaded_specs.keys.inspect" );
-        assertEquals(list, "[\"rake\", \"jruby-openssl\", \"jar-dependencies\"]");
+        list = (String) jruby.runScriptlet( "Gem.loaded_specs.keys.sort.inspect" );
+        assertEquals(list, "[\"jar-dependencies\", \"jruby-openssl\", \"rake\"]");
 
         // ensure we can load can load embedded gems
         loaded = (Boolean) jruby.runScriptlet( "require 'virtus'" );
         assertEquals(true, loaded);
 
-        list = (String) jruby.runScriptlet( "Gem.loaded_specs.keys.inspect" );
-        assertEquals(list, "[\"rake\", \"jruby-openssl\", \"jar-dependencies\", \"thread_safe\", \"descendants_tracker\", \"equalizer\", \"coercible\", \"ice_nine\", \"axiom-types\", \"virtus\"]");
+        list = (String) jruby.runScriptlet( "Gem.loaded_specs.keys.sort.inspect" );
+        assertEquals(list, "[\"axiom-types\", \"coercible\", \"descendants_tracker\", \"equalizer\", \"ice_nine\", \"jar-dependencies\", \"jruby-openssl\", \"rake\", \"thread_safe\", \"virtus\"]");
     }
 }
