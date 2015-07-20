623a6f1f27763e03c38129cd1ec159f39acce006
kares <self@kares.org>
2015-07-20 14:12:02 +0200

    a unit test since we can not influence getMethods order (closer to failure at travis-ci)

---
+++ core/src/test/java/org/jruby/java/dispatch/CallableSelectorTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2015 JRuby.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jruby.java.dispatch;
+
+import java.lang.reflect.Method;
+import org.jruby.Ruby;
+import org.jruby.RubyProc;
+import org.jruby.javasupport.JavaMethod;
+import org.jruby.runtime.Arity;
+import org.jruby.runtime.Binding;
+import org.jruby.runtime.Block;
+import org.jruby.runtime.BlockBody;
+import org.jruby.runtime.Frame;
+import org.jruby.runtime.NullBlockBody;
+import org.jruby.runtime.backtrace.BacktraceElement;
+import org.jruby.runtime.builtin.IRubyObject;
+import org.jruby.util.collections.IntHashMap;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author kares
+ */
+public class CallableSelectorTest {
+
+    @Test
+    public void testCallableProcToIfaceMatchIsNotOrderSensitive() throws Exception {
+        final Ruby runtime = Ruby.newInstance();
+
+        final Method list1 = java.io.File.class.getMethod("listFiles", java.io.FileFilter.class);
+        final Method list2 = java.io.File.class.getMethod("listFiles", java.io.FilenameFilter.class);
+
+        IntHashMap cache;
+        JavaMethod[] methods;
+        Binding binding = new Binding(new Frame(), null, null, new BacktraceElement());
+        BlockBody body1 = new NullBlockBody() {
+            @Override public Arity arity() { return Arity.ONE_ARGUMENT; }
+        };
+        RubyProc dummyProc = RubyProc.newProc(runtime, new Block(body1, binding), Block.Type.PROC);
+
+        JavaMethod result; IRubyObject[] args;
+
+        cache = IntHashMap.nullMap();
+        methods = new JavaMethod[] {
+            new JavaMethod(runtime, list1), new JavaMethod(runtime, list2)
+        };
+        result = CallableSelector.matchingCallableArityOne(runtime, cache, methods, dummyProc);
+        assertEquals(new JavaMethod(runtime, list1), result);
+
+        cache = IntHashMap.nullMap();
+        args = new IRubyObject[] { dummyProc };
+        result = CallableSelector.matchingCallableArityN(runtime, cache, methods, args);
+        assertEquals(new JavaMethod(runtime, list1), result);
+
+        cache = IntHashMap.nullMap();
+        methods = new JavaMethod[] {
+            new JavaMethod(runtime, list2), new JavaMethod(runtime, list1)
+        };
+        result = CallableSelector.matchingCallableArityOne(runtime, cache, methods, dummyProc);
+        assertEquals(new JavaMethod(runtime, list1), result);
+
+        cache = IntHashMap.nullMap();
+        args = new IRubyObject[] { dummyProc };
+        result = CallableSelector.matchingCallableArityN(runtime, cache, methods, args);
+        assertEquals(new JavaMethod(runtime, list1), result);
+
+        //
+
+        BlockBody body2 = new NullBlockBody() {
+            @Override public Arity arity() { return Arity.TWO_ARGUMENTS; }
+        };
+        dummyProc = RubyProc.newProc(runtime, new Block(body2, binding), Block.Type.PROC);
+
+        cache = IntHashMap.nullMap();
+        methods = new JavaMethod[] {
+            new JavaMethod(runtime, list1), new JavaMethod(runtime, list2)
+        };
+        result = CallableSelector.matchingCallableArityOne(runtime, cache, methods, dummyProc);
+        assertEquals(new JavaMethod(runtime, list2), result);
+
+        cache = IntHashMap.nullMap();
+        args = new IRubyObject[] { dummyProc };
+        result = CallableSelector.matchingCallableArityN(runtime, cache, methods, args);
+        assertEquals(new JavaMethod(runtime, list2), result);
+
+        cache = IntHashMap.nullMap();
+        methods = new JavaMethod[] {
+            new JavaMethod(runtime, list2), new JavaMethod(runtime, list1)
+        };
+        result = CallableSelector.matchingCallableArityOne(runtime, cache, methods, dummyProc);
+        assertEquals(new JavaMethod(runtime, list2), result);
+
+        cache = IntHashMap.nullMap();
+        args = new IRubyObject[] { dummyProc };
+        result = CallableSelector.matchingCallableArityN(runtime, cache, methods, args);
+        assertEquals(new JavaMethod(runtime, list2), result);
+    }
+
+}
