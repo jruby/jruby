6b88cd8fb7a22a462b48f73a6a77b604258f623a
Chris Seaton <chris@chrisseaton.com>
2015-07-24 23:10:14 +0100

    [Truffle] Add the final modifier in the OM DSL if there is no setter.

--- truffle/src/main/java/org/jruby/truffle/om/dsl/processor/layout/LayoutGenerator.java
+++ truffle/src/main/java/org/jruby/truffle/om/dsl/processor/layout/LayoutGenerator.java
@@ -64,6 +64,10 @@ public class LayoutGenerator {
                 modifiers.add("LocationModifier.NonNull");
             }
 
+            if (!property.hasSetter()) {
+                modifiers.add("LocationModifier.Final");
+            }
+
             final String modifiersExpression;
 
             if (modifiers.isEmpty()) {
