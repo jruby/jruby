05379fce5e67b17fde1264af5cd7e105a8de4ca9
Kevin Menard <kevin@nirvdrum.com>
2015-07-27 18:47:19 -0400

    [Truffle] Added a missing bitmask.

--- truffle/src/main/java/org/jruby/truffle/nodes/rubinius/ByteArrayNodes.java
+++ truffle/src/main/java/org/jruby/truffle/nodes/rubinius/ByteArrayNodes.java
@@ -65,7 +65,7 @@ public abstract class ByteArrayNodes {
 
         @Specialization
         public int getByte(RubyBasicObject bytes, int index) {
-            return getBytes(bytes).get(index);
+            return getBytes(bytes).get(index) & 0xff;
         }
 
     }
