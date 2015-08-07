704b29f7f8257423c6846e9f9fcf6be0048939b4
Thomas Hurst <tom@hur.st>
2015-08-07 16:49:44 +0100

    Rewrite PTY support.
    Passes MRI's test_pty.rb on FreeBSD 10.1.  Untested elsewhere.

--- lib/ruby/stdlib/pty.rb
+++ lib/ruby/stdlib/pty.rb
@@ -1,6 +1,17 @@
 require 'ffi'
+require 'fcntl'
 
 module PTY
+  module LibUtil
+    extend FFI::Library
+    ffi_lib FFI::Library::LIBC
+    # openpty(3) is in libutil on linux and BSD, libc on MacOS
+    if FFI::Platform.linux? || (FFI::Platform.bsd? && !FFI::Platform.mac?)
+      ffi_lib 'libutil'
+    end
+    attach_function :openpty, [ :buffer_out, :buffer_out, :buffer_in, :buffer_in, :buffer_in ], :int
+  end
+
   class ChildExited < RuntimeError
     attr_reader :status
 
@@ -15,93 +26,131 @@ module PTY
 
   class << self
     def spawn(*args, &block)
-      if args.size > 1
-        self.fork_exec_pty(*args, &block)
+      if args.size > 0
+        exec_pty(args, &block)
       else
-        self.fork_exec_pty("/bin/sh", "-c", *args, &block)
+        exec_pty(get_shell, &block)
       end
     end
     alias :getpty :spawn
 
-    def open(*args)
-      raise NotImplementedError
+    def open
+      masterfd, slavefd, slave_name = openpty
+
+      master = IO.for_fd(masterfd, IO::RDWR)
+      slave = File.for_fd(slavefd, IO::RDWR)
+
+      File.chmod(0600, slave_name)
+
+      slave.define_singleton_method(:path) do
+        slave_name
+      end
+
+      slave.define_singleton_method(:tty?) do
+        true
+      end
+
+      fds = [master, slave]
+      fds.each do |fd|
+        fd.sync = true
+
+        hack_close_on_exec(fd)
+      end
+
+      return fds unless block_given?
+
+      begin
+        retval = yield(fds.dup)
+      ensure
+        fds.reject(&:closed?).each(&:close)
+      end
+
+      retval
     end
 
     def check(target_pid, exception = false)
       pid, status = Process.waitpid2(target_pid, Process::WNOHANG|Process::WUNTRACED)
 
       # I sometimes see #<Process::Status: pid 0 signal 36> here.
+      # See Github issue #3117
       if pid == target_pid && status
         if exception
           raise ChildExited.new(status)
         else
-          status
+          return status
         end
       end
-    rescue SystemCallError
+    rescue SystemCallError => e
       nil
     end
-  end
 
-  private
-  module LibUtil
-    extend FFI::Library
-    ffi_lib FFI::Library::LIBC
-    # forkpty(3) is in libutil on linux and BSD, libc on MacOS
-    if FFI::Platform.linux? || (FFI::Platform.bsd? && !FFI::Platform.mac?)
-      ffi_lib 'libutil'
+    private
+
+    def openpty
+      master = FFI::Buffer.alloc_out :int
+      slave  = FFI::Buffer.alloc_out :int
+      name   = FFI::Buffer.alloc_out 1024
+
+      result = LibUtil.openpty(master, slave, name, nil, nil)
+      if result != 0
+        raise(exception_for_errno(FFI.errno) || SystemCallError.new("errno=#{FFI.errno}"))
+      end
+
+      [master.get_int(0), slave.get_int(0), name.get_string(0)]
     end
-    attach_function :forkpty, [ :buffer_out, :buffer_out, :buffer_in, :buffer_in ], :pid_t
-  end
-  module LibC
-    extend FFI::Library
-    ffi_lib FFI::Library::LIBC
-    attach_function :close, [ :int ], :int
-    attach_function :strerror, [ :int ], :string
-    attach_function :execv, [ :string, :buffer_in ], :int
-    attach_function :execvp, [ :string, :buffer_in ], :int
-    attach_function :dup2, [ :int, :int ], :int
-    attach_function :dup, [ :int ], :int
-    attach_function :_exit, [ :int ], :void
-  end
-  Buffer = FFI::Buffer
-  def self.build_args(args)
-    cmd, argv0 = args.shift
-    cmd_args = args.map do |arg|
-      FFI::MemoryPointer.from_string(arg)
+
+    def exec_pty(args)
+      master, slave = open
+
+      read, write = IO.pipe
+      pid = Process.spawn(*args, in: read, out: slave, err: slave, close_others: true, pgroup: true)
+      [read, slave].each(&:close)
+
+      hack_close_on_exec(master)
+      hack_close_on_exec(write)
+
+      ret = [master, write, pid]
+
+      if block_given?
+        begin
+          retval = yield(ret.dup)
+        ensure
+          ret[0, 2].reject(&:closed?).each(&:close)
+        end
+        retval
+      else
+        ret
+      end
     end
-    exec_args = FFI::MemoryPointer.new(:pointer, 1 + cmd_args.length + 1)
-    exec_cmd = FFI::MemoryPointer.from_string(argv0 || cmd)
-    exec_args[0].put_pointer(0, exec_cmd)
-    cmd_args.each_with_index do |arg, i|
-      exec_args[i + 1].put_pointer(0, arg)
+
+    def get_shell
+      if shell = ENV['SHELL']
+        return shell
+      elsif pwent = Etc.getpwuid(Process.uid) && pwent.shell
+        return pwent.shell
+      else
+        "/bin/sh"
+      end
     end
-    [ cmd, exec_args ]
-  end
-  def self.fork_exec_pty(*args)
-    mfdp = Buffer.alloc_out :int
-    name = Buffer.alloc_out 1024
-    exec_cmd, exec_args = build_args(args)
-    pid = LibUtil.forkpty(mfdp, name, nil, nil)
-    #
-    # We want to do as little as possible in the child process, since we're running
-    # without any GC thread now, so test for the child case first
-    #
-    if pid == 0
-      LibC.execvp(exec_cmd, exec_args)
-      LibC._exit(1)
+
+    def hack_close_on_exec(fd)
+      # I assume this isn't actually supported for good reasons.
+      # but let's see how close to passing test_pty we can get.
+      fl = fd.fcntl(Fcntl::F_GETFL, 0)
+      fd.fcntl(Fcntl::F_SETFL, Fcntl::FD_CLOEXEC|fl)
+      fd.define_singleton_method(:close_on_exec?) do
+        true
+      end
     end
-    raise "forkpty failed: #{LibC.strerror(FFI.errno)}" if pid < 0
-    masterfd = mfdp.get_int(0)
-    rfp = FFI::IO.for_fd(masterfd, "r")
-    wfp = FFI::IO.for_fd(LibC.dup(masterfd), "w")
-    if block_given?
-      retval = yield rfp, wfp, pid
-      begin; rfp.close; rescue Exception; end
-      begin; wfp.close; rescue Exception; end
-      retval
-    else
-      [ rfp, wfp, pid ]
+
+    def exception_for_errno(errno)
+      Errno.constants.each do |name|
+        err = Errno.const_get(name)
+        if err.constants.include?(:Errno) && err.const_get(:Errno) == errno
+          return err
+        end
+      end
+      SystemCallError.new("errno=#{errno}")
     end
   end
 end
