e66fb7ca090564f72598137db18cc88790207b88
kares <self@kares.org>
2015-08-15 23:32:55 +0200

    use (Java 6+) compiler API instead of `javac` (to make sure right is used)

--- spec/java_integration/interfaces/static_methods_spec.rb
+++ spec/java_integration/interfaces/static_methods_spec.rb
@@ -1,37 +1,50 @@
 require File.dirname(__FILE__) + "/../spec_helper"
-require 'tmpdir'
 
-if (ENV_JAVA['java.specification.version'] >= '1.8')
-  describe "An interface with static methods" do
-    before :all do
-      src = <<-JAVA
-      public interface Java8Interface {
-        static String message() {
-          return "hello";
-        }
-      }
-      JAVA
-      @tmpdir = Dir.mktmpdir
-      $CLASSPATH << @tmpdir
-      @src = File.open(@tmpdir + "/Java8Interface.java", 'w'){ |f| f.print(src) }
+describe "an interface with static methods (Java 8+)" do
 
-      system "javac #{@tmpdir + "/Java8Interface.java"}"
-    end
+  before :all do
+    src = <<-JAVA
+    public interface Java8Interface {
+      static String message() { return "hello"; }
+      static String message(CharSequence name) { return "hello " + name; }
+    }
+    JAVA
 
-    after :all do
-      FileUtils.rm_rf @tmpdir
-    end
+    require 'tmpdir'; @tmpdir = Dir.mktmpdir
+    @src = File.open(file = "#{@tmpdir}/Java8Interface.java", 'w') { |f| f.print(src) }
 
-    it "binds those methods on the proxy module" do
-      expect(Java::Java8Interface.message).to eq("hello")
-    end
-
-    it "exposes those methods via java_send" do
-      expect(Java::Java8Interface.java_send(:message)).to eq("hello")
-    end
-
-    it "exposes those methods via java_method" do
-      expect(Java::Java8Interface.java_method(:message).call).to eq("hello")
-    end
+    fail "#{file.inspect} compilation failed!" unless javac_compile [ file ]
+    # puts Dir.entries(@tmpdir).inspect
+    $CLASSPATH << @tmpdir
   end
-end
+
+  after :all do
+    FileUtils.rm_rf @tmpdir
+  end
+
+  it "binds those methods on the proxy module" do
+    expect(Java::Java8Interface.message).to eq("hello")
+  end
+
+  it "exposes those methods via java_send" do
+    expect(Java::Java8Interface.java_send(:message)).to eq("hello")
+  end
+
+  it "exposes those methods via java_send with type" do
+    expect(Java::Java8Interface.java_send(:message, [java.lang.CharSequence], 'world')).to eq("hello world")
+  end
+
+  it "exposes those methods via java_method" do
+    expect(Java::Java8Interface.java_method(:message).call).to eq("hello")
+  end
+
+  def javac_compile(files)
+    compiler = javax.tools.ToolProvider.getSystemJavaCompiler
+    fmanager = compiler.getStandardFileManager(nil, nil, nil)
+    diagnostics = nil
+    units = fmanager.getJavaFileObjectsFromStrings( files.map { |fname| fname.to_s } )
+    compilation_task = compiler.getTask(nil, fmanager, diagnostics, nil, nil, units)
+    compilation_task.call # returns boolean
+  end
+
+end if ENV_JAVA['java.specification.version'] >= '1.8'
