a7e959f3bffc26809121c1b0dbd09bef021d8b71
kares <self@kares.org>
2015-08-25 19:44:01 +0200

    messed up require path in test during last 1_7 merge

--- test/jruby/test_load_gem_extensions.rb
+++ test/jruby/test_load_gem_extensions.rb
@@ -19,14 +19,14 @@ class TestLoad < Test::Unit::TestCase
 
   def test_require_extension_file_via_uri_protocol
     return skip 'needs jruby-home from filesystem' if inside_jar?
-    require 'uri:file:./lib/ruby/shared/json/ext/parser'
+    require 'uri:file:./lib/ruby/stdlib/json/ext/parser'
     # just check if extension class exists
     JSON::Ext::Parser
   end
 
   def test_require_extension_file_via_uri_classloader_protocol
     return skip 'needs jruby-home from filesystem' if inside_jar?
-    require 'uri:classloader:/lib/ruby/shared/json/ext/generator'
+    require 'uri:classloader:/lib/ruby/stdlib/json/ext/generator'
     # just check if extension class exists
     JSON::Ext::Generator
   end
