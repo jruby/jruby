bf3ad9cf46877ccc0b186a4462aab000a34b2820
kares <self@kares.org>
2015-08-25 21:15:21 +0200

    seems skip helper might be actually needed on 9K as well (test:jruby failing)

--- test/jruby/test_helper.rb
+++ test/jruby/test_helper.rb
@@ -115,22 +115,22 @@ module TestHelper
     assert run_in_sub_runtime(script)
   end
 
-#  def self.included(base)
-#    if defined? Test::Unit::TestCase
-#      if base < Test::Unit::TestCase
-#        Test::Unit::TestCase.class_eval do
-#          unless method_defined?(:skip)
-#            if method_defined?(:omit)
-#              alias skip omit
-#            else
-#              def skip(msg = nil)
-#                warn "Skipped: #{caller[0]} #{msg}"
-#              end
-#            end
-#          end
-#        end
-#      end
-#    end
-#  end
+  def self.included(base)
+    if defined? Test::Unit::TestCase
+      if base < Test::Unit::TestCase
+        Test::Unit::TestCase.class_eval do
+          unless method_defined?(:skip)
+            if method_defined?(:omit)
+              alias skip omit
+            else
+              def skip(msg = nil)
+                warn "Skipped: #{caller[0]} #{msg}"
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 
 end
