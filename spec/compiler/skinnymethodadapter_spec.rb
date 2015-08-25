e996bcfc716e52780ad612a784416074656007b5
Ben Lovell <benjamin.lovell@gmail.com>
2015-08-25 20:06:28 +0100

    Removes trailing whitespace

--- spec/compiler/skinnymethodadapter_spec.rb
+++ spec/compiler/skinnymethodadapter_spec.rb
@@ -22,11 +22,11 @@ class MockMethodVisitor
   end
 end
 
-describe "SkinnyMethodAdapter" do  
+describe "SkinnyMethodAdapter" do
   it "supports all JVM opcodes" do
     insn_opcodes = Opcodes.constants.select do |c|
       case c.to_s # for 1.9's symbols
-        
+
       when /ACC_/, # access modifiers
            /V1_/, # version identifiers
            /T_/, # type identifiers
@@ -34,30 +34,30 @@ describe "SkinnyMethodAdapter" do
            /H_/, # method handles
            /ASM/ # ASM version stuff
         false
-        
+
       when "DOUBLE", "FLOAT", "INTEGER", "LONG", "NULL", "TOP", "UNINITIALIZED_THIS"
         false
-        
+
       when "GOTO", "RETURN", "INSTANCEOF", "NEW"
         false
-        
+
       when "INVOKEDYNAMIC_OWNER"
         false
-        
+
       else
         true
       end
     end
-    
+
     # 1.9 makes them symbols, so to_s the lot
     insn_opcodes = insn_opcodes.map(&:to_s)
     instance_methods = SkinnyMethodAdapter.instance_methods.map(&:to_s)
-    
+
     insn_opcodes.each do |opcode|
       opcode = opcode.downcase
       expect(instance_methods).to include(opcode)
     end
-    
+
     expect(instance_methods).to include(
       "go_to",
       "voidreturn",
