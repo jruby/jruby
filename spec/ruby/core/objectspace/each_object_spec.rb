286c29b3a5130c33a76a838cad98c0b7d66af1aa
Chris Seaton <chris@chrisseaton.com>
2015-08-31 19:08:15 +0100

    Add specs for where ObjectSpace.each_object can find objects.

--- spec/ruby/core/objectspace/each_object_spec.rb
+++ spec/ruby/core/objectspace/each_object_spec.rb
@@ -1,4 +1,7 @@
 require File.expand_path('../../../spec_helper', __FILE__)
+require File.expand_path('../fixtures', __FILE__)
+
+require 'weakref'
 
 describe "ObjectSpace.each_object" do
   it "calls the block once for each living, non-immediate object in the Ruby process" do
@@ -42,5 +45,81 @@ describe "ObjectSpace.each_object" do
     counter.each{}.should == 1
     # this is needed to prevent the new_obj from being GC'd too early
     new_obj.should_not == nil
+  end
+
+  it "finds an object stored in a global variable" do
+    $object_space_global_variable = ObjectSpaceFixtures::ObjectToBeFound.new(:global)
+    ObjectSpaceFixtures::to_be_found_symbols.should include(:global)
+  end
+
+  it "finds an object stored in a top-level constant" do
+    ObjectSpaceFixtures::to_be_found_symbols.should include(:top_level_constant)
+  end
+
+  it "finds an object stored in a top-level constant" do
+    ObjectSpaceFixtures::to_be_found_symbols.should include(:second_level_constant)
+  end
+
+  it "finds an object stored in a local variable" do
+    local = ObjectSpaceFixtures::ObjectToBeFound.new(:local)
+    ObjectSpaceFixtures::to_be_found_symbols.should include(:local)
+  end
+
+  it "finds an object stored in a local variable captured in a block" do
+    proc = Proc.new {
+      local_in_block = ObjectSpaceFixtures::ObjectToBeFound.new(:local_in_block)
+      Proc.new { }
+    }.call
+
+    ObjectSpaceFixtures::to_be_found_symbols.should include(:local_in_block)
+  end
+
+  it "finds an object stored in a local variable captured in a Proc#binding" do
+    binding = Proc.new {
+      local_in_proc_binding = ObjectSpaceFixtures::ObjectToBeFound.new(:local_in_proc_binding)
+      Proc.new{ }.binding
+    }.call
+
+    ObjectSpaceFixtures::to_be_found_symbols.should include(:local_in_proc_binding)
+  end
+
+  it "finds an object stored in a local variable captured in a Kernel#binding" do
+    b = Proc.new {
+      local_in_kernel_binding = ObjectSpaceFixtures::ObjectToBeFound.new(:local_in_kernel_binding)
+      binding
+    }.call
+
+    ObjectSpaceFixtures::to_be_found_symbols.should include(:local_in_kernel_binding)
+  end
+
+  it "finds an object stored in a local variable set in a binding manually" do
+    b = binding
+    b.local_variable_set(:local, ObjectSpaceFixtures::ObjectToBeFound.new(:local_in_manual_binding))
+    ObjectSpaceFixtures::to_be_found_symbols.should include(:local_in_manual_binding)
+  end
+
+  it "finds an object stored in an array" do
+    array = [ObjectSpaceFixtures::ObjectToBeFound.new(:array)]
+    ObjectSpaceFixtures::to_be_found_symbols.should include(:array)
+  end
+
+  it "finds an object stored in a hash key" do
+    hash = {ObjectSpaceFixtures::ObjectToBeFound.new(:hash_key) => :value}
+    ObjectSpaceFixtures::to_be_found_symbols.should include(:hash_key)
+  end
+
+  it "finds an object stored in a hash value" do
+    hash = {a: ObjectSpaceFixtures::ObjectToBeFound.new(:hash_value)}
+    ObjectSpaceFixtures::to_be_found_symbols.should include(:hash_value)
+  end
+
+  it "finds an object stored in an instance variable" do
+    local = ObjectSpaceFixtures::ObjectWithInstanceVariable.new
+    ObjectSpaceFixtures::to_be_found_symbols.should include(:instance_variable)
+  end
+
+  it "doesn't find an object stored in a WeakRef that should have been cleared" do
+    weak_ref = WeakRef.new(ObjectSpaceFixtures::ObjectToBeFound.new(:weakref))
+    ObjectSpaceFixtures::to_be_found_symbols.should_not include(:weakref)
   end
 end
--- spec/ruby/core/objectspace/fixtures.rb
+++ spec/ruby/core/objectspace/fixtures.rb
@@ -32,4 +32,29 @@ module ObjectSpaceFixtures
     return Proc.new { wr.write "finalized"; wr.close }
   end
 
+  class ObjectToBeFound
+    attr_reader :id
+
+    def initialize(id)
+      @id = id
+    end
+  end
+
+  class ObjectWithInstanceVariable
+    def initialize
+      @instance_variable = ObjectToBeFound.new(:instance_variable)
+    end
+  end
+
+  def self.to_be_found_symbols
+    GC.start
+    ObjectSpace.each_object(ObjectToBeFound).map do |o|
+      o.id
+    end
+  end
+
+  SECOND_LEVEL_CONSTANT = ObjectToBeFound.new(:second_level_constant)
+
 end
+
+OBJECT_SPACE_TOP_LEVEL_CONSTANT = ObjectSpaceFixtures::ObjectToBeFound.new(:top_level_constant)
