34c2316a6a2ff17915d4d8080f77c42cf99a4501
Charles Oliver Nutter <headius@headius.com>
2015-09-03 16:56:41 -0500

    Update to jnr-ffi-2.0.4-SNAPSHOT for #3285.
    See also jnr/jnr-ffi#50.

--- core/pom.rb
+++ core/pom.rb
@@ -47,7 +47,7 @@ project 'JRuby Core' do
   jar 'com.github.jnr:jnr-unixsocket:0.8', :exclusions => ['com.github.jnr:jnr-ffi']
   jar 'com.github.jnr:jnr-posix:3.0.15', :exclusions => ['com.github.jnr:jnr-ffi']
   jar 'com.github.jnr:jnr-constants:0.9.0', :exclusions => ['com.github.jnr:jnr-ffi']
-  jar 'com.github.jnr:jnr-ffi:2.0.3'
+  jar 'com.github.jnr:jnr-ffi:2.0.4-SNAPSHOT'
   jar 'com.github.jnr:jffi:${jffi.version}'
   jar 'com.github.jnr:jffi:${jffi.version}:native'
 
