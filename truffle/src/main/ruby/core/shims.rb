592098f85a7b0bad232aa2aa0fa9e4aef447769c
Petr Chalupa <git@pitr.ch>
2015-09-10 11:19:29 +0200

    [Truffle] code formatting

--- truffle/src/main/ruby/core/shims.rb
+++ truffle/src/main/ruby/core/shims.rb
@@ -1,7 +1,7 @@
 # Copyright (c) 2014, 2015 Oracle and/or its affiliates. All rights reserved. This
 # code is released under a tri EPL/GPL/LGPL license. You can use it,
 # redistribute it and/or modify it under the terms of the:
-# 
+#
 # Eclipse Public License version 1.0
 # GNU General Public License version 2
 # GNU Lesser General Public License version 2.1
@@ -206,12 +206,16 @@ unless ENV['HOME']
   end
 end
 
-class Exception 
+class Exception
 
   def locations
     # These should be Rubinius::Location
     # and use the internal backtrace, never the custom one.
-    backtrace.each { |s| def s.position; self; end }
+    backtrace.each do |s|
+      def s.position
+        self
+      end
+    end
   end
 
   def to_s
