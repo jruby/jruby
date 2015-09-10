a9f470355a857cfe4cca98a04c9d422e07d268b4
kares <self@kares.org>
2015-09-10 08:25:29 +0200

    [find-bugs] avoid using a ternary operator to cast a boolean to true/false

--- core/src/main/java/org/jruby/RubyHash.java
+++ core/src/main/java/org/jruby/RubyHash.java
@@ -2073,7 +2073,7 @@ public class RubyHash extends RubyObject implements Map {
     public boolean equals(Object other) {
         if (!(other instanceof RubyHash)) return false;
         if (this == other) return true;
-        return op_equal(getRuntime().getCurrentContext(), (RubyHash)other).isTrue() ? true : false;
+        return op_equal(getRuntime().getCurrentContext(), (RubyHash)other).isTrue();
     }
 
     public Set keySet() {
