be6a2bf575902fe3acd8a881d92388bcbe1a53a0
kares <self@kares.org>
2015-09-15 10:22:32 +0200

    one ':' is enough on ": unhandled exception" & find-bugs-style cleanup

--- core/src/main/java/org/jruby/runtime/backtrace/TraceType.java
+++ core/src/main/java/org/jruby/runtime/backtrace/TraceType.java
@@ -246,16 +246,17 @@ public class TraceType {
     }
 
     protected static String printBacktraceMRI(RubyException exception, boolean console) {
-        Ruby runtime = exception.getRuntime();
-        ThreadContext context = runtime.getCurrentContext();
-        IRubyObject backtrace = exception.callMethod(context, "backtrace");
+        final Ruby runtime = exception.getRuntime();
+        final ThreadContext context = runtime.getCurrentContext();
+
+        final IRubyObject backtrace = exception.callMethod(context, "backtrace");
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PrintStream errorStream = new PrintStream(baos);
         boolean printedPosition = false;
         if (backtrace.isNil() || !(backtrace instanceof RubyArray)) {
             if (context.getFile() != null && context.getFile().length() > 0) {
-                errorStream.print(context.getFile() + ":" + context.getLine());
+                errorStream.print(context.getFile() + ':' + context.getLine());
                 printedPosition = true;
             } else {
                 errorStream.print(context.getLine());
@@ -277,11 +278,10 @@ public class TraceType {
         RubyClass type = exception.getMetaClass();
         String info = exception.toString();
 
-        if (printedPosition) errorStream.print(": ");
-
         if (type == runtime.getRuntimeError() && (info == null || info.length() == 0)) {
             errorStream.print(": unhandled exception\n");
         } else {
+            if (printedPosition) errorStream.print(": ");
             String path = type.getName();
 
             if (info.length() == 0) {
@@ -292,9 +292,10 @@ public class TraceType {
                 }
 
                 String tail = null;
-                if (info.indexOf("\n") != -1) {
-                    tail = info.substring(info.indexOf("\n") + 1);
-                    info = info.substring(0, info.indexOf("\n"));
+                int idx = info.indexOf('\n');
+                if (idx != -1) {
+                    tail = info.substring(idx + 1);
+                    info = info.substring(0, idx);
                 }
 
                 errorStream.print(info);
@@ -320,13 +321,14 @@ public class TraceType {
     private static final String CLEAR_COLOR = "\033[0m";
 
     protected static String printBacktraceJRuby(RubyException exception, boolean console) {
-        Ruby runtime = exception.getRuntime();
+        final Ruby runtime = exception.getRuntime();
+        final ThreadContext context = runtime.getCurrentContext();
 
         StringBuilder buffer = new StringBuilder();
         boolean color = console && runtime.getInstanceConfig().getBacktraceColor();
 
         // exception line
-        String message = exception.message(runtime.getCurrentContext()).toString();
+        String message = exception.message(context).toString();
         if (exception.getMetaClass() == runtime.getRuntimeError() && message.length() == 0) {
             message = "No current exception";
         }
@@ -412,7 +414,7 @@ public class TraceType {
         for (int i = 0; i < trace.length; i++) {
             RubyStackTraceElement element = trace[i];
 
-            RubyString str = RubyString.newString(runtime, element.getFileName() + ":" + element.getLineNumber() + ":in `" + element.getMethodName() + "'");
+            RubyString str = RubyString.newString(runtime, element.getFileName() + ':' + element.getLineNumber() + ":in `" + element.getMethodName() + "'");
             traceArray.append(str);
         }
 
@@ -422,10 +424,10 @@ public class TraceType {
     private static void printErrorPos(ThreadContext context, PrintStream errorStream) {
         if (context.getFile() != null && context.getFile().length() > 0) {
             if (context.getFrameName() != null) {
-                errorStream.print(context.getFile() + ":" + context.getLine());
+                errorStream.print(context.getFile() + ':' + context.getLine());
                 errorStream.print(":in '" + context.getFrameName() + '\'');
             } else if (context.getLine() != 0) {
-                errorStream.print(context.getFile() + ":" + context.getLine());
+                errorStream.print(context.getFile() + ':' + context.getLine());
             } else {
                 errorStream.print(context.getFile());
             }
