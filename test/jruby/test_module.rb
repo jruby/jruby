793fb039eecb38b694baf41795c889d61fb529a5
kares <self@kares.org>
2015-09-17 19:03:12 +0200

    make test_module.rb a "real" unit test-case

--- test/jruby/test_module.rb
+++ test/jruby/test_module.rb
@@ -1,5 +1,13 @@
-def test_prepend_features_type_error
-  assert_raise(TypeError) do
-    Module.new.instance_eval { prepend_features(1) }
+require 'test/unit'
+require 'test/jruby/test_helper'
+
+class ModuleTest < Test::Unit::TestCase
+  # include TestHelper
+
+  def test_prepend_features_type_error
+    assert_raise(TypeError) do
+      Module.new.instance_eval { prepend_features(1) }
+    end
   end
+
 end
