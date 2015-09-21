4d5f1f92fae6dfad69df3201cf60ae7e5ee22661
Kevin Menard <kevin@nirvdrum.com>
2015-09-21 09:27:50 -0400

    String#oct shouldn't allow leading '_'.

--- core/src/main/java/org/jruby/RubyString.java
+++ core/src/main/java/org/jruby/RubyString.java
@@ -3311,7 +3311,7 @@ public class RubyString extends RubyObject implements EncodingCapable, MarshalEn
         if (!value.getEncoding().isAsciiCompatible()) {
             throw context.runtime.newEncodingCompatibilityError("ASCII incompatible encoding: " + value.getEncoding());
         }
-        return stringToInum(-8, false);
+        return stringToInum19(-8, false);
     }
 
     /** rb_str_hex
