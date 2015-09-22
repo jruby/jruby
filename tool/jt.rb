da934a017f07da833fe8a26571dba40df6b75cf9
Kevin Menard <kevin@nirvdrum.com>
2015-09-21 21:44:45 -0400

    [Truffle] JVMCI options are space-delimited, not comma-delimited.

--- tool/jt.rb
+++ tool/jt.rb
@@ -413,11 +413,11 @@ module Commands
     case command
     when 'debug'
       if args.delete '--ruby-backtrace'
-        compilation_exceptions_behaviour = ",+TruffleCompilationExceptionsAreThrown"
+        compilation_exceptions_behaviour = '+TruffleCompilationExceptionsAreThrown'
       else
-        compilation_exceptions_behaviour = ",+TruffleCompilationExceptionsAreFatal"
+        compilation_exceptions_behaviour = '+TruffleCompilationExceptionsAreFatal'
       end
-      env_vars = env_vars.merge({'JRUBY_OPTS' => "-J-Djvmci.options=+TraceTruffleCompilation,+DumpOnError,-GraphPE#{compilation_exceptions_behaviour}"})
+      env_vars = env_vars.merge({'JRUBY_OPTS' => "-J-Djvmci.options='+TraceTruffleCompilation #{compilation_exceptions_behaviour}'"})
       bench_args += ['score', 'jruby-9000-dev-truffle-graal', '--show-commands', '--show-samples']
       raise 'specify a single benchmark for run - eg classic-fannkuch-redux' if args.size != 1
     when 'reference'
