df4269603595f79972f5ba951b8a1ae9e636274e
Chris Seaton <chris@chrisseaton.com>
2015-10-02 15:42:56 +0100

    [Truffle] Tag Binding specs while we figure out the FrameDescriptor issue.

--- test/truffle/pe/core/binding_pe.rb
+++ test/truffle/pe/core/binding_pe.rb
@@ -10,24 +10,24 @@
 
 # Kernel#binding
 tagged_example "x = 14; binding.local_variable_get(:x)", 14
-example "x = 14; binding.local_variable_get(:x) * 2", 28
+tagged_example "x = 14; binding.local_variable_get(:x) * 2", 28
 
 # Proc#binding
 tagged_example "x = 14; p = Proc.new { }; p.binding.local_variable_get(:x)", 14
-example "x = 14; p = Proc.new { }; p.binding.local_variable_get(:x) * 2", 28
+tagged_example "x = 14; p = Proc.new { }; p.binding.local_variable_get(:x) * 2", 28
 
 # set + get
 tagged_example "b = binding; b.local_variable_set(:x, 14); b.local_variable_get(:x)", 14
-example "b = binding; b.local_variable_set(:x, 14); b.local_variable_get(:x) * 2", 28
+tagged_example "b = binding; b.local_variable_set(:x, 14); b.local_variable_get(:x) * 2", 28
 
 # get (2 levels)
 tagged_example "x = 14; y = nil; 1.times { y = binding.local_variable_get(:x) }; y", 14
-example "x = 14; y = nil; 1.times { y = binding.local_variable_get(:x) }; y * 2", 28
+tagged_example "x = 14; y = nil; 1.times { y = binding.local_variable_get(:x) }; y * 2", 28
 
 # set (2 levels)
 tagged_example "x = 14; 1.times { binding.local_variable_set(:x, 15) }; x", 15
-example "x = 14; 1.times { binding.local_variable_set(:x, 15) }; x * 2", 30
+tagged_example "x = 14; 1.times { binding.local_variable_set(:x, 15) }; x * 2", 30
 
 # get + set (2 levels)
 tagged_example "x = 14; y = nil; 1.times { binding.local_variable_set(:x, 15); y = binding.local_variable_get(:x) }; y", 15
-example "x = 14; y = nil; 1.times { binding.local_variable_set(:x, 15); y = binding.local_variable_get(:x) }; y * 2", 30
+tagged_example "x = 14; y = nil; 1.times { binding.local_variable_set(:x, 15); y = binding.local_variable_get(:x) }; y * 2", 30
