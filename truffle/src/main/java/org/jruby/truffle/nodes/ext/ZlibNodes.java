3dafa1835fe8ce065d8647a2854e7ef06572de9e
Kevin Menard <kevin@nirvdrum.com>
2015-10-06 17:53:20 -0400

    [Truffle] Switched from a Field to a MethodHandle.
    This should perform better since the access check is performed up front rather than on each invocation. It also plays better with SVM.

--- truffle/src/main/java/org/jruby/truffle/nodes/ext/ZlibNodes.java
+++ truffle/src/main/java/org/jruby/truffle/nodes/ext/ZlibNodes.java
@@ -25,6 +25,8 @@ import org.jruby.truffle.runtime.layouts.Layouts;
 import org.jruby.util.ByteList;
 import org.jruby.util.StringSupport;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
 import java.util.zip.CRC32;
 import java.util.zip.DataFormatException;
@@ -153,15 +155,17 @@ public abstract class ZlibNodes {
     @CoreMethod(names = "adler32", isModuleFunction = true, required = 0, optional = 2, lowerFixnumParameters = 1)
     public abstract static class Adler32Node extends CoreMethodArrayArgumentsNode {
 
-        private static final Field ADLER_PRIVATE_FIELD;
+        private static final MethodHandle ADLER_PRIVATE_FIELD;
 
         static {
             try {
-                ADLER_PRIVATE_FIELD = Adler32.class.getDeclaredField("adler");
-            } catch (NoSuchFieldException e) {
+                final Field f = Adler32.class.getDeclaredField("adler");
+                f.setAccessible(true);
+
+                ADLER_PRIVATE_FIELD = MethodHandles.lookup().unreflectSetter(f);
+            } catch (IllegalAccessException | NoSuchFieldException e) {
                 throw new RuntimeException(e);
             }
-            ADLER_PRIVATE_FIELD.setAccessible(true);
         }
 
         public Adler32Node(RubyContext context, SourceSection sourceSection) {
@@ -189,8 +193,8 @@ public abstract class ZlibNodes {
             final Adler32 adler32 = new Adler32();
 
             try {
-                ADLER_PRIVATE_FIELD.setInt(adler32, adler);
-            } catch (IllegalAccessException e) {
+                ADLER_PRIVATE_FIELD.invoke(adler32, adler);
+            } catch (Throwable e) {
                 throw new RuntimeException(e);
             }
 
