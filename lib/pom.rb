ee635b001995a15699cda2824f15b5ba3e789075
Christian Meier <meier.kristian@gmail.com>
2015-10-22 18:28:33 +0200

    [build] remove the gems maven-repos

--- lib/pom.rb
+++ lib/pom.rb
@@ -69,10 +69,10 @@ project 'JRuby Lib Setup' do
   # just depends on jruby-core so we are sure the jruby.jar is in place
   jar "org.jruby:jruby-core:#{version}", :scope => 'test'
 
-  repository( :url => 'https://otto.takari.io/content/repositories/rubygems/maven/releases',
-              :id => 'rubygems-releases' )
-  repository( :url => 'http://rubygems-proxy.torquebox.org/releases',
-              :id => 'tb-rubygems-releases' )
+  #repository( :url => 'https://otto.takari.io/content/repositories/rubygems/maven/releases',
+  #            :id => 'rubygems-releases' )
+  #repository( :url => 'http://rubygems-proxy.torquebox.org/releases',
+  #            :id => 'tb-rubygems-releases' )
   # for testing out jruby-ossl before final release :
   #repository( :url => 'http://oss.sonatype.org/content/repositories/staging',
   #            :id => 'gem-staging' )
