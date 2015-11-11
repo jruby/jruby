4d48532fa011dca06bfc8150e1bc7a092076eb03
Chris Seaton <chris@chrisseaton.com>
2015-11-11 16:10:19 +0000

    [Truffle] Disable Sinatra test for now.

--- test/truffle/integration-tests.sh
+++ test/truffle/integration-tests.sh
@@ -38,10 +38,11 @@ bin/jruby -X-T bin/gem install rack
 bin/jruby -X+T -Ilib/ruby/gems/shared/gems/rack-1.6.4/lib test/truffle/simple-webrick-server.rb &
 test_server
 
-echo "Simple Sinatra web server..."
-bin/jruby -X-T bin/gem install sinatra
-bin/jruby -X+T -Ilib/ruby/gems/shared/gems/rack-1.6.4/lib -Ilib/ruby/gems/shared/gems/tilt-2.0.1/lib -Ilib/ruby/gems/shared/gems/rack-protection-1.5.3/lib -Ilib/ruby/gems/shared/gems/sinatra-1.4.6/lib test/truffle/simple-sinatra-server.rb &
-test_server
+# TODO CS 11-Nov-15 doesn't work on GraalVm as where do the gems get installed?
+#echo "Simple Sinatra web server..."
+#bin/jruby -X-T bin/gem install sinatra
+#bin/jruby -X+T -Ilib/ruby/gems/shared/gems/rack-1.6.4/lib -Ilib/ruby/gems/shared/gems/tilt-2.0.1/lib -Ilib/ruby/gems/shared/gems/rack-protection-1.5.3/lib -Ilib/ruby/gems/shared/gems/sinatra-1.4.6/lib test/truffle/simple-sinatra-server.rb &
+#test_server
 
 #echo "Coverage..."
 #bin/jruby -X+T -Xtruffle.coverage=true test/truffle/coverage/test.rb
