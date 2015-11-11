d47fd14b74c9e928f987107f903f80ddde446e89
kares <self@kares.org>
2015-11-11 06:56:45 +0100

    [findbugs] use chars instead of strings in concat

--- core/src/main/java/org/jruby/runtime/backtrace/RubyStackTraceElement.java
+++ core/src/main/java/org/jruby/runtime/backtrace/RubyStackTraceElement.java
@@ -53,8 +53,8 @@ public class RubyStackTraceElement {
     public String toString() {
         return element.toString();
     }
-    
+
     public String mriStyleString() {
-        return element.getFileName() + ":" + element.getLineNumber() + ":in `" + element.getMethodName() + "'";
+        return element.getFileName() + ':' + element.getLineNumber() + ":in `" + element.getMethodName() + '\'';
     }
 }
