3cc7f862d409b9db361a4e5e0e9778521f90d1b5
Kevin Menard <kevin@nirvdrum.com>
2015-11-23 19:01:22 -0500

    [Truffle] Simplified the fix in 5446cc2d255551dfe93121449e1797e78fe6892b.

--- truffle/src/main/java/org/jruby/truffle/runtime/methods/SharedMethodInfo.java
+++ truffle/src/main/java/org/jruby/truffle/runtime/methods/SharedMethodInfo.java
@@ -41,7 +41,7 @@ public class SharedMethodInfo {
         this.arity = arity;
         this.name = name;
         this.isBlock = isBlock;
-        this.argumentDescriptors = argumentDescriptors;
+        this.argumentDescriptors = argumentDescriptors == null ? new ArgumentDescriptor[] {} : argumentDescriptors;
         this.alwaysClone = alwaysClone;
         this.alwaysInline = alwaysInline;
         this.needsCallerFrame = needsCallerFrame;
@@ -68,10 +68,6 @@ public class SharedMethodInfo {
     }
 
     public ArgumentDescriptor[] getArgumentDescriptors() {
-        if (argumentDescriptors == null) {
-            return new ArgumentDescriptor[] {};
-        }
-
         return Arrays.copyOf(argumentDescriptors, argumentDescriptors.length);
     }
 
