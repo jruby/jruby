e50459e7a7fe44e0c80ab8972050f0477c540885
Charles Oliver Nutter <headius@headius.com>
2016-01-01 17:39:14 -0600

    Accept numeric strings for port.

--- core/src/main/java/org/jruby/ext/socket/SocketUtils.java
+++ core/src/main/java/org/jruby/ext/socket/SocketUtils.java
@@ -651,6 +651,12 @@ public class SocketUtils {
     }
 
     public static int portToInt(IRubyObject port) {
-        return port.isNil() ? 0 : RubyNumeric.fix2int(port);
+        if (port.isNil()) return 0;
+
+        Ruby runtime = port.getRuntime();
+
+        IRubyObject maybeStr = TypeConverter.checkStringType(runtime, port);
+        if (!maybeStr.isNil()) return RubyNumeric.fix2int(maybeStr.convertToString().to_i());
+        return RubyNumeric.fix2int(port);
     }
 }
