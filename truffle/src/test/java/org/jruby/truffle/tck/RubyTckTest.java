c2011c3da9c4dcea5eecfdca9cd3f9cabbc1919d
Chris Seaton <chris@chrisseaton.com>
2016-01-01 20:33:17 +0000

    [Truffle] Fix extra newline.

--- truffle/src/test/java/org/jruby/truffle/tck/RubyTckTest.java
+++ truffle/src/test/java/org/jruby/truffle/tck/RubyTckTest.java
@@ -132,7 +132,6 @@ public class RubyTckTest extends TruffleTCK {
          * Not running this test as it clears the engine, but we're caching that globally to avoid creating tens of
          * engines concurrently.
          */
-
     }
 
 }
