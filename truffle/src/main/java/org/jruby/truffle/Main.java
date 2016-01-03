288efcbf8b87f7d0ec9102d254acc83acab5d80d
Chris Seaton <chris@chrisseaton.com>
2016-01-03 21:52:53 +0000

    [Truffle] Remove DATA stuff from the SVM entry point.

--- truffle/src/main/java/org/jruby/truffle/Main.java
+++ truffle/src/main/java/org/jruby/truffle/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Oracle and/or its affiliates. All rights reserved. This
+ * Copyright (c) 2015, 2016 Oracle and/or its affiliates. All rights reserved. This
  * code is released under a tri EPL/GPL/LGPL license. You can use it,
  * redistribute it and/or modify it under the terms of the:
  *
@@ -55,13 +55,6 @@ public class Main {
 
             RootNode scriptNode = (RootNode) runtime.parseFromMain(filename, in);
 
-            // If no DATA, we're done with the stream, shut it down
-            if (runtime.fetchGlobalConstant("DATA") == null) {
-                try {
-                    in.close();
-                } catch (IOException ioe) {
-                }
-            }
             ThreadContext context = runtime.getCurrentContext();
 
             String oldFile = context.getFile();
