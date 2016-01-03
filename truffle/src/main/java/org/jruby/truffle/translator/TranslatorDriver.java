d5e5b2b1b858ff96ed9744d6b45ac891aa5e3bb3
Chris Seaton <chris@chrisseaton.com>
2016-01-03 21:23:11 +0000

    [Truffle] Entirely remove DATA stuff from TranslatorDriver

--- truffle/src/main/java/org/jruby/truffle/translator/TranslatorDriver.java
+++ truffle/src/main/java/org/jruby/truffle/translator/TranslatorDriver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2015 Oracle and/or its affiliates. All rights reserved. This
+ * Copyright (c) 2013, 2016 Oracle and/or its affiliates. All rights reserved. This
  * code is released under a tri EPL/GPL/LGPL license. You can use it,
  * redistribute it and/or modify it under the terms of the:
  *
@@ -144,14 +144,6 @@ public class TranslatorDriver {
             for (String name : argumentNames) {
                 environment.declareVar(name);
             }
-        }
-
-        // Get the DATA constant
-
-        final Object data = getData(context);
-
-        if (data != null) {
-            Layouts.MODULE.getFields(context.getCoreLibrary().getObjectClass()).setConstant(context, currentNode, "DATA", data);
         }
 
         // Translate to Ruby Truffle nodes
