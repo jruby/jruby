8511e2bbe90eaffdbaaa554f59b2065a438c6b53
Charles Oliver Nutter <headius@headius.com>
2016-01-09 00:36:37 -0600

    Remove unused fields and code from compiled blocks.

--- core/src/main/java/org/jruby/runtime/CompiledIRBlockBody.java
+++ core/src/main/java/org/jruby/runtime/CompiledIRBlockBody.java
@@ -10,17 +10,10 @@ import java.lang.invoke.MethodHandle;
 
 public class CompiledIRBlockBody extends IRBlockBody {
     protected final MethodHandle handle;
-    protected boolean pushScope;
-    protected boolean reuseParentScope;
-    protected boolean usesKwargs;
 
     public CompiledIRBlockBody(MethodHandle handle, IRScope closure, long encodedSignature) {
         super(closure, Signature.decode(encodedSignature));
         this.handle = handle;
-        // FIXME: duplicated from InterpreterContext
-        this.reuseParentScope = closure.getFlags().contains(IRFlags.REUSE_PARENT_DYNSCOPE);
-        this.pushScope = !closure.getFlags().contains(IRFlags.DYNSCOPE_ELIMINATED) && !this.reuseParentScope;
-        this.usesKwargs = closure.receivesKeywordArgs();
 
         // Done in the interpreter (WrappedIRClosure) but we do it here
         closure.getStaticScope().determineModule();
@@ -61,45 +54,4 @@ public class CompiledIRBlockBody extends IRBlockBody {
             return null; // not reached
         }
     }
-
-//    @Override
-//    protected IRubyObject commonYieldPath(ThreadContext context, Block block, Block.Type type,  IRubyObject[] args, IRubyObject self, Block blockArg) {
-//        Binding binding = block.getBinding();
-//        Visibility oldVis = binding.getFrame().getVisibility();
-//        Frame prevFrame = context.preYieldNoScope(binding);
-//
-//        // SSS FIXME: Maybe, we should allocate a NoVarsScope/DummyScope for for-loop bodies because the static-scope here
-//        // probably points to the parent scope? To be verified and fixed if necessary. There is no harm as it is now. It
-//        // is just wasteful allocation since the scope is not used at all.
-//        DynamicScope prevScope = binding.getDynamicScope();
-//        if (this.pushScope) {
-//            // SSS FIXME: for lambdas, this behavior is different
-//            // compared to what InterpretedIRBlockBody and MixedModeIRBlockBody do
-//            context.pushScope(DynamicScope.newDynamicScope(getStaticScope(), prevScope, this.evalType.get()));
-//        } else if (this.reuseParentScope) {
-//            // Reuse! We can avoid the push only if surrounding vars aren't referenced!
-//            context.pushScope(prevScope);
-//        }
-//
-//        self = IRRuntimeHelpers.updateBlockState(block, self);
-//
-//        if (usesKwargs) IRRuntimeHelpers.frobnicateKwargsArgument(context, getSignature().required(), args);
-//
-//        try {
-//            return (IRubyObject) handle.invokeExact(context, block, getStaticScope(), self, args, blockArg, binding.getMethod(), block.type);
-//        } catch (Throwable t) {
-//            Helpers.throwException(t);
-//            return null; // not reached
-//        } finally {
-//            // IMPORTANT: Do not clear eval-type in case this is reused in bindings!
-//            // Ex: eval("...", foo.instance_eval { binding })
-//            // The dyn-scope used for binding needs to have its eval-type set to INSTANCE_EVAL
-//            binding.getFrame().setVisibility(oldVis);
-//            if (this.pushScope || this.reuseParentScope) {
-//                context.postYield(binding, prevFrame);
-//            } else {
-//                context.postYieldNoScope(prevFrame);
-//            }
-//        }
-//    }
 }
