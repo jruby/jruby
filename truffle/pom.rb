0c6960def8c3f977124dba9c993f409928e09049
Chris Seaton <chris@chrisseaton.com>
2016-01-16 13:47:28 +0000

    [Truffle] Update API version.

--- truffle/pom.rb
+++ truffle/pom.rb
@@ -18,7 +18,7 @@ project 'JRuby Truffle' do
   repository( :url => 'http://lafo.ssw.uni-linz.ac.at/nexus/content/repositories/snapshots/',
               :id => 'truffle' )
 
-  truffle_version = '2643b968c0c63613f58adbce02c0087c05072d12-SNAPSHOT'
+  truffle_version = '6b76a24fffbd52e713a9477f0de62d071674cc90-SNAPSHOT'
   jar 'com.oracle.truffle:truffle-api:' + truffle_version
   jar 'com.oracle.truffle:truffle-debug:' + truffle_version
   jar 'com.oracle.truffle:truffle-dsl-processor:' + truffle_version, :scope => 'provided'
