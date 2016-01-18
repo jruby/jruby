0f8beaf42adb311d2f59380bf53dbe8e859bfb82
Chris Seaton <chris@chrisseaton.com>
2016-01-18 20:34:11 +0000

    [Truffle] Move some stuff from test to tool.

--- tool/truffle/java-coverage/coverage.sh
+++ tool/truffle/java-coverage/coverage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 curl -L -o jacoco-agent.jar 'http://search.maven.org/remotecontent?filepath=org/jacoco/org.jacoco.agent/0.7.4.201502262128/org.jacoco.agent-0.7.4.201502262128-runtime.jar'
 curl -L -O 'http://central.maven.org/maven2/org/ow2/asm/asm-all/5.0.4/asm-all-5.0.4.jar'
 curl -L -O 'http://central.maven.org/maven2/org/jacoco/org.jacoco.core/0.7.4.201502262128/org.jacoco.core-0.7.4.201502262128.jar'
@@ -13,6 +15,5 @@ export VERIFY_JRUBY=1
 export JAVA_OPTS="-javaagent:$JACOCO_AGENT=destfile=$JACOCO_LOG"
 
 rm jacoco.exec
-tool/jt.rb test
-test/truffle/integration-tests.sh
-ant -f test/truffle/java-coverage/build.xml
+bin/jruby tool/jt.rb test
+ant -f tool/truffle/java-coverage/build.xml
