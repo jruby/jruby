1a5caa78ce6a61693fccb2d80ea765d3f824f406
Kevin Menard <kevin@nirvdrum.com>
2016-01-19 10:41:04 -0500

    [Truffle] Added more String PE tests.

--- test/truffle/compiler/pe/core/string_pe.rb
+++ test/truffle/compiler/pe/core/string_pe.rb
@@ -12,4 +12,24 @@ example "Truffle::Primitive.create_simple_string.length", simple_string.length
 example "Truffle::Primitive.create_simple_string.getbyte(0)", simple_string.getbyte(0)
 
 example "'abc'.length", 3
+example "'こにちわ'.length", 4
+
+example "'abc'.bytesize", 3
+example "'こにちわ'.bytesize", 12
+
 tagged_example "'abc' == 'abc'", true # seems to fail sometimes
+example "x = 'abc'; x == x", true
+example "x = 'abc'; x == x.dup", true
+
+example "'abc'.ascii_only?", true
+example "'こにちわ'.ascii_only?", false
+
+example "''.ascii?", false
+example "'abc'.ascii?", true
+
+example "'abc'.valid_encoding?", true
+example "'こにちわ'.valid_encoding?", true
+
+example "''.empty?", true
+example "'abc'.empty?", false
+example "'こにちわ'.empty?", false
