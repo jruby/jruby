jruby/jruby

commit d13405b222f6d98177c8ce9e16c08b2ab3b1d56d

Improved impls of Queue and SizedQueue.
* Queue is now based on JDK LinkedBlockingQueue
* SizeQueue is now based on JDK ArrayBlockingQueue
* Using the new RubyThread task logic

core/src/main/java/org/jruby/ext/thread/SizedQueue.java
@@ -39,19 +39,15 @@ import org.jruby.runtime.Visibility;
 import org.jruby.runtime.builtin.IRubyObject;
 import org.jruby.runtime.Visibility;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 /**
  * The "SizedQueue" class from the 'thread' library.
  */
 @JRubyClass(name = "SizedQueue", parent = "Queue")
 public class SizedQueue extends Queue {
     private int capacity;
-
-    @JRubyMethod(name = "new", rest = true, meta = true)
-    public static IRubyObject newInstance(ThreadContext context, IRubyObject recv, IRubyObject[] args, Block block) {
-        SizedQueue result = new SizedQueue(context.runtime, (RubyClass) recv);
-        result.callInit(context, args, block);
-        return result;
-    }
 
     public SizedQueue(Ruby runtime, RubyClass type) {
         super(runtime, type);
@@ -71,19 +67,19 @@ public class SizedQueue extends Queue {
 
     @JRubyMethod
     @Override
-    public synchronized IRubyObject clear(ThreadContext context) {
+    public IRubyObject clear(ThreadContext context) {
         super.clear(context);
         notifyAll();
         return context.runtime.getNil();
     }
 
     @JRubyMethod
-    public synchronized RubyNumeric max(ThreadContext context) {
+    public RubyNumeric max(ThreadContext context) {
         return RubyNumeric.int2fix(context.runtime, capacity);
     }
 
     @JRubyMethod(name = "max=")
-    public synchronized IRubyObject max_set(ThreadContext context, IRubyObject arg) {
+    public IRubyObject max_set(ThreadContext context, IRubyObject arg) {
         initialize(context, arg);
         return arg;
     }
@@ -104,46 +100,12 @@ public class SizedQueue extends Queue {
         if (difference > 0) {
             notifyAll();
         }
-        return context.runtime.getNil();
-    }
-
-    @JRubyMethod(name = {"pop", "deq", "shift"})
-    @Override
-    public synchronized IRubyObject pop(ThreadContext context) {
-        IRubyObject result = super.pop(context);
-        notifyAll();
-        return result;
-    }
-
-    @JRubyMethod(name = {"pop", "deq", "shift"})
-    @Override
-    public synchronized IRubyObject pop(ThreadContext context, IRubyObject arg0) {
-        IRubyObject result = super.pop(context, arg0);
-        notifyAll();
-        return result;
-    }
-
-    @JRubyMethod(name = {"push", "<<"})
-    @Override
-    public synchronized IRubyObject push(ThreadContext context, IRubyObject value) {
-        checkShutdown(context);
-        if (java_length() >= capacity) {
-            numWaiting++;
-            try {
-                while (java_length() >= capacity) {
-                    try {
-                        context.getThread().wait_timeout(this, null);
-                    } catch (InterruptedException e) {
-                    }
-                    checkShutdown(context);
-                }
-            } finally {
-                numWaiting--;
-            }
+        BlockingQueue<IRubyObject> queue = this.queue;
+        if (queue == null) {
+            this.queue = new ArrayBlockingQueue<IRubyObject>(capacity, false);
+        } else {
+            this.queue = new ArrayBlockingQueue<IRubyObject>(capacity, false, queue);
         }
-        super.push(context, value);
-        notifyAll();
-        return context.runtime.getNil();
+        return context.nil;
     }
-    
 }

core/src/main/java/org/jruby/ext/thread/Queue.java
@@ -27,18 +27,22 @@
  ***** END LICENSE BLOCK *****/
 package org.jruby.ext.thread;
 
-import java.util.LinkedList;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.jruby.Ruby;
 import org.jruby.RubyBoolean;
 import org.jruby.RubyClass;
 import org.jruby.RubyNumeric;
 import org.jruby.RubyObject;
+import org.jruby.RubyThread;
 import org.jruby.anno.JRubyClass;
 import org.jruby.anno.JRubyMethod;
 import org.jruby.exceptions.RaiseException;
-import org.jruby.runtime.Block;
 import org.jruby.runtime.ObjectAllocator;
 import org.jruby.runtime.ThreadContext;
+import org.jruby.runtime.Visibility;
 import org.jruby.runtime.builtin.IRubyObject;
 
 /**
@@ -46,19 +50,39 @@ import org.jruby.runtime.builtin.IRubyObject;
  */
 @JRubyClass(name = "Queue")
 public class Queue extends RubyObject {
-    private LinkedList entries;
-    protected volatile int numWaiting = 0;
+    protected BlockingQueue<IRubyObject> queue;
+    protected AtomicLong numWaiting = new AtomicLong();
+    final Ruby runtime;
 
-    @JRubyMethod(name = "new", rest = true, meta = true)
-    public static IRubyObject newInstance(ThreadContext context, IRubyObject recv, IRubyObject[] args, Block block) {
-        Queue result = new Queue(context.runtime, (RubyClass) recv);
-        result.callInit(context, args, block);
-        return result;
-    }
+    final RubyThread.Task<BlockingQueue<IRubyObject>, IRubyObject> takeTask = new RubyThread.Task<BlockingQueue<IRubyObject>, IRubyObject>() {
+        @Override
+        public IRubyObject run(ThreadContext context, BlockingQueue<IRubyObject> queue) throws InterruptedException {
+            return queue.take();
+        }
+
+        @Override
+        public void wakeup(RubyThread self) {
+            self.getNativeThread().interrupt();
+        }
+    };
+
+    final RubyThread.Task<IRubyObject, IRubyObject> putTask = new RubyThread.Task<IRubyObject, IRubyObject>() {
+        @Override
+        public IRubyObject run(ThreadContext context, IRubyObject data) throws InterruptedException {
+            final BlockingQueue<IRubyObject> queue = getQueueSafe();
+            queue.put(data);
+            return context.nil;
+        }
+
+        @Override
+        public void wakeup(RubyThread self) {
+            self.getNativeThread().interrupt();
+        }
+    };
 
     public Queue(Ruby runtime, RubyClass type) {
         super(runtime, type);
-        entries = new LinkedList();
+        this.runtime = runtime;
     }
 
     public static void setup(Ruby runtime) {
@@ -72,92 +96,99 @@ public class Queue extends RubyObject {
         cQueue.defineAnnotatedMethods(Queue.class);
     }
 
+    @JRubyMethod(visibility = Visibility.PRIVATE)
+    public IRubyObject initialize(ThreadContext context) {
+        queue = new LinkedBlockingQueue<IRubyObject>();
+        return context.nil;
+    }
+
     @JRubyMethod(name = "shutdown!")
-    public synchronized IRubyObject shutdown(ThreadContext context) {
-        entries = null;
-        notifyAll();
+    public IRubyObject shutdown(ThreadContext context) {
+        queue = null;
         return context.runtime.getNil();
     }
     
     public synchronized void shutdown() {
-        entries = null;
-        notifyAll();
+        queue = null;
     }
 
     public boolean isShutdown() {
-        return entries == null;
+        return queue == null;
     }
 
-    public synchronized void checkShutdown(ThreadContext context) {
-        if (entries == null) {
-            throw new RaiseException(context.runtime, context.runtime.getThreadError(), "queue shut down", false);
+    public BlockingQueue<IRubyObject> getQueueSafe() {
+        BlockingQueue<IRubyObject> queue = this.queue;
+        checkShutdown();
+        return queue;
+    }
+
+    public synchronized void checkShutdown() {
+        if (queue == null) {
+            throw new RaiseException(runtime, runtime.getThreadError(), "queue shut down", false);
         }
     }
 
     @JRubyMethod
     public synchronized IRubyObject clear(ThreadContext context) {
-        checkShutdown(context);
-        entries.clear();
+        BlockingQueue<IRubyObject> queue = getQueueSafe();
+        queue.clear();
         return context.runtime.getNil();
     }
 
     @JRubyMethod(name = "empty?")
-    public synchronized RubyBoolean empty_p(ThreadContext context) {
-        checkShutdown(context);
-        return context.runtime.newBoolean(entries.size() == 0);
+    public RubyBoolean empty_p(ThreadContext context) {
+        BlockingQueue<IRubyObject> queue = getQueueSafe();
+        return context.runtime.newBoolean(queue.size() == 0);
     }
 
     @JRubyMethod(name = {"length", "size"})
-    public synchronized RubyNumeric length(ThreadContext context) {
-        checkShutdown(context);
-        return RubyNumeric.int2fix(context.runtime, entries.size());
+    public RubyNumeric length(ThreadContext context) {
+        checkShutdown();
+        return RubyNumeric.int2fix(context.runtime, queue.size());
     }
 
-    protected synchronized long java_length() {
-        return entries.size();
+    protected long java_length() {
+        return queue.size();
     }
 
     @JRubyMethod
     public RubyNumeric num_waiting(ThreadContext context) {
-        return context.runtime.newFixnum(numWaiting);
+        return context.runtime.newFixnum(numWaiting.longValue());
     }
 
     @JRubyMethod(name = {"pop", "deq", "shift"})
-    public synchronized IRubyObject pop(ThreadContext context) {
+    public IRubyObject pop(ThreadContext context) {
         return pop(context, true);
     }
 
     @JRubyMethod(name = {"pop", "deq", "shift"})
-    public synchronized IRubyObject pop(ThreadContext context, IRubyObject arg0) {
+    public IRubyObject pop(ThreadContext context, IRubyObject arg0) {
         return pop(context, !arg0.isTrue());
     }
 
     @JRubyMethod(name = {"push", "<<", "enq"})
-    public synchronized IRubyObject push(ThreadContext context, IRubyObject value) {
-        checkShutdown(context);
-        entries.addLast(value);
-        notify();
-        return context.runtime.getNil();
+    public IRubyObject push(ThreadContext context, final IRubyObject value) {
+        checkShutdown();
+        try {
+            return context.getThread().executeTask(context, value, putTask);
+        } catch (InterruptedException ie) {
+            throw context.runtime.newThreadError("interrupted in " + getMetaClass().getName() + "#push");
+        }
     }
 
-    private synchronized IRubyObject pop(ThreadContext context, boolean should_block) {
-        checkShutdown(context);
-        if (!should_block && entries.size() == 0) {
+    private IRubyObject pop(ThreadContext context, boolean should_block) {
+        final BlockingQueue<IRubyObject> queue = getQueueSafe();
+        if (!should_block && queue.size() == 0) {
             throw new RaiseException(context.runtime, context.runtime.getThreadError(), "queue empty", false);
         }
-        numWaiting++;
+        numWaiting.incrementAndGet();
         try {
-            while (java_length() == 0) {
-                try {
-                    context.getThread().wait_timeout(this, null);
-                } catch (InterruptedException e) {
-                }
-                checkShutdown(context);
-            }
+            return context.getThread().executeTask(context, queue, takeTask);
+        } catch (InterruptedException ie) {
+            throw context.runtime.newThreadError("interrupted in " + getMetaClass().getName() + "#pop");
         } finally {
-            numWaiting--;
+            numWaiting.decrementAndGet();
         }
-        return (IRubyObject) entries.removeFirst();
     }
     
 }

commit d54e4cd8f8c2e69073d82557a69bf62c89d5be51

Fix SizedQueue#num_waiting regression
Introduced in d13405b222f6d98177

core/src/main/java/org/jruby/ext/thread/SizedQueue.java
@@ -12,7 +12,7 @@
  * rights and limitations under the License.
  *
  * Copyright (C) 2006 MenTaLguY <mental@rydia.net>
- * 
+ *
  * Alternatively, the contents of this file may be used under the terms of
  * either of the GNU General Public License Version 2 or later (the "GPL"),
  * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
@@ -100,5 +100,20 @@ public class SizedQueue extends Queue {
         this.queue = new ArrayBlockingQueue<IRubyObject>(new_capacity, false);
 
         return this;
+    }
+
+    @JRubyMethod(name = {"push", "<<", "enq"}, required = 1, optional = 1)
+    @Override
+    public IRubyObject push(ThreadContext context, final IRubyObject[] args) {
+        checkShutdown();
+        numWaiting.incrementAndGet();
+        try {
+            context.getThread().executeTask(context, args, putTask);
+            return this;
+        } catch (InterruptedException ie) {
+            throw context.runtime.newThreadError("interrupted in " + getMetaClass().getName() + "#push");
+        } finally {
+            numWaiting.decrementAndGet();
+        }
     }
 }

spec/ruby/library/thread/sizedqueue/num_waiting_spec.rb
@@ -4,4 +4,15 @@ require File.expand_path('../../shared/queue/num_waiting', __FILE__)
 
 describe "Thread::SizedQueue#num_waiting" do
   it_behaves_like :queue_num_waiting, :num_waiting, SizedQueue.new(10)
+
+  it "reports the number of threads waiting to push" do
+    q = SizedQueue.new(1)
+    q.push(1)
+    t = Thread.new { q.push(2) }
+    sleep 0.05 until t.stop?
+    q.num_waiting.should == 1
+
+    q.pop
+    t.join
+  end
 end