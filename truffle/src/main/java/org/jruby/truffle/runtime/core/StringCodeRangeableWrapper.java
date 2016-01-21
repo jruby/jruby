5a2465fad57ce24aceac9e0201333367a3c50377
Kevin Menard <kevin@nirvdrum.com>
2016-01-21 11:12:48 -0500

    [Truffle] Initial work on tidying up StringSupport.checkEncoding.

--- truffle/src/main/java/org/jruby/truffle/runtime/core/StringCodeRangeableWrapper.java
+++ truffle/src/main/java/org/jruby/truffle/runtime/core/StringCodeRangeableWrapper.java
@@ -80,4 +80,8 @@ public class StringCodeRangeableWrapper implements CodeRangeable {
         return StringOperations.getByteList(string);
     }
 
+
+    public DynamicObject getString() {
+        return string;
+    }
 }
