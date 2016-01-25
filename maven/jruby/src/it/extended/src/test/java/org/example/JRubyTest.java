2e5e942a103ac257163fdbcefb5eaf92b36a4600
kares <self@kares.org>
2016-01-25 16:17:13 +0100

    remove unused imports in maven it tests

--- maven/jruby/src/it/extended/src/test/java/org/example/JRubyTest.java
+++ maven/jruby/src/it/extended/src/test/java/org/example/JRubyTest.java
@@ -2,15 +2,6 @@ package org.example;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
-import java.io.File;
-import java.io.StringWriter;
-import java.net.URL;
-import java.net.URLClassLoader;
-
-import org.jruby.embed.LocalContextScope;
-import org.jruby.embed.ScriptingContainer;
-
 import org.junit.Before;
 import org.junit.Test;
 
