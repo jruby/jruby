16b13e1cca6d74c5d09bf9ad9a5d6f7686c95b0d
Charles Oliver Nutter <headius@headius.com>
2016-01-26 10:23:23 -0600

    Fixes to Time and BigDecimal for #3616.
    * BigDecimal#to_r was not producing correct results.
    * Time.at was not attempting to coerce using to_r.
    * Time.at rational logic was over-complicated.
    
    Fixes #3616 and untags a few specs.

--- core/src/main/java/org/jruby/ext/bigdecimal/RubyBigDecimal.java
+++ core/src/main/java/org/jruby/ext/bigdecimal/RubyBigDecimal.java
@@ -1518,31 +1518,11 @@ public class RubyBigDecimal extends RubyNumeric {
     public IRubyObject to_r(ThreadContext context) {
         checkFloatDomain();
 
-        RubyArray i = split(context);
-        long sign = (long)i.get(0);
-        String digits = (String)i.get(1).toString();
-        long base = (long)i.get(2);
-        long power = (long)i.get(3);
-        long denomi_power = power - digits.length();
+        int scale = value.scale();
+        BigInteger numerator = value.scaleByPowerOfTen(scale).toBigInteger();
+        BigInteger denominator = BigInteger.valueOf((long)Math.pow(10, scale));
 
-        IRubyObject bigDigits = RubyBignum.newBignum(getRuntime(), (String)digits).op_mul(context, sign);
-        RubyBignum numerator;
-        if(bigDigits instanceof RubyBignum) {
-          numerator = (RubyBignum)bigDigits;
-        }
-        else {
-          numerator = RubyBignum.newBignum(getRuntime(), bigDigits.toString());
-        }
-        IRubyObject num, den;
-        if(denomi_power < 0) {
-            num = numerator;
-            den = RubyFixnum.newFixnum(getRuntime(), base).op_mul(context, RubyFixnum.newFixnum(getRuntime(), -denomi_power));
-        }
-        else {
-            num = numerator.op_pow(context, RubyFixnum.newFixnum(getRuntime(), base).op_mul(context, RubyFixnum.newFixnum(getRuntime(), denomi_power)));
-            den = RubyFixnum.newFixnum(getRuntime(), 1);
-        }
-        return RubyRational.newInstance(context, context.runtime.getRational(), num, den);
+        return RubyRational.newInstance(context, context.runtime.getRational(), RubyBignum.newBignum(context.runtime, numerator), RubyBignum.newBignum(context.runtime, denominator));
     }
 
     public IRubyObject to_int19() {
