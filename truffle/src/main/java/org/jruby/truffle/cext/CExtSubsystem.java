221932a3256dab7bcc58cbf14f68eadba2e43c27
Chris Seaton <chris@chrisseaton.com>
2016-01-29 23:37:28 +0100

    [Truffle] Move cext to top level.

--- truffle/src/main/java/org/jruby/truffle/cext/CExtSubsystem.java
+++ truffle/src/main/java/org/jruby/truffle/cext/CExtSubsystem.java
@@ -7,7 +7,7 @@
  * GNU General Public License version 2
  * GNU Lesser General Public License version 2.1
  */
-package org.jruby.truffle.runtime.cext;
+package org.jruby.truffle.cext;
 
 public interface CExtSubsystem {
 
