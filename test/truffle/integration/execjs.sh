cb42d928b9f5da321aeefff06d60702f68062fc2
Chris Seaton <chris@chrisseaton.com>
2016-02-04 17:22:05 +0000

    [Truffle] Let integration tests detect JS for themselves.

--- test/truffle/integration/execjs.sh
+++ test/truffle/integration/execjs.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-# Run with for example JRUBY_OPTS='-J-cp ..../trufflejs.jar'
-
-if [[ $JRUBY_OPTS != *"trufflejs.jar"* ]]
-then
-  echo 'No trufflejs.jar found in $JRUBY_OPTS - skipping ExecJS integration test'
-  exit 0
-fi
-
 bin/jruby bin/gem install execjs -v 2.6.0
 ruby -X+T -Ilib/ruby/gems/shared/gems/execjs-2.6.0/lib test/truffle/integration/execjs/checkruntime.rb
 ruby -X+T -Ilib/ruby/gems/shared/gems/execjs-2.6.0/lib test/truffle/integration/execjs/simple.rb
--- test/truffle/integration/execjs/checkruntime.rb
+++ test/truffle/integration/execjs/checkruntime.rb
@@ -1,3 +1,8 @@
+unless defined?(Truffle) && Truffle::Interop.mime_type_supported?('application/javascript')
+  puts "JavaScript doesn't appear to be available - skipping execjs test"
+  exit
+end
+
 require 'execjs'
 require 'truffle/execjs'
 
