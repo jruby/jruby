f1a24dde519b6cedfd4b56e3c51c963b1a5dea03
Chris Seaton <chris.seaton@oracle.com>
2016-02-07 18:58:34 +0000

    [Truffle] IdProvider is only used in callgraph.

--- truffle/src/main/java/org/jruby/truffle/tools/callgraph/IdProvider.java
+++ truffle/src/main/java/org/jruby/truffle/tools/callgraph/IdProvider.java
@@ -7,7 +7,7 @@
  * GNU General Public License version 2
  * GNU Lesser General Public License version 2.1
  */
-package org.jruby.truffle.util;
+package org.jruby.truffle.tools.callgraph;
 
 import java.util.IdentityHashMap;
 import java.util.Map;
