3a193b8eaace78ac4cb429e1cc2c738fba634d83
Chris Seaton <chris.seaton@oracle.com>
2016-02-09 18:45:29 +0000

    [Truffle] Remove some old documentation.

--- test/truffle/integration/rack-server/rack-server.rb
+++ test/truffle/integration/rack-server/rack-server.rb
@@ -7,16 +7,13 @@
 # GNU Lesser General Public License version 2.1
 
 require 'rack'
- 
-# bin/jruby bin/gem install rack
-# jt run -Ilib/ruby/gems/shared/gems/rack-1.6.1/lib simple-rack-server.rb
- 
+
 class Example
   def call(env)
     return [200, {}, ["Hello Rack!\n"]]
   end
 end
- 
+
 Rack::Handler::WEBrick.run(
   Example.new,
   :Port => 8080
