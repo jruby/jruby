8a846d370eec4ab9c00d7621bfb4562bb3f1a63a
Chris Seaton <chris.seaton@oracle.com>
2016-02-09 03:21:44 +0000

    [Truffle] Mandelbrot and code load demos for metrics.

---
+++ test/truffle/metrics/codeload.rb
@@ -0,0 +1,21 @@
+10_000.times do |n|
+  eval %{
+    def foo#{n}(a, b, c)
+      if a == b
+        a + b + c # comments
+      else
+        a.foo + b.foo + c.foo
+      end
+    end
+    
+    class Foo#{n}
+      def bar#{n}
+        [#{n}, #{n} + 1, #{n} + 2]
+      end
+      
+      def baz#{n}
+        {a: #{n}, b: #{n + 1}, c: #{n + 2}}
+      end
+    end
+  }
+end
