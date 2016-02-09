1e30600bdbbf483a0dbd10da538cddc7d4b1c5bf
Charles Oliver Nutter <headius@headius.com>
2016-02-10 00:32:31 +0100

    Fall back on old JVM logic for system under chdir. Fixes #3653.

---
+++ spec/regression/GH-3653_system_under_chdir_expands_env.rb
@@ -0,0 +1,21 @@
+require 'tmpdir'
+
+describe "Kernel#system when called during a Dir.chdir" do
+  it "does not expand environment variables" do
+    def sh(*cmd)
+      res = system(*cmd)
+      status = $?
+      p [res, status]
+    end
+
+    ENV['GH3653'] = 'someval'
+
+
+    Dir.chdir(Dir.tmpdir) do
+      sh ENV_JAVA['jruby.home'] + '/bin/jruby', File.dirname(__FILE__) + '/GH-3653_check_no_expansion.rb', '$GH3653'
+    end
+
+    expect($?.exitstatus).to eq(0)
+  end
+end
+
