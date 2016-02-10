8694c3cb62dc44c7d59bb457f415c5b68e5b4c77
Chris Seaton <chris.seaton@oracle.com>
2016-02-10 13:30:09 +0000

    [Truffle] Fix copyright year.

--- tool/jt.rb
+++ tool/jt.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-# Copyright (c) 2015 Oracle and/or its affiliates. All rights reserved. This
-# code is released under a tri EPL/GPL/LGPL license. You can use it,
+# Copyright (c) 2015, 2016 Oracle and/or its affiliates. All rights reserved.
+# This code is released under a tri EPL/GPL/LGPL license. You can use it,
 # redistribute it and/or modify it under the terms of the:
 #
 # Eclipse Public License version 1.0
