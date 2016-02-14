34b32e9c786b916471c1077755340129b1d90d56
Chris Seaton <chris.seaton@oracle.com>
2016-02-14 15:32:55 +0000

    [Truffle] Fix coverage script.

--- tool/truffle/java-coverage/coverage.sh
+++ tool/truffle/java-coverage/coverage.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-curl -L -o jacoco-agent.jar 'http://search.maven.org/remotecontent?filepath=org/jacoco/org.jacoco.agent/0.7.4.201502262128/org.jacoco.agent-0.7.4.201502262128-runtime.jar'
-curl -L -O 'http://central.maven.org/maven2/org/ow2/asm/asm-all/5.0.4/asm-all-5.0.4.jar'
-curl -L -O 'http://central.maven.org/maven2/org/jacoco/org.jacoco.core/0.7.4.201502262128/org.jacoco.core-0.7.4.201502262128.jar'
-curl -L -O 'http://central.maven.org/maven2/org/jacoco/org.jacoco.ant/0.7.4.201502262128/org.jacoco.ant-0.7.4.201502262128.jar'
-curl -L -O 'http://central.maven.org/maven2/org/jacoco/org.jacoco.report/0.7.4.201502262128/org.jacoco.report-0.7.4.201502262128.jar'
+curl -Lo jacoco-agent.jar 'http://search.maven.org/remotecontent?filepath=org/jacoco/org.jacoco.agent/0.7.4.201502262128/org.jacoco.agent-0.7.4.201502262128-runtime.jar'
+curl -LO 'http://central.maven.org/maven2/org/ow2/asm/asm-all/5.0.4/asm-all-5.0.4.jar'
+curl -LO 'http://central.maven.org/maven2/org/jacoco/org.jacoco.core/0.7.4.201502262128/org.jacoco.core-0.7.4.201502262128.jar'
+curl -LO 'http://central.maven.org/maven2/org/jacoco/org.jacoco.ant/0.7.4.201502262128/org.jacoco.ant-0.7.4.201502262128.jar'
+curl -LO 'http://central.maven.org/maven2/org/jacoco/org.jacoco.report/0.7.4.201502262128/org.jacoco.report-0.7.4.201502262128.jar'
 
 JACOCO_AGENT=`pwd`/jacoco-agent.jar
 JACOCO_LOG=`pwd`/jacoco.exec
@@ -14,6 +14,6 @@ JACOCO_LOG=`pwd`/jacoco.exec
 export VERIFY_JRUBY=1
 export JAVA_OPTS="-javaagent:$JACOCO_AGENT=destfile=$JACOCO_LOG"
 
-rm jacoco.exec
+rm -f jacoco.exec
 bin/jruby tool/jt.rb test
 ant -f tool/truffle/java-coverage/build.xml
