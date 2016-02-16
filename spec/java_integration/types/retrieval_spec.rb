jruby/jruby

commit e7ea92d7c39e1565c02f681dd6eafc8bcd1001bd

[ji] support retrieving (non-public) Java inner classes similar to Ruby's private constants
fixes #3333

core/src/main/java/org/jruby/java/proxies/JavaProxy.java
@@ -500,6 +500,12 @@ public class JavaProxy extends RubyObject {
 
     public static class ClassMethods {
 
+        // handling non-public inner classes retrieval ... like private constants
+        @JRubyMethod(name = "const_missing", required = 1, meta = true, visibility = Visibility.PRIVATE)
+        public static IRubyObject const_missing(ThreadContext context, IRubyObject self, IRubyObject name) {
+            return Java.get_inner_class(context, (RubyModule) self, name);
+        }
+
         @JRubyMethod(meta = true)
         public static IRubyObject java_method(ThreadContext context, IRubyObject proxyClass, IRubyObject rubyName) {
             String name = rubyName.asJavaString();
@@ -558,7 +564,7 @@ public class JavaProxy extends RubyObject {
                 case 2: return java_send(context, recv, args[0], args[1]);
                 case 3: return java_send(context, recv, args[0], args[1], args[2]);
             }
-            
+
             final Ruby runtime = context.runtime;
 
             String name = args[0].asJavaString();

core/src/main/java/org/jruby/javasupport/Java.java
@@ -1013,6 +1013,33 @@ public class Java implements Library {
 
     }
 
+    private static RubyModule getProxyUnderClass(final ThreadContext context,
+        final RubyModule enclosingClass, final String name) {
+        final Ruby runtime = context.runtime;
+
+        if ( name.length() == 0 ) throw runtime.newArgumentError("empty class name");
+
+        Class<?> enclosing = JavaClass.getJavaClass(context, enclosingClass);
+
+        final String fullName = enclosing.getName() + '$' + name;
+
+        final RubyModule result = getProxyClassOrNull(runtime, fullName);
+        //if ( result != null && cacheMethod ) bindJavaPackageOrClassMethod(enclosingClass, name, result);
+        return result;
+    }
+
+    public static IRubyObject get_inner_class(final ThreadContext context,
+        final RubyModule self, final IRubyObject name) { // const_missing delegate
+        final String constName = name.asJavaString();
+
+        final RubyModule innerClass = getProxyUnderClass(context, self, constName);
+        if ( innerClass == null ) { // NOTE: probably better to just call super
+            final String fullName = self.getName() + "::" + constName;
+            throw context.runtime.newNameErrorObject("uninitialized constant " + fullName, context.runtime.newSymbol(fullName));
+        }
+        return cacheConstant(self, constName, innerClass, true); // hidden == true (private_constant)
+    }
+
     @JRubyMethod(meta = true)
     public static IRubyObject const_missing(final ThreadContext context,
         final IRubyObject self, final IRubyObject name) {
@@ -1021,18 +1048,23 @@ public class Java implements Library {
         // it's fine to not add the "cached" method here - when users sticking to
         // constant access won't pay the "penalty" for adding dynamic methods ...
         final RubyModule packageOrClass = getTopLevelProxyOrPackage(runtime, constName, false);
+        if ( packageOrClass == null ) return context.nil; // TODO compatibility (with packages)
+        return cacheConstant((RubyModule) self, constName, packageOrClass, false);
+    }
+
+    private static RubyModule cacheConstant(final RubyModule owner, // e.g. ::Java
+        final String constName, final RubyModule packageOrClass, final boolean hidden) {
         if ( packageOrClass != null ) {
-            final RubyModule Java = (RubyModule) self;
             // NOTE: if it's a package createPackageModule already set the constant
             // ... but in case it's a (top-level) Java class name we still need to:
-            synchronized (Java) {
-                final IRubyObject alreadySet = Java.fetchConstant(constName);
+            synchronized (owner) {
+                final IRubyObject alreadySet = owner.fetchConstant(constName);
                 if ( alreadySet != null ) return (RubyModule) alreadySet;
-                Java.setConstant(constName, packageOrClass);
+                owner.setConstant(constName, packageOrClass, hidden);
             }
             return packageOrClass;
         }
-        return context.nil; // TODO compatibility - should be throwing instead, right !?
+        return null;
     }
 
     @JRubyMethod(name = "method_missing", meta = true, required = 1)

spec/java_integration/types/retrieval_spec.rb
@@ -125,6 +125,34 @@ describe "A Java class with inner classes" do
     expect( constants ).to_not include :PrivateInner
   end
 
+  it "allows to retrieve non-public inner classes" do
+    # InnerClasses::PackageInner
+    expect( InnerClasses.const_get :PackageInner ).to_not be nil
+    class InnerClasses
+      PACKAGE_INNER = PackageInner
+    end
+    expect( InnerClasses.constants ).to_not include :PackageInner
+    expect( InnerClasses::PACKAGE_INNER ).to eql JavaUtilities.get_proxy_class('java_integration.fixtures.InnerClasses$PackageInner')
+
+    expect( InnerClasses.const_get :PrivateInner ).to_not be nil
+    expect( InnerClasses.const_get :ProtectedInner ).to_not be nil
+
+    class InnerClasses
+      PROTECTED_INNER = ProtectedInner
+    end
+
+    expect( InnerClasses::PROTECTED_INNER.const_get :Nested ).to_not be nil
+
+    expect { InnerClasses::MissingInner }.to raise_error(NameError)
+    begin
+      InnerClasses.const_get :MissingInner
+    rescue NameError => e
+      expect( e.message ).to start_with 'uninitialized constant Java::Java_integrationFixtures::InnerClasses::MissingInner'
+    else
+      fail 'did not raise!'
+    end
+  end
+
   it "raises error importing lower-case names" do
     expect do
       java_import InnerClasses::lowerInnerClass

spec/java_integration/fixtures/InnerClasses.java
@@ -48,6 +48,8 @@ public class InnerClasses {
 
     protected static interface ProtectedInner {
         public static String VALUE = ProtectedInner.class.getName();
+
+        class Nested extends PackageInner {}
     }
 
     static class PackageInner {

commit de96f4f3addb2fe569c31a2f950e7fcbfdee6665

[ji] make sure const_missing is properly delegated on Java proxies (fixes #3669)
... this has regressed since e7ea92d7c39e1565c02f681dd6eafc8bcd1001bd

core/src/main/java/org/jruby/java/proxies/JavaProxy.java
@@ -501,7 +501,7 @@ public class JavaProxy extends RubyObject {
     public static class ClassMethods {
 
         // handling non-public inner classes retrieval ... like private constants
-        @JRubyMethod(name = "const_missing", required = 1, meta = true, visibility = Visibility.PRIVATE)
+        @JRubyMethod(name = "const_missing", required = 1, meta = true, visibility = Visibility.PRIVATE, frame = true)
         public static IRubyObject const_missing(ThreadContext context, IRubyObject self, IRubyObject name) {
             return Java.get_inner_class(context, (RubyModule) self, name);
         }

core/src/main/java/org/jruby/javasupport/Java.java
@@ -1074,9 +1074,8 @@ public class Java implements Library {
         final String constName = name.asJavaString();
 
         final RubyModule innerClass = getProxyUnderClass(context, self, constName);
-        if ( innerClass == null ) { // NOTE: probably better to just call super
-            final String fullName = self.getName() + "::" + constName;
-            throw context.runtime.newNameErrorObject("uninitialized constant " + fullName, context.runtime.newSymbol(fullName));
+        if ( innerClass == null ) {
+            return Helpers.invokeSuper(context, self, name, Block.NULL_BLOCK);
         }
         return cacheConstant(self, constName, innerClass, true); // hidden == true (private_constant)
     }

spec/java_integration/types/retrieval_spec.rb
@@ -153,6 +153,31 @@ describe "A Java class with inner classes" do
     end
   end
 
+  it "delegates const_missing" do # crucial for ActiveSupport::Dependencies
+    const_missing = Module.instance_method(:const_missing)
+    begin
+      Module.module_eval do
+        remove_method(:const_missing)
+        def const_missing(name)
+          @_const_missing_names ||= []
+          @_const_missing_names << name
+        end
+      end
+
+      InnerClasses::MissingInner
+      InnerClasses::AnotherMissingInner
+      InnerClasses::MissingInner
+
+      missing_names = InnerClasses.instance_variable_get(:@_const_missing_names)
+      expect( missing_names ).to eql [ :MissingInner, :AnotherMissingInner, :MissingInner ]
+
+    ensure
+      Module.module_eval do
+        define_method :const_missing, const_missing
+      end
+    end
+  end
+
   it "raises error importing lower-case names" do
     expect do
       java_import InnerClasses::lowerInnerClass