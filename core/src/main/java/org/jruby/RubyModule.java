c460290796ceb0bc05abb377d4529b716251f780
kares <self@kares.org>
2016-02-16 13:23:19 +0100

    let setConstantCommon do all the constant storing work when autoload can't

--- core/src/main/java/org/jruby/RubyModule.java
+++ core/src/main/java/org/jruby/RubyModule.java
@@ -3650,10 +3650,9 @@ public class RubyModule extends RubyObject {
     private IRubyObject setConstantCommon(String name, IRubyObject value, boolean hidden, boolean warn) {
         IRubyObject oldValue = fetchConstant(name);
         if (oldValue != null) {
-            if (oldValue == UNDEF) {
-                setAutoloadConstant(name, value);
-            } else {
-                if (warn) {
+            boolean notAutoload = oldValue != UNDEF;
+            if (notAutoload || !setAutoloadConstant(name, value)) {
+                if (warn && notAutoload) {
                     getRuntime().getWarnings().warn(ID.CONSTANT_ALREADY_INITIALIZED, "already initialized constant " + name);
                 }
                 setParentForModule(name, value);
@@ -4170,17 +4169,14 @@ public class RubyModule extends RubyObject {
     /**
      * Set an Object as a defined constant in autoloading.
      */
-    private void setAutoloadConstant(String name, IRubyObject value) {
+    private boolean setAutoloadConstant(String name, IRubyObject value) {
         final Autoload autoload = getAutoloadMap().get(name);
         if ( autoload != null ) {
-            if ( ! autoload.setConstant(getRuntime().getCurrentContext(), value) ) {
-                storeConstant(name, value);
-                removeAutoload(name);
-            }
+            boolean set = autoload.setConstant(getRuntime().getCurrentContext(), value);
+            if ( ! set ) removeAutoload(name);
+            return set;
         }
-        else {
-            storeConstant(name, value);
-        }
+        return false;
     }
 
     /**
