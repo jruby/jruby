9a89810a3de5290c498ec0f8b66c374bcf0be279
Chris Seaton <chris.seaton@oracle.com>
2016-02-18 22:31:31 +0000

    [Truffle] Tidy up the elidable result node.

--- truffle/src/main/java/org/jruby/truffle/language/control/ElidableResultNode.java
+++ truffle/src/main/java/org/jruby/truffle/language/control/ElidableResultNode.java
@@ -18,11 +18,8 @@ import org.jruby.truffle.RubyContext;
 import org.jruby.truffle.language.RubyNode;
 
 /**
- * This node has a pair of children - one required and one elidable result. The required node is
- * always executed, but its result is discarded. Therefore it should perform some useful side
- * effects. The elidable node is executed, and its result value returned, if an execute method with
- * a non-void type is used. It is not executed at all if a void typed execute method is used.
- * Therefore it should not perform any observable side effects.
+ * This node has a pair of children. One has side effects and the other returns the
+ * result. If the result isn't needed all we execute is the side effects.
  */
 @NodeInfo(cost = NodeCost.NONE)
 public class ElidableResultNode extends RubyNode {
@@ -34,18 +31,6 @@ public class ElidableResultNode extends RubyNode {
         super(context, sourceSection);
         this.required = required;
         this.elidableResult = elidableResult;
-    }
-
-    @Override
-    public int executeInteger(VirtualFrame frame) throws UnexpectedResultException {
-        required.executeVoid(frame);
-        return elidableResult.executeInteger(frame);
-    }
-
-    @Override
-    public double executeDouble(VirtualFrame frame) throws UnexpectedResultException {
-        required.executeVoid(frame);
-        return elidableResult.executeDouble(frame);
     }
 
     @Override
