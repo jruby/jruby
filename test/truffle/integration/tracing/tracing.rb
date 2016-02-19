aa62a6245d760c3ae61d72434c66cfda26b223a2
Chris Seaton <chris.seaton@oracle.com>
2016-02-19 20:56:30 +0000

    [Truffle] Use .inspect to print failures in tracing test.

--- test/truffle/integration/tracing/tracing.rb
+++ test/truffle/integration/tracing/tracing.rb
@@ -38,34 +38,34 @@ def check(file)
   
   expected.zip(actual).each do |e, a|
     unless a[0] == e[0]
-      puts "Expected #{e[0]}, actually #{a[0]}"
+      puts "Expected #{e[0].inspect}, actually #{a[0].inspect}"
       success = false
     end
     
     unless a[1].end_with?(e[1])
-      puts "Expected #{e[1]}, actually #{a[1]}"
+      puts "Expected #{e[1].inspect}, actually #{a[1].inspect}"
       success = false
     end
   
     unless a[2] == e[2]
-      puts "Expected #{e[2]}, actually #{a[2]}"
+      puts "Expected #{e[2].inspect}, actually #{a[2].inspect}"
       success = false
     end
   
     unless a[3] == e[3]
-      puts "Expected #{e[3]}, actually #{a[3]}"
+      puts "Expected #{e[3].inspect}, actually #{a[3].inspect}"
       success = false
     end
     
     ab = Hash[a[4].local_variables.sort.map { |v| [v, a[4].local_variable_get(v)] }]
   
     unless ab == e[4]
-      puts "Expected Binding, actually #{ab}"
+      puts "Expected Binding, actually #{ab.inspect}"
       success = false
     end
   
     unless a[5] == e[5]
-      puts "Expected #{e[5]}, actually #{a[5]}"
+      puts "Expected #{e[5].inspect}, actually #{a[5].inspect}"
       success = false
     end
   end
