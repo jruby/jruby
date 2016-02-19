14ae9cd4be13e1b54474e7b916af6979f40dfc0c
Charles Oliver Nutter <headius@headius.com>
2016-02-19 10:20:00 -0600

    We always convert path to String, so we lose encoding for error.

--- test/mri/excludes/TestRequire.rb
+++ test/mri/excludes/TestRequire.rb
@@ -1,5 +1,6 @@
 exclude :test_define_class, "an MRI inconsistency - NameError when class BasicSocket already defined!"
 exclude :test_frozen_loaded_features, "error message does not match"
+exclude :test_load_ospath, "we lose encoding on the way into load, can't preserve for LoadError message"
 exclude :test_load2, "needs investigation"
 exclude :test_loading_fifo_threading_raise, "we do not use IO to read files and so will not show as stopped while loading"
 exclude :test_loading_fifo_threading_success, "we do not use IO to read files and so will not show as stopped while loading"
