280fcb02d64d62cd5ce67c400e35d634615c12bf
Charles Oliver Nutter <headius@headius.com>
2016-02-20 16:59:17 -0600

    Exclude some socket-related tests we'll fix on socket branch.

--- test/mri/excludes/TestSocket_BasicSocket.rb
+++ test/mri/excludes/TestSocket_BasicSocket.rb
@@ -1,3 +1,4 @@
+exclude :test_for_fd, "needs investigation"
 exclude :test_getsockopt, "needs investigation"
 exclude :test_setsockopt, "needs investigation"
 exclude :test_listen, "needs investigation"
