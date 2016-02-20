1cb6e4c7661687f1cc14fd3c5998e60462a9e76c
Charles Oliver Nutter <headius@headius.com>
2016-02-20 11:19:46 -0600

    Exclude this since all our exceptions get transcoded thru String.

--- test/mri/excludes/TestException.rb
+++ test/mri/excludes/TestException.rb
@@ -9,5 +9,6 @@ exclude :test_errinfo_encoding_in_debug, "parser issue with Japanese encodings (
 exclude /test_machine_stackoverflow/, reason
 exclude :test_machine_stackoverflow_by_define_method, reason
 exclude :test_name_error_local_variables, "NameError#local_variables is internal and specific to MRI"
+exclude :test_output_string_encoding, "Exception messages always go through Java String for us"
 exclude :test_stackoverflow, reason
 exclude :test_too_many_args_in_eval, "MRI raises SystemStackError for huge number of args, for some reason"
