3402af69af286b16592ed9f8b73d0b582828e925
Charles Oliver Nutter <headius@headius.com>
2016-02-22 14:36:21 -0600

    Fix from https://bugs.ruby-lang.org/issues/11486 did not work.

--- test/mri/excludes/TestM17NComb.rb
+++ test/mri/excludes/TestM17NComb.rb
@@ -1,3 +1,4 @@
+exclude :test_bug11486, "fix from https://bugs.ruby-lang.org/issues/11486 did not appear to fix it for us"
 exclude :test_str_intern, "needs investigation"
 exclude :test_str_count, "does not raise compatibility error"
 exclude :test_str_crypt_nonstrict, "#crypt failing: Errno::EINVAL: Invalid argument"
