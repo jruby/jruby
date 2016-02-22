d8ab6ebd7a0f3e6ddb2e6f6f001ffcc6c124ee66
kares <self@kares.org>
2016-02-22 18:41:39 +0100

    invent a getInterpreterFrame for class and method name in one call to FrameType

--- core/src/main/java/org/jruby/runtime/backtrace/BacktraceData.java
+++ core/src/main/java/org/jruby/runtime/backtrace/BacktraceData.java
@@ -31,13 +31,14 @@ public class BacktraceData implements Serializable {
             false,
             false);
 
-    public RubyStackTraceElement[] getBacktrace(Ruby runtime) {
+    public final RubyStackTraceElement[] getBacktrace(Ruby runtime) {
         if (backtraceElements == null) {
             backtraceElements = constructBacktrace(runtime.getBoundMethods());
         }
         return backtraceElements;
     }
 
+    @SuppressWarnings("unchecked")
     public RubyStackTraceElement[] getBacktraceWithoutRuby() {
         return constructBacktrace(Collections.EMPTY_MAP);
     }
@@ -109,23 +110,23 @@ public class BacktraceData implements Serializable {
             }
 
             // Interpreted frames
-            if ( rubyFrameIndex >= 0 && FrameType.isInterpreterFrame(className, methodName) ) {
+            final FrameType frameType;
+            if ( rubyFrameIndex >= 0 && (frameType = FrameType.getInterpreterFrame(className, methodName)) != null ) {
 
                 // pop interpreter frame
                 BacktraceElement rubyFrame = rubyTrace[rubyFrameIndex--];
 
-                FrameType frameType = FrameType.getInterpreterFrame(methodName);
-
                 // construct Ruby trace element
-                String newName = rubyFrame.method;
+                final String newName;
                 switch (frameType) {
                     case METHOD: newName = rubyFrame.method; break;
                     case BLOCK: newName = "block in " + rubyFrame.method; break;
-                    case CLASS: newName = "<class:" + rubyFrame.method + ">"; break;
-                    case MODULE: newName = "<module:" + rubyFrame.method + ">"; break;
+                    case CLASS: newName = "<class:" + rubyFrame.method + '>'; break;
+                    case MODULE: newName = "<module:" + rubyFrame.method + '>'; break;
                     case METACLASS: newName = "singleton class"; break;
                     case ROOT: newName = "<top>"; break;
                     case EVAL: newName = "<eval>"; break;
+                    default: newName = rubyFrame.method;
                 }
                 RubyStackTraceElement rubyElement = new RubyStackTraceElement("RUBY", newName, rubyFrame.filename, rubyFrame.line + 1, false);
 
@@ -151,10 +152,7 @@ public class BacktraceData implements Serializable {
 
     public static String getBoundMethodName(Map<String,Map<String,String>> boundMethods, String className, String methodName) {
         Map<String, String> javaToRuby = boundMethods.get(className);
-
-        if (javaToRuby == null) return null;
-
-        return javaToRuby.get(methodName);
+        return javaToRuby == null ? null : javaToRuby.get(methodName);
     }
 
     private static String packagedFilenameFromElement(final String filename, final String className) {
