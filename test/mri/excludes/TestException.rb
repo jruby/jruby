a6ba916ae1dfbf3240aefe432346ba06525ee69c
Charles Oliver Nutter <headius@headius.com>
2016-03-01 17:03:55 -0600

    Exclude test of Exception message encoding.

--- test/mri/excludes/TestException.rb
+++ test/mri/excludes/TestException.rb
@@ -8,6 +8,7 @@ recover from such situation.
 exclude :test_errinfo_encoding_in_debug, "parser issue with Japanese encodings (https://github.com/jruby/jruby/issues/3679)"
 exclude /test_machine_stackoverflow/, reason
 exclude :test_machine_stackoverflow_by_define_method, reason
+exclude :test_multibyte_and_newline, "Exception messages always go through Java String for us"
 exclude :test_name_error_local_variables, "NameError#local_variables is internal and specific to MRI"
 exclude :test_output_string_encoding, "Exception messages always go through Java String for us"
 exclude :test_stackoverflow, reason
