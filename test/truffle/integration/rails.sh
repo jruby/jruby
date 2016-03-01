ccec8a8df027be550c3452c2ff6c182292a9a987
Petr Chalupa <git@pitr.ch>
2016-03-01 14:45:49 +0100

    [Truffle] better output of test integration rails

--- test/truffle/integration/rails.sh
+++ test/truffle/integration/rails.sh
@@ -15,7 +15,7 @@ $JTR run -r rubygems -- bin/rails server &
 serverpid=$!
 url=http://localhost:3000/people.json
 
-while ! curl $url 2>/dev/null >/dev/null;
+while ! curl -s $url;
 do
   echo -n .
   sleep 1
@@ -23,9 +23,11 @@ done
 
 echo Server is up
 
-test "$(curl $url 2>/dev/null)" = '[{"name":"John Doe","email":"jd@example.com"}]'
-curl --data 'name=Anybody&email=ab@example.com' $url
-test "$(curl $url 2>/dev/null)" = '[{"name":"John Doe","email":"jd@example.com"},{"name":"Anybody","email":"ab@example.com"}]'
+set -x
+
+test "$(curl -s $url)" = '[{"name":"John Doe","email":"jd@example.com"}]'
+curl -s --data 'name=Anybody&email=ab@example.com' $url
+test "$(curl -s $url)" = '[{"name":"John Doe","email":"jd@example.com"},{"name":"Anybody","email":"ab@example.com"}]'
 
 kill %1
 kill $(cat tmp/pids/server.pid)
