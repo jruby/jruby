b5e0310bd6c46b76b2ad296f2928882bd4b0caae
Thomas E. Enebo <tom.enebo@gmail.com>
2016-03-03 13:15:44 -0600

    Fix bad test which forgets tempfile opens as :w which caused crlf creation

--- test/externals/ruby1.8/logger/test_logger.rb
+++ test/externals/ruby1.8/logger/test_logger.rb
@@ -50,6 +50,7 @@ class TestLogger < Test::Unit::TestCase
     logdev.open
     msg = logdev.read
     logdev.close
+    msg.gsub!(/\r\n/, "\n") # tempfile writes with crlf logic on...fix for tests
     msg
   end
 
