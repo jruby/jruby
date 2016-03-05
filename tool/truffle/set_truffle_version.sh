85b0a219d9a4acd945772e82884935dcb4a09b1b
Chris Seaton <chris.seaton@oracle.com>
2016-03-05 21:27:18 +0000

    [Truffle] sed that works on Linux.

--- tool/truffle/set_truffle_version.sh
+++ tool/truffle/set_truffle_version.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sed -i '.orig' "s/  truffle_version = '.*'/  truffle_version = '$1'/g" truffle/pom.rb
+sed -i.orig "s/  truffle_version = '.*'/  truffle_version = '$1'/g" truffle/pom.rb
