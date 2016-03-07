790a3f3dd5be788fa9e79e6bd6e63d59b728875e
Petr Chalupa <git@pitr.ch>
2016-03-07 13:47:39 +0100

    [Truffle] j+tr: Updates
    -   make option blocks avaliable in ci definition
    -   move --interpreter-path option to run
    -   ci command runs setup and run commands within the same process
        (ensures that it will execute on the same Ruby interpreter)
    -   ci definition can define its own options (see default)
    -   remove unused Ruby evaluation in Yaml
    -   ci command accepts --definition NAME option to use different ci defintion
        than default (or with different name than the one of the tested gem)
    -   default ci-definition takes option --version
    -   bundler helper method added to CIEnvironment
    -   bundler has to run it's own process (uses same ruby executable as the tool)

--- lib/ruby/truffle/jruby+truffle/gem_ci/activesupport.rb
+++ lib/ruby/truffle/jruby+truffle/gem_ci/activesupport.rb
@@ -5,7 +5,9 @@ unless File.exists? repository_dir
   git_clone 'https://github.com/rails/rails.git', branch: '4-2-stable'
 end
 
-setup
+use_only_https_git_paths!
+
+has_to_succeed setup
 
 result run(%w[--require-pattern test/**/*_test.rb -r exclude_tests -- -I test -e nil], raise: false)
 
--- lib/ruby/truffle/jruby+truffle/gem_ci/default.rb
+++ lib/ruby/truffle/jruby+truffle/gem_ci/default.rb
@@ -1,8 +1,15 @@
+declare_options git:     ['--git URL', 'Path to the gem\'s repository', STORE_NEW_VALUE, nil],
+                version: ['--version VERSION', 'Version of the gem', STORE_NEW_VALUE, nil]
+
 unless File.exists? repository_dir
-  git_clone option(:git)
+  git_clone option(:git),
+            tag: get_git_tag(option(:version))
 end
 
-setup
+delete_gemfile_lock!
+use_only_https_git_paths!
+
+has_to_succeed setup
 
 result run(%w[-S rake], raise: false)
 
