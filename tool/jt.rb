9d3d6dd04d7ffeef5db020a733d635a18e3114cc
Chris Seaton <chris.seaton@oracle.com>
2016-03-14 18:41:21 +0000

    [Truffle] Silence Graal warning when running tests.

--- tool/jt.rb
+++ tool/jt.rb
@@ -447,6 +447,7 @@ module Commands
 
   def test_compiler(*args)
     env_vars = {}
+    env_vars["JRUBY_OPTS"] = '-Xtruffle.graal.warn_unless=false'
     env_vars["JAVACMD"] = Utilities.find_graal unless args.delete('--no-java-cmd')
     env_vars["PATH"] = "#{Utilities.find_jruby_bin_dir}:#{ENV["PATH"]}"
     Dir["#{JRUBY_DIR}/test/truffle/compiler/*.sh"].each do |test_script|
@@ -459,6 +460,7 @@ module Commands
     no_gems = args.delete('--no-gems')
     fast = args.delete('fast')
     env_vars = {}
+    env_vars["JRUBY_OPTS"] = '-Xtruffle.graal.warn_unless=false'
     env_vars["PATH"] = "#{Utilities.find_jruby_bin_dir}:#{ENV["PATH"]}"
 
     test_names = if args.empty?
