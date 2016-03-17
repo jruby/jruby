13156e1e15cf9944503fcb44f721c83fc3ce21da
Chris Seaton <chris.seaton@oracle.com>
2016-03-17 18:01:54 +0000

    [Truffle] Pull all the nested nodes out of InteropNode.

---
+++ truffle/src/main/java/org/jruby/truffle/interop/InteropIsExecutable.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2013, 2016 Oracle and/or its affiliates. All rights reserved. This
+ * code is released under a tri EPL/GPL/LGPL license. You can use it,
+ * redistribute it and/or modify it under the terms of the:
+ *
+ * Eclipse Public License version 1.0
+ * GNU General Public License version 2
+ * GNU Lesser General Public License version 2.1
+ */
+package org.jruby.truffle.interop;
+
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.interop.ForeignAccess;
+import com.oracle.truffle.api.source.SourceSection;
+import org.jruby.truffle.RubyContext;
+import org.jruby.truffle.language.RubyGuards;
+
+class InteropIsExecutable extends InteropNode {
+    public InteropIsExecutable(RubyContext context, SourceSection sourceSection) {
+        super(context, sourceSection);
+    }
+
+    @Override
+    public Object execute(VirtualFrame frame) {
+        final Object receiver = ForeignAccess.getReceiver(frame);
+        return RubyGuards.isRubyMethod(receiver) || RubyGuards.isRubyProc(receiver);
+    }
+
+}
