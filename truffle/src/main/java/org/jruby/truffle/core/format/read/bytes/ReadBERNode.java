c9d9eb2a193b14a0c6590600fb2fd56c80dc05fa
Chris Seaton <chris.seaton@oracle.com>
2016-03-30 14:17:25 +0100

    [Truffle] Include full JRuby licence in ReadBERNode.

--- truffle/src/main/java/org/jruby/truffle/core/format/read/bytes/ReadBERNode.java
+++ truffle/src/main/java/org/jruby/truffle/core/format/read/bytes/ReadBERNode.java
@@ -6,6 +6,42 @@
  * Eclipse Public License version 1.0
  * GNU General Public License version 2
  * GNU Lesser General Public License version 2.1
+ *
+ * Some logic copied from jruby.util.Pack
+ *
+ *  * Version: EPL 1.0/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Eclipse Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * Copyright (C) 2002-2004 Jan Arne Petersen <jpetersen@uni-bonn.de>
+ * Copyright (C) 2002-2004 Anders Bengtsson <ndrsbngtssn@yahoo.se>
+ * Copyright (C) 2003-2004 Thomas E Enebo <enebo@acm.org>
+ * Copyright (C) 2004 Charles O Nutter <headius@headius.com>
+ * Copyright (C) 2004 Stefan Matthias Aust <sma@3plus4.de>
+ * Copyright (C) 2005 Derek Berner <derek.berner@state.nm.us>
+ * Copyright (C) 2006 Evan Buswell <ebuswell@gmail.com>
+ * Copyright (C) 2007 Nick Sieger <nicksieger@gmail.com>
+ * Copyright (C) 2009 Joseph LaFata <joe@quibb.org>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the EPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the EPL, the GPL or the LGPL.
  */
 package org.jruby.truffle.core.format.read.bytes;
 
@@ -41,47 +77,6 @@ public abstract class ReadBERNode extends FormatNode {
     @Specialization
     protected Object encode(VirtualFrame frame, byte[] source) {
         CompilerDirectives.transferToInterpreter();
-
-        // TODO CS 28-Dec-15 should write our own optimizable version of BER
-
-        /*
-         * Copied from JRuby's Pack class.
-         *
-         * **** BEGIN LICENSE BLOCK *****
-         * Version: EPL 1.0/GPL 2.0/LGPL 2.1
-         *
-         * The contents of this file are subject to the Eclipse Public
-         * License Version 1.0 (the "License"); you may not use this file
-         * except in compliance with the License. You may obtain a copy of
-         * the License at http://www.eclipse.org/legal/epl-v10.html
-         *
-         * Software distributed under the License is distributed on an "AS
-         * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-         * implied. See the License for the specific language governing
-         * rights and limitations under the License.
-         *
-         * Copyright (C) 2002-2004 Jan Arne Petersen <jpetersen@uni-bonn.de>
-         * Copyright (C) 2002-2004 Anders Bengtsson <ndrsbngtssn@yahoo.se>
-         * Copyright (C) 2003-2004 Thomas E Enebo <enebo@acm.org>
-         * Copyright (C) 2004 Charles O Nutter <headius@headius.com>
-         * Copyright (C) 2004 Stefan Matthias Aust <sma@3plus4.de>
-         * Copyright (C) 2005 Derek Berner <derek.berner@state.nm.us>
-         * Copyright (C) 2006 Evan Buswell <ebuswell@gmail.com>
-         * Copyright (C) 2007 Nick Sieger <nicksieger@gmail.com>
-         * Copyright (C) 2009 Joseph LaFata <joe@quibb.org>
-         *
-         * Alternatively, the contents of this file may be used under the terms of
-         * either of the GNU General Public License Version 2 or later (the "GPL"),
-         * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-         * in which case the provisions of the GPL or the LGPL are applicable instead
-         * of those above. If you wish to allow use of your version of this file only
-         * under the terms of either the GPL or the LGPL, and not to allow others to
-         * use your version of this file under the terms of the EPL, indicate your
-         * decision by deleting the provisions above and replace them with the notice
-         * and other provisions required by the GPL or the LGPL. If you do not delete
-         * the provisions above, a recipient may use your version of this file under
-         * the terms of any one of the EPL, the GPL or the LGPL.
-         ***** END LICENSE BLOCK *****/
 
         final ByteBuffer encode = ByteBuffer.wrap(source, getSourcePosition(frame), getSourceLength(frame) - getSourcePosition(frame));
 
