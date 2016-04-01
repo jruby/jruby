1b2ef96288652e47daafafe2f0fa9e2fb29737d8
Chris Seaton <chris.seaton@oracle.com>
2016-04-01 23:22:25 +0100

    [Truffle] Don't need specialisations to get a TypeError for a bad parameter as that's the error you get for a missing specialisation.

--- truffle/src/main/java/org/jruby/truffle/core/array/ArrayNodes.java
+++ truffle/src/main/java/org/jruby/truffle/core/array/ArrayNodes.java
@@ -2485,26 +2485,6 @@ public abstract class ArrayNodes {
             return string;
         }
 
-        @Specialization
-        public Object pack(DynamicObject array, boolean format) {
-            return ruby("raise TypeError");
-        }
-
-        @Specialization
-        public Object pack(DynamicObject array, int format) {
-            return ruby("raise TypeError");
-        }
-
-        @Specialization
-        public Object pack(DynamicObject array, long format) {
-            return ruby("raise TypeError");
-        }
-
-        @Specialization(guards = "isNil(format)")
-        public Object packNil(DynamicObject array, Object format) {
-            return ruby("raise TypeError");
-        }
-
         @Specialization(guards = {"!isRubyString(format)", "!isBoolean(format)", "!isInteger(format)", "!isLong(format)", "!isNil(format)"})
         public Object pack(DynamicObject array, Object format) {
             return ruby("pack(format.to_str)", "format", format);
