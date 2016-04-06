876eab61a31f9172f7781cc2b17f593e9ccae513
Chris Seaton <chris.seaton@oracle.com>
2016-04-06 22:32:47 +0100

    [Truffle] Add some more unsafe test examples.

--- test/truffle/integration/safe.sh
+++ test/truffle/integration/safe.sh
@@ -24,15 +24,17 @@ safe -e "Truffle::Primitive.safe_puts 'hello, world'"
 
 unsafe -Xtruffle.platform.safe_puts=false -e "Truffle::Primitive.safe_puts 'hello, world'"
 
-# Check default unsafe operations
+# Try some unsafe operations
 
 unsafe -e "puts 'hello, world'"
 unsafe -e '`echo foo`'
 unsafe -e 'exit!'
+unsafe -e 'Rubinius::FFI::Pointer.new(1).read_int'
+unsafe -e "File.open('bad.txt')"
 
 # Check we can enable some unsafe operations if we want to
 
-safe -Xtruffle.platform.safe.exit=true -e 'exit'
+safe -Xtruffle.platform.safe.exit=true -e 'exit!'
 
 # Check that safe_puts sanitises correctly
 
