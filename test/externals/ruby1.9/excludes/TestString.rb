4b8fb1cb2e20865d1503e4abb6efd634d64301c0
Thomas E. Enebo <tom.enebo@gmail.com>
2016-04-07 16:09:25 -0500

    Get rid of some windows test noise

--- test/externals/ruby1.9/excludes/TestString.rb
+++ test/externals/ruby1.9/excludes/TestString.rb
@@ -5,7 +5,7 @@ exclude :test_dummy_inspect, "needs investigation"
 exclude :test_gsub_enumerator, "needs investigation"
 exclude :test_hash_random, "needs investigation"
 exclude :test_partition, "needs investigation"
-exclude :test_hash_random, "JRuby #3275" if windows
+exclude :test_hash_random, "JRuby #3275" if !windows
 exclude :test_rpartition, "needs investigation"
 exclude :test_rstrip, "needs investigation"
 exclude :test_split, "needs investigation"
