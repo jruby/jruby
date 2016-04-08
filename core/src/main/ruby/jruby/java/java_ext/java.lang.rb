0fbb4953a83583f593e47f7efabe3e5aaebb0a10
kares <self@kares.org>
2016-04-08 10:19:11 +0200

    re-arrange **java.lang.rb** to be more Ruby-sh ... namespaced inside `java::lang`

--- core/src/main/ruby/jruby/java/java_ext/java.lang.rb
+++ core/src/main/ruby/jruby/java/java_ext/java.lang.rb
@@ -1,199 +1,219 @@
-lang = java::lang
-
-module lang::Runnable
-  def to_proc
-    proc { self.run }
-  end
-end
-
-module lang::Iterable
-  include Enumerable
-
-  def each
-    iter = iterator
-    yield(iter.next) while iter.hasNext
-  end
-
-  def each_with_index
-    index = 0
-    iter = iterator
-    while iter.hasNext
-      yield(iter.next, index)
-      index += 1
-    end
-  end
-end
-
-module lang::Comparable
-  include Comparable
-  def <=>(a)
-    return nil if a.nil?
-    compareTo(a)
-  end
-end
-
-class lang::Throwable
-  def backtrace
-    stack_trace.map(&:to_s)
-  end
-
-  def set_backtrace(trace)
-    # ignored; Java exceptions can't be set to Ruby trace
-    trace
-  end
-
-  def message
-    msg = getLocalizedMessage
-    msg ? msg : ""
-  end
-
-  def to_s
-    message
-  end
-
-  def to_str
-    to_s
-  end
-
-  def inspect
-    to_string
-  end
-
-  class << self
-    alias :old_eqq :===
-    def ===(rhs)
-      if (NativeException == rhs.class) && (java_class.assignable_from?(rhs.cause.java_class))
-        true
-      else
-        old_eqq(rhs)
-      end
-    end
-  end
-end
-
+# @private internal helper
 module JavaUtilities::ModifierShortcuts
-  java_import java.lang.reflect.Modifier
-  
+  # @private
+  Modifier = java.lang.reflect.Modifier
+
   def public?
     Modifier.is_public(modifiers)
   end
-  
+
   def protected?
     Modifier.is_protected(modifiers)
   end
-  
+
   def private?
     Modifier.is_private(modifiers)
   end
-  
+
   def final?
     Modifier.is_final(modifiers)
   end
-  
+
   def static?
     Modifier.is_static(modifiers)
   end
 end
 
-class lang::ClassLoader
-  alias resource_as_stream get_resource_as_stream
-  alias resource_as_url get_resource
-  
-  def resource_as_string(name)
-    resource_as_stream(name).to_io.read
+java::lang.module_eval do
+
+  # `java.lang.Runnable` instances allow for a `to_proc` conversion.
+  module Runnable
+    def to_proc
+      proc { self.run }
+    end
   end
+
+  # A `java.lang.Iterable` will act like a Ruby `Enumerable`.
+  module Iterable
+    include ::Enumerable
+
+    def each
+      iter = iterator
+      yield(iter.next) while iter.hasNext
+    end
+
+    def each_with_index
+      index = 0
+      iter = iterator
+      while iter.hasNext
+        yield(iter.next, index)
+        index += 1
+      end
+    end
+  end
+
+  # `java.lang.Comparable` mixes in Ruby's `Comparable` support.
+  module Comparable
+    include ::Comparable
+    def <=>(a)
+      return nil if a.nil?
+      compareTo(a)
+    end
+  end
+
+  class Throwable
+    def backtrace
+      stack_trace.map(&:to_s)
+    end
+
+    def set_backtrace(trace)
+      # ignored; Java exceptions can't be set to Ruby trace
+      trace
+    end
+
+    def message
+      msg = getLocalizedMessage
+      msg ? msg : ""
+    end
+
+    def to_s
+      message
+    end
+
+    def to_str
+      to_s
+    end
+
+    def inspect
+      to_string
+    end
+
+    class << self
+      alias :old_eqq :===
+      def ===(rhs)
+        if (NativeException == rhs.class) && (java_class.assignable_from?(rhs.cause.java_class))
+          true
+        else
+          old_eqq(rhs)
+        end
+      end
+    end
+  end
+
+  class Character
+    java_alias :isJavaIdentifierStart_char, :isJavaIdentifierStart, [Java::char]
+    java_alias :isJavaIdentifierPart_char, :isJavaIdentifierPart, [Java::char]
+
+    def self.java_identifier_start?(fixnum)
+      isJavaIdentifierStart_char(fixnum);
+    end
+
+    def self.java_identifier_part?(fixnum)
+      isJavaIdentifierPart_char(fixnum);
+    end
+  end
+
+  class Class
+    include ::Comparable
+    include ::JavaUtilities::ModifierShortcuts
+
+    def ruby_class
+      ::JRuby.runtime.java_support.get_proxy_class_from_cache(self)
+    end
+
+    alias to_s name
+
+    def inspect
+      "class #{name}"
+    end
+
+    def resource_as_string(name)
+      resource_as_stream(name).to_io.read
+    end
+
+    alias annotation get_annotation
+
+    def annotations?
+      !annotations.empty?
+    end
+
+    def declared_annotations?
+      !declared_annotations.empty?
+    end
+
+    alias annotation_present? is_annotation_present
+
+    def <=>(other)
+      return nil unless other.class == java::lang::Class
+
+      return 0 if self == other
+      return 1 if self.is_assignable_from(other)
+      return -1 if other.is_assignable_from(self)
+    end
+
+    def java_instance_methods
+      methods.select {|m| !Modifier.is_static(m.modifiers)}.freeze
+    end
+
+    def declared_instance_methods
+      declared_methods.select {|m| !Modifier.is_static(m.modifiers)}.freeze
+    end
+
+    def java_class_methods
+      methods.select {|m| Modifier.is_static(m.modifiers)}.freeze
+    end
+
+    def declared_class_methods
+      declared_methods.select {|m| Modifier.is_static(m.modifiers)}.freeze
+    end
+  end
+
+  class ClassLoader
+    alias resource_as_stream get_resource_as_stream
+    alias resource_as_url get_resource
+
+    def resource_as_string(name)
+      resource_as_stream(name).to_io.read
+    end
+  end
+
 end
 
-class lang::Class
-  include Comparable
-  include JavaUtilities::ModifierShortcuts
+module Java::JavaLangReflect
+  class AccessibleObject
+    include ::JavaUtilities::ModifierShortcuts
 
-  def ruby_class
-    JRuby.runtime.java_support.get_proxy_class_from_cache(self)
+    alias inspect to_s
   end
-  
-  alias to_s name
-  
-  def inspect
-    "class #{name}"
-  end
-  
-  def resource_as_string(name)
-    resource_as_stream(name).to_io.read
-  end
-  
-  alias annotation get_annotation
-  
-  def annotations?
-    !annotations.empty?
-  end
-  
-  def declared_annotations?
-    !declared_annotations.empty?
-  end
-  
-  alias annotation_present? is_annotation_present
-  
-  def <=>(other)
-    return nil unless other.class == java::lang::Class
-    
-    return 0 if self == other
-    return 1 if self.is_assignable_from(other)
-    return -1 if other.is_assignable_from(self)
-  end
-  
-  def java_instance_methods
-    methods.select {|m| !Modifier.is_static(m.modifiers)}.freeze
-  end
-  
-  def declared_instance_methods
-    declared_methods.select {|m| !Modifier.is_static(m.modifiers)}.freeze
-  end
-  
-  def java_class_methods
-    methods.select {|m| Modifier.is_static(m.modifiers)}.freeze
-  end
-  
-  def declared_class_methods
-    declared_methods.select {|m| Modifier.is_static(m.modifiers)}.freeze
-  end
-end
 
-reflect = lang::reflect
+  class Constructor
+    def return_type
+      nil
+    end
 
-class reflect::AccessibleObject
-  include JavaUtilities::ModifierShortcuts
-  
-  alias inspect to_s
-end
-
-class reflect::Constructor
-  def return_type
-    nil
+    alias argument_types parameter_types
   end
-  
-  alias argument_types parameter_types
-end
 
-class reflect::Method
-  def invoke_static(*args)
-    invoke(nil, *args)
-  end
-  
-  alias argument_types parameter_types
-end
+  class Method
+    def invoke_static(*args)
+      invoke(nil, *args)
+    end
 
-class reflect::Field
-  alias value_type name
-  alias value get
-  alias set_value set
-  
-  def static_value
-    get(nil)
+    alias argument_types parameter_types
   end
-  
-  def set_static_value(val)
-    set(nil, val)
+
+  class Field
+    alias value_type name
+    alias value get
+    alias set_value set
+
+    def static_value
+      get(nil)
+    end
+
+    def set_static_value(val)
+      set(nil, val)
+    end
   end
 end
 
@@ -207,20 +227,5 @@ Java::byte[].class_eval do
   def ubyte_set(index, value)
     value -= 256 if value > 127
     self[index] = value
-  end
-end
-
-class lang::Character
-  java_alias :isJavaIdentifierStart_char, :isJavaIdentifierStart, [Java::char]
-  java_alias :isJavaIdentifierPart_char, :isJavaIdentifierPart, [Java::char]
-
-  class << self
-    def java_identifier_start?(fixnum)
-      isJavaIdentifierStart_char(fixnum);
-    end
-
-    def java_identifier_part?(fixnum)
-      isJavaIdentifierPart_char(fixnum);
-    end
   end
 end
