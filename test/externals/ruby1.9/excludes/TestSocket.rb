de344ca7f9eac83e44ae09f2a19167b09c0b4a20
Thomas E. Enebo <tom.enebo@gmail.com>
2016-04-08 14:18:03 -0500

    Accidentally untagged tests which did not run on windows but still did on unixy systems...which have never worked.  retagging

--- test/externals/ruby1.9/excludes/TestSocket.rb
+++ test/externals/ruby1.9/excludes/TestSocket.rb
@@ -1,5 +1,6 @@
 exclude :test_accept_loop, "needs investigation"
 exclude :test_accept_loop_multi_port, "needs investigation"
+exclude :test_accept_loop_with_unix, "needs investigation"
 exclude :test_closed_read, "needs investigation"
 exclude :test_getaddrinfo, "needs investigation"
 exclude :test_getaddrinfo_raises_no_errors_on_port_argument_of_0, "needs investigation"
@@ -13,6 +14,6 @@ exclude :test_tcp, "needs investigation"
 exclude :test_tcp_server_sockets, "needs investigation"
 exclude :test_tcp_server_sockets_port0, "needs investigation"
 exclude :test_udp_server, "needs investigation"
+exclude :test_unix, "needs investigation"
+exclude :test_unix_server_socket, "needs investigation"
 exclude :test_unpack_sockaddr, "needs investigation"
-
-
