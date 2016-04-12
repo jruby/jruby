f15140aaadfb33330e1458b591218ec77c353ffd
kares <self@kares.org>
2016-04-12 23:14:27 +0200

    add more java.util.List specs + some more diversity on tested list impls

--- spec/java_integration/extensions/list_spec.rb
+++ spec/java_integration/extensions/list_spec.rb
@@ -1,11 +1,9 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
-java_import "java.util.ArrayList"
-
 describe "List Ruby extensions" do
   before(:each) do
     @data = ["foo", "quux", "bar", "aa"]
-    @list = ArrayList.new(@data)
+    @list = java.util.ArrayList.new(@data)
   end
 
   it "should support index() with one argument" do
@@ -16,28 +14,54 @@ describe "List Ruby extensions" do
     expect(@list.index(451)).to eq(nil)
   end
 
+  it "should return nil on rindex() with one argument that does not exist" do
+    expect(@list.rindex(-24)).to eq(nil)
+    list = java.util.Stack.new
+    expect(list.rindex('foo')).to eq(nil)
+    expect(list.rindex(nil)).to eq(nil)
+  end
+
   it "should support index() with a block" do
-    expect(@list.index{|x| x == "bar" }).to eq(2)
+    expect(@list.index{ |x| x == 'bar' }).to eq(2)
+  end
+
+  it "should support rindex() with a block" do
+    list = java.util.Vector.new @data; list << 'bar'; list << 'aa'
+    expect(list.rindex{ |x| x == 'bar' }).to eq(4)
   end
 
   it "should support index() with a block that does not exist" do
-    expect(@list.index{|x| x == :nazgul }).to eq(nil)
+    expect(@list.index{ |x| x == :nazgul }).to eq(nil)
   end
 
   it "should support index() with nil (not found)" do
     expect(@list.index(nil)).to eq(nil)
   end
 
+  it "should support rindex() with nil (not found)" do
+    expect(@list.rindex(nil)).to eq(nil)
+  end
+
   it "should support index() with nil (found)" do
-    expect(ArrayList.new(["foo", "quux", nil, "bar", "aa"]).index(nil)).to eq(2)
+    expect(java.util.LinkedList.new(["foo", "quux", nil, "bar", "aa"]).index(nil)).to eq(2)
+  end
+
+  it "should support rindex() with nil (found)" do
+    expect(java.util.ArrayList.new(["foo", "quux", nil, "bar", nil, "aa"]).rindex(nil)).to eq(4)
   end
 
   it "should support index() with no arguments" do
-    expect(@list.index.each {|x| x == "foo" }).to eq(0)
+    expect( @list.index.each { |x| x == "foo" } ).to eq(0)
   end
 
   it "should support index() with no arguments (not existing)" do
-    expect(@list.index.each {|x| x == ":-(" }).to eq(nil)
+    expect( @list.index.each { |x| x == ":-(" } ).to eq(nil)
+  end
+
+  it "should support rindex() with no arguments" do
+    list = java.util.Stack.new
+    list << 1; list << 2; list << 3; list << 1; list << 2; list << 3
+    expect( list.rindex.each {|x| x < 2 } ).to eq(3)
   end
 
   # Java 8 adds a single-parameter sort method to List that sorts in-place
@@ -60,13 +84,13 @@ describe "List Ruby extensions" do
   end
 
   it "should be sortable with sort!() without block" do
-    list = ArrayList.new(@data)
+    list = java.util.LinkedList.new(@data)
     list.sort!
     expect(list.to_a).to eq(@data.sort)
   end
 
   it "should be sortable with sort!() with block" do
-    list = ArrayList.new(@data)
+    list = java.util.ArrayList.new(@data)
     list.sort! do |a, b|
       a.length <=> b.length
     end
@@ -76,6 +100,22 @@ describe "List Ruby extensions" do
     end
 
     expect(list.to_a).to eq(expected)
+  end
+
+  it 'converts to_ary' do
+    list = java.util.LinkedList.new(@data)
+    expect(list.to_ary).to eq(@data)
+
+    list = java.util.ArrayList.new(@data)
+    list.sort!
+    expect(list.to_ary).to eq(@data.sort)
+  end
+
+  it 'reports list size/length' do
+    list = java.util.Vector.new(@data)
+    expect(list.length).to eq(@data.size)
+    expect(list.size).to eq(list.length)
+    expect( java.util.Collections.emptyList.length ).to eq 0
   end
 
   it "should support slicing with 2 arguments" do
@@ -93,8 +133,7 @@ describe "List Ruby extensions" do
   it "should respect to_ary objects defined on iteration" do
     class Pair
       def initialize(a, b)
-        @a = a
-        @b = b
+        @a = a ; @b = b
       end
 
       def to_ary
@@ -102,7 +141,7 @@ describe "List Ruby extensions" do
       end
     end
 
-    ArrayList.new([Pair.new(:x, :y)]).each do |car, cdr|
+    java.util.ArrayList.new([Pair.new(:x, :y)]).each do |car, cdr|
       expect(car).to eq(:x)
       expect(cdr).to eq(:y)
     end
