b8adec31bdda40e80df82962938023d86daa00e9
Martin Mauch <martin.mauch@gmail.com>
2016-04-20 16:29:35 +0200

    Expose two Enumerator-related bugs

--- spec/ruby/core/enumerator/lazy/map_spec.rb
+++ spec/ruby/core/enumerator/lazy/map_spec.rb
@@ -5,4 +5,7 @@ require File.expand_path('../shared/collect', __FILE__)
 
 describe "Enumerator::Lazy#map" do
   it_behaves_like :enumerator_lazy_collect, :map
+  it "doesn't unwrap Arrays" do
+    Enumerator.new {|y| y.yield([1])}.lazy.to_a.should == [[1]]
+  end
 end
