3463e1843201526aaca3f394da8c3907af2c986d
Chris Seaton <chris.seaton@oracle.com>
2016-04-28 21:08:44 +0100

    [Truffle] Move foreign access helpers to the top level.

--- truffle/src/main/java/org/jruby/truffle/interop/ForeignExecuteNode.java
+++ truffle/src/main/java/org/jruby/truffle/interop/ForeignExecuteNode.java
@@ -10,25 +10,13 @@
 package org.jruby.truffle.interop;
 
 import com.oracle.truffle.api.CompilerDirectives;
-import com.oracle.truffle.api.dsl.Cached;
-import com.oracle.truffle.api.dsl.NodeChild;
-import com.oracle.truffle.api.dsl.NodeChildren;
-import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.interop.AcceptMessage;
 import com.oracle.truffle.api.nodes.Node;
-import com.oracle.truffle.api.nodes.Node.Child;
 import com.oracle.truffle.api.object.DynamicObject;
 import org.jruby.truffle.RubyContext;
 import org.jruby.truffle.RubyLanguage;
-import org.jruby.truffle.core.Layouts;
-import org.jruby.truffle.language.RubyNode;
 import org.jruby.truffle.language.RubyObjectType;
-import org.jruby.truffle.language.methods.CallBoundMethodNode;
-import org.jruby.truffle.language.methods.CallBoundMethodNodeGen;
-import org.jruby.truffle.language.methods.DeclarationContext;
-import org.jruby.truffle.language.yield.CallBlockNode;
-import org.jruby.truffle.language.yield.CallBlockNodeGen;
 
 @AcceptMessage(value = "EXECUTE", receiverType = RubyObjectType.class, language = RubyLanguage.class)
 public final class ForeignExecuteNode extends ForeignExecuteBaseNode {
@@ -46,45 +34,10 @@ public final class ForeignExecuteNode extends ForeignExecuteBaseNode {
             CompilerDirectives.transferToInterpreter();
             findContextNode = insert(RubyLanguage.INSTANCE.unprotectedCreateFindContextNode());
             final RubyContext context = RubyLanguage.INSTANCE.unprotectedFindContext(findContextNode);
-            executeMethodNode = insert(ForeignExecuteNodeFactory.ForeignExecuteHelperNodeGen.create(context, null, null));
+            executeMethodNode = insert(ForeignExecuteHelperNodeGen.create(context, null, null));
         }
 
         return executeMethodNode;
-    }
-
-    @NodeChildren({
-            @NodeChild("receiver"),
-            @NodeChild("arguments")
-    })
-    protected static abstract class ForeignExecuteHelperNode extends RubyNode {
-
-        public ForeignExecuteHelperNode(RubyContext context) {
-            super(context, null);
-        }
-
-        public abstract Object executeCall(VirtualFrame frame, Object receiver, Object[] arguments);
-
-        @Specialization(guards = "isRubyProc(proc)")
-        protected Object callProc(VirtualFrame frame, DynamicObject proc, Object[] arguments,
-                @Cached("createCallBlockNode()") CallBlockNode callBlockNode) {
-            Object self = Layouts.PROC.getSelf(proc);
-            return callBlockNode.executeCallBlock(frame, proc, self, null, arguments);
-        }
-
-        protected CallBlockNode createCallBlockNode() {
-            return CallBlockNodeGen.create(getContext(), getSourceSection(), DeclarationContext.BLOCK, null, null, null, null);
-        }
-
-        @Specialization(guards = "isRubyMethod(method)")
-        protected Object callMethod(VirtualFrame frame, DynamicObject method, Object[] arguments,
-                @Cached("createCallBoundMethodNode()") CallBoundMethodNode callBoundMethodNode) {
-            return callBoundMethodNode.executeCallBoundMethod(frame, method, arguments, null);
-        }
-
-        protected CallBoundMethodNode createCallBoundMethodNode() {
-            return CallBoundMethodNodeGen.create(getContext(), getSourceSection(), null, null, null);
-        }
-
     }
 
 }
---
+++ truffle/src/main/java/org/jruby/truffle/interop/ForeignReadStringCachedHelperNode.java
@@ -0,0 +1,131 @@
+package org.jruby.truffle.interop;
+
+/*
+ * Copyright (c) 2013, 2016 Oracle and/or its affiliates. All rights reserved. This
+ * code is released under a tri EPL/GPL/LGPL license. You can use it,
+ * redistribute it and/or modify it under the terms of the:
+ *
+ * Eclipse Public License version 1.0
+ * GNU General Public License version 2
+ * GNU Lesser General Public License version 2.1
+ */
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.NodeChildren;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.object.DynamicObject;
+import org.jruby.truffle.language.RubyNode;
+import org.jruby.truffle.language.dispatch.CallDispatchHeadNode;
+import org.jruby.truffle.language.dispatch.DispatchHeadNodeFactory;
+import org.jruby.truffle.language.dispatch.DoesRespondDispatchHeadNode;
+import org.jruby.truffle.language.objects.ReadObjectFieldNode;
+import org.jruby.truffle.language.objects.ReadObjectFieldNodeGen;
+
+@NodeChildren({
+        @NodeChild("receiver"),
+        @NodeChild("name"),
+        @NodeChild("stringName"),
+        @NodeChild("startsAt")
+})
+abstract class ForeignReadStringCachedHelperNode extends RubyNode {
+
+    @Child
+    private DoesRespondDispatchHeadNode definedNode;
+    @Child
+    private DoesRespondDispatchHeadNode indexDefinedNode;
+    @Child
+    private CallDispatchHeadNode callNode;
+
+    protected final static String INDEX_METHOD_NAME = "[]";
+
+    public abstract Object executeStringCachedHelper(VirtualFrame frame, DynamicObject receiver, Object name,
+                                                     String stringName, boolean startsAt);
+
+    @Specialization(guards = "startsAt(startsAt)")
+    public Object readInstanceVariable(
+            DynamicObject receiver,
+            Object name,
+            String stringName,
+            boolean startsAt,
+            @Cached("createReadObjectFieldNode(stringName)") ReadObjectFieldNode readObjectFieldNode) {
+        return readObjectFieldNode.execute(receiver);
+    }
+
+    protected boolean startsAt(boolean startsAt) {
+        return startsAt;
+    }
+
+    protected ReadObjectFieldNode createReadObjectFieldNode(String name) {
+        return ReadObjectFieldNodeGen.create(name, nil());
+    }
+
+    @Specialization(
+            guards = {
+                    "notStartsAt(startsAt)",
+                    "methodDefined(frame, receiver, stringName, getDefinedNode())"
+            }
+    )
+    public Object callMethod(
+            VirtualFrame frame,
+            DynamicObject receiver,
+            Object name,
+            String stringName,
+            boolean startsAt) {
+        return getCallNode().call(frame, receiver, stringName, null);
+    }
+
+    @Specialization(
+            guards = {
+                    "notStartsAt(startsAt)",
+                    "!methodDefined(frame, receiver, stringName, getDefinedNode())",
+                    "methodDefined(frame, receiver, INDEX_METHOD_NAME, getIndexDefinedNode())"
+            }
+    )
+    public Object index(
+            VirtualFrame frame,
+            DynamicObject receiver,
+            Object name,
+            String stringName,
+            boolean startsAt) {
+        return getCallNode().call(frame, receiver, "[]", null, name);
+    }
+
+    protected boolean notStartsAt(boolean startsAt) {
+        return !startsAt;
+    }
+
+    protected DoesRespondDispatchHeadNode getDefinedNode() {
+        if (definedNode == null) {
+            CompilerDirectives.transferToInterpreter();
+            definedNode = insert(new DoesRespondDispatchHeadNode(getContext(), true));
+        }
+
+        return definedNode;
+    }
+
+    protected DoesRespondDispatchHeadNode getIndexDefinedNode() {
+        if (indexDefinedNode == null) {
+            CompilerDirectives.transferToInterpreter();
+            indexDefinedNode = insert(new DoesRespondDispatchHeadNode(getContext(), true));
+        }
+
+        return indexDefinedNode;
+    }
+
+    protected boolean methodDefined(VirtualFrame frame, DynamicObject receiver, String stringName,
+                                    DoesRespondDispatchHeadNode definedNode) {
+        return definedNode.doesRespondTo(frame, stringName, receiver);
+    }
+
+    protected CallDispatchHeadNode getCallNode() {
+        if (callNode == null) {
+            CompilerDirectives.transferToInterpreter();
+            callNode = insert(DispatchHeadNodeFactory.createMethodCall(getContext(), true));
+        }
+
+        return callNode;
+    }
+
+}
