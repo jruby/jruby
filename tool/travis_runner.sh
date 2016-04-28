a4dbd4f384d162fb3f8ae7b6f0021d0afa211ad8
Benoit Daloze <eregontp@gmail.com>
2016-04-28 14:44:55 +0200

    [Truffle] Try to skip the multiple builds/install for check_ambiguous_arguments.

--- tool/travis_runner.sh
+++ tool/travis_runner.sh
@@ -3,19 +3,16 @@
 set -e
 set -x
 
-if [[ -v JT ]] && [[ "$JT" = "check_ambiguous_arguments" ]]
+if [ -z "$SKIP_BUILD" ]
 then
-  ruby tool/jt.rb $JT
-  exit $?
-fi
+  ./mvnw install -B -Dinvoker.skip=false $PHASE | egrep -v 'Download|\\[exec\\] [[:digit:]]+/[[:digit:]]+|^[[:space:]]*\\[exec\\][[:space:]]*$'
 
-./mvnw install -B -Dinvoker.skip=false $PHASE | egrep -v 'Download|\\[exec\\] [[:digit:]]+/[[:digit:]]+|^[[:space:]]*\\[exec\\][[:space:]]*$'
+  MVN_STATUS=${PIPESTATUS[0]}
 
-MVN_STATUS=${PIPESTATUS[0]}
-
-if [ $MVN_STATUS != 0 ]
-then
-  exit $MVN_STATUS
+  if [ $MVN_STATUS != 0 ]
+  then
+    exit $MVN_STATUS
+  fi
 fi
 
 if [[ -v COMMAND ]]
