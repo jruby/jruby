096e9d1603474836e51ad8dbee1dd1d71f8911fe
Chris Seaton <chris.seaton@oracle.com>
2016-05-02 22:17:12 +0100

    [Truffle] C extension test for rb_define_method.

---
+++ test/truffle/cexts/method/ext/method/method.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include <ruby.h>
+
+VALUE add(VALUE self, VALUE a, VALUE b) {
+  return INT2NUM(NUM2INT(a) + NUM2INT(b));
+}
+
+void Init_method() {
+  VALUE module = rb_define_module("MethodExtension");
+  rb_define_module_function(module, "add", &add, 2);
+}
