a6e34f46ec0e865bec2f5337f8c0d07d14fb5086
Benoit Daloze <eregontp@gmail.com>
2016-05-09 14:53:46 +0200

    Remove old unused truffle testing profiles.

--- pom.rb
+++ pom.rb
@@ -188,19 +188,7 @@ project 'JRuby', 'https://github.com/jruby/jruby' do
     modules [ 'test' ]
   end
 
-  [
-    'rake',
-    'exec',
-    'truffle-specs-language',
-    'truffle-specs-core',
-    'truffle-specs-library',
-    'truffle-specs-truffle',
-    'truffle-specs-language-report',
-    'truffle-specs-core-report',
-    'truffle-specs-library-report',
-    'truffle-test-pe',
-    'truffle-mri-tests'
-  ].each do |name|
+  [ 'rake', 'exec' ].each do |name|
     profile name do
 
       modules [ 'test' ]
