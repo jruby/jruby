40f0c84041e3485bca7b8b72418075fb6a29da71
Pavel Rosický <pavel.rosicky@easy.cz>
2016-05-10 13:35:43 +0200

    expand_path should use the original encoding

--- core/src/main/java/org/jruby/RubyFile.java
+++ core/src/main/java/org/jruby/RubyFile.java
@@ -786,7 +786,6 @@ public class RubyFile extends RubyIO implements EncodingCapable {
     @JRubyMethod(name = "expand_path", required = 1, optional = 1, meta = true)
     public static IRubyObject expand_path19(ThreadContext context, IRubyObject recv, IRubyObject[] args) {
         RubyString path = (RubyString) expandPathInternal(context, recv, args, true, false);
-        path.force_encoding(context, context.runtime.getEncodingService().getDefaultExternal());
 
         return path;
     }
