ac53c54765efc7207c65f4deb8a050b4dc565594
Charles Oliver Nutter <headius@headius.com>
2016-05-12 16:51:29 -0500

    Add a spec for hashing a double-nested array with inner zarray.
    Relates to https://github.com/jruby/jruby/issues/3884, where JRuby
    was always returning the same hash for certain combinations of
    nested array with an innermost empty array.

--- spec/ruby/core/array/hash_spec.rb
+++ spec/ruby/core/array/hash_spec.rb
@@ -79,4 +79,8 @@ describe "Array#hash" do
     a.hash.should == b.hash
     a.should eql(b)
   end
+
+  it "produces different hashes for nested arrays with different values and empty terminator" do
+    [1, [1, []]].hash.should_not == [2, [2, []]]
+  end
 end
