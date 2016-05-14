7d41b96eb7acd49f77ec554cdeb82cf6c21c65ae
Chris Seaton <chris.seaton@oracle.com>
2016-05-14 02:23:53 +0100

    [Truffle] Tag failing tests, don't comment out.

--- test/truffle/compiler/pe/interop/interop_pe.rb
+++ test/truffle/compiler/pe/interop/interop_pe.rb
@@ -17,5 +17,5 @@ end
 
 
 # The Truffle API's these use are different from GraalVM 0.12 and Truffle 0.13
-#example "Truffle::Interop.read(InteropPE::A.new(42), :@foo)", 42
-#example "Truffle::Interop.read(InteropPE::A.new(42), '@foo')", 42
+tagged example "Truffle::Interop.read(InteropPE::A.new(42), :@foo)", 42
+tagged example "Truffle::Interop.read(InteropPE::A.new(42), '@foo')", 42
