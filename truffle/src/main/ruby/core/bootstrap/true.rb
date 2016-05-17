46f6ea05c61b470a2bb192bd9ac552fa9278e905
Chris Seaton <chris.seaton@oracle.com>
2016-05-17 16:09:11 +0100

    [Truffle] common true/false can be hoisted into bootstrap.

--- truffle/src/main/ruby/core/bootstrap/true.rb
+++ truffle/src/main/ruby/core/bootstrap/true.rb
@@ -24,12 +24,16 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# Only part of Rubinius' true.rb
-
 class TrueClass
 
   def to_s
     "true"
+  end
+
+  def inspect
+    # Call #to_s rather than alias it so that people can change #to_s if they
+    # wish. Yes, sounds dumb.
+    to_s
   end
 
 end
