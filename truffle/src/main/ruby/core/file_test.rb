d43caeea29655124c30603e242224a258ef11c57
Chris Seaton <chris.seaton@oracle.com>
2016-05-17 16:59:08 +0100

    [Truffle] Unify file_test.

--- truffle/src/main/ruby/core/file_test.rb
+++ truffle/src/main/ruby/core/file_test.rb
@@ -117,4 +117,29 @@ module FileTest
   def zero?(path)
     File.zero? path
   end
+
+  module_function :blockdev?,
+                  :chardev?,
+                  :directory?,
+                  :executable?,
+                  :executable_real?,
+                  :exist?,
+                  :exists?,
+                  :file?,
+                  :grpowned?,
+                  :identical?,
+                  :owned?,
+                  :pipe?,
+                  :readable?,
+                  :readable_real?,
+                  :setgid?,
+                  :setuid?,
+                  :size,
+                  :size?,
+                  :socket?,
+                  :sticky?,
+                  :symlink?,
+                  :writable?,
+                  :writable_real?,
+                  :zero?
 end
