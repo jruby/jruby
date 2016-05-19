fa37a40ba5dc3d349bc3365efea522cfaba0cf00
Chris Seaton <chris.seaton@oracle.com>
2016-05-19 21:23:34 +0100

    [Truffle] Combine bootstrap and common channel.

--- truffle/src/main/ruby/core/channel.rb
+++ truffle/src/main/ruby/core/channel.rb
@@ -1,3 +1,11 @@
+# Copyright (c) 2015 Oracle and/or its affiliates. All rights reserved. This
+# code is released under a tri EPL/GPL/LGPL license. You can use it,
+# redistribute it and/or modify it under the terms of the:
+#
+# Eclipse Public License version 1.0
+# GNU General Public License version 2
+# GNU Lesser General Public License version 2.1
+
 # Copyright (c) 2007-2015, Evan Phoenix and contributors
 # All rights reserved.
 #
@@ -24,12 +32,36 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-##
-# A communication mechanism based on pi-calculus channels used primarily to
-# communicate between ruby and the VM about events.
-
+# Implementation of Rubinius::Channel using a Queue
 module Rubinius
   class Channel
+    def initialize
+      @queue = Queue.new
+    end
+
+    def send(obj)
+      @queue << obj
+    end
+    alias_method :<<, :send
+
+    def receive
+      @queue.pop
+    end
+
+    def receive_timeout(duration)
+      Rubinius.privately do
+        @queue.receive_timeout(duration)
+      end
+    end
+
+    def try_receive
+      begin
+        @queue.pop(true)
+      rescue ThreadError # queue empty
+        nil
+      end
+    end
+
     def inspect
       "#<Rubinius::Channel:0x#{object_id.to_s(16)}>"
     end
