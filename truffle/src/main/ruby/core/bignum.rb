da2af755bfa2f7b3091312f83a3f1a6cc3ce6b46
Chris Seaton <chris.seaton@oracle.com>
2016-05-19 21:21:49 +0100

    [Truffle] Combine bootstrap and common bignum.

--- truffle/src/main/ruby/core/bignum.rb
+++ truffle/src/main/ruby/core/bignum.rb
@@ -25,6 +25,20 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 class Bignum < Integer
+  def <=>(other)
+    Rubinius.primitive :bignum_compare
+
+    # We do not use redo_compare here because Ruby does not
+    # raise if any part of the coercion or comparison raises
+    # an exception.
+    begin
+      coerced = other.coerce self
+      return nil unless coerced
+      coerced[0] <=> coerced[1]
+    rescue
+      return nil
+    end
+  end
 
   def eql?(value)
     value.is_a?(Bignum) && self == value
