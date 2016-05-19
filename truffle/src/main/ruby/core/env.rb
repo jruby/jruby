1d16f758f8f5e2891d3394d4f14d8a357cd73d10
Chris Seaton <chris.seaton@oracle.com>
2016-05-19 23:52:57 +0100

    [Truffle] Move env to core.

--- truffle/src/main/ruby/core/env.rb
+++ truffle/src/main/ruby/core/env.rb
@@ -28,6 +28,16 @@
 # Interface to process environment variables.
 
 module Rubinius
+  module EnvironmentAccess
+    extend FFI::Library
+
+    attach_function :getenv,   [:string], :string
+    attach_function :putenv,   [:string], :int
+    attach_function :setenv,   [:string,  :string, :int], :int
+    attach_function :unsetenv, [:string], :int
+    attach_function :environ,  'ffi_environ', [], :pointer
+  end
+
   class EnvironmentVariables
     include Enumerable
     include Rubinius::EnvironmentAccess
