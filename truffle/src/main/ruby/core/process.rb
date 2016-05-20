cd5d53422b783b87212d22949ff40296a43e8003
Chris Seaton <chris.seaton@oracle.com>
2016-05-20 17:10:36 +0100

    [Truffle] Combine bootstrap and common process and move to core.

--- truffle/src/main/ruby/core/process.rb
+++ truffle/src/main/ruby/core/process.rb
@@ -74,6 +74,21 @@ module Process
     config "rbx.platform.rlimit", :rlim_cur, :rlim_max
   end
 
+  def self.wait_pid_prim(pid, no_hang)
+    Rubinius.primitive :vm_wait_pid
+    raise PrimitiveFailure, "Process.wait_pid primitive failed"
+  end
+
+  def self.time
+    Rubinius.primitive :vm_time
+    raise PrimitiveFailure, "Process.time primitive failed"
+  end
+
+  def self.cpu_times
+    Rubinius.primitive :vm_times
+    raise PrimitiveFailure, "Process.cpu_times primitive failed"
+  end
+
   ##
   # Sets the process title. Calling this method does not affect the value of
   # `$0` as per MRI behaviour. This method returns the title set.
