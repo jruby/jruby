8c4189a7c824e7429cc04bb51bc2f83ee88a25e3
Chris Seaton <chris.seaton@oracle.com>
2016-05-22 00:38:31 +0100

    [Truffle] Fold in bootstrap dir.

--- truffle/src/main/ruby/core/dir.rb
+++ truffle/src/main/ruby/core/dir.rb
@@ -41,6 +41,57 @@ class Dir
 
   FFI = Rubinius::FFI
 
+  def self.allocate
+    Rubinius.primitive :dir_allocate
+    raise PrimitiveFailure, "Dir.allocate primitive failed"
+  end
+
+  def initialize(path, options=undefined)
+    path = Rubinius::Type.coerce_to_path path
+
+    if options.equal? undefined
+      enc = nil
+    else
+      options = Rubinius::Type.coerce_to options, Hash, :to_hash
+      enc = options[:encoding]
+      enc = Rubinius::Type.coerce_to_encoding enc if enc
+    end
+
+    Rubinius.invoke_primitive :dir_open, self, path, enc
+  end
+
+  private :initialize
+
+  def close
+    Rubinius.primitive :dir_close
+    raise PrimitiveFailure, "Dir#close primitive failed"
+  end
+
+  def closed?
+    Rubinius.primitive :dir_closed_p
+    raise PrimitiveFailure, "Dir#closed? primitive failed"
+  end
+
+  def read
+    entry = Rubinius.invoke_primitive :dir_read, self
+    return unless entry
+
+    if Encoding.default_external == Encoding::US_ASCII && !entry.valid_encoding?
+      entry.force_encoding Encoding::ASCII_8BIT
+      return entry
+    end
+
+    enc = Encoding.default_internal
+    enc ? entry.encode(enc) : entry
+  end
+
+  def control(kind, pos)
+    Rubinius.primitive :dir_control
+    raise PrimitiveFailure, "Dir#__control__ primitive failed"
+  end
+
+  private :control
+
   def self.open(path, options=undefined)
     dir = new path, options
     if block_given?
