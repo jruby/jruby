f9d422abaf83b30eff054f1de82f53c6ec8cebce
Benoit Daloze <eregontp@gmail.com>
2016-05-22 13:01:54 +0200

    [Truffle] Classify C-ext methods by category.
    * Also implement RARRAY_AREF.

--- lib/ruby/truffle/cext/ruby.h
+++ lib/ruby/truffle/cext/ruby.h
@@ -29,6 +29,8 @@ extern "C" {
 typedef void *ID;
 typedef void *VALUE;
 
+// Constants
+
 VALUE get_Qfalse(void);
 VALUE get_Qtrue(void);
 VALUE get_Qnil(void);
@@ -53,6 +55,7 @@ VALUE get_rb_eRuntimeError(void);
 
 #define rb_eRuntimeError get_rb_eRuntimeError()
 
+// Conversions
 
 int NUM2INT(VALUE value);
 unsigned int NUM2UINT(VALUE value);
@@ -69,44 +72,70 @@ VALUE UINT2NUM(unsigned int value);
 VALUE LONG2NUM(long value);
 VALUE LONG2FIX(long value);
 
+// Type checks
+
 int FIXNUM_P(VALUE value);
+
+// Float
 
 VALUE rb_float_new(double value);
 
+// String
+
 char *RSTRING_PTR(VALUE string);
 int RSTRING_LEN(VALUE string);
-ID rb_intern(const char *string);
-VALUE rb_str_new2(const char *string);
-VALUE ID2SYM(ID id);
 VALUE rb_intern_str(VALUE string);
+VALUE rb_str_new2(const char *string);
 void rb_str_cat(VALUE string, const char *to_concat, long length);
 
+// Symbol
+
+ID rb_intern(const char *string);
+VALUE ID2SYM(ID id);
+
+// Array
+
 int RARRAY_LEN(VALUE array);
+int RARRAY_LENINT(VALUE array);
 VALUE *RARRAY_PTR(VALUE array);
+VALUE RARRAY_AREF(VALUE array, long index);
+VALUE rb_ary_new(void);
 VALUE rb_ary_new_capa(long capacity);
 #define rb_ary_new2 rb_ary_new_capa
-VALUE rb_ary_new(void);
 void rb_ary_push(VALUE array, VALUE value);
 VALUE rb_ary_pop(VALUE array);
 void rb_ary_store(VALUE array, long index, VALUE value);
 VALUE rb_ary_entry(VALUE array, long index);
-int RARRAY_LENINT(VALUE array);
 VALUE rb_ary_dup(VALUE array);
+
+// Hash
 
 VALUE rb_hash_new(void);
 VALUE rb_hash_aref(VALUE hash, VALUE key);
 void rb_hash_aset(VALUE hash, VALUE key, VALUE value);
 
+// Utilities
+
 void rb_scan_args(int argc, VALUE *argv, const char *format, ...);
 
+// Calls
+
 VALUE rb_funcall(VALUE object, ID name, int argc, ...);
+
+// Instance variables
 
 VALUE rb_iv_get(VALUE object, const char *name);
 VALUE rb_iv_set(VALUE object, const char *name, VALUE value);
 
+// Accessing constants
+
 VALUE rb_const_get(VALUE object, ID name);
 
+// Raising exceptions
+
 void rb_raise(VALUE exception, const char *format, ...);
+
+// Defining classes, modules and methods
 
 VALUE rb_define_class(const char *name, VALUE superclass);
 VALUE rb_define_module(const char *name);
