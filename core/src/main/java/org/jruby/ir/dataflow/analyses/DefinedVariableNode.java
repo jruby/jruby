e4fbb0b6450dfe48d4b5557cbeb59cb1a2b82700
Subramanya Sastry <sss.lists@gmail.com>
2016-06-07 23:47:30 -0500

    Init bitset size after the # of variables have been computed
    * The bitset size computed was always zero
      => in.and(out) effectively pushed in to all false values since
      there were no true values in that set. Fix this by allocating
      a new 'in' bitset the same size as 'out'.
    
    * This led to unnecessarily conservative init results. Due to the
      order in which the missing inits are added, these conservative inits
      were sometimes clobbering earlier already-initialized values.
      I am not fixing that inserting logic in this patch yet since
      it shouldn't be necessary.
    
    * Fixes spec:compiler run

--- core/src/main/java/org/jruby/ir/dataflow/analyses/DefinedVariableNode.java
+++ core/src/main/java/org/jruby/ir/dataflow/analyses/DefinedVariableNode.java
@@ -18,7 +18,6 @@ public class DefinedVariableNode extends FlowGraphNode<DefinedVariablesProblem, 
 
     @Override
     public void init() {
-        setSize = problem.getDFVarsCount();
         // 'null' acts as the TOP for this dataflow analysis
         out = null;
     }
@@ -38,6 +37,7 @@ public class DefinedVariableNode extends FlowGraphNode<DefinedVariablesProblem, 
     @Override
     public void applyPreMeetHandler() {
         // 'null' acts as the TOP for this dataflow analysis
+        setSize = problem.getDFVarsCount();
         in = null;
     }
 
@@ -48,8 +48,10 @@ public class DefinedVariableNode extends FlowGraphNode<DefinedVariablesProblem, 
             // if in is TOP, init in to a bitset with all 1's
             // so the intersection computes the right value.
             if (in == null) {
-                in = new BitSet(setSize);
-                in.set(0, setSize);
+                // Make sure 'in' and 'out' are the same size!
+                int n = pred.out.size();
+                in = new BitSet(n);
+                in.set(0, n);
             }
 
             in.and(pred.out);
@@ -83,6 +85,8 @@ public class DefinedVariableNode extends FlowGraphNode<DefinedVariablesProblem, 
             parentScopeDepth = 0;
         }
 
+        // System.out.println("BB " + basicBlock + "; state\n" + toString());
+
         initSolution();
         for (Instr i: basicBlock.getInstrs()) {
             // Variables that belong to outer scopes should always
@@ -94,7 +98,7 @@ public class DefinedVariableNode extends FlowGraphNode<DefinedVariablesProblem, 
                     }
 
                     if (!tmp.get(problem.getDFVar(v))) {
-                        // System.out.println("Variable " + v + " in instr " + i + " isn't defined!");
+                        // System.out.println("Variable " + v + " in instr " + i + " in " + basicBlock + " isn't defined!");
                         undefinedVars.add(v);
                     }
                 }
@@ -117,8 +121,43 @@ public class DefinedVariableNode extends FlowGraphNode<DefinedVariablesProblem, 
         out = tmp;
     }
 
+    private String printSet(BitSet set) {
+        StringBuilder buf = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < set.size(); i++) {
+            if (set.get(i)) {
+                count++;
+                buf.append(' ').append(problem.getVariable(i));
+                if (count % 10 == 0) buf.append("\t\n");
+            }
+        }
+
+        if (count % 10 != 0) buf.append("\t\t");
+        return buf.append('\n').toString();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder buf = new StringBuilder();
+        buf.append("\tVars defined on Entry: ");
+        if (in == null) {
+            System.out.println("-- NO in!");
+        } else {
+            buf.append(printSet(in));
+        }
+
+        buf.append("\n\tVars defined on Exit: ");
+        if (out == null) {
+            System.out.println("-- NO out!");
+        } else {
+            buf.append(printSet(out));
+        }
+
+        return buf.append('\n').toString();
+    }
+
     private BitSet in;      // Variables defined at entry of this node
     private BitSet out;     // Variables defined at exit of node
-    private BitSet tmp;  // Temporary state while applying transfer function
+    private BitSet tmp;     // Temporary state while applying transfer function
     private int setSize;    // Size of the "this.in" and "this.out" bit sets
 }
