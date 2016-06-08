a1047ee58fb30046c82a8c78c4741e7588e19ab3
Thomas E. Enebo <tom.enebo@gmail.com>
2016-06-08 16:08:23 -0500

    All StrNodes encountered need not worry about order and can get created out of order.

--- core/src/main/java/org/jruby/ir/IRBuilder.java
+++ core/src/main/java/org/jruby/ir/IRBuilder.java
@@ -561,6 +561,10 @@ public class IRBuilder {
     }
 
     protected Operand buildWithOrder(Node node, boolean preserveOrder) {
+        // Even though String literals are mutable they cannot fail or depend on rest of program's semantics so
+        // they can ignore order.
+        if (node instanceof StrNode) return buildStrRaw((StrNode) node);
+
         Operand value = build(node);
 
         // We need to preserve order in cases (like in presence of assignments) except that immutable
@@ -828,7 +832,7 @@ public class IRBuilder {
         Operand[] elts = new Operand[nodes.length];
         boolean containsAssignments = node.containsVariableAssignment();
         for (int i = 0; i < nodes.length; i++) {
-            elts[i] = nodes[i] instanceof StrNode ? buildStrRaw((StrNode) nodes[i]) : buildWithOrder(nodes[i], containsAssignments);
+            elts[i] = buildWithOrder(nodes[i], containsAssignments);
         }
 
         Operand array = new Array(elts);
