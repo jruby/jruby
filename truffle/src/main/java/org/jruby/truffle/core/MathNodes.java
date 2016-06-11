99c3c5ebd206a6c3116ace4dc02813dc9a858834
Chris Seaton <chris.seaton@oracle.com>
2016-06-11 21:34:38 +0100

    [Truffle] Simplify copyright in MathNodes.

--- truffle/src/main/java/org/jruby/truffle/core/MathNodes.java
+++ truffle/src/main/java/org/jruby/truffle/core/MathNodes.java
@@ -6,6 +6,34 @@
  * Eclipse Public License version 1.0
  * GNU General Public License version 2
  * GNU Lesser General Public License version 2.1
+ *
+ * The contents of this file are subject to the Eclipse Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * Copyright (C) 2001 Chad Fowler <chadfowler@chadfowler.com>
+ * Copyright (C) 2001-2002 Jan Arne Petersen <jpetersen@uni-bonn.de>
+ * Copyright (C) 2002 Benoit Cerrina <b.cerrina@wanadoo.fr>
+ * Copyright (C) 2002-2004 Thomas E Enebo <enebo@acm.org>
+ * Copyright (C) 2004 Stefan Matthias Aust <sma@3plus4.de>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the EPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the EPL, the GPL or the LGPL.
  */
 package org.jruby.truffle.core;
 
@@ -53,8 +81,6 @@ public abstract class MathNodes {
 
         @Override
         protected double doFunction(double a) {
-            // Copied from RubyMath - see copyright notices there
-
             if (Double.isNaN(a)) {
                 return Double.NaN;
             } else if (a < 1) {
@@ -89,8 +115,6 @@ public abstract class MathNodes {
 
         @Override
         protected double doFunction(double a) {
-            // Copied from RubyMath - see copyright notices there
-
             final double y = Math.abs(a);
 
             if (Double.isNaN(a)) {
@@ -135,8 +159,6 @@ public abstract class MathNodes {
 
         @Override
         protected double doFunction(double a) {
-            // Copied from RubyMath - see copyright notices there
-
             if (a < -1.0 || a > 1.0) {
                 CompilerDirectives.transferToInterpreterAndInvalidate();
                 throw new RaiseException(coreExceptions().mathDomainError("atanh", this));
@@ -196,8 +218,6 @@ public abstract class MathNodes {
 
         @Override
         protected double doFunction(double a) {
-            // Copied from RubyMath - see copyright notices there
-
             final double y = Math.abs(a);
 
             if (y <= 1.49012e-08) {
@@ -224,8 +244,6 @@ public abstract class MathNodes {
         }
 
         public static double erfc(double a) {
-            // Copied from RubyMath - see copyright notices there
-
             final double y = Math.abs(a);
 
             if (a <= -6.013687357) {
@@ -293,8 +311,6 @@ public abstract class MathNodes {
 
         @Specialization
         public DynamicObject frexp(double a) {
-            // Copied from RubyMath - see copyright notices there
-
             double mantissa = a;
             short sign = 1;
             long exponent = 0;
@@ -333,8 +349,6 @@ public abstract class MathNodes {
 
         @Override
         protected double doFunction(double a) {
-            // Copied from RubyMath - see copyright notices there
-
             if (a == -1) {
                 CompilerDirectives.transferToInterpreterAndInvalidate();
                 throw new RaiseException(coreExceptions().mathDomainError("gamma", this));
@@ -511,8 +525,6 @@ public abstract class MathNodes {
 
         @Specialization
         public DynamicObject lgamma(double a) {
-            // Copied from RubyMath - see copyright notices there
-
             if (a < 0 && Double.isInfinite(a)) {
                 CompilerDirectives.transferToInterpreterAndInvalidate();
                 throw new RaiseException(coreExceptions().mathDomainError("log2", this));
