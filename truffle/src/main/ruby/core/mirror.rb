7b31609632602b351b5c9934a443cda52faca17b
Petr Chalupa <git@pitr.ch>
2016-06-13 09:20:08 +0200

    [Truffle] add back inspection methods in Mirror

--- truffle/src/main/ruby/core/mirror.rb
+++ truffle/src/main/ruby/core/mirror.rb
@@ -63,10 +63,16 @@ module Rubinius
       end
     end
 
+    def self.subject
+      @subject
+    end
+
     def self.reflect(obj)
       klass = module_mirror(obj)
       klass.new obj if klass
     end
+
+    attr_reader :object
 
     def initialize(obj)
       @object = obj
@@ -74,6 +80,10 @@ module Rubinius
 
     class Object < Mirror
       self.subject = ::Object
+    end
+
+    def inspect
+      "#<#{self.class.name}:0x#{self.object_id.to_s(16)} object=#{@object.inspect}>"
     end
   end
 end
