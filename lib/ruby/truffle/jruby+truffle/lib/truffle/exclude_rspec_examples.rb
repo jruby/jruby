29701fdf05b83385ea4dab48b50e6b6b1d441411
Petr Chalupa <git@pitr.ch>
2016-06-14 08:42:12 +0200

    [Truffle] jtr: allow missing tests when excluding known failures

--- lib/ruby/truffle/jruby+truffle/lib/truffle/exclude_rspec_examples.rb
+++ lib/ruby/truffle/jruby+truffle/lib/truffle/exclude_rspec_examples.rb
@@ -6,11 +6,16 @@ module Truffle
       a_module = Object.const_get mod_name.to_s
 
       Array(tests).each do |test|
-        puts "Excluding: #{a_module}##{test}"
-        a_module.send :undef_method, test
-        a_module.send :define_method, test do
-          skip 'excluded test'
+        print "Excluding: #{a_module}##{test}"
+        begin
+          a_module.send :undef_method, test
+          a_module.send :define_method, test do
+            skip 'excluded test'
+          end
+        rescue NameError => e
+          print ' (NOT FOUND)'
         end
+        puts
       end
 
     end
