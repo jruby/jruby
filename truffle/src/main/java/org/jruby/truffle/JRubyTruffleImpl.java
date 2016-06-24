3e8bd9c5dc506709ed4a9149fdcdb6bd74359f43
Chris Seaton <chris.seaton@oracle.com>
2016-06-24 06:21:40 +0100

    [Truffle] New Source API is stopping me testing against latest compilers.

--- truffle/src/main/java/org/jruby/truffle/JRubyTruffleImpl.java
+++ truffle/src/main/java/org/jruby/truffle/JRubyTruffleImpl.java
@@ -71,7 +71,8 @@ public class JRubyTruffleImpl implements JRubyTruffleInterface {
     }
 
     private Source loadSource(String source, String name) {
-        return Source.newBuilder(source).name(name).mimeType(RubyLanguage.MIME_TYPE).build();
+        return Source.fromText(source, name).withMimeType(RubyLanguage.MIME_TYPE);
+        //return Source.newBuilder(source).name(name).mimeType(RubyLanguage.MIME_TYPE).build();
     }
     
 }
