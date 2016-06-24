9b1a5a090d872d9e5d72c82ef3a6d7bf99dd73d3
kares <self@kares.org>
2016-06-24 08:20:32 +0200

    (c) 2016 ... 15 years on JRuby awesomeness and counting!

--- core/src/main/java/org/jruby/util/cli/OutputStrings.java
+++ core/src/main/java/org/jruby/util/cli/OutputStrings.java
@@ -144,6 +144,6 @@ public class OutputStrings {
     }
 
     public static String getCopyrightString() {
-        return "JRuby - Copyright (C) 2001-2013 The JRuby Community (and contribs)";
+        return "JRuby - Copyright (C) 2001-2016 The JRuby Community (and contribs)";
     }
 }
