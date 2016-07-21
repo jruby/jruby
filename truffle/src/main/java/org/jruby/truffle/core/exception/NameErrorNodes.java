acb2690ad0dc2a3edc73f4070d34bfe4c2856b33
Brandon Fish <brandon.fish@oracle.com>
2016-07-21 16:54:54 -0500

    [Truffle] NameError style and copyright updates

--- truffle/src/main/java/org/jruby/truffle/core/exception/NameErrorNodes.java
+++ truffle/src/main/java/org/jruby/truffle/core/exception/NameErrorNodes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2016 Oracle and/or its affiliates. All rights reserved. This
+ * Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved. This
  * code is released under a tri EPL/GPL/LGPL license. You can use it,
  * redistribute it and/or modify it under the terms of the:
  *
@@ -41,7 +41,7 @@ public abstract class NameErrorNodes {
 
             // TODO BJF July 21, 2016 Implement name error in message field
 
-            if(receiver == null){
+            if (receiver == null) {
                 throw new RaiseException(coreExceptions().argumentErrorNoReceiver(this));
             }
             return receiver;
