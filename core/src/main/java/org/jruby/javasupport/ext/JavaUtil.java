2e47a9d85326b155abe0bc2709f50378d2a003d6
kares <self@kares.org>
2016-07-22 14:45:58 +0200

    [ji] override Enumerable#include? for java.util.Collection
    ... and use contains(Object) directly as the (Enumerable) base impl
    loops over the collection making it highly non-effective (e.g. for sets)
    
    9.0.5.0
    ```
                     user     system      total        real
    ArrayList#include? hit  11.670000   1.150000  12.820000 ( 11.749035)
    ArrayList#include? miss  9.840000   0.080000   9.920000 (  9.707464)
    ArrayList#contains hit   0.730000   0.010000   0.740000 (  0.370432)
    ArrayList#contains miss  0.350000   0.000000   0.350000 (  0.267981)
    HashSet#include? hit    13.720000   0.020000  13.740000 ( 13.593915)
    HashSet#include? miss    9.610000   0.020000   9.630000 (  9.543065)
    HashSet#contains hit     0.410000   0.000000   0.410000 (  0.256134)
    HashSet#contains miss    0.290000   0.000000   0.290000 (  0.213409)
    LHashSet#include? hit   11.560000   0.020000  11.580000 ( 11.453787)
    LHashSet#include? miss  11.840000   0.010000  11.850000 ( 11.752156)
    LHashSet#contains hit    0.320000   0.000000   0.320000 (  0.227460)
    LHashSet#contains miss   0.310000   0.000000   0.310000 (  0.220113)
    ```
    
    before
    ```
                     user     system      total        real
    ArrayList#include? hit   3.060000   0.000000   3.060000 (  3.057502)
    ArrayList#include? miss  3.220000   0.010000   3.230000 (  3.232130)
    ArrayList#contains hit   0.310000   0.000000   0.310000 (  0.308007)
    ArrayList#contains miss  0.320000   0.000000   0.320000 (  0.313187)
    HashSet#include? hit     3.040000   0.000000   3.040000 (  3.036603)
    HashSet#include? miss    3.240000   0.010000   3.250000 (  3.237065)
    HashSet#contains hit     0.160000   0.000000   0.160000 (  0.152719)
    HashSet#contains miss    0.150000   0.000000   0.150000 (  0.154513)
    LHashSet#include? hit    3.000000   0.000000   3.000000 (  2.987810)
    LHashSet#include? miss   3.160000   0.000000   3.160000 (  3.165118)
    LHashSet#contains hit    0.160000   0.000000   0.160000 (  0.160286)
    LHashSet#contains miss   0.230000   0.000000   0.230000 (  0.156899)
    ```
    
    after
    ```
                     user     system      total        real
    ArrayList#include? hit   0.110000   0.000000   0.110000 (  0.098661)
    ArrayList#include? miss  0.090000   0.000000   0.090000 (  0.087545)
    ArrayList#contains hit   0.350000   0.000000   0.350000 (  0.352668)
    ArrayList#contains miss  0.350000   0.000000   0.350000 (  0.351055)
    HashSet#include? hit     0.070000   0.000000   0.070000 (  0.061353)
    HashSet#include? miss    0.060000   0.000000   0.060000 (  0.058946)
    HashSet#contains hit     0.180000   0.000000   0.180000 (  0.172549)
    HashSet#contains miss    0.170000   0.000000   0.170000 (  0.171588)
    LHashSet#include? hit    0.090000   0.000000   0.090000 (  0.071044)
    LHashSet#include? miss   0.070000   0.000000   0.070000 (  0.060303)
    LHashSet#contains hit    0.300000   0.000000   0.300000 (  0.208889)
    LHashSet#contains miss   0.170000   0.000000   0.170000 (  0.166072)
    ```

--- core/src/main/java/org/jruby/javasupport/ext/JavaUtil.java
+++ core/src/main/java/org/jruby/javasupport/ext/JavaUtil.java
@@ -137,6 +137,12 @@ public abstract class JavaUtil {
             return JavaLang.Iterable.each_with_index(context, self, block);
         }
 
+        @JRubyMethod(name = { "include?", "member?" })
+        public static RubyBoolean include_p(final ThreadContext context, final IRubyObject self, final IRubyObject obj) {
+            final java.util.Collection coll = unwrapJavaObject(self);
+            return context.runtime.newBoolean( coll.contains( obj.toJava(java.lang.Object.class) ) );
+        }
+
         // NOTE: first might conflict with some Java types (e.g. java.util.Deque) thus providing a ruby_ alias
         @JRubyMethod(name = { "first", "ruby_first" }) // re-def Enumerable#first
         public static IRubyObject first(final ThreadContext context, final IRubyObject self) {
