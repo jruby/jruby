d52fe8e15f15d81b31ae1922b9133a560a3d5d59
Chris Seaton <chris.seaton@oracle.com>
2016-07-28 14:55:53 +0100

    [Truffle] More info if the Asciidoctor test fails.

--- test/truffle/gems/asciidoctor.sh
+++ test/truffle/gems/asciidoctor.sh
@@ -9,6 +9,7 @@ ruby -X+T $GEM_HOME/gems/asciidoctor-1.5.4/bin/asciidoctor test/truffle/gems/asc
 if ! cmp --silent test/truffle/gems/asciidoctor/userguide.html test/truffle/gems/asciidoctor/userguide-expected.html
 then
   echo Asciidoctor output was not as expected
+  diff -u test/truffle/gems/asciidoctor/userguide-expected.html test/truffle/gems/asciidoctor/userguide.html
   rm -f test/truffle/gems/asciidoctor/userguide.html
   exit 1
 else
