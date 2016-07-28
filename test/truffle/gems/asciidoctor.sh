ba9c631cc2f45571017fe1296838aed3e4a3d734
Chris Seaton <chris.seaton@oracle.com>
2016-07-28 12:02:53 +0100

    [Truffle] Asciidoctor test.

---
+++ test/truffle/gems/asciidoctor.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+GEM_HOME=${GEM_HOME:-lib/ruby/gems/shared}
+
+ruby -X+T $GEM_HOME/gems/asciidoctor-1.5.4/bin/asciidoctor test/truffle/gems/asciidoctor/userguide.adoc
+
+if ! cmp --silent test/truffle/gems/asciidoctor/userguide.html test/truffle/gems/asciidoctor/userguide-expected.html
+then
+  echo Asciidoctor output was not as expected
+  rm -f test/truffle/gems/asciidoctor/userguide.html
+  exit 1
+else
+  rm -f test/truffle/gems/asciidoctor/userguide.html
+fi
