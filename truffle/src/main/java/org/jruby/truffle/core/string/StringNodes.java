a8c28306e2b5d294e938d13e9cd0bef5b839a31c
Benoit Daloze <eregontp@gmail.com>
2016-07-30 11:18:40 +0200

    [Truffle] Make sure the codepoint value is not larger than an unsigned int.

--- truffle/src/main/java/org/jruby/truffle/core/string/StringNodes.java
+++ truffle/src/main/java/org/jruby/truffle/core/string/StringNodes.java
@@ -3218,7 +3218,7 @@ public abstract class StringNodes {
         }
 
         @TruffleBoundary(throwsControlFlowException = true)
-        @Specialization(guards = {"isRubyEncoding(rubyEncoding)", "!isSimple(code, rubyEncoding)"})
+        @Specialization(guards = { "isRubyEncoding(rubyEncoding)", "!isSimple(code, rubyEncoding)", "isCodepoint(code)" })
         public DynamicObject stringFromCodepoint(long code, DynamicObject rubyEncoding) {
             final Encoding encoding = EncodingOperations.getEncoding(rubyEncoding);
             final int length;
@@ -3246,6 +3246,11 @@ public abstract class StringNodes {
             }
 
             return createString(RopeOperations.create(bytes, encoding, CodeRange.CR_VALID));
+        }
+
+        protected boolean isCodepoint(long code) {
+            // Fits in an unsigned int
+            return code >= 0 && code < (1L << 32);
         }
 
         protected boolean isSimple(long code, DynamicObject encoding) {
