29081b79cb1efe3bce7829d46f497b0a738e9a3d
Benoit Daloze <eregontp@gmail.com>
2016-08-01 20:48:46 +0200

    [Truffle] Allow the YAML parser to compile.

---
+++ truffle/src/main/java/org/jruby/truffle/util/BoundaryUtils.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved. This
+ * code is released under a tri EPL/GPL/LGPL license. You can use it,
+ * redistribute it and/or modify it under the terms of the:
+ *
+ * Eclipse Public License version 1.0
+ * GNU General Public License version 2
+ * GNU Lesser General Public License version 2.1
+ */
+package org.jruby.truffle.util;
+
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import java.util.Iterator;
+
+public class BoundaryUtils {
+
+    public static class BoundaryIterable<E> implements Iterable<E> {
+
+        public static <E> Iterable<E> wrap(Iterable<E> iterable) {
+            return new BoundaryIterable<>(iterable);
+        }
+
+        private final Iterable<E> iterable;
+
+        public BoundaryIterable(Iterable<E> iterable) {
+            this.iterable = iterable;
+        }
+
+        @Override
+        public Iterator<E> iterator() {
+            return new BoundaryIterator<E>(iterable.iterator());
+        }
+    }
+
+    public static class BoundaryIterator<E> implements Iterator<E> {
+
+        private final Iterator<E> iterator;
+
+        public BoundaryIterator(Iterator<E> iterator) {
+            this.iterator = iterator;
+        }
+
+        @TruffleBoundary
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @TruffleBoundary
+        @Override
+        public E next() {
+            return iterator.next();
+        }
+
+    }
+
+}
