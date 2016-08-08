1e8c65aed1e3d4b316a3214cdfa47658569ce19b
Thomas E. Enebo <tom.enebo@gmail.com>
2016-08-08 16:27:44 -0500

    Spec out Array#max a tiny bit

--- spec/ruby/core/array/max_spec.rb
+++ spec/ruby/core/array/max_spec.rb
@@ -1,6 +1,19 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "Array#max" do
+  it "returns nil with no values" do
+    [].max.should == nil
+  end
+
+  it "returns only element in one element array" do
+    [1].max.should == 1
+  end
+
+  it "returns largest value with multiple elements" do
+    [1,2].max.should == 2
+    [2,1].max.should == 2
+  end
+  
   describe "given a block with one argument" do
     it "yields in turn the last length-1 values from the array" do
       ary = []
