91ff2b85395de6f79979ae1d942246bae494fee5
kares <self@kares.org>
2016-08-13 21:45:55 +0200

    [ji] move Class#java_class internal impl to Java (for potential re-use)
    ... should be also faster + matters very little if its not Ruby-friendly

--- core/src/main/java/org/jruby/javasupport/Java.java
+++ core/src/main/java/org/jruby/javasupport/Java.java
@@ -79,6 +79,7 @@ import org.jruby.internal.runtime.methods.DynamicMethod;
 import org.jruby.internal.runtime.methods.JavaMethod.JavaMethodN;
 import org.jruby.internal.runtime.methods.JavaMethod.JavaMethodZero;
 import org.jruby.java.addons.ArrayJavaAddons;
+import org.jruby.java.addons.ClassJavaAddons;
 import org.jruby.java.addons.IOJavaAddons;
 import org.jruby.java.addons.KernelJavaAddons;
 import org.jruby.java.addons.StringJavaAddons;
@@ -187,6 +188,7 @@ public class Java implements Library {
         runtime.getKernel().defineAnnotatedMethods(KernelJavaAddons.class);
         runtime.getString().defineAnnotatedMethods(StringJavaAddons.class);
         runtime.getIO().defineAnnotatedMethods(IOJavaAddons.class);
+        runtime.getClassClass().defineAnnotatedMethods(ClassJavaAddons.class);
 
         if ( runtime.getObject().isConstantDefined("StringIO") ) {
             ((RubyClass) runtime.getObject().getConstant("StringIO")).defineAnnotatedMethods(IOJavaAddons.AnyIO.class);
