b4d13bf0e7d05cb15504c310474ac08c3680e4dd
kares <self@kares.org>
2016-08-26 09:27:38 +0200

    [ji] tidy-up Java field reader/writer impls - use same exception types
    - for IllegalAccessException on get/set now always map to SecurityError
    - IllegalArgumentException on field set mapped as TypeError (as before)

--- core/src/main/java/org/jruby/java/invokers/FieldMethodOne.java
+++ core/src/main/java/org/jruby/java/invokers/FieldMethodOne.java
@@ -1,20 +1,40 @@
 package org.jruby.java.invokers;
 
 import java.lang.reflect.Field;
+
 import org.jruby.Ruby;
 import org.jruby.RubyModule;
-import org.jruby.internal.runtime.methods.JavaMethod.JavaMethodOne;
+import org.jruby.internal.runtime.methods.JavaMethod;
 import org.jruby.runtime.Visibility;
+import org.jruby.runtime.builtin.IRubyObject;
 
-public abstract class FieldMethodOne extends JavaMethodOne {
+/**
+ * Java field setter (writer) base implementation e.g. `self.myField = value`.
+ */
+public abstract class FieldMethodOne extends JavaMethod.JavaMethodOne {
 
     final Field field;
 
     FieldMethodOne(String name, RubyModule host, Field field) {
+        this(host, field);
+    }
+
+    protected FieldMethodOne(RubyModule host, Field field) {
         super(host, Visibility.PUBLIC);
-        if (!Ruby.isSecurityRestricted()) {
-            field.setAccessible(true);
-        }
+        if ( ! Ruby.isSecurityRestricted() ) field.setAccessible(true);
         this.field = field;
     }
+
+    protected Object retrieveTarget(final IRubyObject self) {
+        return FieldMethodZero.retrieveTargetImpl(self);
+    }
+
+    protected final IRubyObject handleSetException(final Ruby runtime, final IllegalAccessException ex) {
+        throw runtime.newSecurityError("illegal access setting field '" + field + "' : " + ex.getMessage());
+    }
+
+    protected final IRubyObject handleSetException(final Ruby runtime, final IllegalArgumentException ex) {
+        throw runtime.newTypeError(ex.getMessage());
+    }
+
 }
