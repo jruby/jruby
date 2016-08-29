0c345e1b186bd457ebd96143c0816abe93b18fdf
Charles Oliver Nutter <headius@headius.com>
2016-08-29 12:09:19 -0500

    Don't try to read inaccessible field before setting it accessible.
    This also attempts to make this hackery happen only once, in case
    this logic is called from many places (e.g. many JRuby instances
    in the same JVM).
    
    See #4101.

--- core/src/main/java/org/jruby/util/SecurityHelper.java
+++ core/src/main/java/org/jruby/util/SecurityHelper.java
@@ -10,11 +10,13 @@ import java.lang.reflect.Modifier;
 public abstract class SecurityHelper {
 
     private static final Logger LOG = LoggerFactory.getLogger(SecurityHelper.class);
+    private static boolean attempted = false;
 
     // attempt to enable unlimited-strength crypto on OracleJDK
     public static void checkCryptoRestrictions(final Ruby runtime) {
-        if ( isOracleJRE() ) {
+        if ( isOracleJRE() && !attempted) {
             setNonRestricted();
+            attempted = true;
             // NOTE: this is not 'really' enough and there's more to be done :
             // JceSecurity#defaultPolicy should add: javax.crypto.CryptoAllPermission
             //
@@ -27,17 +29,18 @@ public abstract class SecurityHelper {
         try {
             Class jceSecurity = Class.forName("javax.crypto.JceSecurity");
             Field isRestricted = jceSecurity.getDeclaredField("isRestricted");
-            if ( Boolean.TRUE.equals(isRestricted.get(null)) ) {
-                if ( Modifier.isFinal(isRestricted.getModifiers()) ) {
-                    Field modifiers = Field.class.getDeclaredField("modifiers");
-                    modifiers.setAccessible(true);
-                    modifiers.setInt(isRestricted, isRestricted.getModifiers() & ~Modifier.FINAL);
-                }
-                isRestricted.setAccessible(true);
-                isRestricted.setBoolean(null, false); // isRestricted = false;
-                isRestricted.setAccessible(false);
-                return true;
+
+            if ( Modifier.isFinal(isRestricted.getModifiers()) ) {
+                Field modifiers = Field.class.getDeclaredField("modifiers");
+                modifiers.setAccessible(true);
+                modifiers.setInt(isRestricted, isRestricted.getModifiers() & ~Modifier.FINAL);
             }
+
+            isRestricted.setAccessible(true);
+            isRestricted.setBoolean(null, false); // isRestricted = false;
+            isRestricted.setAccessible(false);
+
+            return true;
         }
         catch (ClassNotFoundException e) {
             LOG.info("unable un-restrict jce security: " + e);
