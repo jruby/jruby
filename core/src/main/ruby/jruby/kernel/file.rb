01030ab2f5f3b2ce79f24d201a5555f046a8ec24
Charles Oliver Nutter <headius@headius.com>
2016-09-20 15:08:22 -0500

    Move Solaris flock struct to ffi platform files. #4162

--- core/src/main/ruby/jruby/kernel/file.rb
+++ core/src/main/ruby/jruby/kernel/file.rb
@@ -184,20 +184,12 @@ if org.jruby.platform.Platform::IS_SOLARIS
   if defined?(::FFI)
 
     require 'ffi/platform/x86_64-solaris/fcntl'
+    require 'ffi/platform/x86_64-solaris/fcntl-flock'
+
     module Fcntl
       extend FFI::Library
       ffi_lib 'c'
       attach_function :fcntl, [:short, :short, :varargs], :int
-      class Flock < FFI::Struct
-        self.size = 44
-        layout :l_type, :short, 0,
-               :l_whence, :short, 2,
-               :l_start, :off_t, 4,
-               :l_len, :off_t, 12,
-               :l_sysid, :int, 20,
-               :l_pid, :int, 24,
-               :l_pad, :int, 28
-      end
     end
 
     class File
