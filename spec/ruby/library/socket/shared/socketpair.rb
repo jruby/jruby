303e5d837b166d2cae404cfb027b8752c2dde1ca
Konstantin Shabanov <mail@etehtsea.me>
2016-10-05 22:10:49 +0700

    Fix Socket#socketpair shared spec

--- spec/ruby/library/socket/shared/socketpair.rb
+++ spec/ruby/library/socket/shared/socketpair.rb
@@ -1,7 +1,7 @@
 describe :socket_socketpair, shared: true do
   platform_is_not :windows do
     it "ensures the returned sockets are connected" do
-      s1, s2 = Socket.socketpair(Socket::AF_UNIX, 1, 0)
+      s1, s2 = Socket.public_send(@method, Socket::AF_UNIX, 1, 0)
       s1.puts("test")
       s2.gets.should == "test\n"
       s1.close
@@ -9,7 +9,7 @@ describe :socket_socketpair, shared: true do
     end
 
     it "responses with array of two sockets" do
-      s1, s2 = Socket.socketpair(:UNIX, :STREAM)
+      s1, s2 = Socket.public_send(@method, :UNIX, :STREAM)
 
       s1.should be_an_instance_of(Socket)
       s2.should be_an_instance_of(Socket)
