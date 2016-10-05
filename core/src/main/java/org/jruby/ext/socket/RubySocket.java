cb3a35df9b3f40d008f60fe51aae88a723841c37
Konstantin Shabanov <mail@etehtsea.me>
2016-10-05 13:40:20 +0700

    Fix Socket#socketpair
    sock2 was left uninitialized

--- core/src/main/java/org/jruby/ext/socket/RubySocket.java
+++ core/src/main/java/org/jruby/ext/socket/RubySocket.java
@@ -388,21 +388,19 @@ public class RubySocket extends RubyBasicSocket {
 
         try {
             sp = UnixSocketChannel.pair();
-
             final RubyClass socketClass = runtime.getClass("Socket");
-            RubySocket sock = new RubySocket(runtime, socketClass);
-            sock.soDomain = AddressFamily.AF_UNIX;
-            sock.soType = Sock.SOCK_STREAM;
-            sock.soProtocolFamily = ProtocolFamily.PF_UNIX;
-            sock.initSocket(newChannelFD(runtime, sp[0]));
 
-            RubySocket sock2 = new RubySocket(runtime, socketClass);
-            sock2.soDomain = AddressFamily.AF_UNIX;
-            sock2.soType = Sock.SOCK_STREAM;
-            sock2.soProtocolFamily = ProtocolFamily.PF_UNIX;
-            sock.initSocket(newChannelFD(runtime, sp[1]));
+            RubySocket sock0 = new RubySocket(runtime, socketClass);
+            ChannelFD fd0 = newChannelFD(runtime, sp[0]);
+            sock0.initFieldsFromDescriptor(runtime, fd0);
+            sock0.initSocket(fd0);
 
-            return runtime.newArray(sock, sock2);
+            RubySocket sock1 = new RubySocket(runtime, socketClass);
+            ChannelFD fd1 = newChannelFD(runtime, sp[1]);
+            sock1.initFieldsFromDescriptor(runtime, fd1);
+            sock1.initSocket(fd1);
+
+            return runtime.newArray(sock0, sock1);
 
         } catch (IOException ioe) {
             throw runtime.newIOErrorFromException(ioe);
