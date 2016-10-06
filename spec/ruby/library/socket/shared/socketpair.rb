3dfa50510b4beeb42b76957d7ef739ad65ae96b8
Charles Oliver Nutter <headius@headius.com>
2016-10-06 14:18:37 -0500

    Fix typo in socketpair_spec.rb.

--- spec/ruby/library/socket/shared/socketpair.rb
+++ spec/ruby/library/socket/shared/socketpair.rb
@@ -9,13 +9,15 @@ describe :socket_socketpair, shared: true do
     end
 
     it "responses with array of two sockets" do
-      s1, s2 = Socket.public_send(@method, :UNIX, :STREAM)
+      begin
+        s1, s2 = Socket.public_send(@method, :UNIX, :STREAM)
 
-      s1.should be_an_instance_of(Socket)
-      s2.should be_an_instance_of(Socket)
-    ensure
-      s1.close
-      s2.close
+        s1.should be_an_instance_of(Socket)
+        s2.should be_an_instance_of(Socket)
+      ensure
+        s1.close
+        s2.close
+      end
     end
   end
 end
