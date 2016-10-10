9d880cf67366bc735368dc2a80871356b4befa85
Chris Seaton <chris.seaton@oracle.com>
2016-10-10 16:51:48 +0100

    [Truffle] Switch to GraalVM 0.17

--- tool/jt.rb
+++ tool/jt.rb
@@ -19,7 +19,7 @@ require 'timeout'
 require 'yaml'
 require 'open3'
 
-GRAALVM_VERSION = '0.16'
+GRAALVM_VERSION = '0.17'
 
 JRUBY_DIR = File.expand_path('../..', __FILE__)
 M2_REPO = File.expand_path('~/.m2/repository')
