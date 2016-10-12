jruby/jruby

commit cc79119315496ea019496affe7a6a79ebc1dbed9

Fix altered behavior for UNIXServer#accept_nonblock.
This broke due to fixing jnr-unixsocket to return null on failed
nonblocking accept.

core/src/main/java/org/jruby/ext/socket/RubyUNIXServer.java
@@ -129,6 +129,8 @@ public class RubyUNIXServer extends RubyUNIXSocket {
                 try {
                     UnixSocketChannel socketChannel = ((UnixServerSocketChannel) selectable).accept();
 
+                    if (socketChannel == null) throw runtime.newErrnoEAGAINReadableError("accept(2) would block");
+
                     RubyUNIXSocket sock = (RubyUNIXSocket)(Helpers.invoke(context, runtime.getClass("UNIXSocket"), "allocate"));
 
                     sock.init_sock(context.runtime, socketChannel, "");

commit 57e672c3ab47022fe0d3e1add313918ccd4492cf

Fix UNIXServer#accept_nonblock(exception: false)
Introduced in cc79119

core/src/main/java/org/jruby/ext/socket/RubyUNIXServer.java
@@ -129,7 +129,10 @@ public class RubyUNIXServer extends RubyUNIXSocket {
                 try {
                     UnixSocketChannel socketChannel = ((UnixServerSocketChannel) selectable).accept();
 
-                    if (socketChannel == null) throw runtime.newErrnoEAGAINReadableError("accept(2) would block");
+                    if (socketChannel == null) {
+                        if (!ex) return runtime.newSymbol("wait_readable");
+                        throw runtime.newErrnoEAGAINReadableError("accept(2) would block");
+                    }
 
                     RubyUNIXSocket sock = (RubyUNIXSocket)(Helpers.invoke(context, runtime.getClass("UNIXSocket"), "allocate"));
 