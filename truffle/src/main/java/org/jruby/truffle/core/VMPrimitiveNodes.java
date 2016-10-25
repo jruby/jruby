ae5a1831d9db25b339097f318a66d1a2728e3687
Petr Chalupa <git@pitr.ch>
2016-10-25 11:30:12 +0200

    [Truffle] implement vm_set_process_title primitive as no-op

--- truffle/src/main/java/org/jruby/truffle/core/VMPrimitiveNodes.java
+++ truffle/src/main/java/org/jruby/truffle/core/VMPrimitiveNodes.java
@@ -590,5 +590,16 @@ public abstract class VMPrimitiveNodes {
 
     }
 
+    @Primitive(name = "vm_set_process_title", needsSelf = false)
+    public abstract static class VMSetProcessTitlePrimitiveNode extends PrimitiveArrayArgumentsNode {
+
+        @Specialization(guards = "isRubyString(title)")
+        public DynamicObject setClass(DynamicObject title) {
+            // no-op
+            return title;
+        }
+
+    }
+
 
 }
