37584e0da3276e7d6bb0c864f0406122f5326b9d
Benoit Daloze <eregontp@gmail.com>
2016-10-29 14:15:51 +0200

    Require squiggly_heredoc only in examples so they can easily be tagged and the file be loaded
    * When the parser does not support squiggly heredoc fully.

--- spec/ruby/language/heredoc_spec.rb
+++ spec/ruby/language/heredoc_spec.rb
@@ -54,29 +54,33 @@ HERE
   end
 
   ruby_version_is "2.3" do
-    require File.expand_path('../fixtures/squiggly_heredoc', __FILE__)
-
     it "allows HEREDOC with <<~'identifier', allowing to indent identifier and content" do
+      require File.expand_path('../fixtures/squiggly_heredoc', __FILE__)
       SquigglyHeredocSpecs.message.should == "character density, n.:\n  The number of very weird people in the office.\n"
     end
 
     it "trims trailing newline character for blank HEREDOC with <<~'identifier'" do
+      require File.expand_path('../fixtures/squiggly_heredoc', __FILE__)
       SquigglyHeredocSpecs.blank.should == ""
     end
 
     it 'allows HEREDOC with <<~identifier, interpolated' do
+      require File.expand_path('../fixtures/squiggly_heredoc', __FILE__)
       SquigglyHeredocSpecs.unquoted.should == "unquoted interpolated\n"
     end
 
     it 'allows HEREDOC with <<"identifier", interpolated' do
+      require File.expand_path('../fixtures/squiggly_heredoc', __FILE__)
       SquigglyHeredocSpecs.doublequoted.should == "doublequoted interpolated\n"
     end
 
     it "allows HEREDOC with <<'identifier', no interpolation" do
+      require File.expand_path('../fixtures/squiggly_heredoc', __FILE__)
       SquigglyHeredocSpecs.singlequoted.should == "singlequoted \#{\"interpolated\"}\n"
     end
 
     it "selects the least-indented line and removes its indentation from all the lines" do
+      require File.expand_path('../fixtures/squiggly_heredoc', __FILE__)
       SquigglyHeredocSpecs.least_indented_on_the_last_line.should == "    a\n  b\nc\n"
     end
   end
