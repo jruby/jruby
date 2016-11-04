6583f64b5bc25295797acc566fb6a0c3f401a975
Chris Seaton <chris.seaton@oracle.com>
2016-11-04 13:47:07 +0900

    [Truffle] Remove use of classic context context in interpolated regexp node.

--- truffle/src/main/java/org/jruby/truffle/core/regexp/ClassicRegexp.java
+++ truffle/src/main/java/org/jruby/truffle/core/regexp/ClassicRegexp.java
@@ -38,6 +38,7 @@ package org.jruby.truffle.core.regexp;
 import org.jcodings.Encoding;
 import org.jcodings.specific.ASCIIEncoding;
 import org.jcodings.specific.USASCIIEncoding;
+import org.jcodings.specific.UTF8Encoding;
 import org.joni.Matcher;
 import org.joni.NameEntry;
 import org.joni.Option;
@@ -72,8 +73,10 @@ import org.jruby.runtime.encoding.EncodingCapable;
 import org.jruby.runtime.encoding.MarshalEncoding;
 import org.jruby.runtime.marshal.MarshalStream;
 import org.jruby.runtime.marshal.UnmarshalStream;
+import org.jruby.truffle.RubyContext;
 import org.jruby.util.ByteList;
 import org.jruby.util.KCode;
+import org.jruby.util.Pack;
 import org.jruby.util.RegexpOptions;
 import org.jruby.util.RegexpSupport;
 import org.jruby.util.StringSupport;
@@ -82,11 +85,15 @@ import org.jruby.util.cli.Options;
 import org.jruby.util.collections.WeakValuedMap;
 import org.jruby.util.io.EncodingUtils;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 
 import static org.jruby.anno.FrameField.BACKREF;
 import static org.jruby.anno.FrameField.LASTLINE;
+import static org.jruby.util.StringSupport.CR_BROKEN;
+import static org.jruby.util.StringSupport.CR_UNKNOWN;
 import static org.jruby.util.StringSupport.EMPTY_STRING_ARRAY;
+import static org.jruby.util.StringSupport.codeRangeScan;
 
 public class ClassicRegexp extends RubyObject implements ReOptions, EncodingCapable, MarshalEncoding {
     private Regex pattern;
@@ -487,6 +494,311 @@ public class ClassicRegexp extends RubyObject implements ReOptions, EncodingCapa
         if (hasProperty && fixedEnc[0] == null) fixedEnc[0] = enc;
     }
 
+    private static void preprocessLight(RubyContext context, ByteList str, Encoding enc, Encoding[]fixedEnc, RegexpSupport.ErrorMode mode) {
+        if (enc.isAsciiCompatible()) {
+            fixedEnc[0] = null;
+        } else {
+            fixedEnc[0] = enc;
+        }
+
+        boolean hasProperty = unescapeNonAscii(context, null, str.getUnsafeBytes(), str.getBegin(), str.getBegin() + str.getRealSize(), enc, fixedEnc, str, mode);
+        if (hasProperty && fixedEnc[0] == null) fixedEnc[0] = enc;
+    }
+
+    public static boolean unescapeNonAscii(RubyContext context, ByteList to, byte[] bytes, int p, int end, Encoding enc, Encoding[] encp, ByteList str, RegexpSupport.ErrorMode mode) {
+        boolean hasProperty = false;
+        byte[] buf = null;
+
+        while (p < end) {
+            int cl = StringSupport.preciseLength(enc, bytes, p, end);
+            if (cl <= 0) raisePreprocessError(context, str, "invalid multibyte character", mode);
+            if (cl > 1 || (bytes[p] & 0x80) != 0) {
+                if (to != null) to.append(bytes, p, cl);
+                p += cl;
+                if (encp[0] == null) {
+                    encp[0] = enc;
+                } else if (encp[0] != enc) {
+                    raisePreprocessError(context, str, "non ASCII character in UTF-8 regexp", mode);
+                }
+                continue;
+            }
+            int c;
+            switch (c = bytes[p++] & 0xff) {
+                case '\\':
+                    if (p == end) raisePreprocessError(context, str, "too short escape sequence", mode);
+
+                    switch (c = bytes[p++] & 0xff) {
+                        case '1': case '2': case '3':
+                        case '4': case '5': case '6': case '7': /* \O, \OO, \OOO or backref */
+                            if (StringSupport.scanOct(bytes, p - 1, end - (p - 1)) <= 0177) {
+                                if (to != null) to.append('\\').append(c);
+                                break;
+                            }
+
+                        case '0': /* \0, \0O, \0OO */
+                        case 'x': /* \xHH */
+                        case 'c': /* \cX, \c\M-X */
+                        case 'C': /* \C-X, \C-\M-X */
+                        case 'M': /* \M-X, \M-\C-X, \M-\cX */
+                            p -= 2;
+                            if (enc == USASCIIEncoding.INSTANCE) {
+                                if (buf == null) buf = new byte[1];
+                                int pbeg = p;
+                                p = readEscapedByte(context, buf, 0, bytes, p, end, str, mode);
+                                c = buf[0];
+                                if (c == (char)-1) return false;
+                                if (to != null) {
+                                    to.append(bytes, pbeg, p - pbeg);
+                                }
+                            }
+                            else {
+                                p = unescapeEscapedNonAscii(context, to, bytes, p, end, enc, encp, str, mode);
+                            }
+                            break;
+
+                        case 'u':
+                            if (p == end) raisePreprocessError(context, str, "too short escape sequence", mode);
+                            if (bytes[p] == (byte)'{') { /* \\u{H HH HHH HHHH HHHHH HHHHHH ...} */
+                                p++;
+                                p = unescapeUnicodeList(context, to, bytes, p, end, encp, str, mode);
+                                if (p == end || bytes[p++] != (byte)'}') raisePreprocessError(context, str, "invalid Unicode list", mode);
+                            } else { /* \\uHHHH */
+                                p = unescapeUnicodeBmp(context, to, bytes, p, end, encp, str, mode);
+                            }
+                            break;
+                        case 'p': /* \p{Hiragana} */
+                            if (encp[0] == null) hasProperty = true;
+                            if (to != null) to.append('\\').append(c);
+                            break;
+
+                        default:
+                            if (to != null) to.append('\\').append(c);
+                            break;
+                    } // inner switch
+                    break;
+
+                default:
+                    if (to != null) to.append(c);
+            } // switch
+        } // while
+        return hasProperty;
+    }
+
+    private static int unescapeUnicodeBmp(RubyContext context, ByteList to, byte[] bytes, int p, int end, Encoding[] encp, ByteList str, RegexpSupport.ErrorMode mode) {
+        if (p + 4 > end) raisePreprocessError(context, str, "invalid Unicode escape", mode);
+        int code = StringSupport.scanHex(bytes, p, 4);
+        int len = StringSupport.hexLength(bytes, p, 4);
+        if (len != 4) raisePreprocessError(context, str, "invalid Unicode escape", mode);
+        appendUtf8(context, to, code, encp, str, mode);
+        return p + 4;
+    }
+
+    private static int unescapeUnicodeList(RubyContext context, ByteList to, byte[]bytes, int p, int end, Encoding[]encp, ByteList str, RegexpSupport.ErrorMode mode) {
+        while (p < end && ASCIIEncoding.INSTANCE.isSpace(bytes[p] & 0xff)) p++;
+
+        boolean hasUnicode = false;
+        while (true) {
+            int code = StringSupport.scanHex(bytes, p, end - p);
+            int len = StringSupport.hexLength(bytes, p, end - p);
+            if (len == 0) break;
+            if (len > 6) raisePreprocessError(context, str, "invalid Unicode range", mode);
+            p += len;
+            if (to != null) appendUtf8(context, to, code, encp, str, mode);
+            hasUnicode = true;
+            while (p < end && ASCIIEncoding.INSTANCE.isSpace(bytes[p] & 0xff)) p++;
+        }
+
+        if (!hasUnicode) raisePreprocessError(context, str, "invalid Unicode list", mode);
+        return p;
+    }
+
+    private static void appendUtf8(RubyContext context, ByteList to, int code, Encoding[] enc, ByteList str, RegexpSupport.ErrorMode mode) {
+        checkUnicodeRange(context, code, str, mode);
+
+        if (code < 0x80) {
+            if (to != null) to.append(String.format("\\x%02X", code).getBytes(StandardCharsets.US_ASCII));
+        } else {
+            if (to != null) {
+                to.ensure(to.getRealSize() + 6);
+                to.setRealSize(to.getRealSize() + utf8Decode(context, to.getUnsafeBytes(), to.getBegin() + to.getRealSize(), code));
+            }
+            if (enc[0] == null) {
+                enc[0] = UTF8Encoding.INSTANCE;
+            } else if (!(enc[0].isUTF8())) {
+                raisePreprocessError(context, str, "UTF-8 character in non UTF-8 regexp", mode);
+            }
+        }
+    }
+
+    public static int utf8Decode(RubyContext context, byte[]to, int p, int code) {
+        if (code <= 0x7f) {
+            to[p] = (byte)code;
+            return 1;
+        }
+        if (code <= 0x7ff) {
+            to[p + 0] = (byte)(((code >>> 6) & 0xff) | 0xc0);
+            to[p + 1] = (byte)((code & 0x3f) | 0x80);
+            return 2;
+        }
+        if (code <= 0xffff) {
+            to[p + 0] = (byte)(((code >>> 12) & 0xff) | 0xe0);
+            to[p + 1] = (byte)(((code >>> 6) & 0x3f) | 0x80);
+            to[p + 2] = (byte)((code & 0x3f) | 0x80);
+            return 3;
+        }
+        if (code <= 0x1fffff) {
+            to[p + 0] = (byte)(((code >>> 18) & 0xff) | 0xf0);
+            to[p + 1] = (byte)(((code >>> 12) & 0x3f) | 0x80);
+            to[p + 2] = (byte)(((code >>> 6) & 0x3f) | 0x80);
+            to[p + 3] = (byte)((code & 0x3f) | 0x80);
+            return 4;
+        }
+        if (code <= 0x3ffffff) {
+            to[p + 0] = (byte)(((code >>> 24) & 0xff) | 0xf8);
+            to[p + 1] = (byte)(((code >>> 18) & 0x3f) | 0x80);
+            to[p + 2] = (byte)(((code >>> 12) & 0x3f) | 0x80);
+            to[p + 3] = (byte)(((code >>> 6) & 0x3f) | 0x80);
+            to[p + 4] = (byte)((code & 0x3f) | 0x80);
+            return 5;
+        }
+        if (code <= 0x7fffffff) {
+            to[p + 0] = (byte)(((code >>> 30) & 0xff) | 0xfc);
+            to[p + 1] = (byte)(((code >>> 24) & 0x3f) | 0x80);
+            to[p + 2] = (byte)(((code >>> 18) & 0x3f) | 0x80);
+            to[p + 3] = (byte)(((code >>> 12) & 0x3f) | 0x80);
+            to[p + 4] = (byte)(((code >>> 6) & 0x3f) | 0x80);
+            to[p + 5] = (byte)((code & 0x3f) | 0x80);
+            return 6;
+        }
+        throw new org.jruby.truffle.language.control.RaiseException(context.getCoreExceptions().rangeError("pack(U): value out of range", null));
+    }
+
+    private static void checkUnicodeRange(RubyContext context, int code, ByteList str, RegexpSupport.ErrorMode mode) {
+        // Unicode is can be only 21 bits long, int is enough
+        if ((0xd800 <= code && code <= 0xdfff) /* Surrogates */ || 0x10ffff < code) {
+            raisePreprocessError(context, str, "invalid Unicode range", mode);
+        }
+    }
+
+    private static int unescapeEscapedNonAscii(RubyContext context, ByteList to, byte[]bytes, int p, int end, Encoding enc, Encoding[]encp, ByteList str, RegexpSupport.ErrorMode mode) {
+        byte[]chBuf = new byte[enc.maxLength()];
+        int chLen = 0;
+
+        p = readEscapedByte(context, chBuf, chLen++, bytes, p, end, str, mode);
+        while (chLen < enc.maxLength() && StringSupport.MBCLEN_NEEDMORE_P(StringSupport.preciseLength(enc, chBuf, 0, chLen))) {
+            p = readEscapedByte(context, chBuf, chLen++, bytes, p, end, str, mode);
+        }
+
+        int cl = StringSupport.preciseLength(enc, chBuf, 0, chLen);
+        if (cl == -1) {
+            raisePreprocessError(context, str, "invalid multibyte escape", mode); // MBCLEN_INVALID_P
+        }
+
+        if (chLen > 1 || (chBuf[0] & 0x80) != 0) {
+            if (to != null) to.append(chBuf, 0, chLen);
+
+            if (encp[0] == null) {
+                encp[0] = enc;
+            } else if (encp[0] != enc) {
+                raisePreprocessError(context, str, "escaped non ASCII character in UTF-8 regexp", mode);
+            }
+        } else {
+            if (to != null) to.append(String.format("\\x%02X", chBuf[0] & 0xff).getBytes(StandardCharsets.US_ASCII));
+        }
+        return p;
+    }
+
+    public static int raisePreprocessError(RubyContext context, ByteList str, String err, RegexpSupport.ErrorMode mode) {
+        switch (mode) {
+            case RAISE:
+                throw new org.jruby.truffle.language.control.RaiseException(context.getCoreExceptions().regexpError(err, null));
+            case PREPROCESS:
+                throw new org.jruby.truffle.language.control.RaiseException(context.getCoreExceptions().argumentError("regexp preprocess failed: " + err, null));
+            case DESC:
+                // silent ?
+        }
+        return 0;
+    }
+
+    public static int readEscapedByte(RubyContext context, byte[] to, int toP, byte[] bytes, int p, int end, ByteList str, RegexpSupport.ErrorMode mode) {
+        if (p == end || bytes[p++] != (byte)'\\') raisePreprocessError(context, str, "too short escaped multibyte character", mode);
+
+        boolean metaPrefix = false, ctrlPrefix = false;
+        int code = 0;
+        while (true) {
+            if (p == end) raisePreprocessError(context, str, "too short escape sequence", mode);
+
+            switch (bytes[p++]) {
+                case '\\': code = '\\'; break;
+                case 'n': code = '\n'; break;
+                case 't': code = '\t'; break;
+                case 'r': code = '\r'; break;
+                case 'f': code = '\f'; break;
+                case 'v': code = '\013'; break;
+                case 'a': code = '\007'; break;
+                case 'e': code = '\033'; break;
+
+            /* \OOO */
+                case '0': case '1': case '2': case '3':
+                case '4': case '5': case '6': case '7':
+                    p--;
+                    int olen = end < p + 3 ? end - p : 3;
+                    code = StringSupport.scanOct(bytes, p, olen);
+                    p += StringSupport.octLength(bytes, p, olen);
+                    break;
+
+                case 'x': /* \xHH */
+                    int hlen = end < p + 2 ? end - p : 2;
+                    code = StringSupport.scanHex(bytes, p, hlen);
+                    int len = StringSupport.hexLength(bytes, p, hlen);
+                    if (len < 1) raisePreprocessError(context, str, "invalid hex escape", mode);
+                    p += len;
+                    break;
+
+                case 'M': /* \M-X, \M-\C-X, \M-\cX */
+                    if (metaPrefix) raisePreprocessError(context, str, "duplicate meta escape", mode);
+                    metaPrefix = true;
+                    if (p + 1 < end && bytes[p++] == (byte)'-' && (bytes[p] & 0x80) == 0) {
+                        if (bytes[p] == (byte)'\\') {
+                            p++;
+                            continue;
+                        } else {
+                            code = bytes[p++] & 0xff;
+                            break;
+                        }
+                    }
+                    raisePreprocessError(context, str, "too short meta escape", mode);
+
+                case 'C': /* \C-X, \C-\M-X */
+                    if (p == end || bytes[p++] != (byte)'-') raisePreprocessError(context, str, "too short control escape", mode);
+
+                case 'c': /* \cX, \c\M-X */
+                    if (ctrlPrefix) raisePreprocessError(context, str, "duplicate control escape", mode);
+                    ctrlPrefix = true;
+                    if (p < end && (bytes[p] & 0x80) == 0) {
+                        if (bytes[p] == (byte)'\\') {
+                            p++;
+                            continue;
+                        } else {
+                            code = bytes[p++] & 0xff;
+                            break;
+                        }
+                    }
+                    raisePreprocessError(context, str, "too short control escape", mode);
+                default:
+                    raisePreprocessError(context, str, "unexpected escape sequence", mode);
+            } // switch
+
+            if (code < 0 || code > 0xff) raisePreprocessError(context, str, "invalid escape code", mode);
+
+            if (ctrlPrefix) code &= 0x1f;
+            if (metaPrefix) code |= 0x80;
+
+            to[toP] = (byte)code;
+            return p;
+        } // while
+    }
+
     public static void preprocessCheck(Ruby runtime, ByteList bytes) {
         RegexpSupport.preprocess(runtime, bytes, bytes.getEncoding(), new Encoding[]{null}, RegexpSupport.ErrorMode.RAISE);
     }
@@ -504,6 +816,26 @@ public class ClassicRegexp extends RubyObject implements ReOptions, EncodingCapa
             RubyString str = strings[i].convertToString();
             regexpEnc = processDRegexpElement(runtime, options, regexpEnc, runtime.getCurrentContext().encodingHolder(), str);
             string = string == null ? string = (RubyString)str.dup() : string.append19(str);
+        }
+
+        if (regexpEnc != null) string.setEncoding(regexpEnc);
+
+        return string;
+    }
+
+    public static ByteList preprocessDRegexp(RubyContext context, ByteList[] strings, RegexpOptions options) {
+        ByteList string = null;
+        Encoding regexpEnc = null;
+
+        for (int i = 0; i < strings.length; i++) {
+            ByteList str = strings[i];
+            final Encoding[] encodingHolder = new Encoding[]{null};
+            regexpEnc = processDRegexpElement(context, options, regexpEnc, encodingHolder, str);
+            if (string == null) {
+                string = str.dup();
+            } else {
+                string.append(str);
+            }
         }
 
         if (regexpEnc != null) string.setEncoding(regexpEnc);
@@ -625,6 +957,41 @@ public class ClassicRegexp extends RubyObject implements ReOptions, EncodingCapa
             regexpEnc = fixedEnc[0];
         }
         return regexpEnc;
+    }
+
+    private static Encoding processDRegexpElement(RubyContext context, RegexpOptions options, Encoding regexpEnc, Encoding[] fixedEnc, ByteList str) {
+        Encoding strEnc = str.getEncoding();
+
+        if (options.isEncodingNone() && strEnc != ASCIIEncoding.INSTANCE) {
+            if (scanForCodeRange(str) != StringSupport.CR_7BIT) {
+                throw new org.jruby.truffle.language.control.RaiseException(context.getCoreExceptions().regexpError("/.../n has a non escaped non ASCII character in non ASCII-8BIT script", null));
+            }
+            strEnc = ASCIIEncoding.INSTANCE;
+        }
+
+        // This used to call preprocess, but the resulting bytelist was not
+        // used. Since the preprocessing error-checking can be done without
+        // creating a new bytelist, I added a "light" path.
+        ClassicRegexp.preprocessLight(context, str, strEnc, fixedEnc, RegexpSupport.ErrorMode.PREPROCESS);
+
+        if (fixedEnc[0] != null) {
+            if (regexpEnc != null && regexpEnc != fixedEnc[0]) {
+                throw new org.jruby.truffle.language.control.RaiseException(context.getCoreExceptions().regexpError("encoding mismatch in dynamic regexp: " + new String(regexpEnc.getName()) + " and " + new String(fixedEnc[0].getName()), null));
+            }
+            regexpEnc = fixedEnc[0];
+        }
+        return regexpEnc;
+    }
+
+    private static int scanForCodeRange(ByteList str) {
+        int cr;
+        Encoding enc = str.getEncoding();
+        if (enc.minLength() > 1 && enc.isDummy()) {
+            cr = CR_BROKEN;
+        } else {
+            cr = codeRangeScan(EncodingUtils.getActualEncoding(enc, str), str);
+        }
+        return cr;
     }
 
     private void check() {
