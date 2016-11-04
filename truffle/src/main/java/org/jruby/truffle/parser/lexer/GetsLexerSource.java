ea4f2f53c7a67e52ddbfbad16bcd992f8d2def15
Chris Seaton <chris.seaton@oracle.com>
2016-11-04 16:16:53 +0900

    [Truffle] More dead code.

--- truffle/src/main/java/org/jruby/truffle/parser/lexer/GetsLexerSource.java
+++ truffle/src/main/java/org/jruby/truffle/parser/lexer/GetsLexerSource.java
@@ -27,7 +27,6 @@
 package org.jruby.truffle.parser.lexer;
 
 import org.jcodings.Encoding;
-import org.jruby.RubyArray;
 import org.jruby.RubyEncoding;
 import org.jruby.RubyIO;
 import org.jruby.runtime.builtin.IRubyObject;
@@ -54,25 +53,6 @@ public class GetsLexerSource extends LexerSource {
         this.encoding = encoding;
     }
 
-    // FIXME (enebo 21-Apr-15): ripper probably has same problem as main-line parser so this constructor
-    // may need to be a mix of frobbing the encoding of an incoming object plus defaultEncoding if not.
-    // But main-line parser should not be asking IO for encoding.
-    // Ripper constructor
-    public GetsLexerSource(String sourceName, int line, IRubyObject io, RubyArray scriptLines) {
-        this(sourceName, line, io, scriptLines, frobnicateEncoding(io));
-    }
-
-    // FIXME: Should be a hard failure likely if no encoding is possible
-    public static final Encoding frobnicateEncoding(IRubyObject io) {
-        // Non-ripper IO will not have encoding so we will just use default external
-        if (!io.respondsTo("encoding")) return io.getRuntime().getDefaultExternalEncoding();
-        
-        IRubyObject encodingObject = io.callMethod(io.getRuntime().getCurrentContext(), "encoding");
-
-        return encodingObject instanceof RubyEncoding ? 
-                ((RubyEncoding) encodingObject).getEncoding() : io.getRuntime().getDefaultExternalEncoding();
-    }
-    
     @Override
     public Encoding getEncoding() {
         return encoding;
