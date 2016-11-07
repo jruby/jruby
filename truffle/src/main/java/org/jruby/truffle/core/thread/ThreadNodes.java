7f0c331b30dbc0e4b72f6211c095c87e0c636f75
Benoit Daloze <eregontp@gmail.com>
2016-11-07 22:21:15 +0100

    [Truffle] Copy the small utilities we use from RubyThread.

--- truffle/src/main/java/org/jruby/truffle/core/thread/ThreadNodes.java
+++ truffle/src/main/java/org/jruby/truffle/core/thread/ThreadNodes.java
@@ -6,6 +6,37 @@
  * Eclipse Public License version 1.0
  * GNU General Public License version 2
  * GNU Lesser General Public License version 2.1
+ *
+ * Version: EPL 1.0/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Eclipse Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * Copyright (C) 2002 Jason Voegele <jason@jvoegele.com>
+ * Copyright (C) 2002-2004 Anders Bengtsson <ndrsbngtssn@yahoo.se>
+ * Copyright (C) 2002-2004 Jan Arne Petersen <jpetersen@uni-bonn.de>
+ * Copyright (C) 2004 Thomas E Enebo <enebo@acm.org>
+ * Copyright (C) 2004-2005 Charles O Nutter <headius@headius.com>
+ * Copyright (C) 2004 Stefan Matthias Aust <sma@3plus4.de>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the EPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the EPL, the GPL or the LGPL.
  */
 package org.jruby.truffle.core.thread;
 
@@ -50,11 +81,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
-
-import static org.jruby.RubyThread.RUBY_MAX_THREAD_PRIORITY;
-import static org.jruby.RubyThread.RUBY_MIN_THREAD_PRIORITY;
-import static org.jruby.RubyThread.javaPriorityToRubyPriority;
-import static org.jruby.RubyThread.rubyPriorityToJavaPriority;
 
 @CoreClass("Thread")
 public abstract class ThreadNodes {
@@ -506,10 +532,24 @@ public abstract class ThreadNodes {
                 return Layouts.THREAD.getPriority(thread);
             }
         }
+
+        /*
+         * helper methods to translate Java thread priority (1-10) to Ruby thread priority (-3 to 3)
+         * using a quadratic polynomial ant its inverse passing by (Ruby,Java): (-3,1), (0,5) and
+         * (3,10) i.e., j = r^2/18 + 3*r/2 + 5 r = 3/2*sqrt(8*j + 41) - 27/2
+         */
+        private static int javaPriorityToRubyPriority(int javaPriority) {
+            double d = 1.5 * Math.sqrt(8.0 * javaPriority + 41) - 13.5;
+            return Math.round((float) d);
+        }
     }
 
     @Primitive(name = "thread_set_priority", unsafe = UnsafeGroup.THREADS)
     public static abstract class ThreadSetPriorityPrimitiveNode extends PrimitiveArrayArgumentsNode {
+
+        static final int RUBY_MIN_THREAD_PRIORITY = -3;
+        static final int RUBY_MAX_THREAD_PRIORITY = 3;
+
         public ThreadSetPriorityPrimitiveNode(RubyContext context, SourceSection sourceSection) {
             super(context, sourceSection);
         }
@@ -530,6 +570,12 @@ public abstract class ThreadNodes {
             Layouts.THREAD.setPriority(thread, rubyPriority);
             return rubyPriority;
         }
+
+        private static int rubyPriorityToJavaPriority(int rubyPriority) {
+            double d = (rubyPriority * rubyPriority) / 18.0 + 1.5 * rubyPriority + 5;
+            return Math.round((float) d);
+        }
+
     }
 
     @Primitive(name = "thread_set_group")
