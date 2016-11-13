47b98c69c04378673f14bb3601e99c0c03227403
Benoit Daloze <eregontp@gmail.com>
2016-11-13 14:04:27 +0100

    [Truffle] Add a simple tool to compare stdlib contents.

---
+++ tool/truffle/compare_stdlib.rb
@@ -0,0 +1,15 @@
+name = ARGV.first
+
+require 'path'
+
+org = Path("lib/ruby/stdlib/#{name}")
+dst = Path("lib/ruby/truffle/mri/#{name}")
+
+a = org.glob("**/*.rb").map { |p| p % org }
+b = dst.glob("**/*.rb").map { |p| p % dst }
+
+puts "Extra files:"
+puts b-a
+
+puts "Missing files:"
+puts a-b
