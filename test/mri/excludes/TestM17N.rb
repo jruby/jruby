da08060e6cc3fc95c506f9892788a62df3775b66
Charles Oliver Nutter <headius@headius.com>
2016-11-16 18:14:43 -0600

    New failures in 2.3.1 tests. #4303.

--- test/mri/excludes/TestM17N.rb
+++ test/mri/excludes/TestM17N.rb
@@ -1,3 +1,4 @@
+exclude :test_greek_capital_gap, "needs investigation #4303"
 exclude :test_nonascii_method_name, "lexer is not pulling mbc characters off the wire correctly"
 exclude :test_split, "our impl has diverged and does not appear to handle encoded null char properly"
 exclude :test_symbol, "management of differently-encoded symbols is not right"
