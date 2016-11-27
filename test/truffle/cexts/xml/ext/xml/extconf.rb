9bb8e921da0913399d35cf3f70f2a46faf11fe06
Chris Seaton <chris.seaton@oracle.com>
2016-11-27 13:16:37 +0000

    [Truffle] I think LINK_SO should include LIBS.

--- test/truffle/cexts/xml/ext/xml/extconf.rb
+++ test/truffle/cexts/xml/ext/xml/extconf.rb
@@ -1,4 +1,4 @@
 require 'mkmf'
-$CFLAGS += " -I#{ENV['LIBXML_INCLUDE']}"
-$LDFLAGS += " -l #{ENV['LIBXML_LIB']}"
+$CFLAGS += " -I #{ENV['LIBXML_INCLUDE']}"
+$LIBS += " -l #{ENV['LIBXML_LIB']}"
 create_makefile('xml')
--- test/truffle/cexts/xopenssl/ext/xopenssl/extconf.rb
+++ test/truffle/cexts/xopenssl/ext/xopenssl/extconf.rb
@@ -1,4 +1,4 @@
 require 'mkmf'
-$CFLAGS += " -I#{ENV['OPENSSL_INCLUDE']}"
-$LDFLAGS += " -l #{ENV['OPENSSL_LIB']}"
+$CFLAGS += " -I #{ENV['OPENSSL_INCLUDE']}"
+$LIBS += " -l #{ENV['OPENSSL_LIB']}"
 create_makefile('xopenssl')
