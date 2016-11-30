147510098f0f6c2bc6f386b902b3fd1d10e746e7
Chris Seaton <chris.seaton@oracle.com>
2016-11-30 11:34:03 +0000

    [Truffle] Fix the debug test.

--- truffle/src/test/ruby/factorial.rb
+++ truffle/src/test/ruby/factorial.rb
@@ -1,11 +1,3 @@
-# Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved. This
-# code is released under a tri EPL/GPL/LGPL license. You can use it,
-# redistribute it and/or modify it under the terms of the:
-#
-# Eclipse Public License version 1.0
-# GNU General Public License version 2
-# GNU Lesser General Public License version 2.1
-
 def fac(n)
   if n <= 1
     1
@@ -23,3 +15,5 @@ def main
   res
 end
 Truffle::Interop.export_method(:main)
+
+# Don't add a licence header to this file - we use the line offets!
