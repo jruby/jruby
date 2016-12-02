5ac3a35f773bd2104efb3045ae10c14535eb96b2
kares <self@kares.org>
2016-12-02 13:24:41 +0100

    update jruby-openssl to 0.9.19

--- lib/pom.rb
+++ lib/pom.rb
@@ -10,7 +10,7 @@ end
 
 default_gems =
   [
-   ImportedGem.new( 'jruby-openssl', '0.9.17' ),
+   ImportedGem.new( 'jruby-openssl', '0.9.19' ),
    ImportedGem.new( 'rake', '${rake.version}' ),
    ImportedGem.new( 'rdoc', '${rdoc.version}' ),
    ImportedGem.new( 'json', '1.8.0' ),
