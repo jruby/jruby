c64e360d2eed4c62cac9daa0f8aa869e0951efc8
Christian Meier <meier.kristian@gmail.com>
2016-12-03 14:07:28 +0100

    pick the right javac version depending on the jdk which runs maven
    jdk8 uses 1.8 as specification version and jdk9 is using 9. there could be
    other ways to solve this - just picked one.

--- lib/pom.rb
+++ lib/pom.rb
@@ -62,7 +62,7 @@ project 'JRuby Lib Setup' do
   # just depends on jruby-core so we are sure the jruby.jar is in place
   jar "org.jruby:jruby-core:#{version}", :scope => 'test'
 
-  extension 'org.torquebox.mojo:mavengem-wagon:0.2.2-SNAPSHOT'
+  extension 'org.torquebox.mojo:mavengem-wagon:0.2.2'
 
   repository :id => :mavengems, :url => 'mavengem:https://rubygems.org'
 
