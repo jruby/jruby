beeffd8914e5926203e35547f951eae895cc633b
kares <self@kares.org>
2016-12-05 13:12:43 +0100

    [test] [openssl] untag passing specs

--- test/mri/excludes/OpenSSL/TestPKeyDSA.rb
+++ test/mri/excludes/OpenSSL/TestPKeyDSA.rb
@@ -6,5 +6,3 @@ exclude :test_read_DSAPublicKey_pem, 'needs investigation'
 exclude :test_read_private_key_pem_pw, 'needs investigation'
 exclude :test_read_public_key_der, 'needs investigation'
 exclude :test_read_public_key_pem, 'needs investigation'
-exclude :test_sys_sign_verify, 'needs investigation'
-
