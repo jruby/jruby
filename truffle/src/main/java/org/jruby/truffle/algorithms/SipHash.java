8a5d18632ff25cfc7f9f4f88cfb812332e812c67
Chris Seaton <chris.seaton@oracle.com>
2016-12-11 10:17:23 -0800

    [Truffle] Tidy up SipHash.

--- truffle/src/main/java/org/jruby/truffle/algorithms/SipHash.java
+++ truffle/src/main/java/org/jruby/truffle/algorithms/SipHash.java
@@ -1,22 +1,41 @@
+/***** BEGIN LICENSE BLOCK *****
+ * Version: EPL 1.0/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Eclipse Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * Written by nahi@ruby-lang.org
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the EPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the EPL, the GPL or the LGPL.
+ ***** END LICENSE BLOCK *****/
 package org.jruby.truffle.algorithms;
 
 import java.nio.ByteBuffer;
 
-/**
- * SipHash implementation with hand inlining the SIPROUND.
- *
- * To know details about SipHash, see;
- * "a fast short-input PRF" https://www.131002.net/siphash/
- *
- * @author nahi@ruby-lang.org
- */
 public class SipHash {
 
     public static long hash24(long k0, long k1, byte[] data) {
         return hash24(k0, k1, data, 0, data.length);
     }
     
-    public static long hash24(long k0, long k1, byte[] src, int offset, int length) {
+    private static long hash24(long k0, long k1, byte[] src, int offset, int length) {
         long v0 = 0x736f6d6570736575L ^ k0;
         long v1 = 0x646f72616e646f6dL ^ k1;
         long v2 = 0x6c7967656e657261L ^ k0;
