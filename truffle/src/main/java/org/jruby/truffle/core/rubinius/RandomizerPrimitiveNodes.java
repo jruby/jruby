9be8ce019b824db66a2f982a50a498a55d4183a2
Chris Seaton <chris.seaton@oracle.com>
2016-12-11 16:23:20 -0800

    [Truffle] randomizer_rand_int should accept Bignum limits.

--- truffle/src/main/java/org/jruby/truffle/core/rubinius/RandomizerPrimitiveNodes.java
+++ truffle/src/main/java/org/jruby/truffle/core/rubinius/RandomizerPrimitiveNodes.java
@@ -6,6 +6,30 @@
  * Eclipse Public License version 1.0
  * GNU General Public License version 2
  * GNU Lesser General Public License version 2.1
+ *
+ * Copyright the JRuby team.
+ *
+ * The contents of this file are subject to the Eclipse Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the EPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the EPL, the GPL or the LGPL.
  */
 package org.jruby.truffle.core.rubinius;
 
@@ -134,6 +158,12 @@ public abstract class RandomizerPrimitiveNodes {
             return randInt(r, limit);
         }
 
+        @Specialization(guards = "isRubyBignum(limit)")
+        public DynamicObject randomizerRandInt(DynamicObject randomizer, DynamicObject limit) {
+            final Randomizer r = Layouts.RANDOMIZER.getRandomizer(randomizer);
+            return createBignum(randLimitedBignum(r, Layouts.BIGNUM.getValue(limit)));
+        }
+
         @TruffleBoundary
         protected static long randInt(Randomizer r, long limit) {
             return randLimitedFixnumInner(r, limit);
@@ -161,6 +191,72 @@ public abstract class RandomizerPrimitiveNodes {
                 }
             }
             return val;
+        }
+
+        private static BigInteger randLimitedBignum(Randomizer randomizer, BigInteger limit) {
+            byte[] buf = limit.toByteArray();
+            byte[] bytes = new byte[buf.length];
+            int len = (buf.length + 3) / 4;
+            // take care before code cleanup; it might break random sequence compatibility
+            retry: while (true) {
+                long mask = 0;
+                boolean boundary = true;
+                for (int idx = len - 1; 0 <= idx; --idx) {
+                    long lim = getIntBigIntegerBuffer(buf, idx) & 0xffffffffL;
+                    mask = (mask != 0) ? 0xffffffffL : makeMask(lim);
+                    long rnd;
+                    if (mask != 0) {
+                        rnd = (randomizer.genrandInt32() & 0xffffffffL) & mask;
+                        if (boundary) {
+                            if (lim < rnd) {
+                                continue retry;
+                            }
+                            if (rnd < lim) {
+                                boundary = false;
+                            }
+                        }
+                    } else {
+                        rnd = 0;
+                    }
+                    setIntBigIntegerBuffer(bytes, idx, (int) rnd);
+                }
+                break;
+            }
+            return new BigInteger(bytes);
+        }
+
+        private static int getIntBigIntegerBuffer(byte[] src, int loc) {
+            int v = 0;
+            int idx = src.length - loc * 4 - 1;
+            if (idx >= 0) {
+                v |= (src[idx--] & 0xff);
+                if (idx >= 0) {
+                    v |= (src[idx--] & 0xff) << 8;
+                    if (idx >= 0) {
+                        v |= (src[idx--] & 0xff) << 16;
+                        if (idx >= 0) {
+                            v |= (src[idx--] & 0xff) << 24;
+                        }
+                    }
+                }
+            }
+            return v;
+        }
+
+        private static void setIntBigIntegerBuffer(byte[] dest, int loc, int value) {
+            int idx = dest.length - loc * 4 - 1;
+            if (idx >= 0) {
+                dest[idx--] = (byte) (value & 0xff);
+                if (idx >= 0) {
+                    dest[idx--] = (byte) ((value >> 8) & 0xff);
+                    if (idx >= 0) {
+                        dest[idx--] = (byte) ((value >> 16) & 0xff);
+                        if (idx >= 0) {
+                            dest[idx--] = (byte) ((value >> 24) & 0xff);
+                        }
+                    }
+                }
+            }
         }
 
         private static long makeMask(long x) {
