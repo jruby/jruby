b36615ad07aa1f637e3e3f9855bbab6212583415
Chris Seaton <chris.seaton@oracle.com>
2016-12-13 20:00:58 +0000

    [Truffle] Try installing gems using system Ruby.

--- test/truffle/ecosystem-travis-install.sh
+++ test/truffle/ecosystem-travis-install.sh
@@ -5,7 +5,9 @@ set -x
 
 unset GEM_HOME GEM_PATH
 
-tool/jt.rb ruby -rbundler-workarounds bin/gem install bundler
+# TODO CS 13-Dec-16 We'd like to run this ourselves, but we get issues when we try. Fix later.
+#tool/jt.rb ruby -rbundler-workarounds bin/gem install bundler
+GEM_HOME=lib/ruby/gems/shared gem install bundler
 
 git clone \
     --branch master \
