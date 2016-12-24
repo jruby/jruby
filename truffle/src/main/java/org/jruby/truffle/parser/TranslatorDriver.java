27da7b11f5b40a795ac7fcb05796bc9a5158abe5
Chris Seaton <chris.seaton@oracle.com>
2016-12-24 13:04:24 +0000

    [Truffle] Parser was a pretty shallow abstraction - remove it.

--- truffle/src/main/java/org/jruby/truffle/parser/TranslatorDriver.java
+++ truffle/src/main/java/org/jruby/truffle/parser/TranslatorDriver.java
@@ -6,6 +6,33 @@
  * Eclipse Public License version 1.0
  * GNU General Public License version 2
  * GNU Lesser General Public License version 2.1
+ *
+ * The contents of this file are subject to the Eclipse Public
+ * License Version 1.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * Copyright (C) 2002-2004 Anders Bengtsson <ndrsbngtssn@yahoo.se>
+ * Copyright (C) 2002-2004 Jan Arne Petersen <jpetersen@uni-bonn.de>
+ * Copyright (C) 2004 Thomas E Enebo <enebo@acm.org>
+ * Copyright (C) 2004 Stefan Matthias Aust <sma@3plus4.de>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the EPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the EPL, the GPL or the LGPL.
  */
 package org.jruby.truffle.parser;
 
@@ -22,6 +49,7 @@ import org.jcodings.Encoding;
 import org.jruby.truffle.RubyContext;
 import org.jruby.truffle.core.LoadRequiredLibrariesNode;
 import org.jruby.truffle.core.SetTopLevelBindingNode;
+import org.jruby.truffle.core.string.ByteList;
 import org.jruby.truffle.language.DataNode;
 import org.jruby.truffle.language.LexicalScope;
 import org.jruby.truffle.language.RubyNode;
@@ -30,6 +58,7 @@ import org.jruby.truffle.language.arguments.MissingArgumentBehavior;
 import org.jruby.truffle.language.arguments.ProfileArgumentNode;
 import org.jruby.truffle.language.arguments.ReadPreArgumentNode;
 import org.jruby.truffle.language.arguments.RubyArguments;
+import org.jruby.truffle.language.control.RaiseException;
 import org.jruby.truffle.language.exceptions.TopLevelRaiseHandler;
 import org.jruby.truffle.language.locals.WriteLocalVariableNode;
 import org.jruby.truffle.language.methods.Arity;
@@ -41,13 +70,18 @@ import org.jruby.truffle.language.methods.InternalMethod;
 import org.jruby.truffle.language.methods.SharedMethodInfo;
 import org.jruby.truffle.language.methods.UnsupportedOperationBehavior;
 import org.jruby.truffle.parser.ast.RootParseNode;
-import org.jruby.truffle.parser.parser.Parser;
+import org.jruby.truffle.parser.lexer.ByteListLexerSource;
+import org.jruby.truffle.parser.lexer.LexerSource;
+import org.jruby.truffle.parser.lexer.SyntaxException;
 import org.jruby.truffle.parser.parser.ParserConfiguration;
+import org.jruby.truffle.parser.parser.RubyParser;
+import org.jruby.truffle.parser.parser.RubyParserResult;
 import org.jruby.truffle.parser.scope.DynamicScope;
 import org.jruby.truffle.parser.scope.ManyVarsDynamicScope;
 import org.jruby.truffle.parser.scope.StaticScope;
 import org.jruby.truffle.parser.scope.StaticScopeFactory;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -55,17 +89,15 @@ import java.util.List;
 
 public class TranslatorDriver {
 
+    private final RubyContext context;
     private final ParseEnvironment parseEnvironment;
 
     public TranslatorDriver(RubyContext context) {
+        this.context = context;
         parseEnvironment = new ParseEnvironment(context);
     }
 
     public RubyRootNode parse(RubyContext context, Source source, Encoding defaultEncoding, ParserContext parserContext, String[] argumentNames, FrameDescriptor frameDescriptor, MaterializedFrame parentFrame, boolean ownScopeForAssignments, Node currentNode) {
-        // Set up the JRuby parser
-
-        final Parser parser = new Parser(context);
-
         final StaticScopeFactory staticScopeFactory = new StaticScopeFactory();
         final StaticScope staticScope = staticScopeFactory.newLocalScope(null);
 
@@ -125,9 +157,7 @@ public class TranslatorDriver {
 
         // Parse to the JRuby AST
 
-        RootParseNode node;
-
-        node = (RootParseNode) parser.parse(source.getName(), source.getCode().getBytes(StandardCharsets.UTF_8), dynamicScope, parserConfiguration);
+        RootParseNode node = parse(source.getName(), source.getCode().getBytes(StandardCharsets.UTF_8), dynamicScope, parserConfiguration);
 
         final SourceSection sourceSection = source.createSection(0, source.getCode().length());
         final TempSourceSection tempSourceSection = new TempSourceSection(sourceSection);
@@ -233,6 +263,53 @@ public class TranslatorDriver {
         }
 
         return new RubyRootNode(context, truffleNode.getRubySourceSection().toSourceSection(source), environment.getFrameDescriptor(), sharedMethodInfo, truffleNode, environment.needsDeclarationFrame());
+    }
+
+    public RootParseNode parse(String file, byte[] content, DynamicScope blockScope,
+                           ParserConfiguration configuration) {
+        List<ByteList> list = null;
+        ByteList in = new ByteList(content, configuration.getDefaultEncoding());
+        LexerSource lexerSource = new ByteListLexerSource(file, configuration.getLineNumber(), in,  list);
+        // We only need to pass in current scope if we are evaluating as a block (which
+        // is only done for evals).  We need to pass this in so that we can appropriately scope
+        // down to captured scopes when we are parsing.
+        if (blockScope != null) {
+            configuration.parseAsBlock(blockScope);
+        }
+
+        RubyParser parser = new RubyParser(context, lexerSource, new RubyWarnings(configuration.getContext()));
+        RubyParserResult result;
+        try {
+            result = parser.parse(configuration);
+        } catch (IOException e) {
+            // Enebo: We may want to change this error to be more specific,
+            // but I am not sure which conditions leads to this...so lame message.
+            throw new RaiseException(context.getCoreExceptions().syntaxError("Problem reading source: " + e, null));
+        } catch (SyntaxException e) {
+            switch (e.getPid()) {
+                case UNKNOWN_ENCODING:
+                case NOT_ASCII_COMPATIBLE:
+                    throw new RaiseException(context.getCoreExceptions().argumentError(e.getMessage(), null));
+                default:
+                    StringBuilder buffer = new StringBuilder(100);
+                    buffer.append(e.getFile()).append(':');
+                    buffer.append(e.getLine() + 1).append(": ");
+                    buffer.append(e.getMessage());
+
+                    throw new RaiseException(context.getCoreExceptions().syntaxError(buffer.toString(), null));
+            }
+        }
+
+        // If variables were added then we may need to grow the dynamic scope to match the static
+        // one.
+        // FIXME: Make this so we only need to check this for blockScope != null.  We cannot
+        // currently since we create the DynamicScope for a LocalStaticScope before parse begins.
+        // Refactoring should make this fixable.
+        if (result.getScope() != null) {
+            result.getScope().growIfNeeded();
+        }
+
+        return (RootParseNode) result.getAST();
     }
 
     private TranslatorEnvironment environmentForFrameDescriptor(RubyContext context, FrameDescriptor frameDescriptor) {
