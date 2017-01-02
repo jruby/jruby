a98a54d16238f1698cb2f259fd53bbe4813ae479
Chris Seaton <chris.seaton@oracle.com>
2017-01-02 16:24:50 +0000

    [Truffle] Copyright years.

--- truffle/src/main/java/org/jruby/truffle/core/tracepoint/TracePointNodes.java
+++ truffle/src/main/java/org/jruby/truffle/core/tracepoint/TracePointNodes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved. This
+ * Copyright (c) 2016, 2017 Oracle and/or its affiliates. All rights reserved. This
  * code is released under a tri EPL/GPL/LGPL license. You can use it,
  * redistribute it and/or modify it under the terms of the:
  *
