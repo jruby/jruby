eeb42728d496e751e1f22386b7b871ffef4cf51c
Chris Seaton <chris.seaton@oracle.com>
2017-01-02 22:05:10 +0000

    [Truffle] Tag failing encoding specs.

--- test/truffle/compiler/pe/core/encoding_pe.rb
+++ test/truffle/compiler/pe/core/encoding_pe.rb
@@ -12,7 +12,7 @@ example "Encoding::UTF_16BE.ascii_compatible?", false
 example "Encoding::ISO_2022_JP.dummy?", true
 example "Encoding::UTF_8.dummy?", false
 
-example "Encoding.compatible?('abc', 'def')", Encoding::UTF_8
-example "Encoding.compatible?(Encoding::UTF_8, Encoding::US_ASCII)", Encoding::UTF_8
-example "Encoding.compatible?(Encoding::UTF_8, Encoding::ASCII_8BIT)", nil
-example "Encoding.compatible?('abc', Encoding::US_ASCII)", Encoding::UTF_8
+tagged example "Encoding.compatible?('abc', 'def')", Encoding::UTF_8
+tagged example "Encoding.compatible?(Encoding::UTF_8, Encoding::US_ASCII)", Encoding::UTF_8
+tagged example "Encoding.compatible?(Encoding::UTF_8, Encoding::ASCII_8BIT)", nil
+tagged example "Encoding.compatible?('abc', Encoding::US_ASCII)", Encoding::UTF_8
