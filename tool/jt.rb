c91141a274f00170976cf1c443aa8d618273b1e1
Chris Seaton <chris.seaton@oracle.com>
2017-01-06 14:52:04 +0000

    [Truffle] Upgrade to GraalVM 0.19.

--- tool/jt.rb
+++ tool/jt.rb
@@ -21,7 +21,7 @@ require 'timeout'
 require 'yaml'
 require 'open3'
 
-GRAALVM_VERSION = '0.18'
+GRAALVM_VERSION = '0.19' # also change ci.hocon
 
 JRUBY_DIR = File.expand_path('../..', __FILE__)
 M2_REPO = File.expand_path('~/.m2/repository')
