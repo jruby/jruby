3efa7feaf15cf6e2b8768cd8ae91f63cf7f196be
Thomas E. Enebo <tom.enebo@gmail.com>
2017-01-18 09:27:39 -0600

    Correct some mixed boilerplate which still had a mix of EPL and CPL verbiage

--- core/src/test/java/org/jruby/embed/internal/BiVariableMapTest.java
+++ core/src/test/java/org/jruby/embed/internal/BiVariableMapTest.java
@@ -1,11 +1,11 @@
-/**
- * **** BEGIN LICENSE BLOCK *****
+/*
+ **** BEGIN LICENSE BLOCK *****
  * Version: EPL 1.0/GPL 2.0/LGPL 2.1
  *
- * The contents of this file are subject to the Common Public
+ * The contents of this file are subject to the Eclipse Public
  * License Version 1.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of
- * the License at http://www.eclipse.org/legal/cpl-v10.html
+ * the License at http://www.eclipse.org/legal/epl-v10.html
  *
  * Software distributed under the License is distributed on an "AS
  * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
@@ -25,8 +25,7 @@
  * and other provisions required by the GPL or the LGPL. If you do not delete
  * the provisions above, a recipient may use your version of this file under
  * the terms of any one of the EPL, the GPL or the LGPL.
- * **** END LICENSE BLOCK *****
- */
+ ***** END LICENSE BLOCK *****/
 package org.jruby.embed.internal;
 
 import java.io.IOException;
