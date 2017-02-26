cf888ad1a56bfcb1b0dae33dac91a2ceb3b7f50d
Daniel Smith <jellymann@gmail.com>
2017-02-26 10:52:06 +0200

    fix: use string bytesize instead of length in Fiddle::Pointer#[]=

--- lib/ruby/stdlib/fiddle/jruby.rb
+++ lib/ruby/stdlib/fiddle/jruby.rb
@@ -207,7 +207,7 @@ module Fiddle
 
     def []=(index, length = nil, value)
       if length
-        ffi_ptr.put_bytes(index, value, 0, [length, value.length].min)
+        ffi_ptr.put_bytes(index, value, 0, [length, value.bytesize].min)
       else
         ffi_ptr.put_int8(index, value)
       end
