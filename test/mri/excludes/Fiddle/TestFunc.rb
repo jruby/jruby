50623aad75f0bacee37d8d7c2f4e58d1a59e1a61
Charles Oliver Nutter <headius@headius.com>
2017-03-01 12:17:14 -0600

    Add Fiddle tests to MRI suite index.
    Thanks to @jellyman for Fiddle fixes that enabled these tests to
    run. See #4511.

---
+++ test/mri/excludes/Fiddle/TestFunc.rb
@@ -0,0 +1,4 @@
+exclude :test_qsort1, "needs investigation"
+exclude :test_string, "needs investigation"
+exclude :test_strtod, "needs investigation"
+exclude :test_syscall_with_tainted_string, "needs investigation"
---
+++ test/mri/excludes/Fiddle/TestImport.rb
@@ -0,0 +1,3 @@
+exclude :test_io, "needs investigation"
+exclude :test_strcpy, "needs investigation"
+exclude :test_unsigned_result, "needs investigation"
