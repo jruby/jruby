6a9eee27d6d32a3ec3cf7b1977a8616fdf2e17b7
Charles Oliver Nutter <headius@headius.com>
2017-03-14 21:06:53 -0500

    Mask tests that depend on MRI layout.

--- test/mri/excludes/TestRipper/Generic.rb
+++ test/mri/excludes/TestRipper/Generic.rb
@@ -1,1 +1,5 @@
 exclude :test_parse_files, "assumes MRI source layout to randomly rip .rb files"
+exclude :'test_parse_files:ext', "assumes MRI source layout to randomly rip .rb files"
+exclude :'test_parse_files:lib', "assumes MRI source layout to randomly rip .rb files"
+exclude :'test_parse_files:sample', "assumes MRI source layout to randomly rip .rb files"
+exclude :'test_parse_files:test', "assumes MRI source layout to randomly rip .rb files"
