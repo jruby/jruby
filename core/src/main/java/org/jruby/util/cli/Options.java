b0ef07a5fe4b9c16b0d6d0831ffb584002e388b5
Charles Oliver Nutter <headius@headius.com>
2017-04-13 10:03:39 -0500

    Use natural word order for this option.

--- core/src/main/java/org/jruby/util/cli/Options.java
+++ core/src/main/java/org/jruby/util/cli/Options.java
@@ -172,7 +172,7 @@ public class Options {
     public static final Option<Boolean> USE_FIXNUM_CACHE = bool(MISCELLANEOUS, "fixnum.cache", true, "Use a cache of low-valued Fixnum objects.");
     public static final Option<Integer> FIXNUM_CACHE_RANGE = integer(MISCELLANEOUS, "fixnum.cache.size", 256, "Values to retrieve from Fixnum cache, in the range -X..(X-1).");
     public static final Option<Boolean> PACKED_ARRAYS = bool(MISCELLANEOUS, "packed.arrays", true, "Toggle whether to use \"packed\" arrays for small tuples.");
-    public static final Option<Boolean> FLAT_PARSER = bool(MISCELLANEOUS, "parser.flat", false, "Toggle whether to use the \"flat\" parser rather than the command-based version.");
+    public static final Option<Boolean> FLAT_PARSER = bool(MISCELLANEOUS, "flat.parser", false, "Toggle whether to use the \"flat\" parser rather than the command-based version.");
 
     public static final Option<Boolean> DEBUG_LOADSERVICE = bool(DEBUG, "debug.loadService", false, "Log require/load file searches.");
     public static final Option<Boolean> DEBUG_LOADSERVICE_TIMING = bool(DEBUG, "debug.loadService.timing", false, "Log require/load parse+evaluate times.");
