822a0b56afbd65cacbdce4607a74aa6fba945d70
Charles Oliver Nutter <headius@headius.com>
2017-06-26 15:28:23 -0500

    [2.4] Exception tests from MRI we don't implement currently.
    See #4293, #4687.

--- test/mri/excludes/TestException.rb
+++ test/mri/excludes/TestException.rb
@@ -9,9 +9,11 @@ exclude :test_errinfo_encoding_in_debug, "parser issue with Japanese encodings (
 exclude /test_machine_stackoverflow/, reason
 exclude :test_machine_stackoverflow_by_define_method, reason
 exclude :test_multibyte_and_newline, "Exception messages always go through Java String for us"
-exclude :test_name_error_local_variables, "NameError#local_variables is internal and specific to MRI"
+exclude :test_name_error_info_local_variables, "JRuby does not support extracting local variables from a NameError"
+exclude :test_name_error_info_method, "method_missing errors do not have original call type available"
+exclude :test_name_error_info_method_missing, "method_missing errors do not have original call type available"
+exclude :test_name_error_info_parent_iseq_mark, "JRuby does not support extracting local variables from a NameError"
 exclude :test_output_string_encoding, "Exception messages always go through Java String for us"
+exclude :test_redefined_backtrace, "Our backtrace is lazily set up and the flow does not work with this change"
 exclude :test_stackoverflow, reason
 exclude :test_too_many_args_in_eval, "MRI raises SystemStackError for huge number of args, for some reason"
-exclude :test_name_error_local_variables, "JRuby does not support extracting local variables from a NameError"
-exclude :test_name_error_info_parent_iseq_mark, "JRuby does not support extracting local variables from a NameError"
