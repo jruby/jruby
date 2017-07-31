532edff1549b2b8ba73239f863c9a424baedead0
Charles Oliver Nutter <headius@headius.com>
2017-07-31 11:57:02 -0500

    Update jcodings and joni.
    jcodings: new UTF-8 codepoint length logic
    joni: expanded named capture valid strings

--- core/pom.rb
+++ core/pom.rb
@@ -52,9 +52,9 @@ project 'JRuby Core' do
   jar 'com.github.jnr:jffi:${jffi.version}'
   jar 'com.github.jnr:jffi:${jffi.version}:native'
 
-  jar 'org.jruby.joni:joni:2.1.12-SNAPSHOT'
+  jar 'org.jruby.joni:joni:2.1.12'
   jar 'org.jruby.extras:bytelist:1.0.15'
-  jar 'org.jruby.jcodings:jcodings:1.0.20'
+  jar 'org.jruby.jcodings:jcodings:1.0.21'
   jar 'org.jruby:dirgra:0.3'
 
   jar 'com.headius:invokebinder:1.8-SNAPSHOT'
