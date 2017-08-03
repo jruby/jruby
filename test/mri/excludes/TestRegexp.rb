1404327f2cffc3d689ece336e535de138d99cbc7
Charles Oliver Nutter <headius@headius.com>
2017-08-03 15:52:35 -0500

    Tag two Regexp tests that are related to Unicode update. #4731

--- test/mri/excludes/TestRegexp.rb
+++ test/mri/excludes/TestRegexp.rb
@@ -1,3 +1,4 @@
+exclude :test_cclass_X, "Unicode 9.0 update in 2.4 (#4731)"
 exclude :test_char_class, "needs investigation"
 exclude :test_dup_warn, "needs investigation"
 exclude :test_equal, "needs investigation"
@@ -8,6 +9,7 @@ exclude :test_match_without_regexp, "matches non-unicode with exception message 
 exclude :test_named_capture_nonascii, "needs investigation"
 exclude :test_once_multithread, "fails intermittently"
 exclude :test_options_in_look_behind, "needs investigation"
+exclude :test_quote, "Likely unicode 9 update-related (#4731)"
 exclude :test_raw_hyphen_and_tk_char_type_after_range, "needs investigation"
 exclude :test_regsub_K, "needs investigation"
 exclude :test_source, "needs investigation"
