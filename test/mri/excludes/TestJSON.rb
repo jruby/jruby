fd5bc37296481053f7d04d7eb25a74fc93723123
Charles Oliver Nutter <headius@headius.com>
2017-08-03 16:27:13 -0500

    Tag json + bignum test from MRI (flori/json#336).

--- test/mri/excludes/TestJSON.rb
+++ test/mri/excludes/TestJSON.rb
@@ -1,3 +1,4 @@
+exclude :test_big_integers, "needs work in json lib to gracefully handle Bignum (flori/json#336)"
 exclude :test_load_with_options, "needs investigation"
 exclude :test_parse_generic_object, "needs investigation"
 exclude :test_symbolize_names, "needs investigation"
