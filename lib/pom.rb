79085c6aa5cd728904e2fcb7ddddbe1a8f3749bb
Charles Oliver Nutter <headius@headius.com>
2017-10-27 14:12:50 -0500

    Update mavengems extension to allow building jruby-complete on 9.
    Fixes #4799.

--- lib/pom.rb
+++ lib/pom.rb
@@ -61,7 +61,7 @@ project 'JRuby Lib Setup' do
   # just depends on jruby-core so we are sure the jruby.jar is in place
   jar "org.jruby:jruby-core:#{version}", :scope => 'test'
 
-  extension 'org.torquebox.mojo:mavengem-wagon:0.2.0'
+  extension 'org.torquebox.mojo:mavengem-wagon:0.2.2'
 
   repository :id => :mavengems, :url => 'mavengem:https://rubygems.org'
 
