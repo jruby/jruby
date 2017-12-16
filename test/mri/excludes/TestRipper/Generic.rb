7ecf6eeed35f5463b29b8c87d5790ef07337b8e5
Gustav Munkby <grddev@gmail.com>
2017-12-16 13:01:29 +0100

    Update the MRI ripper spec excludes
    First, this ignores the entire file that relies on the MRI file structure, as it seems unlikely that any of those tests will ever be applicable.
    
    Second, it adds the test_sexp.rb file which was absent from the index for some unknown reason. It also introduces a couple of excludes for failing tests in that file.
    
    Third, it clears the excludes for a couple of tests that are currently passing, and updates the description for one of the excludes.

--- test/mri/excludes/TestRipper/Generic.rb
+++
@@ -1,5 +0,0 @@
-exclude :test_parse_files, "assumes MRI source layout to randomly rip .rb files"
-exclude :'test_parse_files:ext', "assumes MRI source layout to randomly rip .rb files"
-exclude :'test_parse_files:lib', "assumes MRI source layout to randomly rip .rb files"
-exclude :'test_parse_files:sample', "assumes MRI source layout to randomly rip .rb files"
-exclude :'test_parse_files:test', "assumes MRI source layout to randomly rip .rb files"
---
+++ test/mri/excludes/TestRipper/Sexp.rb
@@ -0,0 +1,3 @@
+exclude :test_compile_error, 'needs regexp validation'
+exclude :test_heredoc_content, 'last string_token broken'
+exclude :test_squiggly_heredoc, 'last string_token broken'
