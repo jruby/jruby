806d858594bb49e60862268413d12d12941fbab2
Charles Oliver Nutter <headius@headius.com>
2018-01-05 12:45:27 -0600

    Refactoring.

--- core/src/main/ruby/jruby/jruby.rb
+++ core/src/main/ruby/jruby/jruby.rb
@@ -79,7 +79,7 @@ module JRuby
       end
 
       scope = org.jruby.ir.IRBuilder.build_root(manager, node).scope
-      scope.top_level_binding_scope = node.scope
+      scope.top_level_scope = node.scope
 
       scope
     end
