0d7d15e976dcb6149bab5643832c2d5e221c58c1
Thomas E. Enebo <tom.enebo@gmail.com>
2018-01-09 17:23:30 -0600

    Remove tests which work because of fix for #4895

--- test/mri/excludes/FTPTest.rb
+++ test/mri/excludes/FTPTest.rb
@@ -1,6 +1,4 @@
 exclude :test_abort, ""
 exclude :test_list_read_timeout_exceeded, "needs investigation #4303"
 exclude :test_status, ""
-exclude :test_tls_connect_timeout, "exception-handling problem causing defined?(Colon2) to fail. #4895"
 exclude :test_tls_post_connection_check, "new SSL-related failure in 2.4. See jruby/jruby-openssl#139"
-exclude :test_tls_unknown_ca, "exception-handling problem causing defined?(Colon2) to fail. #4895"
