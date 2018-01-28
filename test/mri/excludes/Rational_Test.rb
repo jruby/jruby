55a891a2c0dc8ee24d2ac3ea31bccd92be812242
yui-knk <spiketeika@gmail.com>
2018-01-28 10:12:27 +0900

    Add `#test_power_of_1_and_minus_1` to test targets
    Currently `#test_power_of_1_and_minus_1` does not fail.

--- test/mri/excludes/Rational_Test.rb
+++ test/mri/excludes/Rational_Test.rb
@@ -1,4 +1,3 @@
 exclude :test_conv, "needs investigation"
 exclude :test_marshal, "needs investigation"
-exclude :test_power_of_1_and_minus_1, "needs investigation"
 exclude :test_ratsub, "needs investigation"
