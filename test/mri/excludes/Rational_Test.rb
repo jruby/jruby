83021d8dffda68566d8f4d47f0aef3f4822b50c2
Charles Oliver Nutter <headius@headius.com>
2018-01-29 15:50:15 -0600

    Fix merge conflict that got committed.

--- test/mri/excludes/Rational_Test.rb
+++ test/mri/excludes/Rational_Test.rb
@@ -1,7 +1,4 @@
 exclude :test_conv, "needs investigation"
 exclude :test_marshal, "needs investigation"
-<<<<<<< HEAD
 exclude :test_power_overflow, "consumes much time and memory on JVM"
-=======
->>>>>>> jruby-9.1
 exclude :test_ratsub, "needs investigation"
