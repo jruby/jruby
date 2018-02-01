3e66fb675156c31ed4634da3eceac78ff4514f03
kares <self@kares.org>
2018-02-01 09:21:38 +0100

    [test] a few (edge) cases - for the date.rb -> native .java rewrite

--- test/jruby/test_date.rb
+++ test/jruby/test_date.rb
@@ -28,6 +28,11 @@ class TestDate < Test::Unit::TestCase
     assert_equal 0, date.send(:hour)
     assert_equal 0, date.send(:min)
     assert_equal 0, date.send(:second)
+
+    date = Date.new(2001, 3, -1)
+    assert_equal 2001, date.year
+    assert_equal 3, date.month
+    assert_equal 31, date.day
   end
 
   def test_new_start
@@ -72,6 +77,29 @@ class TestDate < Test::Unit::TestCase
     # assert_equal 19, new_date.day
   end
 
+  def test_new_offset
+    date = Date.new(2000, 1, 1)
+
+    assert_equal '+00:00', date.send(:zone)
+    new_date = date.send :new_offset, 'nst'
+    assert_equal 1999, new_date.year
+    assert_equal 12, new_date.month
+    assert_equal 31, new_date.day
+    assert_equal '-03:30', new_date.send(:zone)
+    assert_equal 20, new_date.send(:hour)
+    assert_equal 30, new_date.send(:min)
+    assert_equal 0, new_date.send(:sec)
+
+    new_date = date.send :new_offset, Rational(7, 24)
+    assert_equal 2000, new_date.year
+    assert_equal 1, new_date.month
+    assert_equal 1, new_date.day
+    assert_equal '+07:00', new_date.send(:zone)
+    assert_equal 7, new_date.send(:hour)
+    assert_equal 0, new_date.send(:min)
+    assert_equal 0, new_date.send(:sec)
+  end
+
   def test_julian
     date = Date.new(2000, 1, 1)
     assert_equal true, date.gregorian?
@@ -79,6 +107,61 @@ class TestDate < Test::Unit::TestCase
 
     date = Date.new(1000, 1, 1)
     assert_equal true, date.julian?
+  end
+
+  def test_to_s_strftime
+    date = Date.new(2000, 1, 1)
+    assert_equal '2000-01-01', date.to_s
+    assert_equal '2000-01-01', date.strftime
+
+    date = Date.new(-111, 10, 11)
+    assert_equal '-0111-10-11', date.to_s
+  end
+
+  def test_inspect
+    date = Date.new(2000, 12, 31)
+    assert_equal '#<Date: 2000-12-31 ((2451910j,0s,0n),+0s,2299161j)>', date.inspect
+
+    date = Date.today
+    assert_match /#<Date: 20\d\d\-\d\d\-\d\d \(\(\d+j,0s,0n\),\+0s,2299161j\)>/, date.inspect
+  end
+
+  def test_shift
+    date = Date.new(2000, 1, 1)
+    new_date = date >> 11
+    assert_equal 12, new_date.month
+    assert_equal 1, new_date.day
+    assert_equal 1, (date >> 12).day
+    assert_equal 1, (date >> 12).month
+    assert_equal 2001, (date >> 12).year
+
+    assert_raise(TypeError) { date >> "foo" }
+  end
+
+  def test_plus
+    date = Date.new(2000, 1, 1)
+    new_date = date + 31
+    assert_equal 2, new_date.month
+    assert_equal 1, new_date.day
+    assert_equal 2000, new_date.year
+
+    new_date = date + 10.5
+    assert_equal 1, new_date.month
+    assert_equal 11, new_date.day
+    assert_equal 2000, new_date.year
+
+    assert_raise(TypeError) { date + Object.new }
+  end
+
+  def test_day_fraction
+    date = Date.new(2000, 1, 1)
+    assert_equal 0, date.day_fraction
+
+    date = DateTime.new(2000, 1, 10)
+    assert date.day_fraction.eql? Rational(0, 1)
+
+    date = DateTime.new(2000, 10, 10, 12, 24, 36, 48)
+    assert_equal Rational(1241, 2400), date.day_fraction
   end
 
 end
