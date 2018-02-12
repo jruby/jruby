90227748aeeafd9886154e22eb6b8e5ed42e1bf0
Charles Oliver Nutter <headius@headius.com>
2018-02-12 12:36:41 -0600

    Java 7 does not like this type mismatch for some reason.

--- spec/java_integration/fixtures/coll/NonCloneableImmutableList.java
+++ spec/java_integration/fixtures/coll/NonCloneableImmutableList.java
@@ -3,7 +3,7 @@ package java_integration.fixtures.coll;
 import java.util.*;
 
 public final class NonCloneableImmutableList extends AbstractList<Object> {
-    private final List<Object> storage = Arrays.asList(1, 2, 3);
+    private final List<Integer> storage = Arrays.asList(1, 2, 3);
 
     @Override
     public int size() {
