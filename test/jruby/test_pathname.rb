c4afc88b585efa8fe6a5ccc9c8e17a62302e15f2
Charles Oliver Nutter <headius@headius.com>
2018-02-23 13:48:20 -0600

    Eliminate old JRuby tests that use make_tmpname removed by MRI.

--- test/jruby/test_pathname.rb
+++ test/jruby/test_pathname.rb
@@ -10,16 +10,6 @@ class TestPathname < Test::Unit::TestCase
     assert_equal 'some/path', Pathname.new('some/path').dup.to_s
   end
 
-  def test_realpath_symlink; require 'tempfile'
-    target = Tempfile.new 'target'
-    link = Dir::Tmpname.make_tmpname 'link', nil
-    File.symlink(target, link)
-    assert_equal Pathname.new(target).realpath, Pathname.new(link).realpath
-  ensure
-    target.close! if target
-    File.delete(link) if link && File.exists?(link)
-  end unless WINDOWS # Don't have symlinks on Windows.
-
   # GH-3392
   def test_dirname_ending_in_!
     x = "joe"
