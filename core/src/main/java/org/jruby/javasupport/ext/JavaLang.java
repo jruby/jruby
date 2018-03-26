b69aa17d35c104c53b7db03355dbbcc098a76c97
kares <self@kares.org>
2018-03-26 11:38:23 +0200

    [ji] implemenet real? and zero? (Numeric) checks for Java numbers

--- core/src/main/java/org/jruby/javasupport/ext/JavaLang.java
+++ core/src/main/java/org/jruby/javasupport/ext/JavaLang.java
@@ -340,6 +340,15 @@ public abstract class JavaLang {
             return context.runtime.newFloat(val.doubleValue());
         }
 
+        @JRubyMethod(name = "real?")
+        public static IRubyObject real_p(final ThreadContext context, final IRubyObject self) {
+            java.lang.Number val = (java.lang.Number) self.toJava(java.lang.Number.class);
+            return context.runtime.newBoolean(val instanceof Integer || val instanceof Long ||
+                                                    val instanceof Short || val instanceof Byte ||
+                                                    val instanceof Float || val instanceof Double ||
+                                                    val instanceof java.math.BigInteger || val instanceof java.math.BigDecimal);
+        }
+
         @JRubyMethod(name = { "to_i", "to_int" })
         public static IRubyObject to_i(final ThreadContext context, final IRubyObject self) {
             java.lang.Number val = (java.lang.Number) self.toJava(java.lang.Number.class);
@@ -358,6 +367,12 @@ public abstract class JavaLang {
             return context.runtime.newBoolean(val instanceof Integer || val instanceof Long ||
                                                     val instanceof Short || val instanceof Byte ||
                                                     val instanceof java.math.BigInteger);
+        }
+
+        @JRubyMethod(name = "zero?")
+        public static IRubyObject zero_p(final ThreadContext context, final IRubyObject self) {
+            java.lang.Number val = (java.lang.Number) self.toJava(java.lang.Number.class);
+            return context.runtime.newBoolean(Double.compare(val.doubleValue(), 0) == 0);
         }
 
         @JRubyMethod(name = "coerce")
