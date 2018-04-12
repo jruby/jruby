d08501a4e596677aeb685cb765f967d5e49e6abc
Charles Oliver Nutter <headius@headius.com>
2018-04-12 00:36:39 -0500

    Exclude spec that requires callcc.

--- test/mri/excludes/TestBeginEndBlock.rb
+++ test/mri/excludes/TestBeginEndBlock.rb
@@ -1,6 +1,7 @@
 exclude :test_begininclass, "needs investigation"
 exclude :test_begininmethod, "needs investigation"
 exclude :test_beginendblock, "needs investigation"
+exclude :test_callcc_at_exit, "requires callcc"
 exclude :test_propagate_signaled, "needs investigation"
 exclude :test_raise_in_at_exit, "need to run multiple -e as separate top-level scripts (#3674)"
 
