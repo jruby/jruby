f20c4e68e9d747306723dafaa9eb3421d79f3780
Marcin Mielzynski <lopx@gazeta.pl>
2018-04-13 20:59:25 +0200

    update jcodings

--- core/pom.rb
+++ core/pom.rb
@@ -53,7 +53,7 @@ project 'JRuby Core' do
 
   jar 'org.jruby.joni:joni:2.1.15'
   jar 'org.jruby.extras:bytelist:1.0.15'
-  jar 'org.jruby.jcodings:jcodings:1.0.27'
+  jar 'org.jruby.jcodings:jcodings:1.0.29'
   jar 'org.jruby:dirgra:0.3'
 
   jar 'com.headius:invokebinder:1.11'
