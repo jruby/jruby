abac894bd911b9438f3b5dd49ae5646560ad39f7
Charles Oliver Nutter <headius@headius.com>
2018-04-27 09:26:44 -0500

    Set these to proper values.

--- lib/ruby/stdlib/rbconfig/sizeof.rb
+++ lib/ruby/stdlib/rbconfig/sizeof.rb
@@ -9,10 +9,12 @@ module RbConfig
   SIZEOF = sizeof
 
   limits = {}
-  limits['FIXNUM_MAX'] = 0x8000000000000000
-  limits['FIXNUM_MIN'] = 0x7fffffffffffffff
+  limits['FIXNUM_MAX'] = 0x7fffffffffffffff
+  limits['FIXNUM_MIN'] = -0x8000000000000000
   limits['LONG_MAX'] = limits['FIXNUM_MAX']
   limits['LONG_MIN'] = limits['FIXNUM_MIN']
+  limits['INT_MAX'] = 0x7fffffff
+  limits['INT_MIN'] = -0x80000000
   limits.freeze
   LIMITS = limits
 end
