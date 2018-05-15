2f12d0e2f95a55b9885eec8eaab7a42b0a9a19af
Charles Oliver Nutter <headius@headius.com>
2018-05-15 15:05:03 -0500

    Update json + tests to 2.1 and update excludes.

--- test/mri/excludes/TestJSONGenerate.rb
+++
@@ -1,5 +0,0 @@
-exclude :test_broken_bignum, "fails in travis but not locally?"
-exclude :test_gc, "needs investigation"
-exclude :test_hash_likeness_set_string, "needs investigation"
-exclude :test_hash_likeness_set_symbol, "needs investigation"
-exclude :test_string_subclass, "hard call of json blows up; probably fixed in C ext. https://github.com/flori/json/issues/272"
