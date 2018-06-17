b52dcaee03abbbaf1fb1f09f73ee418b3d9d653d
kares <self@kares.org>
2018-06-17 11:04:11 +0200

    [test] adapt uri:classloader testing
    do not assume isolated run - as other tests pollute class-path

--- test/jruby/test_uri_classloader.rb
+++ test/jruby/test_uri_classloader.rb
@@ -19,54 +19,55 @@ class TestURIClassloader < Test::Unit::TestCase
   end
 
   def test_dir_glob_on_uri_classloader_path
-    assert_equal ['uri:classloader://Rakefile'], Dir[ 'uri:classloader://*' ]
+    all_files = Dir[ 'uri:classloader://*' ]
+    assert_include all_files, 'uri:classloader://Rakefile'
 
     ensure_cwd do
       Dir.chdir( 'uri:classloader://' )
-      assert_equal ['Rakefile'], Dir[ '*' ]
+      assert_include Dir[ '*' ], 'Rakefile'
     end
 
     ensure_cwd do
       JRuby.runtime.current_directory = 'uri:classloader://'
-      assert_equal ['Rakefile'], Dir[ '*' ]
+      assert_include Dir[ '*' ], 'Rakefile'
     end
   end
 
   def test_dir_glob_on_uri_classloader_path_with_dot
-    assert_equal ['uri:classloader://./Rakefile'], Dir[ 'uri:classloader://./*' ]
+    assert_include Dir[ 'uri:classloader://./*' ], 'uri:classloader://./Rakefile'
 
     ensure_cwd do
       Dir.chdir( 'uri:classloader://.' )
-      assert_equal ['Rakefile'], Dir[ '*' ]
+      assert_include Dir[ '*' ], 'Rakefile'
     end
 
     ensure_cwd do
       JRuby.runtime.current_directory = 'uri:classloader://.'
-      assert_equal ['Rakefile'], Dir[ '*' ]
+      assert_include Dir[ '*' ], 'Rakefile'
     end
   end
 
   def test_dir_glob_on_uri_classloader_path_with_dot_dot
-    assert_equal ['uri:classloader://lib/../Rakefile'], Dir[ 'uri:classloader://lib/../*' ]
+    assert_include Dir[ 'uri:classloader://lib/../*' ], 'uri:classloader://lib/../Rakefile'
 
     ensure_cwd do
       JRuby.runtime.current_directory = 'uri:classloader://lib/..'
-      assert_equal ['Rakefile'], Dir[ '*' ]
+      assert_include Dir[ '*' ], 'Rakefile'
     end
 
     ensure_cwd do
       Dir.chdir 'uri:classloader://lib/..'
-      assert_equal ['Rakefile'], Dir[ '*' ]
+      assert_include Dir[ '*' ], 'Rakefile'
     end
 
     ensure_cwd do
       JRuby.runtime.current_directory = 'uri:classloader://'
-      assert_equal ['lib/../Rakefile'], Dir[ 'lib/../*' ]
+      assert_include Dir[ 'lib/../*' ], 'lib/../Rakefile'
     end
 
     ensure_cwd do
       Dir.chdir 'uri:classloader://'
-      assert_equal ['lib/../Rakefile'], Dir[ 'lib/../*' ]
+      assert_include Dir[ 'lib/../*' ], 'lib/../Rakefile'
     end
   end
 end
