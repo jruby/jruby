d1dfcb8e5ae386603241053e605e06ea507a123c
Charles Oliver Nutter <headius@headius.com>
2018-07-12 00:34:00 -0500

    Set your sights higher.

--- test/jruby/test_timeout.rb
+++ test/jruby/test_timeout.rb
@@ -11,7 +11,7 @@ class TestTimeout < Test::Unit::TestCase
   def test_timeout_for_loop
     n = 10000000
     assert_raises(Timeout::Error) do
-      Timeout.timeout(1) { for i in 0..n do; (i + i % (i+1)) % (i + 10) ; end }
+      Timeout.timeout(0.1) { for i in 0..n do; (i + i % (i+1)) % (i + 10) ; end }
     end
   end
 
