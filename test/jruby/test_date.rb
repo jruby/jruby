jruby/jruby

commit 8c3a6691125db340d9833aeb1500490b2feddd25

replace if-else with switch + use call-sites on to_i/int conversion

core/src/main/java/org/jruby/util/TypeConverter.java
@@ -393,21 +393,22 @@ public class TypeConverter {
 
     // rb_convert_to_integer
     public static IRubyObject convertToInteger(ThreadContext context, IRubyObject val, int base) {
-        Ruby runtime = context.runtime;
+        final Ruby runtime = context.runtime;
         IRubyObject tmp;
 
         for (;;) {
-            if (val instanceof RubyFloat) {
-                if (base != 0) raiseIntegerBaseError(context);
-                return RubyNumeric.dbl2ival(context.runtime, ((RubyFloat) val).getValue());
-            } else if (val instanceof RubyInteger) {
-                if (base != 0) raiseIntegerBaseError(context);
-                return val;
-            } else if (val instanceof RubyString) {
-                return RubyNumeric.str2inum(context.runtime, (RubyString)val, base, true);
-            } else if (val instanceof RubyNil) {
-                if (base != 0) raiseIntegerBaseError(context);
-                throw context.runtime.newTypeError("can't convert nil into Integer");
+            switch (val.getMetaClass().getClassIndex()) {
+                case FLOAT:
+                    if (base != 0) raiseIntegerBaseError(context);
+                    return RubyNumeric.dbl2ival(context.runtime, ((RubyFloat) val).getValue());
+                case INTEGER:
+                    if (base != 0) raiseIntegerBaseError(context);
+                    return val;
+                case STRING:
+                    return RubyNumeric.str2inum(context.runtime, (RubyString) val, base, true);
+                case NIL:
+                    if (base != 0) raiseIntegerBaseError(context);
+                    throw context.runtime.newTypeError("can't convert nil into Integer");
             }
 
             if (base != 0) {
@@ -419,22 +420,22 @@ public class TypeConverter {
             break;
         }
 
-        tmp = TypeConverter.convertToType(context, val, runtime.getString(), "to_int", false);
-        return (tmp != context.nil) ? tmp : val.convertToInteger("to_i");
+        tmp = TypeConverter.convertToType(context, val, runtime.getInteger(), sites(context).to_int_checked, false);
+        return (tmp != context.nil) ? tmp : TypeConverter.convertToType(context, val, runtime.getInteger(), sites(context).to_i_checked);
     }
 
     // MRI: rb_Array
     public static RubyArray rb_Array(ThreadContext context, IRubyObject val) {
         IRubyObject tmp = checkArrayType(val); // to_ary
 
-        if (tmp.isNil()) {
+        if (tmp == context.nil) {
             TypeConverterSites sites = sites(context);
             tmp = convertToTypeWithCheck(context, val, context.runtime.getArray(), sites.to_a_checked);
-            if (tmp.isNil()) {
+            if (tmp == context.nil) {
                 return context.runtime.newArray(val);
             }
         }
-        return (RubyArray)tmp;
+        return (RubyArray) tmp;
     }
 
     // MRI: to_ary

commit 5470f056fa635b50945bc0345e7779b27ba2e517

prevent an infinite loop, check string subclasses
regressed since 8c3a6691125db340d9833aeb1500490b2feddd25
resolves GH-5252

core/src/main/java/org/jruby/util/TypeConverter.java
@@ -409,6 +409,10 @@ public class TypeConverter {
                 case NIL:
                     if (base != 0) raiseIntegerBaseError(context);
                     throw context.runtime.newTypeError("can't convert nil into Integer");
+                default: // MRI checks String sub-classes
+                    if (val instanceof RubyString) {
+                        return RubyNumeric.str2inum(context.runtime, (RubyString) val, base, true);
+                    }
             }
 
             if (base != 0) {

test/jruby/test_date.rb
@@ -766,6 +766,18 @@ class TestDate < Test::Unit::TestCase
     end
   end
 
+  class SubStr < String; end
+
+  def test_parse_string_sub
+    d = Date.iso8601(:'2018-07-17')
+    assert d.is_a?(Date)
+
+    str = SubStr.new('2018-07-17')
+    d = Date.iso8601(str)
+    assert_equal Date.new(2018, 7, 17), d
+    assert Date._parse(str)
+  end
+
   def test_jd_day_fraction
     t = 86400 * DateTime.new(1970, 1, 1).jd + Time.utc(2018, 3, 18, 23).to_i
     dt = DateTime.jd((t + 0)/86400r)