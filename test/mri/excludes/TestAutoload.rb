dad052a8398a6f62d64b82a76e30091171102175
Charles Oliver Nutter <headius@headius.com>
2018-08-28 16:41:43 -0500

    Exclude racey autoload test from #5294.

--- test/mri/excludes/TestAutoload.rb
+++ test/mri/excludes/TestAutoload.rb
@@ -1,3 +1,3 @@
-#exclude :test_autoload_p, "needs investigation"
 exclude :test_autoload_while_autoloading, "needs investigation"
+exclude :test_bug_13526, "racey test that doesn't behave as expected with concurrent threads #5294"
 exclude :test_require_implemented_in_ruby_is_called, "attempted to dispatch to require but it seems to recursively call itself"
