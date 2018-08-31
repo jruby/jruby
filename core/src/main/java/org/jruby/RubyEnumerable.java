88980f0528b44d7c993c998e2a00823150a6ab64
Miguel Landaeta <miguel@miguel.cc>
2018-08-31 22:31:11 +0100

    Fix some failed Enumerable#find_index specs
    This change removes the assumption that #each would yield passing always
    a single object. It could yield without passing parameters, passing one
    or more objects or yield an explicit array (empty or not).

--- core/src/main/java/org/jruby/RubyEnumerable.java
+++ core/src/main/java/org/jruby/RubyEnumerable.java
@@ -706,8 +706,7 @@ public class RubyEnumerable {
         try {
             callEach(runtime, context, self, callbackArity, new BlockCallback() {
                 public IRubyObject call(ThreadContext ctx, IRubyObject[] largs, Block blk) {
-                    IRubyObject larg = packEnumValues(runtime, largs);
-                    if (block.yield(ctx, larg).isTrue()) throw JumpException.SPECIAL_JUMP;
+                    if (block.yieldValues(ctx, largs).isTrue()) throw JumpException.SPECIAL_JUMP;
                     result[0]++;
                     return runtime.getNil();
                 }
@@ -745,7 +744,7 @@ public class RubyEnumerable {
         final long result[] = new long[] {0};
 
         try {
-            callEach(runtime, context, self, Signature.ONE_ARGUMENT, new BlockCallback() {
+            callEach(runtime, context, self, Signature.OPTIONAL, new BlockCallback() {
                 public IRubyObject call(ThreadContext ctx, IRubyObject[] largs, Block blk) {
                     IRubyObject larg = packEnumValues(ctx, largs);
                     if (equalInternal(ctx, larg, cond)) throw JumpException.SPECIAL_JUMP;
