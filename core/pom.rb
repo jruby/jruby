13a3ec76d883a162b9d46c374c6e9eeea27b3261
Charles Oliver Nutter <headius@headius.com>
2018-09-14 16:26:21 -0500

    Update to modularized jcodings 1.0.34.

--- core/pom.rb
+++ core/pom.rb
@@ -52,7 +52,7 @@ project 'JRuby Core' do
   jar 'com.github.jnr:jffi:${jffi.version}:native'
 
   jar 'org.jruby.joni:joni:2.1.18'
-  jar 'org.jruby.jcodings:jcodings:1.0.32'
+  jar 'org.jruby.jcodings:jcodings:1.0.34'
   jar 'org.jruby:dirgra:0.3'
 
   jar 'com.headius:invokebinder:1.11'
