2f35d31b0e43a6c3452f1d6dc6957446d7256700
Charles Oliver Nutter <headius@headius.com>
2018-09-25 17:32:27 -0500

    Mimic Fiber finalization test since they use the same pool.

--- spec/regression/GH-1076_enumerator_finalization_needs_a_spec.rb
+++ spec/regression/GH-1076_enumerator_finalization_needs_a_spec.rb
@@ -7,41 +7,43 @@ require 'rspec'
 
 describe "An Enumerator that has been abandoned" do
   it "cleans itself up properly" do
-    thread_bean = java.lang.management.ManagementFactory.thread_mx_bean
-    
-    # Thread count before enumerators
-    thread_count = thread_bean.thread_count
-    
-    # Attempt to create and abandon 10000 enumerators. This should blow up quickly
-    # if they are not cleaning up their threads properly.
-    100.times do
+    begin
+      thread_bean = java.lang.management.ManagementFactory.thread_mx_bean
+      max_threads = JRuby.runtime.fiber_executor.maximum_pool_size
+
+      # Thread count before enumerators
+      thread_count = thread_bean.thread_count
+
+      # Attempt to create and abandon 10000 enumerators. This should blow up quickly
+      # if they are not cleaning up their threads properly.
       100.times do
-        Enumerator.new {|y| y.yield(1); y.yield(2) }.next
+        100.times do
+          Enumerator.new {|y| y.yield(1); y.yield(2) }.next
+        end
+        JRuby.gc
       end
-      JRuby.gc
+
+      # Allow GC and other threads plenty of time to clean up
+      10.times do
+        JRuby.gc
+      end
+
+      # Try to force finalizers to run
+      java.lang.Runtime.runtime.run_finalization
+
+      # Spin for a while, hoping to make this pass
+      1000.times do
+        JRuby.gc
+        JRuby.runtime.fiber_executor.maximum_pool_size = 1
+        break if (thread_bean.thread_count - thread_count) < 10
+        Thread.pass
+      end
+
+      # Final thread count should be within ~10 threads of original (allowing for
+      # JVM GC, finalizer, reference queue, etc threads to have spun up).
+      expect(thread_bean.thread_count - thread_count).to be < 10
+    ensure
+      JRuby.runtime.fiber_executor.maximum_pool_size = max_threads
     end
-    
-    # Allow GC and other threads plenty of time to clean up
-    10.times do
-      JRuby.gc
-    end
-    
-    # Try to force finalizers to run
-    java.lang.Runtime.runtime.run_finalization
-    
-    # Spin for a while, hoping to make this pass
-    1000.times do
-      break if (thread_bean.thread_count - thread_count) < 200
-      Thread.pass
-    end
-    
-    # Final thread count should be within ~200 threads of original. Because
-    # Enumerator threads use a thread pool, proper cleanup of the above loop
-    # will leave around 100 threads alive in that pool for each iteration. These
-    # threads are just waiting for more work, and will age out on their own...
-    # but waiting for that here would take too long. Using 200 is a safe
-    # threshold, since if enumerators are not being cleaned up we should have
-    # thousands of extra threads here.
-    expect(thread_bean.thread_count - thread_count).to be < 200
   end
 end
