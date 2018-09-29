d5c4a5240d0a1553540ec84328175acdefa6ea8d
kares <self@kares.org>
2018-09-29 15:15:01 +0200

    [test] get load xxx.rb before xxx.class test running

--- test/jruby/test_load.rb
+++ test/jruby/test_load.rb
@@ -344,3 +344,22 @@ DEPS
   end
 
 end
+
+# This test is for JRUBY-3029, testing that each path is searched for .class and .rb before
+# proceeding to the next path. The .class source loaded has different code from the .rb
+# version, but to make them load and be testable in a specific way. I include the source
+# of the .class version here...
+
+=begin
+$jruby3029 = 'class'
+=end
+
+TestLoad.class_eval do
+  def test_load_class_before_rb
+    $LOAD_PATH << File.expand_path('../dir1', File.dirname(__FILE__))
+    $LOAD_PATH << File.expand_path('../dir2', File.dirname(__FILE__))
+
+    require 'target'
+    assert_equal 'rb', $jruby3029
+  end
+end
