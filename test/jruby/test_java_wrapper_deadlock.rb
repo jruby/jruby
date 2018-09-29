375d5c8e1a806436fab179e94fed331d5227b16e
kares <self@kares.org>
2018-09-29 15:16:06 +0200

    [test] move JRUBY-4538 test into TestArray

--- test/jruby/test_java_wrapper_deadlock.rb
+++ test/jruby/test_java_wrapper_deadlock.rb
@@ -3,10 +3,9 @@ require 'java'
 
 class TestJavaWrapperDeadlock < Test::Unit::TestCase
   import org.jruby.test.Runner
-  import java.lang.Runnable
 
   def test_deadlock_due_to_java_object_wrapping_locking_on_java_instances
-    Runner.getRunner.runJob Runnable.impl {
+    Runner.getRunner.runJob java.lang.Runnable.impl {
       Thread.new do
         runner = Runner.getRunner
         assert runner.isRunning, "runner should be running"
