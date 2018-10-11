7e7d7af33e06dd91c215329fbf5e8c8f161ede9b
Charles Oliver Nutter <headius@headius.com>
2018-10-11 13:35:52 -0500

    Fix arity of this call to timeout.

--- test/jruby/test_timeout.rb
+++ test/jruby/test_timeout.rb
@@ -112,7 +112,7 @@ class TestTimeout < Test::Unit::TestCase
         end
       end
     end
-    assert cls.new.timeout, "timeout should have returned 42"
+    assert cls.new.timeout(0), "timeout should have returned 42"
   end
 
   class Seconds
