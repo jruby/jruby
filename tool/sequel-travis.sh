27cae5b53478e09c8654d0be0bec211797e04293
Charles Oliver Nutter <headius@headius.com>
2018-11-08 17:54:48 -0600

    Adding sequel to build matrix.

---
+++ tool/sequel-travis.sh
@@ -0,0 +1,16 @@
+# set up JRuby
+mvn clean package
+export PATH=`pwd`/bin:$PATH
+gem install bundler
+
+# set up databases
+mysql -e 'create database sequel_test;'
+psql -c 'create database sequel_test;' -U postgres
+
+# set up sequel
+git clone https://github.com/jeremyevans/sequel.git
+cd sequel
+bundle install
+
+# run tests
+bundle exec rake spec_travis
