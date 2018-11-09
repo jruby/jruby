dbf5dfe319f7ff19aca1adc07cd656d04eaf0456
Charles Oliver Nutter <headius@headius.com>
2018-11-08 19:02:10 -0600

    Reduce depth of sequel clone.

--- tool/sequel-travis.sh
+++ tool/sequel-travis.sh
@@ -11,7 +11,7 @@ mysql -e 'create database sequel_test;'
 psql -c 'create database sequel_test;' -U postgres
 
 # set up sequel
-git clone https://github.com/jeremyevans/sequel.git
+git clone --depth=10 https://github.com/jeremyevans/sequel.git
 cd sequel
 cp .travis.gemfile Gemfile
 bundle install
