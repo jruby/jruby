56b03e1bd683ddb1fbde81dca2f7a0a057810523
kares <self@kares.org>
2018-12-07 10:29:32 +0100

    cleanup socket addr impl - avoid reflection due iface.index

--- core/src/main/java/org/jruby/ext/socket/Addrinfo.java
+++ core/src/main/java/org/jruby/ext/socket/Addrinfo.java
@@ -34,8 +34,6 @@ import java.net.NetworkInterface;
 import java.net.SocketAddress;
 import java.net.UnknownHostException;
 
-import org.jruby.exceptions.RaiseException;
-
 import org.jruby.util.TypeConverter;
 import org.jruby.util.io.Sockaddr;
 
@@ -211,8 +209,6 @@ public class Addrinfo extends RubyObject {
                 }
 
             } else {
-
-                RubyString sockaddrString = sockaddr.convertToString();
 
                 InetAddress inetAddress;
 
@@ -664,7 +660,7 @@ public class Addrinfo extends RubyObject {
         return SocketUtilsIPV6.getIPV6Address(in.getHostAddress());
     }
 
-    private InetAddress getRubyInetAddress(IRubyObject node) {
+    private static InetAddress getRubyInetAddress(IRubyObject node) {
         try {
             return SocketUtils.getRubyInetAddresses(node.convertToString().getByteList())[0];
         } catch (UnknownHostException uhe) {
--- core/src/main/java/org/jruby/ext/socket/Ifaddr.java
+++ core/src/main/java/org/jruby/ext/socket/Ifaddr.java
@@ -1,6 +1,5 @@
 package org.jruby.ext.socket;
 
-import java.lang.reflect.Field;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
@@ -8,6 +7,7 @@ import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+
 import org.jruby.Ruby;
 import org.jruby.RubyClass;
 import org.jruby.RubyObject;
@@ -23,9 +23,9 @@ import org.jruby.runtime.builtin.IRubyObject;
 public class Ifaddr extends RubyObject {
 
     private String name;
-    private Boolean isUp;
-    private Boolean isLoopback;
-    private Boolean isPointToPoint;
+    private boolean isUp;
+    private boolean isLoopback;
+    private boolean isPointToPoint;
     private InetAddress address;
     private InetAddress broadcast;
     private InterfaceAddress interfaceAddress;
@@ -52,12 +52,13 @@ public class Ifaddr extends RubyObject {
         super(runtime, metaClass);
     }
 
-    public Ifaddr(Ruby runtime, RubyClass metaClass, NetworkInterface ni, InterfaceAddress it) throws Exception {
+    public Ifaddr(Ruby runtime, RubyClass metaClass, NetworkInterface ni, InterfaceAddress it) throws SocketException {
         super(runtime, metaClass);
-        this.isUp = ni.isUp();
         this.name = ni.getDisplayName();
         this.isLoopback = ni.isLoopback();
+        this.isUp = ni.isUp();
         this.isPointToPoint = ni.isPointToPoint();
+        this.index = ni.getIndex();
         this.networkInterface = ni;
         this.isLink = false;
         this.address = it.getAddress();
@@ -65,20 +66,19 @@ public class Ifaddr extends RubyObject {
         this.interfaceAddress = it;
         setAddr(runtime);
         setNetmask(it);
-        setIndex(ni);
         setInspectString(ni);
     }
 
-    public Ifaddr(Ruby runtime, RubyClass metaClass, NetworkInterface ni) throws Exception {
+    public Ifaddr(Ruby runtime, RubyClass metaClass, NetworkInterface ni) throws SocketException {
         super(runtime, metaClass);
-        this.isUp = ni.isUp();
         this.name = ni.getDisplayName();
         this.isLoopback = ni.isLoopback();
+        this.isUp = ni.isUp();
         this.isPointToPoint = ni.isPointToPoint();
+        this.index = ni.getIndex();
         this.networkInterface = ni;
         this.isLink = true;
         setAddr(runtime);
-        setIndex(ni);
         setInspectString(ni);
     }
 
@@ -145,23 +145,17 @@ public class Ifaddr extends RubyObject {
         }
     }
 
-    private void setNetmask(InterfaceAddress it) throws Exception {
-      if ( ( isLoopback || ( it.getNetworkPrefixLength() != 0 ) ) && ( address instanceof Inet4Address) ) {
-        String subnet = ipAddress() + "/" + it.getNetworkPrefixLength();
-        if ( isLoopback ) {
-          subnet = ipAddress() + "/8";   // because getNetworkPrefixLength() incorrectly returns 0 for IPv4 loopback
+    private void setNetmask(InterfaceAddress it) {
+        if ( ( isLoopback || ( it.getNetworkPrefixLength() != 0 ) ) && ( address instanceof Inet4Address) ) {
+            String subnet = ipAddress() + "/" + it.getNetworkPrefixLength();
+            if ( isLoopback ) {
+                subnet = ipAddress() + "/8"; // because getNetworkPrefixLength() incorrectly returns 0 for IPv4 loopback
+            }
+            SubnetUtils utils = new SubnetUtils(subnet);
+            netmask = utils.getInfo().getNetmask();
+        } else if ( (it.getNetworkPrefixLength() != 0 ) && ( address instanceof Inet6Address) ) {
+            netmask = SocketUtilsIPV6.getIPV6NetMask(ipAddress() + "/" + it.getNetworkPrefixLength());
         }
-        SubnetUtils utils = new SubnetUtils(subnet);
-        netmask = utils.getInfo().getNetmask();
-      } else if ( (it.getNetworkPrefixLength() != 0 ) && ( address instanceof Inet6Address) ) {
-        netmask = SocketUtilsIPV6.getIPV6NetMask(ipAddress() + "/" + it.getNetworkPrefixLength());
-      }
-    }
-
-    private void setIndex(NetworkInterface ni) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-        Field field = ni.getClass().getDeclaredField("index");
-        field.setAccessible(true);
-        index = (Integer) field.get(ni);
     }
 
     private void setInspectString(NetworkInterface nif) throws SocketException {
@@ -206,19 +200,17 @@ public class Ifaddr extends RubyObject {
 
     private String ipAddress() {
         if (address instanceof Inet4Address) {
-            final String addr = address.toString();
-            return addr.substring(1, addr.length());
-        } else if ((address instanceof Inet6Address)) {
-            final String addr = address.toString();
-            return addr.substring(1, addr.length()).split("%")[0];
+            return address.toString().substring(1);
+        }
+        if ((address instanceof Inet6Address)) {
+            return address.toString().substring(1).split("%")[0];
         }
         return "";
     }
 
     private String getBroadcastAsString() {
         if (broadcast == null) return "";
-        final String brdc = broadcast.toString();
-        return brdc.substring(1, brdc.length());
+        return broadcast.toString().substring(1);
     }
 
 }
