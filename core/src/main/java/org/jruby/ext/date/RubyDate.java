5e4a36e744feb115dfb8c2d209122329b34ad2f6
kares <self@kares.org>
2019-01-29 22:14:08 +0100

    [fix] interpret today TZ-relative but use GJ chrono
    this should match the logic that was in place in 9.1.17.0
    expected to, finally and properly, resolve GH-5418

--- core/src/main/java/org/jruby/ext/date/RubyDate.java
+++ core/src/main/java/org/jruby/ext/date/RubyDate.java
@@ -78,7 +78,6 @@ public class RubyDate extends RubyObject {
 
     static final Logger LOG = LoggerFactory.getLogger(RubyDate.class);
 
-    static final GJChronology CHRONO_ITALY = GJChronology.getInstance();
     static final GJChronology CHRONO_ITALY_UTC = GJChronology.getInstance(DateTimeZone.UTC);
 
     // The Julian Day Number of the Day of Calendar Reform for Italy
@@ -714,13 +713,19 @@ public class RubyDate extends RubyObject {
 
     @JRubyMethod(meta = true)
     public static RubyDate today(ThreadContext context, IRubyObject self) { // sg=ITALY
-        return new RubyDate(context.runtime, (RubyClass) self, new DateTime(CHRONO_ITALY).withTimeAtStartOfDay());
+        return new RubyDate(context.runtime, (RubyClass) self, todayDate(context, CHRONO_ITALY_UTC));
     }
 
     @JRubyMethod(meta = true)
     public static RubyDate today(ThreadContext context, IRubyObject self, IRubyObject sg) {
         final long start = val2sg(context, sg);
-        return new RubyDate(context.runtime, (RubyClass) self, new DateTime(getChronology(context, start, 0)).withTimeAtStartOfDay(), 0, start);
+        final Chronology chrono = getChronology(context, start, 0);
+        return new RubyDate(context.runtime, (RubyClass) self, todayDate(context, chrono), 0, start);
+    }
+
+    private static DateTime todayDate(final ThreadContext context, final Chronology chrono) {
+        org.joda.time.LocalDate today = new org.joda.time.LocalDate(RubyTime.getLocalTimeZone(context.runtime));
+        return new DateTime(today.getYear(), today.getMonthOfYear(), today.getDayOfMonth(), 0, 0, chrono);
     }
 
     @JRubyMethod(name = "_valid_civil?", meta = true, required = 3, optional = 1)
