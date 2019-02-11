72a3a44964c64fc0cd5be83d37e59dd67323e64a
Charles Oliver Nutter <headius@headius.com>
2019-02-11 12:28:06 -0600

    Use bounded call to `caller` to get partial trace on Java 9+.

--- core/src/main/ruby/jruby/kernel/kernel.rb
+++ core/src/main/ruby/jruby/kernel/kernel.rb
@@ -4,7 +4,7 @@ module Kernel
     relative_arg = relative_arg.to_path if relative_arg.respond_to? :to_path
     relative_arg = JRuby::Type.convert_to_str(relative_arg)
 
-    caller.first.rindex(/:\d+:in /)
+    caller(1, 1).first.rindex(/:\d+:in /)
     file = $` # just the filename
     raise LoadError, "cannot infer basepath" if /\A\((.*)\)/ =~ file # eval etc.
 
