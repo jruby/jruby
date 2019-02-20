6d631b4ef03bc726bdc5b3370dbedeaeb6147642
kares <self@kares.org>
2019-02-20 15:27:01 +0100

    [test] merge single purpose jruby tests

--- test/jruby/test_array.rb
+++ test/jruby/test_array.rb
@@ -132,4 +132,40 @@ class TestArray < Test::Unit::TestCase
     assert_equal(MyArray, @arr.dup.class)
   end
 
+  class Foo1
+    def initialize
+      @ary = [1,2,3]
+    end
+
+    def ==(other)
+      @ary == other
+    end
+
+    def to_ary
+      @ary
+    end
+  end
+
+  class Foo2
+    def initialize
+      @ary = [1,2,3]
+    end
+
+    def ==(other)
+      @ary == other
+    end
+  end
+
+  def test_delegated_array_equals
+    a = Foo1.new
+    assert_equal(a, a)
+    assert(a == a)
+  end
+
+  def test_badly_delegated_array_equals
+    a = Foo2.new
+    assert_not_equal(a, a)
+    assert(!(a == a))
+  end
+
 end
