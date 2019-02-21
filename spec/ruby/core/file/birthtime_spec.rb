d629c9464791d01f20f5cb805c45f287b06de1c1
Benoit Daloze <eregontp@gmail.com>
2019-02-21 16:47:20 +0100

    Update to ruby/spec@7a16e01

--- spec/ruby/core/file/birthtime_spec.rb
+++ spec/ruby/core/file/birthtime_spec.rb
@@ -24,11 +24,13 @@ describe "File.birthtime" do
     end
   end
 
-  platform_is :linux, :openbsd do
+  platform_is :openbsd do
     it "raises an NotImplementedError" do
       lambda { File.birthtime(@file) }.should raise_error(NotImplementedError)
     end
   end
+
+  # TODO: depends on Linux kernel version
 end
 
 describe "File#birthtime" do
--- spec/ruby/core/numeric/step_spec.rb
+++ spec/ruby/core/numeric/step_spec.rb
@@ -60,6 +60,11 @@ describe "Numeric#step" do
               enum.size.should == Float::INFINITY
             end
           end
+
+          it "defaults to an infinite size" do
+            enum = 1.step
+            enum.size.should == Float::INFINITY
+          end
         end
 
         describe "type" do
@@ -114,7 +119,7 @@ describe "Numeric#step" do
             1.step(to: Float::INFINITY, by: 42).size.should == infinity_value
           end
 
-          it "should return infinity_value when decending towards a limit of -Float::INFINITY" do
+          it "should return infinity_value when descending towards a limit of -Float::INFINITY" do
             1.step(to: -Float::INFINITY, by: -42).size.should == infinity_value
           end
 
