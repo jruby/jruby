e0d0701ae9af25528be798c5c505a310bdb0bb7a
kares <self@kares.org>
2019-02-21 12:21:19 +0100

    [test] that JRuby doesn't do much JI on boot
    including most libraries (to avoid future slow-downs)

--- test/jruby/test_loading_builtin_libraries.rb
+++ test/jruby/test_loading_builtin_libraries.rb
@@ -18,6 +18,8 @@ class TestLoadingBuiltinLibraries < Test::Unit::TestCase
       require 'bigdecimal'
       require 'io/wait'
       require 'etc'
+      require 'rbconfig'
+      require 'tempfile'
     }
   end
 
@@ -32,6 +34,63 @@ class TestLoadingBuiltinLibraries < Test::Unit::TestCase
       require 'jruby/ext'
       require 'java'
     }
+  end if defined? JRUBY_VERSION
+
+  def test_does_not_load_ji_on_boot
+    code  = "all = []; ObjectSpace.each_object(Module) { |mod| all << mod }; "
+    code += "p all.count { |m| m.is_a?(Java::JavaPackage) }; " # <= 3
+    # org.jruby.java.util (SystemPropertiesMap) and dependencies :
+    # java.util (Map) implemented interface
+    # java.lang (Object) super-class
+    code += "all.each { |m| m.inspect }; " # if self-reflecting this would fail (on RubyBasicObject.UNDEF)
+
+    out = `#{RbConfig.ruby} -e '#{code}'`
+    assert $?.success?, "JRuby self-reflected (JI) during boot!"
+    pkg_count = out.strip.to_i
+    assert pkg_count <= 3 # due ENV_JAVA we (still) load 3 Java packages - see ^^^
+
+    requires = [ 'stringio',
+                 'rbconfig',
+                 'enumerator',
+                 'set',
+                 'time',
+                 'cgi',
+                 'date',
+                 'digest',
+                 'digest/md5',
+                 'digest/sha1',
+                 'digest/sha2',
+                 'drb',
+                 'etc',
+                 'ffi',
+                 'fiber',
+                 'irb',
+                 'io/wait',
+                 'bigdecimal',
+                 'ostruct',
+                 'open3',
+                 'pathname',
+                 'pp',
+                 'pty',
+                 'rational',
+                 'securerandom',
+                 'socket',
+                 'strscan',
+                 'thread',
+                 'tempfile',
+                 'tmpdir',
+                 'timeout',
+                 'zlib',
+
+                 'jar-dependencies',
+                 'psych',
+                 'openssl',
+                 'yaml',
+    ]
+    requires = requires.map { |lib| "-r#{lib}" }.join(' ')
+    out = `#{RbConfig.ruby} #{requires} -e '#{code}'`
+    assert $?.success?, "a library self-reflected (JI) during boot!"
+    assert_equal pkg_count.to_s, out.strip
   end if defined? JRUBY_VERSION
 
 end
