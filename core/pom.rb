fab91d4044bad87e3bc862b354c8dd938faae440
Marcin Mielzynski <lopx@gazeta.pl>
2019-03-06 20:01:34 +0100

    bump jcodings 1.0.43 and joni 2.1.26

--- core/pom.rb
+++ core/pom.rb
@@ -49,8 +49,8 @@ project 'JRuby Core' do
   jar 'com.github.jnr:jffi:${jffi.version}'
   jar 'com.github.jnr:jffi:${jffi.version}:native'
 
-  jar 'org.jruby.joni:joni:2.1.25'
-  jar 'org.jruby.jcodings:jcodings:1.0.41'
+  jar 'org.jruby.joni:joni:2.1.26'
+  jar 'org.jruby.jcodings:jcodings:1.0.43'
   jar 'org.jruby:dirgra:0.3'
 
   jar 'com.headius:invokebinder:1.11'
