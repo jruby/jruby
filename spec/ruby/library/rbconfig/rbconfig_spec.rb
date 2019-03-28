8d73050d4e373d0abc61256eb33b92618836584b
Benoit Daloze <eregontp@gmail.com>
2019-03-28 22:24:32 +0100

    Update to ruby/spec@e81b3cd

--- spec/ruby/library/rbconfig/rbconfig_spec.rb
+++ spec/ruby/library/rbconfig/rbconfig_spec.rb
@@ -9,15 +9,18 @@ describe 'RbConfig::CONFIG' do
     end
   end
 
-  it "['rubylibdir'] returns the directory containing Ruby standard libraries" do
-    rubylibdir = RbConfig::CONFIG['rubylibdir']
-    File.directory?(rubylibdir).should == true
-    File.exist?("#{rubylibdir}/fileutils.rb").should == true
-  end
+  # These directories have no meanings before the installation.
+  guard -> { RbConfig::TOPDIR } do
+    it "['rubylibdir'] returns the directory containing Ruby standard libraries" do
+      rubylibdir = RbConfig::CONFIG['rubylibdir']
+      File.directory?(rubylibdir).should == true
+      File.exist?("#{rubylibdir}/fileutils.rb").should == true
+    end
 
-  it "['archdir'] returns the directory containing standard libraries C extensions" do
-    archdir = RbConfig::CONFIG['archdir']
-    File.directory?(archdir).should == true
-    File.exist?("#{archdir}/etc.#{RbConfig::CONFIG['DLEXT']}").should == true
+    it "['archdir'] returns the directory containing standard libraries C extensions" do
+      archdir = RbConfig::CONFIG['archdir']
+      File.directory?(archdir).should == true
+      File.exist?("#{archdir}/etc.#{RbConfig::CONFIG['DLEXT']}").should == true
+    end
   end
 end
