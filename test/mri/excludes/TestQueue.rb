2c869c41425afb105d5ecae6b9111faa56bf96bd
Charles Oliver Nutter <headius@headius.com>
2019-04-25 17:25:34 -0500

    Another attempt, remove case option test altogether.

--- test/mri/excludes/TestQueue.rb
+++ test/mri/excludes/TestQueue.rb
@@ -1,3 +1,2 @@
 exclude :test_dump, "tests MRI-specific exception"
 exclude :test_queue_close_multi_multi, "flakey on Travis: no threads runnning"
-exclude :test_queue_with_trap, "flakey on Travis: intermittently hangs"
