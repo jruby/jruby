56e7348efee4b1c16c7126aa2d4105dccff571d8
Charles Oliver Nutter <headius@headius.com>
2019-05-15 17:15:31 -0500

    Deprecate accessors for Java interface constants.
    See #5730.

--- core/src/main/java/org/jruby/javasupport/binding/StaticFieldGetterInstaller.java
+++ core/src/main/java/org/jruby/javasupport/binding/StaticFieldGetterInstaller.java
@@ -9,14 +9,17 @@ import java.lang.reflect.Field;
 * Created by headius on 2/26/15.
 */
 public class StaticFieldGetterInstaller extends FieldInstaller {
+    final boolean warnConstant;
 
-    public StaticFieldGetterInstaller(String name, Field field) {
+    public StaticFieldGetterInstaller(String name, Field field, boolean warnConstant) {
         super(name, STATIC_FIELD, field);
+
+        this.warnConstant = warnConstant;
     }
 
     @Override void install(final RubyModule proxy) {
         if (isAccessible()) {
-            proxy.getSingletonClass().addMethod(name, new StaticFieldGetter(name, proxy, field));
+            proxy.getSingletonClass().addMethod(name, new StaticFieldGetter(name, proxy, field, warnConstant));
         }
     }
 }
