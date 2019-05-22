856e8accc60f7c522ad5f7656a25abfd02f04eb7
Marcin Mielzynski <lopx@gazeta.pl>
2019-05-22 17:07:52 +0200

    update joni to 2.1.28

--- core/pom.rb
+++ core/pom.rb
@@ -49,7 +49,7 @@ project 'JRuby Core' do
   jar 'com.github.jnr:jffi:${jffi.version}'
   jar 'com.github.jnr:jffi:${jffi.version}:native'
 
-  jar 'org.jruby.joni:joni:2.1.26'
+  jar 'org.jruby.joni:joni:2.1.28'
   jar 'org.jruby.jcodings:jcodings:1.0.44'
   jar 'org.jruby:dirgra:0.3'
 
