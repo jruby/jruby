32b2c6403e6d9a0f525830b136bed80717b1c36c
Charles Oliver Nutter <headius@headius.com>
2019-06-11 18:39:57 -0500

    More form-fitting; try frame class before scope class.
    Perhaps this is closer to what MRI "cbase" is?

--- core/src/main/java/org/jruby/RubyKernel.java
+++ core/src/main/java/org/jruby/RubyKernel.java
@@ -170,9 +170,11 @@ public class RubyKernel {
         return context.runtime.pushExitBlock(context.runtime.newProc(Block.Type.PROC, block));
     }
 
-    @JRubyMethod(name = "autoload?", required = 1, module = true, visibility = PRIVATE, reads = SCOPE)
+    @JRubyMethod(name = "autoload?", required = 1, module = true, visibility = PRIVATE, reads = {CLASS, SCOPE})
     public static IRubyObject autoload_p(ThreadContext context, final IRubyObject recv, IRubyObject symbol) {
-        RubyModule module = context.getCurrentStaticScope().getModule();
+        RubyModule module = context.getFrameKlazz();
+
+        if (module == null) module = context.getCurrentStaticScope().getModule();
 
         if (module.isNil()) {
             return context.nil;
@@ -181,9 +183,11 @@ public class RubyKernel {
         return module.autoload_p(context, symbol);
     }
 
-    @JRubyMethod(required = 2, module = true, visibility = PRIVATE, reads = SCOPE)
+    @JRubyMethod(required = 2, module = true, visibility = PRIVATE, reads = {CLASS, SCOPE})
     public static IRubyObject autoload(ThreadContext context, final IRubyObject recv, IRubyObject symbol, IRubyObject file) {
-        RubyModule module = context.getCurrentStaticScope().getModule();
+        RubyModule module = context.getFrameKlazz();
+
+        if (module == null) module = context.getCurrentStaticScope().getModule();
 
         if (module.isNil()) throw context.runtime.newTypeError("Can not set autoload on singleton class");
 
