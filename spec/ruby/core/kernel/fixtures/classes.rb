a245812c562d68d818f0be8d10bf69dce5d803c9
David Harsha <david.harsha@elastic.co>
2019-07-04 23:41:46 -0700

    Support Time ranges in rand (#5781)
    * Support Time ranges in rand
    
    JRuby is currently converting Time objects to floats when generating a
    random value from a range:
    
    ```
    $ RBENV_VERSION=jruby-9.2.7.0 ruby -e 'puts rand(Time.now..Time.now)'
    1562034267.423605
    ```
    
    MRI returns a Time object:
    
    ```
    $ RBENV_VERSION=2.6.3 ruby -e 'puts rand(Time.now..Time.now)'
    2019-07-01 19:27:25 -0700
    ```
    
    I believe the `RubyNumeric` check I added is the same as what MRI uses
    in `rb_check_to_float`: https://github.com/ruby/ruby/blob/0b858425e1e4f2de40dc0d8e5dd105a2fd93e478/object.c#L3769
    
    * Return float from `CustomRangeFloat#-`
    
    This looks to be the behavior that MRI expects for float-like values in
    ranges.
    
    MRI calls `range.end - range.begin` when generating a random value from
    a range: https://github.com/ruby/ruby/blob/5d9e91afe08c470485333f6c6e034d05ea3ee908/random.c#L1051
    
    If the return value responds to `to_int` it is treated as an integer, so
    `CustomRangeInteger` works fine:
    https://github.com/ruby/ruby/blob/5d9e91afe08c470485333f6c6e034d05ea3ee908/object.c#L3243
    
    Float-like objects that respond to `to_f` are treated differently and must be numeric:
    https://github.com/ruby/ruby/blob/5d9e91afe08c470485333f6c6e034d05ea3ee908/object.c#L3769

--- spec/ruby/core/kernel/fixtures/classes.rb
+++ spec/ruby/core/kernel/fixtures/classes.rb
@@ -424,6 +424,20 @@ module KernelSpecs
     def f2_call_lineno; method(:f3).source_location[1] + 1; end
     def f3_call_lineno; method(:f4).source_location[1] + 1; end
   end
+
+  CustomRangeInteger = Struct.new(:value) do
+    def to_int; value; end
+    def <=>(other); to_int <=> other.to_int; end
+    def -(other); self.class.new(to_int - other.to_int); end
+    def +(other); self.class.new(to_int + other.to_int); end
+  end
+
+  CustomRangeFloat = Struct.new(:value) do
+    def to_f; value; end
+    def <=>(other); to_f <=> other.to_f; end
+    def -(other); to_f - other.to_f; end
+    def +(other); self.class.new(to_f + other.to_f); end
+  end
 end
 
 class EvalSpecs
