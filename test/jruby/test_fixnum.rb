f59a6d4e97e73d162270e93a3b5948811297f176
kares <self@kares.org>
2019-10-26 18:35:37 +0200

    [test] fixnum/float ops (JRuby specific) tests
    for GH-5938

---
+++ test/jruby/test_fixnum.rb
@@ -0,0 +1,56 @@
+require 'test/unit'
+
+class TestGH5939 < Test::Unit::TestCase
+
+  class Primitive
+
+    def _value
+      @__value ||= 0
+    end
+
+    def +(other)
+      _value + other
+    end
+
+    def ==(other)
+      other == _value
+    end
+
+  end
+
+  class Klass; end
+
+  def test_op_indy_regression
+    threads = []
+    30.times do
+      threads << Thread.start do
+        begin
+          i = Klass.new
+
+          def i.version; Primitive.new end
+
+          assert_equal 10, plus_10(i.version)
+          assert_false i.version == 5
+          assert_true 0 == i.version
+
+          true
+        rescue java.lang.Exception => ex
+          warn("#{__method__} FAILED WITH: #{ex}")
+          raise ex
+        end
+      end
+    end
+
+    threads.each &:join
+    threads.each do |thread|
+      assert thread.value # should not raise (if thread raised an exception)
+    end
+
+  end
+
+  def plus_10(version)
+    version + 10
+  end
+  private :plus_10
+
+end
