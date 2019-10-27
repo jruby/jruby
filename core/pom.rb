81f3e487ae08bce7c4c489518129afaa87f96ea1
Charles Oliver Nutter <headius@headius.com>
2019-10-26 19:10:41 -0500

    Update jnr-constants for #5768.

--- core/pom.rb
+++ core/pom.rb
@@ -44,7 +44,7 @@ project 'JRuby Core' do
   jar 'com.github.jnr:jnr-enxio:0.22', :exclusions => ['com.github.jnr:jnr-ffi']
   jar 'com.github.jnr:jnr-unixsocket:0.23', :exclusions => ['com.github.jnr:jnr-ffi']
   jar 'com.github.jnr:jnr-posix:3.0.50', :exclusions => ['com.github.jnr:jnr-ffi']
-  jar 'com.github.jnr:jnr-constants:0.9.12', :exclusions => ['com.github.jnr:jnr-ffi']
+  jar 'com.github.jnr:jnr-constants:0.9.13', :exclusions => ['com.github.jnr:jnr-ffi']
   jar 'com.github.jnr:jnr-ffi:2.1.10'
   jar 'com.github.jnr:jffi:${jffi.version}'
   jar 'com.github.jnr:jffi:${jffi.version}:native'
