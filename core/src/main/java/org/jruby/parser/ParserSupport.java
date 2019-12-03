8ffe5df5f02c608ebc07514113f9604c1be5470a
Thomas E. Enebo <tom.enebo@gmail.com>
2019-12-03 15:26:30 -0600

    Fixes #5976. Incorrect warning line.

--- core/src/main/java/org/jruby/parser/ParserSupport.java
+++ core/src/main/java/org/jruby/parser/ParserSupport.java
@@ -590,7 +590,7 @@ public class ParserSupport {
         if (node instanceof MultipleAsgnNode || node instanceof LocalAsgnNode || node instanceof DAsgnNode || node instanceof GlobalAsgnNode || node instanceof InstAsgnNode) {
             Node valueNode = ((AssignableNode) node).getValueNode();
             if (isStaticContent(valueNode)) {
-                warnings.warn(ID.ASSIGNMENT_IN_CONDITIONAL, lexer.getFile(), lexer.getLineOffset(), "found = in conditional, should be ==");
+                warnings.warn(ID.ASSIGNMENT_IN_CONDITIONAL, lexer.getFile(), valueNode.getLine(), "found = in conditional, should be ==");
             }
             return true;
         } 
