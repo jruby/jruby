bc545defa4b4f675c04fc999ac0039c2dfbd756c
Charles Oliver Nutter <headius@headius.com>
2020-02-12 11:32:17 -0600

    Untag refined send test

--- test/mri/excludes/TestRefinement.rb
+++ test/mri/excludes/TestRefinement.rb
@@ -1,6 +1,5 @@
 exclude :test_prepend_into_refinement, "zsuper in module issue #5585"
 exclude :test_refine_prepended_class, "markers not being applied properly to prepended module"
-exclude :test_send_should_use_refinements, "todo"
 exclude :test_super_to_module, "issues finding proper super hierarchy, perhaps related to #5585"
 exclude :test_undef_original_method, "flaw in how we represent undef methods; they look the same as never-defined"
 exclude :test_undef_prepended_method, "flaw in how we represent undef methods; they look the same as never-defined"
