6b535d99a81e43586748c9d7e665910a5e17e928
Charles Oliver Nutter <headius@headius.com>
2020-02-17 11:09:30 -0600

    Add option category for JVM and add GraalVM native option
    This category will hold JVM-specific flags, like the new flag for
    disabling features not supported by the GraalVM native compiler.

--- core/src/main/java/org/jruby/util/cli/Options.java
+++ core/src/main/java/org/jruby/util/cli/Options.java
@@ -220,6 +220,8 @@ public class Options {
 
     public static final Option<Integer> PROFILE_MAX_METHODS = integer(PROFILING, "profile.max.methods", 100000, "Maximum number of methods to consider for profiling.");
 
+    public static final Option<Boolean> GRAALVM_NATIVE_COMPILE = bool(JVM, "graalvm.native.compile", false, "Disable features that limit or prevent GraalVM native compilation.");
+
     public static final Option<Boolean> CLI_AUTOSPLIT = bool(CLI, "cli.autosplit", false, "Split $_ into $F for -p or -n. Same as -a.");
     public static final Option<Boolean> CLI_DEBUG = bool(CLI, "cli.debug", false, "Enable debug mode logging. Same as -d.");
     public static final Option<Boolean> CLI_PROCESS_LINE_ENDS = bool(CLI, "cli.process.line.ends", false, "Enable line ending processing. Same as -l.");
