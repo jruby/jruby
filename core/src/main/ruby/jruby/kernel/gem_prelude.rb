196143a96d67e2745ab081ba35c7d3f9cde04cb0
Charles Oliver Nutter <headius@headius.com>
2020-03-04 14:11:29 -0600

    Lazily initialize RubyGems to improve startup
    RubyGems by default will parse all default gemspecs and load a
    large amount of code that many simple command-line use cases will
    never need. This patch, based on code from TruffleRuby, uses
    autoload and other tricks to lazily initialize RubyGems only when
    a gem is actually required.
    
    This speeds up startup by around 30% on my system (around 440 gems
    installed) but has some limitations:
    
    * did_you_mean must be a default gem so that it can be loaded
      from stdlib without activating RubyGems.
    * Because default gemspecs are not parsed at startup, it is not
      possible to upgrade any of the default gems in stdlib.
    * There may be threading concerns with loading RubyGems lazily.

--- core/src/main/ruby/jruby/kernel/gem_prelude.rb
+++ core/src/main/ruby/jruby/kernel/gem_prelude.rb
@@ -1,14 +1,51 @@
 if Object.const_defined?(:Gem)
-  begin
-    require 'rubygems.rb'
-  rescue LoadError # java -jar lib/jruby.jar -e '...'
-    warn 'RubyGems not found; disabling gems' if $VERBOSE
-  else
-    begin
-      gem 'did_you_mean'
-      require 'did_you_mean'
-      Gem.clear_paths
-    rescue LoadError # Gem::LoadError < LoadError
-    end if Object.const_defined?(:DidYouMean)
+  module JRuby
+    module LazyRubyGems
+    end
+  end
+
+  module Kernel
+    # Take this alias name so RubyGems will reuse this copy
+    # and skip the method below once RubyGems is loaded.
+    alias :gem_original_require :require
+
+    private def require(path)
+      begin
+        gem_original_require(path)
+      rescue LoadError
+        gem_original_require 'rubygems'
+
+        # Check that #require was redefined by RubyGems, otherwise we would end up in infinite recursion
+        new_require = ::Kernel.instance_method(:require)
+        if new_require == JRuby::LazyRubyGems::LAZY_REQUIRE
+          raise 'RubyGems did not redefine #require as expected, make sure $LOAD_PATH and home are set correctly'
+        end
+        new_require.bind(self).call(path)
+      end
+    end
+
+    JRuby::LazyRubyGems::LAZY_REQUIRE = instance_method(:require)
+
+    private def gem(*args)
+      require 'rubygems'
+      gem(*args)
+    end
+  end
+
+  class Object
+    autoload :Gem, 'rubygems'
+
+    # RbConfig is required by RubyGems, which makes it available in Ruby by default.
+    # Autoload it since we do not load RubyGems eagerly.
+    autoload :RbConfig, 'rbconfig'
+    # Defined by RbConfig
+    autoload :CROSS_COMPILING, 'rbconfig'
+
+    # StringIO is required by RubyGems
+    autoload :StringIO, 'stringio'
+  end
+
+  if Object.const_defined?(:DidYouMean)
+    require 'did_you_mean'
   end
 end
