42eace21e4547d8ec49ba46b27731021f90746a8
Charles Oliver Nutter <headius@headius.com>
2020-03-05 23:28:55 -0600

    Tweaks for String#-@
    * Always freeze and dedup, even if already frozen
    * Dup first if not "bare" (without ivars and with String class)

--- core/src/main/java/org/jruby/RubyString.java
+++ core/src/main/java/org/jruby/RubyString.java
@@ -1165,7 +1165,13 @@ public class RubyString extends RubyObject implements CharSequence, EncodingCapa
 
     @JRubyMethod(name = "-@") // -'foo' returns frozen string
     public final IRubyObject minus_at(ThreadContext context) {
-        return isFrozen() ? this : context.runtime.freezeAndDedupString(this);
+        Ruby runtime = context.runtime;
+
+        RubyString str = this;
+
+        if (!str.isBare(runtime) && !str.isFrozen()) str = str.strDup(runtime);
+
+        return runtime.freezeAndDedupString(str);
     }
 
     @JRubyMethod(name = "+@") // +'foo' returns modifiable string
@@ -6691,6 +6697,13 @@ public class RubyString extends RubyObject implements CharSequence, EncodingCapa
         int pp = nth(value.getEncoding(), value.unsafeBytes(), p, e, nth, singlebyte);
         if (pp == -1) return size;
         return pp - p;
+    }
+
+    /**
+     * Is this a "bare" string, i.e. has no instance vars and class == String.
+     */
+    private boolean isBare(Ruby runtime) {
+        return !hasVariables() && metaClass == runtime.getString();
     }
 
     private static StringSites sites(ThreadContext context) {
