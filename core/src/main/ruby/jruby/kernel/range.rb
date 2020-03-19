672777703b37f09c4e1efb4aea15ed32c61d02d1
Charles Oliver Nutter <headius@headius.com>
2020-03-19 15:48:56 -0500

    Break bsearch up into smaller methods
    This is for both optimization and separation of concerns. Also
    fix a bug in the (nil..i) logic.

--- core/src/main/ruby/jruby/kernel/range.rb
+++ core/src/main/ruby/jruby/kernel/range.rb
@@ -1,94 +1,20 @@
 class Range
   def bsearch(&cond)
-    BSearch.bsearch(self, &cond)
+    b = self.begin
+    e = self.end
+
+    return to_enum(:bsearch) unless block_given?
+
+    if b.is_a?(Float) || e.is_a?(Float)
+      BSearch.float_search(b, e, exclude_end?, &cond)
+    else
+      BSearch.integer_search(b, e, exclude_end?, &cond)
+    end
   end
 
   class BSearch
     java_import java.lang.Double
     java_import java.lang.Math
-
-    def self.bsearch(range, &cond)
-      b = range.begin
-      e = range.end
-      excl = range.exclude_end?
-
-      return to_enum(:bsearch) unless cond
-
-      if b.is_a?(Float) || e.is_a?(Float)
-        return float_search(b, e, excl, &cond)
-
-      else
-        if b.is_a?(Integer) && e.is_a?(Integer)
-          return integer_search(b, e, excl, &cond)
-
-        elsif b.is_a?(Integer) && e.nil?
-          diff = 1
-
-          while true
-            mid = b + diff
-
-            val = mid
-            begin # inlined bsearch check, keep these in sync
-              v = yield val
-              case v
-              when true
-                satisfied = val
-                smaller = true
-              when false, nil
-                smaller = false
-              when Numeric
-                cmp = v <=> 0
-                return val if cmp == 0
-                smaller = cmp < 0
-              else
-                cond_error(v)
-              end
-            end
-
-            if smaller
-              return integer_search(b, mid, false, &cond)
-            end
-            diff *= 2
-          end
-
-        elsif b.nil? && e.is_a?(Integer)
-          diff = -1
-
-          while true
-            mid = b + diff
-
-            val = mid
-            begin # inlined bsearch check, keep these in sync
-              v = yield val
-              case v
-              when true
-                satisfied = val
-                smaller = true
-              when false, nil
-                smaller = false
-              when Numeric
-                cmp = v <=> 0
-                return val if cmp == 0
-                smaller = cmp < 0
-              else
-                cond_error(v)
-              end
-            end
-
-            unless smaller
-              return integer_search(mid, e, false, &cond)
-            end
-            
-            diff *= 2
-          end
-
-        else
-          raise TypeError, "can't do binary search for #{b.class}"
-        end
-      end
-    end
-
-    private
 
     def self.float_search(b, e, excl)
       satisfied = nil
@@ -159,7 +85,21 @@ class Range
       satisfied
     end
 
-    def self.integer_search(low, high, excl)
+    def self.integer_search(b, e, excl, &cond)
+      if b.is_a?(Integer) && e.is_a?(Integer)
+        binary_search(b, e, excl, &cond)
+      elsif b.is_a?(Integer) && e.nil?
+        integer_begin(b, &cond)
+      elsif b.nil? && e.is_a?(Integer)
+        integer_end(e, &cond)
+      else
+        raise TypeError, "can't do binary search for #{b.class}"
+      end
+    end
+
+    private
+
+    def self.binary_search(low, high, excl)
       satisfied = nil
 
       high -= 1 if excl
@@ -217,6 +157,67 @@ class Range
       end
 
       satisfied
+    end
+
+    def self.integer_begin(b, &cond)
+      diff = 1
+
+      while true
+        mid = b + diff
+
+        val = mid
+        begin # inlined bsearch check, keep these in sync
+          v = yield val
+          case v
+          when true
+            smaller = true
+          when false, nil
+            smaller = false
+          when Numeric
+            cmp = v <=> 0
+            return val if cmp == 0
+            smaller = cmp < 0
+          else
+            cond_error(v)
+          end
+        end
+
+        if smaller
+          return binary_search(b, mid, false, &cond)
+        end
+        diff *= 2
+      end
+    end
+
+    def self.integer_end(e, &cond)
+      diff = -1
+
+      while true
+        mid = e + diff
+
+        val = mid
+        begin # inlined bsearch check, keep these in sync
+          v = yield val
+          case v
+          when true
+            smaller = true
+          when false, nil
+            smaller = false
+          when Numeric
+            cmp = v <=> 0
+            return val if cmp == 0
+            smaller = cmp < 0
+          else
+            cond_error(v)
+          end
+        end
+
+        unless smaller
+          return binary_search(mid, e, false, &cond)
+        end
+
+        diff *= 2
+      end
     end
 
     def self.double_as_long(double)
