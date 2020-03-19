419b6e61cdab2885bc62cfb1b536d102ffa0f63a
Charles Oliver Nutter <headius@headius.com>
2020-03-19 13:20:14 -0500

    Update pure-Ruby bsearch for 2.6

--- core/src/main/ruby/jruby/kernel/range.rb
+++ core/src/main/ruby/jruby/kernel/range.rb
@@ -1,47 +1,147 @@
 class Range
-  def bsearch
-    return to_enum(:bsearch) unless block_given?
-    from = self.begin
-    to   = self.end
-    unless from.is_a?(Numeric) && to.is_a?(Numeric)
-      raise TypeError, "can't do binary search for #{from.class}"
-    end
-
-    midpoint = nil
-    if from.is_a?(Integer) && to.is_a?(Integer)
-      convert = Proc.new{ midpoint }
-    else
-      map = Proc.new do |pk, unpk, nb|
-        result, = [nb.abs].pack(pk).unpack(unpk)
-        nb < 0 ? -result : result
-      end
-      from = map['D', 'q', from.to_f]
-      to   = map['D', 'q', to.to_f]
-      convert = Proc.new{ map['q', 'D', midpoint] }
-    end
-    to -= 1 if exclude_end?
-    satisfied = nil
-    while from <= to do
-      midpoint = (from + to).div(2)
-      result = yield(cur = convert.call)
-      case result
-      when Numeric
-        return cur if result == 0
-        result = result < 0
-      when true
-        satisfied = cur
-      when nil, false
-        # nothing to do
-      else
-        raise TypeError, "wrong argument type #{result.class} (must be numeric, true, false or nil)"
-      end
-
-      if result
-        to = midpoint - 1
-      else
-        from = midpoint + 1
-      end
-    end
-    satisfied
+  def bsearch(&block)
+    BSearch.new(self, block).bsearch
   end
+
+  class BSearch
+    java_import java.lang.Double
+    java_import java.lang.Math
+
+    def initialize(range, cond)
+      @begin = range.begin
+      @end = range.end
+      @excl = range.exclude_end?
+      @cond = cond
+      @satisfied = nil
+      @smaller = false
+    end
+
+    def double_as_long(double)
+      val = Double.doubleToLongBits(Math.abs(double))
+      double < 0 ? -val : val
+    end
+
+    def long_as_double(long)
+      if long < 0
+        long = -long
+        -(Double.longBitsToDouble(long))
+      else
+        Double.longBitsToDouble(long)
+      end
+    end
+
+    def bsearch()
+      return to_enum(:bsearch) unless @cond
+
+      beg = @begin
+      endd = @end
+
+      if beg.is_a?(Float) || endd.is_a?(Float)
+        low = double_as_long(beg.nil? ? -(Float::INFINITY) : beg)
+        high = double_as_long(endd.nil? ? Float::INFINITY : endd)
+        return fast_search(low, high) {|i| long_as_double(i)}
+      elsif beg.is_a?(Integer) && endd.is_a?(Integer)
+        return integer_search(beg, endd, @excl)
+      elsif beg.is_a?(Integer) && endd.nil?
+        diff = 1
+        while true
+          mid = beg + diff
+          ret = check(mid)
+          return ret if ret
+          if @smaller
+            return integer_search(beg, mid, false)
+          end
+          diff *= 2
+        end
+      elsif beg.nil? && endd.is_a?(Integer)
+        diff = -1
+        while true
+          mid = beg + diff
+          ret = check(mid)
+          return ret if ret
+          unless @smaller
+            return integer_search(mid, endd, false)
+          end
+          diff *= 2
+        end
+      else
+        raise TypeError, "can't do binary search for #{beg.class}"
+      end
+    end
+
+    def integer_search(low, high, excl)
+      high -= 1 if excl
+
+      org_high = high
+
+      while (low <=> high) < 0
+        mid = (high + low) / 2
+        ret = check(mid)
+        return ret if ret
+        if @smaller
+          high = mid
+        else
+          low = mid + 1
+        end
+      end
+
+      if low == org_high
+        ret = check(low)
+        return ret if ret
+        return nil unless @smaller
+      end
+
+      @satisfied
+    end
+
+    def check(val)
+      v = @cond.call val
+      if v == true
+        @satisfied = val
+        @smaller = true
+      elsif v == false || v == nil
+        @smaller = false
+      elsif v.is_a?(Numeric)
+        cmp = v <=> 0
+        return val if cmp == 0
+        @smaller = cmp < 0
+      else
+        raise TypeError, "wrong argument type #{v.class} (must be numeric, true, false or nil)"
+      end
+      false
+    end
+
+    def fast_search(low, high)
+      if @excl
+        high -= 1
+      end
+      org_high = high
+      while low < high
+        mid = if (high < 0) == (low < 0)
+                low + ((high - low) / 2)
+              elsif low < -high
+                -((-1 - low - high) / 2 + 1)
+              else
+                (low + high) / 2
+              end
+
+        ret = check(yield(mid))
+        return ret if ret
+        if @smaller
+          high = mid
+        else
+          low = mid + 1
+        end
+      end
+
+      if low == org_high
+        ret = check(yield(low))
+        return ret if ret
+        return nil unless @smaller
+      end
+
+      @satisfied
+    end
+  end
+  private_constant :BSearch
 end
