24db5f963250d07070d5d4e1d3e0271e79554d44
Charles Oliver Nutter <headius@headius.com>
2020-03-30 08:01:40 -0500

    Tag two new refinement features
    See #5576

--- test/mri/excludes/TestRefinement.rb
+++ test/mri/excludes/TestRefinement.rb
@@ -1,5 +1,7 @@
 exclude :test_prepend_into_refinement, "zsuper in module issue #5585"
+exclude :test_public_send_should_use_refinements, "not implemented, jruby/jruby#5576"
 exclude :test_refine_prepended_class, "markers not being applied properly to prepended module"
 exclude :test_super_to_module, "issues finding proper super hierarchy, perhaps related to #5585"
+exclude :test_to_proc, "not implemented, jruby/jruby#5576"
 exclude :test_undef_original_method, "flaw in how we represent undef methods; they look the same as never-defined"
 exclude :test_undef_prepended_method, "flaw in how we represent undef methods; they look the same as never-defined"
