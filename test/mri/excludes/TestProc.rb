e2bcff2f164b023eff9155ee55e41f9845463ffb
Charles Oliver Nutter <headius@headius.com>
2020-04-02 15:15:18 -0500

    Tag binding source location spec for #6163

--- test/mri/excludes/TestProc.rb
+++ test/mri/excludes/TestProc.rb
@@ -1,4 +1,5 @@
 exclude :test_attr_source_location, "needs investigation"
+exclude :test_binding_source_location, "binding source location only looks at interpreter backtrace (#6163)"
 exclude :test_dup_clone, "needs investigation"
 exclude :test_parameters, "needs investigation"
 exclude :test_proc_lambda, "needs investigation"
