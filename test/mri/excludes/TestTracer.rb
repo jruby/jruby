4643e113e2385187c28dfffc586f5509a6f601a1
Charles Oliver Nutter <headius@headius.com>
2020-04-07 23:32:56 -0500

    These work but have minor differences in output

--- test/mri/excludes/TestTracer.rb
+++ test/mri/excludes/TestTracer.rb
@@ -1,2 +1,2 @@
-exclude :test_tracer_with_option_r, "not implemented, jruby/jruby#6149"
-exclude :test_tracer_with_option_r_without_gems, "not implemented, jruby/jruby#6149"
+exclude :test_tracer_with_option_r, "minor differences in output at toplevel"
+exclude :test_tracer_with_option_r_without_gems, "minor differences in output at toplevel"
