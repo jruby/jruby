19069b5f74f4c9e3e7de5227fc418f8c8e47d297
Thomas E. Enebo <tom.enebo@gmail.com>
2020-04-16 16:03:30 -0500

    whoops missed a isBuiltin deprecated method

--- core/src/main/java/org/jruby/ir/runtime/IRRuntimeHelpers.java
+++ core/src/main/java/org/jruby/ir/runtime/IRRuntimeHelpers.java
@@ -2158,7 +2158,7 @@ public class IRRuntimeHelpers {
 
     @JIT
     public static IRubyObject callOptimizedAref(ThreadContext context, IRubyObject caller, IRubyObject target, RubyString keyStr, CallSite site) {
-        if (target instanceof RubyHash && ((CachingCallSite) site).isBuiltin(target.getMetaClass())) {
+        if (target instanceof RubyHash && ((CachingCallSite) site).isBuiltin(target)) {
             // call directly with cached frozen string
             return ((RubyHash) target).op_aref(context, keyStr);
         }
