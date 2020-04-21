e48d9f9a3ad60685a87245473fb3a7b64f66f1ca
Charles Oliver Nutter <headius@headius.com>
2020-04-21 15:34:09 -0500

    Update to latest joni to align jcodings dependency

--- core/pom.rb
+++ core/pom.rb
@@ -49,7 +49,7 @@ project 'JRuby Core' do
   jar 'com.github.jnr:jffi:${jffi.version}'
   jar 'com.github.jnr:jffi:${jffi.version}:native'
 
-  jar 'org.jruby.joni:joni:2.1.32'
+  jar 'org.jruby.joni:joni:2.1.33'
   jar 'org.jruby.jcodings:jcodings:1.0.49'
   jar 'org.jruby:dirgra:0.3'
 
