f0a84adbeb44a22ff353a169e7ee7e40582d3b56
Charles Oliver Nutter <headius@headius.com>
2020-04-26 18:22:51 -0500

    Update tags for transcoding and string tests/specs

--- test/mri/excludes/TestM17N.rb
+++ test/mri/excludes/TestM17N.rb
@@ -1,7 +1,6 @@
 exclude :test_greek_capital_gap, "needs investigation #4303"
 exclude :test_nonascii_method_name, "lexer is not pulling mbc characters off the wire correctly"
 exclude :test_setbyte_range, "unfinished in initial 2.6 work, #6161"
-exclude :test_split, "our impl has diverged and does not appear to handle encoded null char properly"
 exclude :test_str_dump, "unfinished in initial 2.6 work, #6161"
 exclude :test_symbol, "management of differently-encoded symbols is not right"
 exclude :test_utf_without_bom_valid, "needs investigation"
--- test/mri/excludes/TestTranscode.rb
+++
@@ -1,1 +0,0 @@
-exclude :test_to_cp50221, "needs investigation"
