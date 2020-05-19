c6ca3051423046917f9c83a7b89a01bb5c4feac4
Charles Oliver Nutter <headius@headius.com>
2020-05-19 05:52:52 -0500

    Update jcodings to get proper emoji version
    There is no 12.1.

--- core/pom.rb
+++ core/pom.rb
@@ -49,8 +49,8 @@ project 'JRuby Core' do
   jar 'com.github.jnr:jffi:${jffi.version}'
   jar 'com.github.jnr:jffi:${jffi.version}:native'
 
-  jar 'org.jruby.joni:joni:2.1.37'
-  jar 'org.jruby.jcodings:jcodings:1.0.53'
+  jar 'org.jruby.joni:joni:2.1.38'
+  jar 'org.jruby.jcodings:jcodings:1.0.54'
   jar 'org.jruby:dirgra:0.3'
 
   jar 'com.headius:invokebinder:1.11'
