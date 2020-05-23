9ee068b31497e822dcc289801e92b120ff99a636
Charles Oliver Nutter <headius@headius.com>
2020-05-23 02:51:44 -0500

    Need input

--- tool/maven-ci-script.sh
+++ tool/maven-ci-script.sh
@@ -6,7 +6,7 @@ set -x
 if [[ -v PHASE ]]
 then
   DOWNLOAD_OUTPUT_FILTER='Download|\\[exec\\] [[:digit:]]+/[[:digit:]]+|^[[:space:]]*\\[exec\\][[:space:]]*$'
-  ./mvnw $MAVEN_CLI_OPTS package -B -Dinvoker.skip=false $PHASE | egrep -v "$DOWNLOAD_OUTPUT_FILTER"
+  ./mvnw $MAVEN_CLI_OPTS package -X -B -Dinvoker.skip=false $PHASE | egrep -v "$DOWNLOAD_OUTPUT_FILTER"
 
   MVN_STATUS=${PIPESTATUS[0]}
 
