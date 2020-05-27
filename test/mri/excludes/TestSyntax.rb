6c820da1dfc17ecd22e04c86e8e2a26f0e85fbec
Thomas E. Enebo <tom.enebo@gmail.com>
2020-05-27 16:38:58 -0500

    Tests now passing after fixing bad tests

--- test/mri/excludes/TestSyntax.rb
+++ test/mri/excludes/TestSyntax.rb
@@ -1,4 +1,2 @@
-exclude :test_invalid_next, "lazy method build make this fail"
-exclude :test_invalid_break, "lazy method build makes this fail"
 exclude :test_method_call_location, "needs investigation"
 
