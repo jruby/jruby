14c02c093c378789be1c9756c13c2d29df1d78db
Thomas E. Enebo <tom.enebo@gmail.com>
2020-05-27 15:00:26 -0500

    Yay this was already passing with proper mri backtrace style enabled

--- test/mri/excludes/TestSyntax.rb
+++ test/mri/excludes/TestSyntax.rb
@@ -1,4 +1,3 @@
-exclude :test_invalid_jump, "needs investigation"
 exclude :test_invalid_next, "lazy method build make this fail"
 exclude :test_invalid_break, "lazy method build makes this fail"
 exclude :test_method_call_location, "needs investigation"
