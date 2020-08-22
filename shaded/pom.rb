58e8758501b3b4965565e7c53dc4bdab3fcb87fb
Christian Meier <meier.kristian@gmail.com>
2020-08-22 13:25:47 +0200

    remove obsolete inclusions

--- shaded/pom.rb
+++ shaded/pom.rb
@@ -41,12 +41,6 @@ project 'JRuby Core' do
                        id: 'shade dependencies into jar',
                        phase: 'package',
                        artifactSet: {
-                         # IMPORTANT these needs to match exclusions in
-                         # maven/jruby-complete/pom.rb
-                         includes: [ 'com.github.jnr:jnr-ffi',
-                                     'me.qmx.jitescript:jitescript',
-                                     'org.ow2.asm:*'
-                         ],
                          excludes: 'javax.annotation:javax.annotation-api'
                        },
                        relocations: [
