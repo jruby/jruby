6257dfc3c3d1cdf18511f6f936ea1bdf14e111ed
Karol Bucek <kares@users.noreply.github.com>
2020-10-21 06:08:37 +0200

    update jruby-openssl to 0.10.5 (#6423)
    expected to avoid left-over reflection warnings on Java 9+

--- lib/pom.rb
+++ lib/pom.rb
@@ -21,7 +21,7 @@ default_gems = [
     ['ipaddr', '1.2.0'],
     ['jar-dependencies', '${jar-dependencies.version}'],
     ['jruby-readline', '1.3.7'],
-    ['jruby-openssl', '0.10.4'],
+    ['jruby-openssl', '0.10.5'],
     ['json', '${json.version}'],
     ['psych', '3.2.0'],
     ['rake-ant', '1.0.4'],
