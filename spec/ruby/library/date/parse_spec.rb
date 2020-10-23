642482f51697216624cc333ce7544d314f563950
Josh Cronemeyer <joshuacronemeyer@gmail.com>
2020-10-23 10:16:59 -0700

    If the year isn't given with the week we assume current year. Revert changes to valid_date_frags? Issue is in RubyDate.java but i'm testing a temporary fix in complete_frags that should behave the same as that fix. Getting feedback on the idea.

--- spec/ruby/library/date/parse_spec.rb
+++ spec/ruby/library/date/parse_spec.rb
@@ -65,6 +65,11 @@ describe "Date#parse" do
     d.should == Date.civil(1910, 11, 1)
   end
 
+  it "parses W## as week number and defaults to this year first day of week" do
+    d = Date.parse('W09') #9th week of the year assumes first day of the week.
+    d.should == Date.civil(2020, 2, 24) #Mon. 2/24/2020. Week always starts with a Monday.
+  end
+
   it "parses YYYY-W## as year and week number and defaults to first day of week" do
     d = Date.parse('2020-W09') #9th week of the year assumes first day of the week.
     d.should == Date.civil(2020, 2, 24) #Mon. 2/24/2020. Week always starts with a Monday.
