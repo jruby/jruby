675c1844f78a047ee0a43090960b5f44cbb150b3
Charles Oliver Nutter <headius@headius.com>
2020-12-02 15:02:37 -0600

    Update to ASM 9.0 for convergence with jnr-ffi

--- pom.rb
+++ pom.rb
@@ -82,7 +82,7 @@ project 'JRuby', 'https://github.com/jruby/jruby' do
               'jar-dependencies.version' => '0.4.1',
               'jruby-launcher.version' => '1.1.6',
               'ant.version' => '1.9.8',
-              'asm.version' => '7.1',
+              'asm.version' => '9.0',
               'jffi.version' => '1.3.1',
               'joda.time.version' => '2.10.5' )
 
