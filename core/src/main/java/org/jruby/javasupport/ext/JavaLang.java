bada6eff7c98132efaa00639d541d51551f6c292
kares <self@kares.org>
2020-12-14 06:00:49 +0100

    [ji] inspect (simply toString) for enum types

--- core/src/main/java/org/jruby/javasupport/ext/JavaLang.java
+++ core/src/main/java/org/jruby/javasupport/ext/JavaLang.java
@@ -75,6 +75,7 @@ public abstract class JavaLang {
         });
         JavaExtensions.put(runtime, java.lang.CharSequence.class, (proxyClass) -> CharSequence.define(runtime, proxyClass));
         JavaExtensions.put(runtime, java.lang.String.class, (proxyClass) -> String.define(runtime, (RubyClass) proxyClass));
+        JavaExtensions.put(runtime, java.lang.Enum.class, (proxyClass) -> proxyClass.defineAlias("inspect", "to_s"));
     }
 
     @JRubyModule(name = "Java::JavaLang::Iterable", include = "Enumerable")
