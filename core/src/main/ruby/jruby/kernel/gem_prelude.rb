096c3dc713fee3f85fe16ec09b1027d1ef1e9a7f
Charles Oliver Nutter <headius@headius.com>
2020-12-22 15:05:25 -0600

    Disabling gems should disable d_y_m

--- core/src/main/ruby/jruby/kernel/gem_prelude.rb
+++ core/src/main/ruby/jruby/kernel/gem_prelude.rb
@@ -8,12 +8,12 @@ if Object.const_defined?(:Gem)
       warn 'RubyGems not found; disabling gems' if $VERBOSE
     end
   end
-end
 
-if Object.const_defined?(:DidYouMean)
-  # did_you_mean is a default gem  and can load with or without RubyGems
-  begin
-    require 'did_you_mean'
-  rescue LoadError # Gem::LoadError < LoadError
+  if Object.const_defined?(:DidYouMean)
+    # did_you_mean is a default gem  and can load with or without RubyGems
+    begin
+      require 'did_you_mean'
+    rescue LoadError # Gem::LoadError < LoadError
+    end
   end
 end
