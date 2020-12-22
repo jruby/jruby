55ae1ca345d42588afadbb504a8d5dc42a941108
Charles Oliver Nutter <headius@headius.com>
2020-12-22 10:28:12 -0600

    Add default gem upgrade support to lazy RubyGems
    This is based on the same code in TruffleRuby which has our same
    licenses but which has not been contributed back to RubyGems or
    CRuby.

--- core/src/main/ruby/jruby/kernel/gem_prelude.rb
+++ core/src/main/ruby/jruby/kernel/gem_prelude.rb
@@ -1,6 +1,109 @@
 if Object.const_defined?(:Gem)
   module JRuby
-    module LazyRubyGems
+    module GemUtil
+      # Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved. This
+      # code is released under a tri EPL/GPL/LGPL license. You can use it,
+      # redistribute it and/or modify it under the terms of the:
+      #
+      # Eclipse Public License version 2.0, or
+      # GNU General Public License version 2, or
+      # GNU Lesser General Public License version 2.1.
+
+      DEFAULT_GEMS = {
+            'bundler' => true,
+            'cmath' => true,
+            'csv' => true,
+            'did_you_mean' => true,
+            'e2mmap' => true,
+            'ffi' => true,
+            'fileutils' => true,
+            'forwardable' => true,
+            'ipaddr' => true,
+            'irb' => true,
+            'jar-dependencies' => true,
+            'jruby-openssl' => true,
+            'jruby-readline' => true,
+            'json' => true,
+            'logger' => true,
+            'matrix' => true,
+            'mutex_m' => true,
+            'ostruct' => true,
+            'prime' => true,
+            'psych' => true,
+            'racc' => true,
+            'rake-ant' => true,
+            'rdoc' => true,
+            'rexml' => true,
+            'rss' => true,
+            'scanf' => true,
+            'shell' => true,
+            'sync' => true,
+            'thwait' => true,
+            'tracer' => true,
+            'webrick' => true,
+        }
+
+      def self.upgraded_default_gem?(feature)
+        if i = feature.index('/')
+          first_component = feature[0...i]
+        else
+          first_component = feature
+        end
+
+        if DEFAULT_GEMS.include?(first_component)
+          # No need to check for 'io/nonblock' and 'io/wait', just for 'io/console'
+          return false if first_component == 'io' and !feature.start_with?('io/console')
+
+          matcher = "#{first_component}-"
+          gem_paths.each do |gem_dir|
+            spec_dir = "#{gem_dir}/specifications"
+            if File.directory?(spec_dir)
+              Dir.each_child(spec_dir) do |spec|
+                if spec.start_with?(matcher) and digit = spec[matcher.size] and '0' <= digit && digit <= '9'
+                  return true
+                end
+              end
+            end
+          end
+        end
+
+        false
+      end
+
+      # Gem.path, without needing to load RubyGems
+      def self.gem_paths
+        @gem_paths ||= begin
+          home = ENV['GEM_HOME'] || "#{ENV_JAVA['jruby.home']}/lib/ruby/gems/shared"
+          paths = [home]
+
+          if gem_path = ENV['GEM_PATH']
+            paths.concat gem_path.split(File::PATH_SEPARATOR)
+          else
+            user_dir = "#{Dir.home}/.gem/jruby/#{RUBY_ENGINE_VERSION}"
+            paths << user_dir
+          end
+
+          paths.map { |path| expand(path) }.uniq
+        end
+      end
+
+      def self.expand(path)
+        if File.directory?(path)
+          File.realpath(path)
+        else
+          path
+        end
+      end
+
+      def self.get_path(pathlike)
+        if feature.kind_of? String
+          feature
+        elsif feature.respond_to? :to_path
+          feature.to_path
+        else
+          feature.to_str
+        end
+      end
     end
   end
 
@@ -9,27 +112,40 @@ if Object.const_defined?(:Gem)
     # and skip the method below once RubyGems is loaded.
     alias :gem_original_require :require
 
-    private def require(path)
-      begin
-        gem_original_require(path)
-      rescue LoadError
-        gem_original_require 'rubygems'
+    def require(feature)
+      feature = JRuby::GemUtil.get_path(feature)
 
-        # Check that #require was redefined by RubyGems, otherwise we would end up in infinite recursion
-        new_require = ::Kernel.instance_method(:require)
-        if new_require == JRuby::LazyRubyGems::LAZY_REQUIRE
-          raise 'RubyGems did not redefine #require as expected, make sure $LOAD_PATH and home are set correctly'
+      lazy_rubygems = true # TODO add way to turn off
+      upgraded_default_gem = lazy_rubygems && JRuby::GemUtil.upgraded_default_gem?(feature)
+
+      # TODO find file to know if it will load
+      if !upgraded_default_gem #and path = Primitive.find_file(feature)
+        # gem_original_require(path)
+        gem_original_require(feature)
+      else
+        if lazy_rubygems
+          gem_original_require 'rubygems'
+
+          # Check that #require was redefined by RubyGems, otherwise we would end up in infinite recursion
+          new_require = ::Kernel.instance_method(:require)
+          if new_require == JRuby::GemUtil::LAZY_REQUIRE
+            raise 'RubyGems did not redefine #require as expected, make sure $LOAD_PATH and home are set correctly'
+          end
+          new_require.bind(self).call(feature)
+        else
+          raise LoadError.new(feature)
         end
-        new_require.bind(self).call(path)
       end
     end
+    module_function :require
 
-    JRuby::LazyRubyGems::LAZY_REQUIRE = instance_method(:require)
+    JRuby::GemUtil::LAZY_REQUIRE = instance_method(:require)
 
-    private def gem(*args)
+    def gem(*args)
       require 'rubygems'
       gem(*args)
     end
+    private :gem
   end
 
   class Object
