d921bddaf6dc1a8fc232ed5ab33582cb97415a4b
Édipo Féderle <edipofederle@gmail.com>
2021-06-03 13:59:43 -0300

    Untag fixed tests

--- test/mri/excludes/TestDir.rb
+++ test/mri/excludes/TestDir.rb
@@ -1,6 +1,5 @@
 exclude :test_chdir, "uses user.home as a fallback home path"
 exclude :test_glob, '\0 delimiter is not handled'
-exclude :test_glob_cases, "fails to return files with their correct casing (#2150)"
 exclude :test_glob_gc_for_fd, "tweaks rlimit and never restores it, depends on GC effects"
 exclude :test_glob_too_may_open_files, "our glob does not appear to open files, and so the expected EMFILE does not happen"
 exclude :test_home, "uses user.home as a fallback home path"
