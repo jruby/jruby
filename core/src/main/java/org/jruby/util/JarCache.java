a7f3f455d300d6bd120b0a86ba5af202af148d80
Dirk Mahler <dirk.mahler@buschmais.com>
2021-07-04 13:47:46 +0200

    #6730 use cached value for last modification timestamp of resource JARs in JarCache

--- core/src/main/java/org/jruby/util/JarCache.java
+++ core/src/main/java/org/jruby/util/JarCache.java
@@ -17,25 +17,31 @@ import java.util.jar.JarFile;
 import static org.jruby.RubyFile.canonicalize;
 
 /**
-  * Instances of JarCache provides jar index information.
+ * Instances of JarCache provides jar index information.
+ *
+ * <p>
+ * Implementation is threadsafe.
   *
-  * <p>
-  *     Implementation is threadsafe.
+ * Since loading index information is O(jar-entries) we cache the snapshot in a WeakHashMap.
+ * The implementation pays attention to lastModified timestamp of the jar and will invalidate
+ * the cache entry if jar has been updated since the snapshot calculation.
+ * </p>
   *
-  *     Since loading index information is O(jar-entries) we cache the snapshot in a WeakHashMap.
-  *     The implementation pays attention to lastModified timestamp of the jar and will invalidate
-  *     the cache entry if jar has been updated since the snapshot calculation.
-  * </p>
+ * ******************************************************************************************
+ * DANGER DANGER DANGER DANGER DANGER DANGER DANGER DANGER DANGER DANGER DANGER DANGER DANGER
+ * ******************************************************************************************
   *
-  * ******************************************************************************************
-  * DANGER DANGER DANGER DANGER DANGER DANGER DANGER DANGER DANGER DANGER DANGER DANGER DANGER
-  * ******************************************************************************************
-  *
-  * The spec for this cache is disabled currently for #2655, because of last-modified time
-  * oddities on CloudBees. Please be cautious modifying this code and make sure you run the
-  * associated spec locally.
-  */
+ * The spec for this cache is disabled currently for #2655, because of last-modified time
+ * oddities on CloudBees. Please be cautious modifying this code and make sure you run the
+ * associated spec locally.
+ */
 class JarCache {
+
+    /**
+     * The timeout in milliseconds for caching the last modified timestamp of JAR files.
+     */
+    private static final long LAST_MODIFIED_EXPIRATION_TIME_MILLISECONDS = 500;
+
     static class JarIndex {
         private static final String ROOT_KEY = "";
 
@@ -87,12 +93,22 @@ class JarCache {
             this.cachedDirEntries = Collections.unmodifiableMap(cachedDirEntries);
         }
 
+        /**
+         * Determines the last modification timestamp of a file.
+         * <p>
+         * NOTE: Getting the value is an expensive operation on Windows systems (see
+         * {@link <a href="https://github.com/jruby/jruby/issues/6730}").
+         * Therefore a cached value is used with a maximum lifetime of {@link #LAST_MODIFIED_EXPIRATION_TIME_MILLISECONDS} milliseconds.
+         *
+         * @param jarPath The path to the JAR file.
+         * @return The last modification timestamp.
+         */
         private long getLastModified(String jarPath) {
             long currentTimeMillis = System.currentTimeMillis();
             if (lastModifiedExpiration != null && currentTimeMillis < lastModifiedExpiration) {
                 return lastModified;
             }
-            this.lastModifiedExpiration = currentTimeMillis + 500;
+            this.lastModifiedExpiration = currentTimeMillis + LAST_MODIFIED_EXPIRATION_TIME_MILLISECONDS;
             return new File(jarPath).lastModified();
         }
 
@@ -111,7 +127,8 @@ class JarCache {
         public void release() {
             try {
                 jar.close();
-            } catch (IOException ioe) { }
+            } catch (IOException ioe) {
+            }
         }
 
         public boolean isValid() {
@@ -163,15 +180,14 @@ class JarCache {
         }
     }
 
-    public boolean remove(String jarPath){
+    public boolean remove(String jarPath) {
         String cacheKey = jarPath;
 
         synchronized (indexCache) {
             JarIndex index = indexCache.get(cacheKey);
-            if (index == null){
+            if (index == null) {
                 return false;
-            }
-            else{
+            } else {
                 index.release();
                 indexCache.remove(cacheKey);
                 return true;
