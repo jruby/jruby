c0a52bce302f60046a53b995e3e53307a9fb0bce
Charles Oliver Nutter <headius@headius.com>
2021-07-26 15:28:23 -0500

    Split up Windows and Solaris FFI file logic

--- core/src/main/ruby/jruby/kernel/file.rb
+++ core/src/main/ruby/jruby/kernel/file.rb
@@ -4,8 +4,8 @@ if JRuby::Util::ON_WINDOWS
 
   class File
     def self.symlink(target, link)
-      # lazy load FFI-based file logic
-      require_relative 'jruby/file_ffi'
+      # lazy load FFI-based File.symlink logic
+      require_relative 'jruby/windows_ffi_file'
 
       symlink(target, link)
     end
@@ -27,8 +27,8 @@ if JRuby::Util::ON_SOLARIS
 
   class File
     def flock(operation)
-      # lazy load FFI-based file logic
-      require_relative 'jruby/file_ffi'
+      # lazy load FFI-based File#flock logic
+      require_relative 'jruby/solaris_ffi_file'
 
       flock(operation)
     end
