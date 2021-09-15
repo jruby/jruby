3f57abbfba73b8896955612155a1fd47e7fb148f
Thomas E. Enebo <tom.enebo@gmail.com>
2021-09-15 16:15:19 -0500

    This is messing up Java 9+ somehow

--- shaded/src/main/java/Nothing.java
+++
@@ -1,6 +0,0 @@
-// Not part of our sources but we put this in so docs will generate a jar to
-// satisy sonatype requirements.  There must be a way to solve this purely
-// through Maven but we did not figure that out yet.
-
-public class Nothing {
-}
