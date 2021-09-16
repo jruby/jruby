e6cceb61483325adbb43bb6058e2c96e7eb0315f
Pavel Rosický <pavel.rosicky@easy.cz>
2021-09-16 21:24:43 +0200

    inspect hostname

--- core/src/main/java/org/jruby/ext/socket/Addrinfo.java
+++ core/src/main/java/org/jruby/ext/socket/Addrinfo.java
@@ -283,6 +283,11 @@ public class Addrinfo extends RubyObject {
         } else if (protocol != null && protocol.getProto() != 0) {
             val.append(" ").append(String.format("UNKNOWN PROTOCOL(%d)", protocol.getProto()));
         }
+
+        String inspectName = inspectname();
+        if (inspectName != null) {
+            val.append(" (").append(inspectName).append(")");
+        }
        
         return context.runtime.newString(String.format(base, val.toString()));
     }
@@ -673,6 +678,16 @@ public class Addrinfo extends RubyObject {
 
         if (in.isLoopbackAddress()) return "::1";
         return SocketUtilsIPV6.getIPV6Address(in.getHostAddress());
+    }
+
+    private String inspectname() {
+        if (socketAddress instanceof InetSocketAddress) {
+            return getInetSocketAddress().getAddress().getHostName();
+        } else if (socketAddress instanceof UnixSocketAddress) {
+            return getUnixSocketAddress().path();
+        } else {
+            return null;
+        }
     }
 
     private static InetAddress getRubyInetAddress(IRubyObject node) {
