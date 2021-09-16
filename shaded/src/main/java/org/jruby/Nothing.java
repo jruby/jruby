c405da5c2c38eafcd50da5a38287e8bbf3013fc2
Thomas E. Enebo <tom.enebo@gmail.com>
2021-09-16 10:13:35 -0500

    Trick Maven and Sonatype into releasable stuff

---
+++ shaded/src/main/java/org/jruby/Nothing.java
@@ -0,0 +1,8 @@
+package org.jruby;
+
+// This class is not used but exists so that maven will create a javadocs jar
+// for jruby-core.  This in turn makes the creation of jruby-complete happy
+// which makes sonatype artifact publishing.  Obviously this is a hack so
+// if someone has some better Maven skills we can then get rid of this file.
+public class Nothing {
+}
