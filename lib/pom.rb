a8532ff5822a82d7797433c7a71d63bfbc5bb880
Charles Oliver Nutter <headius@headius.com>
2021-09-23 15:31:18 -0500

    Update logger to v1.4.3

--- lib/pom.rb
+++ lib/pom.rb
@@ -40,7 +40,7 @@ default_gems = [
     ['jruby-readline', '1.3.7'],
     ['jruby-openssl', '0.10.7'],
     ['json', '2.5.1'],
-    ['logger', '1.3.0'],
+    ['logger', '1.4.3'],
     ['matrix', '0.3.0'],
     ['mutex_m', '0.1.0'],
     ['ostruct', '0.3.3'],
