34854aa37963b2c8b2b1185d9d0c1a20a1777192
Thomas E. Enebo <tom.enebo@gmail.com>
2021-10-24 09:56:25 -0500

    "%1" was an infinite loop

--- core/src/main/java/org/jruby/util/SprintfParser.java
+++ core/src/main/java/org/jruby/util/SprintfParser.java
@@ -663,6 +663,7 @@ public class SprintfParser {
 
                         if (current != '$') {
                             if (token.isWidthIndexed()) error("width given twice");
+                            if (current == EOF) return;
                             unread();
                         }
 
