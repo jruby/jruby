d7a7b272b3e7beebb485e5ab52315097ab7f6f6d
Édipo Féderle <edipofederle@gmail.com>
2021-10-27 16:18:14 -0300

    [Feature] pager for --help

--- core/src/main/java/org/jruby/util/cli/OutputStrings.java
+++ core/src/main/java/org/jruby/util/cli/OutputStrings.java
@@ -1,10 +1,12 @@
 package org.jruby.util.cli;
 
 import com.headius.options.Option;
-import java.time.LocalDate;
 import org.jruby.ext.rbconfig.RbConfigLibrary;
+import org.jruby.platform.Platform;
 import org.jruby.runtime.Constants;
 import org.jruby.util.SafePropertyAccessor;
+
+import java.time.LocalDate;
 
 /**
  * Utility methods to generate the command-line output strings for help,
@@ -12,76 +14,95 @@ import org.jruby.util.SafePropertyAccessor;
  */
 public class OutputStrings {
     public static String getBasicUsageHelp() {
-        StringBuilder sb = new StringBuilder();
-        sb
-                .append("Usage: jruby [switches] [--] [programfile] [arguments]\n")
-                .append("  -0[octal]                specify record separator (\\0, if no argument)\n")
-                .append("  -a                       autosplit mode with -n or -p (splits $_ into $F)\n")
-                .append("  -c                       check syntax only\n")
-                .append("  -Cdirectory              cd to directory, before executing your script\n")
-                .append("  -d                       set debugging flags (set $DEBUG to true)\n")
-                .append("  -e 'command'             one line of script. Several -e's allowed. Omit [programfile]\n")
-                .append("  -Eex[:in]                specify the default external and internal character encodings\n")
-                .append("  -Fpattern                split() pattern for autosplit (-a)\n")
-                .append("  -G                       load a Bundler Gemspec before executing any user code\n")
-                .append("  -i[extension]            edit ARGV files in place (make backup if extension supplied)\n")
-                .append("  -Idirectory              specify $LOAD_PATH directory (may be used more than once)\n")
-                .append("  -J[java option]          pass an option on to the JVM (e.g. -J-Xmx512m)\n")
-                .append("                             use --properties to list JRuby properties\n")
-                .append("                             run 'java -help' for a list of other Java options\n")
-                .append("  -l                       enable line ending processing\n")
-                .append("  -n                       assume 'while gets(); ... end' loop around your script\n")
-                .append("  -p                       assume loop like -n but print line also like sed\n")
-                .append("  -rlibrary                require the library, before executing your script\n")
-                .append("  -s                       enable some switch parsing for switches after script name\n")
-                .append("  -S                       look for the script in bin or using PATH environment variable\n")
-                .append("  -T[level]                turn on tainting checks\n")
-                .append("  -U                       use UTF-8 as default internal encoding\n")
-                .append("  -v                       print version number, then turn on verbose mode\n")
-                .append("  -w                       turn warnings on for your script\n")
-                .append("  -W[level]                set warning level; 0=silence, 1=medium, 2=verbose (default)\n")
-                .append("  -x[directory]            strip off text before #!ruby line and perhaps cd to directory\n")
-                .append("  -X[option]               enable extended option (omit option to list)\n")
-                .append("  -y                       enable parsing debug output\n")
-                .append("  --backtrace-limit[lines] limits the maximum length of a backtrace\n")
-                .append("  --copyright              print the copyright\n")
-                .append("  --debug                  sets the execution mode most suitable for debugger\n")
-                .append("                             functionality\n")
-                .append("  --jdb                    runs JRuby process under JDB\n")
-                .append("  --properties             list all configuration Java properties\n")
-                .append("                             (prepend \"jruby.\" when passing directly to Java)\n")
-                .append("  --environment            log environment and command line flags but do not run JRuby\n")
-                .append("  --sample                 run with profiling using the JVM's sampling profiler\n")
-                .append("  --profile                run with instrumented (timed) profiling, flat format\n")
-                .append("  --profile.api            activate Ruby profiler API\n")
-                .append("  --profile.flat           synonym for --profile\n")
-                .append("  --profile.graph          run with instrumented (timed) profiling, graph format\n")
-                .append("  --profile.html           run with instrumented (timed) profiling, graph format in HTML\n")
-                .append("  --profile.json           run with instrumented (timed) profiling, graph format in JSON\n")
-                .append("  --profile.out            [file]\n")
-                .append("  --profile.service        <ProfilingService implementation classname>\n")
-                .append("                              output profile data to [file]\n")
-                .append("  --client                 use the non-optimizing \"client\" JVM\n")
-                .append("                             (improves startup; default)\n")
-                .append("  --server                 use the optimizing \"server\" JVM (improves perf)\n")
-                .append("  --headless               do not launch a GUI window, no matter what\n")
-                .append("  --dev                    prioritize startup time over long term performance\n")
-                .append("  --manage                 enable remote JMX management and monitoring of JVM and JRuby\n")
-                .append("  --bytecode               show the JVM bytecode produced by compiling specified code\n")
-                .append("  --version                print the version\n")
-                .append("  --disable-gems           do not load RubyGems on startup\n")
-                .append("  --enable=feature[,...],  --disable=feature[,...]\n")
-                .append("                              enable or disable features\n");
+        String[][] basicUsageOptions = {
+                {"-0[octal]", "specify record separator (\\0, if no argument)"},
+                {"-a", "autosplit mode with -n or -p (splits $_ into $F)"},
+                {"-c", "check syntax only"},
+                {"-Cdirectory", "cd to directory, before executing your script"},
+                {"-d", "set debugging flags (set $DEBUG to true)"},
+                {"-e 'command'", "one line of script. Several -e's allowed. Omit [programfile]"},
+                {"-Eex[:in]", "specify the default external and internal character encodings"},
+                {"-Fpattern", "split() pattern for autosplit (-a)"},
+                {"-G", "load a Bundler Gemspec before executing any user code"},
+                {"-i[extension]", "edit ARGV files in place (make backup if extension supplied)"},
+                {"-Idirectory", "specify $LOAD_PATH directory (may be used more than once)"},
+                {"-J[java option]", "pass an option on to the JVM (e.g. -J-Xmx512m)/nuse --properties to list JRuby properties run 'java -help' for a list of other Java options"},
+                {"-l", "enable line ending processing"},
+                {"-n", "assume 'while gets(); ... end' loop around your script"},
+                {"-p", "assume loop like -n but print line also like sed"},
+                {"-rlibrary", "require the library, before executing your script"},
+                {"-s", "enable some switch parsing for switches after script name"},
+                {"-S", "look for the script in bin or using PATH environment variable"},
+                {"-T[level]", "turn on tainting checks"},
+                {"-U", "use UTF-8 as default internal encoding"},
+                {"-v", "print version number, then turn on verbose mode"},
+                {"-w", "turn warnings on for your script"},
+                {"-W[level]", "set warning level; 0=silence, 1=medium, 2=verbose (default)"},
+                {"-x[directory]", "strip off text before #!ruby line and perhaps cd to directory"},
+                {"-X[option]", "enable extended option (omit option to list)"},
+                {"-y", "enable parsing debug output"},
+                {"--backtrace-limit[lines]", "limits the maximum length of a backtrace"},
+                {"--copyright", "print the copyright"},
+                {"--debug", "sets the execution mode most suitable for debugger functionality"},
+                {"--jdb", "runs JRuby process under JDB"},
+                {"--properties", "List all configuration Java properties (prepend \"jruby.\" when passing directly to Java)"},
+                {"--environment", "Log environment and command line flags but do not run JRuby"},
+                {"--sample", "run with profiling using the JVM's sampling profiler"},
+                {"--profile", "run with instrumented (timed) profiling, flat format"},
+                {"--profile.api", "activate Ruby profiler API"},
+                {"--profile.flat", "synonym for --profile"},
+                {"--profile.graph", "run with instrumented (timed) profiling, graph format"},
+                {"--profile.html", "run with instrumented (timed) profiling, graph format in HTML"},
+                {"--profile.json", "run with instrumented (timed) profiling, graph format in JSON"},
+                {"--profile.out", "[file]"},
+                {"--profile.service", "<ProfilingService implementation classname> output profile data to [file]"},
+                {"--client", "use the non-optimizing \"client\" JVM (improves startup; default)"},
+                {"--server", "use the optimizing \"server\" JVM (improves perf)"},
+                {"--headless", "do not launch a GUI window, no matter what"},
+                {"--dev", "prioritize startup time over long term performance"},
+                {"--manage", "enable remote JMX management and monitoring of JVM and JRuby"},
+                {"--bytecode", "show the JVM bytecode produced by compiling specified code"},
+                {"--version", "print the version"},
+                {"--disable-gems", "do not load RubyGems on startup"},
+                {"--enable=feature[,...], --disable=feature[,...]", "enable or disable features"}
+        };
 
+        String header = strBold("Usage:") + " jruby [switches] [--] [programfile] [arguments]";
+        return buildOutputOptions(basicUsageOptions, header);
+    }
+
+    private static String buildOutputOptions(String[][] options, String header) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(strBold(header)).append("\n");
+
+        int max = Integer.MIN_VALUE;
+        for (String[] strings : options) {
+            int s = strings[0].length();
+            if (s > max) {
+                max = s;
+            }
+        }
+
+        for (String[] option : options) {
+            String key = option[0];
+            String value = option[1];
+
+            String text = breakLine(value, 60, max + 8);
+            sb.append("   ").append(strBold(key)).append(generateSpaces(max + 5 - key.length())).append(text).append("\n");
+        }
         return sb.toString();
     }
 
-    public static String getFeaturesHelp() { return
-        "Features:\n" +
-        "  gems                   rubygems (default: " + (Options.CLI_RUBYGEMS_ENABLE.defaultValue() ? "enabled" : "disabled") + ")\n" +
-        "  did_you_mean           did_you_mean (default: " + (Options.CLI_DID_YOU_MEAN_ENABLE.defaultValue() ? "enabled" : "disabled") + ")\n" +
-        "  rubyopt                RUBYOPT environment variable (default: " + (Options.CLI_RUBYOPT_ENABLE.defaultValue() ? "enabled" : "disabled") + ")\n" +
-        "  frozen-string-literal  freeze all string literals (default: disabled)\n" ;
+    public static String getFeaturesHelp() {
+        String header = "Features:";
+
+        String[][] options = {
+                {"gems", "rubygems (default: " + (Options.CLI_RUBYGEMS_ENABLE.defaultValue() ? "enabled" : "disabled") + ")"},
+                {"did_you_mean", "did_you_mean (default: " + (Options.CLI_DID_YOU_MEAN_ENABLE.defaultValue() ? "enabled" : "disabled") + ")"},
+                {"rubyopt", "RUBYOPT environment variable (default: " + (Options.CLI_RUBYOPT_ENABLE.defaultValue() ? "enabled" : "disabled") + ")"},
+                {"frozen-string-literal", "freeze all string literals (default: disabled)"}};
+
+        return buildOutputOptions(options, header);
     }
 
     public static String getExtendedHelp() { return
@@ -99,7 +120,6 @@ public class OutputStrings {
 
     public static String getPropertyHelp() {
         StringBuilder sb = new StringBuilder();
-
         sb
                 .append("# These properties can be used to alter runtime behavior for perf or compatibility.\n")
                 .append("# Specify them by passing -X<property>=<value>\n")
@@ -109,14 +129,12 @@ public class OutputStrings {
                 .append("# This dump is a valid .jrubyrc file of current settings. Uncomment and modify\n")
                 .append("# settings to customize.\n");
 
-        sb.append(Option.formatOptions(Options.PROPERTIES));
-
-        return sb.toString();
+        return sb.append(Option.formatOptions(Options.PROPERTIES)).toString();
     }
 
     public static String getVersionString() {
         return String.format(
-            "jruby %s (%s) %s %s %s %s on %s%s%s [%s-%s]",
+                "jruby %s (%s) %s %s %s %s on %s%s%s [%s-%s]",
                 Constants.VERSION,
                 Constants.RUBY_VERSION,
                 Constants.COMPILE_DATE,
@@ -133,5 +151,42 @@ public class OutputStrings {
 
     public static String getCopyrightString() {
         return String.format("JRuby - Copyright (C) 2001-%s The JRuby Community (and contribs)", LocalDate.now().getYear());
+    }
+
+    private static String strBold(String str) {
+        if(Platform.IS_WINDOWS)
+            return str;
+
+        return "\033[1m" + str + "\033[0m";
+    }
+
+    private static String generateSpaces(int total) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < total; i++) {
+            sb.append(" ");
+        }
+
+        return sb.toString();
+    }
+
+    private static String breakLine(String str, int index, int spaces) {
+        StringBuilder sb = new StringBuilder();
+
+        String[] words = str.split("\\s");
+        int counter = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+
+            if (counter + word.length() <= index) {
+                counter += word.length();
+                sb.append(word).append(" ");
+            } else {
+                counter = 0;
+                if (i == words.length - 1) sb.append("\n" + generateSpaces(spaces) + word);
+                else sb.append("\n").append(generateSpaces(spaces));
+            }
+        }
+        return sb.toString();
     }
 }
