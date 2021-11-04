9b091f2002c124b3c4bde6ee363cdec03d9ca042
Thomas E. Enebo <tom.enebo@gmail.com>
2021-11-04 14:26:55 -0500

    tag out test we fail due to test impl

--- test/mri/excludes/TestParse.rb
+++ test/mri/excludes/TestParse.rb
@@ -3,3 +3,4 @@ exclude :test_method_location_in_rescue, "raise is not adding call_location elem
 exclude :test_negative_line_number, "JVM emits negative like some overflow (JIT only)"
 exclude :test_string, "lots of very specific error messages in which we differ a little"
 exclude :test_truncated_source_line, "2.5 truncates long source lines...we dont yet"
+exclude :test_void_expr_stmts_value, "We pass the last invalid next but the test harness runs with eval for JRbuy which then changes semantics"
