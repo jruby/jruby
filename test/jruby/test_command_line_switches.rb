f7a44d885b7b7f4c629d795e082208e21fd4db33
Charles Oliver Nutter <headius@headius.com>
2021-11-18 05:00:56 -0600

    Eliminate defunct taint and safe mode tests

--- test/jruby/test_command_line_switches.rb
+++ test/jruby/test_command_line_switches.rb
@@ -113,9 +113,9 @@ class TestCommandLineSwitches < Test::Unit::TestCase
     end
   end
 
-  def test_dash_little_v_version_verbose_T_taint_d_debug_K_kcode_r_require_b_benchmarks_a_splitsinput_I_loadpath_C_cwd_F_delimeter_J_javaprop_19
-    e_line = 'puts $VERBOSE, $SAFE, $DEBUG, Encoding.default_external, $F.join(59.chr), $LOAD_PATH.join(44.chr), Dir.pwd, Java::java::lang::System.getProperty(:foo.to_s)'
-    args = "-v -T3 -d -a -n -Ihello -C .. -F, -e #{q + e_line + q}"
+  def test_dash_little_v_version_verbose_d_debug_K_kcode_r_require_b_benchmarks_a_splitsinput_I_loadpath_C_cwd_F_delimeter_J_javaprop_19
+    e_line = 'puts $VERBOSE, $DEBUG, Encoding.default_external, $F.join(59.chr), $LOAD_PATH.join(44.chr), Dir.pwd, Java::java::lang::System.getProperty(:foo.to_s)'
+    args = "-v -d -a -n -Ihello -C .. -F, -e #{q + e_line + q}"
     # the options at the end will add -J-Dfoo=bar to the args
     lines = jruby_with_pipe("echo 1,2,3", args, :foo => 'bar').split("\n")
     assert_equal 0, $?.exitstatus, "failed execution with output:\n#{lines}"
@@ -123,14 +123,13 @@ class TestCommandLineSwitches < Test::Unit::TestCase
 
     assert_match(/jruby \d+(\.\d+\.\d+)?/, lines[0])
     assert_match(/true$/, lines[1])
-    assert_equal "0", lines[2]
-    assert_equal "true", lines[3]
-    assert_equal "UTF-8", lines[4]
-    assert_equal "1;2;3", lines[5].rstrip
-    assert_match(/^hello/, lines[6])
+    assert_equal "true", lines[2]
+    assert_equal "UTF-8", lines[3]
+    assert_equal "1;2;3", lines[4].rstrip
+    assert_match(/^hello/, lines[5])
     # The gsub is for windows
-    assert_equal "#{parent_dir}", lines[7].gsub('\\', '/')
-    assert_equal "bar", lines[8]
+    assert_equal "#{parent_dir}", lines[6].gsub('\\', '/')
+    assert_equal "bar", lines[7]
 
     e_line = 'puts Gem'
     args = " -rrubygems -e #{q + e_line + q}"
