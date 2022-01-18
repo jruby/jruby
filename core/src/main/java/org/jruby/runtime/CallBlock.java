53f18bc936001eaeab4a5344f034b31d015f3aa4
Thomas E. Enebo <tom.enebo@gmail.com>
2022-01-18 15:26:08 -0600

    Add back a deprecated method (for now)

--- core/src/main/java/org/jruby/runtime/CallBlock.java
+++ core/src/main/java/org/jruby/runtime/CallBlock.java
@@ -52,6 +52,12 @@ public class CallBlock extends BlockBody {
         return new Block(body, binding);
     }
 
+    // Put back because fishwife 1.10.1 still relies on this.
+    @Deprecated
+    public static Block newCallClosure(IRubyObject self, RubyModule imClass, Arity arity, BlockCallback callback, ThreadContext context) {
+        return newCallClosure(self, imClass, Signature.from(arity), callback, context);
+    }
+
     private CallBlock(ThreadContext context, Signature signature, BlockCallback callback) {
         super(signature);
         this.callback = callback;
