84170d5d40a0a4fdbdf8a3a4afac2f36528357e2
Charles Oliver Nutter <headius@headius.com>
2022-01-26 17:51:48 -0600

    Update default and bundled gems for 3.1
    Several gems have added JRuby support but we are stiil unable to
    support many others. I have added issues and links for most gems
    we do not currently support. I did not file issues for debug, rbs,
    and typeprof because they are heavily vested in CRuby internals
    and it is unclear how will will support them in the future.

--- tool/sync_ruby_files.rb
+++ tool/sync_ruby_files.rb
@@ -1,23 +1,15 @@
 STDLIB_FILES = %w[
-  English.rb
   date
   date.rb
-  debug.rb
   net
   resolv.rb
   tmpdir.rb
   unicode_normalize
-  uri
-  uri.rb
   weakref.rb
-  yaml
-  yaml.rb
 ]
 
 EXT_FILES = {
   'ext/bigdecimal/lib/bigdecimal' => 'bigdecimal',
-  'ext/digest/lib/digest.rb' => 'digest.rb',
-  'ext/digest/sha2/lib/sha2.rb' => 'digest/sha2.rb',
   'ext/nkf/lib/kconv.rb' => 'kconv.rb',
   'ext/pathname/lib/pathname.rb' => 'pathname.rb',
   'ext/pty/lib/expect.rb' => 'expect.rb',
