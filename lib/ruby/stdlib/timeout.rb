8248f4694fff96a59074de14686fa28263416067
Charles Oliver Nutter <headius@headius.com>
2022-02-09 15:20:31 -0600

    Move native Timeout into Ruby
    This largely works the same as the Java code, but written in Ruby.
    
    It passes all but one test, test_skip_rescue, which requires the
    timeout sub-thread to throw rather than raise the exception, so
    that it can bypass rescue blocks and still be raised even if the
    body of the timeout swallows the actual exception (throw is not
    subject to rescues).

--- lib/ruby/stdlib/timeout.rb
+++ lib/ruby/stdlib/timeout.rb
@@ -131,3 +131,70 @@ module Timeout
 
   module_function :timeout
 end
+
+if RUBY_ENGINE == "jruby"
+  module Timeout
+    module JRuby
+      java_import java.util.concurrent.ScheduledThreadPoolExecutor
+      java_import java.lang.Runtime
+      java_import org.jruby.threading.DaemonThreadFactory
+      java_import java.util.concurrent.atomic.AtomicBoolean
+      java_import org.jruby.RubyTime
+      java_import java.util.concurrent.TimeUnit
+      java_import java.util.concurrent.ExecutionException
+      java_import java.lang.InterruptedException
+
+      EXECUTOR = ScheduledThreadPoolExecutor.new(
+        Runtime.runtime.available_processors,
+        DaemonThreadFactory.new)
+      EXECUTOR.remove_on_cancel_policy = true
+    end
+
+    module_function def timeout(sec, klass = nil, message = nil, &block)
+      #:yield: +sec+
+      return yield(sec) if sec == nil or sec.zero?
+
+      message ||= "execution expired".freeze
+
+      if Fiber.respond_to?(:current_scheduler) && (scheduler = Fiber.current_scheduler)&.respond_to?(:timeout_after)
+        return scheduler.timeout_after(sec, klass || Error, message, &block)
+      end
+
+      current_thread = Thread.current
+      latch = JRuby::AtomicBoolean.new(false);
+
+      id = klass.nil? ? Object.new : nil;
+
+      sec_float = JRuby::RubyTime.convert_time_interval sec
+
+      timeout_runnable = -> {
+        # check latch to see if we have been canceled
+        if latch.compare_and_set(false, true)
+          if klass.nil?
+            timeout_exception = Timeout::Error.new(message)
+            timeout_exception.instance_variable_set(:@exception_id, id)
+            current_thread.raise(timeout_exception)
+          else
+            current_thread.raise(klass, message);
+          end
+        end
+      }
+
+      timeout_future = JRuby::EXECUTOR.schedule(timeout_runnable, sec_float * 1_000_000, JRuby::TimeUnit::MICROSECONDS)
+      begin
+        yield sec
+      ensure
+        if latch.compare_and_set(false, true) && timeout_future.cancel(false)
+          # ok, exception will not fire (also cancel caused task to be removed)
+        else
+          # future is not cancellable, wait for it to run
+          begin
+            timeout_future.get
+          rescue JRuby::ExecutionException
+          rescue JRuby::InterruptedException
+          end
+        end
+      end
+    end
+  end
+end
