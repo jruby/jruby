a6ffd6a6753b9ff163f318725f6714105436f7d9
Charles Oliver Nutter <headius@headius.com>
2022-03-29 15:07:28 -0500

    Update excludes for ostruct
    The two existing failures were resolved:
    
    * test_method_missing depends on the native method_missing not
      appearing in NoMethodError backtrace.
    * test_to_h_with_block was likely fixed by updating ostruct.
    
    Two new failures are due to ruby/ostruct#41 and will be fixed by
    ruby/ostruct#45: test_each_pair, test_initialize

--- test/mri/excludes/TC_OpenStruct.rb
+++ test/mri/excludes/TC_OpenStruct.rb
@@ -1,2 +1,2 @@
-exclude :test_method_missing, "depends on __callee__ which is broken"
-exclude :test_to_h_with_block, "test is newer than the released ostruct 0.1.0 (https://github.com/ruby/ostruct/issues/31)"
+exclude :test_each_pair, "Bug in each_pair in ostruct 0.5.4 (https://github.com/ruby/ostruct/pull/45)"
+exclude :test_initialize, "Bug in each_pair in ostruct 0.5.4 (https://github.com/ruby/ostruct/pull/45)"
