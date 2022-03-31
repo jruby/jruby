26dbcbedb1ef4eb43d954182ebe160a4b3383556
Charles Oliver Nutter <headius@headius.com>
2022-03-31 11:07:45 -0500

    All ostruct tests pass

--- test/mri/excludes/TC_OpenStruct.rb
+++
@@ -1,2 +0,0 @@
-exclude :test_each_pair, "Bug in each_pair in ostruct 0.5.4 (https://github.com/ruby/ostruct/pull/45)"
-exclude :test_initialize, "Bug in each_pair in ostruct 0.5.4 (https://github.com/ruby/ostruct/pull/45)"
