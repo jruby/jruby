d5586ff51dcd1f5076cae82246c170f3f93d25f5
kares <self@kares.org>
2022-09-07 14:56:25 +0200

    [fix] properly set/restore $! in ensure

--- core/src/main/java/org/jruby/ir/IRBuilder.java
+++ core/src/main/java/org/jruby/ir/IRBuilder.java
@@ -2840,9 +2840,7 @@ public class IRBuilder {
 
         // Record $! save var if we had a non-empty rescue node.
         // $! will be restored from it where required.
-        if (ensureBodyNode instanceof RescueNode) {
-            ebi.savedGlobalException = savedGlobalException;
-        }
+        ebi.savedGlobalException = savedGlobalException;
 
         ensureBodyBuildStack.push(ebi);
         Operand ensureRetVal = ensureNode == null ? manager.getNil() : build(ensureNode);
@@ -2887,6 +2885,9 @@ public class IRBuilder {
 
         // Now emit the ensure body's stashed instructions
         if (ensureNode != null) {
+            Variable exc2 = createTemporaryVariable();
+            addInstr(new ReceiveRubyExceptionInstr(exc2));
+            addInstr(new RestoreErrorInfoInstr(exc2));
             ebi.emitBody(this);
         }
 
