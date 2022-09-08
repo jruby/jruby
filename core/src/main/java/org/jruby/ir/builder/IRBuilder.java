4ddc713787d3706752f1c9cecfa1611f13a294bb
kares <self@kares.org>
2022-09-08 13:57:37 +0200

    [temp] partial revert to debug regression

--- core/src/main/java/org/jruby/ir/builder/IRBuilder.java
+++ core/src/main/java/org/jruby/ir/builder/IRBuilder.java
@@ -250,7 +250,7 @@ public abstract class IRBuilder<U, V, W, X, Y, Z> {
 
         // Now emit the ensure body's stashed instructions
         if (ensureNode != null) {
-            addInstr(new RestoreErrorInfoInstr(exc));
+//            addInstr(new RestoreErrorInfoInstr(exc));
             ebi.emitEnsureBody(this);
         }
 
