5b56439b2c1574100d6b58907b97e87a5778e12a
Charles Oliver Nutter <headius@headius.com>
2022-11-03 10:55:32 -0500

    Another stdlib wip

--- test/mri/excludes_wip/TestNetHTTPS.rb
+++ test/mri/excludes_wip/TestNetHTTPS.rb
@@ -1,3 +1,4 @@
 exclude :"test_get", "work in progress"
 exclude :"test_get_SNI", "work in progress"
 exclude :"test_get_SNI_failure", "work in progress"
+exclude :"test_skip_hostname_verification", "work in progress"
