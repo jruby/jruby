2199b81fbf679607ed80ffd3c7fdb1624940801e
Kiichi Hiromasa <k77ch7@gmail.com>
2022-11-13 15:35:40 +0900

    remove wip tags

--- test/mri/excludes_wip/Rational_Test.rb
+++ test/mri/excludes_wip/Rational_Test.rb
@@ -1,5 +1,1 @@
-exclude :"test_Rational_without_exception", "work in progress"
-exclude :"test_conv", "work in progress"
 exclude :"test_gcd_no_memory_leak", "work in progress"
-exclude :"test_ratsub", "work in progress"
-
