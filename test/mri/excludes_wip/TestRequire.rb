f85e422f399572c2cb90c1f29b049aaa2d0e5b8c
Charles Oliver Nutter <headius@headius.com>
2022-11-14 11:17:22 -0600

    Remove WIP exclude for wrapped load

--- test/mri/excludes_wip/TestRequire.rb
+++ test/mri/excludes_wip/TestRequire.rb
@@ -1,4 +1,3 @@
-exclude :"test_load_into_module", "work in progress"
 exclude :"test_provide_in_required_file", "work in progress"
 exclude :"test_require_nonascii_path", "work in progress"
 exclude :"test_resolve_feature_path", "work in progress"
