05b2f3b48e5b786ae24d56f5e68ea2a222c9e28a
Charles Oliver Nutter <headius@headius.com>
2022-11-17 15:49:49 -0600

    Clean up test_hash excludes

--- test/mri/excludes/TestHash/TestSubHash.rb
+++ test/mri/excludes/TestHash/TestSubHash.rb
@@ -1,9 +1,10 @@
-exclude :test_AREF_fstring_key, "Depends on MRI-specific GC.stat key"
-exclude :test_ASET_fstring_key, "due https://github.com/jruby/jruby/commit/f3f0091da7d98c5df285"
-
-# These are all excluded as a group because we do not generally randomize hashes.
-# We may want or need to follow MRI lead here if we are concerned about the other hashDOS vectors.
-# See https://bugs.ruby-lang.org/issues/13002
-exclude :test_float_hash_random, "JRuby does not randomize hash calculation for Hash"
-exclude :test_integer_hash_random, "JRuby does not randomize hash calculation for Hash"
-exclude :test_symbol_hash_random, "JRuby does not randomize hash calculation for Hash"
+exclude :test_AREF_fstring_key, "Depends on MRI-specific ObjectSpace.count_objects"
+exclude :test_callcc, "needs investigation"
+exclude :test_callcc_escape, "needs investigation"
+exclude :test_callcc_iter_level, "needs investigation"
+exclude :test_callcc_reenter, "needs investigation"
+exclude :test_eql, "needs investigation"
+exclude :test_exception_in_rehash_memory_leak, "no working assert_no_memory_leak method"
+exclude :test_fetch_error, "needs investigation"
+exclude :test_integer_hash_random, "JRuby does not randomize hash calculation for integer keys, see https://bugs.ruby-lang.org/issues/13002"
+exclude :test_replace_memory_leak, "no working assert_no_memory_leak method"
