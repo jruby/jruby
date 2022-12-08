278bf62cbd66d36b872a70b991e48d2aeca1dee8
Charles Oliver Nutter <headius@headius.com>
2022-12-08 06:19:08 -0600

    Start moving shape reference into shaped objects
    In order to evolve the shape of objects as more instance variables
    are discovered, we need to be able to access  multiple different
    shaped instances with their own layouts. Previously, only one
    shape could be associated with a given class, based on a static
    inspection of all instance variable accesses in the class's
    hierarchy. In order to keep stale-shaped objects functional, this
    commit adds a shape reference to all shaped RubyObject subtypes.
    With this we can allocate a first object using no instance vars
    (falling back on the default varTable layout) and as instance vars
    are encountered modify allocation to create wider object shapes.
    
    The step here simply adds the shape reference to all shaped
    objects; evolving that shape and updating the allocator will come
    in future commits.

--- core/src/main/java/org/jruby/RubyBasicObject.java
+++ core/src/main/java/org/jruby/RubyBasicObject.java
@@ -472,6 +472,10 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
         return ((RubyBasicObject) arg).metaClass;
     }
 
+    public VariableTableManager getShape() {
+        return metaClass.getVariableTableManager();
+    }
+
     /** rb_singleton_class
      *
      * Note: this method is specialized for RubyFixnum, RubySymbol,
@@ -1051,7 +1055,7 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
      * object is frozen.
      */
     protected long getObjectId() {
-        return metaClass.getRealClass().getVariableTableManager().getObjectId(this);
+        return getShape().getObjectId(this);
     }
 
     /** rb_obj_inspect
@@ -1129,7 +1133,7 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
         final ThreadContext context = runtime.getCurrentContext();
 
         boolean first = true;
-        for (Map.Entry<String, VariableAccessor> entry : metaClass.getVariableTableManager().getVariableAccessorsForRead().entrySet()) {
+        for (Map.Entry<String, VariableAccessor> entry : getShape().getVariableAccessorsForRead().entrySet()) {
             Object value = entry.getValue().get(this);
             if (!(value instanceof IRubyObject)) continue;
             RubySymbol symbol = runtime.newSymbol(entry.getKey());
@@ -1287,15 +1291,15 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
     public void setVariable(int index, Object value) {
         ensureInstanceVariablesSettable();
         if (index < 0) return;
-        metaClass.getVariableTableManager().setVariableInternal(this, index, value);
+        getShape().setVariableInternal(this, index, value);
     }
 
     public final Object getFFIHandle() {
-        return metaClass.getVariableTableManager().getFFIHandle(this);
+        return getShape().getFFIHandle(this);
     }
 
     public final void setFFIHandle(Object value) {
-        metaClass.getVariableTableManager().setFFIHandle(this, value);
+        getShape().setFFIHandle(this, value);
     }
 
     //
@@ -1309,7 +1313,7 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
      */
     @Override
     public boolean hasVariables() {
-        return metaClass.getVariableTableManager().hasVariables(this);
+        return getShape().hasVariables(this);
     }
 
     /**
@@ -1319,7 +1323,7 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
      * @return true if there are set instance variables, false otherwise
      */
     protected boolean hasInstanceVariables() {
-        return metaClass.getVariableTableManager().hasInstanceVariables(this);
+        return getShape().hasInstanceVariables(this);
     }
 
     /**
@@ -1396,7 +1400,7 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
      * table, and returning the removed value.
      */
     protected Object variableTableRemove(String name) {
-        return metaClass.getVariableTableManager().clearVariable(this, name);
+        return getShape().clearVariable(this, name);
     }
 
     /**
@@ -1467,7 +1471,7 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
      */
     @Override
     public void syncVariables(IRubyObject other) {
-        metaClass.getVariableTableManager().syncVariables(this, other);
+        getShape().syncVariables(this, other);
     }
 
     //
@@ -2983,7 +2987,7 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
         oos.defaultWriteObject();
         oos.writeUTF(metaClass.getName());
 
-        metaClass.getVariableTableManager().serializeVariables(this, oos);
+        getShape().serializeVariables(this, oos);
     }
 
     /**
@@ -3009,7 +3013,7 @@ public class RubyBasicObject implements Cloneable, IRubyObject, Serializable, Co
         ois.defaultReadObject();
         metaClass = (RubyClass)ruby.getClassFromPath(ois.readUTF());
 
-        metaClass.getVariableTableManager().deserializeVariables(this, ois);
+        getShape().deserializeVariables(this, ois);
     }
 
     /**
