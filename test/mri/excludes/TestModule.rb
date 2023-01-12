5bcd9acdb4c5768402cf69fa3a9468c1de6047e7
Charles Oliver Nutter <headius@headius.com>
2023-01-12 13:00:07 -0600

    Remove passing excludes for *_method_defined

--- test/mri/excludes/TestModule.rb
+++ test/mri/excludes/TestModule.rb
@@ -8,8 +8,6 @@ exclude :test_deprecate_nil_constant, "constants may recache after crossing JIT 
 exclude :test_include_module_with_constants_does_not_invalidate_method_cache, "no RubyVM constant in JRuby"
 exclude :test_initialize_copy_empty, "needs investigation"
 exclude :test_invalid_attr, "needs investigation"
-exclude :test_method_defined, "unfinished in initial 2.6 work, #6161"
-exclude :test_method_defined_without_include_super, "unfinished in initial 2.6 work, #6161"
 exclude :test_method_redefinition, "needs investigation"
 exclude :test_private_constant_reopen, "needs investigation"
 exclude :test_private_constant_with_no_args, "needs investigation"
