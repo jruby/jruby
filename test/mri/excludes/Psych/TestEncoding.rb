d74adfd9e063715c51b31dbd4c09deb7781f432d
Charles Oliver Nutter <headius@headius.com>
2023-01-26 14:24:56 -0600

    Remove passing excludes for Psych

--- test/mri/excludes/Psych/TestEncoding.rb
+++ test/mri/excludes/Psych/TestEncoding.rb
@@ -3,5 +3,4 @@ exclude :test_dump_non_ascii_string_to_file, "missing fix, see tenderlove/psych#
 exclude :test_emit_alias, "encoding support not implemented"
 exclude :test_start_mapping, "encoding support not implemented"
 exclude :test_start_sequence, "encoding support not implemented"
-exclude :test_io_shiftjis, "encoding support not implemented"
 
