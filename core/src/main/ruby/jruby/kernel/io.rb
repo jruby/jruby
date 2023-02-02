c61c7220984e99dfaf5adfb2f54ef3ed70c565c1
Charles Oliver Nutter <headius@headius.com>
2023-02-02 09:56:16 -0600

    Initial stab at IO::Buffer

--- core/src/main/ruby/jruby/kernel/io.rb
+++ core/src/main/ruby/jruby/kernel/io.rb
@@ -56,4 +56,253 @@ class IO
     end
     result
   end
+
+  class Buffer
+    include Comparable
+
+    class LockedError < RuntimeError; end
+    class AllocationError < RuntimeError; end
+    class AccessError < RuntimeError; end
+    class InvalidatedError < RuntimeError; end
+    class MaskError < RuntimeError; end
+
+    EXTERNAL = 1
+    INTERAL = 2
+    MAPPED = 3
+    SHARED = 4
+    LOCKED = 5
+    PRIVATE = 6
+    READONLY = 7
+
+    LITTLE_ENDIAN = 8
+    BIG_ENDIAN = 9
+    HOST_ENDIAN = 10
+    NETWORK_ENDIAN = 11
+
+    PAGE_SIZE = 0
+    DEFAULT_SIZE = 0
+
+    java_import java.nio.ByteBuffer
+    java_import java.nio.channels.FileChannel
+
+    def self.for(type)
+
+    end
+
+    def self.map
+
+    end
+
+    def self.size_of
+
+    end
+
+    def initialize(size = DEFAULT_SIZE, flags = io_flags_for_size(size)
+      warn "Buffer is an experimental feature"
+
+      base = nil
+      if size.kind_of? ByteBuffer
+        base = size
+        size = base.capacity
+      end
+
+      buffer_initialize(base, size, flags, nil)
+    end
+
+    private def buffer_initialize(base, size, flags, source)
+      if base
+      elsif size
+        if flags & INTERNAL != 0
+          base = ByteBuffer.alloc(size)
+        elsif flags & MAPPED != 0
+          base = io_buffer_map_memory(size, flags)
+        end
+
+        raise BufferAllocationError, "Could not allocate buffer!"
+      else
+        return
+      end
+
+      @base = base
+      @size = size
+      @flags = flags
+      @source = source
+    end
+
+    private def io_buffer_map_memory(size, flags)
+      # TODO: flags
+      map_mode = FileChannel::MapMode::READ_WRITE;
+      if flags & SHARED != 0
+        # ???
+      else
+        map_mode = FileChannel::MapMode::PRIVATE
+      end
+
+      begin
+        FileChannel.map(map_mode, 0, size)
+      rescue => e
+        raise SystemError, "failed to memory map buffer"
+      end
+    end
+
+    private def io_flags_for_size(size)
+      return size > PAGE_SIZE ? MAPPED : INTERNAL
+    end
+
+    def initialize_copy(copy)
+
+    end
+
+    def inspect
+
+    end
+
+    def hexdump
+
+    end
+
+    def to_s
+
+    end
+
+    def size
+      @size
+    end
+
+    def valid?
+      true
+    end
+
+    def transfer
+
+    end
+
+    def null?
+      @base == nil
+    end
+
+    def empty?
+      @size == 0
+    end
+
+    def external?
+      @base.direct?
+    end
+
+    def internal?
+      !external?
+    end
+
+    def mapped?
+      @base kind_of MappedByteBuffer
+    end
+
+    def shared?
+    end
+
+    def locked?
+    end
+
+    def readonly?
+      @base.read_only?
+    end
+
+    def locked
+    end
+
+    def slice(offset = 0, length = @size - offset)
+      if offset < 0
+        raise ArgumentError, "Offset can't be negative!"
+      end
+
+      if (length < 0)
+        raise ArgumentError, "Length can't be negative!"
+      end
+
+      # TODO: honor offset and length
+      Buffer.new(@base.slice)
+    end
+
+    def <=>(other)
+
+    end
+
+    def resize
+      
+    end
+
+    def clear
+
+    end
+
+    def free
+
+    end
+
+    def get_value
+    end
+
+    def get_values
+    end
+
+    def each
+    end
+
+    def values
+    end
+
+    def each_byte
+    end
+
+    def set_value
+    end
+
+    def set_values
+    end
+
+    def copy
+    end
+
+    def get_string
+    end
+
+    def set_string
+    end
+
+    def &
+    end
+
+    def |
+    end
+
+    def ^
+    end
+
+    def ~
+    end
+
+    def and!
+    end
+
+    def or!
+    end
+
+    def xor!
+    end
+
+    def not!
+    end
+
+    def read
+    end
+
+    def pread
+    end
+
+    def write
+    end
+
+    def pwrite
+    end
+  end
 end
