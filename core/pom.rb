2041056044feb5c7ec9bfa71271eaa455f08daef
Charles Oliver Nutter <headius@headius.com>
2023-03-16 07:41:36 -0500

    Use invokebinder 1.13 to avoid logger security check
    See https://github.com/headius/invokebinder/pull/15 and #7720.
    
    Fixes #7726.

--- core/pom.rb
+++ core/pom.rb
@@ -55,7 +55,7 @@ project 'JRuby Base' do
   jar 'org.jruby.jcodings:jcodings:1.0.58'
   jar 'org.jruby:dirgra:0.3'
 
-  jar 'com.headius:invokebinder:1.12'
+  jar 'com.headius:invokebinder:1.13'
   jar 'com.headius:options:1.6'
 
   jar 'com.jcraft:jzlib:1.1.3'
