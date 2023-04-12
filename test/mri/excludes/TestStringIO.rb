02630479a7ad2c0f0527f61c8a005e71e8687530
Charles Oliver Nutter <headius@headius.com>
2023-04-12 15:23:25 -0500

    Exclude failing StringIO test temporarily
    This will be fixed once ruby/stringio#48 is merged and released.

--- test/mri/excludes/TestStringIO.rb
+++ test/mri/excludes/TestStringIO.rb
@@ -1,2 +1,3 @@
 exclude :test_overflow, "unusual subprocess test trying to overflow some value"
+exclude :test_read_nonblock_no_exceptions, "temporary until StringIO ext does manual arity-checking (ruby/stringio#48)"
 exclude :test_write_integer_overflow, "JVM does not support > 32bit signed array offsets, so our StringIO cannot either"
