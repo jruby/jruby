5a865a342fd6ad0e97fdf832b0926dbacaa439f0
Charles Oliver Nutter <headius@headius.com>
2023-05-12 15:14:41 +0900

    Basic set/get_string and read/write

--- core/src/main/ruby/jruby/kernel/io.rb
+++ core/src/main/ruby/jruby/kernel/io.rb
@@ -2,7 +2,7 @@ class IO
   class EAGAINWaitReadable < Errno::EAGAIN
     include IO::WaitReadable
   end
-  
+
   class EAGAINWaitWritable < Errno::EAGAIN
     include IO::WaitWritable
   end
@@ -56,7 +56,6 @@ class IO
     end
     result
   end
-
   class Buffer
     include Comparable
 
@@ -67,13 +66,12 @@ class IO
     class MaskError < RuntimeError; end
 
     EXTERNAL = 1
-    INTERAL = 2
+    INTERNAL = 2
     MAPPED = 3
     SHARED = 4
     LOCKED = 5
     PRIVATE = 6
     READONLY = 7
-
     LITTLE_ENDIAN = 8
     BIG_ENDIAN = 9
     HOST_ENDIAN = 10
@@ -83,29 +81,23 @@ class IO
     DEFAULT_SIZE = 0
 
     java_import java.nio.ByteBuffer
-    java_import java.nio.channels.FileChannel
 
     def self.for(type)
-
     end
 
     def self.map
-
     end
 
     def self.size_of
-
     end
 
-    def initialize(size = DEFAULT_SIZE, flags = io_flags_for_size(size)
+    def initialize(size = DEFAULT_SIZE, flags = io_flags_for_size(size))
       warn "Buffer is an experimental feature"
-
       base = nil
       if size.kind_of? ByteBuffer
         base = size
         size = base.capacity
       end
-
       buffer_initialize(base, size, flags, nil)
     end
 
@@ -113,16 +105,14 @@ class IO
       if base
       elsif size
         if flags & INTERNAL != 0
-          base = ByteBuffer.alloc(size)
+          base = ByteBuffer.allocate(size)
         elsif flags & MAPPED != 0
           base = io_buffer_map_memory(size, flags)
         end
-
-        raise BufferAllocationError, "Could not allocate buffer!"
+        # raise BufferAllocationError, "Could not allocate buffer!"
       else
         return
       end
-
       @base = base
       @size = size
       @flags = flags
@@ -137,7 +127,6 @@ class IO
       else
         map_mode = FileChannel::MapMode::PRIVATE
       end
-
       begin
         FileChannel.map(map_mode, 0, size)
       rescue => e
@@ -150,19 +139,15 @@ class IO
     end
 
     def initialize_copy(copy)
-
     end
 
     def inspect
-
     end
 
     def hexdump
-
     end
 
     def to_s
-
     end
 
     def size
@@ -174,7 +159,6 @@ class IO
     end
 
     def transfer
-
     end
 
     def null?
@@ -194,7 +178,7 @@ class IO
     end
 
     def mapped?
-      @base kind_of MappedByteBuffer
+      @base.kind_of MappedByteBuffer
     end
 
     def shared?
@@ -214,29 +198,23 @@ class IO
       if offset < 0
         raise ArgumentError, "Offset can't be negative!"
       end
-
       if (length < 0)
         raise ArgumentError, "Length can't be negative!"
       end
-
       # TODO: honor offset and length
       Buffer.new(@base.slice)
     end
 
     def <=>(other)
-
     end
 
     def resize
-      
     end
 
     def clear
-
     end
 
     def free
-
     end
 
     def get_value
@@ -263,10 +241,22 @@ class IO
     def copy
     end
 
-    def get_string
+    def get_string(str, offset = 0, length = nil, encoding = nil)
+      @base.position(offset)
+      if length
+        buffer = Java::byte[length].new
+      else
+        buffer = Java::byte[@base.size].new
+      end
+      @base.get(buffer)
+      str = String.from_java_bytes(buffer)
+      str.force_encoding(encoding) if encoding
+      str
     end
 
-    def set_string
+    def set_string(str, offset = 0, length = nil, source_offset = 0)
+      @base.position(offset)
+      @base.put(str.to_java_bytes, source_offset, length ? length : str.length)
     end
 
     def &
@@ -293,13 +283,19 @@ class IO
     def not!
     end
 
-    def read
+    def read(io, length)
+      @base.position(0)
+      @base.limit(length)
+      i = io.to_channel.read(@base)
     end
 
     def pread
     end
 
-    def write
+    def write(io, length)
+      @base.position(0)
+      @base.limit(length)
+      io.to_channel.write(@base)
     end
 
     def pwrite
