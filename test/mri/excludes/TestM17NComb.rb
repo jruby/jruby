5073e31de6daf71da0ddf4a36959e977e1fd7ffc
Charles Oliver Nutter <headius@headius.com>
2023-05-22 17:48:44 -0500

    Exclude this when run with indy
    Fails in CI with indy enabled for unknown reasons.
    
    See jruby/jruby#7803.

--- test/mri/excludes/TestM17NComb.rb
+++ test/mri/excludes/TestM17NComb.rb
@@ -2,4 +2,5 @@ exclude :test_bug11486, "fix from https://bugs.ruby-lang.org/issues/11486 did no
 exclude :test_str_intern, "needs investigation"
 exclude :test_str_count, "does not raise compatibility error"
 exclude :test_str_crypt_nonstrict, "#crypt failing: Errno::EINVAL: Invalid argument"
+exclude :test_str_squeeze, "fails in CI with indy, see jruby/jruby#7803" if org.jruby.util.cli.Options::COMPILE_INVOKEDYNAMIC.load
 exclude :test_str_smart_chomp, "see jruby/jruby#3692"
