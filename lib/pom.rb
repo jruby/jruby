e478df1e3f303e43a9f3abf70c9b2580cf5426e8
Charles Oliver Nutter <headius@headius.com>
2023-10-19 19:05:04 -0500

    Switch tempfile to default gem
    See https://github.com/ruby/tempfile/issues/7

--- lib/pom.rb
+++ lib/pom.rb
@@ -105,8 +105,7 @@ default_gems = [
       ['ffi-bindings-libfixposix', '0.5.1.0'],
     # https://github.com/ruby/syslog/issues/1
     # ['syslog', '0.1.0'],
-    # https://github.com/ruby/tempfile/issues/7
-    # ['tempfile', '0.1.2'],
+    ['tempfile', '0.1.3'],
     ['time', '0.2.2'],
     ['timeout', '0.3.2'],
     # https://github.com/ruby/tmpdir/issues/13
