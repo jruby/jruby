705d90253102abf8a78421d3569e15e995a9f79d
Charles Oliver Nutter <headius@headius.com>
2023-12-04 21:29:14 -0600

    Switch JIT literal re-construct tags to #8030

--- test/mri/excludes/TestSubHash.rb
+++ test/mri/excludes/TestSubHash.rb
@@ -1,2 +1,2 @@
-exclude :test_NEWHASH_fstring_key, "https://github.com/jruby/jruby/issues/7713"
+exclude :test_NEWHASH_fstring_key, "https://github.com/jruby/jruby/issues/8030"
 exclude :test_replace_memory_leak, "no working assert_no_memory_leak method"
