9ca7bc9a4d962d6996ca66a06a5b6cac736d77a2
Charles Oliver Nutter <headius@headius.com>
2024-01-17 17:22:15 -0600

    Call initialize_dup and _clone from Proc#dup and #clone

--- core/src/main/java/org/jruby/RubyProc.java
+++ core/src/main/java/org/jruby/RubyProc.java
@@ -48,6 +48,7 @@ import org.jruby.runtime.BlockBody;
 import org.jruby.runtime.ClassIndex;
 import org.jruby.runtime.Helpers;
 import org.jruby.runtime.IRBlockBody;
+import org.jruby.runtime.JavaSites;
 import org.jruby.runtime.MethodBlockBody;
 import org.jruby.runtime.ObjectAllocator;
 import org.jruby.runtime.Signature;
@@ -212,14 +213,20 @@ public class RubyProc extends RubyObject implements DataType {
     }
 
     @JRubyMethod(name = "clone")
-    @Override
-    public IRubyObject rbClone() {
-    	return newProc(getRuntime(), block, type, file, line);
+    public IRubyObject rbClone(ThreadContext context) {
+        RubyProc clone = procDup();
+        sites(context).initialize_clone.call(context, clone, clone, this);
+        return clone;
     }
 
     @JRubyMethod(name = "dup")
-    @Override
-    public IRubyObject dup() {
+    public IRubyObject dup(ThreadContext context) {
+        RubyProc dup = procDup();
+        sites(context).initialize_dup.call(context, dup, dup, this);
+        return dup;
+    }
+
+    private RubyProc procDup() {
         return newProc(getRuntime(), block, type, file, line);
     }
 
@@ -453,6 +460,10 @@ public class RubyProc extends RubyObject implements DataType {
         return type.equals(Block.Type.THREAD);
     }
 
+    private static JavaSites.ProcSites sites(ThreadContext context) {
+        return context.sites.Proc;
+    }
+
     @Deprecated
     public final IRubyObject call19(ThreadContext context, IRubyObject[] args, Block block) {
         return call(context, args, block);
@@ -466,6 +477,16 @@ public class RubyProc extends RubyObject implements DataType {
     @Deprecated
     public final IRubyObject call(ThreadContext context, IRubyObject[] args, IRubyObject self, Block passedBlock) {
         return block.call(context, args, passedBlock);
+    }
+
+    @Deprecated
+    public IRubyObject rbClone() {
+        return rbClone(getRuntime().getCurrentContext());
+    }
+
+    @Deprecated
+    public IRubyObject dup() {
+        return dup(getRuntime().getCurrentContext());
     }
 
     public void setFromMethod() {
