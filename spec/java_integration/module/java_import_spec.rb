222c75d1b61732736c540753c15e6f09bc09783c
kares <self@kares.org>
2024-02-10 15:02:47 +0100

    [ji] fix java_import when constant exists in parent (Object)

--- spec/java_integration/module/java_import_spec.rb
+++ spec/java_integration/module/java_import_spec.rb
@@ -46,5 +46,35 @@ describe "java_import" do
     expect(mod::CapsInnerClass).to eq(InnerClasses::CapsInnerClass)
   end
 
+  class Object
+    java_import 'javax.lang.model.element.Element'
+  end
+
+  module JavaImportSpec
+    java_import 'javax.lang.model.element.Element'
+    module Nested
+      java_import org.w3c.dom.Element
+      module Nested2
+        java_import 'org.w3c.dom.Element'
+      end
+    end if Element
+    module NestedAgain
+      java_import 'javax.lang.model.element.Element'
+    end
+  end
+
+  it "sets constant into proper scope (when parent has same name)" do
+    Object.send(:remove_const, :Element)
+    expect(JavaImportSpec::Element).to be(javax.lang.model.element.Element) # used to fail in JRuby <= 9.4.5
+
+    expect(JavaImportSpec::Nested::Element).to be(org.w3c.dom.Element)
+
+    expect(JavaImportSpec::Nested::Nested2.constants).to eql [:Element]
+    expect(JavaImportSpec::NestedAgain.constants).to eql [:Element]
+
+    expect(JavaImportSpec::Nested::Nested2::Element).to be(org.w3c.dom.Element)
+    expect(JavaImportSpec::NestedAgain::Element).to be(javax.lang.model.element.Element)
+  end
+
 end
 
