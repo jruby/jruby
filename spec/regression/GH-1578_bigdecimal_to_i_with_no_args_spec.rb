213f8758119867284eefc0fbb304e642505eff2b
Charles Oliver Nutter <headius@headius.com>
2024-02-29 13:39:47 -0600

    Fixnum is now Integer

--- spec/regression/GH-1578_bigdecimal_to_i_with_no_args_spec.rb
+++ spec/regression/GH-1578_bigdecimal_to_i_with_no_args_spec.rb
@@ -2,24 +2,24 @@ require 'bigdecimal'
 
 describe "BigDecimal#round" do
   it "returns fixnum if no args are passed" do
-    expect(BigDecimal('1').round).to be_a(Fixnum)
+    expect(BigDecimal('1').round).to be_a(Integer)
   end
 end
 
 describe "BigDecimal#truncate" do
   it "returns fixnum if no args are passed" do
-    expect(BigDecimal('1').truncate).to be_a(Fixnum)
+    expect(BigDecimal('1').truncate).to be_a(Integer)
   end
 end
 
 describe "BigDecimal#floor" do
   it "returns fixnum if no args are passed" do
-    expect(BigDecimal('1').floor).to be_a(Fixnum)
+    expect(BigDecimal('1').floor).to be_a(Integer)
   end
 end
 
 describe "BigDecimal#ceil" do
   it "returns fixnum if no args are passed" do
-    expect(BigDecimal('1').ceil).to be_a(Fixnum)
+    expect(BigDecimal('1').ceil).to be_a(Integer)
   end
 end
