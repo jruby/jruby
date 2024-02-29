97a015cf3c2055878ac9843172b454c7200db00f
Charles Oliver Nutter <headius@headius.com>
2024-02-29 13:39:10 -0600

    eval now shows where it was called

--- spec/regression/GH-1446_line_number_in_warnings_spec.rb
+++ spec/regression/GH-1446_line_number_in_warnings_spec.rb
@@ -3,7 +3,7 @@
 
 describe "JRuby warnings" do
   specify "have the correct line number" do
-    warning = "(eval):1: warning: already initialized constant GH1446\n"
+    warning = /\(eval at .*spec\/regression\/GH-1446_line_number_in_warnings_spec\.rb:8\):1: warning: already initialized constant GH1446\n/
     expect($stderr).to receive(:write).with(warning)
     eval "GH1446=0;GH1446=1"
   end
