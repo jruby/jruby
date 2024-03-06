c86d1af5184046d316873d97dd2bd614e77d58ed
Charles Oliver Nutter <headius@headius.com>
2024-03-06 14:42:24 -0600

    These are not in public headers

--- core/src/main/java/org/jruby/api/MRI.java
+++ core/src/main/java/org/jruby/api/MRI.java
@@ -23,24 +23,12 @@ import static org.jruby.RubyString.newString;
 import static org.jruby.util.StringSupport.searchNonAscii;
 
 public class MRI {
-    public static IRubyObject rb_sys_fail_path(ThreadContext context, String path) {
-        return API.sysFailWithPath(context, path);
-    }
-
     public static int rb_pipe(ThreadContext context, int[] pipes) {
         return API.newPipe(context, pipes);
     }
 
     public static int rb_cloexec_pipe(ThreadContext context, int[] pipes) {
         return API.cloexecPipe(context, pipes);
-    }
-
-    public static void rb_maygvl_fd_fix_cloexec(ThreadContext context, int fd) {
-        API.fdFixCloexec(context, fd);
-    }
-
-    public static <T> T rb_rescue_typeerror(ThreadContext context, T dflt, Supplier<T> func) {
-        return API.rescueTypeError(context, dflt, func);
     }
 
     ///////////////////////////////////////////////////////////////////////////
