9fb14c55ddc5cf13a598f24dd5c0ad19d323188f
Charles Oliver Nutter <headius@headius.com>
2024-03-29 09:03:25 -0500

    Tag new hanging tests from MRI 3.4

--- test/mri/excludes/OpenSSL/TestPKeyDH.rb
+++ test/mri/excludes/OpenSSL/TestPKeyDH.rb
@@ -5,5 +5,6 @@ exclude :test_generate_key, 'needs investigation'
 exclude :test_key_exchange, 'needs investigation'
 exclude :test_marshal, "work in progress"
 exclude :test_new_break, 'needs investigation'
+exclude :test_new_break_on_non_fips, "hangs (tested on macos M1)"
 exclude :test_params_ok?, "work in progress"
 exclude :test_public_key, 'needs investigation'
