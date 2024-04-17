189dacf4f47226973fe68e428c43c852b11bedd2
Thomas E. Enebo <tom.enebo@gmail.com>
2024-04-17 14:00:39 -0400

    Hack in edge labels which seem to only show up in tooltip

--- core/src/main/java/org/jruby/ir/representations/IGVCFGVisitor.java
+++ core/src/main/java/org/jruby/ir/representations/IGVCFGVisitor.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import jnr.ffi.annotations.In;
 import org.jruby.ir.Tuple;
 import org.jruby.ir.instructions.BranchInstr;
 import org.jruby.ir.instructions.Instr;
@@ -25,10 +24,21 @@ import static org.jruby.ir.util.IGVHelper.startTag;
  * visitor altogether not accessed via CFG.
  */
 public class IGVCFGVisitor {
+    private class Edge {
+        final int from;
+        final Object to;
+        final String name;
+
+        public Edge(int from, Object to, String name) {
+            this.from = from;
+            this.to = to;
+            this.name = name;
+        }
+    }
     final PrintStream writer;
     final Map<BasicBlock, Integer> indexOffsets = new HashMap();
-    final List<Tuple<Integer, Integer>> instrEdges = new ArrayList();
-    final List<Tuple<Integer, JumpTargetInstr>> extraInstrEdges = new ArrayList();
+    final List<Edge> instrEdges = new ArrayList();
+    final List<Edge> extraInstrEdges = new ArrayList();
     Instr lastInstr = null; // Last instr from the previous BB.
     final IGVInstrListener listener;
 
@@ -78,12 +88,12 @@ public class IGVCFGVisitor {
 
             // Last BB processed needs to hook up to first in next one if not a Jump (fallthrough)
             if (lastInstr != null && !(lastInstr instanceof JumpInstr)) {
-                instrEdges.add(new Tuple(System.identityHashCode(lastInstr), lastIPC));
+                instrEdges.add(new Edge(System.identityHashCode(lastInstr), lastIPC, "fallthrough"));
             }
 
             for (int i = 1; i < size; i++) {
                 int ipc = Instr(instrs.get(i));
-                instrEdges.add(new Tuple(lastIPC, ipc));
+                instrEdges.add(new Edge(lastIPC, ipc, "exception"));
                 lastIPC = ipc;
             }
 
@@ -111,12 +121,14 @@ public class IGVCFGVisitor {
 
         startTag(writer, "edges");
 
-        for (Tuple<Integer, Integer> edge: instrEdges) {
-            emptyTag(writer, "edge", "from", edge.a, "to", edge.b);
+        for (Edge edge: instrEdges) {
+            emptyTag(writer, "edge", "from", edge.from, "to", edge.to, "label", edge.name, "name", edge.name);
         }
 
-        for (Tuple<Integer, JumpTargetInstr> edge: extraInstrEdges) {
-            emptyTag(writer, "edge", "from", edge.a, "to", indexOffsets.get(cfg.getBBForLabel(edge.b.getJumpTarget())));
+        for (Edge edge: extraInstrEdges) {
+            emptyTag(writer, "edge", "from", edge.from,
+                    "to", indexOffsets.get(cfg.getBBForLabel((((JumpTargetInstr) edge.to).getJumpTarget()))),
+                    "label", edge.name, "name", edge.name);
         }
 
         for (Tuple<Instr, Instr> edge: listener.getRemovedEdges()) {
@@ -141,7 +153,7 @@ public class IGVCFGVisitor {
         property(writer, "name", instr);
 
         // We have not processed all BBs yet so we cannot resolve ipc locations of the jumps destinations.
-        if (instr instanceof BranchInstr) extraInstrEdges.add(new Tuple(ipc, (JumpTargetInstr) instr));
+        if (instr instanceof BranchInstr) extraInstrEdges.add(new Edge(ipc, instr, "branch"));
 
         endTag(writer, "properties");
         endTag(writer, "node");
