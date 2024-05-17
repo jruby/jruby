91b06b2477ab0b8c41acc88320050a97ef124085
Édipo Féderle <edipofederle@gmail.com>
2024-05-17 07:50:24 -0300

    Fix ENV mri tests

--- core/src/main/java/org/jruby/RubyGlobal.java
+++ core/src/main/java/org/jruby/RubyGlobal.java
@@ -211,7 +211,7 @@ public class RubyGlobal {
         runtime.defineVariable(new BacktraceGlobalVariable(runtime, "$@"), THREAD);
 
         initSTDIO(runtime, globals);
-        
+
         runtime.defineVariable(new LoadedFeatures(runtime, "$\""), GLOBAL);
         runtime.defineVariable(new LoadedFeatures(runtime, "$LOADED_FEATURES"), GLOBAL);
 
@@ -431,6 +431,9 @@ public class RubyGlobal {
         public IRubyObject op_aref(ThreadContext context, IRubyObject arg) {
             IRubyObject key = arg.convertToString();
             IRubyObject value = internalGet(key);
+
+            EnvStringValidation.ensureValidEnvString(context.runtime, key, "key");
+
             if (value == null) return context.nil;
 
             RubyString string = (RubyString) newName(context, key, value);
@@ -450,11 +453,34 @@ public class RubyGlobal {
 
         @JRubyMethod(name = "assoc")
         public IRubyObject assoc(final ThreadContext context, IRubyObject obj) {
+            EnvStringValidation.ensureValidEnvString(context.getRuntime(), obj, "value");
+
             return super.assoc(context, verifyStringLike(context, obj).convertToString());
+        }
+
+        @JRubyMethod(name = "fetch", required = 1, optional = 2)
+        public IRubyObject fetch(ThreadContext context, IRubyObject[] args, Block block) {
+
+            EnvStringValidation.ensureValidEnvString(context.runtime, args[0], "key");
+            switch(args.length) {
+                case 1: return super.fetch(context, args[0], block);
+                case 2: return super.fetch(context, args[0], args[1], block);
+            }
+
+            return null;
+        }
+
+        @JRubyMethod(name = "fetch", required = 1)
+        public IRubyObject fetch(ThreadContext context, IRubyObject key, Block block) {
+            EnvStringValidation.ensureValidEnvString(context.getRuntime(), key, "key");
+
+            return super.fetch(context, verifyStringLike(context, key.convertToString()), block);
         }
 
         @JRubyMethod
         public IRubyObject delete(ThreadContext context, IRubyObject key, Block block) {
+            EnvStringValidation.ensureValidEnvString(context.getRuntime(), key, "key");
+
             return super.delete(context, verifyStringLike(context, key), block);
         }
 
@@ -488,11 +514,15 @@ public class RubyGlobal {
 
         @JRubyMethod(name = {"has_key?", "key?", "include?", "member?"})
         public RubyBoolean has_key_p(ThreadContext context, IRubyObject key) {
+            EnvStringValidation.ensureValidEnvString(context.runtime, key, "key");
+
             return internalGetEntry(verifyStringLike(context, key)) == NO_ENTRY ? context.fals : context.tru;
         }
 
         @JRubyMethod(name = {"has_value?", "value?"})
         public IRubyObject has_value_pp(ThreadContext context, IRubyObject expected) {
+            EnvStringValidation.ensureValidEnvString(context.runtime, expected, "value");
+
             if (!isStringLike(expected)) return context.nil;
 
             return super.has_value_p(context, expected.convertToString());
@@ -720,6 +750,9 @@ public class RubyGlobal {
                 if (valueAsStr == context.nil) {
                     synchronized (Object.class) { posix.unsetenv(keyAsJava); }
                 } else {
+                    EnvStringValidation.ensureValidEnvString(context.getRuntime(), value, "value");
+                    EnvStringValidation.ensureValidEnvString(context.getRuntime(), key, "key");
+
                     final String valueAsJava = valueAsStr.asJavaString();
                     synchronized (Object.class) { posix.setenv(keyAsJava, valueAsJava, 1); }
                 }
@@ -1245,4 +1278,18 @@ public class RubyGlobal {
             "$.", "$INPUT_LINE_NUMBER", // ARGF current line number
             "$FILENAME" // ARGF current file name
     ));
+
+    private static class EnvStringValidation {
+        public static void ensureValidEnvString(Ruby runtime, IRubyObject str, String type) {
+            RubyString value = str.convertToString();
+
+            if(!value.getByteList().getEncoding().isAsciiCompatible()){
+                throw runtime.newArgumentError("bad environment variable " + type + ": ASCII incompatible encoding: " + value.getByteList().getEncoding().toString());
+            }
+
+            if (value.toString().contains("\0")) {
+                throw runtime.newArgumentError("bad environment variable " + type + ": contains null byte");
+            }
+        }
+    }
 }
