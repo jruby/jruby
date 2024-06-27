jruby/jruby

commit e8dbe9c269f7723badb89d137b2a1d52b3d3400b

Remove or replace additional setAccessible with quiet version

core/src/main/java/org/jruby/util/io/SeekableByteChannelImpl.java
@@ -10,6 +10,8 @@
  */
 package org.jruby.util.io;
 
+import org.jruby.javasupport.Java;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.lang.reflect.Field;
@@ -147,7 +149,7 @@ final class SeekableByteChannelImpl extends AbstractInterruptibleChannel
     private static Field accessibleField(final String name) {
         try {
             Field field = ByteArrayInputStream.class.getDeclaredField(name);
-            field.setAccessible(true);
+            Java.trySetAccessible(field);
             return field;
         }
         catch (NoSuchFieldException ex) {

core/src/main/java/org/jruby/ext/ffi/io/FileDescriptorIO.java
@@ -102,30 +102,4 @@ public class FileDescriptorIO extends RubyIO {
     public static RubyIO wrap(ThreadContext context, IRubyObject recv, IRubyObject fd) {
         return new FileDescriptorIO(context.runtime, fd);
     }
-
-    static class FileDescriptorHelper {
-        static final java.lang.reflect.Constructor<java.io.FileDescriptor> CONSTRUCTOR;
-        static {
-            java.lang.reflect.Constructor<java.io.FileDescriptor> constructor;
-            try {
-                constructor = java.io.FileDescriptor.class.getDeclaredConstructor(int.class);
-                constructor.setAccessible(true);
-            } catch (Throwable t) {
-                constructor = null;
-            }
-            CONSTRUCTOR = constructor;
-        }
-
-        public static java.io.FileDescriptor wrap(int fileno) {
-            try {
-                return CONSTRUCTOR != null ? CONSTRUCTOR.newInstance(fileno) : new java.io.FileDescriptor();
-            } catch (IllegalAccessException iae) {
-                return new java.io.FileDescriptor();
-            } catch (InstantiationException ie) {
-                return new java.io.FileDescriptor();
-            } catch (java.lang.reflect.InvocationTargetException ite) {
-                return new java.io.FileDescriptor();
-            }
-        }
-    }
 }

core/src/main/java/org/jruby/javasupport/binding/MethodGatherer.java
@@ -1,6 +1,5 @@
 package org.jruby.javasupport.binding;
 
-import com.headius.backport9.modules.Modules;
 import org.jruby.Ruby;
 import org.jruby.RubyModule;
 import org.jruby.internal.runtime.methods.JavaMethod;
@@ -8,7 +7,6 @@ import org.jruby.java.invokers.ConstructorInvoker;
 import org.jruby.javasupport.Java;
 import org.jruby.javasupport.JavaClass;
 import org.jruby.javasupport.JavaSupport;
-import org.jruby.javasupport.JavaUtil;
 import org.jruby.runtime.Block;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.builtin.IRubyObject;
@@ -669,7 +667,7 @@ public class MethodGatherer {
             if (Modifier.isPrivate(mod)) return false;
 
             // Skip protected methods if we can't set accessible
-            if (!Modifier.isPublic(mod) && !Modules.trySetAccessible(method, Java.class)) return false;
+            if (!Modifier.isPublic(mod) && !Java.trySetAccessible(method)) return false;
 
             // ignore bridge methods because we'd rather directly call methods that this method
             // is bridging (and such methods are by definition always available.)

core/src/main/java/org/jruby/util/ShellLauncher.java
@@ -64,6 +64,7 @@ import org.jruby.RubyInstanceConfig;
 import org.jruby.RubyModule;
 import org.jruby.RubyString;
 import jnr.posix.util.Platform;
+import org.jruby.javasupport.Java;
 import org.jruby.runtime.Helpers;
 import org.jruby.ext.rbconfig.RbConfigLibrary;
 import org.jruby.runtime.ThreadContext;
@@ -661,7 +662,7 @@ public class ShellLauncher {
         try {
             up = Class.forName("java.lang.UNIXProcess");
             pid = up.getDeclaredField("pid");
-            pid.setAccessible(true);
+            Java.trySetAccessible(pid);
         } catch (Exception e) {
             // ignore and try windows version
         }
@@ -673,7 +674,7 @@ public class ShellLauncher {
         try {
             pi = Class.forName("java.lang.ProcessImpl");
             handle = pi.getDeclaredField("handle");
-            handle.setAccessible(true);
+            Java.trySetAccessible(pid);
         } catch (Exception e) {
             // ignore and use hashcode
         }

core/src/main/java/org/jruby/util/unsafe/UnsafeHolder.java
@@ -27,8 +27,9 @@
 
 package org.jruby.util.unsafe;
 
+import org.jruby.javasupport.Java;
+
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 
 public final class UnsafeHolder {
     
@@ -43,7 +44,7 @@ public final class UnsafeHolder {
         try {
             Class unsafeClass = Class.forName("sun.misc.Unsafe");
             Field f = unsafeClass.getDeclaredField("theUnsafe");
-            f.setAccessible(true);
+            Java.trySetAccessible(f);
             return (sun.misc.Unsafe) f.get(null);
         } catch (Exception e) {
             return null;

commit 33e821eeb7e9e80d5d43b347bb9d52123baa445b

Properly fall back if can't setAccessible
Changes in e8dbe9c26 broke this fallback by silencing errors from
the old setAccessible (as part of becoming more module-friendly).
This patch checks if the setAccessible was successful and returns
null for any fields that could not be made accessible. This in
turn allows the USABLE fallback to properly reflect actual
usability.

core/src/main/java/org/jruby/util/io/SeekableByteChannelImpl.java
@@ -149,15 +149,17 @@ final class SeekableByteChannelImpl extends AbstractInterruptibleChannel
     private static Field accessibleField(final String name) {
         try {
             Field field = ByteArrayInputStream.class.getDeclaredField(name);
-            Java.trySetAccessible(field);
-            return field;
+            if (Java.trySetAccessible(field)) {
+                return field;
+            }
         }
         catch (NoSuchFieldException ex) {
-            return null; // should never happen
+            // should never happen
         }
         catch (SecurityException ex) {
-            return null;
         }
+
+        return null;
     }
 
 }