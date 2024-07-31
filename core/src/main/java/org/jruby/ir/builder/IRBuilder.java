19ac874c28abab1a43db423b658be134b5794de2
Charles Oliver Nutter <headius@headius.com>
2024-07-31 11:48:35 -0500

    Eliminate dynscope for leaf blocks with break/return
    This is a patch to eliminate the need to push a new DynamicScope
    for leaf blocks that have non-local flow control (break or return)
    by using the self block's binding's DynamicScope to find the
    jump target.

--- core/src/main/java/org/jruby/ir/builder/IRBuilder.java
+++ core/src/main/java/org/jruby/ir/builder/IRBuilder.java
@@ -311,7 +311,7 @@ public abstract class IRBuilder<U, V, W, X, Y, Z> {
     }
 
     private boolean computeNeedsDynamicScopeFlag() {
-        return scope.hasNonLocalReturns() ||
+        return //scope.hasNonLocalReturns() ||
                 scope.canCaptureCallersBinding() ||
                 scope.canReceiveNonlocalReturns() ||
                 flags.contains(BINDING_HAS_ESCAPED);
