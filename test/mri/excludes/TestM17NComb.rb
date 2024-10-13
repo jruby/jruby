8b3901b5e3fc46f4553cecf3a7dd03f80293b631
Charles Oliver Nutter <headius@headius.com>
2024-10-13 11:42:03 -0500

    Omit this test again on indy runs
    Will investigate on a branch and fix the issue.

--- test/mri/excludes/TestM17NComb.rb
+++ test/mri/excludes/TestM17NComb.rb
@@ -3,3 +3,6 @@ exclude :test_str_intern, "needs investigation"
 exclude :test_str_count, "does not raise compatibility error"
 exclude :test_str_crypt_nonstrict, "#crypt failing: Errno::EINVAL: Invalid argument"
 exclude :test_str_smart_chomp, "see jruby/jruby#3692"
+if org.jruby.util.cli.Options::COMPILE_INVOKEDYNAMIC.load
+  exclude :test_str_squeeze, "fails sometimes with invokedynamic enabled"
+end
