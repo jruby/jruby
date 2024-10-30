2e57d7a8bfd9e06c918b331bb2b579d201ad7433
Thomas E. Enebo <tom.enebo@gmail.com>
2024-10-30 15:02:07 -0500

    add spacing for new hash inspect behavior

--- spec/java_integration/types/map_spec.rb
+++ spec/java_integration/types/map_spec.rb
@@ -176,10 +176,10 @@ describe "a java.util.Map instance" do
     h2.put("b", 254); h2.put("c", 300)
     test_equal({"a"=>100, "b"=>254, "c"=>300}, h1.ruby_merge(h2))
     test_equal({"a"=>100, "b"=>454, "c"=>300}, h1.ruby_merge(h2) { |k, o, n| o+n })
-    expect( h1.inspect ).to include "{\"a\"=>100, \"b\"=>200}"
+    expect( h1.inspect ).to include "{\"a\" => 100, \"b\" => 200}"
 
     h1.merge!(h2) { |k, o, n| o }
-    test_equal('#<Java::JavaUtil::LinkedHashMap: {"a"=>100, "b"=>200, "c"=>300}>', h1.inspect)
+    test_equal('#<Java::JavaUtil::LinkedHashMap: {"a" => 100, "b" => 200, "c" => 300}>', h1.inspect)
     test_equal(Java::JavaUtil::LinkedHashMap, h1.class)
 
     h.clear
@@ -218,7 +218,7 @@ describe "a java.util.Map instance" do
 
     h2 = {"b"=>254, "c"=>300}
     test_equal({"a"=>100, "b"=>200, "c"=>300}, h.update(h2) { |k, o, n| o })
-    expect( h.inspect ).to include "{\"a\"=>100, \"b\"=>200, \"c\"=>300}"
+    expect( h.inspect ).to include "{\"a\" => 100, \"b\" => 200, \"c\" => 300}"
     test_equal(Java::JavaUtil::LinkedHashMap, h.class)
     test_equal([100, 200], h.values_at("a", "b"))
     test_equal([100, 200, nil], h.values_at("a", "b", "z"))
