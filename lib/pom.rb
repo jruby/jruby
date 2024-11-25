6fb687c396e3f27c63873739d0adf3875f0ccfce
kares <self@kares.org>
2024-11-25 11:16:49 +0100

    [deps] bump jruby-openssl to 0.15.2

--- lib/pom.rb
+++ lib/pom.rb
@@ -58,7 +58,7 @@ default_gems = [
     ['irb', '1.4.2'],
     ['jar-dependencies', '0.4.1'],
     ['jruby-readline', '1.3.7'],
-    ['jruby-openssl', '0.15.0'],
+    ['jruby-openssl', '0.15.2'],
     ['json', '2.7.1'],
     ['logger', '1.5.1'],
     ['mutex_m', '0.1.1'],
