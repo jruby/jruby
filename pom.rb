ebd194f99382899d76d9bb8334671aec41cec559
Charles Oliver Nutter <headius@headius.com>
2024-12-10 23:41:06 -0600

    Update to jruby-maven-plugins 3.0.4
    Various small fixes, but notably File.exists? replaced so it works
    with JRuby 10.

--- pom.rb
+++ pom.rb
@@ -63,7 +63,7 @@ project 'JRuby', 'https://github.com/jruby/jruby' do
               'github.global.server' => 'github',
               'polyglot.dump.pom' => 'pom.xml',
               'polyglot.dump.readonly' => 'true',
-              'jruby.plugins.version' => '3.0.2',
+              'jruby.plugins.version' => '3.0.4',
 
               # versions for default gems with bin executables
               # used in ./lib/pom.rb and ./maven/jruby-stdlib/pom.rb
