ae13e6e2ce2a99fd629d98769249745e5394a983
Thomas E. Enebo <tom.enebo@gmail.com>
2024-12-13 16:46:13 -0600

    New Warn#warningDeprecated and update BigDecimal to use it

--- core/src/main/java/org/jruby/api/Warn.java
+++ core/src/main/java/org/jruby/api/Warn.java
@@ -6,4 +6,8 @@ public class Warn {
     public static void warn(ThreadContext context, String message) {
         context.runtime.getWarnings().warn(message);
     }
+
+    public static void warningDeprecated(ThreadContext context, String message) {
+        context.runtime.getWarnings().warningDeprecated(message);
+    }
 }
