f0ddc9a8ffdf05714614c20c4e6aa5907e03d08b
Charles Oliver Nutter <headius@headius.com>
2025-01-15 17:20:28 -0600

    Update to jcodings 1.0.59
    Issues fixed:
    
    * https://github.com/jruby/jcodings/pull/61
    * https://github.com/jruby/jcodings/pull/62
    * https://github.com/jruby/jcodings/issues/60

--- core/pom.rb
+++ core/pom.rb
@@ -52,7 +52,7 @@ project 'JRuby Base' do
   jar 'com.github.jnr:jffi:${jffi.version}:native'
 
   jar 'org.jruby.joni:joni:2.2.1'
-  jar 'org.jruby.jcodings:jcodings:1.0.58'
+  jar 'org.jruby.jcodings:jcodings:1.0.59'
   jar 'org.jruby:dirgra:0.3'
 
   jar 'com.headius:invokebinder:1.13'
