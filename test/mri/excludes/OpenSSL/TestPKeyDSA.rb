7db1724a7b92d3968f02c08eaaf602424828dae0
kares <self@kares.org>
2025-01-19 17:49:01 +0100

    [test] review OpenSSL tags due latest jruby-openssl

--- test/mri/excludes/OpenSSL/TestPKeyDSA.rb
+++ test/mri/excludes/OpenSSL/TestPKeyDSA.rb
@@ -1,11 +1,4 @@
 exclude :test_export_password_length, 'needs investigation'
-exclude :test_new, 'needs investigation'
 exclude :test_new_break, 'needs investigation'
-exclude :test_private, 'needs investigation'
-exclude :test_read_DSAPublicKey_pem, 'needs investigation'
-exclude :test_read_private_key_pem_pw, 'needs investigation'
-exclude :test_read_public_key_der, 'needs investigation'
-exclude :test_read_public_key_pem, 'needs investigation'
-exclude :test_sys_sign_verify, 'needs investigation'
-exclude :"test_PUBKEY", "work in progress"
-exclude :"test_sign_verify_raw", "work in progress"
+exclude :test_sign_verify_raw, "TODO: sign_raw not implemented"
+exclude :test_dup, 'passes except for setting (invalid) `key2.p + 1` as validation happens early'
