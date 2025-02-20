3b00b957f2e910573259942774998a2421451017
Charles Oliver Nutter <headius@headius.com>
2025-02-20 08:35:19 -0600

    Update stringio to 3.1.5
    This updates stringio to 3.1.5, which includes all the fixes from
    ruby/stringio#116 and the regression ruby/stringio#119 fixed by
    ruby/stringio#121. All CRuby tests and ruby/spec specs for stringio
    are now green.
    
    Tests are from v3.1.5 of the ruby/stringio repo.

--- lib/pom.rb
+++ lib/pom.rb
@@ -97,7 +97,7 @@ default_gems = [
     # ['set', '1.0.2'],
     ['shellwords', '0.1.0'],
     ['singleton', '0.1.1'],
-    ['stringio', '3.1.2'],
+    ['stringio', '3.1.5'],
     ['strscan', '3.1.0'],
     ['subspawn', '0.1.1'], # has 3 transitive deps:
       ['subspawn-posix', '0.1.1'],
