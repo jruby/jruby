0d2732b19c7bfd7cccd5bbeb06d5682de9fc1cfd
Charles Oliver Nutter <headius@headius.com>
2025-02-20 08:36:49 -0600

    Remove all stringio tags and excludes

--- test/mri/excludes/TestStringIO.rb
+++
@@ -1,6 +0,0 @@
-exclude :test_each, "needs investigation"
-exclude :test_gets_chomp, "needs investigation"
-exclude :test_gets_chomp_eol, "needs investigation"
-exclude :test_overflow, "unusual subprocess test trying to overflow some value"
-exclude :test_read_nonblock_no_exceptions, "temporary until StringIO ext does manual arity-checking (ruby/stringio#48)"
-exclude :test_write_integer_overflow, "JVM does not support > 32bit signed array offsets, so our StringIO cannot either"
