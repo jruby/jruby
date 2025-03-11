8e6ca502d178a2bc9161935235096da1dd387df7
Charles Oliver Nutter <headius@headius.com>
2025-03-11 18:50:32 -0500

    Add excludes to get stdlib tests running
    These were hangs on macos m1 and may free up the suite to run to
    completion on linux.

--- test/mri/excludes/TestIO_Console.rb
+++ test/mri/excludes/TestIO_Console.rb
@@ -1,1 +1,4 @@
+exclude :test_getch_timeout, 'hangs on macos m1'
 exclude :test_getpass, 'not portable'
+exclude :test_raw_minchar, 'hangs on macos m1'
+exclude :test_raw_timeout, 'hangs on macos m1'
