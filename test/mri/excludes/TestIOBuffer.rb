009484d208509dd2f170002e094513225765560b
Charles Oliver Nutter <headius@headius.com>
2025-03-25 15:43:03 -0500

    Exclude another IO::Buffer memory-mapping test
    See https://github.com/jruby/jruby/issues/8714

--- test/mri/excludes/TestIOBuffer.rb
+++ test/mri/excludes/TestIOBuffer.rb
@@ -1,3 +1,5 @@
-exclude :test_file_mapped, ""
+exclude :test_file_mapped, "open files can't be memory-mapped due to jnr-enxio (https://github.com/jruby/jruby/issues/8714)"
 exclude :test_resize_zero_external, ""
 exclude :test_string_mapped_buffer_locked, ""
+exclude :test_private, "open files can't be memory-mapped due to jnr-enxio (https://github.com/jruby/jruby/issues/8714)"
+
