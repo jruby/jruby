919cac14d3cf86199986b334ac38ce9281d29486
Charles Oliver Nutter <headius@headius.com>
2025-03-26 14:14:10 -0500

    Exclude test dependent on CRuby call framing

--- test/mri/excludes/TestProc.rb
+++ test/mri/excludes/TestProc.rb
@@ -1,4 +1,5 @@
 exclude :test_attr_source_location, "needs investigation"
+exclude :test_binding_error_unless_ruby_frame, "appears to rely on CRuby framing logic"
 exclude :test_binding_source_location, "binding source location only looks at interpreter backtrace (#6163)"
 exclude :test_dup_clone, "needs investigation"
 exclude :test_lambda_warning_pass_block, "work in progress"
