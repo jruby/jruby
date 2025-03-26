fcd2ffc7f05b6b47baa8b2e3cd7faf30a5669481
Charles Oliver Nutter <headius@headius.com>
2025-03-26 15:59:26 -0500

    Update comments for a couple Range excludes

--- test/mri/excludes/TestRange.rb
+++ test/mri/excludes/TestRange.rb
@@ -1,3 +1,3 @@
-exclude :test_bsearch_for_other_numerics, "needs investigation"
-exclude :test_comparison_when_recursive, "needs investigation"
+exclude :test_bsearch_for_other_numerics, "should raise TypeError"
+exclude :test_comparison_when_recursive, "stack overflow"
 exclude :test_uninitialized_range, "Unsupported partial initialization of objects"
