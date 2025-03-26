23c7f2393d4d06522ff9bd353cfe8e31bce9b6bf
Charles Oliver Nutter <headius@headius.com>
2025-03-25 20:33:37 -0500

    Exclude one Kernel#system test that uses each_object

--- test/mri/excludes/TestSystem.rb
+++ test/mri/excludes/TestSystem.rb
@@ -2,3 +2,4 @@ exclude :test_fallback_to_sh, "appears to fail on Linux but work on OS X. GH #19
 exclude :test_system, "needs investigation"
 exclude :test_system_exception, "unfinished in initial 2.6 work, #6161"
 exclude :test_system_exception_nonascii, "unfinished in initial 2.6 work, #6161"
+exclude :test_system_closed, "uses ObjectSpace to check if IO gets closed"
