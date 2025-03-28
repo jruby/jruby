49efafce0ad64587fcd3e15f5c8a012802931d39
Charles Oliver Nutter <headius@headius.com>
2025-03-28 14:25:30 -0500

    Exclude more CRuby-specific GC tests

--- test/mri/excludes/TestStruct/TopStruct.rb
+++ test/mri/excludes/TestStruct/TopStruct.rb
@@ -1,6 +1,8 @@
 exclude :test_arity, "work in progress"
 exclude :test_attrset_id, "work in progress"
 exclude :test_keyword_args_warning, "work in progress"
+exclude :test_larger_than_largest_pool, "uses internal GC constants"
+exclude :test_named_structs_are_not_rooted, "no working assert_no_memory_leak method"
 exclude :test_new_dupilicate, 'needs investigation #4303'
 exclude :test_nonascii, ''
 exclude :test_parameters, "work in progress"
