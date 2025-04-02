08a53b7e1178f3e0bbd57e2cfc8fdcea4011847e
Charles Oliver Nutter <headius@headius.com>
2025-04-02 16:59:33 -0500

    Add some excludes for hanging tests

---
+++ test/mri/excludes/TestFiberScheduler.rb
@@ -0,0 +1,1 @@
+exclude :test_condition_variable, "hangs on macos M1"
--- test/mri/excludes/TestIO_Console.rb
+++ test/mri/excludes/TestIO_Console.rb
@@ -1,4 +1,5 @@
 exclude :"test_raw!", "work in progress"
+exclude :test_cooked, 'hangs on macos m1'
 exclude :test_echo, "work in progress"
 exclude :test_getch_timeout, 'hangs on macos m1'
 exclude :test_getpass, 'not portable'
@@ -8,6 +9,7 @@ exclude :test_ioflush2, "work in progress"
 exclude :test_noecho, "work in progress"
 exclude :test_noecho2, "work in progress"
 exclude :test_oflush, "work in progress"
+exclude :test_raw, 'hangs on macos m1'
 exclude :test_raw_minchar, 'hangs on macos m1'
 exclude :test_raw_timeout, 'hangs on macos m1'
 exclude :test_setecho, "work in progress"
