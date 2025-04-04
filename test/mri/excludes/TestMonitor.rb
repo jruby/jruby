29067090d37ddfad1f7758991f9edc1a6e3210d6
Charles Oliver Nutter <headius@headius.com>
2025-04-04 17:32:49 -0500

    Update monitor test excludes

--- test/mri/excludes/TestMonitor.rb
+++ test/mri/excludes/TestMonitor.rb
@@ -1,3 +1,1 @@
-exclude :test_killed_thread_in_synchronize, ''
 exclude :test_timedwait, "needs investigation"
-exclude :test_wait_interruption, "may be sporadic, unfinished in initial 2.6 work, #6161"
