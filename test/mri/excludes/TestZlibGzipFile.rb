43069fc83109759fc3134f801b0cb709f6d30cb8
Charles Oliver Nutter <headius@headius.com>
2025-04-05 02:13:33 -0500

    Update zlib test excludes

--- test/mri/excludes/TestZlibGzipFile.rb
+++ test/mri/excludes/TestZlibGzipFile.rb
@@ -1,3 +1,3 @@
-exclude :test_mtime, "flaky"
-exclude :test_path_tmpfile, "work in progress"
-exclude :test_zero_mtime, "work in progress"
+# exclude :test_mtime, "flaky"
+# exclude :test_path_tmpfile, "work in progress"
+# exclude :test_zero_mtime, "work in progress"
--- test/mri/excludes/TestZlibGzipReader.rb
+++ test/mri/excludes/TestZlibGzipReader.rb
@@ -1,1 +1,4 @@
 exclude :test_encoding, "needs investigation"
+exclude :test_readpartial, "needs investigation"
+exclude :test_gzfile_read_size_boundary, "needs investigation"
+exclude :test_read, "needs investigation"
