3fa1571787066e976b302c4c6c423f5ff475218f
Charles Oliver Nutter <headius@headius.com>
2025-04-05 03:44:43 -0500

    For unlinking now to eliminate spurious failure
    The currently-included version of tempfile outputs a "removing..."
    message when $DEBUG is true and a file is left to finalize and
    unlink lazily. These specs never cleaned up the temp file properly,
    and as a result the $DEBUG mode spec will sometimes capture that
    message.
    
    The change here forces the files to be cleaned up immediately by
    passing "true" for "unlink_now", which should avoid emitting the
    lazy removal warning and make the specs more reliable.

--- spec/regression/GH-3934_nameerror_formatting_does_not_warn_or_raise_spec.rb
+++ spec/regression/GH-3934_nameerror_formatting_does_not_warn_or_raise_spec.rb
@@ -20,7 +20,7 @@ describe "NameError created internally using a format string" do
       io.rewind
       expect(io.read).to eq ""
     ensure
-      io.close rescue nil
+      io.close(true) rescue nil
       $VERBOSE = old_verbose
       $stderr.reopen(old_stderr)
     end
@@ -44,7 +44,7 @@ describe "NameError created internally using a format string" do
       io.rewind
       expect(io.read).to eq ""
     ensure
-      io.close rescue nil
+      io.close(true) rescue nil
       $DEBUG = old_debug
       $stderr.reopen(old_stderr)
     end
