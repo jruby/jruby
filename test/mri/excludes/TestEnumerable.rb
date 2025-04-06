3ac9eb95ba0b28d7eed9f41eb748190119c9f6bf
Charles Oliver Nutter <headius@headius.com>
2025-04-06 16:30:02 -0500

    Update test_enum excludes

--- test/mri/excludes/TestEnumerable.rb
+++ test/mri/excludes/TestEnumerable.rb
@@ -1,2 +1,4 @@
 exclude :test_callcc, "Continuations are not supported"
 exclude :test_first, "works - but output is not exactly 'unexpected break' as in MRI"
+exclude :test_ruby_svar, "needs investigation"
+exclude :test_sum, "missing delegate require"
