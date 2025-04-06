82e0eb8e34b3197636e4db4c4e9d65ef055b36c9
Charles Oliver Nutter <headius@headius.com>
2025-04-06 16:53:45 -0500

    Update test_flip exclude description

--- test/mri/excludes/TestFlip.rb
+++ test/mri/excludes/TestFlip.rb
@@ -1,2 +1,2 @@
 exclude :test_input_line_number_range, "needs investigation"
-exclude :test_shared_thread, "needs investigation"
+exclude :test_shared_thread, "need to isolate flip-flop state variable across threads"
