307f79cc822bb389a76d971bfba17db88406391f
Charles Oliver Nutter <headius@headius.com>
2025-04-06 17:35:28 -0500

    Update test_lambda excludes
    Two excludes removed are passing, but the issue filed when they
    were excluded still shows some behavior differences from CRuby.
    
    See https://github.com/jruby/jruby/issues/7707

--- test/mri/excludes/TestLambdaParameters.rb
+++ test/mri/excludes/TestLambdaParameters.rb
@@ -1,5 +1,3 @@
-exclude :test_brace_lambda_source_location, "broken in compiler (#458)"
-exclude :test_create_non_lambda_for_proc_one_level, "https://github.com/jruby/jruby/issues/7707"
-exclude :test_create_non_lambda_for_proc_two_levels, "https://github.com/jruby/jruby/issues/7707"
-exclude :test_do_lambda_source_location, "broken in compiler (#458)"
-exclude :test_location_on_error, "broken in compiler (#458)"
+exclude :test_brace_lambda_source_location, "line number is off by two"
+exclude :test_do_lambda_source_location, "line number is off by two"
+exclude :test_location_on_error, "line number is off by five"
