aec1aa0c607d67840e172833e15efd1234a3f7f2
Charles Oliver Nutter <headius@headius.com>
2025-04-06 16:18:13 -0500

    All test_continuation passing (somehow?!)

--- test/mri/excludes/TestContinuation.rb
+++
@@ -1,6 +0,0 @@
-exclude :test_call, "needs investigation"
-exclude :test_check_localvars, "needs investigation"
-exclude :test_create, "Continuations are not supported"
-exclude :test_error, "Continuations are not supported"
-exclude :test_tracing_with_set_trace_func, "needs investigation"
-exclude :test_tracing_with_thread_set_trace_func, "needs investigation"
