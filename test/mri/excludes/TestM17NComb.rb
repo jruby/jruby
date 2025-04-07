8776a478c88b91f715c165a5f3a809ae319a262b
Charles Oliver Nutter <headius@headius.com>
2025-04-07 01:55:56 -0500

    Update test_m17n_comb excludes

--- test/mri/excludes/TestM17NComb.rb
+++ test/mri/excludes/TestM17NComb.rb
@@ -1,8 +1,3 @@
-exclude :test_bug11486, "fix from https://bugs.ruby-lang.org/issues/11486 did not appear to fix it for us"
 exclude :test_str_count, "does not raise compatibility error"
-exclude :test_str_crypt_nonstrict, "#crypt failing: Errno::EINVAL: Invalid argument"
-exclude :test_str_intern, "needs investigation"
-exclude :test_str_smart_chomp, "see jruby/jruby#3692"
-if org.jruby.util.cli.Options::COMPILE_INVOKEDYNAMIC.load
-  exclude :test_str_squeeze, "fails sometimes with invokedynamic enabled"
-end
+exclude :test_str_intern, "encoding of MBC does not matchuby#3692"
+exclude :test_str_smart_chomp, "needs investigation"
