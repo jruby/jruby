7d98678f218462bd23f23d21295af6bfbb907068
Charles Oliver Nutter <headius@headius.com>
2025-04-07 08:00:57 -0500

    Confirmed this is still failing intermittently
    https://github.com/jruby/jruby/issues/7803

--- test/mri/excludes/TestM17NComb.rb
+++ test/mri/excludes/TestM17NComb.rb
@@ -1,3 +1,6 @@
 exclude :test_str_count, "does not raise compatibility error"
 exclude :test_str_intern, "encoding of MBC does not matchuby#3692"
 exclude :test_str_smart_chomp, "needs investigation"
+if org.jruby.util.cli.Options::COMPILE_INVOKEDYNAMIC.load
+  exclude :test_str_squeeze, "fails sometimes with invokedynamic enabled, https://github.com/jruby/jruby/issues/7803"
+end
