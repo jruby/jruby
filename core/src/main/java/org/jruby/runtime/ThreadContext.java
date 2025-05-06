fd1386d8fe8a72cf8e368368650c63fa8c158c97
Ilmar S. Habibulin <161701313+ngr-ilmarh@users.noreply.github.com>
2025-05-06 15:07:45 +0300

    Refactor (new fix)

--- core/src/main/java/org/jruby/runtime/ThreadContext.java
+++ core/src/main/java/org/jruby/runtime/ThreadContext.java
@@ -908,7 +908,7 @@ public final class ThreadContext {
      * @param length the length of the trace
      * @return an Array with the backtrace locations
      */
-    public IRubyObject createCallerLocations(int level, Integer length, Stream<StackWalker.StackFrame> stackStream) {
+    public IRubyObject createCallerLocations(int level, int length, Stream<StackWalker.StackFrame> stackStream) {
         runtime.incrementCallerCount();
 
         RubyStackTraceElement[] fullTrace = getPartialTrace(level, length, stackStream);
@@ -924,7 +924,7 @@ public final class ThreadContext {
     }
 
     /**
-     * Like {@link #createCallerLocations(int, Integer, Stream)} but accepts a lambda to yield each location and yields
+     * Like {@link #createCallerLocations(int, int, Stream)} but accepts a lambda to yield each location and yields
      * all stack elements until the loop ends or is broken early.
      *
      * @param stackStream the stream of StackFrame objects from JVM
@@ -934,16 +934,9 @@ public final class ThreadContext {
         eachPartialTrace(stackStream, (elt) -> consumer.accept(RubyThread.Location.newLocation(runtime, elt)));
     }
 
-    private RubyStackTraceElement[] getPartialTrace(int level, Integer length, Stream<StackWalker.StackFrame> stackStream) {
-	int len = 0;
-        if (length != null) {
-        	if (length == 0) {
-			return RubyStackTraceElement.EMPTY_ARRAY;
-		} else {
-			len = length;
-		}
-	}
-        return TraceType.Gather.CALLER.getBacktraceData(this, stackStream).getPartialBacktrace(runtime, level + len);
+    private RubyStackTraceElement[] getPartialTrace(int level, int length, Stream<StackWalker.StackFrame> stackStream) {
+         if (length == 0) return RubyStackTraceElement.EMPTY_ARRAY;
+         return TraceType.Gather.CALLER.getBacktraceData(this, stackStream).getPartialBacktrace(runtime, level + length);
     }
 
     private void eachPartialTrace(Stream<StackWalker.StackFrame> stackStream, Consumer<RubyStackTraceElement> consumer) {
@@ -954,7 +947,7 @@ public final class ThreadContext {
         return TraceType.Gather.WARN.getBacktraceData(this, stackStream).getPartialBacktrace(runtime, level + 1);
     }
 
-    private static int safeLength(int level, Integer length, RubyStackTraceElement[] trace) {
+    private static int safeLength(int level, int length, RubyStackTraceElement[] trace) {
         final int baseLength = trace.length - level;
         return Math.min(length, baseLength);
     }
