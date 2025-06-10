35946f81ab717e6cd412412c6cf67a2c6c92a9c6
Thomas E. Enebo <tom.enebo@gmail.com>
2025-06-10 09:51:12 -0500

    Update for lack of S3 and new maven publishing

--- tool/release.sh
+++ tool/release.sh
@@ -28,7 +28,7 @@ rm -rf release
 git clone $REPO release
 cd release
 pwd
-mvn clean deploy -Psonatype-oss-release -Prelease
+mvn clean deploy -Prelease
 jruby -S rake post_process_artifacts
 
 cd release
@@ -39,28 +39,6 @@ mv jruby_windows-x32_${WINDOWS_VERSION}.exe.md5 jruby_windows_${WINDOWS_VERSION}
 mv jruby_windows-x32_${WINDOWS_VERSION}.exe.sha1 jruby_windows_${WINDOWS_VERSION}.exe.sha1
 mv jruby_windows-x32_${WINDOWS_VERSION}.exe.sha256 jruby_windows_${WINDOWS_VERSION}.exe.sha256
 mv jruby_windows-x32_${WINDOWS_VERSION}.exe.sha512 jruby_windows_${WINDOWS_VERSION}.exe.sha512
-
-pwd
-DOWNLOADS=s3://jruby.org/downloads
-VERSION=$JRUBY_VERSION
-
-for FILE in jruby-bin-${VERSION}.tar.gz jruby-bin-${VERSION}.zip jruby-complete-${VERSION}.jar jruby-jars-${VERSION}.gem jruby-src-${VERSION}.tar.gz jruby-src-${VERSION}.zip jruby_windows_${WINDOWS_VERSION}.exe jruby_windows_x64_${WINDOWS_VERSION}.exe
-do
-
-    echo s3cmd sync --verbose ${FILE} ${DOWNLOADS}/${VERSION}/${FILE}
-    s3cmd sync --verbose ${FILE} ${DOWNLOADS}/${VERSION}/${FILE}
-    
-    for FINGERPRINT in md5 sha1 sha256 sha512
-    do
-        
-        s3cmd put --verbose ${FILE}.${FINGERPRINT} ${DOWNLOADS}/${VERSION}/${FILE}.${FINGERPRINT}
-        
-    done
-
-done
-
-
-s3cmd setacl ${DOWNLOADS}/${VERSION} --acl-public --recursive
 
 # At this point you should run scripts on platforms to make sure things look ok.
 # FIXME: we should consider running a collection of scripts we can automate here and add to CI.
