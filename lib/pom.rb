c5f01ceffe1029faec432c17121f3b69563366ed
Charles Oliver Nutter <headius@headius.com>
2025-07-07 18:29:25 -0500

    Update strscan to fix number scanning at nonzero offset
    Fixes #8823

--- lib/pom.rb
+++ lib/pom.rb
@@ -98,7 +98,7 @@ default_gems = [
     ['shellwords', '0.1.0'],
     ['singleton', '0.1.1'],
     ['stringio', '3.1.5'],
-    ['strscan', '3.1.0'],
+    ['strscan', '3.1.5'],
     ['subspawn', '0.1.1'], # has 3 transitive deps:
       ['subspawn-posix', '0.1.1'],
       ['ffi-binary-libfixposix', '0.5.1.1'],
