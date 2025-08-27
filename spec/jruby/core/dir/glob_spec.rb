d2d385d6d2aca37ded21da86862775b9641c6308
Charles Oliver Nutter <headius@headius.com>
2025-08-27 12:47:44 -0500

    Add specs for glob with filesystem-based schemes
    This tests uri:classpath: and file: URIs in Dir.glob against an
    on-filesystem path. See jruby/jruby#8973.

--- spec/jruby/core/dir/glob_spec.rb
+++ spec/jruby/core/dir/glob_spec.rb
@@ -17,14 +17,65 @@ describe "Dir.glob" do
   it 'can glob directories with URI special chars in it (GH-2264)' do
     expect(Dir.glob("#{dir_name}/**/*").size).to eq 1
   end
-end
 
-describe "Dir.glob" do
   # We have common option processing code and a logic mistake made
   # us process argv[2] as both a kwarg then try to convert it to an
   # integer.
   it "does not think the third arg should be an integer when it is kwargs" do
     expect { Dir["*", "*", base: "."] }.not_to raise_error
+  end
+
+  describe "with a uri:classloader: scheme" do
+    before :all do
+      # FIXME: unsanitary because we can't clear entries from $CLASSPATH
+      $CLASSPATH << __dir__
+    end
+
+    it "can find a specific file" do
+      test1_files = Dir.glob("uri:classloader:/fixtures/test1.txt")
+      expect(test1_files[0]).to eq "uri:classloader:/fixtures/test1.txt"
+      expect(File.read(test1_files[0])).to eq "test1"
+    end
+
+    it "can find a set of files for a wildcard filename" do
+      test_txt_files = Dir.glob("uri:classloader:/fixtures/test*.txt")
+      expect(test_txt_files).to eq %w[test1 test2 test3].map { "uri:classloader:/fixtures/#{_1}.txt" }
+      test_txt_files.each { expect(File.read(_1)).to eq File.basename(_1).split(".").first }
+
+      test_txt_files = Dir.glob("uri:classloader:/fixtures/test?.txt")
+      expect(test_txt_files).to eq %w[test1 test2 test3].map { "uri:classloader:/fixtures/#{_1}.txt" }
+      test_txt_files.each { expect(File.read(_1)).to eq File.basename(_1).split(".").first }
+    end
+
+    it "can find a specific file in a double star path" do
+      test5_files = Dir.glob("uri:classloader:/fixtures/**/test4.txt")
+      expect(test5_files[0]).to eq "uri:classloader:/fixtures/testdir/test4.txt"
+      expect(File.read(test5_files[0])).to eq "test4"
+    end
+  end
+
+  describe "with a file: scheme" do
+    it "can find a specific file" do
+      test1_files = Dir.glob("file:#{__dir__}/fixtures/test1.txt")
+      expect(test1_files[0]).to eq "file:#{__dir__}/fixtures/test1.txt"
+      expect(File.read(test1_files[0])).to eq "test1"
+    end
+
+    it "can find a set of files for a wildcard filename" do
+      test_txt_files = Dir.glob("file:#{__dir__}/fixtures/test*.txt")
+      expect(test_txt_files).to eq %w[test1 test2 test3].map { "file:#{__dir__}/fixtures/#{_1}.txt" }
+      test_txt_files.each { expect(File.read(_1)).to eq File.basename(_1).split(".").first }
+
+      test_txt_files = Dir.glob("file:#{__dir__}/fixtures/test?.txt")
+      expect(test_txt_files).to eq %w[test1 test2 test3].map { "file:#{__dir__}/fixtures/#{_1}.txt" }
+      test_txt_files.each { expect(File.read(_1)).to eq File.basename(_1).split(".").first }
+    end
+
+    it "can find a specific file in a double star path" do
+      test5_files = Dir.glob("file:#{__dir__}/fixtures/**/test4.txt")
+      expect(test5_files[0]).to eq "file:#{__dir__}/fixtures/testdir/test4.txt"
+      expect(File.read(test5_files[0])).to eq "test4"
+    end
   end
 end
 
