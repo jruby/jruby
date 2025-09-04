0ce0901c49e9c18cd9e2ba39b8375ef601679ab1
Thomas E. Enebo <tom.enebo@gmail.com>
2025-09-04 14:43:06 -0500

    More String regression specs moved to jruby

--- spec/jruby/core/string/index_spec.rb
+++ spec/jruby/core/string/index_spec.rb
@@ -12,4 +12,32 @@ describe 'String#index given a Regexp and an index past the last character' do
     str = "ßt®íngß — ßt®íngß"
     expect(str.index(/\b/, 18)).to eq(nil)
   end
-end if RUBY_VERSION > "1.9"
+end
+
+
+describe 'JRUBY-6863' do
+  let(:str) do 
+    str = "あいうえおかきくけこ"
+  end
+
+  subject do
+    (+str).slice!(3..-1) # => "えおかきくけこ"
+  end
+
+  it 'String#index without args' do 
+    # See http://jira.codehaus.org/browse/JRUBY-xxxx
+    expect(subject.index(/[^ ]/)).to eq(0)
+  end
+
+  it 'String#index with args' do
+    expect(subject.index(/[^ ]/, 2)).to eq(2)
+  end
+
+  it 'String#rindex without args' do 
+    expect(subject.rindex(/[^ ]/)).to eq(6)
+  end
+
+  it 'String#rindex with args' do 
+    expect(subject.rindex(/[^ ]/, 2)).to eq(2)
+  end
+end
