507a1bb6b70c2ebffe9485b1645cfc360bc50946
Charles Oliver Nutter <headius@headius.com>
2025-11-03 11:28:20 -0600

    Swap single quote for backquote in feature errors

--- core/src/main/ruby/jruby/kernel/gem_prelude.rb
+++ core/src/main/ruby/jruby/kernel/gem_prelude.rb
@@ -4,20 +4,20 @@ begin
 rescue LoadError => e
   raise unless e.path == 'rubygems'
 
-  warn "`RubyGems' were not loaded."
+  warn "'RubyGems' were not loaded."
 else
   require 'bundled_gems'
 
   begin
     require 'did_you_mean'
   rescue LoadError
-    warn "`did_you_mean' was not loaded."
+    warn "'did_you_mean' was not loaded."
   end if defined?(DidYouMean)
 
   begin
     require 'syntax_suggest/core_ext'
   rescue LoadError
-    warn "`syntax_suggest' was not loaded."
+    warn "'syntax_suggest' was not loaded."
   end if defined?(SyntaxSuggest)
 
   # clear RubyGems paths so they can be reinitialized after boot
