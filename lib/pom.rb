7a0b681d599b2c6fb718e606b123e2cbab2a9ba0
Charles Oliver Nutter <headius@headius.com>
2026-01-07 13:36:34 -0600

    Update strscan to 3.1.7
    See https://github.com/jruby/jruby/issues/9147

--- lib/pom.rb
+++ lib/pom.rb
@@ -102,7 +102,7 @@ default_gems = [
   ['shellwords', '0.1.0'],
   ['singleton', '0.1.1'],
   ['stringio', '3.1.5'],
-  ['strscan', '3.1.5'],
+  ['strscan', '3.1.7'],
   ['subspawn', '0.1.1'], # has 3 transitive deps:
   ['subspawn-posix', '0.1.1'],
   ['ffi-binary-libfixposix', '0.5.1.1'],
