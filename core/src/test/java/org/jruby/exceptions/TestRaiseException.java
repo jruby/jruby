fb88ade4a17d84723722b6ca78ee0abe36c67a7a
kares <self@kares.org>
2026-06-15 11:34:41 +0200

    [test] use context.runtime (not bare runtime)

--- core/src/test/java/org/jruby/exceptions/TestRaiseException.java
+++ core/src/test/java/org/jruby/exceptions/TestRaiseException.java
@@ -407,14 +407,16 @@ public class TestRaiseException extends Base {
 
     public void testPreviousRaisedNotConsideredCause() {
         try {
-            runtime.evalScriptlet("raise 'foo'");
+            context.runtime.evalScriptlet("raise 'foo'");
             fail("expected to throw");
         } catch (StandardError ex1) {
             assertNull("ex1 has a cause", ex1.getCause());
+            // clear $! like a Ruby rescue block would, so the next raise doesn't pick it up as cause
+            context.setErrorInfo(context.nil);
         }
 
         try {
-            runtime.evalScriptlet("raise 'bar'");
+            context.runtime.evalScriptlet("raise 'bar'");
             fail("expected to throw");
         } catch (StandardError ex2) {
             assertNull("ex2 has a cause: " + ex2.getCause(), ex2.getCause());
