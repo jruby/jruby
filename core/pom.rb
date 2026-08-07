2ffa94181517c0ea878bc6f99e0ef95143b62948
Charles Oliver Nutter <headius@headius.com>
2026-08-07 17:38:32 -0500

    Update jnr dependencies for BSD aliases
    Updates FreeBSD type aliases and adds OpenBSD type aliases.
    
    Fixes jruby/jruby#9553

--- core/pom.rb
+++ core/pom.rb
@@ -49,7 +49,7 @@ project 'JRuby Base' do
   jar 'com.github.jnr:jnr-unixsocket:0.39.1', exclusions: ['com.github.jnr:jnr-ffi']
   jar 'com.github.jnr:jnr-posix:3.2.1', exclusions: ['com.github.jnr:jnr-ffi']
   jar 'com.github.jnr:jnr-constants:0.11.0', exclusions: ['com.github.jnr:jnr-ffi']
-  jar 'com.github.jnr:jnr-ffi:2.3.0'
+  jar 'com.github.jnr:jnr-ffi:2.3.1-SNAPSHOT'
   jar 'com.github.jnr:jffi:${jffi.version}'
   jar 'com.github.jnr:jffi:${jffi.version}:native'
 
