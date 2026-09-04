57fca6e358eca8e82c1493c5ae8f58ec08ac0a64
kares <self@kares.org>
2026-09-04 19:18:19 +0200

    [test] untag OpenSSL excludes that are passing

--- test/mri/excludes/OpenSSL/TestPKeyDSA.rb
+++ test/mri/excludes/OpenSSL/TestPKeyDSA.rb
@@ -4,4 +4,3 @@ exclude :test_PUBKEY, "new failure with Ruby 4.0 tests, tested on MacOS, https:/
 exclude :test_dup, 'passes except for setting (invalid) `key2.p + 1` as validation happens early'
 exclude :test_new_break, 'needs investigation'
 exclude :test_new_empty, "PKeyError expected, got DSAError - error class hierarchy mismatch with Ruby 4.0 tests"
-exclude :test_sign_verify_raw, "work in progress"
