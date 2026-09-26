24afbc48e2fee40a835c8bc420245e302efc3292
kares <self@kares.org>
2026-09-26 10:16:55 +0200

    remove added reabyte support in 0.19.2

--- test/mri/excludes/OpenSSL/TestPairLowlevelSocket.rb
+++ test/mri/excludes/OpenSSL/TestPairLowlevelSocket.rb
@@ -1,4 +1,2 @@
 exclude :test_connect_accept_nonblock_no_exception, "SSLError: Cannot change mode after SSL traffic has started"
 exclude :test_gets_chomp, "needs investigation"
-exclude :test_readbyte, "undefined method 'readbyte' for an instance of OpenSSL::SSL::SSLSocket"
-exclude :test_readbyte_eof, "undefined method 'readbyte' for an instance of OpenSSL::SSL::SSLSocket"
